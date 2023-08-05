afed01523371e8ec5f129e179cc2c27ca7d65fbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:32 +0000

    Add package beaker-client/26.4/beaker-client-26.4.tar.gz

---
+++ packages/beaker-client/beaker-client-26.4.tar.gz/beaker-client-26.4/setup.py
@@ -0,0 +1,56 @@
+
+import sys
+import commands
+from glob import glob
+from setuptools import setup, find_packages
+from distutils.command.build import build as _build
+
+def bash_completion_dir():
+    status, output = commands.getstatusoutput('pkg-config --variable completionsdir bash-completion')
+    if status or not output:
+        return '/etc/bash_completion.d'
+    return output.strip()
+
+setup(
+    name='beaker-client',
+    version='26.4',
+    description='Command-line client for interacting with Beaker',
+    author='Red Hat, Inc.',
+    author_email='beaker-devel@lists.fedorahosted.org',
+    url='https://beaker-project.org/',
+
+    install_requires=[
+        'beaker-common',
+        'lxml',
+        'requests',
+        'PrettyTable',
+        'Jinja2',
+    ],
+
+    packages=find_packages('src'),
+    package_dir = {'':'src'},
+
+    package_data = {
+        'bkr.client': ['host-filters/*']
+        },
+    namespace_packages = ['bkr'],
+
+    data_files = [
+        ('/etc/beaker', []),
+        (bash_completion_dir(), ['bash-completion/bkr']),
+    ],
+
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.6',
+        'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+    ],
+
+    entry_points = {
+        'console_scripts': (
+            'bkr = bkr.client.main:main',
+            'beaker-wizard = bkr.client.wizard:main',
+        ),
+    },
+)
