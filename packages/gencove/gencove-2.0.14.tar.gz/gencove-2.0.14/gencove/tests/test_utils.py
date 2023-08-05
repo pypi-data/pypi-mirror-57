611783ea4b20263871cf6df0bc933a671cc0cf6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:48 +0000

    Add package gencove/2.0.14/gencove-2.0.14.tar.gz

---
+++ packages/gencove/gencove-2.0.14.tar.gz/gencove-2.0.14/gencove/tests/test_utils.py
@@ -0,0 +1,20 @@
+"""Tests for utils of Gencove CLI."""
+from click.testing import CliRunner
+
+from gencove.command.upload.utils import upload_file
+
+
+def test_upload_file(mocker):
+    """Sanity check upload function."""
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        with open("foo.txt", "w") as fastq_file:
+            fastq_file.write("AAABBB")
+        mocked_s3_client = mocker.Mock()
+        assert upload_file(
+            mocked_s3_client,
+            "foo.txt",
+            "foo-bucket",
+            object_name="foo-object.txt",
+        )
+        mocked_s3_client.upload_file.assert_called_once()
