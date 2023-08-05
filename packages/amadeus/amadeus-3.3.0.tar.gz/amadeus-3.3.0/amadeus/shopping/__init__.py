1916a6242d6b71cf6f6b885983b909ba16a8a4db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:36 +0000

    Add package amadeus/3.3.0/amadeus-3.3.0.tar.gz

---
+++ packages/amadeus/amadeus-3.3.0.tar.gz/amadeus-3.3.0/amadeus/shopping/__init__.py
@@ -0,0 +1,9 @@
+from ._flight_offers import FlightOffers
+from ._flight_dates import FlightDates
+from ._flight_destinations import FlightDestinations
+from ._hotel_offers import HotelOffers
+from ._hotel_offers_by_hotel import HotelOffersByHotel
+from ._hotel_offer import HotelOffer
+
+__all__ = ['FlightOffers', 'FlightDates', 'FlightDestinations',
+           'HotelOffers', 'HotelOffersByHotel', 'HotelOffer']
