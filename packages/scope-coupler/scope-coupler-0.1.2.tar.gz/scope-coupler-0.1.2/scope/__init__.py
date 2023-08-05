f3b04e19b414912b0f03ca69a3a8315af118c585
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:51 +0000

    Add package scope-coupler/0.1.2/scope-coupler-0.1.2.tar.gz

---
+++ packages/scope-coupler/scope-coupler-0.1.2.tar.gz/scope-coupler-0.1.2/scope/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for SCOPE."""
+
+__author__ = """Paul Gierz"""
+__email__ = "pgierz@awi.de"
+__version__ = "__version__ = '0.1.2'"
+
+from .scope import *
