e2f59e799fd824cd1d1e0a2c965ce19541def68d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:59 +0000

    Add package django-richtextfield/1.5.0/django-richtextfield-1.5.0.tar.gz

---
+++ packages/django-richtextfield/django-richtextfield-1.5.0.tar.gz/django-richtextfield-1.5.0/testproject/testapp/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+from djrichtextfield.models import RichTextField
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    lead = RichTextField(field_settings='mini')
+    content = RichTextField()
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.TextField()
+
+    def __str__(self):
+        return 'Comment on "%s"' % self.post.title
