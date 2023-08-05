482a194c5e4eec5e78f46bf5d606faaec8913f64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:55 +0000

    Add package friendly-traceback/0.0.19/friendly_traceback-0.0.19-py3-none-any.whl

---
+++ packages/friendly-traceback/friendly_traceback-0.0.19-py3-none-any.whl/friendly_traceback/utils.py
@@ -0,0 +1,67 @@
+"""utils.py
+
+A few useful objects which do not naturally fit anywhere else.
+"""
+import tokenize
+import os.path
+
+from io import StringIO
+
+
+class Token:
+    """Token as generated from tokenize.generate_tokens written here in
+       a more convenient form for our purpose.
+    """
+
+    def __init__(self, token):
+        self.type = token[0]
+        self.string = token[1]
+        self.start_line, self.start_col = token[2]
+        self.end_line, self.end_col = token[3]
+        # ignore last parameter which is the logical line
+
+    def __repr__(self):
+        return f"type: {self.type}, string: {self.string}"
+
+
+def collect_tokens(line):
+    """Makes a list of tokens on a line, ignoring spaces and comments"""
+    tokens = []
+    try:
+        for tok in tokenize.generate_tokens(StringIO(line).readline):
+            token = Token(tok)
+            if not token.string.strip():  # ignore spaces
+                continue
+            if token.type == tokenize.COMMENT:
+                break
+            tokens.append(token)
+    except tokenize.TokenError:
+        return []
+    except Exception as e:
+        print("%s raised in utils.collect_tokens" % repr(e))
+
+    return tokens
+
+
+PYTHON = os.path.dirname(tokenize.__file__).lower()
+this_dir = os.path.dirname(__file__)
+FRIENDLY = os.path.abspath(os.path.join(this_dir, "..")).lower()
+TESTS = os.path.join(FRIENDLY, "tests").lower()
+HOME = os.path.expanduser("~").lower()
+
+
+def shorten_path(path):
+    # On windows, the filenames are not case sensitive
+    # and the way Python displays filenames may vary.
+    # To properly compare, we convert everything to lowercase
+    # However, we ensure that the shortened path retains its cases
+    path_lower = path.lower()
+    if path_lower.startswith(TESTS):
+        path = "TESTS:" + path[len(TESTS) :]
+    elif path_lower.startswith(FRIENDLY):
+        path = "FRIENDLY:" + path[len(FRIENDLY) :]
+    elif path_lower.startswith(PYTHON):
+        path = "PYTHON:" + path[len(PYTHON) :]
+    elif path_lower.startswith(HOME):
+        path = "~" + path[len(HOME) :]
+    return path
