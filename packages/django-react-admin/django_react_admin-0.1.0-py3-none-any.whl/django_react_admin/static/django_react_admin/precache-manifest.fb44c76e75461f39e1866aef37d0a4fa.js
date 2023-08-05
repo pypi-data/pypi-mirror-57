3180a1ae8418397abc32cb357fdd4c16554d2e1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:11 +0000

    Add package django-react-admin/0.1.0/django_react_admin-0.1.0-py3-none-any.whl

---
+++ packages/django-react-admin/django_react_admin-0.1.0-py3-none-any.whl/django_react_admin/static/django_react_admin/precache-manifest.fb44c76e75461f39e1866aef37d0a4fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1272388fbe72406f644cd33c2855de55",
+    "url": "/static/django_react_admin/index.html"
+  },
+  {
+    "revision": "130cf60b8c47c95264ca",
+    "url": "/static/django_react_admin/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "e20b2c2ba19741d545ab",
+    "url": "/static/django_react_admin/static/js/2.a5cf5eb4.chunk.js"
+  },
+  {
+    "revision": "130cf60b8c47c95264ca",
+    "url": "/static/django_react_admin/static/js/main.f9fe51be.chunk.js"
+  },
+  {
+    "revision": "8c47087bbc668d06d128",
+    "url": "/static/django_react_admin/static/js/runtime-main.18a24d86.js"
+  }
+]);
