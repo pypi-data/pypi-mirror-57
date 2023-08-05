2e47b3f81b0628abbc82f859fd5bb00973cc8002
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:39 +0000

    Add package idact-gui/0.4.0/idact-gui-0.4.0.tar.gz

---
+++ packages/idact-gui/idact-gui-0.4.0.tar.gz/idact-gui-0.4.0/gui/functionality/main_window.py
@@ -0,0 +1,95 @@
+""" Main module of the GUI application. It composes all the widgets
+into one window.
+
+    See: class:`.AddCluster`, :class:`.RemoveCluster`,
+    :class:`.IdactNotebook`, :class:`.AdjustTimeouts`,
+    :class:`.ManageJobs`
+"""
+import logging
+
+from PyQt5.QtCore import QThreadPool
+from PyQt5.QtWidgets import QMainWindow
+from idact import show_cluster
+from idact.detail.log.logger_provider import LoggerProvider
+
+from gui.functionality.add_cluster import AddCluster
+from gui.functionality.adjust_timeouts import AdjustTimeouts
+from gui.functionality.idact_notebook import IdactNotebook
+from gui.functionality.manage_jobs import ManageJobs
+from gui.functionality.program_info_window import ProgramInfoWindow
+from gui.functionality.help_window import HelpWindow
+from gui.functionality.remove_cluster import RemoveCluster
+from gui.functionality.running_notebooks import RunningNotebooks
+from gui.functionality.show_logs_window import ShowLogsWindow
+from gui.helpers.data_provider import DataProvider
+from gui.helpers.deployments_provider import DeploymentsProvider
+from gui.helpers.ui_loader import UiLoader
+
+
+class MainWindow(QMainWindow):
+    """ Main Window of GUI. It is responsible for displaying a proper window
+    after choosing one from menu.
+    """
+    def __init__(self):
+        self.show_logs_window = ShowLogsWindow()
+        logger_provider = LoggerProvider()
+        logger_provider.set_stream(self.show_logs_window)
+        logger_provider.log_level = logging.INFO
+
+        super(MainWindow, self).__init__()
+        self.ui = UiLoader.load_ui_from_file('main-window.ui', self)
+
+        self.data_provider = DataProvider()
+        self.deployments_provider = DeploymentsProvider()
+        self.program_info_window = ProgramInfoWindow()
+        self.help_window = HelpWindow()
+        self.actions_file_name = None
+        self.threadpool = QThreadPool()
+
+        self.setCentralWidget(IdactNotebook(self.data_provider, self.deployments_provider, self))
+
+        self.ui.deploy_notebook_action.triggered.connect(self.handle_deploy_notebook_action)
+        self.ui.manage_jobs_action.triggered.connect(self.handle_manage_jobs_action)
+        self.ui.running_notebooks_action.triggered.connect(self.handle_running_notebooks_action)
+
+        self.ui.add_cluster_action.triggered.connect(self.handle_add_cluster_action)
+        self.ui.remove_cluster_action.triggered.connect(self.handle_remove_cluster_action)
+        self.ui.edit_configuration_action.triggered.connect(self.handle_edit_configuration_action)
+
+        self.ui.show_logs_action.triggered.connect(self.handle_show_logs_action)
+
+        self.ui.see_help_action.triggered.connect(self.handle_see_help_action)
+        self.ui.about_the_program_action.triggered.connect(self.handle_about_the_program_action)
+
+    def handle_deploy_notebook_action(self):
+        self.setCentralWidget(IdactNotebook(self.data_provider, self.deployments_provider, self))
+
+    def handle_manage_jobs_action(self):
+        self.setCentralWidget(ManageJobs(self.data_provider, self))
+
+    def handle_running_notebooks_action(self):
+        self.setCentralWidget(RunningNotebooks(self.data_provider, self.deployments_provider, self))
+
+    def handle_add_cluster_action(self):
+        self.setCentralWidget(AddCluster(self.data_provider, self))
+
+    def handle_remove_cluster_action(self):
+        self.setCentralWidget(RemoveCluster(self.data_provider, self))
+
+    def handle_edit_configuration_action(self):
+        self.setCentralWidget(AdjustTimeouts(self.data_provider, self))
+
+    def handle_show_logs_action(self):
+        self.show_logs_window.show()
+
+    def handle_see_help_action(self):
+        self.help_window.show()
+
+    def handle_about_the_program_action(self):
+        self.program_info_window.show()
+
+    def closeEvent(self, event):
+        for cluster_name in self.deployments_provider.cluster_deployments:
+            cluster = show_cluster(cluster_name)
+            cluster.override_deployments(self.deployments_provider.get_deployments(cluster_name))
+            event.accept()
---
+++ packages/idact-gui/idact-gui-0.4.0.tar.gz/idact-gui-0.4.0/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open('requirements-pip.txt') as requirements_file:
+    REQUIREMENTS = [i for i in requirements_file.readlines() if i]
+
+setuptools.setup(
+    name="idact-gui",
+    version="0.4.0",
+    author="Joanna Chyży, Karolina Piekarz, Piotr Swędrak",
+    author_email="piotrekswedrak@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/intdata-bsc/idact-gui",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    install_requires=REQUIREMENTS,
+    entry_points={
+        'console_scripts': [
+            'idactgui=gui.main:main',
+        ],
+    }
+)
