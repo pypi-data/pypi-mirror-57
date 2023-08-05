492a3f320093f0cd0ed8f1363ab6f5a581b287c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:33 +0000

    Add package ridi-django-oauth2/1.0.5/ridi-django-oauth2-1.0.5.tar.gz

---
+++ packages/ridi-django-oauth2/ridi-django-oauth2-1.0.5.tar.gz/ridi-django-oauth2-1.0.5/ridi_oauth2/introspector/dtos.py
@@ -0,0 +1,89 @@
+import typing
+from base64 import urlsafe_b64decode
+from datetime import datetime, timedelta
+
+from cryptography.hazmat.primitives.asymmetric.rsa import RSAPublicNumbers
+from cryptography.hazmat.primitives.serialization import Encoding, PublicFormat
+from cryptography.hazmat.backends import default_backend
+
+from ridi_django_oauth2_lib.utils.bytes import bytes_to_int
+from ridi_oauth2.introspector.constants import JWK_EXPIRES_MIN
+
+
+class AccessTokenInfo:
+    def __init__(self, subject: str, u_idx: int, expire: int, client_id: str, scope: typing.List):
+        self._subject = subject
+        self._u_idx = u_idx
+        self._expire_timestamp = expire
+        self._expire_date = datetime.fromtimestamp(expire)
+        self._client_id = client_id
+        self._scope = scope
+
+    @property
+    def subject(self) -> str:
+        return self._subject
+
+    @property
+    def u_idx(self) -> int:
+        return self._u_idx
+
+    @property
+    def expire_timestamp(self) -> int:
+        return self._expire_timestamp
+
+    @property
+    def expire_date(self) -> datetime:
+        return self._expire_date
+
+    @property
+    def client_id(self) -> str:
+        return self._client_id
+
+    @property
+    def scope(self) -> typing.List:
+        return self._scope
+
+    @staticmethod
+    def from_dict(dictionary: typing.Dict):
+        return AccessTokenInfo(
+            subject=dictionary['sub'], u_idx=dictionary['u_idx'], expire=dictionary['exp'], client_id=dictionary['client_id'],
+            scope=dictionary['scope'],
+        )
+
+
+class JWKDto:
+    def __init__(self, json):
+        self._json = json
+        self.expires = datetime.now() + timedelta(minutes=JWK_EXPIRES_MIN)
+        decoded_n = bytes_to_int(urlsafe_b64decode(self.n))
+        decoded_e = bytes_to_int(urlsafe_b64decode(self.e))
+        rsa_public_key = RSAPublicNumbers(decoded_e, decoded_n).public_key(default_backend())
+        self.public_key = rsa_public_key.public_bytes(Encoding.PEM, PublicFormat.SubjectPublicKeyInfo).decode()
+
+    @property
+    def alg(self) -> str:
+        return self._json.get('alg')
+
+    @property
+    def kty(self) -> str:
+        return self._json.get('kty')
+
+    @property
+    def use(self) -> str:
+        return self._json.get('use')
+
+    @property
+    def e(self) -> str:
+        return self._json.get('e')
+
+    @property
+    def n(self) -> str:
+        return self._json.get('n')
+
+    @property
+    def kid(self) -> str:
+        return self._json.get('kid')
+
+    @property
+    def is_expired(self) -> bool:
+        return self.expires < datetime.now()
---
+++ packages/ridi-django-oauth2/ridi-django-oauth2-1.0.5.tar.gz/ridi-django-oauth2-1.0.5/ridi_oauth2/introspector/exceptions.py
@@ -0,0 +1,38 @@
+class InvalidJwtSignatureException(Exception):
+    pass
+
+
+class ExpireTokenException(Exception):
+    pass
+
+
+class InvalidToken(Exception):
+    pass
+
+
+class PublicKeyException(Exception):
+    pass
+
+
+class InvalidPublicKey(PublicKeyException):
+    pass
+
+
+class FailToLoadPublicKeyException(PublicKeyException):
+    pass
+
+
+class NotExistedKey(PublicKeyException):
+    pass
+
+
+class PublicRequestException(PublicKeyException):
+    pass
+
+
+class AccountServerException(PublicRequestException):
+    pass
+
+
+class ClientRequestException(PublicRequestException):
+    pass
