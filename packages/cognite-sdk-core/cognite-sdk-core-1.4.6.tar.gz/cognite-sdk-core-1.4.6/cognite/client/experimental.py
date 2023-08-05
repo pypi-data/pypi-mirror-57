ea54d7b3663455c6f90b2cd2ccd1bcf1c1e64ef1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:41 +0000

    Add package cognite-sdk-core/1.4.6/cognite-sdk-core-1.4.6.tar.gz

---
+++ packages/cognite-sdk-core/cognite-sdk-core-1.4.6.tar.gz/cognite-sdk-core-1.4.6/cognite/client/experimental.py
@@ -0,0 +1,10 @@
+from cognite.client._api.model_hosting import ModelHostingAPI
+from cognite.client._api.relationships import RelationshipsAPI
+from cognite.client._cognite_client import CogniteClient as Client
+
+
+class CogniteClient(Client):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.model_hosting = ModelHostingAPI(self._config, api_version="playground", cognite_client=self)
+        self.relationships = RelationshipsAPI(self._config, api_version="playground", cognite_client=self)
