8cd1a9855de7283cf9dc72b92b45c2cc36b398f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:58 +0000

    Add package gea/0.2.26/gea-0.2.26.tar.gz

---
+++ packages/gea/gea-0.2.26.tar.gz/gea-0.2.26/gea/urls.py
@@ -0,0 +1,41 @@
+from django.conf.urls import url
+
+from gea.views import Home, About, \
+    CatastroLocalList, \
+    ExpedienteList, ExpedienteDetail, \
+    PersonaList, PersonaDetail, \
+    solicitud, visacion, \
+    plano, set, catastro, \
+    caratula, dvapi, sie
+
+
+urlpatterns = [
+    # Inicio
+    url(r'^$', Home.as_view(), name='home'),
+    # About
+    url(r'^acerca/$', About.as_view(), name='about'),
+    # Catastros Locales
+    url(r'^catastros-locales/$', CatastroLocalList.as_view(),
+        name='catastros_locales'),
+    # Expedientes
+    url(r'^expedientes/$', ExpedienteList.as_view(), name="expedientes"),
+    url(r'^expedientes/(?P<pk>\d+)/$', ExpedienteDetail.as_view(),
+        name="expediente"),
+    # Personas
+    url(r'^personas/$', PersonaList.as_view(), name="personas"),
+    url(r'^personas/(?P<pk>\d+)/$', PersonaDetail.as_view(), name="persona"),
+    # Notas
+    url(r'^solicitud/$', solicitud, name="solicitud"),
+    url(r'^visacion/$', visacion, name="visacion"),
+    # Búsquedas
+    url(r'^plano/$', plano, name="buscar_plano"),
+    url(r'^set/$', set, name="buscar_set"),
+    url(r'^catastro/$', catastro, name="buscar_catastro"),
+    # Herramientas
+    url(r'^caratula/$', caratula, name="caratula"),
+    url(r'^dvapi/$', dvapi, name='dvapi'),
+    url(r'^sie/$', sie, name='sie'),
+    # url(r'^cal/(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/$', calendar,
+    #     name='cal'),
+    # url(r'^cal/$', calendar, name='cal'),
+]
