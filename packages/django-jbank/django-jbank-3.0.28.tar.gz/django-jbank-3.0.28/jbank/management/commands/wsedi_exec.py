5bf0a95dd10ad5ca6493782fbcdb36a26fc3f152
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:38 +0000

    Add package django-jbank/3.0.28/django-jbank-3.0.28.tar.gz

---
+++ packages/django-jbank/django-jbank-3.0.28.tar.gz/django-jbank-3.0.28/jbank/management/commands/wsedi_exec.py
@@ -0,0 +1,28 @@
+import logging
+from django.core.management import CommandParser
+from jutil.command import SafeCommand
+
+from jbank.models import WsEdiConnection
+from jbank.wsedi import wsedi_execute
+
+logger = logging.getLogger(__name__)
+
+
+class Command(SafeCommand):
+    help = """
+    Executes WS-EDI command using direct bank connection.
+    """
+
+    def add_arguments(self, parser: CommandParser):
+        parser.add_argument('--ws', type=int, default=1)
+        parser.add_argument('--cmd', type=str, default='DownloadFileList')
+        parser.add_argument('--file-reference', type=str)
+        parser.add_argument('--file-type', type=str)
+
+    def do(self, *args, **options):
+        ws = WsEdiConnection.objects.get(id=options['ws'])
+        assert isinstance(ws, WsEdiConnection)
+        cmd = options['cmd']
+        file_reference = options['file_reference']
+        file_type = options['file_type']
+        wsedi_execute(ws, command=cmd, file_reference=file_reference, file_type=file_type, verbose=True)
