67330dd65067368142893227fd5b4c06224aebdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:11 +0000

    Add package asab/19.11/asab-19.11.tar.gz

---
+++ packages/asab/asab-19.11.tar.gz/asab-19.11/asab/metrics/memstor.py
@@ -0,0 +1,26 @@
+import asab
+
+class MetricsMemstorTarget(asab.ConfigObject):
+
+	def __init__(self, svc, config_section_name, config=None):
+		self.Metrics = {}
+
+
+	async def process(self, now, mlist):
+
+		for metric, values in mlist:
+			# Build metric name with tags
+			name = metric.Name
+			for tk, tv in metric.Tags.items():
+				name += ',{}={}'.format(tk, tv)
+			# Store metric
+			self.Metrics[name] = {
+				"Timestamp": now,
+				"Name": metric.Name,
+				"Values": values,
+				"Tags": metric.Tags
+			}
+
+
+	def rest_get(self):
+		return self.Metrics
---
+++ packages/asab/asab-19.11.tar.gz/asab-19.11/asab/web/authn/middleware.py
@@ -0,0 +1,71 @@
+import aiohttp
+import aiohttp.web
+
+
+def authn_middleware_factory(app, implementation, *args, **kwargs):
+
+	if implementation == "oauth2client":
+		from .oauth import oauthclient_middleware_factory
+		return oauthclient_middleware_factory(app, *args, **kwargs)
+
+	elif implementation == "basicauth":
+		from .basicauth import basicauth_middleware_factory
+		return basicauth_middleware_factory(app, *args, **kwargs)
+
+	elif implementation == "pubkeyauth":
+		from .pubkeyauth import pubkeyauth_middleware_factory
+		return pubkeyauth_middleware_factory(app, *args, **kwargs)
+
+	else:
+		raise RuntimeError("Unknown authentication implementation '{}'".format(implementation))
+
+
+def authn_required_handler(func):
+	'''
+	The web handler method require the authentication.
+	The peer identity is available in the `identity` keywork argument.
+
+	Example of use:
+
+	@asab.web.authn.authn_required_handler
+	async def endpoint(self, request, *, identity):
+		...
+	'''
+
+	async def wrapper(*args, **kargs):
+		request = args[-1]
+
+		try:
+			kargs['identity'] = request.Identity
+		except AttributeError:
+			raise aiohttp.web.HTTPUnauthorized()
+
+		return await func(*args, **kargs)
+
+	return wrapper
+
+
+def authn_optional_handler(func):
+	'''
+	The web handler method request the OPTINAL authentication.
+	The peer identity is available in the `identity` keywork argument.
+	`identity` is None if the peer has not provided authentication info.
+
+	Example of use:
+
+	@asab.web.authn.authn_optional_handler
+	async def endpoint(self, request, *, identity):
+		...
+	'''
+
+	async def wrapper(*args, **kargs):
+		request = args[-1]
+
+		try:
+			kargs['identity'] = request.Identity
+		except AttributeError:
+			kargs['identity'] = None
+
+		return await func(*args, **kargs)
+
+	return wrapper
