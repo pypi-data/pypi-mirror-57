af3f0017f887fa6f9c3c649d78fc79da7769165c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:50 +0000

    Add package PythonCA/1.23.1.3/PythonCA-1.23.1.3.tar.gz

---
+++ packages/PythonCA/PythonCA-1.23.1.3.tar.gz/PythonCA-1.23.1.3/epicsVersion.py
@@ -0,0 +1,15 @@
+#!python
+            
+# -*- coding:utf-8 -*-
+            
+# EPICS version info from /opt/epics/base/include/epicsVersion.h
+            
+EPICS_VERSION = 3
+EPICS_REVISION = 16
+EPICS_MODIFICATION = 0
+EPICS_PATCH_LEVEL = 1
+EPICS_DEV_SNAPSHOT = ""
+EPICS_SITE_VERSION = ""
+EPICS_VERSION_STRING = "EPICS 3.16.0.1"
+epicsReleaseVersion = "EPICS R3.16.0.1 $Date: Thu 2016-03-03 17:36:43 -0600$"
+EPICS_VERSION_INFO = (3, 16, 0, 1)
