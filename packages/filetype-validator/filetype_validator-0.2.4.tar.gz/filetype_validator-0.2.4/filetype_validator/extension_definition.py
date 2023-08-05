ee3ca65ee4823a6125d74d998d7dbb3c5fd9ad1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:19 +0000

    Add package filetype-validator/0.2.4/filetype_validator-0.2.4.tar.gz

---
+++ packages/filetype-validator/filetype_validator-0.2.4.tar.gz/filetype_validator-0.2.4/filetype_validator/extension_definition.py
@@ -0,0 +1,8 @@
+matching_extensions = dict(
+    zip=["zip", "aar", "apk", "docx", "epub", "ipa",
+        "jar", "kmz", "maff", "odp", "ods", "odt", 
+        "pk3", "pk4", "pptx", "usdz", "vsdx", "xlsx", 
+        "xpi"],
+    doc=["doc", "ppt", "xls"],
+    mp4=["m4a", "mp4", "m4v", "flv"]
+)
