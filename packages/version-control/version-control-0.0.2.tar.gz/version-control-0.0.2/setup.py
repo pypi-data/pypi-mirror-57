0f259f45c485968ef54680a687880e617327f318
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:48 +0000

    Add package version-control/0.0.2/version-control-0.0.2.tar.gz

---
+++ packages/version-control/version-control-0.0.2.tar.gz/version-control-0.0.2/setup.py
@@ -0,0 +1,41 @@
+import os
+
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+
+setup(
+    name='version-control',
+    version='0.0.2',
+    packages=find_packages(exclude=["*.swp"]),
+    install_requires=[
+        'Django >=1.6,<2.2',
+    ],
+    include_package_data=True,
+    license='MIT',  # example license
+    description='Django Version Control',
+    long_description=README,
+    long_description_content_type="text/x-rst",
+    url='https://github.com/kuter/django-version-control',
+    author='kuter',
+    author_email='contact@devktr.pl',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.6',
+        'Framework :: Django :: 2.1',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
