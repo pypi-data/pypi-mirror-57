2c51aabf33da168ef556a204eb95ce15ea70e591
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:51 +0000

    Add package tap-googlesearch/0.0.3/tap-googlesearch-0.0.3.tar.gz

---
+++ packages/tap-googlesearch/tap-googlesearch-0.0.3.tar.gz/tap-googlesearch-0.0.3/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup
+
+
+version = os.environ.get("PACKAGE_VERSION") or os.environ.get("CIRCLE_TAG")
+url = "https://github.com/dreamdata-io/tap-googlesearch"
+
+setup(
+    name="tap-googlesearch",
+    version=version,
+    description="Singer.io tap for extracting data from Google Search Analytics",
+    author="Dreamdata",
+    author_email="friends@dreamdata.io",
+    url=url,
+    download_url=f"{url}/archive/v{version}.tar.gz",
+    classifiers=["Programming Language :: Python :: 3 :: Only"],
+    license="MIT",
+    install_requires=[
+        "singer-python>=5.8.1, <6",
+        "google-api-python-client",
+        "ratelimit",
+        "backoff",
+    ],
+    entry_points="""
+        [console_scripts]
+        tap-googlesearch=tap_googlesearch:main
+    """,
+    include_package_data=True,
+    package_data={"tap_googlesearch": ["schemas/*.json"]},
+    packages=["tap_googlesearch"],
+    setup_requires=["pytest-runner"],
+    extras_require={"test": [["pytest"]]},
+)
