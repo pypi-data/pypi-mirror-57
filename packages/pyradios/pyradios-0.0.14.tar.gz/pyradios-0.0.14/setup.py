424915a64b66e9d676455e334cf89edf57e8ba5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:26 +0000

    Add package pyradios/0.0.14/pyradios-0.0.14.tar.gz

---
+++ packages/pyradios/pyradios-0.0.14.tar.gz/pyradios-0.0.14/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+def readme():
+    with open("README.md") as f:
+        return f.read()
+
+
+def required():
+    with open("requirements.txt") as f:
+        return f.read().splitlines()
+
+
+setup(
+    name="pyradios",
+    version="0.0.14",
+    description="A Python wrapper for the http://www.radio-browser.info/webservice",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    keywords="pyradios wrapper radios api",
+    author="André P. Santos",
+    author_email="andreztz@gmail.com",
+    url="https://github.com/andreztz/pyradios",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=required(),
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Environment :: Console",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+    ],
+    test_suite="tests",
+)
