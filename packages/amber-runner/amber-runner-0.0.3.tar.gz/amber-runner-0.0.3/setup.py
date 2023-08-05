49fc9a5a611a2d3fd9e51828fbda4cc9a14e1cb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:42 +0000

    Add package amber-runner/0.0.3/amber-runner-0.0.3.tar.gz

---
+++ packages/amber-runner/amber-runner-0.0.3.tar.gz/amber-runner-0.0.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='amber-runner',
+    maintainer="Sergei Izmailov",
+    maintainer_email="sergei.a.izmailov@gmail.com",
+    description="Batch execution for Amber MD",
+    url="https://github.com/sizmailov/amber-runner",
+    version="0.0.3",
+    long_description=open("README.rst").read(),
+    license="BSD",
+    install_requires=[
+        'remote-runner~=0.0',
+        'f90nml'
+    ],
+    tests_require=[
+        'pytest'
+    ],
+    packages=[
+        'amber_runner'
+    ]
+)
