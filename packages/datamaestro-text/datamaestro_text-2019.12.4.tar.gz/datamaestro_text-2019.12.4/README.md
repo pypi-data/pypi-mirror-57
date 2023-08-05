f5691e7f59c4f5eb032749585c598cead08c6daa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:37 +0000

    Add package datamaestro-text/2019.12.4/datamaestro_text-2019.12.4.tar.gz

---
+++ packages/datamaestro-text/datamaestro_text-2019.12.4.tar.gz/datamaestro_text-2019.12.4/README.md
@@ -0,0 +1,31 @@
+# Text-related datasets
+
+This [datasets](https://github.com/bpiwowar/datasets) plugin covers text-related datasets:
+
+- Information Retrieval
+- Natural Language Processing tasks
+
+The list of available datasets and usage instruction can be found in the [documentation](http://bpiwowar.github.io/datamaestro_text/).
+
+## List of available datasets
+
+### Documents
+
+- Aquaint (TREC)
+- TIPSTER (TREC)
+- WikiText-2 and WikiText-103
+
+### Word embeddings
+
+- [Glove](http://nlp.stanford.edu/projects/glove/) `edu.stanford.glove.XXXX`
+
+### Sentiment analysis
+
+- [IMDB]() `edu.stanford.aclimdb`
+
+### Information Retrieval
+
+#### TREC
+
+- TREC-1 to TREC-8
+- Robust 2004 and 2005
---
+++ packages/datamaestro-text/datamaestro_text-2019.12.4.tar.gz/datamaestro_text-2019.12.4/setup.py
@@ -0,0 +1,47 @@
+from pathlib import Path
+import re
+import sys
+
+try:
+    from setuptools import setup, find_namespace_packages
+except ImportError:
+    print("Please upgrade pip: find_namespace_packages not found")
+    sys.exit(1)
+
+# Date-based versioning
+VERSION='2019.12.04'
+
+RE_BLANCK=re.compile(r"^\s*(#.*)?$")
+with (Path(__file__).parent / 'requirements.txt').open() as f:
+    requirements = [x for x in f.read().splitlines() if not RE_BLANCK.match(x)]
+
+setup(
+    name='datamaestro_text',
+    version=VERSION,
+    description='Text related datasets',
+    author='Benjamin Piwowarski',
+    author_email='benjamin@piwowarski.fr',
+    url='https://github.com/bpiwowar/datamaestro_texts',
+    license='MIT',
+    python_requires='>=3.5',
+    packages=find_namespace_packages(include="datamaestro_text.*"),
+
+    install_requires=requirements,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
+    entry_points={
+        'datamaestro.repositories': [
+            'text = datamaestro_text:Repository'
+        ]
+    },
+
+    test_suite='datamaestro_text.test'
+)
