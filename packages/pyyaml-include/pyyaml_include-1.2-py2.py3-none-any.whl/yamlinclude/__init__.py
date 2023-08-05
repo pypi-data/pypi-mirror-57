1b319687fc1f49bd2266f2c9e52c03ecc4ebed81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:52 +0000

    Add package pyyaml-include/1.2/pyyaml_include-1.2-py2.py3-none-any.whl

---
+++ packages/pyyaml-include/pyyaml_include-1.2-py2.py3-none-any.whl/yamlinclude/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""
+Include YAML files within YAML
+"""
+
+
+from __future__ import absolute_import
+
+from .constructor import *
+from .readers import *
+from .version import version as __version__
