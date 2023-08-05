9ee93e845a544ec087bc18a92a7f52c87833ea82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:16 +0000

    Add package mypy-boto3/1.10.30/mypy_boto3-1.10.30-py3-none-any.whl

---
+++ packages/mypy-boto3/mypy_boto3-1.10.30-py3-none-any.whl/mypy_boto3/glacier/service_resource.py
@@ -0,0 +1,5 @@
+try:
+    # pylint: disable=wildcard-import, unused-wildcard-import
+    from mypy_boto3_glacier.service_resource import *
+except ImportError:
+    raise ImportError("Install boto3-stubs[glacier] to use glacier ServiceResource annotations")
