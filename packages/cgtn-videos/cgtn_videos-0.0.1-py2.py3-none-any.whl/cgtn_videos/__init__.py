81b4ddef71245be36fc02c91b58237109be4d7e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:57 +0000

    Add package cgtn-videos/0.0.1/cgtn_videos-0.0.1-py2.py3-none-any.whl

---
+++ packages/cgtn-videos/cgtn_videos-0.0.1-py2.py3-none-any.whl/cgtn_videos/__init__.py
@@ -0,0 +1,15 @@
+"""Package to retreive CGTN videos """
+from os.path import dirname, basename, isfile
+import glob
+
+from .category import Category, CategoryParser
+from .config import REQUEST_TIMEOUT
+from .livestream import Livestream, LivestreamParser
+from .program import ProgramVideo, ProgramParser
+from .region import Region
+from .video import Video, VideoParser
+
+__version__ = '0.0.1'
+
+MODULES = glob.glob(dirname(__file__)+"/*.py")
+__all__ = [basename(f)[:-3] for f in MODULES if isfile(f) and not f.endswith('__init__.py')]
