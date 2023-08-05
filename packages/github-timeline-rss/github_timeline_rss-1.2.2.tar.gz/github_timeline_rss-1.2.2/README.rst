a1066853491969ccc0aab28967e8b8a51ee3815c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:00 +0000

    Add package github-timeline-rss/1.2.2/github_timeline_rss-1.2.2.tar.gz

---
+++ packages/github-timeline-rss/github_timeline_rss-1.2.2.tar.gz/github_timeline_rss-1.2.2/README.rst
@@ -0,0 +1,57 @@
+===============================
+Github timeline RSS
+===============================
+
+
+.. image:: https://img.shields.io/pypi/v/github_timeline_rss.svg
+        :target: https://pypi.python.org/pypi/github_timeline_rss
+
+.. image:: https://img.shields.io/travis/narfman0/github_timeline_rss.svg
+        :target: https://travis-ci.org/narfman0/github_timeline_rss
+
+
+Github timeline to RSS translation for easier feed integration
+
+
+* Free software: MIT license
+
+
+Features
+--------
+
+* Create RSS feeds from github timelines
+* Command line interface to dump to stdout or a file
+
+Usage
+-----
+
+Optional: set up virtualenv environment with::
+
+    virtualenv venv
+    source venv/bin/activate
+
+Install requirements::
+
+    pip install -r requirements_dev.txt
+
+Run flask app, e.g.::
+
+    cd github_timeline_rss
+    FLASK_APP=rss.py flask run
+
+Test::
+
+    http://127.0.0.1:5000/narfman0
+
+Should show rss feed for activity for narfman0 from github!
+
+CLI
+---
+
+The command line interface may be used to dump a timeline to stdout::
+
+    github_timeline_rss narfman0
+
+or it may be directed at a file with `--file`::
+
+    github_timeline_rss narfman0 --file narfman0.atom
---
+++ packages/github-timeline-rss/github_timeline_rss-1.2.2.tar.gz/github_timeline_rss-1.2.2/github_timeline_rss/util.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from datetime import datetime
+import requests
+from werkzeug.contrib.atom import AtomFeed
+
+
+ENDPOINT = "https://api.github.com/users/{username}/received_events"
+TITLE = "{username}'s github timeline"
+SUBTITLE = "Timeline as of {date}"
+AUTHOR = "{}'s github"
+FEED_TITLE = "{} {} {}"
+URL = "https://github.com/{}"
+
+
+def generate_feed(username):
+    title = TITLE.format(username=username)
+    url = ENDPOINT.format(username=username)
+    subtitle = SUBTITLE.format(date=datetime.now())
+    statuses = requests.get(url).json()
+    feed = AtomFeed(title, feed_url="/", url="/", subtitle=subtitle)
+    for status in statuses:
+        event_type = "starred" if status["type"] == "WatchEvent" else "forked"
+        user = status["actor"]["display_login"]
+        target = status["repo"]["name"]
+        author = AUTHOR.format(username)
+        feed_title = FEED_TITLE.format(user, event_type, target)
+        url = URL.format(target)
+        date = datetime.strptime(status["created_at"], "%Y-%m-%dT%H:%M:%SZ")
+        feed.add(
+            feed_title,
+            "",
+            content_type="html",
+            author=author,
+            url=url,
+            id=status["id"],
+            published=date,
+            updated=date,
+        )
+    return feed.get_response()
