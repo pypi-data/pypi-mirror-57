5b07d8a21a85c22447b3cd48281ea94cf7145e26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:07 +0000

    Add package grpc4bmi/0.2.1/grpc4bmi-0.2.1.tar.gz

---
+++ packages/grpc4bmi/grpc4bmi-0.2.1.tar.gz/grpc4bmi-0.2.1/grpc4bmi/bmi_client_docker.py
@@ -0,0 +1,72 @@
+import os
+import errno
+import time
+
+import docker
+
+from grpc4bmi.bmi_grpc_client import BmiClient
+from grpc4bmi.utils import stage_config_file
+
+
+class BmiClientDocker(BmiClient):
+    """
+    BMI GRPC client for dockerized server processes: the initialization launches the docker container which should have the
+    run-bmi-server as its command. Also, it should expose the tcp port 50001 for communication with this client. Upon
+    destruction, this class terminates the corresponding docker server.
+
+
+    Args:
+        image (str): Docker image name of grpc4bmi wrapped model
+        image_port (int): Port of server inside the image
+        host (str): Host on which the image port is published on a random port
+        input_dir (str): Directory for input files of model
+        output_dir (str): Directory for input files of model
+        user (str): Username or UID of Docker container
+        remove (bool): Automatically remove the container when it exits
+        delay (int): Seconds to wait for Docker container to startup, before connecting to it
+
+    """
+
+    input_mount_point = "/data/input"
+    output_mount_point = "/data/output"
+
+    def __init__(self, image, image_port=50051, host=None,
+                 input_dir=None, output_dir=None,
+                 user=os.getuid(), remove=True, delay=5):
+        port = BmiClient.get_unique_port()
+        client = docker.from_env()
+        volumes = {}
+        self.input_dir = None
+        if input_dir is not None:
+            self.input_dir = os.path.abspath(input_dir)
+            volumes[self.input_dir] = {"bind": BmiClientDocker.input_mount_point, "mode": "rw"}
+        self.output_dir = None
+        if output_dir is not None:
+            self.output_dir = os.path.abspath(output_dir)
+            try:
+                # Create output dir ourselves, otherwise Docker will create it as root user, resulting in permission
+                # errors
+                os.mkdir(self.output_dir)
+            except OSError as e:
+                if e.errno != errno.EEXIST:
+                    raise
+            volumes[self.output_dir] = {"bind": BmiClientDocker.output_mount_point, "mode": "rw"}
+        self.container = client.containers.run(image,
+                                               ports={str(image_port) + "/tcp": port},
+                                               volumes=volumes,
+                                               user=user,
+                                               remove=remove,
+                                               detach=True)
+        time.sleep(delay)
+        super(BmiClientDocker, self).__init__(BmiClient.create_grpc_channel(port=port, host=host))
+
+    def __del__(self):
+        if hasattr(self, "container"):
+            self.container.stop()
+
+    def initialize(self, filename):
+        fn = stage_config_file(filename, self.input_dir, self.input_mount_point)
+        super(BmiClientDocker, self).initialize(fn)
+
+    def get_value_ref(self, var_name):
+        raise NotImplementedError("Cannot exchange memory references across process boundary")
