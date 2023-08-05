6ba34d969388b98d27aa60fb55027c1859dbc347
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:01 +0000

    Add package indy-plenum/1.12.1.dev971/indy-plenum-1.12.1.dev971.tar.gz

---
+++ packages/indy-plenum/indy-plenum-1.12.1.dev971.tar.gz/indy-plenum-1.12.1.dev971/plenum/common/messages/internal_messages.py
@@ -0,0 +1,69 @@
+from typing import NamedTuple, List, Any, Optional
+
+from plenum.common.exceptions import SuspiciousNode
+
+# General recommendation for (naming) internal messages is follows:
+# - internal messages are basically events, not commands
+# - so in most cases from name it should be clear that "something happened"
+# - in some cases message names may indicate that "something needs to happen"
+# - avoid names that tell "do something", messages are not commands
+# - avoid names that are just nouns, messages are not "things"
+
+RequestPropagates = NamedTuple('RequestPropagates',
+                               [('bad_requests', List)])
+
+BackupSetupLastOrdered = NamedTuple('BackupSetupLastOrdered',
+                                    [('inst_id', int)])
+
+NeedMasterCatchup = NamedTuple('NeedMasterCatchup', [])
+
+NeedBackupCatchup = NamedTuple('NeedBackupCatchup',
+                               [('inst_id', int),
+                                ('caught_up_till_3pc', tuple)])
+
+CheckpointStabilized = NamedTuple('CheckpointStabilized',
+                                  [('last_stable_3pc', tuple)])
+
+RaisedSuspicion = NamedTuple('RaisedSuspicion',
+                             [('inst_id', int),
+                              ('ex', SuspiciousNode)])
+
+PreSigVerification = NamedTuple('PreSigVerification',
+                                [('cmsg', Any)])
+
+MissingMessage = NamedTuple('MissingMessage',
+                            [('msg_type', str),
+                             ('key', tuple),
+                             ('inst_id', int),
+                             ('dst', List[str]),
+                             ('stash_data', Optional[tuple])])
+
+# by default view_no for StartViewChange is None meaning that we move to the next view
+NeedViewChange = NamedTuple('NeedViewChange',
+                            [('view_no', int)])
+NeedViewChange.__new__.__defaults__ = (None,) * len(NeedViewChange._fields)
+
+ViewChangeStarted = NamedTuple('ViewChangeStarted',
+                               [('view_no', int)])
+
+NewViewAccepted = NamedTuple('NewViewAccepted',
+                             [('view_no', int),
+                              ('view_changes', list),
+                              ('checkpoint', object),
+                              ('batches', list)])
+
+NewViewCheckpointsApplied = NamedTuple('NewViewCheckpointsApplied',
+                                       [('view_no', int),
+                                        ('view_changes', list),
+                                        ('checkpoint', object),
+                                        ('batches', list)])
+
+ReOrderedInNewView = NamedTuple('ReOrderedInNewView', [])
+
+CatchupFinished = NamedTuple('CatchupFinished', [('last_caught_up_3PC', tuple),
+                                                 ('master_last_ordered', tuple)])
+
+CatchupCheckpointsApplied = NamedTuple('CatchupCheckpointsApplied', [('last_caught_up_3PC', tuple),
+                                                                     ('master_last_ordered', tuple)])
+
+StartViewChange = NamedTuple('StartViewChange', [('view_no', int)])
---
+++ packages/indy-plenum/indy-plenum-1.12.1.dev971.tar.gz/indy-plenum-1.12.1.dev971/plenum/server/view_change/node_view_changer.py
@@ -0,0 +1,76 @@
+import logging
+from typing import Set
+
+from plenum.common.messages.internal_messages import NeedViewChange
+from plenum.common.startable import Mode
+from plenum.server.quorums import Quorums
+from plenum.server.view_change.view_changer import ViewChanger, ViewChangerDataProvider
+from storage.kv_store import KeyValueStorage
+
+
+class ViewChangerNodeDataProvider(ViewChangerDataProvider):
+    def __init__(self, node):
+        self._node = node
+
+    def name(self) -> str:
+        return self._node.name
+
+    def config(self) -> object:
+        return self._node.config
+
+    def quorums(self) -> Quorums:
+        return self._node.quorums
+
+    def node_mode(self) -> Mode:
+        return self._node.mode
+
+    def has_primary(self) -> bool:
+        return self._node.master_replica.hasPrimary
+
+    def is_primary(self):
+        return self._node.master_replica.isPrimary
+
+    def is_primary_disconnected(self) -> bool:
+        return \
+            self._node.primaries_disconnection_times[self._node.master_replica.instId] \
+            and self._node.master_primary_name \
+            and self._node.master_primary_name not in self._node.nodestack.conns
+
+    def is_master_degraded(self) -> bool:
+        return self._node.monitor.isMasterDegraded()
+
+    def pretty_metrics(self) -> str:
+        return self._node.monitor.prettymetrics
+
+    def state_freshness(self) -> float:
+        replica = self._node.master_replica
+        timestamps = replica.get_ledgers_last_update_time().values()
+        oldest_timestamp = min(timestamps)
+        return replica.get_time_for_3pc_batch() - oldest_timestamp
+
+    def connected_nodes(self) -> Set[str]:
+        return self._node.nodestack.connecteds
+
+    def notify_view_change_start(self):
+        self._node.on_view_change_start()
+
+    def discard(self, msg, reason, logMethod=logging.error, cliOutput=False):
+        self._node.discard(msg, reason, logMethod, cliOutput)
+
+    @property
+    def node_status_db(self) -> KeyValueStorage:
+        return self._node.nodeStatusDB
+
+    def start_view_change(self, proposed_view_no: int):
+        for replica in self._node.replicas.values():
+            replica.internal_bus.send(NeedViewChange(view_no=proposed_view_no))
+
+    def view_no(self):
+        return self._node.master_replica.viewNo
+
+    def view_change_in_progress(self):
+        return self._node.master_replica._consensus_data.waiting_for_new_view
+
+
+def create_view_changer(node, vchCls=ViewChanger):
+    return vchCls(ViewChangerNodeDataProvider(node), node.timer)
