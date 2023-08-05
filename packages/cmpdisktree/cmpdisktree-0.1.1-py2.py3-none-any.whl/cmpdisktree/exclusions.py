290fa1b32872b7ed7dd6d0f6e1167d535bcbcf02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:32 +0000

    Add package cmpdisktree/0.1.1/cmpdisktree-0.1.1-py2.py3-none-any.whl

---
+++ packages/cmpdisktree/cmpdisktree-0.1.1-py2.py3-none-any.whl/cmpdisktree/exclusions.py
@@ -0,0 +1,98 @@
+"""
+The default exclusion list
+Data from https://bombich.com/kb/ccc5/some-files-and-folders-are-automatically-excluded-from-backup-task
+"""
+
+STANDARD_EXCLUDE_PATTERNS_STR = """
+# File system implementation details
+.HFS+ Private Directory Data*
+/.journal
+/.journal_info_block
+.afpDeleted*
+._*
+.AppleDouble
+.AppleDB
+/lost+found
+Network Trash Folder
+.TemporaryItems
+# Volume-specific preferences
+.metadata_never_index
+.metadata_never_index_unless_rootfs
+/.com.apple.timemachine.donotpresent
+.VolumeIcon.icns
+/System/Library/CoreServices/.disk_label*
+/TheVolumeSettingsFolder
+# Apple-proprietary data stores
+.DocumentRevisions-V100*
+.Spotlight-V100
+/.fseventsd
+/.hotfiles.btree
+/private/var/db/systemstats
+# Volume-specific cache files
+/private/var/db/dyld/dyld_*
+/System/Library/Caches/com.apple.bootstamps/*
+/System/Library/Caches/com.apple.corestorage/*
+# NetBoot local data store
+/.com.apple.NetBootX
+# Dynamically-generated devices
+/Volumes/*
+/dev/*
+/automount
+/Network
+/.vol/*
+/net
+# Quota real-time data files
+/.quota.user
+/.quota.group
+# Large datastores that are (or should be) erased on startup
+/private/var/vm/*
+/private/tmp/*
+/cores
+/macOS Install Data
+# Trash
+.Trash
+.Trashes
+# Time Machine backups
+/Backups.backupdb
+/.MobileBackups
+/.MobileBackups.trash
+/private/var/db/com.apple.backupd.backupVerification
+# Corrupted iCloud Local Storage
+Library/Mobile Documents/*
+Library/Mobile Documents.*
+.webtmp
+# Special files
+/private/tmp/kacta.txt`
+/private/tmp/kactd.txt
+/private/var/audit/*.crash_recovery
+/private/var/audit/current
+/Library/Caches/CrashPlan
+/PGPWDE01
+/PGPWDE02
+/.bzvol
+/.cleverfiles
+/Library/Application Support/Comodo/AntiVirus/Quarantine
+/private/var/spool/qmaster
+$Recycle.Bin
+Library/Preferences/ByHost/com.apple.loginwindow*
+.dropbox.cache
+/private/var/db/atpstatdb*
+"""
+
+ADD_LIVEFS_EXCLUDE_PATTERNS_STR = """
+.DS_Store
+"""
+
+
+def pattern_list_from_str(pat_str: str):
+    return [
+        pat for pat in pat_str.splitlines() if not (pat.startswith('#') or pat == '')
+    ]
+
+
+STANDARD_EXCLUDE_PATTERNS = pattern_list_from_str(STANDARD_EXCLUDE_PATTERNS_STR)
+ADD_LIVEFS_EXCLUDE_PATTERNS = pattern_list_from_str(ADD_LIVEFS_EXCLUDE_PATTERNS_STR)
+
+if __name__ == '__main__':
+    for p in STANDARD_EXCLUDE_PATTERNS:
+        print(p)
---
+++ packages/cmpdisktree/cmpdisktree-0.1.1-py2.py3-none-any.whl/cmpdisktree/main.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+"""
+The CLI to cmpdisktree
+"""
+
+import os
+import sys
+
+import click
+
+from cmpdisktree import comparer, utils
+
+
+class ExpandedPath(click.Path):
+    def convert(self, value, *args, **kwargs):
+        value = os.path.expanduser(value)
+        return super(ExpandedPath, self).convert(value, *args, **kwargs)
+
+
+@click.command(
+    help=f"""Compare the directories FS1 and FS2 as macOS disk structures
+
+Errors are reported to a file (default '{utils.ERR_LOG_DEFAULT_NAME}')
+"""
+)
+@click.argument('fs1', type=ExpandedPath(exists=True))
+@click.argument('fs2', type=ExpandedPath(exists=True))
+@click.option('-v', '--verbose', is_flag=True, help="Print debug output.")
+@click.option('-q', '--quiet', is_flag=True, help="No informational output.")
+@click.option(
+    '-i',
+    '--report-identical',
+    is_flag=True,
+    help=f"Report identical files to file (default: '{utils.OK_LOG_DEFAULT_NAME}')",
+)
+@click.option(
+    '-1',
+    '--traversal-only',
+    is_flag=True,
+    help="Only traverse FSs (Phase 1). Don't compare file contents",
+)
+@click.option(
+    '-c',
+    '--clear-std-exclusions',
+    is_flag=True,
+    help="Don't use standard exclusions for macOS disk files systems",
+)
+@click.option(
+    '-l',
+    '--live-fs-exclusions',
+    is_flag=True,
+    help="Add exclusions for live filesystems (e.g. boot volume)",
+)
+@click.option(
+    '-r',
+    '--relative-fs-top',
+    is_flag=True,
+    help="Allow relative filesystem top (used when applying the exclusions)",
+)
+@click.option(
+    '-o', '--output-path', type=ExpandedPath(), help="Output path for report file."
+)
+# hidden options - not in help or doco:
+@click.option('--force-progress/--force-progress-off', default=None, hidden=True)
+def main(*args, **kwargs):
+    """
+    Compare the directories FS1 and FS2 as macOS disk structures
+    """
+    cc = comparer.Comparer(**kwargs)
+    status = cc.work()
+    sys.exit(status)
+
+
+if __name__ == '__main__':
+    main()
