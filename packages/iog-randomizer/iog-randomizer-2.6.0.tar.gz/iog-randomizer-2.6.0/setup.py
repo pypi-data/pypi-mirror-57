68a38321a186d20fcb012ab02c57765df95b295f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:57 +0000

    Add package iog-randomizer/2.6.0/iog-randomizer-2.6.0.tar.gz

---
+++ packages/iog-randomizer/iog-randomizer-2.6.0.tar.gz/iog-randomizer-2.6.0/setup.py
@@ -0,0 +1,20 @@
+import os
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name='iog-randomizer',
+    version='2.6.0',
+    description='The Illusion of Gaia Randomizer',
+    author='dontbagume,bryon_w,raeven0',
+    packages=setuptools.find_packages(),
+    package_data={'': [
+        'bin/*.bin'
+    ]},
+    install_requires=['bitstring'],
+    python_requires='>=3.7'
+)
