9aff3105f22f5f567ffe8e42bf86cd9ed5854c36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:43 +0000

    Add package filetype-validator/0.2.1/filetype_validator-0.2.1.tar.gz

---
+++ packages/filetype-validator/filetype_validator-0.2.1.tar.gz/filetype_validator-0.2.1/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    description_text = fh.read()
+
+setuptools.setup(
+  name='filetype_validator',
+  packages=['filetype_validator'],
+  version='0.2.1',
+  license='MIT',
+  description=description_text,
+  author='isna',
+  author_email='isna11583@gmail.com',
+  url='https://github.com/ixna/filetype_validator',
+  download_url='https://github.com/ixna/filetype_validator/releases/tag/v0.2.1',
+  keywords=['filetype', 'extension'],
+  dependency_links=['https://github.com/ixna/filetype.py/tarball/master'],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
