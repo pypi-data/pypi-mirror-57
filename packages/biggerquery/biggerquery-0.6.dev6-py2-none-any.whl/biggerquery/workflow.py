eb3682239a097f28125f9259793d899a2fa245de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:41 +0000

    Add package biggerquery/0.6.dev6/biggerquery-0.6.dev6-py2-none-any.whl

---
+++ packages/biggerquery/biggerquery-0.6.dev6-py2-none-any.whl/biggerquery/workflow.py
@@ -0,0 +1,21 @@
+DEFAULT_SCHEDULE_INTERVAL = '@daily'
+
+
+class Workflow(object):
+    def __init__(self,
+                 definition,
+                 schedule_interval=DEFAULT_SCHEDULE_INTERVAL,
+                 dt_as_datetime=False,
+                 **kwargs):
+        self.definition = definition
+        self.schedule_interval = schedule_interval
+        self.dt_as_datetime = dt_as_datetime
+        self.kwargs = kwargs
+
+    def run(self, runtime):
+        for job in self:
+            job.run(runtime=runtime)
+
+    def __iter__(self):
+        for job in self.definition:
+            yield job
