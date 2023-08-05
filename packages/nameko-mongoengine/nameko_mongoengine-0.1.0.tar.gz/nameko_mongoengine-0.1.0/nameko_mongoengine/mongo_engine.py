1f96100461624475e97baf585c2052aa226f7486
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:06 +0000

    Add package nameko-mongoengine/0.1.0/nameko_mongoengine-0.1.0.tar.gz

---
+++ packages/nameko-mongoengine/nameko_mongoengine-0.1.0.tar.gz/nameko_mongoengine-0.1.0/nameko_mongoengine/mongo_engine.py
@@ -0,0 +1,39 @@
+"""
+    Mongoengine dependency provider extension
+"""
+
+from mongoengine.connection import register_connection, disconnect, DEFAULT_CONNECTION_NAME
+from nameko.extensions import DependencyProvider  # pragma: no cover
+
+from .constants import MONGODB_URI_KEY  # pragma: no cover
+from .database import Database  # pragma: no cover
+
+
+class MongoEngine(DependencyProvider):
+    """
+        MongoEngine dependency provider for Nameko microservice framework
+    """
+    default_connection_uri = "mongodb://localhost:27017"
+
+    def __init__(self):
+        self.aliases = {}
+
+    def _parse_config(self):
+        config = self.container.config
+        value = config.get(MONGODB_URI_KEY, self.default_connection_uri)
+        self.aliases = value if isinstance(value, dict) else {DEFAULT_CONNECTION_NAME: value}
+
+    def setup(self):
+        self._parse_config()
+        for alias, mongo_uri in self.aliases.items():
+            register_connection(alias=alias, host=mongo_uri)
+
+    def get_dependency(self, worker_ctx):
+        return Database(DEFAULT_CONNECTION_NAME)
+
+    def stop(self):
+        for alias in self.aliases:
+            disconnect(alias)
+
+    def kill(self):
+        self.stop()
