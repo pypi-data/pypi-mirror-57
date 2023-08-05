b802acab1d85f32319eaa212247b06dd10acf53c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:24 +0000

    Add package pyverm/0.2.0/pyverm-0.2.0-py3-none-any.whl

---
+++ packages/pyverm/pyverm-0.2.0-py3-none-any.whl/pyverm/__meta__.py
@@ -0,0 +1,35 @@
+########################################################################
+#                                                                      #
+# Copyright (C) 2018,  Marius Hürzeler                                 #
+#                                                                      #
+# This file is part of PyVerm.                                         #
+#                                                                      #
+# PyVerm is free software: you can redistribute it and/or modify       #
+# it under the terms of the GNU General Public License as published by #
+# the Free Software Foundation, either version 3 of the License, or    #
+# (at your option) any later version.                                  #
+#                                                                      #
+# PyVerm is distributed in the hope that it will be useful,            #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of       #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        #
+# GNU General Public License for more details.                         #
+#                                                                      #
+# You should have received a copy of the GNU General Public License    #
+# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.      #
+#                                                                      #
+########################################################################
+
+"""
+Contains all the Package-Metadata
+
+"""
+
+__title__ = "pyverm"
+__description__ = 'Modul for geodetic and surveying calculations'
+__url__ = "https://pyverm.readthedocs.io/en/latest/"
+__version__ = "0.2.0"
+__release__ = __version__ + ""
+__author__ = 'Marius Huerzeler'
+__author_email__ = 'huerzeler.marius@gmail.com'
+__license__ = 'GNU GPLv3+'
+__copyright__ = 'Copyright 2018 Marius Huerzeler'
