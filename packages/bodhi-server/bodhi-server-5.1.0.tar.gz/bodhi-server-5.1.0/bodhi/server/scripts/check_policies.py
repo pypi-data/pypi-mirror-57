b56d6b53cdc22f9f2a0db82ca66746c2396eaf43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:54 +0000

    Add package bodhi-server/5.1.0/bodhi-server-5.1.0.tar.gz

---
+++ packages/bodhi-server/bodhi-server-5.1.0.tar.gz/bodhi-server-5.1.0/bodhi/server/scripts/check_policies.py
@@ -0,0 +1,68 @@
+# Copyright © 2017 Red Hat, Inc.
+#
+# This file is part of Bodhi.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+"""
+Check the enforced policies by Greenwave for each open update.
+
+Ideally, this should be done in a messaging consumer but we currently do not have
+any messages in the message bus yet.
+"""
+import logging
+
+import click
+
+from bodhi.server import config, initialize_db, models, Session
+
+
+logger = logging.getLogger('check-policies')
+logging.basicConfig(level=logging.INFO)
+
+
+@click.command()
+@click.version_option(message='%(version)s')
+def check():
+    """Check the enforced policies by Greenwave for each open update."""
+    initialize_db(config.config)
+    session = Session()
+
+    updates = models.Update.query.filter(
+        models.Update.status.in_(
+            [models.UpdateStatus.pending, models.UpdateStatus.testing])
+    ).filter(
+        models.Update.release_id == models.Release.id
+    ).filter(
+        models.Release.state.in_(
+            [models.ReleaseState.current, models.ReleaseState.pending, models.ReleaseState.frozen])
+    ).order_by(
+        # Check the older updates first so there is more time for the newer to
+        # get their test results
+        models.Update.id.asc()
+    )
+
+    for update in updates:
+        try:
+            update.update_test_gating_status()
+            session.commit()
+        except Exception as e:
+            # If there is a problem talking to Greenwave server, print the error.
+            click.echo(str(e), err=True)
+            session.rollback()
+
+
+if __name__ == '__main__':
+    check()
---
+++ packages/bodhi-server/bodhi-server-5.1.0.tar.gz/bodhi-server-5.1.0/bodhi/server/scripts/clean_old_composes.py
@@ -0,0 +1,72 @@
+# Copyright © 2016-2019 Red Hat, Inc.
+#
+# This file is part of Bodhi.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+"""Cleans up old composes that are left over in compose_dir."""
+import collections
+import os
+import shutil
+
+import click
+
+from bodhi.server import config
+
+
+# How many of the newest compose dirs to keep during cleanup
+NUM_TO_KEEP = 10
+
+
+@click.command()
+@click.version_option(message='%(version)s')
+def clean_up():
+    """Delete any repo composes that are older than the newest 10 from each repo series."""
+    remove_old_composes()
+
+
+# Helper function used in auto clean composes (composer.py)
+def remove_old_composes():
+    """Delete any repo composes that are older than the newest 10 from each repo series."""
+    compose_dir = config.config['compose_dir']
+
+    # This data structure will map the beginning of a group of dirs for the same repo to a list of
+    # the dirs that start off the same way.
+    pattern_matched_dirs = collections.defaultdict(list)
+
+    for directory in [d for d in os.listdir(compose_dir)
+                      if os.path.isdir(os.path.join(compose_dir, d))]:
+        # If this directory ends with a float, it is a candidate for potential deletion
+        try:
+            split_dir = directory.split('-')
+            float(split_dir[-1])
+        except ValueError:
+            # This directory didn't end in a float, so let's just move on to the next one.
+            continue
+
+        pattern = directory.replace(split_dir[-1], '')
+        pattern_matched_dirs[pattern].append(directory)
+
+    dirs_to_delete = []
+
+    for dirs in pattern_matched_dirs.values():
+        if len(dirs) > NUM_TO_KEEP:
+            dirs_to_delete.extend(sorted(dirs, reverse=True)[NUM_TO_KEEP:])
+
+    if dirs_to_delete:
+        print('Deleting the following directories:')
+        for d in dirs_to_delete:
+            d = os.path.join(compose_dir, d)
+            print(d)
+            shutil.rmtree(d)
