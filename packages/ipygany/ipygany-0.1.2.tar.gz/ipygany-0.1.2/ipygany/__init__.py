93c4a351777bf5bb5945a91b3fa33e49ac34be15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:33 +0000

    Add package ipygany/0.1.2/ipygany-0.1.2.tar.gz

---
+++ packages/ipygany/ipygany-0.1.2.tar.gz/ipygany-0.1.2/ipygany/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Copyright (c) Martin Renou.
+# Distributed under the terms of the Modified BSD License.
+
+from .ipygany import PolyMesh, TetraMesh, Scene, Data, Component, Alpha, IsoColor, Threshold, IsoSurface  # noqa
+from ._version import __version__, version_info  # noqa
+
+from .nbextension import _jupyter_nbextension_paths  # noqa
