f166ddc25ac653006b04a7910fb3d2331b23880a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:31 +0000

    Add package gameforflag/0.0.1/gameforflag-0.0.1.tar.gz

---
+++ packages/gameforflag/gameforflag-0.0.1.tar.gz/gameforflag-0.0.1/gameforflag/flag.py
@@ -0,0 +1,2 @@
+def flag():
+	print 'flag{a70483c2fa93f9bd54c4fcf3cca0b484}'
---
+++ packages/gameforflag/gameforflag-0.0.1.tar.gz/gameforflag-0.0.1/setup.py
@@ -0,0 +1,34 @@
+# coding=utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+	name='gameforflag',
+	version='0.0.1',
+	description=(
+		'Just a joke'
+	),
+	author='ZacharyZcR',
+	author_email='2903735704@qq.com',
+	maintainer_email='zacharyzcr@hi-ourlife.com',
+	license='MIT License',
+	packages=find_packages(),
+	platforms=["all"],
+	install_requires=[
+	],
+	classifiers=[
+		'Development Status :: 4 - Beta',
+		'Operating System :: OS Independent',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: BSD License',
+		'Programming Language :: Python',
+		'Programming Language :: Python :: Implementation',
+		'Programming Language :: Python :: 2',
+		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.4',
+		'Programming Language :: Python :: 3.5',
+		'Programming Language :: Python :: 3.6',
+		'Topic :: Software Development :: Libraries'
+	],
+)
