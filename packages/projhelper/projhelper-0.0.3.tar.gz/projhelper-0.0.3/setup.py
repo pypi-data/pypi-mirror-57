ed3ae6f18082c16fc8c96f8efe81192db4c94bbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:13 +0000

    Add package projhelper/0.0.3/projhelper-0.0.3.tar.gz

---
+++ packages/projhelper/projhelper-0.0.3.tar.gz/projhelper-0.0.3/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 19/11/26 11:23
+# @Author  : oujianhua
+# @mail  : ojhtyy@163.com
+# @File    : setup.py
+from __future__ import print_function
+from setuptools import setup, find_packages
+import sys
+
+setup(
+    name = "projhelper",
+    version = "0.0.3",
+    keywords = ("pip", "datacanvas", "eds", "xiaoh"),
+    description = "eds sdk",
+    long_description = "eds sdk for python",
+    license = "MIT Licence",
+
+    url = "http://xiaoh.me",
+    author = "xiaoh",
+    author_email = "huoxingming@gmail.com",
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = []
+)
