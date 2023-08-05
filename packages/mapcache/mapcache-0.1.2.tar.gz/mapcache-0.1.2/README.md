640fb095b4e3ef376b9b5c7ee9d17cb6bf0efd70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:28 +0000

    Add package mapcache/0.1.2/mapcache-0.1.2.tar.gz

---
+++ packages/mapcache/mapcache-0.1.2.tar.gz/mapcache-0.1.2/README.md
@@ -0,0 +1,5 @@
+# mapcache
+
+[<img src="https://img.shields.io/pypi/v/mapcache.svg">](https://pypi.python.org/pypi/mapcache)
+
+Simple caching server for map tiles.
---
+++ packages/mapcache/mapcache-0.1.2.tar.gz/mapcache-0.1.2/setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''setup script for mapcache.'''
+
+from setuptools import setup
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.md') as history_file:
+    history = history_file.read()
+
+requirements = []
+
+setup_requirements = []
+
+test_requirements = []
+
+setup(
+    author='luphord',
+    author_email='luphord@protonmail.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    description='Simple caching server for map tiles',
+    entry_points={
+        'console_scripts': [
+            'mapcache=mapcache:main',
+        ],
+    },
+    install_requires=requirements,
+    license='MIT license',
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='mapcache',
+    name='mapcache',
+    py_modules=['mapcache'],
+    scripts=['mapcache.py'],
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/luphord/mapcache',
+    version='0.1.2',
+    zip_safe=False,
+)
