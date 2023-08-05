0ebdecd2cf357a986e91c939852470aa890310a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:17 +0000

    Add package rapic/0.1.2/rapic-0.1.2.tar.gz

---
+++ packages/rapic/rapic-0.1.2.tar.gz/rapic-0.1.2/rapic/tools/__init__.py
@@ -0,0 +1,36 @@
+import time
+
+
+class dotdict(dict):
+    """dot.notation access to dictionary attributes"""
+    __getattr__ = dict.get
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
+
+
+
+def delete_keys(data):
+    """
+    set  data values  to None
+    :param data:
+    :return:
+    """
+    return {key: None for key in data}
+
+
+def dict_merge(base_dct, merge_dct, add_keys=True):
+    """
+    Recursively merge dict from
+    https://gist.github.com/CMeza99/5eae3af0776bef32f945f34428669437
+    """
+    rtn_dct = base_dct.copy()
+    if add_keys is False:
+        merge_dct = {key: merge_dct[key] for key in set(rtn_dct).intersection(set(merge_dct))}
+
+    rtn_dct.update({
+        key: dict_merge(rtn_dct[key], merge_dct[key], add_keys=add_keys)
+        if isinstance(rtn_dct.get(key), dict) and isinstance(merge_dct[key], dict)
+        else merge_dct[key]
+        for key in merge_dct.keys()
+    })
+    return rtn_dct
