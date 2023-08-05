de0328ed8f0879d1c950cada68e396811b417d79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:04 +0000

    Add package e2j2/0.4.8/e2j2-0.4.8.tar.gz

---
+++ packages/e2j2/e2j2-0.4.8.tar.gz/e2j2-0.4.8/e2j2/tags/file_tag.py
@@ -0,0 +1,10 @@
+from e2j2.helpers.exceptions import E2j2Exception
+
+
+def parse(file_name):
+    try:
+        with open(file_name) as file_handle:
+            return file_handle.read()
+    except IOError:
+        # Mark as failed
+        raise E2j2Exception('IOError raised while reading file')
