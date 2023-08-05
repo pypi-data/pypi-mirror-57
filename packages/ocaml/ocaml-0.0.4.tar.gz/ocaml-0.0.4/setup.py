9638fc69cd353cab407c83042519d16378031e29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:38 +0000

    Add package ocaml/0.0.4/ocaml-0.0.4.tar.gz

---
+++ packages/ocaml/ocaml-0.0.4.tar.gz/ocaml-0.0.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ocaml',
+    version='0.0.4',
+    author='Laurent Mazare',
+    author_email='lmazare@gmail.com',
+    packages=['ocaml'],
+    package_dir={'ocaml': '.'},
+    package_data={'ocaml': ['sharedlib/ocaml.so', 'sharedlib/stdlib.cmi']},
+    install_requires=['wurlitzer'],
+)
