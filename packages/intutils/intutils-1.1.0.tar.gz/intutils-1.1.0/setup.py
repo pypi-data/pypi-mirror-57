77e133a51fb68ca2c6b5dff72789568a27de259e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:12 +0000

    Add package intutils/1.1.0/intutils-1.1.0.tar.gz

---
+++ packages/intutils/intutils-1.1.0.tar.gz/intutils-1.1.0/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+setuptools.setup(
+    name="intutils",
+    version="1.1.0",
+    author="Reece Dunham",
+    author_email="me@rdil.rocks",
+    license="MIT",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    url="https://github.com/rdilweb/intutils",
+    description="Basic integer manipulation library.",
+    python_requires=">3.3",
+    project_urls={
+        "Documentation": "https://docs.rdil.rocks/libraries/intutils"
+    },
+    keywords=[
+        "int",
+        "integer",
+        "utils",
+        "util",
+        "utilities"
+    ]
+)
