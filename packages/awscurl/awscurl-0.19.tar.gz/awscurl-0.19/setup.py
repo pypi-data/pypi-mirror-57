fd69e0f7cff4089dfbd5d4c5b198c817ecbe6e5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:55 +0000

    Add package awscurl/0.19/awscurl-0.19.tar.gz

---
+++ packages/awscurl/awscurl-0.19.tar.gz/awscurl-0.19/setup.py
@@ -0,0 +1,29 @@
+__author__ = 'iokulist'
+
+from setuptools import setup
+
+setup(
+    name='awscurl',
+    version='0.19',
+    description='Curl like tool with AWS request signing',
+    url='http://github.com/okigan/awscurl',
+    author='Igor Okulist',
+    author_email='okigan@gmail.com',
+    license='MIT',
+    packages=['awscurl'],
+    entry_points={
+        'console_scripts': [
+            'awscurl = awscurl.__main__:main',
+        ],
+    },
+    zip_safe=False,
+    install_requires=[
+        'requests',
+        'configargparse',
+        'configparser',
+        'urllib3[secure]<1.24,>=1.21.1'
+    ],
+    extras_require={
+        'awslibs': ["botocore"]
+    }
+)
