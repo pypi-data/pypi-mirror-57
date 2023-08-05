768ab75c4729027082bea91037ab93fb0bd38faf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:22 +0000

    Add package django-scrubber/0.4.3/django-scrubber-0.4.3.tar.gz

---
+++ packages/django-scrubber/django-scrubber-0.4.3.tar.gz/django-scrubber-0.4.3/django_scrubber/__init__.py
@@ -0,0 +1,21 @@
+from django.conf import settings
+
+__version__ = '0.4.3'
+
+defaults = {
+    'SCRUBBER_RANDOM_SEED': 42,  # we prefer idempotent scrubbing
+    'SCRUBBER_ENTRIES_PER_PROVIDER': 1000,
+    'SCRUBBER_GLOBAL_SCRUBBERS': {},
+    'SCRUBBER_SKIP_UNMANAGED': True,
+    'SCRUBBER_APPS_LIST': None,
+    'SCRUBBER_ADDITIONAL_FAKER_PROVIDERS': [],
+}
+
+
+# can be replaced with ChainMap if we only support py3
+def settings_with_fallback(key):
+    return getattr(settings, key, defaults[key])
+
+
+class ScrubberInitError(Exception):
+    pass
