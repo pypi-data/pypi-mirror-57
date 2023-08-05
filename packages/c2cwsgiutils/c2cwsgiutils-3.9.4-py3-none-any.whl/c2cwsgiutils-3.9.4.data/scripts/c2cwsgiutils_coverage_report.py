81e27bb84af1cd1bcfc46fec63a7438569824bed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:59 +0000

    Add package c2cwsgiutils/3.9.4/c2cwsgiutils-3.9.4-py3-none-any.whl

---
+++ packages/c2cwsgiutils/c2cwsgiutils-3.9.4-py3-none-any.whl/c2cwsgiutils-3.9.4.data/scripts/c2cwsgiutils_coverage_report.py
@@ -0,0 +1,21 @@
+#!python
+import coverage
+import os
+import shutil
+import sys
+
+
+def main():
+    sources = sys.argv[1:]
+    report_dir = '/reports/coverage/api'
+    dest_dir = '/tmp/coverage/api'
+    shutil.rmtree(dest_dir, ignore_errors=True)
+    shutil.copytree(report_dir, dest_dir)
+    cov = coverage.Coverage(data_file='coverage', data_suffix=True, source=sources, branch=True)
+    cov.combine([dest_dir], strict=True)
+    cov.html_report(directory=dest_dir, ignore_errors=True)
+    cov.xml_report(outfile=os.path.join(dest_dir, 'coverage.xml'), ignore_errors=True)
+    cov.report(ignore_errors=True)
+
+
+main()
