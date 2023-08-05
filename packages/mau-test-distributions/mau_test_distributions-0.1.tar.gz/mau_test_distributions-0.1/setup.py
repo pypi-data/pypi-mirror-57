3b97d3f68a16d437b6b826f4833df1457c28bf84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:32 +0000

    Add package mau-test-distributions/0.1/mau_test_distributions-0.1.tar.gz

---
+++ packages/mau-test-distributions/mau_test_distributions-0.1.tar.gz/mau_test_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='mau_test_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['mau_test_distributions'],
+      author= 'Mauricio Maroto',
+      zip_safe=False)
