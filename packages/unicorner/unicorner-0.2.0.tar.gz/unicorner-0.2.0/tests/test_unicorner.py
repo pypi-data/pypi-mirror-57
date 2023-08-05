1e6ad1f1a2d3fed4c5c165191318681bd0d891d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:17 +0000

    Add package unicorner/0.2.0/unicorner-0.2.0.tar.gz

---
+++ packages/unicorner/unicorner-0.2.0.tar.gz/unicorner-0.2.0/tests/test_unicorner.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+
+import pytest
+
+from unicorner import SeasonParse
+from unicorner.dtos import SeasonDto, GameDto
+from unicorner.extraction import create_extraction
+
+
+@pytest.fixture
+def data_dir() -> Path:
+    return Path(__file__).parents[0] / "data"
+
+
+def test_parses_season_standings_and_fixtures(data_dir):
+    sp = SeasonParse()
+    sp.parse_standings_page(path=data_dir / "season-114-standings.html")
+    sp.parse_fixtures_page(path=data_dir / "season-114-fixtures.html")
+
+    assert sp.season_name == "Spring 2019"
+    assert sp.season_id == 114
+    assert sp.league_id == 505
+    assert sp.division_id == 3568
+    assert len(sp.teams) == 8
+    assert len(sp.game_days) == 12
+
+
+def test_extraction(data_dir):
+    extraction = create_extraction(input_dir=data_dir)
+    assert len(extraction[SeasonDto]) == 1
+    assert extraction[SeasonDto][0].id == 114
+    assert len(extraction[GameDto]) == 44
