83ef6a0d0f387483038a9055a41c984cfae78965
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:17 +0000

    Add package cfa-taz/0.3.2/cfa-taz-0.3.2.tar.gz

---
+++ packages/cfa-taz/cfa-taz-0.3.2.tar.gz/cfa-taz-0.3.2/taz/_version.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------
+# hermes: _version.py
+#
+# Copyright (C) 2018-2019, Christophe Fauchard
+# -----------------------------------------------------------------
+
+__version_info__ = (0, 3, 2)
+
+__version__ = '.'.join(map(str, __version_info__))
