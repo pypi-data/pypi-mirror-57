fc2567140ef91863de6c150bdb1b1d17ad87ef91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:22 +0000

    Add package explainerdashboard/0.0.4/explainerdashboard-0.0.4.tar.gz

---
+++ packages/explainerdashboard/explainerdashboard-0.0.4.tar.gz/explainerdashboard-0.0.4/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='explainerdashboard',
+    version='0.0.4',
+    description='package to ease and speed up machine learning model explainability',
+    license='MIT',
+    packages=find_packages(),
+    package_dir={'explainerdashboard':'explainerdashboard'}, # the one line where all the magic happens
+    package_data={
+        'explainerdashboard': ['assets/*', 'datasets/*'],
+    },
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Web Environment",
+        "Framework :: Dash",
+        "Framework :: Flask",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence"],
+    install_requires=['dash', 'dash-daq', 'dash-bootstrap-components',
+                    'dtreeviz', 'numpy', 'pandas', 'PDPbox', 'scikit-learn', 'shap'],
+    author='Oege Dijk',
+    author_email='oegedijk@gmail.com',
+    keywords=['machine learning', 'explainability', 'shap', 'feature importances', 'dash'],
+    url='https://github.com/oegedijk/explainerdashboard'
+)
