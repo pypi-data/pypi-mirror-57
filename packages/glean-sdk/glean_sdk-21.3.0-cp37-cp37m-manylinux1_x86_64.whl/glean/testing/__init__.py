22fe1a93b7851c1e74a547ac10d10e67e5492c6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:20 +0000

    Add package glean-sdk/21.3.0/glean_sdk-21.3.0-cp37-cp37m-manylinux1_x86_64.whl

---
+++ packages/glean-sdk/glean_sdk-21.3.0-cp37-cp37m-manylinux1_x86_64.whl/glean/testing/__init__.py
@@ -0,0 +1,39 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+from typing import Optional
+
+
+from glean import Configuration
+from .error_type import ErrorType  # noqa
+
+
+def reset_glean(
+    *,
+    application_id: str,
+    application_version: str,
+    configuration: Optional[Configuration] = None,
+):
+    """
+    Resets the Glean singleton.
+
+    Args:
+        application_id (str): The application id to use when sending pings.
+        application_version (str): The version of the application sending
+            Glean data.
+        configuration (glean.config.Configuration): (optional) An object with
+            global settings.
+    """
+    from glean import Glean
+
+    Glean.reset()
+    Glean.initialize(
+        application_id=application_id,
+        application_version=application_version,
+        configuration=configuration,
+    )
+
+
+__all__ = ["reset_glean", "ErrorType"]
