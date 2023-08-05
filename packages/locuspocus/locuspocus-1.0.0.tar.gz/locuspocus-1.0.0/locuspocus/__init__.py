a9e06ab2fff24faae184e0589967a78b33e7e7e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:29 +0000

    Add package locuspocus/1.0.0/locuspocus-1.0.0.tar.gz

---
+++ packages/locuspocus/locuspocus-1.0.0.tar.gz/locuspocus-1.0.0/locuspocus/__init__.py
@@ -0,0 +1,11 @@
+__version__ = '1.0.0'
+__all__ = ['Locus','RefLoci','Fasta']
+
+
+from .chromosome import Chromosome
+from .fasta import Fasta
+
+from .locus import Locus
+from .loci import Loci
+
+from .term import Term
