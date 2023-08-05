82adf242f39287fca5002dea09b51a4d80263744
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:32 +0000

    Add package izzy/0.1.59/izzy-0.1.59.tar.gz

---
+++ packages/izzy/izzy-0.1.59.tar.gz/izzy-0.1.59/izzy/__init__.py
@@ -0,0 +1,22 @@
+
+from .classification import *
+from .datasets import *
+from .defaults import *
+from .eda import *
+from .features import *
+from .io import *
+from .misc import *
+from .regression import *
+from .viz import *
+
+__all__ = [
+    'classification',
+    'datasets',
+    'eda',
+    'features',
+    'io',
+    'misc',
+    'regression',
+    'set_theme',  # from defaults
+    'viz',
+]
