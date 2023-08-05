11de137fc3cc750dbba8e3b8e06644503e063298
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:55 +0000

    Add package TracWikiExtras/1.3.1/TracWikiExtras-1.3.1-py2-none-any.whl

---
+++ packages/TracWikiExtras/TracWikiExtras-1.3.1-py2-none-any.whl/tracwikiextras/__init__.py
@@ -0,0 +1,10 @@
+import pkg_resources
+from boxes import AboutWikiBoxes, Boxes
+from color import Color
+from entities import Entities
+from icons import AboutWikiIcons, Icons, ShowIcons, Smileys
+from phrases import AboutWikiPhrases, Phrases
+from symbols import Symbols
+from unc_paths import UNCPathLink
+
+pkg_resources.require('Trac >= 1.0')
