e609abc6e7467c215042ed174d0818b69d1bdc58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:50 +0000

    Add package isc-py-common/0.0.91/isc_py_common-0.0.91-py3-none-any.whl

---
+++ packages/isc-py-common/isc_py_common-0.0.91-py3-none-any.whl/isc_common/ws/progressStack.py
@@ -0,0 +1,55 @@
+import json
+
+import websocket
+
+
+class ProgressStack:
+    host = None
+    port = None
+    channel = None
+
+    def __init__(self, **kwargs):
+        for k, v in kwargs.items():
+            setattr(self, k, v() if callable(v) else v)
+
+        if not self.host:
+            raise Exception(f'Not specified a host.')
+
+        if not self.port:
+            raise Exception(f'Not specified a port.')
+
+        if not self.channel:
+            raise Exception(f'Not specified a channel.')
+
+    def _send(self, message):
+        if not isinstance(message, dict):
+            raise Exception(f'Message must be a dict.')
+        url = f'ws://{self.host}:{self.port}/ws/{self.channel}/'
+
+        ws = websocket.create_connection(url)
+        ws.send(json.dumps(message))
+        ws.close()
+
+    def show(self, title, label_contents, id, cntAll=0):
+        self._send(dict(
+            labelContents=label_contents,
+            progressBarId=id,
+            title=title,
+            type='show_progress',
+            cntAll=cntAll
+        ))
+
+    def close(self, id):
+        self._send(dict(type='close_progress', progressBarId=id))
+
+    def setContentsLabel(self, labelContents, id):
+        self._send(dict(type='set_contents_label', labelContents=labelContents, progressBarId=id))
+
+    def setTitleProgress(self, title, id):
+        self._send(dict(type='set_title_progress', title=title, progressBarId=id))
+
+    def setPercentsDone(self, percent, id):
+        self._send(dict(type='set_percent_done_progress', percent=percent, progressBarId=id))
+
+    def setCntDone(self, cnt, id):
+        self._send(dict(type='set_cnt_done_progress', cnt=cnt, progressBarId=id))
