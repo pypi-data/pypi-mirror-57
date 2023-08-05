59ddc052d1657c75ffac686dc0c4acd6bad6d670
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:25 +0000

    Add package pyqalx/0.8.1/pyqalx-0.8.1.tar.gz

---
+++ packages/pyqalx/pyqalx-0.8.1.tar.gz/pyqalx-0.8.1/pyqalx/core/entities/bot.py
@@ -0,0 +1,6 @@
+from pyqalx.core.entities import QalxEntity
+
+
+class Bot(QalxEntity):
+    """QalxEntity with entity_type Bot"""
+    entity_type = 'bot'
---
+++ packages/pyqalx/pyqalx-0.8.1.tar.gz/pyqalx-0.8.1/pyqalx/core/entities/worker.py
@@ -0,0 +1,8 @@
+from pyqalx.core.entities import QalxEntity
+
+
+class Worker(QalxEntity):
+    """QalxEntity with entity_type Worker
+
+    """
+    entity_type = 'worker'
