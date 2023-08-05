c2dbfb043c887125ca19c3eaf9599c3dcdb2ebfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:04 +0000

    Add package demonware/0.0.1/demonware-0.0.1.tar.gz

---
+++ packages/demonware/demonware-0.0.1.tar.gz/demonware-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'demonware',
+  packages = ['demonware'],
+  version = '0.0.1',
+  license='MIT',
+  description = '',
+  author = 'kpan',
+  author_email = 'kpan@demonware.net',
+  url = '',
+  keywords = ['Placeholder'],
+  install_requires=["six"],
+)
