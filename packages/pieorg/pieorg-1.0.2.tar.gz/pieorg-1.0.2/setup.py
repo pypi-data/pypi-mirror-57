18a5c481082d2ecfbcb3b1efa518f95f599234b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:52 +0000

    Add package pieorg/1.0.2/pieorg-1.0.2.tar.gz

---
+++ packages/pieorg/pieorg-1.0.2.tar.gz/pieorg-1.0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+import pathlib
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+setup(name='pieorg',
+      version='1.0.2',
+      description='A Python CLI app to organise every story you read on the internet.',
+      long_description=README,
+      long_description_content_type="text/markdown",
+      author='Yash Prakash',
+      author_email="yashprakash13@gmail.com",
+      py_modules=['app'],
+      license="MIT",
+      url="https://github.com/yashprakash13/PieOrg",
+      packages=["pieorg"],
+      install_requires=['click', 'art'],
+      entry_points='''
+        [console_scripts]
+        pieorg=pieorg.app:begin
+    ''')
