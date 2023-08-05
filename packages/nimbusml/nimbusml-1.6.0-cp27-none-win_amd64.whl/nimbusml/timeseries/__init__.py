cd6d30eb1781c0990fb0fafd05f89483e5a4f28d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:18 +0000

    Add package nimbusml/1.6.0/nimbusml-1.6.0-cp27-none-win_amd64.whl

---
+++ packages/nimbusml/nimbusml-1.6.0-cp27-none-win_amd64.whl/nimbusml/timeseries/__init__.py
@@ -0,0 +1,13 @@
+from .iidspikedetector import IidSpikeDetector
+from .iidchangepointdetector import IidChangePointDetector
+from .ssaspikedetector import SsaSpikeDetector
+from .ssachangepointdetector import SsaChangePointDetector
+from .ssaforecaster import SsaForecaster
+
+__all__ = [
+    'IidSpikeDetector',
+    'IidChangePointDetector',
+    'SsaSpikeDetector',
+    'SsaChangePointDetector',
+    'SsaForecaster'
+]
