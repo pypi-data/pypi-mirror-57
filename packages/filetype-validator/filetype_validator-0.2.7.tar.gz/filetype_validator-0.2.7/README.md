222f59900cc955aebe671cfba3f0dfb30e9bcc9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:58 +0000

    Add package filetype-validator/0.2.7/filetype_validator-0.2.7.tar.gz

---
+++ packages/filetype-validator/filetype_validator-0.2.7.tar.gz/filetype_validator-0.2.7/README.md
@@ -0,0 +1,6 @@
+# FILETYPE VALIDATOR
+A simple wrapper for filetype.py as a validator 
+between file extensions and header signatures 
+based on this list https://en.wikipedia.org/wiki/List_of_file_signatures.
+We need wrapper on this because of the same header signature for
+some different file extensions.
---
+++ packages/filetype-validator/filetype_validator-0.2.7.tar.gz/filetype_validator-0.2.7/filetype_validator/main.py
@@ -0,0 +1,50 @@
+import filetype
+import os
+from .extension_definition import matching_extensions
+from io import BufferedReader
+
+def determine_extension(file_object):
+    file_name = ""
+    try:
+        # first try to extract filename from FileStorage or 
+        # BufferedReader object
+        if type(file_object) == BufferedReader:
+            file_name = file_object.name
+        elif type(file_object) == str:
+            file_name = os.path.basename(file_object)
+        else:
+            # try from werkzeug FileStorage
+            file_name = file_object.filename
+    except AttributeError:
+        # Take no action for now
+        pass
+    else:
+        # return read pointer to initial so buffered reader object 
+        # can be re-read on caller side
+        file_object.seek(0)
+    
+    file_ext = os.path.splitext(file_name)[-1]
+    # extension should always in lowercase to match with return from 
+    # filetype.py detection
+    return file_ext.replace(".", "").lower()
+
+def compare(file_extension, detected_extension):
+    # only check files that have extensions 
+    # check matching extensions for detected type
+    if file_extension and file_extension != detected_extension:
+        return file_extension in matching_extensions.get(detected_extension, [])
+    
+    return True
+
+def validate(file_object):
+    # get mime type from header signature information
+    # extensions for corresponsing header signature reference
+    # https://en.wikipedia.org/wiki/List_of_file_signatures
+    file_extension = determine_extension(file_object)
+    detected_extension = filetype.guess_extension(file_object)
+
+    return compare(file_extension, detected_extension)
+
+
+if __name__ == "__main__":
+     filetype_validator.validate("/Users/isna/temp/xls")
