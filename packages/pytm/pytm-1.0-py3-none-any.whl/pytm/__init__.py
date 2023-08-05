bc93888580d13c8e80c5058c4545d562c422daf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:09 +0000

    Add package pytm/1.0/pytm-1.0-py3-none-any.whl

---
+++ packages/pytm/pytm-1.0-py3-none-any.whl/pytm/__init__.py
@@ -0,0 +1,4 @@
+__all__ = ['Element', 'Server', 'ExternalEntity', 'Datastore', 'Actor', 'Process', 'SetOfProcesses', 'Dataflow', 'Boundary', 'TM', 'Lambda', 'Threat']
+
+from .pytm import Element, Server, ExternalEntity, Dataflow, Datastore, Actor, Process, SetOfProcesses, Boundary, TM, Lambda, Threat
+
