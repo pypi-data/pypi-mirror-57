7a873be788f0a1ce1696cad316286f4edb2ec0ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:53 +0000

    Add package janis-pipelines.core/0.7.8/janis_pipelines.core-0.7.8-py3-none-any.whl

---
+++ packages/janis-pipelines.core/janis_pipelines.core-0.7.8-py3-none-any.whl/janis_core/__init__.py
@@ -0,0 +1,68 @@
+"""
+Welcome to the source code for Janis!
+
+Janis is a framework creating specialised, simple workflow definitions that are
+transpiled to Common Workflow Language or Workflow Definition Language.
+
+Below you'll find the classes you can access by importing Janis:
+
+>>  import janis as j
+
+Some noteworthy classes are Workflow, CommandTool and the janis.translations module
+
+Some terminology:
+    - Edge:
+        - An edge may exist between 2 nodes, it represents a dependency
+        - Every edge will have a source_map which is indexed by the tag of the input on the finish node
+        - This source_map value is singular or a list, as you can connect multiple sources to one input
+
+
+
+_Proudly made on Planet Earth._
+
+"""
+
+import sys
+import pkg_resources
+
+
+# PEP396:  https://www.python.org/dev/peps/pep-0396/
+from janis_core.__meta__ import __version__
+from janis_core.registry.shed import JanisShed
+import janis_core.registry.entrypoints as entrypoints
+from janis_core.utils.scatter import ScatterDescription, ScatterMethods
+from janis_core.hints import CaptureType, Engine, HINTS, Hint, HintEnum, HintArray
+from janis_core.tool.commandtool import CommandTool, ToolArgument, ToolInput, ToolOutput
+from janis_core.tool.tool import Tool, ToolTypes
+from janis_core.translations import SupportedTranslations
+from janis_core.types import (
+    InputSelector,
+    WildcardSelector,
+    MemorySelector,
+    CpuSelector,
+    StringFormatter,
+)
+from janis_core.types.common_data_types import (
+    Boolean,
+    String,
+    Int,
+    Float,
+    Double,
+    File,
+    Directory,
+    Array,
+    Filename,
+    Stdout,
+    Stderr,
+)
+from janis_core.types.data_types import DataType
+from janis_core.utils import get_value_for_hints_and_ordered_resource_tuple
+from janis_core.utils.logger import Logger, LogLevel
+from janis_core.utils.metadata import Metadata, WorkflowMetadata, ToolMetadata
+
+from janis_core.workflow.workflow import Workflow, WorkflowBuilder
+
+# from janis_core.workflow.input import Input
+# from janis_core.workflow.output import Output
+# from janis_core.workflow.step import Step
+# from janis_core.workflow.workflow import Workflow
---
+++ packages/janis-pipelines.core/janis_pipelines.core-0.7.8-py3-none-any.whl/janis_core/tool/tool.py
@@ -0,0 +1,138 @@
+import re
+from abc import ABC, abstractmethod
+from typing import Optional, List, Dict, Any, Union
+
+from janis_core.types import (
+    ParseableType,
+    Selector,
+    Array,
+    get_instantiated_type,
+    DataType,
+)
+from janis_core.utils.logger import Logger
+from janis_core.utils.metadata import Metadata
+from janis_core.utils.validators import Validators
+
+ToolType = str
+
+
+class ToolTypes:
+    Workflow: ToolType = "workflow"
+    CommandTool: ToolType = "command-tool"
+    ExpressionTool: ToolType = "expression-tool"
+
+
+class TInput(object):
+    def __init__(self, tag: str, intype: DataType, default=None, doc: str = None):
+        self.tag = tag
+        self.intype = intype
+        self.default = default
+        self.doc = doc
+
+    def id(self):
+        return self.tag
+
+
+class TOutput(object):
+    def __init__(self, tag, outtype, doc: str = None):
+        self.tag = tag
+        self.outtype = outtype
+        self.doc = doc
+
+    def id(self):
+        return self.tag
+
+
+class Tool(ABC, object):
+    """
+    One of Workflow, CommandLineTool, ExpressionTool* (* unimplemented)
+    """
+
+    def __init__(self, metadata_class=Metadata, **connections):
+        """
+        :param metadata_class:
+        :param connections:
+        """
+
+        self.metadata: metadata_class = metadata_class()
+        meta = self.bind_metadata()
+        if meta:
+            self.metadata = meta
+
+        self.connections = connections
+
+    @classmethod
+    @abstractmethod
+    def type(cls) -> ToolType:
+        raise Exception(f"'{cls}' must implement type() method")
+
+    @abstractmethod
+    def id(self) -> str:
+        raise Exception("Must implement id() method")
+
+    @staticmethod
+    def tool_module():
+        return None
+
+    @staticmethod
+    def tool_provider():
+        return None
+
+    @abstractmethod
+    def tool_inputs(self) -> List[TInput]:
+        raise Exception("Must implement inputs() method")
+
+    @abstractmethod
+    def tool_outputs(self) -> List[TOutput]:
+        raise Exception("Must implement outputs() method")
+
+    def inputs_map(self) -> Dict[str, TInput]:
+        return {inp.tag: inp for inp in self.tool_inputs()}
+
+    def outputs_map(self) -> Dict[str, TOutput]:
+        return {outp.tag: outp for outp in self.tool_outputs()}
+
+    def friendly_name(self) -> Optional[str]:
+        """
+        Overriding this method is not required UNLESS you distribute your tool.
+        Generating the docs will fail if your tool does not provide a name.
+
+        :return: A friendly name of your tool
+        """
+        return None
+
+    def all_input_keys(self) -> List[str]:
+        return [t.id() for t in self.tool_inputs()]
+
+    @abstractmethod
+    def generate_inputs_override(
+        self, additional_inputs=None, with_resource_overrides=False, hints=None
+    ):
+        pass
+
+    def __call__(self, **connections):
+        self.connections = connections
+        return self
+
+    @staticmethod
+    @abstractmethod
+    def version():
+        return None
+
+    def doc(self) -> Optional[str]:
+        return None
+
+    @abstractmethod
+    def translate(
+        self, translation: str, with_docker=True, with_resource_overrides=False
+    ):
+        raise Exception("Subclass must provide implementation for 'translate()' method")
+
+    def bind_metadata(self):
+        """
+        A convenient place to add metadata about the tool. You are guaranteed that self.metadata will exist.
+        It's possible to return a new instance of the ToolMetadata / WorkflowMetadata which will be rebound.
+        This is usually called after the initialiser, though it may be called multiple times.
+        :return:
+        """
+        return self.metadata
