c56c65c4c532f966596ff567f989b5df63805bee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:07 +0000

    Add package ezutils/0.0.4/ezutils-0.0.4.tar.gz

---
+++ packages/ezutils/ezutils-0.0.4.tar.gz/ezutils-0.0.4/ezutils/__init__.py
@@ -0,0 +1,18 @@
+from .files import readlines, writelines, readstr, readjson
+
+__all__ = (
+    "__title__", "__summary__", "__uri__", "__version__", "__author__",
+    "__email__", "__license__", "__copyright__", readlines, writelines, readstr, readjson
+)
+
+__title__ = "ezutils"
+__summary__ = "Utils to save your time on python coding"
+__uri__ = "https://github.com/caojianfeng/ezutils"
+
+__version__ = "0.0.1"
+
+__author__ = "JeffreyCao"
+__email__ = "je255reycao@stufft.io"
+
+__license__ = "MIT Licence"
+__copyright__ = "Copyright 2019 JeffreyCao"
