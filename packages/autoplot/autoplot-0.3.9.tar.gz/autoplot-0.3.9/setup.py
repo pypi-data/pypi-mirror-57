010383cd53a673daef85e65cd23aad6a8b6c97f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:44 +0000

    Add package autoplot/0.3.9/autoplot-0.3.9.tar.gz

---
+++ packages/autoplot/autoplot-0.3.9.tar.gz/autoplot-0.3.9/setup.py
@@ -0,0 +1,30 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    print('setuptools not available. Try:\n\tpip install setuptools --user')
+
+import sys
+import platform
+
+if platform.system() == 'Darwin':
+    import os
+    os.system("export MACOSX_DEPLOYMENT_TARGET=10.10")
+
+install_requires = ["jpype1","numpy"]
+
+if sys.argv[1] == 'develop':
+    install_requires.append("pytest")
+
+setup(
+    name='autoplot',
+    version='0.3.9',
+    author='Jeremy Faden',
+    author_email='faden@cottagesystems.com',
+    packages=find_packages(), 
+    url='http://pypi.python.org/pypi/autoplot',
+    license='LICENSE.txt',
+    description='Interface to Autoplot Java library',
+    long_description=open('README.rst').read(),
+    install_requires=install_requires
+)
+
