c7bd44cad83225fe23bdb28047d0c350ca6b4ea3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:34 +0000

    Add package django-contrib-comments/1.9.2/django-contrib-comments-1.9.2.tar.gz

---
+++ packages/django-contrib-comments/django-contrib-comments-1.9.2.tar.gz/django-contrib-comments-1.9.2/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+long_description = readme + history
+
+setup(
+    name='django-contrib-comments',
+    version='1.9.2',
+    url="https://github.com/django/django-contrib-comments",
+    description='The code formerly known as django.contrib.comments.',
+    long_description=long_description,
+    author='Django Software Foundation',
+    author_email='jacob@jacobian.org',
+    license='BSD',
+    platforms='any',
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
+        'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.0',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    include_package_data=True,
+    test_suite='tests.runtests.main',
+    install_requires=['Django>=1.11', 'six']
+)
