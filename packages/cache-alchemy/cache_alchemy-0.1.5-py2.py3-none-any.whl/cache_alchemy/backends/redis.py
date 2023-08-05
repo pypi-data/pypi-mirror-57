81a844cc71a454a0aed2a3bb3bfb24fcbe5cf4cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:19 +0000

    Add package cache-alchemy/0.1.5/cache_alchemy-0.1.5-py2.py3-none-any.whl

---
+++ packages/cache-alchemy/cache_alchemy-0.1.5-py2.py3-none-any.whl/cache_alchemy/backends/redis.py
@@ -0,0 +1,66 @@
+import json
+from typing import Callable, TypeVar, Union
+
+from configalchemy.types import JsonSerializable
+
+from .base import BaseCache
+from ..config import DefaultConfig
+
+ReturnType = TypeVar("ReturnType", bound=JsonSerializable)
+FunctionType = Callable[..., ReturnType]
+
+
+class DistributedCache(BaseCache):
+    def __init__(self, *, cached_function: FunctionType, **kwargs):
+        super().__init__(cached_function=cached_function, **kwargs)
+        self.client = DefaultConfig.get_current_config().cache_redis_client
+
+    def get(self, *args, **kwargs) -> ReturnType:
+        keyword_args, kwargs, cache_key = self.make_key(args, kwargs)
+        with self.cache_context(cache_key):
+            result = self.client.get(cache_key)
+            if result is None:
+                with self.miss_context(cache_key):
+                    value = self.cached_function(*args, **keyword_args, **kwargs)
+                    self.set(cache_key, value)
+                    return value
+            else:
+                return self.process_result(result)
+
+    def set(self, key: str, value: JsonSerializable) -> None:
+        self.client.sadd(self.namespace_set, self.namespace)
+        value = self.process_value(value)
+        while self.client.scard(self.namespace) >= self.limit:
+            del_key = self.client.spop(self.namespace)
+            self.client.delete(del_key)
+
+        pipe = self.client.pipeline()
+        if self.expire == -1:
+            pipe.set(key, value)
+        else:
+            pipe.setex(key, self.expire, value)
+
+        pipe.sadd(self.namespace, key)
+        pipe.execute()
+
+    def cache_clear(self, args: tuple, kwargs: dict) -> bool:
+        if args or kwargs:
+            pattern = self.make_key_pattern(args=args, kwargs=kwargs,)
+            self.client.delete(
+                *(filter(pattern.match, self.client.smembers(self.namespace)))
+            )
+        else:
+            with self.client.pipeline() as pipe:
+                pipe.delete(*self.client.smembers(self.namespace))
+                pipe.delete(self.namespace)
+                pipe.srem(self.namespace_set, self.namespace)
+                pipe.execute()
+        return True
+
+    def process_value(self, value: ReturnType) -> str:
+        return json.dumps(value)
+
+    def process_result(self, result: Union[str, bytes]) -> ReturnType:
+        if isinstance(result, bytes):
+            result = result.decode()
+        return json.loads(result)
