01edb20118a9eb1779868099a9fa935149f9c32e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:30 +0000

    Add package ocaml/0.0.2/ocaml-0.0.2.tar.gz

---
+++ packages/ocaml/ocaml-0.0.2.tar.gz/ocaml-0.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='ocaml',
+    version='0.0.2',
+    author='Laurent Mazare',
+    author_email='lmazare@gmail.com',
+    packages=[''],
+    package_dir={'': '.'},
+    package_data={'': ['ocaml.so']},
+)
