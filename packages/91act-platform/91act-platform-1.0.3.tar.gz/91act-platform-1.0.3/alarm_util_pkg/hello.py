fff9bfef83c3d36c4f4c452565195b55b21d3215
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:28 +0000

    Add package 91act-platform/1.0.3/91act-platform-1.0.3.tar.gz

---
+++ packages/91act-platform/91act-platform-1.0.3.tar.gz/91act-platform-1.0.3/alarm_util_pkg/hello.py
@@ -0,0 +1,6 @@
+from core import SimpleCronControl
+import os
+
+# SimpleCronControl("lilei").list_cron()
+SimpleCronControl("lilei").cron_control("http://127.0.0.1:5000/test","My Token")
+# print os.path.abspath(__file__)
