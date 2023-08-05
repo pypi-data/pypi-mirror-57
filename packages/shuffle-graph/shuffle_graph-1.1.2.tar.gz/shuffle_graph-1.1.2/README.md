9d11a166e5f7edc7524a783f6ef9390bf8688096
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:23 +0000

    Add package shuffle-graph/1.1.2/shuffle_graph-1.1.2.tar.gz

---
+++ packages/shuffle-graph/shuffle_graph-1.1.2.tar.gz/shuffle_graph-1.1.2/README.md
@@ -0,0 +1,37 @@
+# shuffle_graph
+
+![PyPI](https://img.shields.io/pypi/v/shuffle_graph?color=red)
+![PyPI - Status](https://img.shields.io/pypi/status/shuffle_graph)
+![GitHub Release Date](https://img.shields.io/github/release-date/fsssosei/shuffle_graph)
+[![Build Status](https://scrutinizer-ci.com/g/fsssosei/shuffle_graph/badges/build.png?b=master)](https://scrutinizer-ci.com/g/fsssosei/shuffle_graph/build-status/master)
+[![Code Intelligence Status](https://scrutinizer-ci.com/g/fsssosei/shuffle_graph/badges/code-intelligence.svg?b=master)](https://scrutinizer-ci.com/code-intelligence)
+[![Language grade: Python](https://img.shields.io/lgtm/grade/python/g/fsssosei/shuffle_graph.svg?logo=lgtm&logoWidth=18)](https://lgtm.com/projects/g/fsssosei/shuffle_graph/context:python)
+[![Codacy Badge](https://api.codacy.com/project/badge/Grade/eee9f9c7d45a49808774f88351942b7b)](https://www.codacy.com/manual/fsssosei/shuffle_graph?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=fsssosei/shuffle_graph&amp;utm_campaign=Badge_Grade)
+[![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/fsssosei/shuffle_graph/badges/quality-score.png?b=master)](https://scrutinizer-ci.com/g/fsssosei/shuffle_graph/?branch=master)
+![PyPI - Downloads](https://img.shields.io/pypi/dw/shuffle_graph?label=PyPI%20-%20Downloads)
+![PyPI - Python Version](https://img.shields.io/pypi/pyversions/shuffle_graph)
+![PyPI - License](https://img.shields.io/pypi/l/shuffle_graph)
+
+*Graph shuffling package in python.*
+
+## Installation
+
+Installation can be done through pip. You must have python version >= 3.7
+
+	pip install shuffle-graph
+
+## Usage
+
+The statement to import the package:
+
+	from shuffle_graph_package import *
+	
+Example:
+
+	>>> calculate_number_of_shuffles_required_under_default_random_function(10000)
+	6
+
+	>>> from networkx.classes.graph import Graph
+	>>> G = Graph({0: {1: {}}, 1: {0: {}, 2: {}}, 2: {1: {}, 3: {}}, 3: {2: {}, 4: {}}, 4: {3: {}}})
+	>>> shuffle_graph(G, 1, 65535).adj  #Set seed to make the results repeatable.
+	AdjacencyView({3: {2: {}, 4: {}}, 4: {3: {}}, 1: {0: {}, 2: {}}, 2: {3: {}, 1: {}}, 0: {1: {}}})
---
+++ packages/shuffle-graph/shuffle_graph-1.1.2.tar.gz/shuffle_graph-1.1.2/src/shuffle_graph_package/shuffle_graph_module.py
@@ -0,0 +1,93 @@
+'''
+shuffle_graph - This is a graph shuffling package.
+Copyright (C) 2019  sosei
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
+
+__all__ = ['calculate_number_of_shuffles_required_under_default_random_function', 'shuffle_graph']
+
+def calculate_number_of_shuffles_required_under_default_random_function(node_number: int) -> int:
+    '''
+        Python's random number function USES the Mersenne Twister algorithm, which has a period of 2**19937-1.If the total permutation of graph nodes is larger than the random function period, the card cannot be shuffled only once.
+        The total permutation of a graph node is "factorial of the number of nodes".The number of binary digits of the total number of permutations can be calculated by Stirling's formula.
+        
+        >>> calculate_number_of_shuffles_required_under_default_random_function(1000)
+        1
+        >>> calculate_number_of_shuffles_required_under_default_random_function(10000)
+        6
+    '''
+    
+    import math
+    if node_number > 0:
+        bit_length_of_permutation_number = math.ceil(math.log2(2*math.pi*node_number)/2 + math.log2(node_number/math.e)*node_number)
+        shuffle_number = math.ceil(bit_length_of_permutation_number/19937)
+    else:
+        shuffle_number = 0
+    return shuffle_number
+
+def shuffle_graph(data_graph: 'NetworkXGraphObject', shuffle_number: int, seed: int = None) -> 'NetworkXGraphObject':
+    '''
+        Returns a new graph, shuffling the order of the nodes in the input data_graph, but the relationship between the nodes remains the same. The data_graph doesn't change.
+        
+        Parameters
+        ----------
+        data_graph : NetworkXGraphObject
+            A NetworkX graph object.
+        
+        shuffle_number : integer
+            Set the number of shuffles.
+        
+        seed : integer, random_state, or None (default)
+            Indicator of random number generation state.
+        
+        Returns
+        -------
+        new_order_data_graph : NetworkXGraphObject
+            Returns a new graph that shuffles the order of nodes but keeps the relationships between them the same.
+        
+        Examples
+        --------
+        >>> from networkx.classes.graph import Graph
+        >>> G = Graph({0: {1: {}}, 1: {0: {}, 2: {}}, 2: {1: {}, 3: {}}, 3: {2: {}, 4: {}}, 4: {3: {}}})
+        >>> shuffle_graph(G, 1, 65535).adj  #Set seed to make the results repeatable.
+        AdjacencyView({3: {2: {}, 4: {}}, 4: {3: {}}, 1: {0: {}, 2: {}}, 2: {3: {}, 1: {}}, 0: {1: {}}})
+    '''
+    
+    import random
+    from networkx.classes.graph import Graph
+    from networkx.classes.digraph import DiGraph
+    from networkx.classes.multigraph import MultiGraph
+    from networkx.classes.multidigraph import MultiDiGraph
+    from networkx.convert import from_dict_of_dicts
+    
+    random.seed(seed)
+    
+    list_of_nodes = list(data_graph.nodes)
+    for _i in range(shuffle_number):
+        random.shuffle(list_of_nodes)
+    new_order_data_graph = dict()
+    for node in list_of_nodes:
+        new_order_data_graph.update({node: data_graph[node]})
+    if data_graph.is_directed():
+        if data_graph.is_multigraph():
+            new_order_data_graph = from_dict_of_dicts(new_order_data_graph, create_using = MultiDiGraph, multigraph_input = True)
+        else:
+            new_order_data_graph = from_dict_of_dicts(new_order_data_graph, create_using = DiGraph)
+    else:
+        if data_graph.is_multigraph():
+            new_order_data_graph = from_dict_of_dicts(new_order_data_graph, create_using = MultiGraph, multigraph_input = True)
+        else:
+            new_order_data_graph = from_dict_of_dicts(new_order_data_graph, create_using = Graph)
+    return new_order_data_graph
