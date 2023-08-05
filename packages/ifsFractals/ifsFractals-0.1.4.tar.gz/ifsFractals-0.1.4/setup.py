909d3b1ebb118dc49c64055e660517966905412c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:59 +0000

    Add package ifsFractals/0.1.4/ifsFractals-0.1.4.tar.gz

---
+++ packages/ifsFractals/ifsFractals-0.1.4.tar.gz/ifsFractals-0.1.4/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+from os import path
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name="ifsFractals",
+    packages=["ifsFractals"],
+    version="0.1.4",
+    author="Peter Francis",
+    author_email="franpe02@gettysburg.edu",
+    description="For Generating Fast IFS Fractals",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/francisp336/ifsFractals",
+    classifiers=[],
+    python_requires='>=3.0.0',
+    install_requires=[
+        'imageio',
+        'matplotlib',
+        'numba==0.45.1',
+        'numpy',
+        'Pillow',
+        'scipy',
+        # 'system',
+        'termcolor',
+        'typing'
+    ],
+    keywords='Fractal Generator'
+)
