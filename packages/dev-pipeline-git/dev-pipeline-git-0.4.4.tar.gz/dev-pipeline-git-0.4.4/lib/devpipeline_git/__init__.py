8698cdcf1ef991183db587015df243bd92a44685
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:36 +0000

    Add package dev-pipeline-git/0.4.4/dev-pipeline-git-0.4.4.tar.gz

---
+++ packages/dev-pipeline-git/dev-pipeline-git-0.4.4.tar.gz/dev-pipeline-git-0.4.4/lib/devpipeline_git/__init__.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+The main module for devpipeline_git (a dev-pipeline plugin).  It provides git
+support to dev-pipeline, and exposes the make_git function to create an Scm
+that supports git.
+"""
+
+
+def _check_missing_value(configuration, key, error_fn):
+    for name, component in configuration.items():
+        if component.get("scm.tool") == "git":
+            if key not in component:
+                error_fn("{} doesn't specify {}".format(name, key))
+
+
+def _check_missing_uri(configuration, error_fn):
+    _check_missing_value(configuration, "git.uri", error_fn)
+
+
+def _check_missing_revision(configuration, error_fn):
+    _check_missing_value(configuration, "git.revision", error_fn)
+
+
+_MAJOR = 0
+_MINOR = 4
+_PATCH = 4
+
+_STRING = "{}.{}.{}".format(_MAJOR, _MINOR, _PATCH)
---
+++ packages/dev-pipeline-git/dev-pipeline-git-0.4.4.tar.gz/dev-pipeline-git-0.4.4/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+from setuptools import setup, find_packages
+
+with open("README.rst") as f:
+    long_description = f.read()
+
+_VERSION = "0.4.0"
+
+setup(
+    name="dev-pipeline-git",
+    version="0.4.4",
+    package_dir={"": "lib"},
+    packages=find_packages("lib"),
+    install_requires=[
+        "dev-pipeline-core >= {}".format(_VERSION),
+        "dev-pipeline-scm >= {}".format(_VERSION),
+    ],
+    entry_points={
+        "devpipeline.scms": ["git = devpipeline_git.git:_GIT_SCM"],
+        "devpipeline.config_sanitizers": [
+            "missing-git-uri = devpipeline_git:_check_missing_uri",
+            "missing-git-revision = devpipeline_git:_check_missing_revision",
+        ],
+    },
+    author="Stephen Newell",
+    description="git plugin for dev-pipeline",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    license="BSD-2",
+    url="https://github.com/dev-pipeline/dev-pipeline-git",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Plugins",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Version Control :: Git",
+        "Topic :: Utilities",
+    ],
+)
