0f5c88250dadcacd63afeda83963e6fadb8aefbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:44 +0000

    Add package df-loader/1.0.2/df_loader-1.0.2.tar.gz

---
+++ packages/df-loader/df_loader-1.0.2.tar.gz/df_loader-1.0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(name='df_loader',
+      version='1.0.2',
+      license='MIT',
+      author='Karev Vitaliy',
+      author_email='Vitaliy.Karev@mvideo.ru',
+      description='To optimization load DataFrame from databases',
+      packages=find_packages(exclude=['tests', 'debug']),
+      #packages=['df_loader'],
+      include_package_data=True,
+      long_description=open('README.md').read(),
+      long_description_content_type="text/markdown",
+      install_requires=requirements,
+      test_suite='tests')
