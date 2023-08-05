e916cf4209537eea2c061eb7a3c55c05880ca4de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:06 +0000

    Add package chainladder/0.4.4/chainladder-0.4.4.tar.gz

---
+++ packages/chainladder/chainladder-0.4.4.tar.gz/chainladder-0.4.4/chainladder/utils/__init__.py
@@ -0,0 +1,8 @@
+""" utils should store all utility functions and classes, i.e. things that
+    are used by various modules in the package.
+"""
+from chainladder.utils.weighted_regression import WeightedRegression # noqa (API import)
+from chainladder.utils.exhibits import (
+    DataFrame, Row, Column, Tabs) # noqa (API import)
+from chainladder.utils.utility_functions import ( # noqa (API import)
+    load_dataset, parallelogram_olf, read_pickle, read_json)
---
+++ packages/chainladder/chainladder-0.4.4.tar.gz/chainladder-0.4.4/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+from os import listdir
+# import chainladder
+
+descr = "Chainladder Package - P&C Loss Reserving package "
+name = 'chainladder'
+url = 'https://github.com/jbogaardt/chainladder-python'
+version='0.4.4' # Put this in __init__.py
+
+data_path = ''
+setup(
+    name=name,
+    version=version,
+    maintainer='John Bogaardt',
+    maintainer_email='jbogaardt@gmail.com',
+    packages=['{}.{}'.format(name, p) for p in find_packages(where=name)]+['chainladder'],
+    scripts=[],
+    url=url,
+    download_url='{}/archive/v{}.tar.gz'.format(url, version),
+    license='LICENSE',
+    include_package_data=True,
+    package_data={'data': [data_path+item
+                           for item in listdir('chainladder{}'.format(data_path))]},
+    description=descr,
+    # long_description=open('README.md').read(),
+    install_requires=[
+        "pandas>=0.23.0",
+        "numpy>=1.12.0",
+        "scikit-learn>=0.18.0",
+        "joblib"
+    ],
+)
