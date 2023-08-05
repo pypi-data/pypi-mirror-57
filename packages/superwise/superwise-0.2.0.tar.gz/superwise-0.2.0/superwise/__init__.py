6afcf1829dbb7ce9514fd7ecb618280d035373ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:57 +0000

    Add package superwise/0.2.0/superwise-0.2.0.tar.gz

---
+++ packages/superwise/superwise-0.2.0.tar.gz/superwise-0.2.0/superwise/__init__.py
@@ -0,0 +1,14 @@
+__version__ = '0.2.0'
+import logging.config
+import os
+
+import yaml
+
+from project_root import PROJECT_ROOT
+
+with open(os.path.join(PROJECT_ROOT, "superwise", "resources", "logger_config.yml"), 'r') as f:
+    log_cfg = yaml.safe_load(f.read())
+
+
+logging.config.dictConfig(log_cfg)
+logger = logging.getLogger('superwise')
