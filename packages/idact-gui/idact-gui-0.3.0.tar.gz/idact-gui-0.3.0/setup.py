2cd18023325fcff040e198c7850bdf16686343fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:04 +0000

    Add package idact-gui/0.3.0/idact-gui-0.3.0.tar.gz

---
+++ packages/idact-gui/idact-gui-0.3.0.tar.gz/idact-gui-0.3.0/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="idact-gui",
+    version="0.3.0",
+    author="Joanna Chyży, Karolina Piekarz, Piotr Swędrak",
+    author_email="piotrekswedrak@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/intdata-bsc/idact-gui",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    install_requires=['PyQt5<5.10'],
+    entry_points={
+        'console_scripts': [
+            'idactgui=gui.main:main',
+        ],
+    }
+)
