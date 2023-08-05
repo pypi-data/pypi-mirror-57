a298eaef7ea52446bc5988c5e6b74fda8d0c5fa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:03 +0000

    Add package pytest-pass/1.1/pytest_pass-1.1.tar.gz

---
+++ packages/pytest-pass/pytest_pass-1.1.tar.gz/pytest_pass-1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pytest_pass',
+    version='1.1',
+    packages=find_packages(),
+    author='elil',
+    author_email='eli3143@gmail.com',
+    description='Check out https://github.com/elilutsky/pytest-pass',
+    long_description='Check out https://github.com/elilutsky/pytest-pass'
+)
