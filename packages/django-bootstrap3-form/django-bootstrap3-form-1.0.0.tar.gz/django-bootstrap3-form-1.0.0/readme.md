504b9b1621782683ffb2e920d5dd7ddb53b5d1db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:05 +0000

    Add package django-bootstrap3-form/1.0.0/django-bootstrap3-form-1.0.0.tar.gz

---
+++ packages/django-bootstrap3-form/django-bootstrap3-form-1.0.0.tar.gz/django-bootstrap3-form-1.0.0/readme.md
@@ -0,0 +1,22 @@
+# Django Bootstrap 3 form
+
+[![Build Status](https://travis-ci.org/mbraak/django-bootstrap3-form.svg?branch=master)](https://travis-ci.org/mbraak/django-bootstrap3-form) [![Version](https://badge.fury.io/py/django-bootstrap3-form.svg)](https://pypi.python.org/pypi/django-bootstrap3-form/)
+
+[![Coverage Status](https://img.shields.io/coveralls/mbraak/django-bootstrap3-form.svg)](https://coveralls.io/r/mbraak/django-bootstrap3-form?branch=master)
+[![Requirements Status](https://requires.io/github/mbraak/django-bootstrap3-form/requirements.svg?branch=master)](https://requires.io/github/mbraak/django-bootstrap3-form/requirements/?branch=master)
+
+[![License](https://img.shields.io/pypi/l/django-bootstrap3-form.svg)](https://pypi.python.org/pypi/django-bootstrap3-form/)
+
+**Django-bootstrap3-form** allows you write Django forms that work with Twitter Bootstrap 3.
+
+It supports Django 2.0 - 3.0. Also tested with Python 3.6 - 3.7.
+
+Note that version 0.7.0 also supports Django 1.11.
+
+```python
+import django_bootstrap3_form
+
+class ExampleForm(django_bootstrap3_form.BootstrapForm):
+	username = django_bootstrap3_form.CharField()
+	password = django_bootstrap3_form.CharField(widget=django_bootstrap3_form.PasswordInput)
+```
---
+++ packages/django-bootstrap3-form/django-bootstrap3-form-1.0.0.tar.gz/django-bootstrap3-form-1.0.0/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+
+from os import path
+from io import open
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'readme.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+version = '1.0.0'
+
+setup(
+    name='django-bootstrap3-form',
+    version=version,
+    license='Apache License, Version 2.0',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    description='Django-bootstrap3-form allows you write Django forms that work with Twitter Bootstrap 3',
+    packages=find_packages(exclude=['testproject', 'testproject.*']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'Django',
+        'django_pony_forms'
+    ],
+    author='Marco Braak',
+    author_email='mbraak@ridethepony.nl',
+    url='https://github.com/mbraak/django-bootstrap3-form',
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Django",
+        "Framework :: Django :: 2.0",
+        "Framework :: Django :: 2.1",
+        "Framework :: Django :: 2.2",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7"
+    ]
+)
