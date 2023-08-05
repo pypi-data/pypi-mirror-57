f9369fd8c83eaf99d4ad00c9dcec36ceb43ada9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:51 +0000

    Add package gocept.xmlrpcskin/1.1/gocept.xmlrpcskin-1.1-py2.py3-none-any.whl

---
+++ packages/gocept.xmlrpcskin/gocept.xmlrpcskin-1.1-py2.py3-none-any.whl/gocept/xmlrpcskin/testing.py
@@ -0,0 +1,28 @@
+#############################################################################
+#
+# Copyright (c) 2011 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+import pkg_resources
+import unittest2
+import zope.app.testing.functional
+
+
+layer = zope.app.testing.functional.ZCMLLayer(
+    pkg_resources.resource_filename(__name__, 'ftesting.zcml'),
+    __name__, 'layer', allow_teardown=True)
+
+
+class TestCase(zope.app.testing.functional.FunctionalTestCase,
+               unittest2.TestCase):
+
+    layer = layer
---
+++ packages/gocept.xmlrpcskin/gocept.xmlrpcskin-1.1-py2.py3-none-any.whl/gocept/xmlrpcskin/tests/test_endtoend.py
@@ -0,0 +1,37 @@
+#############################################################################
+#
+# Copyright (c) 2011 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+import gocept.xmlrpcskin.testing
+import gocept.xmlrpcskin.tests.fixture
+import zope.app.testing.xmlrpc
+import zope.configuration.xmlconfig
+import zope.testbrowser.testing
+
+
+class SkinTest(gocept.xmlrpcskin.testing.TestCase):
+    """Testing the registration end to end."""
+
+    def test_view_is_registered_only_for_layer(self):
+        zope.configuration.xmlconfig.file(
+            'configure.zcml', gocept.xmlrpcskin.tests.fixture)
+
+        all = zope.app.testing.xmlrpc.ServerProxy('http://localhost/')
+        foo = zope.app.testing.xmlrpc.ServerProxy(
+            'http://localhost/++skin++foo')
+
+        self.assertEqual(dict(returncode=1), all.all_layers())
+        self.assertEqual(dict(returncode=1), foo.all_layers())
+        self.assertEqual(dict(returncode=2), foo.foo_layer())
+        self.assertRaisesRegex(
+            Exception, 'NotFound.*foo_layer', all.foo_layer)
