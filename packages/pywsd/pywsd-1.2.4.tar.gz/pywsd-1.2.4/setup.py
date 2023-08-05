9d829ef0b6e9500dfe1704f05cc77ea302a88413
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:49 +0000

    Add package pywsd/1.2.4/pywsd-1.2.4.tar.gz

---
+++ packages/pywsd/pywsd-1.2.4.tar.gz/pywsd-1.2.4/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python -*- coding: utf-8 -*-
+#
+# Python Word Sense Disambiguation (pyWSD)
+#
+# Copyright (C) 2014-17 alvations
+# URL:
+# For license information, see LICENSE.md
+
+from distutils.core import setup
+
+setup(
+    name='pywsd',
+    version='1.2.4',
+    packages=['pywsd'],
+    description='Python WSD',
+    long_description='Python Implementations of Word Sense Disambiguation (WSD) technologies',
+    url = 'https://github.com/alvations/pywsd',
+    package_data={'pywsd': ['data/signatures/*.pkl',]},
+    license="MIT",
+    install_requires = ['nltk', 'numpy', 'pandas', 'wn', 'six']
+)
