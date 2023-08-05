f2722a62ab746b9b80fb886db90e0e48f10cc728
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:44 +0000

    Add package shortmomi/1.9.2/shortmomi-1.9.2.tar.gz

---
+++ packages/shortmomi/shortmomi-1.9.2.tar.gz/shortmomi-1.9.2/shortmomi/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+from .affinity_rules import *
+from .connect import ConnectionError
+from .connect import connect
+from .custom_attributes import *
+from .views import *
