32cf982988b10da6e2b673a84a09808aaa7a0950
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:34 +0000

    Add package terminalmushroom/3.0.0/terminalmushroom-3.0.0-py3-none-any.whl

---
+++ packages/terminalmushroom/terminalmushroom-3.0.0-py3-none-any.whl/terminalmushroom/cli.py
@@ -0,0 +1,28 @@
+import sys
+import os
+import os.path
+import terminalmushroom
+
+
+def cli():
+    if len(sys.argv) not in [2, 3]:
+        print('Usage: {script} input-file [output-file]'.format(script=terminalmushroom.__name__))
+        return
+
+    in_file = sys.argv[1]
+    with open(in_file, 'r') as f:
+        flat_string = f.read()
+
+    output = terminalmushroom.compile_source(flat_string, False, os.path.basename(in_file))
+
+    if len(sys.argv) == 3:
+        out_file = sys.argv[2]
+        with open(out_file, 'wb') as f:
+            f.write(output)
+    else:
+        os.write(sys.stdout.fileno(), output)
+        sys.stdout.flush()
+
+
+if __name__ == '__main__':
+    cli()
