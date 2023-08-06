260b525bba23279dfe913592eb6a55f3f3b764a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:16 +0000

    Add package jiraya/0.1.3/jiraya-0.1.3.tar.gz

---
+++ packages/jiraya/jiraya-0.1.3.tar.gz/jiraya-0.1.3/setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+
+setup(
+    name='jiraya',
+    description='Simple Jira Command Line Interface.',
+    author='Rodrigo Chacon',
+    author_email='rochacon@gmail.com',
+    url='https://github.com/rochacon/jiraya',
+    license='MIT',
+    version='0.1.3',
+    install_requires=open('requirements.txt').readlines(),
+    scripts=['jiraya'],
+)
