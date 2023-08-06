a509e520292f4b462a73f7985328a8dae186b127
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:49 +0000

    Add package azurectx/0.0.1/azurectx-0.0.1.tar.gz

---
+++ packages/azurectx/azurectx-0.0.1.tar.gz/azurectx-0.0.1/README.md
@@ -0,0 +1,24 @@
+# azurectx
+
+Script for switching Azure Contexts (Default subscription).  Inspired by the
+awesomeness of [kubectx](https://github.com/ahmetb/kubectx)
+
+## Usage
+
+Run without any arguments to bring up a prompt
+
+```
+azurectx
+```
+
+Set the context non interactively with `-s`
+
+```
+azurectx -s 'Subscription name'
+```
+
+List existing subscriptions with `-l`
+
+```
+azurectx -l
+```
---
+++ packages/azurectx/azurectx-0.0.1.tar.gz/azurectx-0.0.1/setup.py
@@ -0,0 +1,24 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+with open('requirements.txt') as requirements_file:
+    install_requirements = requirements_file.read().splitlines()
+
+setup(
+    name="azurectx",
+    version="0.0.1",
+    author="Drew Stinnett",
+    author_email="drew@drewlink.com",
+    description=("Switch between default Azure subscriptions"),
+    install_requires=install_requirements,
+    license="BSD",
+    keywords="azure az",
+    scripts=['scripts/azurectx'],
+    long_description_content_type="text/markdown",
+    long_description=read('README.md'),
+)
