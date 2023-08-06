abc0c14d667ebe0ad294ae895f271e93b7114f52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:15 +0000

    Add package mypy-boto3-kinesisvideo/1.10.39.0/mypy-boto3-kinesisvideo-1.10.39.0.tar.gz

---
+++ packages/mypy-boto3-kinesisvideo/mypy-boto3-kinesisvideo-1.10.39.0.tar.gz/mypy-boto3-kinesisvideo-1.10.39.0/mypy_boto3_kinesisvideo/__init__.py
@@ -0,0 +1,7 @@
+"Main interface for kinesisvideo service"
+
+from mypy_boto3_kinesisvideo.client import KinesisVideoClient as Client, KinesisVideoClient
+from mypy_boto3_kinesisvideo.paginator import ListSignalingChannelsPaginator, ListStreamsPaginator
+
+
+__all__ = ("Client", "KinesisVideoClient", "ListSignalingChannelsPaginator", "ListStreamsPaginator")
