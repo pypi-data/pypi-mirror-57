a61291650eb66ac3c7f9f26cf2fbb92737d01b14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:07 +0000

    Add package airQInsight/0.2.1/airQInsight-0.2.1.tar.gz

---
+++ packages/airQInsight/airQInsight-0.2.1.tar.gz/airQInsight-0.2.1/README.md
@@ -0,0 +1,4 @@
+# airQ-insight
+Works on Air Quality Indication Dataset collected by **airQ** to give deeper insight, written with :heart:
+
+**More coming soon ...**
---
+++ packages/airQInsight/airQInsight-0.2.1.tar.gz/airQInsight-0.2.1/airQInsight/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+'''
+    A tool designed to power exploratory data analysis, on airQ collected Air Quality Indicator Data.
+'''
+
+__version__ = '0.2.1'
+__author__ = 'Anjan Roy<anjanroy@yandex.com>'
