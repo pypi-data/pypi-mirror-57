584d5ef1c2d01b6278576571a217cf5823646fa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:37 +0000

    Add package legit/1.2.0/legit-1.2.0-py2.py3-none-any.whl

---
+++ packages/legit/legit-1.2.0-py2.py3-none-any.whl/legit/core.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+"""
+legit.core
+~~~~~~~~~~
+
+This module provides the basic functionality of legit.
+"""
+
+from . import bootstrap
+del bootstrap
+
+__version__ = '1.2.0'
+__author__ = 'Kenneth Reitz'
+__license__ = 'BSD'
