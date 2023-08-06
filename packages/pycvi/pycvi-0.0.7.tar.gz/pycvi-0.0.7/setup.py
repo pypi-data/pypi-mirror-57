1c4739c881ac4ae3cfa7f134e5668328a3675754
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:38 +0000

    Add package pycvi/0.0.7/pycvi-0.0.7.tar.gz

---
+++ packages/pycvi/pycvi-0.0.7.tar.gz/pycvi-0.0.7/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+# see https://setuptools.readthedocs.io/en/latest/setuptools.html#basic-use
+
+setup(name='pycvi',
+      version='0.0.7',
+      description='Easily compute clustering cliterions',
+      url='',
+      author='Yohan Foucade',
+      author_email='foucade@lipn.fr',
+      license='MIT',
+      packages=find_packages(exclude=['tests']),
+      zip_safe=False,
+      install_requires=['numpy', 'pandas'])
