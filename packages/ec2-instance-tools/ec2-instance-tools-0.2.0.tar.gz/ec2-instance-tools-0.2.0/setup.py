1665cd599e4a34e4cd0eb7f65518e66740334a6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:47 +0000

    Add package ec2-instance-tools/0.2.0/ec2-instance-tools-0.2.0.tar.gz

---
+++ packages/ec2-instance-tools/ec2-instance-tools-0.2.0.tar.gz/ec2-instance-tools-0.2.0/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import os.path
+
+from ec2_instance_tools import __version__
+from setuptools import setup, find_packages  # @UnresolvedImport
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name="ec2-instance-tools",
+      version=__version__,
+      description="Useful instrumentation for EC2 instances",
+      author="Caltech IMSS ADS",
+      author_email="imss-ads-staff@caltech.edu",
+      url="https://github.com/caltechads/ec2-instance-tools",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      keywords=['aws', 'devops'],
+      classifiers=[
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3"
+      ],
+      packages=find_packages(),
+      include_package_data=True,
+      install_requires=[
+          "boto3 >= 1.7.55",
+      ],
+      entry_points={'console_scripts': [
+          'sshec2 = ec2_instance_tools.sshec2:main',
+          'ec2namer = ec2_instance_tools.namer:main'
+      ]}
+      )
