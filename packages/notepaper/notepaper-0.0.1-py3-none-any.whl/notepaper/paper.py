d9790ea67b056d2a9b63281707aaaffa0aebb617
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:36 +0000

    Add package notepaper/0.0.1/notepaper-0.0.1-py3-none-any.whl

---
+++ packages/notepaper/notepaper-0.0.1-py3-none-any.whl/notepaper/paper.py
@@ -0,0 +1,15 @@
+__all__ = ['get_paper_list']
+
+
+def get_paper_list():
+    paper_list = [
+        {
+            "name": "DeepFM: A Factorization-Machine based Neural Network for CTR Prediction",
+            "url": "https://arxiv.org/pdf/1703.04247.pdf",
+            "year": 2017,
+        }, {
+            "url": "https://arxiv.org/pdf/1703.04247.pdf"
+        }
+    ]
+
+    return paper_list
