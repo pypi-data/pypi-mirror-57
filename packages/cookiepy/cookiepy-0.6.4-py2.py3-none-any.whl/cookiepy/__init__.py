38d408228d5a8df0cbb52dfa7dd73956fbc81da8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:14 +0000

    Add package cookiepy/0.6.4/cookiepy-0.6.4-py2.py3-none-any.whl

---
+++ packages/cookiepy/cookiepy-0.6.4-py2.py3-none-any.whl/cookiepy/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from .files import *
+from .xml_to_csv import *
+from .recipe import *
+"""Top-level package for cookiepy."""
+
+__author__ = """Nikos Koumbakis"""
+__email__ = 'n.koumbakis@gmail.com'
+__version__ = '0.6.4'
