3883484ebd7bd88e4fd58f42c55a336be0cc28be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:50 +0000

    Add package jrpyintroduction/0.1.7/jrpyintroduction-0.1.7.tar.gz

---
+++ packages/jrpyintroduction/jrpyintroduction-0.1.7.tar.gz/jrpyintroduction-0.1.7/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['jrpyintroduction', 'jrpyintroduction.datasets']
+
+package_data = \
+{'': ['*'],
+ 'jrpyintroduction': ['vignettes/*'],
+ 'jrpyintroduction.datasets': ['data/*']}
+
+install_requires = \
+['matplotlib>=3.0', 'numpy>=1.14.4', 'pandas>=0.23']
+
+setup_kwargs = {
+    'name': 'jrpyintroduction',
+    'version': '0.1.7',
+    'description': '',
+    'long_description': None,
+    'author': 'Jamie',
+    'author_email': 'jamie@jumpingrivers.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.5',
+}
+
+
+setup(**setup_kwargs)
