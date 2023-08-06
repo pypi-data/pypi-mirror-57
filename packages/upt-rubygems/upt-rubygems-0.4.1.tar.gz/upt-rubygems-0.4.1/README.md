a05fa07beeecddf3d8a539ef9a4ee9278270d93b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:30 +0000

    Add package upt-rubygems/0.4.1/upt-rubygems-0.4.1.tar.gz

---
+++ packages/upt-rubygems/upt-rubygems-0.4.1.tar.gz/upt-rubygems-0.4.1/README.md
@@ -0,0 +1,2 @@
+# Universal Packaging Tool: RubyGems frontend
+This is a [RubyGems](https://rubygems.org/) frontend for [upt](https://pypi.python.org/pypi/upt).
---
+++ packages/upt-rubygems/upt-rubygems-0.4.1.tar.gz/upt-rubygems-0.4.1/setup.py
@@ -0,0 +1,6 @@
+# Copyright 2018      Cyril Roelandt
+#
+# Licensed under the 3-clause BSD license. See the LICENSE file.
+from setuptools import setup
+
+setup()
