32ee588866460b70b76bf6dd2d8586ec64e941c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:38 +0000

    Add package anyscale/0.0.8.post2/anyscale-0.0.8.post2.tar.gz

---
+++ packages/anyscale/anyscale-0.0.8.post2.tar.gz/anyscale-0.0.8.post2/setup.py
@@ -0,0 +1,32 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import re
+
+from setuptools import setup, find_packages, Distribution
+
+def find_version(path):
+    with open(path) as f:
+        match = re.search(
+            r"^__version__ = ['\"]([^'\"]*)['\"]", f.read(), re.MULTILINE)
+        if match:
+            return match.group(1)
+        raise RuntimeError("Unable to find version string.")
+
+setup(
+    name="anyscale",
+    version=find_version("anyscale/__init__.py"),
+    author="Anyscale Inc.",
+    description=("Command Line Interface for Anyscale"),
+    packages=find_packages(),
+    install_requires=[
+        "boto3", "Click", "GitPython", "jsonschema", "ray", "requests", "tabulate"
+    ],
+    entry_points={
+        "console_scripts": [
+            "anyscale=anyscale.scripts:main"
+        ]
+    },
+    include_package_data=True,
+    zip_safe=False)
