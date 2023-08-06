e5e7eb93480134e0792df666c51171f273210c74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:40 +0000

    Add package mypy-boto3-chime/1.10.34/mypy-boto3-chime-1.10.34.tar.gz

---
+++ packages/mypy-boto3-chime/mypy-boto3-chime-1.10.34.tar.gz/mypy-boto3-chime-1.10.34/mypy_boto3_chime/paginator.py
@@ -0,0 +1,47 @@
+"Main interface for chime service Paginators"
+from __future__ import annotations
+
+from botocore.paginate import Paginator as Boto3Paginator
+from mypy_boto3_chime.type_defs import (
+    ListAccountsPaginatePaginationConfigTypeDef,
+    ListAccountsPaginateResponseTypeDef,
+    ListUsersPaginatePaginationConfigTypeDef,
+    ListUsersPaginateResponseTypeDef,
+)
+
+
+__all__ = ("ListAccountsPaginator", "ListUsersPaginator")
+
+
+class ListAccountsPaginator(Boto3Paginator):
+    """
+    Paginator for `list_accounts`
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self,
+        Name: str = None,
+        UserEmail: str = None,
+        PaginationConfig: ListAccountsPaginatePaginationConfigTypeDef = None,
+    ) -> ListAccountsPaginateResponseTypeDef:
+        """
+        [ListAccounts.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.34/reference/services/chime.html#Chime.Paginator.ListAccounts.paginate)
+        """
+
+
+class ListUsersPaginator(Boto3Paginator):
+    """
+    Paginator for `list_users`
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self,
+        AccountId: str,
+        UserEmail: str = None,
+        PaginationConfig: ListUsersPaginatePaginationConfigTypeDef = None,
+    ) -> ListUsersPaginateResponseTypeDef:
+        """
+        [ListUsers.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.34/reference/services/chime.html#Chime.Paginator.ListUsers.paginate)
+        """
