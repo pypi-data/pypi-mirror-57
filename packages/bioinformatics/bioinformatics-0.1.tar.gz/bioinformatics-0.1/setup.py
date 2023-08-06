ae6292fdff9bcedd5ba92d4d12cf9b63cbba184f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:54 +0000

    Add package bioinformatics/0.1/bioinformatics-0.1.tar.gz

---
+++ packages/bioinformatics/bioinformatics-0.1.tar.gz/bioinformatics-0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name='bioinformatics',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/chembiodrew/bioinformatics',
+      download_url = 'https://github.com/chembiodrew/bioinformatics/archive/v_01.tar.gz', 
+      author='ChemBioDrew',
+      author_email='ajhendrickson97@berkeley.edu',
+      license='MIT',
+      packages=['bioinformatics'],
+      zip_safe=False)
