8c29b346b84f37b49e8d17584844df7fd365336a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:07 +0000

    Add package nesterzsw/1.4.0/nesterzsw-1.4.0.tar.gz

---
+++ packages/nesterzsw/nesterzsw-1.4.0.tar.gz/nesterzsw-1.4.0/nesterzsw.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+'''该模块提供一个名为print_lol的函数，用于输出列表中的各个元素，包含
+   嵌套列表中的元素'''
+import sys
+def print_lol (the_list,indent=False,level=0,WritePath=sys.stdout):
+    '''the_list为输入列表，indent选择是否对嵌套表元素进行缩进输出，
+level选择整体缩进的程度，函数可将列表中的元素输出（包含嵌套表中的元素）,
+WritePath为数组中元素的输出路径'''
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_lol(each_item,indent,level+1,WritePath)
+        else:
+            for tab_stop in range(level):
+                if indent:
+                    print("\t",end='',file=WritePath)
+            print(each_item,file=WritePath)
+
