911c9e74ac0de70f2cb9f9f374e2161ad223ec07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:55 +0000

    Add package jsonlog-cli/3.1.0/jsonlog_cli-3.1.0-py2.py3-none-any.whl

---
+++ packages/jsonlog-cli/jsonlog_cli-3.1.0-py2.py3-none-any.whl/jsonlog_cli/config.py
@@ -0,0 +1,95 @@
+from __future__ import annotations
+
+import dataclasses
+import json
+import logging
+import pathlib
+import typing
+
+import jsonschema
+import xdg
+
+import jsonlog_cli.pattern
+import jsonlog_cli.record
+
+log = logging.getLogger(__name__)
+
+DEFAULT_CONFIG_PATH = xdg.XDG_CONFIG_HOME / "jsonlog" / "config.json"
+DEFAULT_LOG_PATH = xdg.XDG_CACHE_HOME / "jsonlog" / "internal.log"
+DEFAULT_CONFIG = {
+    "jsonlog": jsonlog_cli.pattern.KeyValuePattern(
+        keys=("timestamp", "level", "name", "message"), multiline_keys=("traceback",)
+    ),
+    "highlight": jsonlog_cli.pattern.TemplatePattern(template="{__message__}"),
+    "elasticsearch": jsonlog_cli.pattern.KeyValuePattern(
+        keys=(
+            "timestamp",
+            "level",
+            "type",
+            "component",
+            "cluster.name",
+            "node.name",
+            "message",
+        ),
+        multiline_keys=("stacktrace",),
+        multiline_json=True,
+    ),
+}
+CONFIG_SCHEMA = {
+    "type": "object",
+    "properties": {
+        "patterns": {
+            "type": "object",
+            "additionalProperties": {"$ref": "#/definitions/pattern"},
+        }
+    },
+    "definitions": {
+        "pattern": {
+            "$id": "#pattern",
+            "type": "object",
+            "properties": {
+                "template": {"type": "string"},
+                "level_key": {"type": "string"},
+                "multiline_keys": {"type": "array", "items": {"type": "string"}},
+            },
+            "required": ["template"],
+        }
+    },
+}
+
+
+@dataclasses.dataclass()
+class Config:
+    patterns: typing.Dict[str, jsonlog_cli.pattern.Pattern] = dataclasses.field(
+        default_factory=dict
+    )
+
+    @classmethod
+    def configure(cls, path: typing.Optional[str]) -> Config:
+        config = Config(DEFAULT_CONFIG)
+
+        if path is not None:
+            log.info(f"Reading configuration from file = {path!r}")
+            config.load(pathlib.Path(path))
+        elif DEFAULT_CONFIG_PATH.exists():
+            log.info(f"Reading configuration from default file = {DEFAULT_CONFIG_PATH}")
+            config.load(DEFAULT_CONFIG_PATH)
+
+        return config
+
+    def load(self, path: pathlib.Path) -> None:
+        instance = json.loads(pathlib.Path(path).read_text(encoding="utf-8"))
+        jsonschema.validate(instance=instance, schema=CONFIG_SCHEMA)
+        for k, v in instance.get("patterns", {}).items():
+            self.patterns[k] = jsonlog_cli.pattern.TemplatePattern(**v)
+
+
+def ensure_log_path(path: str) -> typing.Optional[str]:
+    """
+    If given a path to a potential logfile, ensure the containing directory exists.
+    """
+    if path == "-":
+        return None
+
+    pathlib.Path(path).parent.mkdir(exist_ok=True)
+    return path
---
+++ packages/jsonlog-cli/jsonlog_cli-3.1.0-py2.py3-none-any.whl/jsonlog_cli/tests/test_record.py
@@ -0,0 +1,58 @@
+import dataclasses
+
+import pytest
+
+from jsonlog_cli.record import Record
+from jsonlog_cli.pattern import Pattern
+
+
+@dataclasses.dataclass()
+class Example:
+    line: str
+    expected: str
+    pattern: Pattern
+
+    def record(self):
+        return Record.from_string(self.line)
+
+
+@pytest.mark.parametrize(
+    "example",
+    [
+        Example(
+            line='{"timestamp": "2019-06-26", "message": "Hello World"}',
+            expected="2019-06-26 Hello World",
+            pattern=Pattern(template="{timestamp} {message}"),
+        ),
+        Example(
+            line='{"@timestamp": "2019-06-26", "@message": "Hello World"}',
+            expected="2019-06-26 Hello World",
+            pattern=Pattern(template="{@timestamp} {@message}"),
+        ),
+        # Test message coloring.
+        Example(
+            line='{"message": "Hello World", "level": "CRITICAL"}',
+            expected="\x1b[31m\x1b[1mCRITICAL Hello World\x1b[0m",
+            pattern=Pattern(template="{level} {message}", level_key="level"),
+        ),
+        # Test nested keys can be used in both the format string and config keys.
+        Example(
+            line='{"nested": {"message": "Hello World", "multiline": "Lorem Ipsum", "level": "CRITICAL"}}',
+            expected="\x1b[31m\x1b[1mHello World\x1b[0m\n\n    \x1b[2mLorem Ipsum\x1b[0m\n",
+            pattern=Pattern(
+                template="{nested.message}",
+                level_key="nested.level",
+                multiline_keys=["nested.multiline"],
+            ),
+        ),
+        # Test nested data renders nicely.
+        Example(
+            line='{"nested": {"a": 1, "b": "Z", "c": []}}',
+            expected='static\n\n    \x1b[2m{\x1b[0m\n    \x1b[2m  "a": 1,\x1b[0m\n    \x1b[2m  "b": "Z",\x1b[0m\n    \x1b[2m  "c": []\x1b[0m\n    \x1b[2m}\x1b[0m\n',
+            pattern=Pattern(template="static", multiline_keys=["nested"]),
+        ),
+    ],
+)
+def test_examples(example: Example):
+    actual = example.record().format_record(formatter=example.pattern)
+    assert actual == example.expected, repr(actual)
