c0936293941b3735befa50856310726a2b1b0264
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:24 +0000

    Add package searchanonamuse/0.5/searchanonamuse-0.5.tar.gz

---
+++ packages/searchanonamuse/searchanonamuse-0.5.tar.gz/searchanonamuse-0.5/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='searchanonamuse',
+      version='0.05',
+      description='Receives login, password and path to ChromeDriver.exe as parameters and has a method'
+                  ' that do the search in MAM and returns the HTML resulting of the search',
+      url='https://github.com/samuelsonsev/searchanonamuse',
+      author='Sam',
+      author_email='none@example.com',
+      license='MIT',
+      packages=['searchanonamuse'],
+      zip_safe=False, install_requires = ['selenium'])
+
