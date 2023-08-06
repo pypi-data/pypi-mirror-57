efe0b5dfbb1b79030dc1b1ee6ec338598f68f0fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:04 +0000

    Add package datawrapper/0.1.1/datawrapper-0.1.1-py2.py3-none-any.whl

---
+++ packages/datawrapper/datawrapper-0.1.1-py2.py3-none-any.whl/datawrapper/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for datawrapper."""
+
+__author__ = """Sergio Sanchez"""
+__email__ = 'chekos@tacosdedatos.com'
+__version__ = '0.1.1'
+
+from .datawrapper import Datawrapper
+
+datawrapper = Datawrapper()
