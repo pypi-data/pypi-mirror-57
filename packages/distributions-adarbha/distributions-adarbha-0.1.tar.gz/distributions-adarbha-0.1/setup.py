f0db4a34ad940efce1290ac2cb156e0b26a46cc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:04 +0000

    Add package distributions-adarbha/0.1/distributions-adarbha-0.1.tar.gz

---
+++ packages/distributions-adarbha/distributions-adarbha-0.1.tar.gz/distributions-adarbha-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions-adarbha',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions-adarbha'],
+      zip_safe=False)
