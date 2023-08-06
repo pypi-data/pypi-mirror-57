25c1a69b0deffdffca5ee629c94e753b6806d3c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:21 +0000

    Add package pybackupper/2.1/pybackupper-2.1.tar.gz

---
+++ packages/pybackupper/pybackupper-2.1.tar.gz/pybackupper-2.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='pybackupper',
+      version='2.1',
+      url='https://github.com/SavelevMatthew/backupper',
+      license='MIT',
+      author='Matthew Savelev',
+      author_email='savelevmatthew@gmail.com',
+      description='Realise files copy on your Cloud Services',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      packages=find_packages(exclude=['tests']),
+      zip_safe=False)
