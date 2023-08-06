5e7ec2c12913594e60ce4f87c50bf77f1d5391ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:17 +0000

    Add package bolinette/0.0.14/Bolinette-0.0.14-py3-none-any.whl

---
+++ packages/bolinette/Bolinette-0.0.14-py3-none-any.whl/bolinette/cli/commands/create_api.py
@@ -0,0 +1,15 @@
+import os
+import virtualenv
+import subprocess
+
+from bolinette.cli import cli_env, utils
+
+
+def create_api(**kwargs):
+    cwd = cli_env['cwd']
+    origin = utils.join(cli_env['origin'], 'files')
+    params = {
+        'secret_key': utils.random_string(64),
+        'jwt_secret_key': utils.random_string(64),
+    }
+    utils.render_directory(utils.join(origin, 'api'), cwd, params)
