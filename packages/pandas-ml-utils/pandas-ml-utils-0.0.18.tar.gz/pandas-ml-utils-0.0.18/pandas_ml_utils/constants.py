0e782ea53da6739559f38656fbffc616bdd51c71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:59 +0000

    Add package pandas-ml-utils/0.0.18/pandas-ml-utils-0.0.18.tar.gz

---
+++ packages/pandas-ml-utils/pandas-ml-utils-0.0.18.tar.gz/pandas-ml-utils-0.0.18/pandas_ml_utils/constants.py
@@ -0,0 +1,13 @@
+# import privately to allow star import
+import numpy as _np
+
+PREDICTION_COLUMN_NAME = "prediction"
+FEATURE_COLUMN_NAME = "feature"
+TARGET_COLUMN_NAME = "target"
+LABEL_COLUMN_NAME = "label"
+LOSS_COLUMN_NAME = "loss"
+
+
+PROBABILITY_POSTFIX = "_proba"
+
+SIMULATED_VECTOR = _np.ones(10000)
