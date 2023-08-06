0ff4eb0c6824d2854bcb854b81cadfa6621d1c5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:29 +0000

    Add package mayan-edms/3.3.5/mayan_edms-3.3.5-py2.py3-none-any.whl

---
+++ packages/mayan-edms/mayan_edms-3.3.5-py2.py3-none-any.whl/mayan/apps/appearance/apps.py
@@ -0,0 +1,13 @@
+from __future__ import unicode_literals
+
+from django.utils.translation import ugettext_lazy as _
+
+from mayan.apps.common.apps import MayanAppConfig
+
+
+class AppearanceApp(MayanAppConfig):
+    name = 'mayan.apps.appearance'
+    verbose_name = _('Appearance')
+
+    def ready(self):
+        super(AppearanceApp, self).ready()
