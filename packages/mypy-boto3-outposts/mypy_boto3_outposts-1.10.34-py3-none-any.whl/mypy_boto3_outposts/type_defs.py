0c1bd372c2b1dfb903229b397166ccafde298029
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:01 +0000

    Add package mypy-boto3-outposts/1.10.34/mypy_boto3_outposts-1.10.34-py3-none-any.whl

---
+++ packages/mypy-boto3-outposts/mypy_boto3_outposts-1.10.34-py3-none-any.whl/mypy_boto3_outposts/type_defs.py
@@ -0,0 +1,104 @@
+"Main interface for outposts service type defs"
+from __future__ import annotations
+
+import sys
+from typing import List
+
+if sys.version_info >= (3, 8):
+    from typing import TypedDict
+else:
+    from typing_extensions import TypedDict
+
+
+ClientCreateOutpostResponseOutpostTypeDef = TypedDict(
+    "ClientCreateOutpostResponseOutpostTypeDef",
+    {
+        "OutpostId": str,
+        "OwnerId": str,
+        "OutpostArn": str,
+        "SiteId": str,
+        "Name": str,
+        "Description": str,
+        "LifeCycleStatus": str,
+        "AvailabilityZone": str,
+        "AvailabilityZoneId": str,
+    },
+    total=False,
+)
+
+ClientCreateOutpostResponseTypeDef = TypedDict(
+    "ClientCreateOutpostResponseTypeDef",
+    {"Outpost": ClientCreateOutpostResponseOutpostTypeDef},
+    total=False,
+)
+
+ClientGetOutpostInstanceTypesResponseInstanceTypesTypeDef = TypedDict(
+    "ClientGetOutpostInstanceTypesResponseInstanceTypesTypeDef", {"InstanceType": str}, total=False
+)
+
+ClientGetOutpostInstanceTypesResponseTypeDef = TypedDict(
+    "ClientGetOutpostInstanceTypesResponseTypeDef",
+    {
+        "InstanceTypes": List[ClientGetOutpostInstanceTypesResponseInstanceTypesTypeDef],
+        "NextToken": str,
+        "OutpostId": str,
+        "OutpostArn": str,
+    },
+    total=False,
+)
+
+ClientGetOutpostResponseOutpostTypeDef = TypedDict(
+    "ClientGetOutpostResponseOutpostTypeDef",
+    {
+        "OutpostId": str,
+        "OwnerId": str,
+        "OutpostArn": str,
+        "SiteId": str,
+        "Name": str,
+        "Description": str,
+        "LifeCycleStatus": str,
+        "AvailabilityZone": str,
+        "AvailabilityZoneId": str,
+    },
+    total=False,
+)
+
+ClientGetOutpostResponseTypeDef = TypedDict(
+    "ClientGetOutpostResponseTypeDef",
+    {"Outpost": ClientGetOutpostResponseOutpostTypeDef},
+    total=False,
+)
+
+ClientListOutpostsResponseOutpostsTypeDef = TypedDict(
+    "ClientListOutpostsResponseOutpostsTypeDef",
+    {
+        "OutpostId": str,
+        "OwnerId": str,
+        "OutpostArn": str,
+        "SiteId": str,
+        "Name": str,
+        "Description": str,
+        "LifeCycleStatus": str,
+        "AvailabilityZone": str,
+        "AvailabilityZoneId": str,
+    },
+    total=False,
+)
+
+ClientListOutpostsResponseTypeDef = TypedDict(
+    "ClientListOutpostsResponseTypeDef",
+    {"Outposts": List[ClientListOutpostsResponseOutpostsTypeDef], "NextToken": str},
+    total=False,
+)
+
+ClientListSitesResponseSitesTypeDef = TypedDict(
+    "ClientListSitesResponseSitesTypeDef",
+    {"SiteId": str, "AccountId": str, "Name": str, "Description": str},
+    total=False,
+)
+
+ClientListSitesResponseTypeDef = TypedDict(
+    "ClientListSitesResponseTypeDef",
+    {"Sites": List[ClientListSitesResponseSitesTypeDef], "NextToken": str},
+    total=False,
+)
