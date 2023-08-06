e98355791198766eeccb0eb0df8bdabdc450d10c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:16 +0000

    Add package upt-cran/0.1/upt_cran-0.1-py3-none-any.whl

---
+++ packages/upt-cran/upt_cran-0.1-py3-none-any.whl/upt_cran/tests/fake_html.py
@@ -0,0 +1,112 @@
+ellipsis_html = '''<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>CRAN - Package ellipsis</title>
+<link rel="stylesheet" type="text/css" href="../../CRAN_web.css" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="citation_title" content="Tools for Working with ... [R package ellipsis version 0.3.0]" />
+<meta name="citation_author" content="Hadley Wickham" />
+<meta name="citation_publication_date.Published" content="2019-09-20" />
+<meta name="citation_public_url" content="https://CRAN.R-project.org/package=ellipsis" />
+<meta name="DC.identifier" content="https://CRAN.R-project.org/package=ellipsis" />
+<meta name="DC.publisher" content="Comprehensive R Archive Network (CRAN)" />
+<style type="text/css">
+  table td { vertical-align: top; }
+</style>
+</head>
+<body>
+<h2>ellipsis: Tools for Working with ...</h2>
+<p>The ellipsis is a powerful tool for extending functions. Unfortunately 
+    this power comes at a cost: misspelled arguments will be silently ignored. 
+    The ellipsis package provides a collection of functions to catch problems
+    and alert the user.</p>
+<table summary="Package ellipsis summary">
+<tr>
+<td>Version:</td>
+<td>0.3.0</td>
+</tr>
+<tr>
+<td>Depends:</td>
+<td>R (&ge; 3.2)</td>
+</tr>
+<tr>
+<td>Imports:</td>
+<td><a href="../rlang/index.html">rlang</a> (&ge; 0.3.0)</td>
+</tr>
+<tr>
+<td>Suggests:</td>
+<td><a href="../covr/index.html">covr</a>, <a href="../testthat/index.html">testthat</a></td>
+</tr>
+<tr>
+<td>Published:</td>
+<td>2019-09-20</td>
+</tr>
+<tr>
+<td>Author:</td>
+<td>Hadley Wickham [aut, cre],
+  RStudio [cph]</td>
+</tr>
+<tr>
+<td>Maintainer:</td>
+<td>Hadley Wickham  &#x3c;&#x68;&#x61;&#x64;&#x6c;&#x65;&#x79;&#x20;&#x61;&#x74;&#x20;&#x72;&#x73;&#x74;&#x75;&#x64;&#x69;&#x6f;&#x2e;&#x63;&#x6f;&#x6d;&#x3e;</td>
+</tr>
+<tr>
+<td>BugReports:</td>
+<td><a href="https://github.com/r-lib/ellipsis/issues">https://github.com/r-lib/ellipsis/issues</a></td>
+</tr>
+<tr>
+<td>License:</td>
+<td><a href="../../licenses/GPL-3">GPL-3</a></td>
+</tr>
+<tr>
+<td>URL:</td>
+<td><a href="https://ellipsis.r-lib.org">https://ellipsis.r-lib.org</a>, <a href="https://github.com/r-lib/ellipsis">https://github.com/r-lib/ellipsis</a></td>
+</tr>
+<tr>
+<td>NeedsCompilation:</td>
+<td>yes</td>
+</tr>
+<tr>
+<td>Materials:</td>
+<td><a href="readme/README.html">README</a> <a href="news/news.html">NEWS</a> </td>
+</tr>
+<tr>
+<td>CRAN&nbsp;checks:</td>
+<td><a href="../../checks/check_results_ellipsis.html">ellipsis results</a></td>
+</tr>
+</table>
+<h4>Downloads:</h4>
+<table summary="Package ellipsis downloads">
+<tr>
+<td> Reference&nbsp;manual: </td>
+<td> <a href="ellipsis.pdf"> ellipsis.pdf </a> </td>
+</tr>
+<tr>
+<td> Package&nbsp;source: </td>
+<td> <a href="../../../src/contrib/ellipsis_0.3.0.tar.gz"> ellipsis_0.3.0.tar.gz </a> </td>
+</tr>
+<tr>
+<td> Windows&nbsp;binaries: </td>
+<td> r-devel: <a href="../../../bin/windows/contrib/4.0/ellipsis_0.3.0.zip">ellipsis_0.3.0.zip</a>, r-devel-gcc8: <a href="../../../bin/windows/contrib/r-devel-gcc8/ellipsis_0.3.0.zip">ellipsis_0.3.0.zip</a>, r-release: <a href="../../../bin/windows/contrib/3.6/ellipsis_0.3.0.zip">ellipsis_0.3.0.zip</a>, r-oldrel: <a href="../../../bin/windows/contrib/3.5/ellipsis_0.3.0.zip">ellipsis_0.3.0.zip</a> </td>
+</tr>
+<tr>
+<td> OS&nbsp;X&nbsp;binaries: </td>
+<td> r-release: <a href="../../../bin/macosx/el-capitan/contrib/3.6/ellipsis_0.3.0.tgz">ellipsis_0.3.0.tgz</a>, r-oldrel: <a href="../../../bin/macosx/el-capitan/contrib/3.5/ellipsis_0.3.0.tgz">ellipsis_0.3.0.tgz</a> </td>
+</tr>
+<tr>
+<td> Old&nbsp;sources: </td>
+<td> <a href="https://CRAN.R-project.org/src/contrib/Archive/ellipsis"> ellipsis archive </a> </td>
+</tr>
+</table>
+<h4>Reverse dependencies:</h4>
+<table summary="Package ellipsis reverse dependencies">
+<tr>
+<td>Reverse&nbsp;imports:</td>
+<td><a href="../dabestr/index.html">dabestr</a>, <a href="../devtools/index.html">devtools</a>, <a href="../distributions3/index.html">distributions3</a>, <a href="../forcats/index.html">forcats</a>, <a href="../learnr/index.html">learnr</a>, <a href="../sparklyr/index.html">sparklyr</a>, <a href="../statsExpressions/index.html">statsExpressions</a>, <a href="../testthat/index.html">testthat</a>, <a href="../tidyr/index.html">tidyr</a>, <a href="../vctrs/index.html">vctrs</a></td>
+</tr>
+</table>
+<h4>Linking:</h4>
+<p>Please use the canonical form
+<a href="https://CRAN.R-project.org/package=ellipsis"><samp>https://CRAN.R-project.org/package=ellipsis</samp></a>
+to link to this page.</p>
+</body>
+</html>'''
