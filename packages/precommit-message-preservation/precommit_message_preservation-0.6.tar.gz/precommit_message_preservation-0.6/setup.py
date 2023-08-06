2e6cc4f3dc37142a6ea28f44bbb4ed16211992c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:15 +0000

    Add package precommit-message-preservation/0.6/precommit_message_preservation-0.6.tar.gz

---
+++ packages/precommit-message-preservation/precommit_message_preservation-0.6.tar.gz/precommit_message_preservation-0.6/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+	install_requires = ["xdg==4.0.1"],
+	extras_require = {
+		"develop": [
+			"nose2",
+			"twine",
+		]
+	},
+	package_data={
+		"precommit_message_preservation": ["py.typed"],
+	},
+)
