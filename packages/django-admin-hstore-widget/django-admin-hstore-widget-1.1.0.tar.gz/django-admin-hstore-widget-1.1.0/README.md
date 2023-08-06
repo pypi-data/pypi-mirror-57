793673880004c273500689868249fe14c17b7854
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:43 +0000

    Add package django-admin-hstore-widget/1.1.0/django-admin-hstore-widget-1.1.0.tar.gz

---
+++ packages/django-admin-hstore-widget/django-admin-hstore-widget-1.1.0.tar.gz/django-admin-hstore-widget-1.1.0/README.md
@@ -0,0 +1,47 @@
+# django-admin-hstore-widget
+
+FormField that properly render HStoreField Data in django Admin based on [djangoauts package](https://github.com/djangonauts/django-hstore)
+
+[![Build Status](https://travis-ci.org/PokaInc/django-admin-hstore-widget.svg?branch=master)](https://travis-ci.org/PokaInc/django-admin-hstore-widget)
+
+
+## Requirements
+ * Python 2.7, 3.4, 3.5, 3.6, 3.7, 3.8
+ * Django 1.11, 2.0, 2.1, 2.2, 3.0
+ 
+ 
+Using pip:
+```bash
+pip install django-admin-hstore-widget
+```
+
+## Installation
+
+Add django_admin_hstore_widget to your INSTALLED_APPS ( in base.py )
+
+## Usage
+
+```python
+# yourmodel/admin.py
+from django.contrib import admin
+from django import forms
+
+from django_admin_hstore_widget.forms import HStoreFormField
+from models import Yourmodel
+
+class MyModelAdminForm(forms.ModelForm):
+    my_hstore_field = HStoreFormField()
+    
+    class Meta:
+       model = Yourmodel
+       exclude = ()
+    
+@admin.register(Yourmodel)
+class YourmodelAdmin(admin.ModelAdmin):
+    form = MyModelAdminForm
+    
+```
+
+## Result
+
+![Rendered result](results.png)
---
+++ packages/django-admin-hstore-widget/django-admin-hstore-widget-1.1.0.tar.gz/django-admin-hstore-widget-1.1.0/setup.py
@@ -0,0 +1,52 @@
+import os
+from setuptools import setup, find_packages
+
+version = '1.1.0'
+
+
+def get_package_data(package):
+    """
+    Return all files under the root package, that are not in a
+    package themselves.
+    """
+    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
+            for dirpath, dirnames, filenames in os.walk(package)
+            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+    filepaths = []
+    for base, filenames in walk:
+        filepaths.extend([os.path.join(base, filename)
+                          for filename in filenames])
+    return {package: filepaths}
+
+
+setup(
+    name='django-admin-hstore-widget',
+    version=version,
+    description="Widget and Field to properly render HStore data in Django-Admin",
+    keywords="django admin hstore render",
+    author='Alexandre Dufour',
+    author_email='adufour@poka.io',
+    maintainer='Alexandre Dufour',
+    maintainer_email='adufour@poka.io',
+    url='https://github.com/PokaInc/django-admin-hstore-widget',
+    packages=find_packages(),
+    package_data=get_package_data('django_admin_hstore_widget'),
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Framework :: Django',
+        'Framework :: Django :: 1.11',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    zip_safe=False,
+)
