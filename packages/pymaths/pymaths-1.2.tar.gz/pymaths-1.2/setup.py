9f67e6479cd33882557a6222728126a7937a2f22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:49 +0000

    Add package pymaths/1.2/pymaths-1.2.tar.gz

---
+++ packages/pymaths/pymaths-1.2.tar.gz/pymaths-1.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='pymaths',
+    version='1.2',
+    description='An advanced maths plugin for Python 3.',
+    author='Matthew Byrne',
+    author_email='matthewlukebyrne19@gmail.com',
+    license='MIT',
+    packages=['pymaths.functions', 'pymaths.constants'],
+    zip_safe=False)
