5652c34eb62c0d39872eeb21e07578fb33f1cbfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:12 +0000

    Add package dagit/0.6.6rc1/dagit-0.6.6rc1.tar.gz

---
+++ packages/dagit/dagit-0.6.6rc1.tar.gz/dagit-0.6.6rc1/dagit/webapp/build/precache-manifest.73ed6ca2941dda147e298f2102542533.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "916f3f70b2bed3b24d097867ccc3d62c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93fd75f414526fca5ae3",
+    "url": "/static/css/2.2123dcc0.chunk.css"
+  },
+  {
+    "revision": "93fd75f414526fca5ae3",
+    "url": "/static/js/2.03c8e868.chunk.js"
+  },
+  {
+    "revision": "077f8556a34fe11169e0d4e5603d53aa",
+    "url": "/static/js/2.03c8e868.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3195e7b0edfec766746d",
+    "url": "/static/js/main.7dca3d37.chunk.js"
+  },
+  {
+    "revision": "81d7b06d9d880039f30b",
+    "url": "/static/js/runtime-main.8246d28f.js"
+  },
+  {
+    "revision": "9468a8ebaf94983770942fae8c470448",
+    "url": "/static/media/icon-dependency-arrow.9468a8eb.svg"
+  },
+  {
+    "revision": "c38e7cf02acb4df1cb996268f777db43",
+    "url": "/static/media/icon-file.c38e7cf0.svg"
+  },
+  {
+    "revision": "0a65b35680fd43d941da24725278ad53",
+    "url": "/static/media/icon-link.0a65b356.svg"
+  },
+  {
+    "revision": "782154b83b6b419269980b375144b1f7",
+    "url": "/static/media/icons-16.782154b8.ttf"
+  },
+  {
+    "revision": "7fb844a70d4ff3aa49441242b4149777",
+    "url": "/static/media/icons-16.7fb844a7.woff"
+  },
+  {
+    "revision": "d4f6722b1ac527dd937155d419ec8ddd",
+    "url": "/static/media/icons-16.d4f6722b.eot"
+  },
+  {
+    "revision": "90afdd237a469c44c9a1a3b0ff2db197",
+    "url": "/static/media/icons-20.90afdd23.eot"
+  },
+  {
+    "revision": "c0e4874e23b8c9100e566ca2ca287128",
+    "url": "/static/media/icons-20.c0e4874e.woff"
+  },
+  {
+    "revision": "d1f0f6c1a1c239a03914f7bd06ccfe68",
+    "url": "/static/media/icons-20.d1f0f6c1.ttf"
+  },
+  {
+    "revision": "cf6b9dd9cbb5700820172af20b801559",
+    "url": "/static/media/nav-logo.cf6b9dd9.png"
+  }
+]);
