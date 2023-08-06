88fb232914c541eaea3b179f186feba793e0b6c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:14 +0000

    Add package markov-model-attribution/0.42/markov_model_attribution-0.42.tar.gz

---
+++ packages/markov-model-attribution/markov_model_attribution-0.42.tar.gz/markov_model_attribution-0.42/markov_model_attribution/__init__.py
@@ -0,0 +1,165 @@
+import pandas as pd
+import numpy as np
+import re
+
+
+def run_model(paths):
+    regex = re.compile('[^a-zA-Z> ]')
+    paths.rename(columns={paths.columns[0]: "Paths"}, inplace=True)
+    paths['Paths'] = paths['Paths'].apply(lambda x: regex.sub('', x))
+    markov_conversions = first_order(paths)
+    return markov_conversions
+
+
+def calculate_removals(df, base_cvr):
+    removal_effect_list = dict()
+    channels_to_remove = df.drop(['conv', 'null', 'start'], axis=1).columns
+    for channel in channels_to_remove:
+        removal_cvr_array = list()
+        removal_channel = channel
+        removal_df = df.drop(removal_channel, axis=1)
+        removal_df = removal_df.drop(removal_channel, axis=0)
+        for col in removal_df.columns:
+            one = float(1)
+            row_sum = np.sum(list(removal_df.loc[col]))
+            null_percent = one - row_sum
+            if null_percent == 0:
+                continue
+            else:
+                removal_df.loc[col]['null'] = null_percent
+        removal_df.loc['null']['null'] = 1.0
+        R = removal_df[['null', 'conv']]
+        R = R.drop(['null', 'conv'], axis=0)
+        Q = removal_df.drop(['null', 'conv'], axis=1)
+        Q = Q.drop(['null', 'conv'], axis=0)
+        t = len(Q.columns)
+
+        N = np.linalg.inv(np.identity(t) - np.asarray(Q))
+        M = np.dot(N, np.asarray(R))
+        removal_cvr = pd.DataFrame(M, index=R.index)[[1]].loc['start'].values[0]
+        removal_effect = 1 - removal_cvr / base_cvr
+        removal_effect_list[channel] = removal_effect
+    return removal_effect_list
+
+
+def first_order(paths):
+    paths = np.array(paths).tolist()
+    sublist = []
+    total_paths = 0
+    for path in paths:
+        for touchpoint in path:
+            userpath = touchpoint.split(' > ')
+            sublist.append(userpath)
+        total_paths += 1
+    paths = sublist
+
+    unique_touch_list = set(x for element in paths for x in element)
+    # get total last touch conversion counts
+    conv_dict = {}
+    total_conversions = 0
+    for item in unique_touch_list:
+        conv_dict[item] = 0
+    for path in paths:
+        if 'conv' in path:
+            total_conversions += 1
+            conv_dict[path[-2]] += 1
+
+    transitionStates = {}
+    base_cvr = total_conversions / total_paths
+    for x in unique_touch_list:
+        for y in unique_touch_list:
+            transitionStates[x + ">" + y] = 0
+
+    for possible_state in unique_touch_list:
+        if possible_state != "null" and possible_state != "conv":
+            # print(possible_state)
+            for user_path in paths:
+
+                if possible_state in user_path:
+                    indices = [i for i, s in enumerate(user_path) if possible_state == s]
+                    for col in indices:
+                        transitionStates[user_path[col] + ">" + user_path[col + 1]] += 1
+
+    transitionMatrix = []
+    actual_paths = []
+    for state in unique_touch_list:
+
+        if state != "null" and state != "conv":
+            counter = 0
+            index = [i for i, s in enumerate(transitionStates) if s.startswith(state + '>')]
+            for col in index:
+                if transitionStates[list(transitionStates)[col]] > 0:
+                    counter += transitionStates[list(transitionStates)[col]]
+            for col in index:
+                if transitionStates[list(transitionStates)[col]] > 0:
+                    state_prob = float((transitionStates[list(transitionStates)[col]])) / float(counter)
+                    actual_paths.append({list(transitionStates)[col]: state_prob})
+    transitionMatrix.append(actual_paths)
+
+    flattened_matrix = [item for sublist in transitionMatrix for item in sublist]
+    transState = []
+    transMatrix = []
+    for item in flattened_matrix:
+        for key in item:
+            transState.append(key)
+        for key in item:
+            transMatrix.append(item[key])
+
+    tmatrix = pd.DataFrame({'paths': transState,
+                            'prob': transMatrix})
+    # unique_touch_list = model['unique_touch_list']
+    tmatrix = tmatrix.join(tmatrix['paths'].str.split('>', expand=True).add_prefix('channel'))[
+        ['channel0', 'channel1', 'prob']]
+    column = list()
+    for k, v in tmatrix.iterrows():
+        if v['channel0'] in column:
+            continue
+        else:
+            column.append(v['channel0'])
+    test_df = pd.DataFrame()
+    for col in unique_touch_list:
+        test_df[col] = 0.00
+        test_df.loc[col] = 0.00
+    for k, v in tmatrix.iterrows():
+        x = v['channel0']
+        y = v['channel1']
+        val = v['prob']
+        test_df.loc[x][y] = val
+    test_df.loc['conv']['conv'] = 1.0
+    test_df.loc['null']['null'] = 1.0
+    R = test_df[['null', 'conv']]
+    R = R.drop(['null', 'conv'], axis=0)
+    Q = test_df.drop(['null', 'conv'], axis=1)
+    Q = Q.drop(['null', 'conv'], axis=0)
+    O = pd.DataFrame()
+    t = len(Q.columns)
+    for col in range(0, t):
+        O[col] = 0.00
+    for col in range(0, len(R.columns)):
+        O.loc[col] = 0.00
+    N = np.linalg.inv(np.identity(t) - np.asarray(Q))
+    M = np.dot(N, np.asarray(R))
+    base_cvr = pd.DataFrame(M, index=R.index)[[1]].loc['start'].values[0]
+    removal_effects = calculate_removals(test_df, base_cvr)
+    denominator = np.sum(list(removal_effects.values()))
+    allocation_amount = list()
+    for i in removal_effects.values():
+        allocation_amount.append((i / denominator) * total_conversions)
+    # print(allocation_amount)
+    markov_conversions = dict()
+    i = 0
+    for channel in removal_effects.keys():
+        markov_conversions[channel] = allocation_amount[i]
+        i += 1
+    conv_dict.pop('conv', None)
+    conv_dict.pop('null', None)
+    conv_dict.pop('start', None)
+
+    return {'markov_conversions': markov_conversions,
+            'last_touch_conversions': conv_dict,
+            'removal_effects': removal_effects,
+            'base_cvr': base_cvr,
+            'transition_matrix': test_df,
+            'absorption_matrix': M
+           }
+
---
+++ packages/markov-model-attribution/markov_model_attribution-0.42.tar.gz/markov_model_attribution-0.42/readme.md
@@ -0,0 +1,66 @@
+
+## Markov Chains for Attribution Modeling
+
+This is a proof-of-concept I built out that leverages a first order Markov chain to reallocate conversions in the manner explained by [Anderl, Eva and Becker, Ingo and Wangenheim, Florian V. and Schumann, Jan Hendrik](https://papers.ssrn.com/sol3/papers.cfm?abstract_id=2343077) in " Mapping the Customer Journey: A Graph-Based Framework for Online Attribution Modeling"
+
+If this concept is new, check out the post on [markov chain attribution modeling](https://nelson.eu.org/how-to-use-markov-chains-for-attribution/).   
+
+Big thanks to people writing and commenting on this [AnalyzeCore article](https://analyzecore.com/2016/08/03/attribution-model-r-part-1/) for a very useful article and comment section.
+
+### Motivation
+
+There is an amazing R package called ChannelAttribution which does this as well as higher-order models very well.  My day-to-day workflow is centered around Python so I wanted to build out a version to A) have something I can go-to for connecting directly to SQL tables to path journeys and B) better understand the process by which these attribution models are generated.  ChannelAttribution makes it very easy to faceroll a fractional attribution model without really understanding what's going on.  Which is great!  But if you want to understand better, building your own tends to help.
+
+To get started quickly you can install via pip.
+
+### Installation
+Make sure your version is at least 0.4 - prior versions included suboptimal ways of generating our initial transition states that resulted in negative conversions in large datasets.
+```#python
+pip install markov-model-attribution
+```
+
+### Use
+* This package currently accepts a single-column Pandas dataframe. 
+* Each path should begin with "start" and end with either "conv" or "null".
+* Each path should be delimited by " > "
+
+The argument to pass is ```paths```, where paths is the Pandas dataframe containing your paths.
+
+
+```#python
+import markov_model_attribution as mma
+import pandas as pd
+
+# generate a sample dataset
+df = pd.DataFrame({'Paths':['start > cone > ctwo > cthree > conv',
+                           'start > cone > null',
+                           'start > ctwo > cthree > null']})
+
+model = mma.run_model(paths=df)
+
+```
+
+Once you have the model constructed you can access a couple of things to compare how a fractional model does against a standard last touch model.
+
+You can access these via
+
+```python
+
+print(model['markov_conversions'])
+
+# This outputs a dictionary containing the markov conversion count
+# {'cone': 0.2, 'cthree': 0.4, 'ctwo': 0.4}
+
+
+print(model['last_touch_conversions'])
+# This outputs the last touch conversions for comparison
+# {'cone': 0, 'cthree': 1, 'ctwo': 0}
+```
+
+You can also access the removal effect matrix of the underlying result.  
+
+```python
+print(model['removal_effects'])
+
+# {'cone': 0.5, 'cthree': 1.0, 'ctwo': 1.0}
+```
