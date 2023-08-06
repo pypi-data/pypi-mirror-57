a8e2fd4e75c69844ce74459d9bca6655f4fe0e42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:49:55 +0000

    Add package uxapi/1.0.4/uxapi-1.0.4.tar.gz

---
+++ packages/uxapi/uxapi-1.0.4.tar.gz/uxapi-1.0.4/uxapi/topic.py
@@ -0,0 +1,25 @@
+from typing import NamedTuple
+
+
+class UXTopic(NamedTuple):
+    id: str
+    exchange_id: str
+    market_type: str
+    datatype: str
+    extrainfo: str = ''
+
+    def __str__(self):
+        if self.extrainfo:
+            return f'{self.datatype}:{self.extrainfo}'
+        else:
+            return self.datatype
+
+    @property
+    def maintype(self):
+        _maintype, *_ = self.datatype.split('.')
+        return _maintype
+
+    @property
+    def subtypes(self):
+        _, *_subtypes = self.datatype.split('.')
+        return _subtypes
