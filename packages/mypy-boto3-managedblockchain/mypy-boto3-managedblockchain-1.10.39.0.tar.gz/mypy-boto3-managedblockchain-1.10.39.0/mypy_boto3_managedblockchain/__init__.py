e9646b0113e3b0d6dfb9a5dccac211c29b5ff79b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:16 +0000

    Add package mypy-boto3-managedblockchain/1.10.39.0/mypy-boto3-managedblockchain-1.10.39.0.tar.gz

---
+++ packages/mypy-boto3-managedblockchain/mypy-boto3-managedblockchain-1.10.39.0.tar.gz/mypy-boto3-managedblockchain-1.10.39.0/mypy_boto3_managedblockchain/__init__.py
@@ -0,0 +1,9 @@
+"Main interface for managedblockchain service"
+
+from mypy_boto3_managedblockchain.client import (
+    ManagedBlockchainClient as Client,
+    ManagedBlockchainClient,
+)
+
+
+__all__ = ("Client", "ManagedBlockchainClient")
