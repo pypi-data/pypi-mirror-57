d1a57687e2a1f28e90ed8b8cda4c674536aa4f8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:55 +0000

    Add package lingam/1.1.0/lingam-1.1.0.tar.gz

---
+++ packages/lingam/lingam-1.1.0.tar.gz/lingam-1.1.0/lingam/__init__.py
@@ -0,0 +1,13 @@
+"""
+The lingam module includes implementation of the LiNGAM algorithms.
+The LiNGAM Project: https://sites.google.com/site/sshimizu06/lingam
+"""
+
+from .ica_lingam import ICALiNGAM
+from .direct_lingam import DirectLiNGAM
+from .bootstrap import BootstrapResult
+from .multi_group_direct_lingam import MultiGroupDirectLiNGAM
+
+__all__ = ['ICALiNGAM', 'DirectLiNGAM', 'BootstrapResult', 'MultiGroupDirectLiNGAM']
+
+__version__ = '1.1.0'
