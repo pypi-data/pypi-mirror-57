0e86db6efc7aaa528f65d8465f528b84bf27767a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:32 +0000

    Add package Write-Log-Tool/0.0.1/Write_Log_Tool-0.0.1.tar.gz

---
+++ packages/Write-Log-Tool/Write_Log_Tool-0.0.1.tar.gz/Write_Log_Tool-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Write_Log_Tool',
+    version='0.0.1',
+    keywords='write log',
+    description='a write log tool',
+    license='MIT License',
+    url='https://github.com/xuehongbo/test_tool',
+    author='HongBo Xue',
+    author_email='505386086@qq.com',
+    packages=find_packages(),
+    platforms='any',
+    install_requires=[],
+)
