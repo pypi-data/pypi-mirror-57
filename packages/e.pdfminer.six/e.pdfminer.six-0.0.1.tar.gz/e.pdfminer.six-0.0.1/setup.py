7cb8fa345e812c7f19094554acdad996f0ae5f15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:01 +0000

    Add package e.pdfminer.six/0.0.1/e.pdfminer.six-0.0.1.tar.gz

---
+++ packages/e.pdfminer.six/e.pdfminer.six-0.0.1.tar.gz/e.pdfminer.six-0.0.1/setup.py
@@ -0,0 +1,51 @@
+from setuptools import setup
+
+import pdfminer as package
+
+setup(
+    name='e.pdfminer.six',
+    version=package.__version__,
+    packages=['pdfminer'],
+    package_data={'pdfminer': ['cmap/*.pickle.gz']},
+    install_requires=[
+        'chardet ; python_version > "3.0"',
+        'pycryptodome',
+        'six',
+        'sortedcontainers',
+    ],
+    extras_require={
+        "dev": ["nose", "tox"],
+        "docs": ["sphinx", "sphinx-argparse"],
+    },
+    description='PDF parser and analyzer',
+    long_description=package.__doc__,
+    license='MIT/X',
+    author='Yusuke Shinyama + Philippe Guglielmetti',
+    author_email='pdfminer@goulu.net',
+    url='https://github.com/pdfminer/pdfminer.six',
+    scripts=[
+        'tools/pdf2txt.py',
+        'tools/dumppdf.py',
+        'tools/latin2ascii.py',
+    ],
+    keywords=[
+        'pdf parser',
+        'pdf converter',
+        'layout analysis',
+        'text mining',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Text Processing',
+    ],
+)
