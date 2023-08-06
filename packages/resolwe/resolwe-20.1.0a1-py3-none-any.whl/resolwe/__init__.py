192c6152949985dd9ea1a9eec73e3932d6d1bd91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:07 +0000

    Add package resolwe/20.1.0a1/resolwe-20.1.0a1-py3-none-any.whl

---
+++ packages/resolwe/resolwe-20.1.0a1-py3-none-any.whl/resolwe/__init__.py
@@ -0,0 +1,12 @@
+""".. Ignore pydocstyle D400.
+
+=======
+Resolwe
+=======
+
+Open source enterprise dataflow engine in Django.
+
+"""
+from resolwe.__about__ import (
+    __author__, __copyright__, __email__, __license__, __summary__, __title__, __url__, __version__,
+)
