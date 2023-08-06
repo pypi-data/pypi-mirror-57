76bcdbd2fbc489af305b574fdb937493ca396a51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:11 +0000

    Add package mitch-dm-mypkg/0.1.4/mitch_dm_mypkg-0.1.4.tar.gz

---
+++ packages/mitch-dm-mypkg/mitch_dm_mypkg-0.1.4.tar.gz/mitch_dm_mypkg-0.1.4/mitch_dm_mypkg/__init__.py
@@ -0,0 +1,8 @@
+__version__ = '0.1.4'
+
+import run
+
+
+def main():
+    x = input("How are you? ")
+    print(f"{x}!")
---
+++ packages/mitch-dm-mypkg/mitch_dm_mypkg-0.1.4.tar.gz/mitch_dm_mypkg-0.1.4/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['mitch_dm_mypkg']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['boto3>=1.10,<2.0', 'click>=7.0,<8.0']
+
+entry_points = \
+{'console_scripts': ['disl = mitch_dm_mypkg:main']}
+
+setup_kwargs = {
+    'name': 'mitch-dm-mypkg',
+    'version': '0.1.4',
+    'description': '',
+    'long_description': None,
+    'author': 'Mitch Della Marta',
+    'author_email': 'mitchdellamarta@gmail.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
