62f12a26225f70aef53a3cd8aeed6aafe2b94f24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:24 +0000

    Add package mediumMuncher/0.0.1/mediumMuncher-0.0.1.tar.gz

---
+++ packages/mediumMuncher/mediumMuncher-0.0.1.tar.gz/mediumMuncher-0.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup 
+import os
+
+args=dict()
+with open("README.md","r",encoding='utf-8') as f:
+    args["long_description"] = f.read()
+args['long_description_content_type']='text/markdown'
+args["install_requires"]=["requests==2.22.0"]
+args["url"]="https://github.com/norton120/mediumMuncher"
+args["packages"]=["medium_muncher"]
+args["name"] = "mediumMuncher"
+args["version"]="0.0.01"
+args["summary"]="Converts medium stories and author feeds into plain HTML."
+args["keywords"]="Medium"
+args["author"]="Ethan Knox"
+args["author-email"]="ethan.m.knox@gmail.com"
+args["license"]="MIT"
+python_requires=">=3.7"
+
+setup(**args)
