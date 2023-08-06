38419139530266fc354d1e3956568b40f12774e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:47 +0000

    Add package rustpy-tools/0.2.0/rustpy_tools-0.2.0.tar.gz

---
+++ packages/rustpy-tools/rustpy_tools-0.2.0.tar.gz/Readme.md
@@ -0,0 +1,38 @@
+# rustpy-tools
+
+![AppVeyor](https://img.shields.io/badge/build-passing-brightgreen)
+
+A useful tool Python module implement in rust-lang.  
+
+### Installation
+```bash
+pip install rustpy-tools
+```
+
+### Quick Start
+
+```python
+import rustpy_tools
+from rustpy_tools import RegexUtil, AES
+
+rustpy_tools.version
+# 0.2.0
+
+RegexUtil.is_match(r'^\w+@\w+$', 'xxx@gamil.com')
+# True
+
+message = 'hello, world'
+key = 'yrbv8qlrGJsfo28C'
+iv = 'TcwqrwsShZ2FfUSi'
+rustpy_tools.AES.encrypt(message, key, iv)
+# GSk0f16bYJm+SUbd49PBkw==
+
+rustpy_tools.AES.decrypt('GSk0f16bYJm+SUbd49PBkw==', key, iv)
+# 'hello, world'
+```
+
+### Credit
+[PyO3](https://github.com/PyO3)
+
+### License
+rustpy-tools is licensed under the MIT license.
---
+++ packages/rustpy-tools/rustpy_tools-0.2.0.tar.gz/src/lib.rs
@@ -0,0 +1,51 @@
+extern crate crypto;
+extern crate regex;
+
+use pyo3::prelude::*;
+use pyo3::wrap_pyfunction;
+use regex::Regex;
+
+pub mod cryptography;
+
+#[pyclass]
+struct AES {}
+
+#[pymethods]
+impl AES {
+    #[staticmethod]
+    fn encrypt(payload: &str, key: &str, iv: &str) -> String {
+        cryptography::encrypt_to_base64(payload, key, iv)
+    }
+
+    #[staticmethod]
+    fn decrypt(payload: &str, key: &str, iv: &str) -> String {
+        cryptography::decrypt_from_base64(payload, key, iv)
+    }
+}
+
+#[pyclass]
+struct RegexUtil {}
+
+#[pymethods]
+impl RegexUtil {
+    #[staticmethod]
+    fn is_match(pattern: &str, content: &str) -> bool {
+        is_match(pattern, content)
+    }
+}
+
+#[pyfunction]
+fn is_match(pattern: &str, content: &str) -> bool {
+    let re = Regex::new(pattern).unwrap();
+    re.is_match(content)
+}
+
+#[pymodule]
+fn rustpy_tools(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<RegexUtil>()?;
+    m.add_class::<AES>()?;
+    m.add_wrapped(wrap_pyfunction!(is_match))?;
+    m.add("version", "0.2.0")?;
+
+    Ok(())
+}
