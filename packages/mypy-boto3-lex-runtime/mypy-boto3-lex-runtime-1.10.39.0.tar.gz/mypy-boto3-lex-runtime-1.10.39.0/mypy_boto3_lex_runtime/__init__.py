d95aebb0386538f8f9ccad13ad25ce2f7c794b40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:16 +0000

    Add package mypy-boto3-lex-runtime/1.10.39.0/mypy-boto3-lex-runtime-1.10.39.0.tar.gz

---
+++ packages/mypy-boto3-lex-runtime/mypy-boto3-lex-runtime-1.10.39.0.tar.gz/mypy-boto3-lex-runtime-1.10.39.0/mypy_boto3_lex_runtime/__init__.py
@@ -0,0 +1,6 @@
+"Main interface for lex-runtime service"
+
+from mypy_boto3_lex_runtime.client import LexRuntimeServiceClient, LexRuntimeServiceClient as Client
+
+
+__all__ = ("Client", "LexRuntimeServiceClient")
