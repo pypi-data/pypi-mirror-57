c045751539d0ced0e51d86e50fb458cc47fd1b09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:18 +0000

    Add package intranet-invoices/0.0.1/intranet_invoices-0.0.1-py3-none-any.whl

---
+++ packages/intranet-invoices/intranet_invoices-0.0.1-py3-none-any.whl/models/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from . import models_sm_invoice_report
+from . import models_sm_invoice_report_wizard
+from . import models_sm_invoice_line
+from . import models_sm_teletac
+from . import models_sm_member
+from . import models_smp_report_reservation_compute
+from . import models_smp_batch_reservation_compute
