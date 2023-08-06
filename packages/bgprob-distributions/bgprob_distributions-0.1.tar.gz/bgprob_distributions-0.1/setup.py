8618363a0d3a6c06c077677d00db10997f321397
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:12 +0000

    Add package bgprob-distributions/0.1/bgprob_distributions-0.1.tar.gz

---
+++ packages/bgprob-distributions/bgprob_distributions-0.1.tar.gz/bgprob_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='bgprob_distributions',
+      version='0.1',
+      description='Gaussian and Binomila distributions',
+      packages=['bgprob_distributions'],
+      author='Vinit Kumar',
+      author_email='vinit_kumarin@yahoo.com',
+      zip_safe=False)
