0e0cc835bf57ad1ae5783c60f6351b74bf19052c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:12 +0000

    Add package widgetastic.patternfly4/0.13.2/widgetastic.patternfly4-0.13.2.tar.gz

---
+++ packages/widgetastic.patternfly4/widgetastic.patternfly4-0.13.2.tar.gz/widgetastic.patternfly4-0.13.2/testing/test_breadcrumb.py
@@ -0,0 +1,16 @@
+from widgetastic.widget import View
+
+from widgetastic_patternfly4 import BreadCrumb
+
+
+def test_breadcrumb(browser):
+    class TestView(View):
+        ROOT = ".//main[@role='main']"
+        breadcrumb = BreadCrumb()
+
+    view = TestView(browser)
+    assert view.breadcrumb.is_displayed
+    assert len(view.breadcrumb.locations) == 4
+    assert view.breadcrumb.locations[0] == "Section Home"
+    assert view.breadcrumb.read() == "Section Landing"
+    view.breadcrumb.click_location(view.breadcrumb.locations[0])
