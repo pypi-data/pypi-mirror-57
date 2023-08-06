97b31604f4276747eb24a405e4fb042087389a02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:55 +0000

    Add package pycox/0.1.1/pycox-0.1.1-py3-none-any.whl

---
+++ packages/pycox/pycox-0.1.1-py3-none-any.whl/pycox/datasets/__init__.py
@@ -0,0 +1,14 @@
+from pycox.datasets import from_deepsurv
+from pycox.datasets import from_rdatasets
+from pycox.datasets import from_kkbox
+from pycox.datasets import from_simulations
+
+
+support = from_deepsurv._Support()
+metabric = from_deepsurv._Metabric()
+gbsg = from_deepsurv._Gbsg()
+flchain = from_rdatasets._Flchain()
+nwtco = from_rdatasets._Nwtco()
+kkbox_v1 = from_kkbox._DatasetKKBoxChurn()
+sac3 = from_simulations._SAC3()
+rr_nl_nhp = from_simulations._RRNLNPH()
