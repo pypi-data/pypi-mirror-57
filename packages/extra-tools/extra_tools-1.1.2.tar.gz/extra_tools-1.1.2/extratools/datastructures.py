7e2019aeedafd542082c5cf081d76b82df230a07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:40 +0000

    Add package extra-tools/1.1.2/extra_tools-1.1.2.tar.gz

---
+++ packages/extra-tools/extra_tools-1.1.2.tar.gz/extra_tools-1.1.2/extratools/datastructures.py
@@ -0,0 +1,22 @@
+class StackError(Exception):
+	pass
+
+class Stack:
+	def __init__(self,size):
+		self.stack = []
+		self.index = 0
+		self.size = size
+	def push(self,element):
+		if self.index == self.size:
+			raise StackError("Stack Overflow!")
+		else:
+			self.stack.append(element)
+			self.index += 1
+			return "Pushed!"
+	def pop(self):
+		if self.index == 0:
+			raise StackError("Stack Underflow!")
+		else:
+			self.size -= 1
+			return self.stack.pop()
+
