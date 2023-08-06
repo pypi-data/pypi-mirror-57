423feef4c0339d0a590c68807efc146f7da77604
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:20 +0000

    Add package superjacob/0.0.2/superjacob-0.0.2.tar.gz

---
+++ packages/superjacob/superjacob-0.0.2.tar.gz/superjacob-0.0.2/superjacob/__init__.py
@@ -0,0 +1,5 @@
+__all__ = ['expression', 'operations', 'superjacob', 'make_expression']
+
+from superjacob.expression import *
+from superjacob.superjacob import *
+from superjacob import operations as ops
