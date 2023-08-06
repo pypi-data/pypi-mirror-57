9b4e106e1ea5899c95db50c1b560c032fafb2fb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:28 +0000

    Add package iso3166/1.0.1/iso3166-1.0.1.tar.gz

---
+++ packages/iso3166/iso3166-1.0.1.tar.gz/iso3166-1.0.1/setup.py
@@ -0,0 +1,44 @@
+import os.path
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(name='iso3166',
+      version="1.0.1",
+      author="Mike Spindel",
+      author_email="mike@spindel.is",
+      license="MIT",
+      keywords="iso 3166-1 country codes",
+      url="http://github.com/deactivated/python-iso3166",
+      description='Self-contained ISO 3166-1 country definitions.',
+      packages=find_packages(exclude=['ez_setup']),
+      long_description=read('README.rst'),
+      zip_safe=False,
+      classifiers=[
+          "Development Status :: 5 - Production/Stable",
+          "License :: OSI Approved :: MIT License",
+          "Intended Audience :: Developers",
+          "Natural Language :: English",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.1",
+          "Programming Language :: Python :: 3.2",
+          "Programming Language :: Python :: 3.3",
+          "Programming Language :: Python :: 3.4",
+          "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: 3.6",
+          "Programming Language :: Python :: 3.7",
+          "Programming Language :: Python :: 3.8",
+          "Programming Language :: Python :: 3.9",
+      ])
