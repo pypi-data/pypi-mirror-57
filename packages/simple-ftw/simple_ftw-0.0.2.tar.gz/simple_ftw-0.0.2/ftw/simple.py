4e02c61924b39baf02a075ddb1e3846cc29b8f4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:04 +0000

    Add package simple-ftw/0.0.2/simple_ftw-0.0.2.tar.gz

---
+++ packages/simple-ftw/simple_ftw-0.0.2.tar.gz/simple_ftw-0.0.2/ftw/simple.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python3
+import sys
+
+
+def introduction(name: str):
+    print(f"Hello World!\n\tLet me introduce: {name}")
+
+
+def reply(response: str):
+    print(response)
+
+
+if __name__ == '__main__':
+    inputs = sys.argv[1:]
+    if inputs:
+        run_simple(" ".join(inputs))
+    else:
+        print("🙄 I require inputs!")
+
