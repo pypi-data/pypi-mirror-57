52f99bd9cd113299f697b4393f67e4e14df170c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:22 +0000

    Add package mssql-cli/0.17.3/mssql_cli-0.17.3-py2.py3-none-manylinux1_x86_64.whl

---
+++ packages/mssql-cli/mssql_cli-0.17.3-py2.py3-none-manylinux1_x86_64.whl/mssqlcli/mssqlbuffer.py
@@ -0,0 +1,40 @@
+from __future__ import unicode_literals
+
+from prompt_toolkit.enums import DEFAULT_BUFFER
+from prompt_toolkit.filters import Condition
+from prompt_toolkit.application import get_app
+from .packages.parseutils.utils import is_open_quote
+
+
+def mssql_is_multiline(mssql_cli):
+    @Condition
+    def cond():
+        doc = get_app().layout.get_buffer_by_name(DEFAULT_BUFFER).document
+
+        if not mssql_cli.multiline:
+            return False
+        if mssql_cli.multiline_mode == 'safe':
+            return True
+        return not _multiline_exception(doc.text)
+
+    return cond
+
+
+def _is_complete(sql):
+    # A complete command is an sql statement that ends with a semicolon, unless
+    # there's an open quote surrounding it, as is common when writing a
+    # CREATE FUNCTION command
+    return sql.endswith(';') and not is_open_quote(sql)
+
+
+def _multiline_exception(text):
+    text = text.strip()
+    return (
+        text.startswith('\\') or  # Special Command
+        text.endswith(r'\e') or  # Ended with \e which should launch the editor
+        _is_complete(text) or  # A complete SQL command
+        (text == 'exit') or  # Exit doesn't need semi-colon
+        (text == 'quit') or  # Quit doesn't need semi-colon
+        (text == ':q') or  # To all the vim fans out there
+        (text == '')  # Just a plain enter without any text
+    )
