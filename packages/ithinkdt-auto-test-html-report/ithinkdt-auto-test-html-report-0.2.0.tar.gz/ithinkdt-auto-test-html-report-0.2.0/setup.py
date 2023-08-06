68fc4042aa3b09ac499def40c00964a0e7e1add0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:27 +0000

    Add package ithinkdt-auto-test-html-report/0.2.0/ithinkdt-auto-test-html-report-0.2.0.tar.gz

---
+++ packages/ithinkdt-auto-test-html-report/ithinkdt-auto-test-html-report-0.2.0.tar.gz/ithinkdt-auto-test-html-report-0.2.0/setup.py
@@ -0,0 +1,19 @@
+# !usr/bin/env python
+# _*_ coding: utf-8 _*_
+
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(
+    name='ithinkdt-auto-test-html-report',  # 项目代码所在目录，也是pip 要上传的项目名称
+    version='0.2.0',  # 工具的版本号
+    description='python auto test of form a test report',
+    long_description='python libs of form a test report',
+    license='MIT',
+    author='huiLi',
+    author_email='lihui@ithinkdt.com',
+    packages=find_packages(where='./ithinkdt-auto-test-html-report'),  # 查找包的路径
+    package_dir={'': 'ithinkdt-auto-test-html-report'},      # 包的root 路径映射到的实际路径
+    platforms='any'
+)
