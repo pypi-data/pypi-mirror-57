befdf6defb00b68a0a1dcae11af0d9fcf7bdc981
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:34 +0000

    Add package ppinot4py/0.0.3/PPINot4Py-0.0.3.tar.gz

---
+++ packages/ppinot4py/PPINot4Py-0.0.3.tar.gz/PPINot4Py-0.0.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PPINot4Py", # Replace with your own username
+    version="0.0.3",
+    author="Alejandro Gomez Caballero",
+    author_email="agcaballero@us.es",
+    description="Python version of PPINot",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/isa-group/ppinot4py",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
