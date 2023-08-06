0125e03be07e4c720538ed641128445e7b4e7f4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:01 +0000

    Add package kumaoche/0.1.11/kumaoche-0.1.11.tar.gz

---
+++ packages/kumaoche/kumaoche-0.1.11.tar.gz/kumaoche-0.1.11/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kumaoche',
+    version='0.1.11',
+    description='Pythonic Dev Repository & Dev Environment Management tool.',
+    long_description='',
+    author='kumak1',
+    author_email='kumaki0@gmail.com',
+    install_requires=['invoke', 'pyyaml'],
+    url='https://github.com/kumak1/kumaoche',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs', 'examples')),
+    test_suite='tests'
+)
