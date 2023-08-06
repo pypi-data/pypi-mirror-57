1769b0beb0169e4a37286751669a39c4c554c780
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:08 +0000

    Add package comicdl/0.0.3/comicdl-0.0.3.tar.gz

---
+++ packages/comicdl/comicdl-0.0.3.tar.gz/comicdl-0.0.3/comic_dl/utils/exceptions.py
@@ -0,0 +1,33 @@
+
+class InvalidRangeException(Exception):
+    ...
+
+
+class PlatformNotSupported(Exception):
+    ...
+
+
+class ArgumentNotSpecified(Exception):
+    ...
+
+
+class ComicDoesNotExist(Exception):
+    msg = 'The alias specified is not related with any comic in the database,'\
+        ' add comic with comic_dl --add="<query>" to set alias'
+
+    def __init__(self, msg=msg, *args, **kwargs):
+        super().__init__(msg, *args, **kwargs)
+
+
+class AliasNotSpecified(ArgumentNotSpecified):
+    msg = 'Positional argument alias not specified'
+
+    def __init__(self, msg=msg, *args, **kwargs):
+        super().__init__(msg, *args, **kwargs)
+
+
+class NetworkError(Exception):
+    msg = 'No internet connection available or page not found.'
+
+    def __init__(self, msg=msg, *args, **kwargs):
+        super().__init__(msg, *args, **kwargs)
