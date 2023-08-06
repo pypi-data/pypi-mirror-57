029445b761137fc0dd56bec2ca8b8d4d951323f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:31 +0000

    Add package graphene-field-permission/0.0.8/graphene-field-permission-0.0.8.tar.gz

---
+++ packages/graphene-field-permission/graphene-field-permission-0.0.8.tar.gz/graphene-field-permission-0.0.8/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+from os import sys
+import pypandoc
+
+needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
+pytest_runner = ['pytest-runner'] if needs_pytest else []
+
+long_description = pypandoc.convert_file('README.md', 'rst')
+long_description = long_description.replace("\r", "")
+
+setuptools.setup(
+    name="graphene-field-permission",
+    version="0.0.8",
+    author="Dave O'Connor",
+    author_email="github@dead-pixels.org",
+    description="A package to add field permission support for Graphene",
+    long_description=long_description,
+    url="https://github.com/daveoconnor/graphene-field-permission",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    tests_require=["pytest"],
+    install_requires=[
+        "pypandoc",
+        "pytest",
+    ],
+    setup_requires=[] + pytest_runner,
+
+)
