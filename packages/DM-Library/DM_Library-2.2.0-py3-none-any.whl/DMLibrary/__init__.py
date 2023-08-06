e607cafb1b9ba6d597338596056c2081aad2fe6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:03 +0000

    Add package DM-Library/2.2.0/DM_Library-2.2.0-py3-none-any.whl

---
+++ packages/DM-Library/DM_Library-2.2.0-py3-none-any.whl/DMLibrary/__init__.py
@@ -0,0 +1,8 @@
+# if you have a class inside a module
+from DM_Library.Graph import Graph
+
+# if you have a functions inside a module
+import DM_Library.Huffman
+from DM_Library import Huffman
+
+# if you have .py and no class nothing needed
