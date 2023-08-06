652baa40a9650d142282ec0e87321c86363d3cd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:50 +0000

    Add package mypy-boto3-kinesis-video-archived-media/1.10.34.1/mypy-boto3-kinesis-video-archived-media-1.10.34.1.tar.gz

---
+++ packages/mypy-boto3-kinesis-video-archived-media/mypy-boto3-kinesis-video-archived-media-1.10.34.1.tar.gz/mypy-boto3-kinesis-video-archived-media-1.10.34.1/mypy_boto3_kinesis_video_archived_media/paginator.py
@@ -0,0 +1,29 @@
+"Main interface for kinesis-video-archived-media service Paginators"
+from __future__ import annotations
+
+from botocore.paginate import Paginator as Boto3Paginator
+from mypy_boto3_kinesis_video_archived_media.type_defs import (
+    FragmentSelectorTypeDef,
+    ListFragmentsOutputTypeDef,
+    PaginatorConfigTypeDef,
+)
+
+
+__all__ = ("ListFragmentsPaginator",)
+
+
+class ListFragmentsPaginator(Boto3Paginator):
+    """
+    [Paginator.ListFragments documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.34/reference/services/kinesis-video-archived-media.html#KinesisVideoArchivedMedia.Paginator.ListFragments)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self,
+        StreamName: str,
+        FragmentSelector: FragmentSelectorTypeDef = None,
+        PaginationConfig: PaginatorConfigTypeDef = None,
+    ) -> ListFragmentsOutputTypeDef:
+        """
+        [ListFragments.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.34/reference/services/kinesis-video-archived-media.html#KinesisVideoArchivedMedia.Paginator.ListFragments.paginate)
+        """
