5dc1a95e6f400f0304cc06e09c4b962edda38de3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:18 +0000

    Add package lunuan/0.1.2/lunuan-0.1.2.tar.gz

---
+++ packages/lunuan/lunuan-0.1.2.tar.gz/lunuan-0.1.2/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='lunuan',
+    version='0.1.2',
+    description=('Python开发中常用的类'),
+    long_description=(
+        'Python开发中常用的类'
+    ),
+    author='huaxing',
+    author_email='huaxing@lunuan.com.cn',
+    maintainer='huaxing',
+    maintainer_email='huaxing@lunuan.com.cn',
+    license='BSD License',
+    packages=find_packages(),
+    platforms=["all"],
+    url=' ',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
