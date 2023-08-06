03377ffebd4ea409eb7a11dd552e64482d85e1e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:27 +0000

    Add package hendria-distributions/0.1/hendria_distributions-0.1.tar.gz

---
+++ packages/hendria-distributions/hendria_distributions-0.1.tar.gz/hendria_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='hendria_distributions',
+      version='0.1',
+      description='Gaussian & Binomial distributions',
+      packages=['hendria_distributions'],
+      zip_safe=False)
