4a32aa41237fb25757e865233923ec1054407d9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:42 +0000

    Add package skillbridge/1.0.7/skillbridge-1.0.7.tar.gz

---
+++ packages/skillbridge/skillbridge-1.0.7.tar.gz/skillbridge-1.0.7/skillbridge/__main__.py
@@ -0,0 +1,33 @@
+from os.path import abspath, dirname, join
+from argparse import ArgumentParser
+
+from . import generate_static_completion
+
+
+def print_skill_script_location() -> None:
+    folder = dirname(abspath(__file__))
+    skill_source = join(folder, 'server', 'python_server.il')
+    print(skill_source.replace('\\', '\\\\'))
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser(
+        'skillbridge',
+        description="""
+    Without arguments, prints the location of the skill script.
+    The correct location is required for the load command.
+    """,
+    )
+    parser.add_argument(
+        '-g',
+        '--generate',
+        help="generate static completion file",
+        action='store_true',
+        default=False,
+    )
+    args = parser.parse_args()
+
+    if args.generate:
+        generate_static_completion()
+    else:
+        print_skill_script_location()
