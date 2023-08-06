0c70fa26ffe0ffc70a000f7b92c22802d513466d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:27 +0000

    Add package telegram-util/0.0.21/telegram_util-0.0.21.tar.gz

---
+++ packages/telegram-util/telegram_util-0.0.21.tar.gz/telegram_util-0.0.21/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="telegram_util",
+    version="0.0.21",
+    author="Yunzhi Gao",
+    author_email="gaoyunzhi@gmail.com",
+    description="Telegram Util.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gaoyunzhi/telegram_util",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+    ],
+    python_requires='>=3.0',
+)
