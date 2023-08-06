7606f96202e552bd24c51ea78f7919339f6c6bf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:33 +0000

    Add package mypy-boto3-batch/1.10.36.0/mypy_boto3_batch-1.10.36.0-py3-none-any.whl

---
+++ packages/mypy-boto3-batch/mypy_boto3_batch-1.10.36.0-py3-none-any.whl/mypy_boto3_batch/__init__.py
@@ -0,0 +1,18 @@
+"Main interface for batch service"
+
+from mypy_boto3_batch.client import Client
+from mypy_boto3_batch.paginator import (
+    DescribeComputeEnvironmentsPaginator,
+    DescribeJobDefinitionsPaginator,
+    DescribeJobQueuesPaginator,
+    ListJobsPaginator,
+)
+
+
+__all__ = (
+    "Client",
+    "DescribeComputeEnvironmentsPaginator",
+    "DescribeJobDefinitionsPaginator",
+    "DescribeJobQueuesPaginator",
+    "ListJobsPaginator",
+)
