55362a7213c9b4b4988a174a707336bb4f4624fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:43 +0000

    Add package ailab-api/0.1/ailab-api-0.1.tar.gz

---
+++ packages/ailab-api/ailab-api-0.1.tar.gz/ailab-api-0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ailab-api',
+    version='0.1',
+    description='ailab-daejin-restful-api',
+    author='donggu.kang',
+    author_email='kang154123@naver.com',
+    url='https://github.com/GoopyAspirin/aida',
+    download_url='https://github.com/GoopyAspirin/aida/blob/master/download/aida-0.1.tar.gz',
+    keywords=['api'],
+    python_requires='>=3',
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
