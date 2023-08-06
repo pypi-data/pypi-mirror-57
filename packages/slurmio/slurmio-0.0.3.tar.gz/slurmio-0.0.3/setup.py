b6dd16d4bb036a644200a8079a5894766da31796
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:15 +0000

    Add package slurmio/0.0.3/slurmio-0.0.3.tar.gz

---
+++ packages/slurmio/slurmio-0.0.3.tar.gz/slurmio-0.0.3/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+requirements = ["luddite"]
+
+
+setuptools.setup(
+    name="slurmio",
+    version="0.0.3",
+    author="Adam Tyson",
+    author_email="adam.tyson@ucl.ac.uk",
+    description="Python tools to interface with slurm",
+    install_requires=requirements,
+    extras_require={"dev": ["black", "pytest-cov", "pytest", "coveralls"]},
+    python_requires=">=3.6",
+    url="https://github.com/adamltyson/slurmio",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Intended Audience :: Developers",
+    ],
+)
