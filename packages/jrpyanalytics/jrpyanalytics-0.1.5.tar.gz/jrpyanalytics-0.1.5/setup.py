62f052067d4ad2904462ad5598a6190eec599108
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:55 +0000

    Add package jrpyanalytics/0.1.5/jrpyanalytics-0.1.5.tar.gz

---
+++ packages/jrpyanalytics/jrpyanalytics-0.1.5.tar.gz/jrpyanalytics-0.1.5/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['jrpyanalytics', 'jrpyanalytics.datasets']
+
+package_data = \
+{'': ['*'],
+ 'jrpyanalytics': ['vignettes/*'],
+ 'jrpyanalytics.datasets': ['data/*']}
+
+install_requires = \
+['graphviz>=0.10.1,<0.11.0',
+ 'matplotlib>=3.0,<4.0',
+ 'numpy>=1.15,<2.0',
+ 'pandas>=0.23.4,<0.24.0',
+ 'seaborn>=0.9.0,<0.10.0',
+ 'sklearn>=0.0.0,<0.0.1']
+
+setup_kwargs = {
+    'name': 'jrpyanalytics',
+    'version': '0.1.5',
+    'description': '',
+    'long_description': None,
+    'author': 'Jamie',
+    'author_email': 'jamie@jumpingrivers.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.5,<4.0',
+}
+
+
+setup(**setup_kwargs)
