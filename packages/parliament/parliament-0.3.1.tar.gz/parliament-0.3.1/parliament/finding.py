eb4ac3a6d18bdd705f74ded40dde5d109884aa6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:29 +0000

    Add package parliament/0.3.1/parliament-0.3.1.tar.gz

---
+++ packages/parliament/parliament-0.3.1.tar.gz/parliament-0.3.1/parliament/finding.py
@@ -0,0 +1,21 @@
+import yaml
+
+
+class Finding:
+    """ Class for storing findings """
+
+    issue = ""
+    detail = ""
+    location = {}
+    severity = ""
+    title = ""
+    description = ""
+
+    def __init__(self, issue, detail, location):
+        self.issue = issue
+        self.detail = detail
+        self.location = location
+
+    def __repr__(self):
+        """ Return a string for printing """
+        return "{} - {} - {}".format(self.issue, self.detail, self.location)
