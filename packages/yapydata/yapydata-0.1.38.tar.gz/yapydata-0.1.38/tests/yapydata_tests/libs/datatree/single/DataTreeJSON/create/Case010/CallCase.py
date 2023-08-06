830a40bc2c831b03f67f1be7949e7d443c7fb678
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:02 +0000

    Add package yapydata/0.1.38/yapydata-0.1.38.tar.gz

---
+++ packages/yapydata/yapydata-0.1.38.tar.gz/yapydata-0.1.38/tests/yapydata_tests/libs/datatree/single/DataTreeJSON/create/Case010/CallCase.py
@@ -0,0 +1,48 @@
+from __future__ import absolute_import
+from __future__ import print_function
+
+__author__ = 'Arno-Can Uestuensoez'
+__license__ = "Artistic-License-2.0 + Forced-Fairplay-Constraints"
+__copyright__ = "Copyright (C) 2010-2016 Arno-Can Uestuensoez" \
+                " @Ingenieurbuero Arno-Can Uestuensoez"
+__version__ = '0.1.10'
+__uuid__ = "60cac28d-efe6-4a8d-802f-fa4fc94fa741"
+
+__docformat__ = "restructuredtext en"
+
+import unittest
+import os
+import sys
+
+from yapydata.datatree.synjson import DataTreeJSON
+
+
+
+class CallUnits(unittest.TestCase):
+
+
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+ #       sys.path.insert(0, mypath) 
+        self.maxDiff = None
+
+    def testCase010(self):
+        resx = {
+                'a': {
+                    'b': [
+                        {
+                            'c': [
+                                123,
+                                456,
+                                789,
+                            ]
+                        }
+                    ]
+                } 
+            }
+        _cap = DataTreeJSON(resx)
+        self.assertEqual(_cap.data, resx)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ packages/yapydata/yapydata-0.1.38.tar.gz/yapydata-0.1.38/tests/yapydata_tests/libs/datatree/single/DataTreeXML/readfile/striproot/basic/core/ext/int/check/CallCase.py
@@ -0,0 +1,37 @@
+from __future__ import absolute_import
+from __future__ import print_function
+
+__author__ = 'Arno-Can Uestuensoez'
+__license__ = "Artistic-License-2.0 + Forced-Fairplay-Constraints"
+__copyright__ = "Copyright (C) 2010-2016 Arno-Can Uestuensoez" \
+                " @Ingenieurbuero Arno-Can Uestuensoez"
+__version__ = '0.1.10'
+__uuid__ = "60cac28d-efe6-4a8d-802f-fa4fc94fa741"
+
+__docformat__ = "restructuredtext en"
+
+import unittest
+import os
+import sys
+
+from yapydata.datatree.synxml import DataTreeXML, YapyDataXMLError
+
+
+
+class CallUnits(unittest.TestCase):
+
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+ #       sys.path.insert(0, mypath) 
+        self.maxDiff = None
+
+    def testCase010(self):
+        _cap = DataTreeXML()
+        try:
+            _cap.import_data(os.path.dirname(__file__) + os.sep + 'data', striproot=True)
+        except YapyDataXMLError:
+            pass
+
+
+if __name__ == '__main__':
+    unittest.main()
