e1a59e590d502f83b2def9b252eb3b6d8949e398
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:15 +0000

    Add package searchanonamuse/0.3/searchanonamuse-0.3.tar.gz

---
+++ packages/searchanonamuse/searchanonamuse-0.3.tar.gz/searchanonamuse-0.3/searchanonamuse/__init__.py
@@ -0,0 +1,33 @@
+import time
+from selenium import webdriver
+
+
+class GetHtml:
+    """Class that receives login, password and path to ChromeDriver.exe as parameters and has a method that do the search in MAM
+    and returns the HTML resulting of the search
+    """
+    password: str
+
+    def __init__(self, login, passw):
+        self.driver = webdriver.Remote("http://127.0.0.1:4446/wd/hub", DesiredCapabilities.FIREFOX)  # Optional argument, if not specified will search path.
+        self.login = login
+        self.password=passw
+        # Login
+        self.driver.get('https://www.myanonamouse.net/login.php');
+        time.sleep(1)  # Let the user actually see something!
+        search_box = self.driver.find_element_by_name('email')
+        search_box.send_keys(self.login)
+        search_box2 = self.driver.find_element_by_name('password')
+        search_box2.send_keys(self.password)
+        search_box2.submit( )
+
+    def search(self, keyword):
+        search_box3 = self.driver.find_element_by_name('tor[text]')
+        search_box3.send_keys(keyword)
+        search_box3.submit()
+        time.sleep(2)
+        html = self.driver.page_source
+        return html
+
+    def stop(self):
+        self.driver.quit( )
---
+++ packages/searchanonamuse/searchanonamuse-0.3.tar.gz/searchanonamuse-0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='searchanonamuse',
+      version='0.03',
+      description='Receives login, password and path to ChromeDriver.exe as parameters and has a method'
+                  ' that do the search in MAM and returns the HTML resulting of the search',
+      url='https://github.com/samuelsonsev/searchanonamuse',
+      author='Sam',
+      author_email='none@example.com',
+      license='MIT',
+      packages=['searchanonamuse'],
+      zip_safe=False, install_requires = ['selenium'])
