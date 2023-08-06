3dcf63b2a195379ef5f530493834253b0b9ca05a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:34 +0000

    Add package pytap2/1.6.0/pytap2-1.6.0.tar.gz

---
+++ packages/pytap2/pytap2-1.6.0.tar.gz/pytap2-1.6.0/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+import sys
+
+import setuptools
+
+
+install_requires = []
+if sys.version_info < (3, 4):
+    install_requires.append('enum34')
+
+setuptools.setup(
+    name='pytap2',
+    version='1.6.0',
+
+    description='Object-oriented wrapper around the Linux Tun/Tap device for Python 2 and 3',
+    long_description=open('README.rst').read(),
+    keywords=('pytap', 'tun', 'tap', 'networking'),
+
+    author='John Hagen',
+    author_email='johnthagen@gmail.com',
+    url='https://github.com/johnthagen/pytap2',
+    packages=setuptools.find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    install_requires=install_requires,
+    zip_safe=False,
+
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
