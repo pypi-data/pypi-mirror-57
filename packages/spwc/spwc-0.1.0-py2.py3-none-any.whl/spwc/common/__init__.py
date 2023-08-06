a73a403c6db5bf28561dac24f9dc1dcdb724953c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:45 +0000

    Add package spwc/0.1.0/spwc-0.1.0-py2.py3-none-any.whl

---
+++ packages/spwc/spwc-0.1.0-py2.py3-none-any.whl/spwc/common/__init__.py
@@ -0,0 +1,27 @@
+import diskcache as dc
+from appdirs import user_cache_dir
+from datetime import datetime, timezone
+from dateutil.parser import parse
+
+cache = dc.Cache(user_cache_dir("SciQLop", "LPP"))
+
+
+def get_cache_entries(server, product_name, start_date, stop_date):
+    pass
+
+
+def add_cache_entry(server, product_name, dataframe):
+    pass
+
+
+def listify(obj: list or object) -> list:
+    if type(obj) is list:
+        return obj
+    else:
+        return [obj]
+
+
+def make_utc_datetime(input_dt: str or datetime) -> datetime:
+    if type(input_dt) is str:
+        input_dt = parse(input_dt)
+    return input_dt.replace(tzinfo=timezone.utc)
