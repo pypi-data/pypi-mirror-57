7cb73dd4507b5d73bb4a1b9899db8992d87e5288
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:58 +0000

    Add package mypy-boto3-kinesis/1.10.34/mypy_boto3_kinesis-1.10.34-py3-none-any.whl

---
+++ packages/mypy-boto3-kinesis/mypy_boto3_kinesis-1.10.34-py3-none-any.whl/mypy_boto3_kinesis/waiter.py
@@ -0,0 +1,47 @@
+"Main interface for kinesis service Waiters"
+from __future__ import annotations
+
+from botocore.waiter import Waiter as Boto3Waiter
+from mypy_boto3_kinesis.type_defs import (
+    StreamExistsWaitWaiterConfigTypeDef,
+    StreamNotExistsWaitWaiterConfigTypeDef,
+)
+
+
+__all__ = ("StreamExistsWaiter", "StreamNotExistsWaiter")
+
+
+class StreamExistsWaiter(Boto3Waiter):
+    """
+    Waiter for `stream_exists` name.
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def wait(
+        self,
+        StreamName: str,
+        Limit: int = None,
+        ExclusiveStartShardId: str = None,
+        WaiterConfig: StreamExistsWaitWaiterConfigTypeDef = None,
+    ) -> None:
+        """
+        [stream_exists.wait documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.34/reference/services/kinesis.html#Kinesis.Waiter.stream_exists.wait)
+        """
+
+
+class StreamNotExistsWaiter(Boto3Waiter):
+    """
+    Waiter for `stream_not_exists` name.
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def wait(
+        self,
+        StreamName: str,
+        Limit: int = None,
+        ExclusiveStartShardId: str = None,
+        WaiterConfig: StreamNotExistsWaitWaiterConfigTypeDef = None,
+    ) -> None:
+        """
+        [stream_not_exists.wait documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.34/reference/services/kinesis.html#Kinesis.Waiter.stream_not_exists.wait)
+        """
