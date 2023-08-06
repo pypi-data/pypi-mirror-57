7c7ae3bcad6bdb7d167f4b8bd0e8860c2c7edd8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:17 +0000

    Add package Volavola/1.3/Volavola-1.3.tar.gz

---
+++ packages/Volavola/Volavola-1.3.tar.gz/Volavola-1.3/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 13 23:37:16 2019
+
+@author: dhk13
+"""
+
+import setuptools
+
+setuptools.setup(
+    name="Volavola",
+    version="v1.3",
+    license='CPL v3.0',
+    author="dhk1349",
+    author_email="dhk1349@naver.com",
+    description="Volvola is memory forensic tool based on Volatility2",
+    long_description=open('README.md').read(),
+    url="https://github.com/dhk1349/Volavola",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        # 패키지에 대한 태그
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: Microsoft :: Windows"
+    ],
+)
