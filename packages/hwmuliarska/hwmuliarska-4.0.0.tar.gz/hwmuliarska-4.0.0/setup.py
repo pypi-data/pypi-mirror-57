841204b294ebe20eb4175ed0ffee6ec9295e382c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:15 +0000

    Add package hwmuliarska/4.0.0/hwmuliarska-4.0.0.tar.gz

---
+++ packages/hwmuliarska/hwmuliarska-4.0.0.tar.gz/hwmuliarska-4.0.0/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name='hwmuliarska',
+    packages=['hwmuliarska'],
+    version="4.0.0",
+    license='MIT',
+    author="Yana Muliarska",
+    author_email="muliarska@ucu.edu.ua",
+    description="Five the most successful decades",
+    long_description = """
+    This package conducts the research based on file 'ratings.list'. 
+    It identifies five the most successful decades and their average rating based on the ratings of films made during each decade. 
+    It creates a pie chart rendering.
+    """,
+    include_package_data=True,
+    long_description_content_type="text/markdown",
+
+)
