9f8f14413142ee8197b42aaf451d033cac309412
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:04 +0000

    Add package py3mltoolbox/0.0.3/py3mltoolbox-0.0.3.tar.gz

---
+++ packages/py3mltoolbox/py3mltoolbox-0.0.3.tar.gz/py3mltoolbox-0.0.3/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name          = 'py3mltoolbox',
+  packages      = ['py3mltoolbox'],
+  version       = '0.0.3',
+  description   = 'A Python3 toolbox for Machine Learning',
+  author        = 'Great Tomorrow',
+  author_email  = 'gr82morozr@gmail.com',
+  url           = 'https://github.com/gr82morozr/py3mltoolbox.git',  
+  download_url  = 'https://github.com/gr82morozr/py3mltoolbox.git', 
+  keywords      = ['AI','Data Science','Machine Learning','Utility', 'Tools' ], 
+  classifiers   = [],
+  install_requires=['']
+)
