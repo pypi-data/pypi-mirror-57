3365c7738dab50a4c4aac290377f0232c8a0b960
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:28 +0000

    Add package jupyterhub-ltiauthenticator/0.4.0/jupyterhub-ltiauthenticator-0.4.0.tar.gz

---
+++ packages/jupyterhub-ltiauthenticator/jupyterhub-ltiauthenticator-0.4.0.tar.gz/jupyterhub-ltiauthenticator-0.4.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jupyterhub-ltiauthenticator',
+    version='0.4.0',
+    description='JupyterHub authenticator implementing LTI v1',
+    url='https://github.com/yuvipanda/jupyterhub-ltiauthenticator',
+    author='Yuvi Panda',
+    author_email='yuvipanda@gmail.com',
+    license='3 Clause BSD',
+    packages=find_packages(),
+    python_requires=">=3.5",
+    install_requires=[
+        'jupyterhub>=0.8',
+        'oauthlib>=3.0'
+    ]
+)
