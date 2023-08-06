7bbcdb6966c3a8162d6e1a270023dd97edec6557
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:02 +0000

    Add package nesterzsw/1.1.0/nesterzsw-1.1.0.tar.gz

---
+++ packages/nesterzsw/nesterzsw-1.1.0.tar.gz/nesterzsw-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+      name         =  'nesterzsw',
+      version      =  '1.1.0',
+      py_modules   =  ['nesterzsw'],
+      author       =  'zhoushiwei',
+      author_email =  '1575175968@qq.com',
+      description  =  'A simple printer of nested lists',
+      )
