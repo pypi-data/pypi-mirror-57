dac92cd365add1a85d3a47834aedd1c0afc4dcb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:43 +0000

    Add package mypy-boto3-elastictranscoder/1.10.34.1/mypy-boto3-elastictranscoder-1.10.34.1.tar.gz

---
+++ packages/mypy-boto3-elastictranscoder/mypy-boto3-elastictranscoder-1.10.34.1.tar.gz/mypy-boto3-elastictranscoder-1.10.34.1/mypy_boto3_elastictranscoder/paginator.py
@@ -0,0 +1,78 @@
+"Main interface for elastictranscoder service Paginators"
+from __future__ import annotations
+
+from botocore.paginate import Paginator as Boto3Paginator
+from mypy_boto3_elastictranscoder.type_defs import (
+    ListJobsByPipelineResponseTypeDef,
+    ListJobsByStatusResponseTypeDef,
+    ListPipelinesResponseTypeDef,
+    ListPresetsResponseTypeDef,
+    PaginatorConfigTypeDef,
+)
+
+
+__all__ = (
+    "ListJobsByPipelinePaginator",
+    "ListJobsByStatusPaginator",
+    "ListPipelinesPaginator",
+    "ListPresetsPaginator",
+)
+
+
+class ListJobsByPipelinePaginator(Boto3Paginator):
+    """
+    [Paginator.ListJobsByPipeline documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.34/reference/services/elastictranscoder.html#ElasticTranscoder.Paginator.ListJobsByPipeline)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self,
+        PipelineId: str,
+        Ascending: str = None,
+        PaginationConfig: PaginatorConfigTypeDef = None,
+    ) -> ListJobsByPipelineResponseTypeDef:
+        """
+        [ListJobsByPipeline.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.34/reference/services/elastictranscoder.html#ElasticTranscoder.Paginator.ListJobsByPipeline.paginate)
+        """
+
+
+class ListJobsByStatusPaginator(Boto3Paginator):
+    """
+    [Paginator.ListJobsByStatus documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.34/reference/services/elastictranscoder.html#ElasticTranscoder.Paginator.ListJobsByStatus)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self, Status: str, Ascending: str = None, PaginationConfig: PaginatorConfigTypeDef = None
+    ) -> ListJobsByStatusResponseTypeDef:
+        """
+        [ListJobsByStatus.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.34/reference/services/elastictranscoder.html#ElasticTranscoder.Paginator.ListJobsByStatus.paginate)
+        """
+
+
+class ListPipelinesPaginator(Boto3Paginator):
+    """
+    [Paginator.ListPipelines documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.34/reference/services/elastictranscoder.html#ElasticTranscoder.Paginator.ListPipelines)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self, Ascending: str = None, PaginationConfig: PaginatorConfigTypeDef = None
+    ) -> ListPipelinesResponseTypeDef:
+        """
+        [ListPipelines.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.34/reference/services/elastictranscoder.html#ElasticTranscoder.Paginator.ListPipelines.paginate)
+        """
+
+
+class ListPresetsPaginator(Boto3Paginator):
+    """
+    [Paginator.ListPresets documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.34/reference/services/elastictranscoder.html#ElasticTranscoder.Paginator.ListPresets)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self, Ascending: str = None, PaginationConfig: PaginatorConfigTypeDef = None
+    ) -> ListPresetsResponseTypeDef:
+        """
+        [ListPresets.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.34/reference/services/elastictranscoder.html#ElasticTranscoder.Paginator.ListPresets.paginate)
+        """
