703095681abef8d60286a6773ba2185082284fc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:00 +0000

    Add package checkov/1.0.29/checkov-1.0.29-py3-none-any.whl

---
+++ packages/checkov/checkov-1.0.29-py3-none-any.whl/tests/terraform/test_scanner_registry.py
@@ -0,0 +1,20 @@
+import unittest
+
+from checkov.terraform.checks.resource.registry import Registry
+
+
+class TestScannerRegistry(unittest.TestCase):
+
+    def test_num_of_scanners(self):
+        registry = Registry()
+        scanners_counter = 0
+        for key in list(registry.checks.keys()):
+            scanners_counter+=len(registry.checks[key])
+
+        self.assertEqual(41,scanners_counter)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
