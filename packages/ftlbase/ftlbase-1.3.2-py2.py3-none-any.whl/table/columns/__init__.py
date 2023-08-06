9b430fb0ca995e5454220c979e93f89d705d9dc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:46 +0000

    Add package ftlbase/1.3.2/ftlbase-1.3.2-py2.py3-none-any.whl

---
+++ packages/ftlbase/ftlbase-1.3.2-py2.py3-none-any.whl/table/columns/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+from .base import Column, BoundColumn  # NOQA
+from .calendarcolumn import MonthsColumn, WeeksColumn, DaysColumn, CalendarColumn  # NOQA
+from .checkboxcolumn import CheckboxColumn  # NOQA
+from .datecolumn import DateColumn  # NOQA
+from .datetimecolumn import DatetimeColumn  # NOQA
+from .linkcolumn import LinkColumn, Link, ImageLink, FontAwesomeLink, GlyphiconSpanLink, ActionColumn  # NOQA
+from .sequencecolumn import SequenceColumn  # NOQA
---
+++ packages/ftlbase/ftlbase-1.3.2-py2.py3-none-any.whl/table/columns/choicecolumn.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from django.utils.html import escape
+
+from table.utils import Accessor
+from .base import Column
+
+
+class ChoiceColumn(Column):
+    SIM_NAO_C = (('S', 'Sim'), ('N', 'Não'))
+
+    def __init__(self, field, header=None, choices=SIM_NAO_C, **kwargs):
+        self.choices = choices
+        super().__init__(field, header, **kwargs)
+
+    def render(self, obj):
+        obj_data = Accessor(self.field).resolve(obj)
+        if obj_data:
+            text = [name for val, name in self.choices if obj_data == val]
+            try:
+                text = text[0]
+            except:
+                text = ''
+        else:
+            text = ''
+        return escape(text)
