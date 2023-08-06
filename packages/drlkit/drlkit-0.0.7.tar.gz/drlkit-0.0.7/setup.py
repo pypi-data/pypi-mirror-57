9fd2a6579d9f0e0043c315033b260288d936a8f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:36 +0000

    Add package drlkit/0.0.7/drlkit-0.0.7.tar.gz

---
+++ packages/drlkit/drlkit-0.0.7.tar.gz/drlkit-0.0.7/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+from setuptools import find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='drlkit',
+    version='0.0.7',
+    description='A High Level Python Deep Reinforcement Learning library. Great for beginners, for prototyping and quickly comparing algorithms',
+    install_requires=[
+            'torch',
+            'numpy',
+            'tqdm',
+            'matplotlib',
+            'box2d-py',
+            'gym',
+            'keras',
+        ],
+    license='MIT',
+    py_modules=['drlkit'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+        "Operating System :: OS Independent",
+        ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/FranckNdame/drlkit",
+    author="Franck Ndame",
+    author_email="franck.mpouli@gmail.com",
+    packages=find_packages(),
+    python_requires='>=3.6',
+)
