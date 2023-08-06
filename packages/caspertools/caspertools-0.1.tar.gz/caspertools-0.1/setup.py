1cb30a195094d27592343d96843008b430ad0104
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:13 +0000

    Add package caspertools/0.1/caspertools-0.1.tar.gz

---
+++ packages/caspertools/caspertools-0.1.tar.gz/caspertools-0.1/setup.py
@@ -0,0 +1,16 @@
+#! usr/bin/python
+# -*- coding: utf-8 *-* 
+
+from distutils.core import setup
+setup(
+  name = 'caspertools',
+  packages = ['caspertools'],
+  version = '0.1',
+  description = 'Tools used for hidden navigation',
+  author = 'José Fº Queija',
+  author_email = 'pepekiko@gmail.com',
+  url = 'https://github.com/jfqueija/caspertools',
+  download_url = 'https://github.com/jfqueija/caspertools/tarball/0.1',
+  keywords = ['hidden', 'navigation', 'tor','privoxy'],
+  classifiers = [],
+)
