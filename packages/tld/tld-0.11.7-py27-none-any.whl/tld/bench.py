d2867c1461e9951b02afea28e7dfed161c1ab658
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:02 +0000

    Add package tld/0.11.7/tld-0.11.7-py27-none-any.whl

---
+++ packages/tld/tld-0.11.7-py27-none-any.whl/tld/bench.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+from tld import get_tld
+URLS_COUNT = 10000
+URLS = [u'http://www.google.co.uk', u'http://www.v2.google.co.uk', u'http://www.google.co.uk:8001/lorem-ipsum/', u'http://www.me.cloudfront.net',
+        u'http://www.v2.forum.tech.google.co.uk:8001/lorem-ipsum/', u'https://pantheon.io/', u'delusionalinsanity.com', u'www.baidu.com.cn', u'i.dont.exist', u'http://delusionalinsanity.com']
+TIMES = 10000
+for _ in range(TIMES):
+    for url in URLS:
+        tld = get_tld(url, fix_protocol=True, fail_silently=True)
---
+++ packages/tld/tld-0.11.7-py27-none-any.whl/tld/defaults.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+from os.path import dirname
+__title__ = u'tld.defaults'
+__author__ = u'Artur Barseghyan'
+__copyright__ = u'2013-2019 Artur Barseghyan'
+__license__ = u'MPL-1.1 OR GPL-2.0-only OR LGPL-2.0-or-later'
+__all__ = (u'DEBUG', u'NAMES_LOCAL_PATH_PARENT')
+NAMES_LOCAL_PATH_PARENT = dirname(__file__)
+DEBUG = False
