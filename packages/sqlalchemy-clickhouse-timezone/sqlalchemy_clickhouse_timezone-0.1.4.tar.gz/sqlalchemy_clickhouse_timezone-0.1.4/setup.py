0a4cee779f40846942a8b42e7a8d73bc964cf72f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:04 +0000

    Add package sqlalchemy-clickhouse-timezone/0.1.4/sqlalchemy_clickhouse_timezone-0.1.4.tar.gz

---
+++ packages/sqlalchemy-clickhouse-timezone/sqlalchemy_clickhouse_timezone-0.1.4.tar.gz/sqlalchemy_clickhouse_timezone-0.1.4/setup.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+from os import path, getenv
+from setuptools import setup
+from codecs import open
+
+VERSION = [0, 1, 4]
+readme = open('README.rst').read()
+
+setup(
+    name='sqlalchemy_clickhouse_timezone',
+    version='.'.join('%d' % v for v in VERSION[0:3]),
+    description='ClickHouse SQLAlchemy Dialect with timezone support',
+    long_description = readme,
+    author = '21499836@qq.com',
+    author_email = '21499836@qq.com',
+    license = 'Apache License, Version 2.0',
+    url = 'https://github.com/ufully/sqlalchemy-clickhouse-timezone',
+    keywords = "db database cloud analytics clickhouse with tiemzone support",
+    download_url = 'https://github.com/ufully/sqlalchemy-clickhouse-timezone/releases/tag/v0.1.0',
+    install_requires = [
+        'sqlalchemy>=1.0.0',
+        'infi.clickhouse_orm==1.0.4',
+        'tzlocal>=2.0.0'
+    ],
+    packages=[
+        'sqlalchemy_clickhouse_timezone',
+    ],
+    package_dir={
+        'sqlalchemy_clickhouse_timezone': '.',
+    },
+    package_data={
+        'sqlalchemy_clickhouse_timezone': ['LICENSE.txt'],
+    },
+    entry_points={
+        'sqlalchemy.dialects': [
+            'clickhouse=sqlalchemy_clickhouse_timezone.base',
+        ]
+    },
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+
+        'Environment :: Console',
+        'Environment :: Other Environment',
+
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+
+        'License :: OSI Approved :: Apache Software License',
+
+        'Operating System :: OS Independent',
+
+        'Programming Language :: SQL',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+
+        'Topic :: Database',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
