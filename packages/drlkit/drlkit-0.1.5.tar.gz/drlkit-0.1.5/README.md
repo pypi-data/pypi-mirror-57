29c742df421f30ae96b3e47db80dd3ecae5f5c0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:26 +0000

    Add package drlkit/0.1.5/drlkit-0.1.5.tar.gz

---
+++ packages/drlkit/drlkit-0.1.5.tar.gz/drlkit-0.1.5/README.md
@@ -0,0 +1,73 @@
+<p align="center">
+ <img src="https://raw.githubusercontent.com/FranckNdame/drlkit/master/images/drl-kit-banner.png" width=70% alt="Gitter">
+</p>
+
+--------------------------------------------------------------------------------
+<p align="center">
+   <a>
+      <img src="https://img.shields.io/badge/python-3.6+-blue.svg" alt="Gitter">
+   </a>
+   <a>
+      <img src="https://github.com/FranckNdame/drlkit/blob/master/images/torchbadge.svg" alt="Pytorch">
+   </a>
+   <a>
+      <img src="https://camo.githubusercontent.com/7ce7d8e78ad8ddab3bea83bb9b98128528bae110/68747470733a2f2f616c65656e34322e6769746875622e696f2f6261646765732f7372632f74656e736f72666c6f772e737667" alt="Gitter">
+   </a>
+   <a href="https://opensource.org/licenses/MIT">
+      <img src="https://img.shields.io/badge/License-MIT-yellow.svg" alt="Gitter">
+   </a>
+   <a href="https://opensource.org/licenses/MIT">
+      <img src="https://app.fossa.com/api/projects/git%2Bgithub.com%2Fwau%2Fkeras-rl2.svg?type=shield" alt="Gitter">
+   </a>
+  </p>
+
+A High Level Python Deep Reinforcement Learning library. Great for beginners,  prototyping and quickly comparing algorithms
+<br><br>
+<p align="center">
+ <img src="https://github.com/FranckNdame/drlkit/blob/master/images/environments.gif?raw=true" width=95% alt="Environments">
+</p>
+
+# UNDER CONSTRUCTION!
+Do not use yet!
+
+| System | 3.5 | 3.6 | 3.7 |
+| :---: | :---: | :---: | :--: |
+| Linux CPU | [![Build Status](https://ci.pytorch.org/jenkins/job/pytorch-master/badge/icon)](https://ci.pytorch.org/jenkins/job/pytorch-master/) | [![Build Status](https://ci.pytorch.org/jenkins/job/pytorch-master/badge/icon)](https://ci.pytorch.org/jenkins/job/pytorch-master/) | <center>—</center> |
+| Linux GPU | [![Build Status](https://ci.pytorch.org/jenkins/job/pytorch-master/badge/icon)](https://ci.pytorch.org/jenkins/job/pytorch-master/) | [![Build Status](https://ci.pytorch.org/jenkins/job/pytorch-master/badge/icon)](https://ci.pytorch.org/jenkins/job/pytorch-master/) | <center>—</center> |
+| Windows CPU / GPU | <center>—</center> | [![Build Status](https://ci.pytorch.org/jenkins/job/pytorch-builds/job/pytorch-win-ws2016-cuda9-cudnn7-py3-trigger/badge/icon)](https://ci.pytorch.org/jenkins/job/pytorch-builds/job/pytorch-win-ws2016-cuda9-cudnn7-py3-trigger/) |  <center>—</center> |
+| Linux (ppc64le) CPU | [![Build Status](https://powerci.osuosl.org/job/pytorch-master-nightly-py2-linux-ppc64le/badge/icon)](https://powerci.osuosl.org/job/pytorch-master-nightly-py2-linux-ppc64le/) | — | [![Build Status](https://powerci.osuosl.org/job/pytorch-master-nightly-py3-linux-ppc64le/badge/icon)](https://powerci.osuosl.org/job/pytorch-master-nightly-py3-linux-ppc64le/) |
+| Linux (ppc64le) GPU | [![Build Status](https://powerci.osuosl.org/job/pytorch-linux-cuda9-cudnn7-py2-mpi-build-test-gpu/badge/icon)](https://powerci.osuosl.org/job/pytorch-linux-cuda9-cudnn7-py2-mpi-build-test-gpu/) | — | [![Build Status](https://powerci.osuosl.org/job/pytorch-linux-cuda92-cudnn7-py3-mpi-build-test-gpu/badge/icon)](https://powerci.osuosl.org/job/pytorch-linux-cuda92-cudnn7-py3-mpi-build-test-gpu/) |
+
+
+## Installation
+
+Run the following to install:
+
+```python
+pip install drlkit
+```
+
+## Usage
+
+```python
+import numpy as np
+from drlkit import TorchAgent, Plot, EnvironmentWrapper
+
+ENV_NAME = "LunarLander-v2"
+env = EnvironmentWrapper(ENV_NAME)
+agent = TorchAgent(state_size=8, action_size=env.env.action_space.n, seed=0)
+
+# Train the agent
+env.fit(agent, n_episodes=1000)
+
+# See the results
+Plot.basic_plot(np.arange(len(env.scores)), env.scores, xlabel='Episode #', ylabel='Score')
+
+
+# Play untrained agent
+env.load_model(agent, env="LunarLander", elapsed_episodes=3000)
+env.play(num_episodes=10, trained=False)
+
+# Play trained agent
+env.play(num_episodes=10, trained=True)
+```
---
+++ packages/drlkit/drlkit-0.1.5.tar.gz/drlkit-0.1.5/drlkit/environments/wrapper.py
@@ -0,0 +1,140 @@
+import gym
+import os
+from collections import deque
+import sys
+import torch
+import numpy as np
+sys.path.append("..")
+from drlkit import models
+
+class EnvironmentWrapper(object):
+	def __init__(
+		self, name, max_ts=1_000, 
+		eps_start=1.0, eps_min =0.01, eps_decay=0.995,
+		seed=0, print_info=False
+	):
+		self.env_name = name
+		env = gym.make(name)
+		env.seed(seed)
+		self.env = env
+		
+		self.scores = []
+		self.scores_window = deque(maxlen=100)
+		self.eps = eps_start
+		
+		
+		self.max_ts = max_ts
+		self.eps_min = eps_min
+		self.eps_decay = eps_decay
+		self.print_info = print_info
+		
+		self.done = False
+		self.best_score = 0
+		
+		
+	def fit(self, agent, n_episodes):
+		self.agent = agent
+		self.n_episodes = n_episodes
+		for i_episode in range(1, n_episodes+1):
+			state = self.env.reset()
+			score = 0
+			for t in range(self.max_ts):
+				action = agent.act(state, self.eps)
+				next_state, reward, self.done, info = self.env.step(action)
+				if self.print_info: print(info)
+				agent.step(state, action, reward, next_state, self.done)
+				state = next_state
+				score += reward
+				if self.done:
+					break
+			# After the episode
+			self.scores_window.append(score) # push recent score
+			self.scores.append(score) # save recent score
+			self.eps = max(self.eps_min, self.eps*self.eps_decay) # decrease exploration rate
+			self.monitor_progress(i_episode)
+		print("==== Training complete! =====")
+		print(f"# Episodes: {n_episodes} || score: {np.mean(self.scores_window)}")
+			
+	def monitor_progress(self, episode):
+		print('\rEpisode {}\tAverage Score: {:.2f}'.format(episode, np.mean(self.scores_window)), end="")
+		if not episode % 100:
+			print('\rEpisode {}\tAverage Score: {:.2f}'.format(episode, np.mean(self.scores_window)))
+		if np.mean(self.scores_window)>=200.0:
+				if np.mean(self.scores_window) > self.best_score:
+					print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(episode, np.mean(self.scores_window)))
+					filename = f'{self.env_name}-{episode}.pth'
+					self.best_score = np.mean(self.scores_window)
+					self.save_model(filename, self.agent.target_network.state_dict())
+			
+		
+				
+		
+	def save_model(self, filename, file):
+		"""Save model
+				
+		Params
+		======
+			dir (string): directory
+			filename (string): filename
+			file (dictionary): model parameters		
+		"""
+
+		path = "./models/"
+		if not os.path.exists(path):
+			os.makedirs(path)
+			print("Directory created")
+		torch.save(file, path+filename)
+		print(f"Model saved! @ {path+filename}")
+			
+	def list_models(self):
+		path = path = "./models/"
+		if not os.path.exists(path):
+			print(f"No model found!")
+		else:
+			lst = os.listdir(path)
+			print(f"Models Available")
+			print("===========================")
+			i = 1
+			for item in lst:
+				if item != "__init__.py":
+					print(f"{i} - {item}") 
+					i += 1
+		
+		
+	def load_model(self, agent, env_name, elapsed_episodes):
+		self.agent = agent
+		path = os.path.dirname(models.__file__)
+		path += f"/{env_name}/episode-{elapsed_episodes}.pth"
+		if not os.path.exists(path):
+			print(f"No such model saved! @ {path}")
+			return
+		if torch.cuda.is_available():	
+			agent.target_network.load_state_dict(torch.load(path))
+		else:
+			agent.target_network.load_state_dict(torch.load(path, map_location=torch.device('cpu')))
+		print("Model Loaded!")
+		
+	def load_prebuilt_model(self, agent, path):
+			self.agent = agent
+			if not os.path.exists(path):
+				print(f"No such model saved!  @ {path}")
+				return
+			if torch.cuda.is_available():	
+				agent.target_network.load_state_dict(torch.load(path))
+			else:
+				agent.target_network.load_state_dict(torch.load(path, map_location=torch.device('cpu')))
+				
+			print("Model Loaded!")
+		
+		
+	def play(self, num_episodes=10, max_ts=200, trained=True):
+		for i in range(1,num_episodes+1):
+			state = self.env.reset()
+			for ts in range(max_ts):
+				action = self.agent.act(state) if trained else self.env.action_space.sample()
+				self.env.render()
+				state, reward, done, _ = self.env.step(action)
+				if done:
+					break 
+					
+		self.env.close()
