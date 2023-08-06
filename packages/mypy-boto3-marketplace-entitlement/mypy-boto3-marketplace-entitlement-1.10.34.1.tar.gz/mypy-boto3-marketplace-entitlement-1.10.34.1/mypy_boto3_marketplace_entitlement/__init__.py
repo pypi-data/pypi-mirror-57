928471e37f4f1444403bb73cd1c593d5fbb38d30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:53 +0000

    Add package mypy-boto3-marketplace-entitlement/1.10.34.1/mypy-boto3-marketplace-entitlement-1.10.34.1.tar.gz

---
+++ packages/mypy-boto3-marketplace-entitlement/mypy-boto3-marketplace-entitlement-1.10.34.1.tar.gz/mypy-boto3-marketplace-entitlement-1.10.34.1/mypy_boto3_marketplace_entitlement/__init__.py
@@ -0,0 +1,7 @@
+"Main interface for marketplace-entitlement service"
+
+from mypy_boto3_marketplace_entitlement.client import Client
+from mypy_boto3_marketplace_entitlement.paginator import GetEntitlementsPaginator
+
+
+__all__ = ("Client", "GetEntitlementsPaginator")
