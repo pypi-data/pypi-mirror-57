0f9522a7248bfe900ccb0f110fab46d07ce65e8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:41 +0000

    Add package django-s3-image-service/1.0.1/django_s3_image_service-1.0.1-py3-none-any.whl

---
+++ packages/django-s3-image-service/django_s3_image_service-1.0.1-py3-none-any.whl/s3imageservice/cms/views/list.py
@@ -0,0 +1,13 @@
+from s3imageservice.cms.views.base import CMSBaseView
+from django.views.generic.base import RedirectView
+
+
+class List(CMSBaseView):
+    template_name = "list.html"
+
+    def test_model_instance_exists(self, request, *args, **kwargs):
+        pass
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
---
+++ packages/django-s3-image-service/django_s3_image_service-1.0.1-py3-none-any.whl/s3imageservice/cms/views/upload.py
@@ -0,0 +1,13 @@
+from s3imageservice.cms.views.base import CMSBaseView
+from django.views.generic.base import RedirectView
+
+
+class Upload(CMSBaseView):
+    template_name = "upload.html"
+
+    def test_model_instance_exists(self, request, *args, **kwargs):
+        pass
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
