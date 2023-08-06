bff3492cc8ddd58c60fb794d7f300f778481f0d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:10 +0000

    Add package mypy-boto3-kinesis-video-media/1.10.39.2/mypy-boto3-kinesis-video-media-1.10.39.2.tar.gz

---
+++ packages/mypy-boto3-kinesis-video-media/mypy-boto3-kinesis-video-media-1.10.39.2.tar.gz/mypy-boto3-kinesis-video-media-1.10.39.2/mypy_boto3_kinesis_video_media/__init__.py
@@ -0,0 +1,8 @@
+"Main interface for kinesis-video-media service"
+from mypy_boto3_kinesis_video_media.client import (
+    KinesisVideoMediaClient as Client,
+    KinesisVideoMediaClient,
+)
+
+
+__all__ = ("Client", "KinesisVideoMediaClient")
