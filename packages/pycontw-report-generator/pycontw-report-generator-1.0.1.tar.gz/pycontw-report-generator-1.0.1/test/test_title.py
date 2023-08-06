ce68da9f4e49ff854cbe3b20cf591966ae4a8baf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:33 +0000

    Add package pycontw-report-generator/1.0.1/pycontw-report-generator-1.0.1.tar.gz

---
+++ packages/pycontw-report-generator/pycontw-report-generator-1.0.1.tar.gz/pycontw-report-generator-1.0.1/test/test_title.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import csv
+from report_generator.cleaner.title import cat_title
+
+
+class TestTitleCategory:
+    def test_mapping(self):
+        with open("test/data/title-category.csv") as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                assert cat_title(row["QUESTION"]) == row["ANSWER"]
