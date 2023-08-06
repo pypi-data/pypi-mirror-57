690b99ec94159fe22177c6c101ffb0031e55dc18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:17 +0000

    Add package cozytouchpy/1.4/cozytouchpy-1.4-py3-none-any.whl

---
+++ packages/cozytouchpy/cozytouchpy-1.4-py3-none-any.whl/cozytouchpy/__init__.py
@@ -0,0 +1,10 @@
+#-*- coding:utf-8 -*-
+
+'''
+Provides authentification and row access to Cozytouch modules.
+'''
+name='cozytouchpy'
+__version__ = '1.4'
+__all__ = ['cozytouchpy']
+
+from .client import CozytouchClient
