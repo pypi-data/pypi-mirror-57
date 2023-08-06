e52a97932ac0b27e9e4f68cc7a354b056b063a50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:15 +0000

    Add package pysfmov/0.2/pysfmov-0.2.tar.gz

---
+++ packages/pysfmov/pysfmov-0.2.tar.gz/pysfmov-0.2/setup.py
@@ -0,0 +1,28 @@
+with open('README.rst', 'r') as f:
+    readme = f.read()
+    
+def parse_requirements(filename):
+    ''' Load requirements from a pip requirements file '''
+    with open(filename, 'r') as fd:
+        lines = []
+        for line in fd:
+            line.strip()
+            if line and not line.startswith("#"):
+                lines.append(line)
+    return lines
+
+requirements = parse_requirements('requirements.txt')
+
+#from distutils.core import setup, Extension
+from setuptools import setup, Extension
+from pysfmov import __version__
+setup(name='pysfmov',
+      version='0.2',
+      author='Lorenzo Capponi, Domen Gorjup, Janko Slavič',
+      author_email='lorenzocapponi@outlook.it, domen.gorjup@fs.uni-lj.si, janko.slavic@fs.uni-lj.si',
+      description='Module for reading ResearchIR SFMOV image sequences',
+      url='https://github.com/LolloCappo/pysfmov',
+      py_modules=['pysfmov'],
+      long_description=readme,
+      install_requires=requirements
+      )
