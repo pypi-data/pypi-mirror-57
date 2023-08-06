d4a274d266346571401c4a8937fc63544b24d40c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:07 +0000

    Add package clabe/0.2.3/clabe-0.2.3.tar.gz

---
+++ packages/clabe/clabe-0.2.3.tar.gz/clabe-0.2.3/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+
+setuptools.setup(
+    name='clabe',
+    version='0.2.3',
+    author='Cuenca',
+    author_email='dev@cuenca.com',
+    description='Validate and generate the control digit of a CLABE in Mexico',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/cuenca-mx/clabe',
+    packages=setuptools.find_packages(),
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    extras_require={
+        'dev': [
+            'pytest>=3',
+            'pycodestyle',
+            'coverage'
+        ]
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+)
