bcc00e39690395e75b690ff6ea887dcefd587cb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:57 +0000

    Add package apidaora/0.12.0/apidaora-0.12.0.tar.gz

---
+++ packages/apidaora/apidaora-0.12.0.tar.gz/apidaora-0.12.0/apidaora/__init__.py
@@ -0,0 +1,28 @@
+"""
+ASGI App using dataclasses module for request/response objects
+"""
+
+__version__ = '0.12.0'
+
+from apidaora.app import appdaora
+from apidaora.bodies import GZipFactory
+from apidaora.content import ContentType
+from apidaora.exceptions import BadRequestError
+from apidaora.header import Header
+from apidaora.method import MethodType
+from apidaora.responses import html, json, text
+from apidaora.route.decorator import route
+
+
+__all__ = [
+    'BadRequestError',
+    'GZipFactory',
+    'ContentType',
+    'MethodType',
+    'appdaora',
+    'Header',
+    'html',
+    'json',
+    'text',
+    'route',
+]
