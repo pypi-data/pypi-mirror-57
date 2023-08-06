c3a67443953fff4459eb7196fd014e2eec1afc58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:25 +0000

    Add package pymedphys/0.14.2/pymedphys-0.14.2.tar.gz

---
+++ packages/pymedphys/pymedphys-0.14.2.tar.gz/pymedphys-0.14.2/pymedphys/electronfactors.py
@@ -0,0 +1,16 @@
+"""A suite of functions that model electron insert/cutout factors by
+parameterising them as equivalent ellipses.
+"""
+
+# pylint: disable = unused-import
+
+from ._electronfactors import (
+    calculate_deformability,
+    calculate_percent_prediction_differences,
+    convert2_ratio_perim_area,
+    create_transformed_mesh,
+    parameterise_insert,
+    spline_model,
+    spline_model_with_deformability,
+    visual_alignment_of_equivalent_ellipse,
+)
