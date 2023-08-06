38d0b19261823de13f879121fb4f90de7bc51852
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:08 +0000

    Add package explorator/0.0.12/explorator-0.0.12.tar.gz

---
+++ packages/explorator/explorator-0.0.12.tar.gz/explorator-0.0.12/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+version = '0.0.12'
+setup(
+    name='explorator',
+    version=version,
+    description='Package to quickly explore data in pandas dataframe',
+    license='',
+    packages=['explorator'],
+    author='Boris Zakhir',
+    author_email='bz@omnichannel.ru',
+    keywords=['pandas','exploratory'],
+    url=''
+)
