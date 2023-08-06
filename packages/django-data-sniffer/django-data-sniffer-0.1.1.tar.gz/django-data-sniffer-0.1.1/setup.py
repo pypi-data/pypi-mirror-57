967f8325cd1c9bfa86b33e4852434d172d4736e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:32 +0000

    Add package django-data-sniffer/0.1.1/django-data-sniffer-0.1.1.tar.gz

---
+++ packages/django-data-sniffer/django-data-sniffer-0.1.1.tar.gz/django-data-sniffer-0.1.1/setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.md')).read()
+
+setup(
+    name='django-data-sniffer',
+    version='0.1.1',
+    packages=[
+        'data_sniffer',
+    ],
+    include_package_data=True,
+    description='Sniff your django models to look for any data issues',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    author='Fedor Garin',
+    author_email='fedor.garin@gmail.com',
+    url='https://github.com/thefedoration/django-data-sniffer/',
+    license='MIT',
+    install_requires=[
+        'Django>=1.7,<1.9',
+        'requests'
+    ]
+)
