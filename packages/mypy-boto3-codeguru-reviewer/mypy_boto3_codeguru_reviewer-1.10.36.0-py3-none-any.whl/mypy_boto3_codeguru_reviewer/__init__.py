675b15876563bb2969fb2441a5213ae7ab584e6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:33 +0000

    Add package mypy-boto3-codeguru-reviewer/1.10.36.0/mypy_boto3_codeguru_reviewer-1.10.36.0-py3-none-any.whl

---
+++ packages/mypy-boto3-codeguru-reviewer/mypy_boto3_codeguru_reviewer-1.10.36.0-py3-none-any.whl/mypy_boto3_codeguru_reviewer/__init__.py
@@ -0,0 +1,7 @@
+"Main interface for codeguru-reviewer service"
+
+from mypy_boto3_codeguru_reviewer.client import Client
+from mypy_boto3_codeguru_reviewer.paginator import ListRepositoryAssociationsPaginator
+
+
+__all__ = ("Client", "ListRepositoryAssociationsPaginator")
