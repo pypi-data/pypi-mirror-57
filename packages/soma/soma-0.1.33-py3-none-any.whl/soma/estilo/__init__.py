156b7a3e6df2304d9dfaabbccbbae6151010dd64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:14 +0000

    Add package soma/0.1.33/soma-0.1.33-py3-none-any.whl

---
+++ packages/soma/soma-0.1.33-py3-none-any.whl/soma/estilo/__init__.py
@@ -0,0 +1,17 @@
+from .utils import connect_and_query
+from .utils import format_produto_animale
+from .utils import calculate_giro
+from .utils import clean_animale_dataset
+from .utils import load_provao_dataset
+from .utils import label_encode_col
+from .utils import reorganize_tags
+
+from .modelling import correlation_matrix
+from .modelling import distance_matrix
+from .modelling import distance_sorter
+from .modelling import consolidate_votes
+from .modelling import count_unique_votes
+from .modelling import make_model
+from .modelling import search_models
+
+from .datasets import Datasets
