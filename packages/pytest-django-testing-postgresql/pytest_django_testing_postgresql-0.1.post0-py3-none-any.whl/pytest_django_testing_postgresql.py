8525e9a81a00a46aea58b0a5c9f9abdd8e6ab35a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:04 +0000

    Add package pytest-django-testing-postgresql/0.1.post0/pytest_django_testing_postgresql-0.1.post0-py3-none-any.whl

---
+++ packages/pytest-django-testing-postgresql/pytest_django_testing_postgresql-0.1.post0-py3-none-any.whl/pytest_django_testing_postgresql.py
@@ -0,0 +1,17 @@
+import os
+
+from dj_database_url import parse
+from django.conf import settings
+import pytest
+from testing.postgresql import Postgresql
+
+_POSTGRESQL = Postgresql()
+
+
+@pytest.hookimpl(tryfirst=True)
+def pytest_load_initial_conftests(early_config, parser, args):
+    os.environ['DJANGO_SETTINGS_MODULE'] = early_config.getini('DJANGO_SETTINGS_MODULE')
+    settings.DATABASES['default'] = parse(_POSTGRESQL.url())
+
+def pytest_unconfigure(config):
+    _POSTGRESQL.stop()
