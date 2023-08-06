b19da57daba1bf71bf5f6f028d2f9562ad33d18e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:00 +0000

    Add package awesome-finder/1.2.3/awesome_finder-1.2.3-py2.py3-none-any.whl

---
+++ packages/awesome-finder/awesome_finder-1.2.3-py2.py3-none-any.whl/awesome/__init__.py
@@ -0,0 +1,6 @@
+"""
+awesome-finder - A TUI based awesome curated list finder
+"""
+__version__ = '1.2.3'
+__author__ = 'mingrammer'
+__license__ = 'MIT'
---
+++ packages/awesome-finder/awesome_finder-1.2.3-py2.py3-none-any.whl/awesome/parser_loader.py
@@ -0,0 +1,39 @@
+from .parsers.android import AwesomeAndroidParser
+from .parsers.awesome import AwesomeParser
+from .parsers.elixir import AwesomeElixirParser
+from .parsers.erlang import AwesomeErlangParser
+from .parsers.go import AwesomeGoParser
+from .parsers.ios import AwesomeIosParser
+from .parsers.java import AwesomeJavaParser
+from .parsers.javascript import AwesomeJavaScriptParser
+from .parsers.php import AwesomePHPParser
+from .parsers.python import AwesomePythonParser
+from .parsers.ruby import AwesomeRubyParser
+from .parsers.rust import AwesomeRustParser
+from .parsers.scala import AwesomeScalaParser
+from .parsers.shell import AwesomeShellParser
+from .parsers.swift import AwesomeSwiftParser
+from .parsers.vue import AwesomeVueParser
+from .parsers.nodejs import AwesomeNodejsParser
+
+
+def load_parsers():
+    return {
+        'android': AwesomeAndroidParser,
+        'awesome': AwesomeParser,
+        'elixir': AwesomeElixirParser,
+        'erlang': AwesomeErlangParser,
+        'go': AwesomeGoParser,
+        'ios': AwesomeIosParser,
+        'java': AwesomeJavaParser,
+        'javascript': AwesomeJavaScriptParser,
+        'php': AwesomePHPParser,
+        'python': AwesomePythonParser,
+        'ruby': AwesomeRubyParser,
+        'rust': AwesomeRustParser,
+        'scala': AwesomeScalaParser,
+        'shell': AwesomeShellParser,
+        'swift': AwesomeSwiftParser,
+        'vue': AwesomeVueParser,
+        'nodejs': AwesomeNodejsParser,
+    }
