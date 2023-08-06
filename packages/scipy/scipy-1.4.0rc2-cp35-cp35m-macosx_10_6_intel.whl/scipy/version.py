ba2c23cce0d2aef133a7e1ea77aebd0b93197348
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:00 +0000

    Add package scipy/1.4.0rc2/scipy-1.4.0rc2-cp35-cp35m-macosx_10_6_intel.whl

---
+++ packages/scipy/scipy-1.4.0rc2-cp35-cp35m-macosx_10_6_intel.whl/scipy/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM SCIPY SETUP.PY
+short_version = '1.4.0rc2'
+version = '1.4.0rc2'
+full_version = '1.4.0rc2'
+git_revision = '1a4463e21b9b9fb6f7ba2e9505d3cd192681cc76'
+release = True
+
+if not release:
+    version = full_version
