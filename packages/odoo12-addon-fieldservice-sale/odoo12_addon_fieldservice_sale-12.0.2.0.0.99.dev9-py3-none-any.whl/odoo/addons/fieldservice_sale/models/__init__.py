a6bfa34077f02b856fc43d6eec0f6943099c7730
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:55 +0000

    Add package odoo12-addon-fieldservice-sale/12.0.2.0.0.99.dev9/odoo12_addon_fieldservice_sale-12.0.2.0.0.99.dev9-py3-none-any.whl

---
+++ packages/odoo12-addon-fieldservice-sale/odoo12_addon_fieldservice_sale-12.0.2.0.0.99.dev9-py3-none-any.whl/odoo/addons/fieldservice_sale/models/__init__.py
@@ -0,0 +1,10 @@
+# Copyright (C) 2018 Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import (
+    fsm_location,
+    product_template,
+    sale_order,
+    sale_order_line,
+    fsm_order
+)
