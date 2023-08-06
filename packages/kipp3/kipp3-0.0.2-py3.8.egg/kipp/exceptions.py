de99c1ba20f9548c5b321bfab1d8fc16dd596110
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:04 +0000

    Add package kipp3/0.0.2/kipp3-0.0.2-py3.8.egg

---
+++ packages/kipp3/kipp3-0.0.2-py3.8.egg/kipp/exceptions.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+---------------
+Kipp Exceptions
+---------------
+
+KippException
+  ├─ KippAIOException
+  │    └─ KippAIOTimeoutError
+  ├─ DBError
+  │    ├─ DBValidateError
+  │    ├─ DuplicateIndexError
+  │    └─ RecordNotFound
+  ├─ KippRunnerException
+  │    └─ KippRunnerTimeoutException
+
+"""
+
+
+from __future__ import unicode_literals
+
+from kipp.libs.exceptions import KippAIOException, KippAIOTimeoutError, KippException
+from kipp.models.exceptions import (
+    DBError,
+    DBValidateError,
+    DuplicateIndexError,
+    RecordNotFound,
+)
+from kipp.options import KippOptionsException, OptionKeyTypeConflictError
