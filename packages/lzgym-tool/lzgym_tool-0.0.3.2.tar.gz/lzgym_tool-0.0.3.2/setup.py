832207dc99a4db28d3acd7ccdcb73d4229f9d859
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:08 +0000

    Add package lzgym-tool/0.0.3.2/lzgym_tool-0.0.3.2.tar.gz

---
+++ packages/lzgym-tool/lzgym_tool-0.0.3.2.tar.gz/lzgym_tool-0.0.3.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lzgym_tool",
+    version="0.0.3.2",
+    author="Jacky Lee",
+    author_email="emulezhi@gmail.com",
+    description="LZGYM's Tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jackyli0318/lzgym-tool",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
