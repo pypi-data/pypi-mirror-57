116aea52c7fa5b03735a096d1c5c5d9973ac749e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:22 +0000

    Add package typeidea-czz/0.12/typeidea_czz-0.12-py3-none-any.whl

---
+++ packages/typeidea-czz/typeidea_czz-0.12-py3-none-any.whl/config/views.py
@@ -0,0 +1,17 @@
+from django.views.generic import ListView
+
+from blog.views import CommonViewMixin
+from task.views import TaskCommonViewMixin
+from .models import Link
+
+
+class LinkListView(CommonViewMixin, ListView):
+    queryset = Link.objects.filter(status=Link.STATUS_NORMAL)
+    template_name = 'config/links.html'
+    context_object_name = 'link_list'
+
+
+class TaskLinkListView(TaskCommonViewMixin, ListView):
+    queryset = Link.objects.filter(status=Link.STATUS_NORMAL)
+    template_name = 'config/links.html'
+    context_object_name = 'link_list'
