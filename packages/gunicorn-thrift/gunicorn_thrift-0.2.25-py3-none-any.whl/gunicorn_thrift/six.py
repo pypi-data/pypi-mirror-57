e4626525561ae7b62893ab5d055814556ceb0f4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:32 +0000

    Add package gunicorn-thrift/0.2.25/gunicorn_thrift-0.2.25-py3-none-any.whl

---
+++ packages/gunicorn-thrift/gunicorn_thrift-0.2.25-py3-none-any.whl/gunicorn_thrift/six.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+import sys
+
+PY_VERSION = sys.version_info[:3]
+
+
+if PY_VERSION <= (2, 8, 0):
+    DEFAULT_WORKER = "thrift_sync"
+    DEFAULT_TRANSPORT = "thrift.transport.TTransport:TBufferedTransportFactory"
+    DEFAULT_PROTOCOL = \
+        "thrift.protocol.TBinaryProtocol:TBinaryProtocolAcceleratedFactory"
+    AVAILABLE_WORKERS = {
+        'thrift_sync': 'gunicorn_thrift.sync_worker.SyncThriftWorker',
+        'thrift_gevent': 'gunicorn_thrift.gevent_worker.GeventThriftWorker',
+        'thriftpy_sync': 'gunicorn_thrift.thriftpy_sync_worker.SyncThriftPyWorker',
+        'thriftpy_gevent': 'gunicorn_thrift.thriftpy_gevent_worker.GeventThriftPyWorker',
+        }
+else:
+    DEFAULT_WORKER = "thriftpy_sync"
+    DEFAULT_TRANSPORT = "thriftpy2.transport:TBufferedTransportFactory"
+    DEFAULT_PROTOCOL = "thriftpy2.protocol:TBinaryProtocolFactory"
+    AVAILABLE_WORKERS = {
+        'thriftpy_sync': 'gunicorn_thrift.thriftpy_sync_worker.SyncThriftPyWorker',
+        'thriftpy_gevent': 'gunicorn_thrift.thriftpy_gevent_worker.GeventThriftPyWorker',
+        }
