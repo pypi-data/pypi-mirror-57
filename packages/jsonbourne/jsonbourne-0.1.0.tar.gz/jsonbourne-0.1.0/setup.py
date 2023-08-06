63c3892713f6e85809e7c1a41f552c29fd519332
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:47 +0000

    Add package jsonbourne/0.1.0/jsonbourne-0.1.0.tar.gz

---
+++ packages/jsonbourne/jsonbourne-0.1.0.tar.gz/jsonbourne-0.1.0/setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+modules = \
+['jsonbourne']
+setup_kwargs = {
+    'name': 'jsonbourne',
+    'version': '0.1.0',
+    'description': 'Import the best JSON lib',
+    'long_description': None,
+    'author': 'jesse',
+    'author_email': 'jesse@dgi.com',
+    'url': None,
+    'py_modules': modules,
+    'python_requires': '>=3.5,<4.0',
+}
+
+
+setup(**setup_kwargs)
