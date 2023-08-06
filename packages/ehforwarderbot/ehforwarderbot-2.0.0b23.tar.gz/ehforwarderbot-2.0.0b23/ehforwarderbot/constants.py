ec609584b73470d2b989e2f4f495bc3e8736d3a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:35 +0000

    Add package ehforwarderbot/2.0.0b23/ehforwarderbot-2.0.0b23.tar.gz

---
+++ packages/ehforwarderbot/ehforwarderbot-2.0.0b23.tar.gz/ehforwarderbot-2.0.0b23/ehforwarderbot/constants.py
@@ -0,0 +1,78 @@
+# coding=utf-8
+
+from enum import Enum
+
+
+class MsgType(Enum):
+    Text = "Text"
+    """Text message"""
+
+    Image = "Image"
+    """
+    Image (picture) message.
+
+    Notes:
+        Animated GIF images shall use :attr:`Animation` type instead.
+    """
+
+    Audio = "Audio"
+    """Audio message. Including music and voice message."""
+
+    Animation = "Animation"
+    """
+    Message with an animation, usually in the form of GIF or
+    soundless video.
+    """
+
+    Video = "Video"
+    """Video message"""
+
+    File = "File"
+    """Messages sent as a file."""
+
+    Location = "Location"
+    """Location message."""
+
+    Link = "Link"
+    """
+    Message that is mainly one specific link, or a
+    text message with one link preview.
+    """
+
+    Sticker = "Sticker"
+    """
+    Pictures sent with few text caption, usually a
+    transparent background, and a limited number
+    of options that is usually not from the user's
+    photo gallery.
+    """
+
+    Status = "Status"
+    """
+    Status from a user in a chat, usually typing and
+    uploading.
+    """
+
+    Unsupported = "Unsupported"
+    """
+    Any type of message that is not listed above.
+    A text representation is required.
+    """
+
+
+class ChatType(Enum):
+    User = "User"
+    Group = "Group"
+    System = "System"
+    Unknown = "Unknown"
+
+
+class TargetType(Enum):
+    Member = "Member"
+    Message = "Message"
+    Substitution = "Substitution"
+
+
+class ChannelType(Enum):
+    Master = "Master"
+    Slave = "Slave"
