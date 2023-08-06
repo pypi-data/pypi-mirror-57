b35696bdb9bc0487837e32814ab5f108eb25d19c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:17 +0000

    Add package gestionatr/2.0.0rc2/gestionatr-2.0.0rc2.tar.gz

---
+++ packages/gestionatr/gestionatr-2.0.0rc2.tar.gz/gestionatr-2.0.0rc2/gestionatr/input/messages/A1_03.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+from .A1_44 import *
+
+
+class A1_03(A1_44):
+
+    steps = []
+
+    @property
+    def annulmentreason(self):
+        tree = '{0}.annulmentreason'.format(self._header)
+        data = get_rec_attr(self.obj, tree, False)
+        if data is not None and data is not False:
+            return data.text
+        else:
+            return False
---
+++ packages/gestionatr/gestionatr-2.0.0rc2.tar.gz/gestionatr-2.0.0rc2/gestionatr/input/messages/A3.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+from .C2 import C2
+from .Deadlines import DeadLine, Workdays, Naturaldays
+from gestionatr.utils import get_rec_attr
+
+
+class A3(C2):
+    """Clase que implementa A3."""
+
+    steps = [
+        DeadLine('01', Workdays(5)),
+        DeadLine('02', Workdays(1)),
+        DeadLine('03', Naturaldays(30)),
+        DeadLine('05', Workdays(1)),
+        DeadLine('06', Workdays(5)),
+    ]
+
