82e0f7e04b08bb7715057ec28bfda4ecf78b4b01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:23 +0000

    Add package demisto-sdk/0.1.8/demisto-sdk-0.1.8.tar.gz

---
+++ packages/demisto-sdk/demisto-sdk-0.1.8.tar.gz/demisto-sdk-0.1.8/demisto_sdk/main.py
@@ -0,0 +1,11 @@
+from demisto_sdk.core import DemistoSDK
+
+
+def main():
+    # TODO: Typings and docstrings
+    sdk = DemistoSDK()
+    sdk.parse_args()
+
+
+if __name__ == '__main__':
+    main()
