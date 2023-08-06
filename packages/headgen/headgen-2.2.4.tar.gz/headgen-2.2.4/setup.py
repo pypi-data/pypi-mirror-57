d587f2e949701aa8e2d03b30719063dc122934ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:51 +0000

    Add package headgen/2.2.4/headgen-2.2.4.tar.gz

---
+++ packages/headgen/headgen-2.2.4.tar.gz/headgen-2.2.4/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+
+
+setup(
+	name='headgen',
+	version='2.2.4',
+	packages=find_packages(),
+	author='Miasnenko Dmitry',
+	author_email='d2ms2nk@gmail.com',
+	url='https://github.com/YoungMeatBoy/headgen.git',
+	install_requires = ['ansicolors', 'colorama', 'fnmatch', ]
+)
