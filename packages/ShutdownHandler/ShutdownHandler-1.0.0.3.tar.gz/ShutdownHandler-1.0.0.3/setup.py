881c0876da7cd359ccfd5fe3542c0db8aa7dd6a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:55 +0000

    Add package ShutdownHandler/1.0.0.3/ShutdownHandler-1.0.0.3.tar.gz

---
+++ packages/ShutdownHandler/ShutdownHandler-1.0.0.3.tar.gz/ShutdownHandler-1.0.0.3/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup, find_packages
+
+setup(name='ShutdownHandler',
+      version='1.0.0.3',
+      url='http://github.com/NicholasTancredi/ShutdownHandler',
+      packages=find_packages())
