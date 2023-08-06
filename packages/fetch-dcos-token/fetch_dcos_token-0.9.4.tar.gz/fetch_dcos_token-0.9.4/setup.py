332045e8d991ae456928c5348ce51ee1d22fcc2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:14 +0000

    Add package fetch-dcos-token/0.9.4/fetch_dcos_token-0.9.4.tar.gz

---
+++ packages/fetch-dcos-token/fetch_dcos_token-0.9.4.tar.gz/fetch_dcos_token-0.9.4/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+
+setuptools.setup(
+    name='fetch_dcos_token',
+    version='0.9.4',
+    author='Martijn Dekkers',
+    author_email='pypi@dekkers.org.uk',
+    description='Simple module to get a JWT token from a DC/OS master',
+    url='https://github.com/m-dekkers/fetch_dcos_token',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=['fetch_dcos_token'],
+    install_requires=['jwt', 'requests', 'datetime'],
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
