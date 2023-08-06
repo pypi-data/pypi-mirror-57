69587809a99e8cdfcfb05ce7e0a43f2b74623723
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:30 +0000

    Add package dpaste/3.4/dpaste-3.4-py2.py3-none-any.whl

---
+++ packages/dpaste/dpaste-3.4-py2.py3-none-any.whl/dpaste/settings/local.py
@@ -0,0 +1,45 @@
+from django.utils.translation import ugettext_lazy as _
+
+from dpaste.apps import dpasteAppConfig
+from dpaste.settings.base import *
+
+DEBUG = True
+
+DATABASES = {
+    'default': {'ENGINE': 'django.db.backends.sqlite3', 'NAME': 'dpaste.db'}
+}
+
+SECRET_KEY = 'changeme'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+INSTALLED_APPS += ('sslserver',)
+
+# Optionally run the runserver as `manage.py runsslserver` to locally
+# test correct cookie and csp behavior.
+if not 'runsslserver' in sys.argv:
+    SESSION_COOKIE_SECURE = False
+    CSRF_COOKIE_SECURE = False
+
+#
+# Uncomment the block below to use a custom AppConfig.
+#
+
+
+class ProductionDpasteAppConfig(dpasteAppConfig):
+    SLUG_LENGTH = 8
+    LEXER_DEFAULT = 'js'
+    EXPIRE_CHOICES = (
+        ('onetime', _(u'One Time Snippet')),
+        (3600, _(u'Expire in one hour')),
+        (3600 * 24, _('Expire in one day')),
+        (3600 * 24 * 7, _('Expire in one week')),
+        (3600 * 24 * 31, _('Expire in one month')),
+    )
+    EXPIRE_DEFAULT = 3600 * 24 * 7
+    RAW_MODE_ENABLED = True
+    RAW_MODE_PLAIN_TEXT = False
+
+
+INSTALLED_APPS.remove('dpaste.apps.dpasteAppConfig')
+INSTALLED_APPS.append('dpaste.settings.local.ProductionDpasteAppConfig')
