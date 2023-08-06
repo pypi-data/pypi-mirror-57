f087bb6870cc6d39fa60e458cd39c9de5ba0385e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:53 +0000

    Add package sight-machine/0.3/sight_machine-0.3.tar.gz

---
+++ packages/sight-machine/sight_machine-0.3.tar.gz/sight_machine-0.3/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='sight_machine',
+    version='0.3',
+    author="Denis Bajet",
+    author_email="dbajet@gmail.com",
+    description='Simple binary logger',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='http://github.com/dbajet/sight_machine/',
+    packages=setuptools.find_packages(),
+    python_requires='>=3.7',
+)
