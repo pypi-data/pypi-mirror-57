d0b657c95456d96461c79c437bbf2bdd6e9e80e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:02 +0000

    Add package pptop/0.5.23/pptop-0.5.23.tar.gz

---
+++ packages/pptop/pptop-0.5.23.tar.gz/pptop-0.5.23/setup.py
@@ -0,0 +1,33 @@
+__version__ = '0.5.23'
+
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+pptop_injector = setuptools.Extension('__pptop_injector',
+                                      sources=['src/__pptop_injector.c'])
+
+setuptools.setup(
+    name='pptop',
+    version=__version__,
+    author='Altertech',
+    author_email='div@altertech.com',
+    description='Open, extensible Python injector/profiler/analyzer',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://pptop.io/',
+    packages=setuptools.find_packages(),
+    license='MIT',
+    scripts=['bin/pptop'],
+    include_package_data=True,
+    ext_modules=[pptop_injector],
+    install_requires=[
+        'wheel', 'unipath', 'psutil', 'rapidtables', 'atasker>=0.6.4',
+        'pyyaml', 'yappi', 'neotermcolor', 'pygments', 'pyaltt2>=0.0.10'
+    ],
+    classifiers=('Programming Language :: Python :: 3',
+                 'License :: OSI Approved :: MIT License',
+                 'Topic :: Software Development :: Debuggers',
+                 'Topic :: Software Development :: Testing'),
+)
