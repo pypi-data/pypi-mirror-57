2d67e9060c74273ae692217f344a7c78f07abfbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:15 +0000

    Add package ibm-cos-sdk-core/2.6.0/ibm-cos-sdk-core-2.6.0.tar.gz

---
+++ packages/ibm-cos-sdk-core/ibm-cos-sdk-core-2.6.0.tar.gz/ibm-cos-sdk-core-2.6.0/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+import ibm_botocore
+import sys
+from setuptools import setup, find_packages
+
+# IbmCos sdk python version check
+_valid  =  sys.version_info[:2] == (2, 7) or sys.version_info >= (3,4)
+if not _valid:
+    sys.exit("Sorry, IBM COS SDK only supports versions 2.7, 3.4, 3.5, 3.6, 3.7 of python.")
+
+
+requires = ['jmespath>=0.7.1,<1.0.0',
+            'docutils>=0.10,<0.16',
+            'requests>=2.18,<2.23 ',
+            'python-dateutil>=2.1,<3.0.0']
+
+setup(
+    name='ibm-cos-sdk-core',
+    version=ibm_botocore.__version__,
+    description='Low-level, data-driven core of IBM SDK for Python',
+    long_description=open('README.rst').read(),
+    long_description_content_type='text/x-rst',
+    author='IBM',
+    url='https://github.com/ibm/ibm-cos-sdk-python-core',
+    scripts=[],
+    packages=find_packages(exclude=['tests*']),
+    package_data={'ibm_botocore': ['cacert.pem', 'data/*.json', 'data/*/*.json']},
+    include_package_data=True,
+    install_requires=requires,
+    license="Apache License 2.0",
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ),
+)
---
+++ packages/ibm-cos-sdk-core/ibm-cos-sdk-core-2.6.0.tar.gz/ibm-cos-sdk-core-2.6.0/tests/functional/test_retry.py
@@ -0,0 +1,71 @@
+# Copyright 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+import contextlib
+from tests import BaseSessionTest, mock, ClientHTTPStubber
+
+from ibm_botocore.exceptions import ClientError
+from ibm_botocore.config import Config
+
+
+class TestRetry(BaseSessionTest):
+    def setUp(self):
+        super(TestRetry, self).setUp()
+        self.region = 'us-west-2'
+        self.sleep_patch = mock.patch('time.sleep')
+        self.sleep_patch.start()
+
+    def tearDown(self):
+        self.sleep_patch.stop()
+
+    @contextlib.contextmanager
+    def assert_will_retry_n_times(self, client, num_retries):
+        num_responses = num_retries + 1
+        with ClientHTTPStubber(client) as http_stubber:
+            for _ in range(num_responses):
+                http_stubber.add_response(status=500, body=b'{}')
+            with self.assertRaisesRegexp(
+                    ClientError, 'reached max retries: %s' % num_retries):
+                yield
+            self.assertEqual(len(http_stubber.requests), num_responses)
+
+    def test_setting_max_attempts_does_not_set_for_other_clients(self):
+        # Make one client with max attempts configured.
+        self.session.create_client(
+            's3', self.region, config=Config(
+                retries={'max_attempts': 1}))
+
+        # Make another client that has no custom retry configured.
+        client = self.session.create_client('s3', self.region)
+        # It should use the default max retries, which should be four retries
+        # for this service.
+        with self.assert_will_retry_n_times(client, 4):
+            client.list_buckets()
+
+    
+    def test_set_max_attempts_on_session(self):
+        self.session.set_default_client_config(
+            Config(retries={'max_attempts': 1}))
+        # Max attempts should be inherited from the session.
+        client = self.session.create_client('s3', self.region)
+        with self.assert_will_retry_n_times(client, 1):
+            client.list_buckets()
+
+    def test_can_clobber_max_attempts_on_session(self):
+        self.session.set_default_client_config(
+            Config(retries={'max_attempts': 1}))
+        # Max attempts should override the session's configured max attempts.
+        client = self.session.create_client(
+            's3', self.region, config=Config(
+                retries={'max_attempts': 0}))
+        with self.assert_will_retry_n_times(client, 0):
+            client.list_buckets()
