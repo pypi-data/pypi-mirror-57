9877fb1787180ae5224d65111b450535e6599007
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:28 +0000

    Add package pynomer/0.1.0/pynomer-0.1.0.tar.gz

---
+++ packages/pynomer/pynomer-0.1.0.tar.gz/pynomer-0.1.0/pynomer/__init__.py
@@ -0,0 +1,16 @@
+"""pynomer - A python wrapper for nomer"""
+
+__version__ = "0.1.0"
+__author__ = "nleguillarme <nicolas.leguillarme@univ-grenoble-alpes.fr>"
+__all__ = []
+
+from .pynomer import (
+    version,
+    clean,
+    input_schema,
+    output_schema,
+    properties,
+    matchers,
+    replace,
+    append,
+)
