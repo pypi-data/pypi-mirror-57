1becb2ea1b01d6080431a4e93e199054100e421e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:12 +0000

    Add package DaPy/1.11.5/DaPy-1.11.5-py2.py3-none-any.whl

---
+++ packages/DaPy/DaPy-1.11.5-py2.py3-none-any.whl/DaPy/methods/core/__init__.py
@@ -0,0 +1,6 @@
+from .base import Activators
+from .base import BaseEngineModel
+from .base import Dense, Input
+from .multilayer_perceptron import BaseMLP
+from .linear_model import BaseLinearModel
+from .pagerank import PageRank
