c4e3c91280bec4a20c6b2d7f41cfeea7c989e146
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:36 +0000

    Add package mypy-boto3-imagebuilder/1.10.37.0/mypy-boto3-imagebuilder-1.10.37.0.tar.gz

---
+++ packages/mypy-boto3-imagebuilder/mypy-boto3-imagebuilder-1.10.37.0.tar.gz/mypy-boto3-imagebuilder-1.10.37.0/mypy_boto3_imagebuilder/__init__.py
@@ -0,0 +1,6 @@
+"Main interface for imagebuilder service"
+
+from mypy_boto3_imagebuilder.client import ImagebuilderClient as Client, ImagebuilderClient
+
+
+__all__ = ("Client", "ImagebuilderClient")
