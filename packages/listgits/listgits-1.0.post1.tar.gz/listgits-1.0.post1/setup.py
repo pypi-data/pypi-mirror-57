d99579b1c014452d0425668cb5a1795ffb342b6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:22 +0000

    Add package listgits/1.0.post1/listgits-1.0.post1.tar.gz

---
+++ packages/listgits/listgits-1.0.post1.tar.gz/listgits-1.0.post1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="listgits", 
+    version="1.0.post1",
+    author="Terje Sandstrom",
+    author_email="terje@hermit.no",
+    description="Python command line program for listing git repositories under a folder root, and optionally run any git command on those repos",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/OsirisTerje/listgits",
+    packages=['listgits'],
+    entry_points={'console_scripts': ['listgits = listgits.listgits:main']},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
