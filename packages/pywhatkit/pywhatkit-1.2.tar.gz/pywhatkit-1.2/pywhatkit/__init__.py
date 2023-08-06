61015173b9f745001529f2dc56e7423e0e44152e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:46 +0000

    Add package pywhatkit/1.2/pywhatkit-1.2.tar.gz

---
+++ packages/pywhatkit/pywhatkit-1.2.tar.gz/pywhatkit-1.2/pywhatkit/__init__.py
@@ -0,0 +1,81 @@
+import time
+import webbrowser as web
+import pyautogui as pg
+import wikipedia
+import requests
+from bs4 import BeautifulSoup
+
+last = time.time()
+
+sleeptm = "None, You can use this function to print the remaining time in seconds."
+def prnt_sleeptm():
+    return sleeptm
+
+def sendwhatmsg(phone_no, message, time_hour, time_min, print_waitTime=True):
+    '''Sends whatsapp message to a particulal number at given time
+Phone number should be in string format not int'''
+    if "+" not in phone_no:
+        raise Exception("Country code missing from phone_no")
+    if time_hour == 0:
+        time_hour = 24
+    callsec = (time_hour*3600)+(time_min*60)
+
+    curr = time.localtime()
+    currhr = curr.tm_hour
+    currmin = curr.tm_min
+    currsec = curr.tm_sec
+
+    currtotsec = (currhr*3600)+(currmin*60)+(currsec)
+    lefttm = callsec-currtotsec
+
+    if lefttm <= 0:
+        lefttm = 86400+lefttm
+
+    if lefttm < 60:
+        raise Exception("Call time must be greater than one minute")
+
+    else:
+        global sleeptm
+        sleeptm = lefttm-60
+        if print_waitTime :
+            print(prnt_sleeptm(),"+ 60 seconds left")
+        time.sleep(sleeptm)
+        web.open('https://web.whatsapp.com/send?phone='+phone_no+'&text='+message)
+        time.sleep(60)
+        width,height = pg.size()
+        pg.click(width,height/2)
+        pg.press('enter')
+
+def info(topic,lines=3):
+    '''Gives information on the topic'''
+    spe = wikipedia.summary(topic, sentences = lines)
+    print(spe)
+    
+def playonyt(title):
+    '''Opens YouTube video with following title'''
+    url = 'https://www.youtube.com/results?q=' + title
+    sc = requests.get(url)
+    sctext = sc.text
+    soup = BeautifulSoup(sctext,"html.parser")
+    songs = soup.findAll("div",{"class":"yt-lockup-video"})
+    song = songs[0].contents[0].contents[0].contents[0]
+    songurl = song["href"]
+    web.open("https://www.youtube.com"+songurl)
+
+def search(topic):
+    '''Searches about the topic on Google'''
+    link = 'https://www.google.com/search?q={}'.format(topic)
+    web.open(link)
+
+try : 
+    requests.get("https://www.google.com")
+    current = time.time()
+    tyme = current-last
+        
+except Exception:
+    exc = ("NO INTERNET - Pywhatkit needs active internet connection")
+    raise Exception(exc)
+
+if tyme >= 5:
+        raise Warning("INTERNET IS SLOW, extraction of information might take longer time")
+#Made by Ankit Raj Mahapatra ;-)
---
+++ packages/pywhatkit/pywhatkit-1.2.tar.gz/pywhatkit-1.2/setup.py
@@ -0,0 +1,40 @@
+from distutils.core import setup
+import setuptools
+
+def readme():
+    with open(r'C:\Users\pc\Desktop\pywhatkit\README.txt') as f:
+        README = f.read()
+    return README
+
+setup(
+    name = 'pywhatkit',
+    packages = setuptools.find_packages(),
+    version = '1.2',
+    license='MIT',
+    description = 'pywhatkit is a Python library for Sending whatsapp message at certain time, it has several other features too.',   # Give a short description about your library
+    author = 'Ankit Raj Mahapatra',                   # Type in your name
+    author_email = 'ankitrajjitendra816@gmail.com',      # Type in your E-Mail
+    url = 'https://github.com/Ankit404butfound/awesomepy',   # Provide either the link to your github or to your website
+    download_url = 'https://github.com/Ankit404butfound/awesomepy/archive/1.0.tar.gz',    # I explain this later on
+    keywords = ['sendwhatmsg', 'info', 'playonyt', 'search'],   # Keywords that define your package best
+    install_requires=[           
+          'pyautogui',
+          'beautifulsoup4',
+          'wikipedia',
+          'requests',
+          'pyttsx3',
+      ],
+    include_package_data=True,
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    ],
+)
