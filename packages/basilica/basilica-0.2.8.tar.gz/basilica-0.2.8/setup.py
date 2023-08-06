00365daef5700c4cb4e2c432d44440a3608f60d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:21 +0000

    Add package basilica/0.2.8/basilica-0.2.8.tar.gz

---
+++ packages/basilica/basilica-0.2.8.tar.gz/basilica-0.2.8/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='basilica',
+      version='0.2.8',
+      description='Client bindings for basilica.ai embeddings.',
+      long_description='Client bindings for basilica.ai embeddings.',
+      url='http://basilica.ai',
+      author='Michael Lucy',
+      author_email='mlucy@basilica.ai',
+      license='MIT',
+      packages=['basilica'],
+      install_requires=[
+          'requests',
+          'six',
+          'Pillow',
+      ],
+      zip_safe=True)
