a156e8412219fe8ccec50de026cec99eaca66cd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:47 +0000

    Add package django-slack/5.14.1/django-slack-5.14.1.tar.gz

---
+++ packages/django-slack/django-slack-5.14.1.tar.gz/django-slack-5.14.1/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='django-slack',
+    url="https://chris-lamb.co.uk/projects/django-slack",
+    version='5.14.1',
+    description="Provides easy-to-use integration between Django projects and "
+    "the Slack group chat and IM tool.",
+    author="Chris Lamb",
+    author_email="chris@chris-lamb.co.uk",
+    license="BSD-3-Clause",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=('Django>=1.8.0', 'requests', 'six',),
+)
