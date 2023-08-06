f281d9adedc98190c7379a5730f27011643476b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:56 +0000

    Add package mypy-boto3-mediastore/1.10.34.1/mypy-boto3-mediastore-1.10.34.1.tar.gz

---
+++ packages/mypy-boto3-mediastore/mypy-boto3-mediastore-1.10.34.1.tar.gz/mypy-boto3-mediastore-1.10.34.1/mypy_boto3_mediastore/paginator.py
@@ -0,0 +1,22 @@
+"Main interface for mediastore service Paginators"
+from __future__ import annotations
+
+from botocore.paginate import Paginator as Boto3Paginator
+from mypy_boto3_mediastore.type_defs import ListContainersOutputTypeDef, PaginatorConfigTypeDef
+
+
+__all__ = ("ListContainersPaginator",)
+
+
+class ListContainersPaginator(Boto3Paginator):
+    """
+    [Paginator.ListContainers documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.34/reference/services/mediastore.html#MediaStore.Paginator.ListContainers)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self, PaginationConfig: PaginatorConfigTypeDef = None
+    ) -> ListContainersOutputTypeDef:
+        """
+        [ListContainers.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.34/reference/services/mediastore.html#MediaStore.Paginator.ListContainers.paginate)
+        """
