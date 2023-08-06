285da28c3c1e5a28dfd6e6772f334f648c029d9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:17 +0000

    Add package autobench2/0.1.0/autobench2-0.1.0.tar.gz

---
+++ packages/autobench2/autobench2-0.1.0.tar.gz/autobench2-0.1.0/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='autobench2',
+    version='0.1.0',
+    description='HTTP benchmarking suite for wrk2',
+    long_description=readme,
+    author='Chiyu Zhong',
+    author_email='zhongchiyu@gmail.com',
+    url='https://github.com/cattail/autobench2',
+    license="MIT License",
+    packages=find_packages(exclude=('tests', 'docs')),
+    entry_points={
+        'console_scripts': ['autobench2=autobench2.cli:cli']
+    },
+)
