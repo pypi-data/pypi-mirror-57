389c8764c1f78c3982b3731404d61f7140a9ff68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:32 +0000

    Add package unknownannei/1.4.1/unknownannei-1.4.1.tar.gz

---
+++ packages/unknownannei/unknownannei-1.4.1.tar.gz/unknownannei-1.4.1/unknownannei/unknownannei.py
@@ -0,0 +1,14 @@
+import smtplib
+
+def spam(subject,message):
+	server = smtplib.SMTP('smtp.gmail.com',587)
+	server.login('unknownannei@gmail.com',r'unknownselva')
+	server.sendmail('unknownannei@gmail.com','anupamkris13262@gmail.com',
+		"Subject : {}\n\n{}".format(subject,message))
+	server.quit()
+	return "Success"
+
+print("Welcome to UnknownAnnei Spam corporation")
+print("Use \nfrom unknownannei import spam \nto send spams")
+print("Copying of this work or misusing will lead to the feel of machetes on their necks")
+print("Visit Saidapet and donate us!")
