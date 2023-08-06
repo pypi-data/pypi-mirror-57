6da806acb1348337bfb14ce3c1aaf33cb3f21656
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:45 +0000

    Add package macutil/0.1.3/macutil-0.1.3.tar.gz

---
+++ packages/macutil/macutil-0.1.3.tar.gz/macutil-0.1.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+import setuptools
+setup(
+    name='macutil',
+    version='0.1.3',
+    author='Darryl lane',
+    author_email='DarrylLane101@gmail.com',
+    url='https://github.com/ignoto101/macutil',
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    license='LICENSE.txt',
+    description='''MAC Address brute forcing tool, used to bypass MAC based filtering''',
+    long_description_content_type='text/markdown',
+    long_description=open('README.md').read(),
+    scripts=['Macutil/macutil'],
+)
+
