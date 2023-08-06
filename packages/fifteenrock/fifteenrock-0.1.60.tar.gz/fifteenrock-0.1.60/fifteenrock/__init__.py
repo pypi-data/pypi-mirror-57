34934594550815818f35486d63bd16a8fc4e1dff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:08 +0000

    Add package fifteenrock/0.1.60/fifteenrock-0.1.60.tar.gz

---
+++ packages/fifteenrock/fifteenrock-0.1.60.tar.gz/fifteenrock-0.1.60/fifteenrock/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for fifteenrock."""
+
+__author__ = """Rajiv Abraham"""
+__email__ = 'rajiv.abraham@15rock.com'
+__version__ = '0.1.60'
+
+from fifteenrock.core.core import database, load_project, get_master_data, deploy_powerBI
+from fifteenrock.core.compute_client import compute
+from fifteenrock.core.fr_notebook import deploy_form
+
