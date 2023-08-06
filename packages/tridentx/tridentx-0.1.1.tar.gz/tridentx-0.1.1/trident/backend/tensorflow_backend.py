552890f164daec5f0ce4f657710d6352ab1b8864
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:17 +0000

    Add package tridentx/0.1.1/tridentx-0.1.1.tar.gz

---
+++ packages/tridentx/tridentx-0.1.1.tar.gz/tridentx-0.1.1/trident/backend/tensorflow_backend.py
@@ -0,0 +1,69 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+import sys
+import numpy as np
+import tensorflow as tf
+from tensorflow.python.framework import ops
+from tensorflow.python.eager import context
+from ..layers.tensorflow_activations import *
+from ..layers.tensorflow_normalizations import *
+from ..layers.tensorflow_activations import *
+from ..layers.tensorflow_normalizations import *
+from ..data.tensorflow_datasets import *
+
+
+version=tf.version
+sys.stderr.write('Tensorflow version:{0}.\n'.format(version.VERSION))
+
+if version.VERSION<'2.0.0':
+    raise ValueError('Not support Tensorflow below 2.0' )
+
+
+def to_numpy(x) -> np.ndarray:
+
+    """
+    Convert whatever to numpy array
+    :param x: List, tuple, PyTorch tensor or numpy array
+    :return: Numpy array
+    """
+    if isinstance(x, np.ndarray):
+        return x
+    elif isinstance(x, tf.Variable):
+        sess = tf.compat.v1.Session()
+        x = sess.run(x.value())
+        return x
+    elif isinstance(x, ops.Tensor):
+        sess = tf.compat.v1.Session()
+        x= sess.run(x)
+        return x
+    elif hasattr(x, 'numpy'):
+        with context.eager_mode():
+            return x.numpy()
+    elif isinstance(x, (list, tuple, int, float)):
+        return np.array(x)
+    else:
+        raise ValueError("Unsupported type")
+
+
+def to_tensor(x, dtype=None) ->ops.Tensor:
+    return ops.convert_to_tensor(x, dtype=dtype)
+
+
+
+def get_flops(model):
+    run_meta = tf.compat.v1.RunMetadata()
+    opts = tf.compat.v1.profiler.ProfileOptionBuilder.float_operation()
+
+    # We use the Keras session graph in the call to the profiler.
+    flops = tf.compat.v1.profiler.profile(graph=tf.compat.v1.keras.backend.get_session().graph,
+                                run_meta=run_meta, cmd='op', options=opts)
+
+    return flops.total_float_ops  # Prints the "flops" of the model.
+
+
+
+
+
+
+
