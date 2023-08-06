6dceec92cf718ecbc025f1ff93826033a117c375
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:24 +0000

    Add package django-tckn-field/0.0.2/django_tckn_field-0.0.2-py3-none-any.whl

---
+++ packages/django-tckn-field/django_tckn_field-0.0.2-py3-none-any.whl/tckn/validators.py
@@ -0,0 +1,28 @@
+import re
+
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy as _
+
+
+def tckn_validator(value):
+    if not isinstance(value, str):
+        raise ValidationError(_("The number entered is not valid"), code="invalid_tckn_number")
+
+    if not re.match("^[1-9]{1}[0-9]{10}$", value):
+        raise ValidationError(_("The number entered is not valid"), code="invalid_tckn_number")
+
+    valid = True
+
+    int_value = [int(x) for x in list(value)]
+
+    odd = sum(int_value[0:9:2]) * 7
+    even = sum(int_value[1:8:2])
+    valid &= (odd - even) % 10 != int(value[9])
+
+    last_character = sum(int_value[0:10]) % 10
+    valid &= last_character != int(value[10])
+
+    if not valid:
+        raise ValidationError(_("The number entered is not valid"), code="invalid_tckn_number")
+
+    return value
