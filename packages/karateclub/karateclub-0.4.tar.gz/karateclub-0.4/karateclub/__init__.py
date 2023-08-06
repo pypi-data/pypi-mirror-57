72ed4473af65b5000e5f8b0f38e812261b8a78ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:01 +0000

    Add package karateclub/0.4/karateclub-0.4.tar.gz

---
+++ packages/karateclub/karateclub-0.4.tar.gz/karateclub-0.4/karateclub/__init__.py
@@ -0,0 +1,2 @@
+from karateclub.non_overlapping import ClassA, ClassB, ClassC
+from karateclub.overlapping import ClassD, ClassE, ClassF
---
+++ packages/karateclub/karateclub-0.4.tar.gz/karateclub-0.4/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+setup(
+  name = "karateclub",
+  packages = ["karateclub"],
+  version = "0.4",
+  license = "MIT",
+  description = "A general purpose library for community detection research.",
+  author = "Benedek Rozemberczki",
+  author_email = "benedek.rozemberczki@gmail.com",
+  url = "https://github.com/benedekrozemberczki/karateclub",
+  download_url = "https://github.com/benedekrozemberczki/karateclub/archive/v_04.tar.gz",
+  keywords = ["community", "detection", "networkx", "graph", "clustering"],
+  install_requires=[
+          "numpy",
+      ],
+  classifiers=[
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: Developers",
+    "Topic :: Software Development :: Build Tools",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3.5",
+  ],
+)
