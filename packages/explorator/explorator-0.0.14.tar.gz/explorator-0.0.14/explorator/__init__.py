c016ea19c96bc831afb17fec05628c227af1c097
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:26 +0000

    Add package explorator/0.0.14/explorator-0.0.14.tar.gz

---
+++ packages/explorator/explorator-0.0.14.tar.gz/explorator-0.0.14/explorator/__init__.py
@@ -0,0 +1,15 @@
+from . import general_info
+from . import total_review
+from . import functions
+
+def libinfo():
+    print('''
+    Library explorator.
+    Structure:
+    1. General info
+        get_total
+    2. Functions
+        - Convert series to str
+        - Convert series to date
+        - Convert series to numeric (specify int or float)
+    ''')
