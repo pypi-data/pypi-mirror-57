b59b32204da43ab1bb0c6bcd2b325208f3117fe2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:37 +0000

    Add package kumaoche/0.1.15/kumaoche-0.1.15-py3-none-any.whl

---
+++ packages/kumaoche/kumaoche-0.1.15-py3-none-any.whl/kumaoche/config/config_pack.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+from .service import GitConfig, PackageManagerConfig
+
+
+class ConfigPack(object):
+    def __init__(self, parsed_yaml):
+        if parsed_yaml is None:
+            parsed_yaml = {}
+
+        # 全設定共有のテンプレート用変数
+        self.environment = self.assign_dict(parsed_yaml, 'environment')
+        self.git = GitConfig(parsed_yaml)
+        self.services = []
+
+        for service in parsed_yaml.get('services', []):
+            if service is not None:
+                self.services.extend([PackageManagerConfig(service)])
+
+    @classmethod
+    def assign_dict(cls, parsed_yaml: {}, key: str):
+        configs = parsed_yaml.get(key, {})
+        presets_configs = parsed_yaml.get('presets', {}).get(key, {})
+
+        dictionary = {}
+        for key in configs.keys():
+            dictionary.setdefault(key, configs.get(key, ''))
+
+        for key in presets_configs.keys():
+            dictionary.setdefault(key, presets_configs.get(key, ''))
+
+        return dictionary
