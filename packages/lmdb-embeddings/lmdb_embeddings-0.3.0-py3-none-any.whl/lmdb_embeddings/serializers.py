4fc9b541597d3fae21ee3c6df5d55b3ec3607da7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:52 +0000

    Add package lmdb-embeddings/0.3.0/lmdb_embeddings-0.3.0-py3-none-any.whl

---
+++ packages/lmdb-embeddings/lmdb_embeddings-0.3.0-py3-none-any.whl/lmdb_embeddings/serializers.py
@@ -0,0 +1,67 @@
+"""
+LMDB Embeddings - Fast word vectors with little memory usage in Python.
+dom.hudson@thoughtriver.com
+
+Copyright (C) 2018 ThoughtRiver Limited
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+import pickle
+import pickletools
+
+import msgpack
+import msgpack_numpy
+
+
+class PickleSerializer:
+
+    @staticmethod
+    def serialize(vector):
+        """ Serializer a vector using pickle.
+
+        :param np.array vector:
+        :return bytes:
+        """
+        return pickletools.optimize(pickle.dumps(vector, pickle.HIGHEST_PROTOCOL))
+
+    @staticmethod
+    def unserialize(serialized_vector):
+        """ Unserialize a vector using pickle.
+
+        :param bytes serialized_vector:
+        :return np.array:
+        """
+        return pickle.loads(serialized_vector)
+
+
+class MsgpackSerializer:
+
+    @staticmethod
+    def serialize(vector):
+        """ Serializer a vector using msgpack.
+
+        :param np.array vector:
+        :return bytes:
+        """
+        return msgpack.packb(vector, default = msgpack_numpy.encode)
+
+    @staticmethod
+    def unserialize(serialized_vector):
+        """ Unserialize a vector using msgpack.
+
+        :param bytes serialized_vector:
+        :return np.array:
+        """
+        return msgpack.unpackb(serialized_vector, object_hook = msgpack_numpy.decode)
