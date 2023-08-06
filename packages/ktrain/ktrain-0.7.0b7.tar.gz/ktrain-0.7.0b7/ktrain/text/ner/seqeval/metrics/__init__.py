e8f6bd9698b18f90ebc4020120bc540f27d2dca1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:14 +0000

    Add package ktrain/0.7.0b7/ktrain-0.7.0b7.tar.gz

---
+++ packages/ktrain/ktrain-0.7.0b7.tar.gz/ktrain-0.7.0b7/ktrain/text/ner/seqeval/metrics/__init__.py
@@ -0,0 +1,6 @@
+from .sequence_labeling import accuracy_score
+from .sequence_labeling import classification_report
+from .sequence_labeling import f1_score
+from .sequence_labeling import precision_score
+from .sequence_labeling import recall_score
+from .sequence_labeling import performance_measure
