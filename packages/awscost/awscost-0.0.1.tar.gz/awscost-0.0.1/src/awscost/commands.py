bc7b012af819bd3316dadfd2e73ff17d43ed014b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:18 +0000

    Add package awscost/0.0.1/awscost-0.0.1.tar.gz

---
+++ packages/awscost/awscost-0.0.1.tar.gz/awscost-0.0.1/src/awscost/commands.py
@@ -0,0 +1,132 @@
+import click
+import json
+from datetime import datetime
+from .billing import Billing
+from .cost_explorer import CostExplorer
+from .util import Util
+from .validator import Validator
+from .date_util import DateUtil
+from . import constants
+
+
+class Mash(object):
+    pass
+
+
+@click.group(invoke_without_command=True)
+@click.option(
+    '--debug/--no-debug',
+    default=False,
+    help='enable debug logging. (default: False)'
+)
+@click.option(
+    '--profile',
+    type=str,
+    help='aws profile name.'
+)
+@click.pass_context
+def cli(ctx, debug, profile):
+    ctx.obj = Mash()
+    ctx.obj.debug = debug
+    ctx.obj.profile = profile
+
+
+@cli.command(help='list cost explorer')
+@click.option(
+    '--granularity',
+    '-g',
+    type=click.Choice(['DAILY', 'MONTHLY']),
+    default="MONTHLY",
+    help='granularity. (default: MONTHLY)'
+)
+@click.option(
+    '--point',
+    '-p',
+    type=int,
+    default=constants.DEFAULT_POINT,
+    help=f'duration. if granularity is MONTHLY, {constants.DEFAULT_POINT} month ago start. if granularity is DAILY, {constants.DEFAULT_POINT} day ago start. (default: {constants.DEFAULT_POINT})'
+)
+@click.option(
+    '--start',
+    callback=Validator.validate_dateformat,
+    type=str,
+    help=f'range of start day. default is {constants.DEFAULT_POINT} month ago.'
+)
+@click.option(
+    '--end',
+    callback=Validator.validate_dateformat,
+    type=str,
+    default=datetime.today().strftime("%Y-%m-%d"),
+    help='range of end day. default is now.'
+)
+@click.option(
+    '--tablefmt',
+    '-t',
+    type=str,
+    default='simple',
+    help='tabulate format. (default: simple)'
+)
+@click.option(
+    '--group-by',
+    type=click.Choice(constants.AVAILABLE_DIMENSIONS),
+    multiple=True,
+    default=['SERVICE'],
+    help='group by keys. (default: ["SERVICE"])'
+)
+@click.option(
+    '--filter',
+    type=json.loads,
+    help='filter of dimensions. default is no filter.'
+)
+@click.option(
+    '--metrics',
+    type=click.Choice(constants.AVAILABLE_METRICS),
+    default=constants.DEFAULT_METRICS,
+    help='metrics. (default: UnblendedCost)'
+)
+@click.pass_context
+def list_ce(ctx, granularity, point, start, end, tablefmt, group_by, filter, metrics):
+    cost_explorer = CostExplorer(
+        granularity,
+        start or DateUtil.get_start(granularity, point),
+        end,
+        group_by=group_by,
+        filter_dimensions=filter,
+        metrics=metrics,
+        debug=ctx.obj.debug,
+        profile=ctx.obj.profile
+    )
+    currencies = cost_explorer.get_cost_and_usage_total_and_group_by()
+    print(Util.convert_tabulate(currencies, tablefmt=tablefmt))
+
+
+@cli.command(help='list cloudwatch billing')
+@click.option(
+    '--range',
+    '-r',
+    type=click.Choice(['month', 'week', 'day']),
+    required=True
+)
+@click.option(
+    '--tablefmt',
+    '-t',
+    type=str,
+    default='simple',
+    help='tabulate format. (default: simple)'
+)
+@click.option(
+    '--point',
+    '-p',
+    type=int,
+    default=constants.DEFAULT_POINT,
+    help=f'number of data point. (default: {constants.DEFAULT_POINT})'
+)
+@click.pass_context
+def list_billing(ctx, range, tablefmt, point):
+    billing = Billing(ctx.obj.debug, ctx.obj.profile)
+    currencies = billing.get_currencies_per_service(range, point)
+    print(Util.convert_tabulate(currencies, tablefmt=tablefmt))
+
+
+def main():
+    cli(obj={})
---
+++ packages/awscost/awscost-0.0.1.tar.gz/awscost-0.0.1/src/awscost/cost_explorer.py
@@ -0,0 +1,106 @@
+from datetime import datetime
+from .logger import get_logger
+from .cost_explorer_client import CostExplorerClient
+from . import constants
+
+
+class CostExplorer:
+    def __init__(
+        self,
+        granularity,
+        start,
+        end,
+        group_by=[],
+        filter_dimensions=None,
+        metrics=constants.DEFAULT_METRICS,
+        profile=None,
+        debug=False
+    ):
+        self.cost_explorer_client = CostExplorerClient(
+            granularity,
+            start,
+            end,
+            filter_dimensions=filter_dimensions,
+            metrics=metrics,
+            profile=profile,
+            debug=debug
+        )
+        self.granularity = granularity
+        self.group_by = group_by
+        self.metrics = metrics
+        self.logger = get_logger(debug=debug)
+
+    def get_cost_and_usage_total_and_group_by(self):
+        """
+        startとendを指定してcostの時系列データを取得し、totalとgroup_byをmergeする
+        """
+        # totalを取得
+        total = self.get_cost_and_usage_total()
+
+        # group別を取得
+        results = self.get_cost_and_usage_group_by()
+
+        # totalとgroup byをmergeする
+        merged = dict(total, **results)
+        self.logger.debug(merged)
+        return merged
+
+    def get_cost_and_usage_total(self):
+        """
+        startとendを指定してcostの時系列データを取得する
+        """
+        # totalを取得
+        cost_and_usage = self.cost_explorer_client.get_cost_and_usage()
+        total = self._convert_results_total(cost_and_usage)
+        self.logger.debug(total)
+        return total
+
+    def get_cost_and_usage_group_by(self):
+        """
+        startとendを指定してcostの時系列データを取得する
+        """
+        cost_and_usage_per_service = self.cost_explorer_client.get_cost_and_usage(group_by=self.group_by)
+        results = self._convert_results_group_by(cost_and_usage_per_service)
+        self.logger.debug(results)
+        return results
+
+    def _convert_results_group_by(self, cost_and_usage_per_service):
+        """
+        group-byが指定されているデータ構造のparse
+        """
+        results = {}
+        for result in cost_and_usage_per_service:
+            start_period = result.get('TimePeriod').get('Start')
+            time_key = self._convert_period(start_period)
+            groups = result.get('Groups')
+            for group in groups:
+                group_by_key = ','.join(group.get('Keys'))
+                if results.get(group_by_key) is None:
+                    results[group_by_key] = {}
+                else:
+                    results[group_by_key] = results.get(group_by_key)
+                metrics = group.get('Metrics')
+                amount = metrics.get(self.metrics).get('Amount')
+                results[group_by_key][time_key] = round(float(amount), 2)
+        return results
+
+    def _convert_results_total(self, cost_and_usage_per_service):
+        """
+        Totalのデータ構造のparse
+        """
+        results = {"Total": {}}
+        for result in cost_and_usage_per_service:
+            start_period = result.get('TimePeriod').get('Start')
+            time_key = self._convert_period(start_period)
+            metrics = result.get('Total')
+            amount = metrics.get(self.metrics).get('Amount')
+            results["Total"][time_key] = round(float(amount), 2)
+        return results
+
+    def _convert_period(self, start_period):
+        """
+        headerに表示させる日付をmonthlyとdailyで切り替え
+        """
+        if self.granularity == "MONTHLY":
+            return datetime.strptime(start_period, '%Y-%m-%d').strftime('%Y-%m')
+        return datetime.strptime(start_period, '%Y-%m-%d').strftime('%m-%d')
