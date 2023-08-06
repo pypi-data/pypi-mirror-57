17ae130728d16468b735d76669a72f2ca63dc268
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:21 +0000

    Add package pactor/0.1.0/pactor-0.1.0.tar.gz

---
+++ packages/pactor/pactor-0.1.0.tar.gz/pactor-0.1.0/setup.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import print_function
+
+import io
+import re
+from glob import glob
+from os.path import basename
+from os.path import dirname
+from os.path import join
+from os.path import splitext
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+def read(*names, **kwargs):
+    with io.open(
+        join(dirname(__file__), *names),
+        encoding=kwargs.get('encoding', 'utf8')
+    ) as fh:
+        return fh.read()
+
+
+setup(
+    name='pactor',
+    use_scm_version={
+        'local_scheme': 'dirty-tag',
+        'write_to': 'src/pactor/_version.py',
+        'fallback_version': '0.0.0',
+    },
+    license='MIT',
+    description='A minimalist actor framework in Python.',
+    long_description='%s\n%s' % (
+        re.compile('^.. start-badges.*^.. end-badges', re.M | re.S).sub('', read('README.rst')),
+        re.sub(':[a-z]+:`~?(.*?)`', r'``\1``', read('CHANGELOG.rst'))
+    ),
+    author='Todd Carlson',
+    author_email='none@notset.com',
+    url='https://github.com/rtoddcarlson/pactor',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Unix',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Utilities',
+    ],
+    project_urls={
+        'Documentation': 'https://github.com/rtoddcarlson/pactor',
+        'Issue Tracker': 'https://github.com/rtoddcarlson/pactor/issues',
+    },
+    keywords=[
+        'actor',
+    ],
+    python_requires='>=3.6.*',
+    install_requires=[
+    ],
+    extras_require={
+    },
+    setup_requires=[
+        'setuptools_scm>=3.3.1',
+    ],
+    entry_points={
+        'console_scripts': [
+            'pactor = pactor.cli:main',
+        ]
+    },
+)
---
+++ packages/pactor/pactor-0.1.0.tar.gz/pactor-0.1.0/src/pactor/__init__.py
@@ -0,0 +1,87 @@
+import multiprocessing
+
+from pkg_resources import get_distribution, DistributionNotFound
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    pass
+
+
+class Actor:
+    __close_message__ = '_*CLOSE*_'
+
+    """
+        Wraps a pickleable class as an actor to run in a dedicated process.
+        The returned Actor instance has a .proxy attribute that should be used
+        to invoke the methods of the wrapped class.
+    """
+    def __init__(self, actor_instance):
+        # Initialize a queue that can be shared across processes
+        manager = multiprocessing.Manager()
+        self.queue = manager.Queue()
+
+        # Initialize the underlying instance and process
+        setattr(actor_instance, 'enqueue', EnqueueDirectCall(self.queue))
+        self.actor_process = multiprocessing.Process(target=self.__run_actor__, args=(self.queue, actor_instance))
+        self.actor_process.start()
+
+        # Build the proxy
+        self.proxy = Proxy()
+        for attr in dir(actor_instance.__class__):
+            if callable(getattr(actor_instance, attr)) and not attr.startswith('__'):
+                setattr(self.proxy, attr, EnqueueNamedCall(self.queue, attr))
+
+    def close(self):
+        """ Closes the actor process """
+        self.queue.put_nowait((self.__close_message__,))
+
+    def join(self):
+        """ Joins the calling thread to the process for the actor. """
+        self.actor_process.join()
+
+    def __run_actor__(self, queue, actor_instance):
+        """ Runs the actor process by reading from the queue and executing serially. """
+        while True:
+            task = queue.get()  # Will block until a task is available
+            name = task[0]
+            if name == self.__close_message__:
+                return
+
+            target = getattr(actor_instance, name)
+
+            if len(task) > 1:
+                args = task[1]
+                target(*args)
+            else:
+                target()
+
+
+class Proxy:
+    """ An empty class definition used to create a proxy for an Actor """
+    pass
+
+
+class EnqueueNamedCall:
+    """
+        A callable that writes to a queue.  The tuple value written to the queue includes the name specified
+        when creating the QueueRedirect as well as the arguments passed to the call.
+    """
+    def __init__(self, queue, name):
+        self.queue = queue
+        self.name = name
+
+    def __call__(self, *args):
+        self.queue.put_nowait((self.name, args))
+
+
+class EnqueueDirectCall:
+    """
+        A callable that writes to a queue from a callable target.  The tuple value written to the queue includes the
+        name of the callable_target as well as the arguments passed to the call.
+    """
+    def __init__(self, queue):
+        self.queue = queue
+
+    def __call__(self, callable_target, *args):
+        self.queue.put_nowait((callable_target.__name__, args))
