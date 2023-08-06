546b08ab710235d8d277a52fa42e03c6488d0ede
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:53 +0000

    Add package keychestamp/0.1.8/keychestamp-0.1.8.tar.gz

---
+++ packages/keychestamp/keychestamp-0.1.8.tar.gz/keychestamp-0.1.8/keychestamp/ampconfig.py
@@ -0,0 +1,207 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+***
+Module:
+***
+"""
+#  Copyright (c) KeyChest Ltd, 2019. File provided under the license as described in the "LICENSE" file
+#   included with this project. A copy of the license is also available from https://gitlab.com/keychest,
+#   when you select the relevant project or you can request it at support@keychest.net.
+#
+#   This file is owned exclusively by KeyChest Ltd. Unauthorized copying of this file, via any medium is
+#    strictly prohibited
+#
+import datetime
+import errno
+import getopt
+import json
+import logging
+import multiprocessing
+import os
+import socket
+import sys
+import tempfile
+
+from keychestamp.queue_handler import QueueHandler
+from keychestamp.version import proxy_version
+
+__copyright__ = 'KeyChest Ltd'
+__email__ = 'support@keychest.net'
+__status__ = 'Development'
+
+
+class AmpConfig(object):
+    """
+    A static class with the configuration of the process.
+    """
+
+    port = 8443
+    address = "0.0.0.0"
+    api_logging = True
+    file_logging = None
+    log_level = logging.INFO
+    api_timeout = 5
+    ch = None
+    temp_ca = None
+    config_dir = '/etc/keychestamp'
+    main_stop = multiprocessing.Event()
+
+    @staticmethod
+    def print_help():
+        """
+        A simple help printing
+        """
+        print("keychestamp, version %s. \n(c) KeyChest Ltd. Visit https://gitlab.com/keychest .\n\n"
+              "The proxy accepts the following parameters:" % proxy_version)
+        print("  -h, --help - this help, if present it will stop the process\n")
+        print("  -l, --level <DEBUG|ERROR|WARNING|INFO|CRITICAL> - minimum level to log, default is INFO\n")
+        print("  -n, --noapi - you can disable API logging\n")
+        print("  -f --file <file> - path to a file where to store the logs, be default this is disabled\n")
+        print("  -p, --port <int> - setting the listening port of the proxy\n")
+        print("  -a, --addr <IP addr> - by default it responds to all requests\n")
+        print("  -c, --ca - just returns its root certificate in PEM format and exits")
+
+    @staticmethod
+    def parse_params():
+
+        logging.getLogger().setLevel(logging.INFO)  # default level
+
+        try:
+            opts, args = getopt.getopt(sys.argv[1:], 'hl:nf:cp:a:',
+                                       ['help', 'level=', 'noapi', 'file=', 'ca', 'port=', 'addr='])
+        except getopt.GetoptError:
+            # print help information and exit:
+            AmpConfig.print_help()
+            print("You provided %s" % ' '.join(sys.argv[1:]))
+            if AmpConfig.ch:
+                AmpConfig.ch.close()
+            return -1
+
+        for o, a in opts:
+            if o in ('-h', '--help'):
+                AmpConfig.print_help()
+                if AmpConfig.ch:
+                    AmpConfig.ch.close()
+                return 1
+            else:
+                if o in ('-n', '--noapi'):
+                    AmpConfig.api_logging = None
+                if o in ('-l', '--level'):
+                    if a.lower() in ['debug', 'info', 'warning', 'error', 'critical']:
+                        if a.lower() == 'debug':
+                            AmpConfig.log_level = logging.DEBUG
+                        elif a.lower() == 'info':
+                            AmpConfig.log_level = logging.INFO
+                        elif a.lower() == 'warning':
+                            AmpConfig.log_level = logging.WARNING
+                        elif a.lower() == 'error':
+                            AmpConfig.log_level = logging.ERROR
+                        elif a.lower() == 'critical':
+                            AmpConfig.log_level = logging.CRITICAL
+                        logging.getLogger().setLevel(AmpConfig.log_level)
+
+                if o in ('-f', '--file'):
+                    try:
+                        with open(a, 'a') as f:
+                            f.write('')
+                        AmpConfig.file_logging = a
+                    except IOError as x:
+                        if x.errno == errno.EACCES:
+                            sys.stderr.write("Can't access the file: %s" % a)
+                        elif x.errno == errno.EISDIR:
+                            sys.stderr.write("This is a folder, a file expected: %s" % a)
+                        return -1
+
+                if o in ('-p', '--port'):
+                    try:
+                        new_port = int(a)
+                        if (new_port < 1) or (new_port > 65535):
+                            sys.stderr.write("The port is not a valid value between 1-65535\n\n")
+                            return -1
+                        AmpConfig.port = new_port
+                    except ValueError:
+                        sys.stderr.write("The port you entered is not an integer\n\n")
+                        return -1
+                if o in ('-a', '--addr'):
+                    AmpConfig.address = a.strip()
+                if o in ('-c', '--ca'):
+                    cert = AmpConfig.getRootCert()
+                    if AmpConfig.temp_ca is None:
+                        _, temp_path = tempfile.mkstemp(suffix='.pem', prefix='keychestamp')
+                        AmpConfig.temp_ca = temp_path
+
+                    file = open(AmpConfig.temp_ca, 'wb')
+                    file.write(cert)
+                    file.close()
+
+                    sys.stdout.write(AmpConfig.temp_ca)
+                    return 1
+        return 0
+        pass
+
+    @staticmethod
+    def set_logging():
+        def usage_log(message, *args, **kws):
+            # noinspection PyUnresolvedReferences
+            if logging.getLogger().isEnabledFor(logging.OPS_LEVEL):
+                # let's do JSON encoding here
+                # noinspection PyUnresolvedReferences
+                _temp = {
+                    'msg': message,
+                    'params': {},
+                    'id': logging.use_counter,
+                    'time': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                    'host': logging.host,
+                    'ip': logging.ipaddress,
+                    'version': proxy_version
+                }
+                # noinspection PyUnresolvedReferences
+                logging.use_counter += 1
+                if kws and (len(kws) > 0):
+                    for key, item in kws.items():
+                        if key.lower() == 'code':
+                            _temp['code'] = item
+                        else:
+                            _temp['params'][key] = item
+
+                message = json.dumps(_temp)
+                dummy = {}
+                # noinspection PyUnresolvedReferences
+                logging.log(logging.OPS_LEVEL, message)
+
+        logging.OPS_LEVEL = 60
+        # noinspection PyUnresolvedReferences
+        logging.addLevelName(logging.OPS_LEVEL, "USE")
+        logging.Logger.usage = usage_log
+        logging.usage = usage_log
+
+        # items to be added to all usage logs
+        logging.use_counter = 1
+        logging.host = socket.gethostname()
+        logging.ipaddress = socket.gethostbyname(socket.gethostname())
+
+        logger = logging.getLogger()
+        logging.basicConfig(datefmt='%Y-%m-%d %H:%M:%S')
+        logger.setLevel(logging.INFO)
+        AmpConfig.ch = QueueHandler(api_logging=AmpConfig.api_logging, file_logging=AmpConfig.file_logging)
+        fmt = logging.Formatter('%(asctime)s:%(name)s:%(levelname)s: %(message)s')
+        AmpConfig.ch.setFormatter(fmt)
+        AmpConfig.ch.setLevel(logging.INFO)
+        for x in logger.handlers:
+            logger.removeHandler(x)
+        logger.addHandler(AmpConfig.ch)
+        pass
+
+    @classmethod
+    def getRootCert(cls):
+
+        ca_cert = AmpConfig.join_with_script_dir('ca.crt')
+        with open(ca_cert, 'rb') as f:
+            data = f.read()
+        return data
+
+    @staticmethod
+    def join_with_script_dir(path):
+        return os.path.join(os.path.dirname(os.path.abspath(__file__)), path)
---
+++ packages/keychestamp/keychestamp-0.1.8.tar.gz/keychestamp-0.1.8/keychestamp/main.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+***
+Module:
+***
+
+ Copyright (C) KeyChest Ltd, registered in the United Kingdom.
+ This file is owned exclusively by KeyChest Ltd.
+ Unauthorized copying of this file, via any medium is strictly prohibited
+ Proprietary and confidential
+ Written by Dan Cvrcek <support@keychest.net>, 2019
+"""
+import logging
+import signal
+import sys
+import time
+
+from keychestamp import ampconfig
+from keychestamp.ampconfig import AmpConfig
+from keychestamp.proxy import ProxyRequestHandler, ThreadingHTTPServer
+
+__copyright__ = 'KeyChest Ltd'
+__email__ = 'support@keychest.net'
+__status__ = 'Development'
+
+
+# noinspection PyUnusedLocal
+def signal_handler(sig, frame):
+    """
+    Signal handler
+    :param sig:
+    :param frame:
+    :return:
+    """
+
+    AmpConfig.main_stop.set()
+    sys.stderr.write("Waiting 1 second to exit\n")
+    logger = logging.getLogger()
+    for handler in logger.handlers[:]:
+        logger.removeHandler(handler)
+
+    time.sleep(1)
+
+    raise SystemExit('Terminating FoxyProxy')  # this is supposed to kill main()
+
+
+def process_watch():
+    """
+    A simple process watch function that will try to close worker processes that died
+    :return:
+    """
+
+    re_raise = False
+    while True:
+        time.sleep(10)
+        logging.debug("watcher run started at %d" % int(time.time()))
+        # noinspection PyBroadException
+        try:
+            pass
+        except Exception:
+            pass
+        finally:
+            logging.debug("watch run finished at %d" % int(time.time()))
+            if re_raise:
+                raise Exception("terminating process_watch")
+
+
+def process_proxy():
+
+    server_address = (AmpConfig.address, AmpConfig.port)
+
+    ProxyRequestHandler.protocol_version  = "HTTP/1.1"
+    httpd = ThreadingHTTPServer(server_address, ProxyRequestHandler)
+
+    sa = httpd.socket.getsockname()
+    logging.info("Serving HTTP Proxy on", iface=sa[0], port=sa[1])
+    httpd.serve_forever()
+    pass
+
+
+def main():
+    """
+    Main entry function
+    """
+
+    # noinspection PyBroadException
+    try:
+        signal.signal(signal.SIGINT, signal_handler)
+    except Exception:
+        pass
+    # noinspection PyBroadException
+    try:
+        signal.signal(signal.SIGTERM, signal_handler)
+    except Exception:
+        pass
+
+    to_exit = ampconfig.AmpConfig.parse_params()
+    if to_exit < 0:
+        sys.stdout.write("Error in parameters\n\n")
+        sys.exit(-1)
+    elif to_exit > 0:
+        sys.exit(0)
+
+    AmpConfig.set_logging()
+    logging.usage("KeyChestAmp started")
+    logging.usage("KeyChestAmp started", x=3, y="s")
+    logging.info("KeyChestAmp 2")
+    logging.critical("KeyChestAmp 3")
+
+    time.sleep(10000)
+    # alternative logbook logging
+    # log_dispatcher_thread = LogDispatcher(logger_stop_event, ip="127.0.0.1")
+    # log_dispatcher_thread.name = "LogDispatcher"
+    # log_dispatcher_thread.start()
+
+    # setup_logging(log_level, true_socket=True)  # alternative logbook logging
+    # logging.getLogger().setLevel(log_level)
+
+
+if __name__ == '__main__':
+    try:
+        sys.stderr.write("xxxx\n\n")
+        sys.stderr.flush()
+        main()
+        time.sleep(100000)
+    except Exception as ex:
+        sys.stderr.write(str(ex))
+        sys.stderr.flush()
+    finally:
+        sys.stderr.write("KeyChestAmp terminated\n")
+        sys.stderr.flush()
+        pass
