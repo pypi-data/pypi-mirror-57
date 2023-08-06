0cb5e9f2675eddb6305a00967af856f790fcdb5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:12 +0000

    Add package flask-check-json/0.1.4/flask-check-json-0.1.4.tar.gz

---
+++ packages/flask-check-json/flask-check-json-0.1.4.tar.gz/flask-check-json-0.1.4/flask_check_json/validator/__init__.py
@@ -0,0 +1,29 @@
+# Flask imports
+from flask import request, abort
+
+# JSON SChema Imports
+from jsonschema import validate
+from jsonschema.exceptions import ValidationError
+
+
+def validate_json(f):
+    # Validate the json
+    try:
+        request.get_json()
+    except Exception:
+        msg = "payload must be a valid json"
+        return abort(400, {"error": msg})
+
+
+def validate_schema(schema=None, force=False, data=None):
+    if not data:
+        # Validate the schema of the json
+        data = request.get_json(force=force)
+
+    if data is None:
+        return abort(400, 'Failed to decode JSON object')
+
+    try:
+        validate(data, schema)
+    except ValidationError as e:
+        return abort(400, e)
