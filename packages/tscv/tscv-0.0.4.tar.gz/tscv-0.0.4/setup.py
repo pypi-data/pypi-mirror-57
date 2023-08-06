a6f660dca1fea6a047e490b8755fd87f049e6096
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:55 +0000

    Add package tscv/0.0.4/tscv-0.0.4.tar.gz

---
+++ packages/tscv/tscv-0.0.4.tar.gz/tscv-0.0.4/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tscv",
+    version="0.0.4",
+    author="Wenjie Zheng",
+    author_email="work@zhengwenjie.net",
+    description="Time series cross-validation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/WenjieZ/TSCV",
+    license='new BSD',
+    packages=setuptools.find_packages(),
+    classifiers=['Intended Audience :: Science/Research',
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved',
+                 'Programming Language :: Python',
+                 'Topic :: Software Development',
+                 'Topic :: Scientific/Engineering',
+                 'Operating System :: Microsoft :: Windows',
+                 'Operating System :: POSIX',
+                 'Operating System :: Unix',
+                 'Operating System :: MacOS',
+                  ],
+)
