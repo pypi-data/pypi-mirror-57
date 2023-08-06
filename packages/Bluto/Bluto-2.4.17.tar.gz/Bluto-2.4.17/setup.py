2972e6a0ee312975c15c1727161d5985e5541bae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:36 +0000

    Add package Bluto/2.4.17/Bluto-2.4.17.tar.gz

---
+++ packages/Bluto/Bluto-2.4.17.tar.gz/Bluto-2.4.17/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Bluto',
+    version='2.4.17',
+    author='Darryl lane',
+    author_email='DarrylLane101@gmail.com',
+    url='https://github.com/darryllane/Bluto',
+    packages=['Bluto'],
+    include_package_data=True,
+    license='LICENSE.txt',
+    description='''
+    DNS Recon | Brute Forcer | DNS Zone Transfer | DNS Wild Card Checks
+    DNS Wild Card Brute Forcer | Email Enumeration | Staff Enumeration
+    Compromised Account Checking''',
+    long_description_content_type='text/markdown',
+    long_description=open('README.md').read(),
+    scripts=['Bluto/bluto'],
+    install_requires=[
+    	"docopt",
+        "dnspython",
+        "termcolor",
+        "BeautifulSoup4",
+        "requests[security]",
+        "pythonwhois",
+        "lxml",
+        "oletools",
+        "pdfminer==20140328"
+    ],
+)
+
