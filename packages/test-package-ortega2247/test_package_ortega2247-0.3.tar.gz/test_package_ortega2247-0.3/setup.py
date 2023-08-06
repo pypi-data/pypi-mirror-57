59d9038de8d25c658c4ea953566f1a667495b2cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:13 +0000

    Add package test-package-ortega2247/0.3/test_package_ortega2247-0.3.tar.gz

---
+++ packages/test-package-ortega2247/test_package_ortega2247-0.3.tar.gz/test_package_ortega2247-0.3/setup.py
@@ -0,0 +1,20 @@
+import sys
+from setuptools import setup
+
+setup(
+    name = "test_package_ortega2247",        # what you want to call the archive/egg
+    version = "0.3",
+    packages=["helloworld"],    # top-level python modules you can import like
+                                #   'import foo'
+    dependency_links = [],      # custom links to a specific project
+    install_requires=[],
+    extras_require={},      # optional features that other packages can require
+                            #   like 'helloworld[foo]'
+    package_data = {},
+    author="David Barnett",
+    author_email = "davidbarnett2@gmail.com",
+    description = "The familiar example program in Python",
+    license = "BSD",
+    keywords= "example documentation tutorial",
+    url = "http://github.com/dbarnett/python-helloworld",
+)
