eb530e20425cec19da3746009ae76431b4fbc733
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:32 +0000

    Add package zumidashboard/1.32/zumidashboard-1.32-py3-none-any.whl

---
+++ packages/zumidashboard/zumidashboard-1.32-py3-none-any.whl/zumidashboard/dashboard/precache-manifest.f823ecabbcea697c9468796e6195e924.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a0815f009a5c48a1ac1158caab0dbad",
+    "url": "/static/media/shutdown_red.9a0815f0.svg"
+  },
+  {
+    "revision": "29e26114bbf187d14e01",
+    "url": "/static/css/main.29455e21.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "de84d69570af7f004e06",
+    "url": "/static/js/2.bd5cc42f.chunk.js"
+  },
+  {
+    "revision": "801dbb9a832a1016a2d090132832ef92",
+    "url": "/static/media/explore_bg.801dbb9a.png"
+  },
+  {
+    "revision": "da48b7c9501eefc16a33c82a1bdfad00",
+    "url": "/static/media/learn_bg.da48b7c9.png"
+  },
+  {
+    "revision": "e05c10dfe41c6e170783c26d99b3b0c7",
+    "url": "/static/media/zumiface_thinking.e05c10df.png"
+  },
+  {
+    "revision": "b90e7ca6fa6127fdef3ca2b9c8c25224",
+    "url": "/static/media/battery.b90e7ca6.svg"
+  },
+  {
+    "revision": "cd075e84c297842589f6294ff2853215",
+    "url": "/static/media/arrow.cd075e84.svg"
+  },
+  {
+    "revision": "de69c17f481141c7d327dd6d5c0877bc",
+    "url": "/static/media/zumiface_blanklarge.de69c17f.png"
+  },
+  {
+    "revision": "0896842f941af3fb5f12a50246e95aeb",
+    "url": "/static/media/onnetwork_notonline.0896842f.svg"
+  },
+  {
+    "revision": "bac84c3a5c793993dc48f7b478573a15",
+    "url": "/static/media/onlineicon.bac84c3a.svg"
+  },
+  {
+    "revision": "ab5a3f1ab6af3900c70c9b73c0505175",
+    "url": "/static/media/battery_std_24px.ab5a3f1a.svg"
+  },
+  {
+    "revision": "ba818cd1b5c37614bfe2ef3ab78c52f2",
+    "url": "/static/media/charging_electricity.ba818cd1.svg"
+  },
+  {
+    "revision": "760254d92365dd21dd1a12040fe7c183",
+    "url": "/static/media/no_pw.760254d9.svg"
+  },
+  {
+    "revision": "cafb838b7133c2be147a951145422c39",
+    "url": "/static/media/settings_icon.cafb838b.svg"
+  },
+  {
+    "revision": "29e26114bbf187d14e01",
+    "url": "/static/js/main.44d837a1.chunk.js"
+  },
+  {
+    "revision": "58e807634c7d331506fc8d842798ee2d",
+    "url": "/static/media/circle.58e80763.svg"
+  },
+  {
+    "revision": "9fc3da3ddc5d3c6cc51ffafeca43e583",
+    "url": "/static/media/info.9fc3da3d.svg"
+  },
+  {
+    "revision": "4eee4a18272b64b3d1a6ddbf67fabcfc",
+    "url": "/static/media/notification.4eee4a18.svg"
+  },
+  {
+    "revision": "e4831958dc68381596bf8e94eeae4af4",
+    "url": "/static/media/sleep_purple.e4831958.svg"
+  },
+  {
+    "revision": "65b5d8fd6fa98e2d3cd61619de53b0f6",
+    "url": "/static/media/zumiface_neutral.65b5d8fd.png"
+  },
+  {
+    "revision": "f65cac4689448505492afe9a89f1a188",
+    "url": "/static/media/zumiface_happy.f65cac46.png"
+  },
+  {
+    "revision": "fcd42c0f7e6e3070910cc51c01a8997c",
+    "url": "/static/media/show_pw.fcd42c0f.svg"
+  },
+  {
+    "revision": "6e852638634e8123d0e22950821b2e35",
+    "url": "/static/media/spinner.6e852638.svg"
+  },
+  {
+    "revision": "dc9a4216319b6ca3c6e6fbee1ea76cb7",
+    "url": "/static/media/turningpad.dc9a4216.svg"
+  },
+  {
+    "revision": "33b26440a1ff8ae33cc67266e4670c05",
+    "url": "/static/media/turnleft.33b26440.svg"
+  },
+  {
+    "revision": "4acafb9a624ca4ce03f2cb8515f01707",
+    "url": "/static/media/turnright.4acafb9a.svg"
+  },
+  {
+    "revision": "3b3fbebf67cf16dc3d394644b3a0fec2",
+    "url": "/static/media/4_directionpad.3b3fbebf.svg"
+  },
+  {
+    "revision": "03af70305d744133fc28f0f12522215a",
+    "url": "/static/media/stop.03af7030.svg"
+  },
+  {
+    "revision": "00948d9546fdd9095d392512b16789cc",
+    "url": "/static/media/camera.00948d95.svg"
+  },
+  {
+    "revision": "005c5e22a800248646e5b8c05e7efae8",
+    "url": "/static/media/reboot_orange.005c5e22.svg"
+  },
+  {
+    "revision": "73abac37993b419dffaea6e77a0f4a43",
+    "url": "/index.html"
+  }
+];
