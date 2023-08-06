32f161f9c0d6e18cc224377f65cc6066d24cb543
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:55 +0000

    Add package matrixabm/0.1.3/matrixabm-0.1.3.tar.gz

---
+++ packages/matrixabm/matrixabm-0.1.3.tar.gz/matrixabm-0.1.3/setup.py
@@ -0,0 +1,44 @@
+"""Setup."""
+
+from setuptools import setup
+
+package_name = "matrixabm"
+description = "The Matrix ABM: An Agent Based Modeling Framework"
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name=package_name,
+    description=description,
+
+    author="Parantapa Bhattacharya",
+    author_email="pb+pypi@parantapa.net",
+
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    packages=[package_name],
+
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+
+    install_requires=[
+        "click",
+        "click_completion",
+        "logbook",
+        "more-itertools",
+        "python-dateutil",
+        "sortedcontainers",
+        "xactor",
+        "numpy",
+        "tensorboard",
+        "tensorboardx",
+    ],
+
+    url="http://github.com/nssac/matrixabm",
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ),
+)
