3809cc9108bda105e6c7d7b4d648bd3075a455ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:05 +0000

    Add package apu-cas/0.2.2/apu_cas-0.2.2.tar.gz

---
+++ packages/apu-cas/apu_cas-0.2.2.tar.gz/apu_cas-0.2.2/apu_cas/__init__.py
@@ -0,0 +1,10 @@
+from .service_ticket import (
+    require_service_ticket,
+)
+from .user_cas_attributes import (
+    UserCasAttributes,
+    get_user_cas_attributes,
+)
+from .user_role import (
+    UserRole,
+)
