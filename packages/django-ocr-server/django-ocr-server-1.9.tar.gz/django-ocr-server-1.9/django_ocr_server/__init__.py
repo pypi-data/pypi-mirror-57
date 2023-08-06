1c5c75653eab0228c8aaacb785ac9962af8ff282
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:11 +0000

    Add package django-ocr-server/1.9/django-ocr-server-1.9.tar.gz

---
+++ packages/django-ocr-server/django-ocr-server-1.9.tar.gz/django-ocr-server-1.9/django_ocr_server/__init__.py
@@ -0,0 +1,12 @@
+"""
+django_ocr_server/__init__.py OCR Server
+"""
+__author__ = 'shmakovpn <shmakovpn@yandex.ru>'
+__date__ = '2019-10-22,2019-04-15,2019-10-11,2019-12-03,2019-12-05'
+__version__ = '1.9'
+
+
+default_app_config = __package__ + '.apps.DjangoOcrServerConfig'
+
+
+
