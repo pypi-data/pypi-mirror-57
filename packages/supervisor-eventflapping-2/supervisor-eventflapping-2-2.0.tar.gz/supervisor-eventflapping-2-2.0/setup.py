7a6a8b98fcfec991b3fea9cfd9c23f3ae9de58d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:50 +0000

    Add package supervisor-eventflapping-2/2.0/supervisor-eventflapping-2-2.0.tar.gz

---
+++ packages/supervisor-eventflapping-2/supervisor-eventflapping-2-2.0.tar.gz/supervisor-eventflapping-2-2.0/setup.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+py_version = sys.version_info[:2]
+
+if py_version < (2, 6):
+    raise RuntimeError(
+        'On Python 2, supervisor-serialrestart requires Python 2.6 or later')
+elif (3, 0) < py_version < (3, 2):
+    raise RuntimeError(
+        'On Python 3, supervisor-serialrestart requires Python 3.2 or later')
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+setup(
+    name='supervisor-eventflapping-2',
+    version='2.0',
+    description='Adds eventlistener to detect rapidly exited processes',
+    author='Oleg Babushkin',
+    author_email='o.babushkin@city-mobil.ru',
+    url='https://github.com/mightydok/supervisor-eventflapping',
+    include_package_data=True,
+    install_requires=[
+        'supervisor==3.3.5',
+        'requests==2.21.0',
+    ],
+    packages=[
+        'supervisor_eventflapping',
+    ],
+    package_dir={'supervisor_eventflapping': 'supervisor_eventflapping'},
+    license="BSD",
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+    ],
+    test_suite='tests.run_tests.run_all',
+)
