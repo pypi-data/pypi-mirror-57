730207acfe9f810de7618d3d46a9521d6fd1f698
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:15 +0000

    Add package pypfm/1.0/pypfm-1.0.tar.gz

---
+++ packages/pypfm/pypfm-1.0.tar.gz/pypfm-1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+from setuptools import find_packages, setup
+
+setup(name='pypfm',
+      version='1.0',
+      description='Python pfm files reader',
+      author='Wats0ns',
+      author_email='Wats0ns@github.com',
+      url='https://github.com/Wats0ns/pypfm',
+      packages=['pypfm'],
+      install_requires=['pyzfp', 'numpy']
+     )
