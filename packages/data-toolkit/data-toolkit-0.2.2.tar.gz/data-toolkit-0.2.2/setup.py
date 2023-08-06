1fa1c44143159e9338fec274bbca0a3c4ae58e4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:53 +0000

    Add package data-toolkit/0.2.2/data-toolkit-0.2.2.tar.gz

---
+++ packages/data-toolkit/data-toolkit-0.2.2.tar.gz/data-toolkit-0.2.2/setup.py
@@ -0,0 +1,34 @@
+
+from setuptools import setup, find_packages
+from dt.core.version import get_version
+
+VERSION = get_version()
+
+f = open('README.md', 'r')
+LONG_DESCRIPTION = f.read()
+f.close()
+
+setup(
+    name='data-toolkit',
+    version=VERSION,
+    description='ML & data helper code!',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    author='Jakub Langr',
+    author_email='james.langr@gmail.com',
+    url='https://github.com/johndoe/myapp/',
+    license='unlicensed',
+    packages=find_packages(exclude=['ez_setup', 'tests*']),
+    package_data={'dt': ['templates/*', 'dt/scripts/*']},
+    include_package_data=True,
+    entry_points="""
+        [console_scripts]
+        dt = dt.main:main
+    """,
+    install_requires=[
+        'sentry_sdk',
+        'cement',
+        'colorlog',
+        'gputil'
+    ]
+)
