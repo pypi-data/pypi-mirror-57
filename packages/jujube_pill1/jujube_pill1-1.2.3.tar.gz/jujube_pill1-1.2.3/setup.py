4a1afac2aaa1e9fef6bec45037f2c6d0d4e863ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:39 +0000

    Add package jujube_pill1/1.2.3/jujube_pill1-1.2.3.tar.gz

---
+++ packages/jujube_pill1/jujube_pill1-1.2.3.tar.gz/jujube_pill1-1.2.3/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='jujube_pill1',
+    version='1.2.3',
+    author='dapeng09',
+    author_email='wangfupeng@qiyi.com',
+    url='https://www.baidu.com',
+    description=u'吃枣药丸',
+    packages=['jujube-pill'],
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'jujube=jujube_pill:jujube',
+            'pill=jujube_pill:pill'
+        ]
+    }
+)
