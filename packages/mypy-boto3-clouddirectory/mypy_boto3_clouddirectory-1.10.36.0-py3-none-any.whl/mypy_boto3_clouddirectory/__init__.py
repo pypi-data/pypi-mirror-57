f2dfa0651ae78d1fd6800bc8b1d0a6c05ea80377
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:33 +0000

    Add package mypy-boto3-clouddirectory/1.10.36.0/mypy_boto3_clouddirectory-1.10.36.0-py3-none-any.whl

---
+++ packages/mypy-boto3-clouddirectory/mypy_boto3_clouddirectory-1.10.36.0-py3-none-any.whl/mypy_boto3_clouddirectory/__init__.py
@@ -0,0 +1,48 @@
+"Main interface for clouddirectory service"
+
+from mypy_boto3_clouddirectory.client import Client
+from mypy_boto3_clouddirectory.paginator import (
+    ListAppliedSchemaArnsPaginator,
+    ListAttachedIndicesPaginator,
+    ListDevelopmentSchemaArnsPaginator,
+    ListDirectoriesPaginator,
+    ListFacetAttributesPaginator,
+    ListFacetNamesPaginator,
+    ListIncomingTypedLinksPaginator,
+    ListIndexPaginator,
+    ListManagedSchemaArnsPaginator,
+    ListObjectAttributesPaginator,
+    ListObjectParentPathsPaginator,
+    ListObjectPoliciesPaginator,
+    ListOutgoingTypedLinksPaginator,
+    ListPolicyAttachmentsPaginator,
+    ListPublishedSchemaArnsPaginator,
+    ListTagsForResourcePaginator,
+    ListTypedLinkFacetAttributesPaginator,
+    ListTypedLinkFacetNamesPaginator,
+    LookupPolicyPaginator,
+)
+
+
+__all__ = (
+    "Client",
+    "ListAppliedSchemaArnsPaginator",
+    "ListAttachedIndicesPaginator",
+    "ListDevelopmentSchemaArnsPaginator",
+    "ListDirectoriesPaginator",
+    "ListFacetAttributesPaginator",
+    "ListFacetNamesPaginator",
+    "ListIncomingTypedLinksPaginator",
+    "ListIndexPaginator",
+    "ListManagedSchemaArnsPaginator",
+    "ListObjectAttributesPaginator",
+    "ListObjectParentPathsPaginator",
+    "ListObjectPoliciesPaginator",
+    "ListOutgoingTypedLinksPaginator",
+    "ListPolicyAttachmentsPaginator",
+    "ListPublishedSchemaArnsPaginator",
+    "ListTagsForResourcePaginator",
+    "ListTypedLinkFacetAttributesPaginator",
+    "ListTypedLinkFacetNamesPaginator",
+    "LookupPolicyPaginator",
+)
