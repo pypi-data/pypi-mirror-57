119e5ed58e311cf6154755392ff487676b1b5186
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:34 +0000

    Add package pulp-file/0.1.0/pulp-file-0.1.0.tar.gz

---
+++ packages/pulp-file/pulp-file-0.1.0.tar.gz/pulp-file-0.1.0/pulp_file/tests/functional/api/test_publish.py
@@ -0,0 +1,77 @@
+# coding=utf-8
+"""Tests that publish file plugin repositories."""
+import unittest
+from random import choice
+
+from requests.exceptions import HTTPError
+
+from pulp_smash import api, config
+from pulp_smash.pulp3.utils import gen_repo, get_content, get_versions, sync, modify_repo
+
+from pulp_file.tests.functional.constants import (
+    FILE_CONTENT_NAME,
+    FILE_PUBLICATION_PATH,
+    FILE_REMOTE_PATH,
+    FILE_REPO_PATH,
+)
+from pulp_file.tests.functional.utils import create_file_publication, gen_file_remote
+from pulp_file.tests.functional.utils import set_up_module as setUpModule  # noqa:F401
+
+
+class PublishAnyRepoVersionTestCase(unittest.TestCase):
+    """Test whether a particular repository version can be published.
+
+    This test targets the following issues:
+
+    * `Pulp #3324 <https://pulp.plan.io/issues/3324>`_
+    * `Pulp Smash #897 <https://github.com/PulpQE/pulp-smash/issues/897>`_
+    """
+
+    def test_all(self):
+        """Test whether a particular repository version can be published.
+
+        1. Create a repository with at least 2 repository versions.
+        2. Create a publication by supplying the latest ``repository_version``.
+        3. Assert that the publication ``repository_version`` attribute points
+           to the latest repository version.
+        4. Create a publication by supplying the non-latest ``repository_version``.
+        5. Assert that the publication ``repository_version`` attribute points
+           to the supplied repository version.
+        6. Assert that an exception is raised when providing two different
+           repository versions to be published at same time.
+        """
+        cfg = config.get_config()
+        client = api.Client(cfg, api.json_handler)
+
+        body = gen_file_remote()
+        remote = client.post(FILE_REMOTE_PATH, body)
+        self.addCleanup(client.delete, remote["pulp_href"])
+
+        repo = client.post(FILE_REPO_PATH, gen_repo())
+        self.addCleanup(client.delete, repo["pulp_href"])
+
+        sync(cfg, remote, repo)
+
+        # Step 1
+        repo = client.get(repo["pulp_href"])
+        for file_content in get_content(repo)[FILE_CONTENT_NAME]:
+            modify_repo(cfg, repo, remove_units=[file_content])
+        version_hrefs = tuple(ver["pulp_href"] for ver in get_versions(repo))
+        non_latest = choice(version_hrefs[:-1])
+
+        # Step 2
+        publication = create_file_publication(cfg, repo)
+
+        # Step 3
+        self.assertEqual(publication["repository_version"], version_hrefs[-1])
+
+        # Step 4
+        publication = create_file_publication(cfg, repo, non_latest)
+
+        # Step 5
+        self.assertEqual(publication["repository_version"], non_latest)
+
+        # Step 6
+        with self.assertRaises(HTTPError):
+            body = {"repository": repo["pulp_href"], "repository_version": non_latest}
+            client.post(FILE_PUBLICATION_PATH, body)
