0789728f8cdee3a490df2c30681d0b1803c1acc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:51 +0000

    Add package mypy-boto3-kinesisvideo/1.10.34.1/mypy_boto3_kinesisvideo-1.10.34.1-py3-none-any.whl

---
+++ packages/mypy-boto3-kinesisvideo/mypy_boto3_kinesisvideo-1.10.34.1-py3-none-any.whl/mypy_boto3_kinesisvideo/paginator.py
@@ -0,0 +1,46 @@
+"Main interface for kinesisvideo service Paginators"
+from __future__ import annotations
+
+from botocore.paginate import Paginator as Boto3Paginator
+from mypy_boto3_kinesisvideo.type_defs import (
+    ChannelNameConditionTypeDef,
+    ListSignalingChannelsOutputTypeDef,
+    ListStreamsOutputTypeDef,
+    PaginatorConfigTypeDef,
+    StreamNameConditionTypeDef,
+)
+
+
+__all__ = ("ListSignalingChannelsPaginator", "ListStreamsPaginator")
+
+
+class ListSignalingChannelsPaginator(Boto3Paginator):
+    """
+    [Paginator.ListSignalingChannels documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.34/reference/services/kinesisvideo.html#KinesisVideo.Paginator.ListSignalingChannels)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self,
+        ChannelNameCondition: ChannelNameConditionTypeDef = None,
+        PaginationConfig: PaginatorConfigTypeDef = None,
+    ) -> ListSignalingChannelsOutputTypeDef:
+        """
+        [ListSignalingChannels.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.34/reference/services/kinesisvideo.html#KinesisVideo.Paginator.ListSignalingChannels.paginate)
+        """
+
+
+class ListStreamsPaginator(Boto3Paginator):
+    """
+    [Paginator.ListStreams documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.34/reference/services/kinesisvideo.html#KinesisVideo.Paginator.ListStreams)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self,
+        StreamNameCondition: StreamNameConditionTypeDef = None,
+        PaginationConfig: PaginatorConfigTypeDef = None,
+    ) -> ListStreamsOutputTypeDef:
+        """
+        [ListStreams.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.34/reference/services/kinesisvideo.html#KinesisVideo.Paginator.ListStreams.paginate)
+        """
