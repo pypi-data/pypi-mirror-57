fe71901714f8d280739e7a1522193fa4f217d6f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:39 +0000

    Add package django-access-logger-middleware/0.0.2/django_access_logger_middleware-0.0.2.tar.gz

---
+++ packages/django-access-logger-middleware/django_access_logger_middleware-0.0.2.tar.gz/django_access_logger_middleware-0.0.2/django_access_logger_middleware/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ApiConfig(AppConfig):
+    name = 'django_access_logger_middleware'
+	verbose_name = "Django Application Access Logger"
---
+++ packages/django-access-logger-middleware/django_access_logger_middleware-0.0.2.tar.gz/django_access_logger_middleware-0.0.2/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='django_access_logger_middleware',
+    version='0.0.2',
+    description='Access logging package for Django',
+    url='https://github.com/sharafjaffri/django-access-logger.git',
+    author='Sharaf Ali',
+    author_email='sharafjaffri@live.com',
+    license='GNU GENERAL PUBLIC LICENSE Version 3',
+    packages=['django_access_logger_middleware'],
+    zip_safe=False,
+    install_requires=['Django'],
+    python_requires=">=3.5",
+)
