f38f29354475ea3e47674f3d177bc8d2fd317382
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:53 +0000

    Add package ocaml/0.0.9/ocaml-0.0.9.tar.gz

---
+++ packages/ocaml/ocaml-0.0.9.tar.gz/ocaml-0.0.9/__init__.py
@@ -0,0 +1,36 @@
+import os
+from .sharedlib import ocaml
+from .sharedlib.ocaml import toploop
+_topdir = os.path.dirname(os.path.abspath(ocaml.__file__))
+toploop.add_topdir(_topdir)
+
+def _register_ipython_magic():
+  try:
+    from wurlitzer import sys_pipes
+    from IPython.core.magic import register_line_magic, register_cell_magic
+
+    @register_line_magic
+    def ocaml(line):
+      with sys_pipes():
+        return toploop.get(line)
+
+    del ocaml
+
+    @register_line_magic
+    def ocaml_t(line):
+      type_, body = line.split(':', maxsplit=1)
+      with sys_pipes():
+        return toploop.get(type_, body)
+
+    del ocaml_t
+
+    @register_cell_magic
+    def ocaml(line, cell):
+      with sys_pipes():
+        return toploop.eval(cell)
+
+    del ocaml
+  except:
+    pass
+
+_register_ipython_magic()
