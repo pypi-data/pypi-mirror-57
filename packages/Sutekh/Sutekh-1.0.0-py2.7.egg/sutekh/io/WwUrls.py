48a09d645f2d0d17b805692181c7aef41e2eec06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:25 +0000

    Add package Sutekh/1.0.0/Sutekh-1.0.0-py2.7.egg

---
+++ packages/Sutekh/Sutekh-1.0.0-py2.7.egg/sutekh/io/WwUrls.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# vim:fileencoding=utf-8 ai ts=4 sts=4 et sw=4
+# Copyright 2008 Simon Cross <hodgestar@gmail.com>
+# GPL - see COPYING for details
+
+"""Data about the urls for the cardlist and other useful data files.
+   """
+
+WW_CARDLIST_DATAPACK = "cardlist"
+
+WW_CARDLIST_URL = ("http://bitbucket.org/hodgestar/sutekh-extras/"
+                   "raw/tip/CardList/cardlist-data/cardlist.txt")
+
+EXTRA_CARD_URL = ("http://bitbucket.org/hodgestar/sutekh-extras/"
+                  "raw/tip/extra_list.txt")
+
+WW_RULINGS_URL = ("http://bitbucket.org/hodgestar/sutekh-extras/"
+                  "raw/tip/Rulebooks/rulebook-data/rulings.html")
+
+EXP_DATA_URL = ("http://bitbucket.org/hodgestar/sutekh-extras/"
+                "raw/tip/ExpansionDates/expansion-data/expansions.json")
+
+LOOKUP_DATA_URL = ("http://bitbucket.org/hodgestar/sutekh-extras/"
+                   "raw/tip/Lookups/lookup.csv")
