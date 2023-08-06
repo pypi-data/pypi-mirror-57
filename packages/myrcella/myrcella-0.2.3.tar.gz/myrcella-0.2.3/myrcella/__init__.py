ee949e79c77936f53289fa1c1a2960bcd6a72973
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:42 +0000

    Add package myrcella/0.2.3/myrcella-0.2.3.tar.gz

---
+++ packages/myrcella/myrcella-0.2.3.tar.gz/myrcella-0.2.3/myrcella/__init__.py
@@ -0,0 +1,50 @@
+name = "myrcella"
+version = 0,2,3
+versionstr = ".".join(str(n) for n in version)
+desc = "Simple API server to collect data according to dynamically loaded yaml specifications"
+author = {
+	"name": "Johannes Krattenmacher",
+	"email": "python@krateng.dev",
+	"github": "krateng"
+}
+requires = [
+	"bottle>=0.12.16",
+	"waitress>=1.3",
+	"doreah>=1.4"
+]
+resources = [
+	"data_files/*/*"
+]
+commands = {
+	"myrcella":"control:main"
+}
+
+
+
+
+
+
+### DOREAH CONFIGURATION
+
+from doreah import config
+import pkg_resources
+
+config(
+	packageutils={
+		"packagename": name,
+		"populate": pkg_resources.resource_filename(__name__,"data_files")
+	}
+)
+from doreah.packageutils import pkgdata, init_datafolder
+init_datafolder()
+config(
+	logging={
+		"logfolder": pkgdata("logs")
+	},
+	settings={
+		"files":[
+			pkgdata("settings","default.ini"),
+			pkgdata("settings","settings.ini")
+		]
+	}
+)
