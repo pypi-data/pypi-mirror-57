7938e1db84cae1363055e5c72cf0b8d684d64441
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:51 +0000

    Add package aaapi/0.1.1/aaapi-0.1.1.tar.gz

---
+++ packages/aaapi/aaapi-0.1.1.tar.gz/aaapi-0.1.1/setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+import os, pkg_resources
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+    str(requirement)
+    for requirement in pkg_resources.parse_requirements(
+        open(os.path.join(os.path.dirname(__file__), "requirements.txt"))
+    )
+]
+
+setup_requirements = [ ]
+
+test_requirements = [ ]
+
+setup(
+    author="Ye Yu",
+    author_email='rafolwen98@gmail.com',
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    description="Another Audio API - Collection of audio and music processing API with massive amount of dependencies",
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='aaapi',
+    name='aaapi',
+    packages=find_packages(include=['aaapi', 'aaapi.*']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/ye-yu/aaapi',
+    version='0.1.1',
+    zip_safe=False,
+)
