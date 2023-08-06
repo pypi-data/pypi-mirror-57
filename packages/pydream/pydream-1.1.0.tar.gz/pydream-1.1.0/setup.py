d13836fcf7d56790393ee517ea5235089227aecb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:45 +0000

    Add package pydream/1.1.0/pydream-1.1.0.tar.gz

---
+++ packages/pydream/pydream-1.1.0.tar.gz/pydream-1.1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='pydream',
+      version='1.1.0',
+      description='A Python implementation of the MT-DREAM(ZS) algorithm from Laloy and Vrugt 2012.',
+      long_description=readme(),
+      classifiers=['Programming Language :: Python :: 2'],
+      url='https://github.com/LoLab-VU/PyDREAM',
+      author='Erin Shockley',
+      author_email='erin.shockley@vanderbilt.edu',
+      packages=['pydream'],
+      install_requires=['numpy', 'scipy', 'multiprocess'],
+      zip_safe=False,
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      include_package_data=True
+      )
