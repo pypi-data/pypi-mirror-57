baf068d61275f50d39b9cf66ba747e9d1c99ce4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:46 +0000

    Add package spatialist/0.4/spatialist-0.4.tar.gz

---
+++ packages/spatialist/spatialist-0.4.tar.gz/spatialist-0.4/spatialist/__init__.py
@@ -0,0 +1,17 @@
+from . import vector
+from . import envi
+from . import ancillary
+from . import raster
+from . import sqlite_util
+from .auxil import crsConvert, haversine, gdalbuildvrt, gdalwarp, gdal_translate, ogr2ogr, gdal_rasterize
+
+from .vector import Vector, bbox, centerdist, intersect
+from .raster import Raster, stack, rasterize
+from .sqlite_util import sqlite_setup, sqlite3
+
+from pkg_resources import get_distribution, DistributionNotFound
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    pass
