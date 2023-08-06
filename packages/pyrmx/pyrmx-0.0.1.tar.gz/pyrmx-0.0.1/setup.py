619bcdd0e7d826b96c0920761928211aeeb56999
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:56 +0000

    Add package pyrmx/0.0.1/pyrmx-0.0.1.tar.gz

---
+++ packages/pyrmx/pyrmx-0.0.1.tar.gz/pyrmx-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pyrmx',
+    version='0.0.1',
+    description='A stub pyrmx package',
+    long_description='',
+    url='https://rmxwallet.io',
+    author='RMX Wallet',
+    author_email='pocin@rmxwallet.io',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
