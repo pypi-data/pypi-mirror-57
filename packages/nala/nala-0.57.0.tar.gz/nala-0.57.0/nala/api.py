6ab5fd08e703ffd9bf283734f88094e2870c1a42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:15 +0000

    Add package nala/0.57.0/nala-0.57.0.tar.gz

---
+++ packages/nala/nala-0.57.0.tar.gz/nala-0.57.0/nala/api.py
@@ -0,0 +1,28 @@
+from .inspect import collect_mocked_functions
+from .generator import GeneratedMock
+from .generator import FileGenerator
+
+
+NALA_C_FUNCTIONS = [
+    'snprintf',
+    'memcpy',
+    'strcmp',
+    'strncmp'
+]
+
+
+def generate_mocks(expanded_code, output_directory, rename_parameters_file):
+    """Identify mocked functions and generate the source and header files.
+
+    """
+
+    generator = FileGenerator()
+
+    for function in collect_mocked_functions(expanded_code, rename_parameters_file):
+        if function.name in NALA_C_FUNCTIONS:
+            raise Exception(
+                f"'{function.name}()' cannot be mocked as it is used by Nala.")
+
+        generator.add_mock(function)
+
+    generator.write_to_directory(output_directory)
