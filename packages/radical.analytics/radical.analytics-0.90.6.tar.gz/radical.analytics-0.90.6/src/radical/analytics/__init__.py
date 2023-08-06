0af8dcd7dfe437f5bef64ba618b52998c999e081
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:32 +0000

    Add package radical.analytics/0.90.6/radical.analytics-0.90.6.tar.gz

---
+++ packages/radical.analytics/radical.analytics-0.90.6.tar.gz/radical.analytics-0.90.6/src/radical/analytics/__init__.py
@@ -0,0 +1,28 @@
+
+__copyright__ = "Copyright 2013-2016, http://radical.rutgers.edu"
+__license__   = "GPL.v2"
+
+
+# ------------------------------------------------------------------------------
+
+from .experiment import Experiment
+from .session    import Session
+from .plotter    import Plotter
+
+
+# ------------------------------------------------------------------------------
+#
+import os
+import radical.utils as ru
+
+pwd  = os.path.dirname (__file__)
+root = "%s" % pwd
+version_short, version_detail, version_base, \
+        version_branch, sdist_name, sdist_path = ru.get_version(paths=[root])
+version = version_short
+
+logger = ru.Logger('radical.analytics')
+logger.info('radical.analytics    version: %s' % version_detail)
+
+# ------------------------------------------------------------------------------
+
