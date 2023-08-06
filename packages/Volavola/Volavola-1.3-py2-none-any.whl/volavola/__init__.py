921fafe1b5b711cf8fe55b767b23a2b7afe1badc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:17 +0000

    Add package Volavola/1.3/Volavola-1.3-py2-none-any.whl

---
+++ packages/Volavola/Volavola-1.3-py2-none-any.whl/volavola/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 14 15:24:17 2019
+
+@author: dhk13
+"""
+
+print("executing init")
+print(a+b)
---
+++ packages/Volavola/Volavola-1.3-py2-none-any.whl/volavola/testfile.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 13 23:42:16 2019
+
+@author: dhk13
+"""
+
+print("This is test module")
