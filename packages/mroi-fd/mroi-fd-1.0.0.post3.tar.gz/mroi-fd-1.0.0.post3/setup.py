7bf3a0c222f374a0bfbcf7d32e1ed989f720dd56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:44 +0000

    Add package mroi-fd/1.0.0.post3/mroi-fd-1.0.0.post3.tar.gz

---
+++ packages/mroi-fd/mroi-fd-1.0.0.post3.tar.gz/mroi-fd-1.0.0.post3/setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mroi-fd",
+    version="1.0.0post3",
+    author="ailabuser",
+    author_email="lab@ailab.space",
+    description=\
+        """
+        Face detection wrapper using hybrid margin-based region of interest
+        (MROI) approach."
+        """,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/ailabuser/bacha-hybrid-mroi-face-detection",
+
+    packages=setuptools.find_packages(),
+
+    # Refer to  https://pypi.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+        "Operating System :: Unix"
+    ],
+    python_requires='>=3.7.0',
+
+    install_requires=[
+        'opencv-python>=4.1.1.26',
+        'numpy>=1.17.2',
+        'mtcnn>=0.1.0',
+        'pandas>=0.25.2',
+        'llist>=0.6',
+        'tensorflow>=2.0.0'
+    ]
+)
