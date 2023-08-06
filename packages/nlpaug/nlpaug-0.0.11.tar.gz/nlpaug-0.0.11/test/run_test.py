b5be858c55dfbc6062f9d93c625f1a0af6b45665
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:27 +0000

    Add package nlpaug/0.0.11/nlpaug-0.0.11.tar.gz

---
+++ packages/nlpaug/nlpaug-0.0.11.tar.gz/nlpaug-0.0.11/test/run_test.py
@@ -0,0 +1,36 @@
+import unittest
+import sys
+import logging
+
+
+if __name__ == '__main__':
+    sys.path.append('../nlpaug')
+
+    # disable transformer's info logging
+    for file_name in ['tokenization_utils', 'file_utils', 'modeling_utils', 'modeling_xlnet',
+                      'configuration_utils']:
+        logging.getLogger('transformers.' + file_name).setLevel(logging.ERROR)
+
+    test_dirs = [
+        'test/augmenter/char/',
+        'test/augmenter/word/',
+        'test/augmenter/sentence/',
+        'test/augmenter/audio/',
+        'test/augmenter/spectrogram/',
+        'test/model/char/',
+        'test/util/selection/',
+        'test/flow/'
+    ]
+
+    runner = unittest.TextTestRunner()
+
+    for test_dir in test_dirs:
+       loader = unittest.TestLoader()
+       suite = loader.discover(test_dir)
+       runner.run(suite)
+
+    # suite = unittest.TestLoader().loadTestsFromName('augmenter.sentence.test_context_word_embs_sentence')
+    # runner.run(suite)
+    #
+    # suite = unittest.TestLoader().loadTestsFromName('model.char.test_keyboard')
+    # runner.run(suite)
