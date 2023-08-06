e710a9044f37ce8431bc8e0a99c66e89b3690946
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:36 +0000

    Add package bnlp-toolkit/1.2.0/bnlp_toolkit-1.2.0.tar.gz

---
+++ packages/bnlp-toolkit/bnlp_toolkit-1.2.0.tar.gz/bnlp_toolkit-1.2.0/bnlp/__init__.py
@@ -0,0 +1,13 @@
+__version__="1.1.0"
+
+
+import os
+from bnlp.sentencepiece_tokenizer import SP_Tokenizer
+from bnlp.nltk_tokenizer import NLTK_Tokenizer
+from bnlp.basic_tokenizer import BasicTokenizer
+from bnlp.bengali_word2vec import Bengali_Word2Vec
+from bnlp.bengali_fasttext import Bengali_Fasttext
+from bnlp.glove_wordvector import BN_Glove
+
+
+
---
+++ packages/bnlp-toolkit/bnlp_toolkit-1.2.0.tar.gz/bnlp_toolkit-1.2.0/bnlp/nltk_tokenizer.py
@@ -0,0 +1,44 @@
+import nltk
+
+# try:
+#     nltk.data.find('tokenizers/punkt')
+# except LookupError:
+#     print("punkt not found. downloading...")
+#     nltk.download('punkt')
+
+
+class NLTK_Tokenizer(object):
+    def __init__(self, text):
+        self.text = text
+        """
+        :text: (str) input text for tokenization
+
+        """
+
+    def word_tokenize(self):
+        tokens = nltk.word_tokenize(self.text)
+        new_tokens = []
+        for token in tokens:
+            if token[-1] == "।" and len(token)>1:
+                token_1 = token[:-1]
+                token_2 = token[-1]
+                new_tokens.append(token_1)
+                new_tokens.append(token_2)
+            else:
+              new_tokens.append(token)
+        return new_tokens
+    
+    def sentence_tokenize(self):
+        text = self.text.replace("।", ".")
+        tokens = nltk.tokenize.sent_tokenize(text)
+        new_tokens = []
+        for token in tokens:
+            if token[-1] == ".":
+                token = token[:-2] + token[-2:].replace(".","।")
+            new_tokens.append(token)
+        return new_tokens
+
+
+
+
+
