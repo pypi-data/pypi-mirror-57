e7b9f7bad215385c2b74ec7e26e68665cf96d9d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:26 +0000

    Add package perfplot/0.7.0/perfplot-0.7.0.tar.gz

---
+++ packages/perfplot/perfplot-0.7.0.tar.gz/perfplot-0.7.0/perfplot/__init__.py
@@ -0,0 +1,22 @@
+from perfplot.__about__ import (
+    __author__,
+    __author_email__,
+    __copyright__,
+    __license__,
+    __status__,
+    __version__,
+)
+from perfplot.main import bench, plot, save, show
+
+__all__ = [
+    "__author__",
+    "__author_email__",
+    "__copyright__",
+    "__license__",
+    "__version__",
+    "__status__",
+    "bench",
+    "plot",
+    "show",
+    "save",
+]
