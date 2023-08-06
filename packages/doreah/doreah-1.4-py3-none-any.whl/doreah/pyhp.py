9e35caf8d94a51365986115a24c304d6d59ea1f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:35 +0000

    Add package doreah/1.4/doreah-1.4-py3-none-any.whl

---
+++ packages/doreah/doreah-1.4-py3-none-any.whl/doreah/pyhp.py
@@ -0,0 +1,13 @@
+from ._internal import DoreahConfig
+
+config = DoreahConfig("pyhp",
+	interpret=str,
+	version=2
+)
+
+if config["version"] == 1:
+	print("Using PYHP version 1")
+	from .pyhp1 import *
+elif config["version"] == 2:
+	print("Using PYHP version 2")
+	from .pyhp2 import *
