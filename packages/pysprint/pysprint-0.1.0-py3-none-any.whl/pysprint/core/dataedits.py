144e3a2bb4a4217de573e3a30b6f9f13449b1f8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:35 +0000

    Add package pysprint/0.1.0/pysprint-0.1.0-py3-none-any.whl

---
+++ packages/pysprint/pysprint-0.1.0-py3-none-any.whl/pysprint/core/dataedits.py
@@ -0,0 +1,92 @@
+"""
+Methods for manipulating the loaded data
+"""
+import numpy as np
+from scipy.signal import find_peaks, savgol_filter, gaussian, convolve
+from scipy.interpolate import interp1d
+from pysprint.utils import findNearest, _handle_input, between
+
+
+def savgol(
+	initSpectrumX, initSpectrumY, referenceArmY, sampleArmY, window=101,
+	order=3):
+
+	Xdata, Ydata = _handle_input(
+		initSpectrumX, initSpectrumY, referenceArmY, sampleArmY
+		)
+	xint, yint = interpolate_data(Xdata, Ydata, [], [])
+	if window > order:
+		try:
+			if window % 2 == 1:
+				fil = savgol_filter(yint, window_length = window, polyorder = order)
+				return xint, fil
+			else:
+				fil = savgol_filter(yint, window_length = window + 1, polyorder = order)
+				return xint, fil
+		except Exception as e:
+			print(e)
+	else:
+		raise ValueError('window must be bigger than order (currently {}/{})'.format(window, order))
+
+def find_peak(
+	initSpectrumX, initSpectrumY, referenceArmY, sampleArmY,
+	proMax=1, proMin=1, threshold=0.1, except_around=None):   
+
+	Xdata, Ydata = _handle_input(initSpectrumX, initSpectrumY, referenceArmY, sampleArmY)
+	maxIndexes, _ = find_peaks(Ydata, prominence=proMax) 
+	Ydata_rec = 1/Ydata
+	minIndexes, _ = find_peaks(Ydata_rec, prominence=proMin)
+	min_idx = []
+	max_idx = []
+	for idx in maxIndexes:
+		if np.abs(Ydata[idx]) > threshold or between(Xdata[idx], except_around):
+			max_idx.append(idx)
+	for idx in minIndexes:
+		if np.abs(Ydata[idx]) > threshold or between(Xdata[idx], except_around):
+			min_idx.append(idx)
+
+	if len(Xdata[max_idx]) != len(Ydata[max_idx]) or len(Xdata[min_idx]) != len(Ydata[min_idx]):
+		raise ValueError('Something went wrong, try to cut the edges of data.')
+
+	return Xdata[max_idx], Ydata[max_idx], Xdata[min_idx], Ydata[min_idx]
+
+
+def convolution(
+	initSpectrumX, initSpectrumY, referenceArmY, sampleArmY,
+	win_len ,standev=200):
+	Xdata, Ydata = _handle_input(
+		initSpectrumX, initSpectrumY, referenceArmY, sampleArmY
+		)
+	if win_len < 0 or win_len > len(Xdata):
+		raise ValueError('Window length must be 0 < window_length < len(x)')
+
+	xint, yint = interpolate_data(Xdata, Ydata, [], [])
+	window = gaussian(win_len, std=standev)
+	smoothed = convolve(yint, window/window.sum(), mode='same')
+	return xint, smoothed
+
+def interpolate_data(initSpectrumX, initSpectrumY, referenceArmY, sampleArmY):
+	Xdata, Ydata = _handle_input(
+		initSpectrumX, initSpectrumY, referenceArmY, sampleArmY
+		)
+	xint = np.linspace(Xdata[0], Xdata[-1], len(Xdata))
+	intp = interp1d(Xdata,Ydata, kind='linear')
+	yint = intp(xint)
+	return xint, yint
+
+
+def cut_data(
+	initSpectrumX, initSpectrumY, referenceArmY, sampleArmY,
+	startValue=-9999, endValue=9999):
+
+	Xdata, Ydata = _handle_input(
+		initSpectrumX, initSpectrumY, referenceArmY, sampleArmY
+		)
+	if startValue < endValue:
+		lowItem, lowIndex = findNearest(Xdata, startValue)
+		highItem, highIndex = findNearest(Xdata, endValue)
+		mask = np.where((Xdata>=lowItem) & (Xdata<=highItem))
+		return Xdata[mask], Ydata[mask]
+	else:
+		pass
+
---
+++ packages/pysprint/pysprint-0.1.0-py3-none-any.whl/pysprint/utils/accessories.py
@@ -0,0 +1,119 @@
+from functools import wraps
+
+import numpy as np
+from scipy.interpolate import interp1d
+
+
+__all__ = ['scipy_disp', 'lmfit_disp', 'findNearest', 'find_closest',
+           '_handle_input', 'print_disp', 'fourier_interpolate',
+           'between', 'get_closest', 'run_from_ipython']
+
+def run_from_ipython():
+    try:
+        __IPYTHON__
+        return True
+    except NameError:
+        return False
+
+def get_closest(xValue, xArray, yArray):
+	idx = (np.abs(xArray-xValue)).argmin()
+	value = xArray[idx]
+	return value, yArray[idx], idx
+
+def between(val, except_around):
+	if except_around is None:
+		return False
+	elif len(except_around) != 2:
+		raise ValueError(f'Invalid interval. Try [start, end] instead of {except_around}')
+	else:
+		lower = float(min(except_around))
+		upper = float(max(except_around))
+	if val <= upper and val >= lower:
+		return True
+	return False
+
+def scipy_disp(r):
+	for idx in range(len(r)):
+		dispersion[idx] = dispersion[idx] * factorial(idx+1)
+		dispersion_std[idx] = dispersion_std[idx] * factorial(idx+1)
+	return dispersion, dispersion_std
+
+
+def lmfit_disp(r):
+	dispersion, dispersion_std = [], []
+	for name, par in r:
+		dispersion.append(par.value)
+		dispersion_std.append(par.stderr)
+	return dispersion, dispersion_std
+
+
+def findNearest(array, value):
+	#Finds the nearest element to the given value in the array
+	#returns tuple: (element, element's index)
+	
+    array = np.asarray(array)
+    idx = (np.abs(value - array)).argmin()
+    return array[idx], idx
+
+
+def _handle_input(initSpectrumX, initSpectrumY, referenceArmY, sampleArmY):
+	"""
+	Instead of handling the inputs in every function, there is this private method.
+
+	Parameters
+	----------
+
+	initSpectrumX: array-like
+	x-axis data
+
+	initSpectrumY: array-like
+	y-axis data
+
+	referenceArmY, sampleArmY: array-like
+	reference and sample arm spectrum evaluated at initSpectrumX
+
+	Returns
+	-------
+	initSpectrumX: array-like
+	unchanged x data
+
+	Ydata: array-like
+	the transformed y data
+
+	"""
+	if (len(initSpectrumX) > 0) and (len(referenceArmY) > 0) and (len(sampleArmY) > 0):
+		Ydata = (initSpectrumY-referenceArmY-sampleArmY)/(2*np.sqrt(referenceArmY*sampleArmY))
+	elif (len(referenceArmY) == 0) or (len(sampleArmY) == 0):
+		Ydata = initSpectrumY
+	elif len(initSpectrumX) == 0:
+		raise ValueError('Please load the spectrum!\n')
+	elif len(initSpectrumY) == 0:
+		raise ValueError('Please load the spectrum!\n')
+	else:
+		raise TypeError('Input types are wrong.\n')
+	return initSpectrumX,  Ydata
+
+
+def find_closest(xValue, xArray, yArray):
+	idx = (np.abs(xArray-xValue)).argmin()
+	value = xArray[idx]
+	return value, yArray[idx]
+
+
+def print_disp(f):
+    @wraps(f)
+    def wrapping(*args, **kwargs):
+        disp, disp_std, stri = f(*args, **kwargs)
+        labels = ('GD', 'GDD','TOD', 'FOD', 'QOD')  
+        for i, (label, disp_item, disp_std_item) in enumerate(zip(labels, disp, disp_std)):
+             print(f'{label} = {disp_item} ± {disp_std_item} fs^{i+1}')
+        return disp, disp_std, stri
+    return wrapping
+
+
+def fourier_interpolate(x, y):
+    ''' Simple linear interpolation for FFTs'''
+    xs = np.linspace(x[0], x[-1], len(x))
+    intp = interp1d(x, y, kind='linear', fill_value = 'extrapolate')
+    ys = intp(xs)
+    return xs, ys
