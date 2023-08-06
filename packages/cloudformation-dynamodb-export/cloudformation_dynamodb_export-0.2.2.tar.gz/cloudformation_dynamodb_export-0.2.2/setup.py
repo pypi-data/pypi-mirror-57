80797bbe5662ecbd8d59a0615de4f824b40b5691
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:09 +0000

    Add package cloudformation-dynamodb-export/0.2.2/cloudformation_dynamodb_export-0.2.2.tar.gz

---
+++ packages/cloudformation-dynamodb-export/cloudformation_dynamodb_export-0.2.2.tar.gz/cloudformation_dynamodb_export-0.2.2/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+requires = ["awscli>=1.16"]
+
+setuptools.setup(
+    name="cloudformation_dynamodb_export",
+    version="0.2.2",
+    author="tomoya55",
+    author_email="hiranotomoya@gmail.com",
+    description="Export DynamoDB definition in Cloudformation template files as json",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tomoya55/cloudformation-dynamodb-export",
+    packages=setuptools.find_packages(),
+    entry_points={
+        'console_scripts': [
+            'cfnddb_export = cloudformation_dynamodb_export.cfn2ddb:export',
+        ]
+    },
+    install_requires=requires,
+    keywords="aws cloudformation dynamodb",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
