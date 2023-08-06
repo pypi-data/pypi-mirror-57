08a4af68e653525ccfb1a84050471174dbafae9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:44 +0000

    Add package jrpyml/0.2.1/jrpyml-0.2.1.tar.gz

---
+++ packages/jrpyml/jrpyml-0.2.1.tar.gz/jrpyml-0.2.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['jrpyml', 'jrpyml.datasets']
+
+package_data = \
+{'': ['*'], 'jrpyml': ['vignettes/*'], 'jrpyml.datasets': ['data/*']}
+
+install_requires = \
+['flake8',
+ 'graphviz>=0.10.1,<0.11.0',
+ 'matplotlib>=3.0,<4.0',
+ 'numpy>=1.15,<2.0',
+ 'pandas>=0.23.4,<0.24.0',
+ 'seaborn>=0.9.0,<0.10.0',
+ 'sklearn>=0.0.0,<0.0.1']
+
+setup_kwargs = {
+    'name': 'jrpyml',
+    'version': '0.2.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Jamie',
+    'author_email': 'jamie@jumpingrivers.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.5,<4.0',
+}
+
+
+setup(**setup_kwargs)
