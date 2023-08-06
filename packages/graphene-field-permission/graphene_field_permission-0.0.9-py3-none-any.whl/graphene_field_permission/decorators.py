5b0754125d1d446d38f076ae5994be50c8cc1a2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:38 +0000

    Add package graphene-field-permission/0.0.9/graphene_field_permission-0.0.9-py3-none-any.whl

---
+++ packages/graphene-field-permission/graphene_field_permission-0.0.9-py3-none-any.whl/graphene_field_permission/decorators.py
@@ -0,0 +1,27 @@
+import logging
+from . import api
+
+logger = logging.getLogger(__name__)
+
+
+class has_field_access:
+    def __init__(self, *req_perms, filter_field=None):
+        self.filter_field = filter_field
+        self.req_perms = req_perms
+
+    def __call__(self, func, *args, **kwargs):
+        def check(data, info, *args, **kwargs):
+            try:
+                api.check_field_access(
+                    *self.req_perms,
+                    info_context=info.context,
+                    filter_field=self.filter_field,
+                    filter_data=data
+                )
+            except PermissionError:
+                field = '_'.join(func.__name__.split('_')[1:])
+                error_msg = "No access for user on field '{}'"
+                raise Exception(error_msg.format(field))
+
+            return func(data, info=info, *args, **kwargs)
+        return check
