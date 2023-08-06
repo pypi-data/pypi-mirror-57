01671a2b225fe366b71421bae110c4a036617678
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:37 +0000

    Add package RetroUFO/0.9.5/RetroUFO-0.9.5.tar.gz

---
+++ packages/RetroUFO/RetroUFO-0.9.5.tar.gz/RetroUFO-0.9.5/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="RetroUFO",
+    version="0.9.5",
+    author="Melon Bread",
+    author_email="rain@melonbread.dev",
+    description="Easily upgrade all libreto cores from the build bot",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    keywords=["libretro", "retroarch", "core"],
+    url="https://github.com/pypa/sampleproject",
+    packages=["RetroUFO"],
+    install_requires=[
+          'PySide2',
+      ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            "RetroUFO=RetroUFO.RetroUFO:main",
+            "RetroUFO-GUI=RetroUFO.RetroUFO_GUI:main"
+        ]
+    },
+)
