5f8505832b6038cfb6e8a30756865e07947c9a0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:13 +0000

    Add package OTSun/2.3.3/OTSun-2.3.3.tar.gz

---
+++ packages/OTSun/OTSun-2.3.3.tar.gz/OTSun-2.3.3/otsun/__init__.py
@@ -0,0 +1,14 @@
+from .math import *
+from .optics import *
+from .outputs import *
+from .materials import *
+from .ray import *
+from .source import *
+from .scene import *
+from .experiments import *
+from .logging_unit import *
+
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
