ae365befc51f862beb50dd01062818266123420c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:09 +0000

    Add package stylist/0.1/stylist-0.1.tar.gz

---
+++ packages/stylist/stylist-0.1.tar.gz/stylist-0.1/README.md
@@ -0,0 +1,34 @@
+![](https://github.com/MetOffice/stylist/workflows/Python%20package/badge.svg)
+
+# Stylist
+
+The immediate need was for a code style checking tool which understood Fortran.
+Such things being quite thin on the ground. However very few large software
+projects are implemented in a single language. Thus a tool was developed which
+provided a framework to aid in delivering support for multiple languages and
+multiple style choices.
+
+The framework supports multiple styles, each consisting of multiple rules.
+Each of these may be one of two types, those which treat the source as a text
+file and those which treat it as a parse tree.
+
+The project is still in its infancy so only a few rules have been implemented
+and only for fortran.
+
+## Design
+
+In the past we have used text based tools and there's a surprising amount you
+can do with regular expressions. However once context becomes important they
+can become a liability. In those cases it is much better to have the parse
+tree.
+
+At the moment the tool is only able to flag up where a style rule has been
+broken. For some things this is the best we can hope for but for others
+we might aspire to correct the problem.
+
+For instance the current check for missing `implicit none` statements might,
+in the future, insert them where they are missing.
+
+Some UML class diagrams have been prepared which may be found in the
+`documentation` directory. These make use of the
+[PlantUML](https://plantuml.com) tool to render.
---
+++ packages/stylist/stylist-0.1.tar.gz/stylist-0.1/setup.py
@@ -0,0 +1,59 @@
+"""Setup Stylist for distribution on PyPI."""
+
+from ast import literal_eval
+import os
+from setuptools import setup, find_packages
+
+# Get the long description from the README file
+HERE = os.path.dirname(__file__)
+with open(os.path.join(HERE, 'README.md'), encoding='utf-8',) as handle:
+    LONG_DESCRIPTION = handle.read()
+with open(
+    os.path.join(HERE, 'source', 'stylist', '__init__.py'),
+    encoding='utf-8',
+) as handle:
+    for line in handle:
+        items = line.split('=', 1)
+        if items[0].strip() == '__version__':
+            VERSION = literal_eval(items[1].strip())
+            break
+    else:
+        raise RuntimeError('Cannot determine package version.')
+
+setup(
+    name='stylist',
+    version=VERSION,
+    description=(
+        'Extensible code style checker'
+        ' currently supporting Fortran, PSyclone DSL, etc'
+    ),
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    url='https://github.com/MetOffice/stylist',
+    author='Met Office',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Quality Assurance',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    keywords='linter fortran psyclone',
+    package_dir={'': 'source'},
+    packages=find_packages(where='source'),
+    python_requires='>=3.6, <4',
+    install_requires=['fparser'],
+    extras_require={
+        'dev': ['check-manifest', 'flake8'],
+        'test': ['pytest', 'pytest-cov'],
+    },
+    entry_points={
+        'console_scripts': ['stylist=stylist.__main__:main'],
+    },
+    project_urls={
+        'Bug Reports': 'https://github.com/MetOffice/stylist/issues',
+        'Source': 'https://github.com/MetOffice/stylist/',
+    },
+)
