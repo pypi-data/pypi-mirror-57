1b43f4d2436c97025acc7a17d614e97ccdef2de6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:30 +0000

    Add package mypy-boto3-acm-pca/1.10.38.0/mypy-boto3-acm-pca-1.10.38.0.tar.gz

---
+++ packages/mypy-boto3-acm-pca/mypy-boto3-acm-pca-1.10.38.0.tar.gz/mypy-boto3-acm-pca-1.10.38.0/mypy_boto3_acm_pca/__init__.py
@@ -0,0 +1,25 @@
+"Main interface for acm-pca service"
+
+from mypy_boto3_acm_pca.client import ACMPCAClient as Client, ACMPCAClient
+from mypy_boto3_acm_pca.paginator import (
+    ListCertificateAuthoritiesPaginator,
+    ListPermissionsPaginator,
+    ListTagsPaginator,
+)
+from mypy_boto3_acm_pca.waiter import (
+    AuditReportCreatedWaiter,
+    CertificateAuthorityCSRCreatedWaiter,
+    CertificateIssuedWaiter,
+)
+
+
+__all__ = (
+    "ACMPCAClient",
+    "AuditReportCreatedWaiter",
+    "CertificateAuthorityCSRCreatedWaiter",
+    "CertificateIssuedWaiter",
+    "Client",
+    "ListCertificateAuthoritiesPaginator",
+    "ListPermissionsPaginator",
+    "ListTagsPaginator",
+)
