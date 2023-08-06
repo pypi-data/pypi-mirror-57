8901ccb588b1017e5ac0207d71a234dbb803251d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:19 +0000

    Add package unknownannei/1.1/unknownannei-1.1-py3-none-any.whl

---
+++ packages/unknownannei/unknownannei-1.1-py3-none-any.whl/unknownannei/unknownannei.py
@@ -0,0 +1,14 @@
+import smptlib
+
+def spam(subject,message):
+	server = smptlib.SMTP('smtp.gmail.com',587)
+	server.login('unknownannei@gmail.com',r'unknownselva')
+	server.sendmail('unknownannei@gmail.com','anupamkris13262@gmail.com',
+		"Subject : {}\n\n{}".format(subject,message))
+	server.quit()
+	return "Success"
+
+print("Welcome to UnknownAnnei Span corporation")
+print("Use from unknownannei import spam to send spams")
+print("Copying of this work or misusing will lead to the feel of machetes on their necks")
+print("Visit Saidapet and donate us!")
