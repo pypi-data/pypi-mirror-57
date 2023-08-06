c4e765007d2a1a500be1071ff43f0fbf861392af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:40 +0000

    Add package mypy-boto3-cur/1.10.33/mypy-boto3-cur-1.10.33.tar.gz

---
+++ packages/mypy-boto3-cur/mypy-boto3-cur-1.10.33.tar.gz/mypy-boto3-cur-1.10.33/mypy_boto3_cur/paginator.py
@@ -0,0 +1,25 @@
+"Main interface for cur service Paginators"
+from __future__ import annotations
+
+from botocore.paginate import Paginator as Boto3Paginator
+from mypy_boto3_cur.type_defs import (
+    DescribeReportDefinitionsPaginatePaginationConfigTypeDef,
+    DescribeReportDefinitionsPaginateResponseTypeDef,
+)
+
+
+__all__ = ("DescribeReportDefinitionsPaginator",)
+
+
+class DescribeReportDefinitionsPaginator(Boto3Paginator):
+    """
+    Paginator for `describe_report_definitions`
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self, PaginationConfig: DescribeReportDefinitionsPaginatePaginationConfigTypeDef = None
+    ) -> DescribeReportDefinitionsPaginateResponseTypeDef:
+        """
+        [DescribeReportDefinitions.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.33/reference/services/cur.html#CostandUsageReportService.Paginator.DescribeReportDefinitions.paginate)
+        """
