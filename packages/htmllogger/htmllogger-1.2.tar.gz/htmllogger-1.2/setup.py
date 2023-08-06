0d31c01bfe67b249143895fdaf556442a738a8e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:25 +0000

    Add package htmllogger/1.2/htmllogger-1.2.tar.gz

---
+++ packages/htmllogger/htmllogger-1.2.tar.gz/htmllogger-1.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, Extension ,find_packages
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name="htmllogger",
+    version="1.2",
+    author = "Prashant Pawar",
+    author_email="prashantpawar7090@gmail.com",
+    description="Python library provides interactive test report for selenium",
+    license="MIT",
+    keywords="selenium, page object model, pom, pages, page factory, htmllogger, html report",
+    install_requires=['selenium'],
+    include_package_data=True,
+    url="https://github.com/PrashantP9070/htmllogger",
+    packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
