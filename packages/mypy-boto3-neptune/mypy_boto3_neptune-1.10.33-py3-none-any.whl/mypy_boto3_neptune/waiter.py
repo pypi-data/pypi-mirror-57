7a63bff25ca26902486fc702b2ca6f81662426f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:06 +0000

    Add package mypy-boto3-neptune/1.10.33/mypy_boto3_neptune-1.10.33-py3-none-any.whl

---
+++ packages/mypy-boto3-neptune/mypy_boto3_neptune-1.10.33-py3-none-any.whl/mypy_boto3_neptune/waiter.py
@@ -0,0 +1,52 @@
+"Main interface for neptune service Waiters"
+from __future__ import annotations
+
+from typing import List
+from botocore.waiter import Waiter as Boto3Waiter
+from mypy_boto3_neptune.type_defs import (
+    DbInstanceAvailableWaitFiltersTypeDef,
+    DbInstanceAvailableWaitWaiterConfigTypeDef,
+    DbInstanceDeletedWaitFiltersTypeDef,
+    DbInstanceDeletedWaitWaiterConfigTypeDef,
+)
+
+
+__all__ = ("DBInstanceAvailableWaiter", "DBInstanceDeletedWaiter")
+
+
+class DBInstanceAvailableWaiter(Boto3Waiter):
+    """
+    Waiter for `db_instance_available` name.
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def wait(
+        self,
+        DBInstanceIdentifier: str = None,
+        Filters: List[DbInstanceAvailableWaitFiltersTypeDef] = None,
+        MaxRecords: int = None,
+        Marker: str = None,
+        WaiterConfig: DbInstanceAvailableWaitWaiterConfigTypeDef = None,
+    ) -> None:
+        """
+        [db_instance_available.wait documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.33/reference/services/neptune.html#Neptune.Waiter.db_instance_available.wait)
+        """
+
+
+class DBInstanceDeletedWaiter(Boto3Waiter):
+    """
+    Waiter for `db_instance_deleted` name.
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def wait(
+        self,
+        DBInstanceIdentifier: str = None,
+        Filters: List[DbInstanceDeletedWaitFiltersTypeDef] = None,
+        MaxRecords: int = None,
+        Marker: str = None,
+        WaiterConfig: DbInstanceDeletedWaitWaiterConfigTypeDef = None,
+    ) -> None:
+        """
+        [db_instance_deleted.wait documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.33/reference/services/neptune.html#Neptune.Waiter.db_instance_deleted.wait)
+        """
