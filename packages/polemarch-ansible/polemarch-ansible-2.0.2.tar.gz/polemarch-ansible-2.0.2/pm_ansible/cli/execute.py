fe060555f759f6665790f7ce19343aee6f5b439b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:43 +0000

    Add package polemarch-ansible/2.0.2/polemarch-ansible-2.0.2.tar.gz

---
+++ packages/polemarch-ansible/polemarch-ansible-2.0.2.tar.gz/polemarch-ansible-2.0.2/pm_ansible/cli/execute.py
@@ -0,0 +1,44 @@
+from __future__ import print_function, unicode_literals
+import os
+import sys
+import signal
+import subprocess
+from .base import get_parser
+
+
+ON_POSIX = 'posix' in sys.builtin_module_names
+
+
+def get_ansible_command(command):
+    python_exec_dir = os.path.dirname(sys.executable)
+    new_command = '{}/{}'.format(python_exec_dir, command)
+    if os.path.exists(new_command):
+        command = new_command
+    return command
+
+
+def handler(args=sys.argv[1:], parser=get_parser()):
+    command, arguments = get_ansible_command(args[0]), args[1:]
+    os.environ.setdefault('ANSIBLE_FORCE_COLOR', 'true')
+    process = subprocess.Popen(
+        [command] + arguments,
+        stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
+        bufsize=0, env=os.environ.copy(), close_fds=ON_POSIX,
+    )
+    os.kill(process.pid, signal.SIG_DFL)
+    rc = None
+    while True:
+        output = process.stdout.read(1)
+        if rc is not None:
+            break
+        if output:
+            try:
+                output = output.decode('utf-8')
+            except:  # nocv
+                continue
+            print(output, end='')
+            sys.stdout.flush()
+        rc = process.poll()
+
+    process.stdout.close()
+    sys.exit(rc) if rc else None
