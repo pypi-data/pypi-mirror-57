47cc208eec92ff8281d4dc13beb60c1c408cd3a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:43 +0000

    Add package aws-lambda-deployment-test-utils/0.0.13/aws_lambda_deployment_test_utils-0.0.13.tar.gz

---
+++ packages/aws-lambda-deployment-test-utils/aws_lambda_deployment_test_utils-0.0.13.tar.gz/aws_lambda_deployment_test_utils-0.0.13/setup.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python
+import os
+
+from setuptools import setup
+
+PROJECT_ROOT, _ = os.path.split(__file__)
+REVISION = "0.0.13"
+PROJECT_NAME = "aws_lambda_deployment_test_utils"
+PROJECT_AUTHORS = "Salim Fadhley"
+PROJECT_EMAILS = "salimfadhley@ecs.co.uk"
+PROJECT_URL = "https://bitbucket.org/salimfadhley_ecs/aws_lambda_deployment_test_utils/src/master/"
+SHORT_DESCRIPTION = "A package of tools which help developers make pre and post integration tests for AWS Lambda functions."
+
+try:
+    DESCRIPTION = open(os.path.join(PROJECT_ROOT, "README.md")).read()
+except IOError:
+    DESCRIPTION = SHORT_DESCRIPTION
+
+try:
+    REQUIREMENTS = list(open("requirements.txt").read().splitlines())
+except IOError:
+    REQUIREMENTS = []
+
+
+try:
+    DEV_REQUIREMENTS = list(open("dev_requirements.txt").read().splitlines())
+except IOError:
+    DEV_REQUIREMENTS = []
+
+setup(
+    name=PROJECT_NAME.lower(),
+    version=REVISION,
+    author=PROJECT_AUTHORS,
+    author_email=PROJECT_EMAILS,
+    packages=["lambda_deployment_test_utils"],
+    zip_safe=True,
+    include_package_data=False,
+    install_requires=REQUIREMENTS,
+    tests_require=DEV_REQUIREMENTS,
+    url=PROJECT_URL,
+    description=SHORT_DESCRIPTION,
+    long_description=DESCRIPTION,
+    license="GPL4",
+)
