423f05f67dfcc9639ca92973fe724b7414f3a01f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:15 +0000

    Add package mypy-boto3-accessanalyzer/1.10.36.1/mypy-boto3-accessanalyzer-1.10.36.1.tar.gz

---
+++ packages/mypy-boto3-accessanalyzer/mypy-boto3-accessanalyzer-1.10.36.1.tar.gz/mypy-boto3-accessanalyzer-1.10.36.1/mypy_boto3_accessanalyzer/__init__.py
@@ -0,0 +1,6 @@
+"Main interface for accessanalyzer service"
+
+from mypy_boto3_accessanalyzer.client import AccessAnalyzerClient as Client, AccessAnalyzerClient
+
+
+__all__ = ("AccessAnalyzerClient", "Client")
