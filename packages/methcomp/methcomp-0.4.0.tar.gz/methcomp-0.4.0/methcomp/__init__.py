b5f8cdfdbcfe6caf69e3b47ca89de98d20e8866a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:00 +0000

    Add package methcomp/0.4.0/methcomp-0.4.0.tar.gz

---
+++ packages/methcomp/methcomp-0.4.0.tar.gz/methcomp-0.4.0/methcomp/__init__.py
@@ -0,0 +1,11 @@
+# Capture the original matplotlib rcParams
+import matplotlib as mpl
+
+_orig_rc_params = mpl.rcParams.copy()
+
+# Import functions from all modules
+from .blandaltman import *
+from .regression import *
+
+# Define version
+__version__ = "0.4.0"
