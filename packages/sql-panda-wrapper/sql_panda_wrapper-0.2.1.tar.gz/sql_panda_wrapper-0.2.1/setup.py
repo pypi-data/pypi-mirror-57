d0f830c7405c72e13ddb8cea6d040c146382403e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:40 +0000

    Add package sql-panda-wrapper/0.2.1/sql_panda_wrapper-0.2.1.tar.gz

---
+++ packages/sql-panda-wrapper/sql_panda_wrapper-0.2.1.tar.gz/sql_panda_wrapper-0.2.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+import setuptools
+
+setup(name='sql_panda_wrapper',
+      version='0.2.1',
+      description='Panda wrapper for SQLite',
+      url='https://github.com/nsragow/SQLPanda',
+      author='Noah Sragow',
+      author_email='nsragow@gmail.com',
+      license='MIT',
+      packages=setuptools.find_packages(),
+      zip_safe=False)
