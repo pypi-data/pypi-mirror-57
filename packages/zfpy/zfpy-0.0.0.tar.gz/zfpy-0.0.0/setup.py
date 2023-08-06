a0db809ab37d65edfe0531c2c4868a0aaa8bf58b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:01 +0000

    Add package zfpy/0.0.0/zfpy-0.0.0.tar.gz

---
+++ packages/zfpy/zfpy-0.0.0.tar.gz/zfpy-0.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='zfpy',
+    author='Ouroboros Chrysopoeia',
+    author_email='impredicative@users.nomail.github.com',
+    version='0.0.0',
+    packages=find_packages(),
+)
