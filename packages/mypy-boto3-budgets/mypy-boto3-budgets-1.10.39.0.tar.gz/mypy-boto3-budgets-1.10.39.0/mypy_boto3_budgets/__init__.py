beedfa4f34a222defd8fb51737bd6d0a6a40add3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:13 +0000

    Add package mypy-boto3-budgets/1.10.39.0/mypy-boto3-budgets-1.10.39.0.tar.gz

---
+++ packages/mypy-boto3-budgets/mypy-boto3-budgets-1.10.39.0.tar.gz/mypy-boto3-budgets-1.10.39.0/mypy_boto3_budgets/__init__.py
@@ -0,0 +1,17 @@
+"Main interface for budgets service"
+
+from mypy_boto3_budgets.client import BudgetsClient, BudgetsClient as Client
+from mypy_boto3_budgets.paginator import (
+    DescribeBudgetsPaginator,
+    DescribeNotificationsForBudgetPaginator,
+    DescribeSubscribersForNotificationPaginator,
+)
+
+
+__all__ = (
+    "BudgetsClient",
+    "Client",
+    "DescribeBudgetsPaginator",
+    "DescribeNotificationsForBudgetPaginator",
+    "DescribeSubscribersForNotificationPaginator",
+)
