97df3961bea68a626fbfb2b97475e294c01bf3b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:08 +0000

    Add package lymask/0.1.2/lymask-0.1.2.tar.gz

---
+++ packages/lymask/lymask-0.1.2.tar.gz/lymask-0.1.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+import os
+
+
+def readme():
+    with open('README.md', 'r') as fx:
+      return fx.read()
+
+
+setup(name='lymask',
+      version='0.1.2',
+      description='Mask dataprep with klayout',
+      long_description=readme(),
+      author='Alex Tait, Adam McCaughan, Sonia Buckley, Jeff Chiles, Jeff Shainline, Rich Mirin, Sae Woo Nam',
+      author_email='alexander.tait@nist.gov',
+      license='MIT',
+      packages=['lymask'],
+      install_requires=['klayout', 'lygadgets', 'PyYAML'],
+      entry_points={'console_scripts': ['lymask=lymask.command_line:cm_main']},
+      # package_data={'': ['*.lym']},
+      include_package_data=True,
+      cmdclass={},
+      )
