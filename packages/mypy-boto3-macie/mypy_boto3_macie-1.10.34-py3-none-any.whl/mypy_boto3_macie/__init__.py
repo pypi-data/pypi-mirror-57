7703352909b7c686ff594921127232fd3c1e0ffe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:59 +0000

    Add package mypy-boto3-macie/1.10.34/mypy_boto3_macie-1.10.34-py3-none-any.whl

---
+++ packages/mypy-boto3-macie/mypy_boto3_macie-1.10.34-py3-none-any.whl/mypy_boto3_macie/__init__.py
@@ -0,0 +1,7 @@
+"Main interface for macie service"
+
+from mypy_boto3_macie.client import Client
+from mypy_boto3_macie.paginator import ListMemberAccountsPaginator, ListS3ResourcesPaginator
+
+
+__all__ = ("Client", "ListMemberAccountsPaginator", "ListS3ResourcesPaginator")
