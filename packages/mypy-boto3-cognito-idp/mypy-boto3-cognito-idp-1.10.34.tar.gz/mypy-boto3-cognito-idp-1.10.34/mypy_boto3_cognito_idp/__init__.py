da5fc15802538d37740a32712c88bcfca9bffb6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:44 +0000

    Add package mypy-boto3-cognito-idp/1.10.34/mypy-boto3-cognito-idp-1.10.34.tar.gz

---
+++ packages/mypy-boto3-cognito-idp/mypy-boto3-cognito-idp-1.10.34.tar.gz/mypy-boto3-cognito-idp-1.10.34/mypy_boto3_cognito_idp/__init__.py
@@ -0,0 +1,28 @@
+"Main interface for cognito-idp service"
+
+from mypy_boto3_cognito_idp.client import Client
+from mypy_boto3_cognito_idp.paginator import (
+    AdminListGroupsForUserPaginator,
+    AdminListUserAuthEventsPaginator,
+    ListGroupsPaginator,
+    ListIdentityProvidersPaginator,
+    ListResourceServersPaginator,
+    ListUserPoolClientsPaginator,
+    ListUserPoolsPaginator,
+    ListUsersInGroupPaginator,
+    ListUsersPaginator,
+)
+
+
+__all__ = (
+    "Client",
+    "AdminListGroupsForUserPaginator",
+    "AdminListUserAuthEventsPaginator",
+    "ListGroupsPaginator",
+    "ListIdentityProvidersPaginator",
+    "ListResourceServersPaginator",
+    "ListUserPoolClientsPaginator",
+    "ListUserPoolsPaginator",
+    "ListUsersPaginator",
+    "ListUsersInGroupPaginator",
+)
