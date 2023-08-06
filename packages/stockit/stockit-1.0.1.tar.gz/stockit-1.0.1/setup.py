484711fc7fe6652c5f5c9e71fde4e365bf3e1eba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:38 +0000

    Add package stockit/1.0.1/stockit-1.0.1.tar.gz

---
+++ packages/stockit/stockit-1.0.1.tar.gz/stockit-1.0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name='stockit',
+    url='https://github.com/BenCaunt8300/stockit',
+    download_url = 'https://github.com/BenCaunt8300/stockit/archive/v1.0.1.tar.gz',
+    author='Ben Caunt',
+    author_email='bdcaunt@gmail.com',
+    packages=['stockit'],
+    install_requires=['pandas','numpy','matplotlib','tqdm','sklearn'],
+    version='v1.0.1',
+    license='MIT',
+    description='python library for easy stock analysis and future price estimations. FOR EDUCATIONAL PURPOSES ONLY, NOT INVESTMENT ADVICE',
+    classifiers=[
+      'Development Status :: 3 - Alpha',
+      'Intended Audience :: Developers',
+      'Topic :: Software Development :: Build Tools',
+      'License :: OSI Approved :: MIT License',
+      'Programming Language :: Python :: 3',
+      'Programming Language :: Python :: 3.4',
+      'Programming Language :: Python :: 3.5',
+      'Programming Language :: Python :: 3.6',
+      'Programming Language :: Python :: 3.7',
+      'Programming Language :: Python :: 3.8',
+    ],
+
+)
+
