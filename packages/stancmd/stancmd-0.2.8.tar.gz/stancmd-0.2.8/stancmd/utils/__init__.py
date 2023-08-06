64892e94e367c7970f55bcc2f52a173d30fd5320
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:20 +0000

    Add package stancmd/0.2.8/stancmd-0.2.8.tar.gz

---
+++ packages/stancmd/stancmd-0.2.8.tar.gz/stancmd-0.2.8/stancmd/utils/__init__.py
@@ -0,0 +1,7 @@
+def bruno():
+    print('Va voir ce que dit le standard CPP17 : http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2017/n4713.pdf')
+
+
+def signal_handler(signum, frame):
+    print(f'[STAN] Désolé mon réveil était cassé...')
+    return
