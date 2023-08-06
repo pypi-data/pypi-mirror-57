380f6f181558acec340b73089977151382e71811
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:45 +0000

    Add package mypy-boto3-elb/1.10.33/mypy_boto3_elb-1.10.33-py3-none-any.whl

---
+++ packages/mypy-boto3-elb/mypy_boto3_elb-1.10.33-py3-none-any.whl/mypy_boto3_elb/paginator.py
@@ -0,0 +1,44 @@
+"Main interface for elb service Paginators"
+from __future__ import annotations
+
+from typing import List
+from botocore.paginate import Paginator as Boto3Paginator
+from mypy_boto3_elb.type_defs import (
+    DescribeAccountLimitsPaginatePaginationConfigTypeDef,
+    DescribeAccountLimitsPaginateResponseTypeDef,
+    DescribeLoadBalancersPaginatePaginationConfigTypeDef,
+    DescribeLoadBalancersPaginateResponseTypeDef,
+)
+
+
+__all__ = ("DescribeAccountLimitsPaginator", "DescribeLoadBalancersPaginator")
+
+
+class DescribeAccountLimitsPaginator(Boto3Paginator):
+    """
+    Paginator for `describe_account_limits`
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self, PaginationConfig: DescribeAccountLimitsPaginatePaginationConfigTypeDef = None
+    ) -> DescribeAccountLimitsPaginateResponseTypeDef:
+        """
+        [DescribeAccountLimits.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.33/reference/services/elb.html#ElasticLoadBalancing.Paginator.DescribeAccountLimits.paginate)
+        """
+
+
+class DescribeLoadBalancersPaginator(Boto3Paginator):
+    """
+    Paginator for `describe_load_balancers`
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self,
+        LoadBalancerNames: List[str] = None,
+        PaginationConfig: DescribeLoadBalancersPaginatePaginationConfigTypeDef = None,
+    ) -> DescribeLoadBalancersPaginateResponseTypeDef:
+        """
+        [DescribeLoadBalancers.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.33/reference/services/elb.html#ElasticLoadBalancing.Paginator.DescribeLoadBalancers.paginate)
+        """
