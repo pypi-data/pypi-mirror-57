35bb9d96271e3d9c703e6c61dfc258f497d3cf19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:14 +0000

    Add package telegram-interface-cli/0.1.5/telegram-interface-cli-0.1.5.tar.gz

---
+++ packages/telegram-interface-cli/telegram-interface-cli-0.1.5.tar.gz/telegram-interface-cli-0.1.5/telegram_interface_cli/__init__.py
@@ -0,0 +1,8 @@
+
+from .__name__ import NAME
+from .__version__ import VERSION
+
+from .exceptions import TelegramInterfaceCLIException
+from .logger import TelegramInterfaceCLILogger
+from .config import TelegramInterfaceCLIConfig
+from .telegram_interface_cli import TelegramInterfaceCLI
