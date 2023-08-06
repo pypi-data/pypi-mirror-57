eb447f5582c5a824006cf5cb2ac0601243f9826d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:31 +0000

    Add package OttoDiff/1.0.0/OttoDiff-1.0.0.tar.gz

---
+++ packages/OttoDiff/OttoDiff-1.0.0.tar.gz/OttoDiff-1.0.0/OttoDiff/visualization.py
@@ -0,0 +1,26 @@
+from graphviz import Digraph
+from reverse import *
+
+def createGraph(root, computationalGraph):
+    # use dfs to traverse the computational graph
+    if len(root.parents) == 0:
+        return
+
+    for par in root.parents:
+        computationalGraph.edge(str(par), str(root))
+        createGraph(par, computationalGraph)
+
+def visualize(final_output_node):
+    computationalGraph = Digraph()
+    createGraph(final_output_node, computationalGraph)
+    computationalGraph.view()
+
+
+# x = VariableNode(3)
+# y = VariableNode(5)
+# z = VariableNode(7)
+# # build the tree
+# v = x / 2 + 2 / x + 2 * y + z
+# print(find_df_dx(f=v, x=x))
+#
+# visualize(v)
