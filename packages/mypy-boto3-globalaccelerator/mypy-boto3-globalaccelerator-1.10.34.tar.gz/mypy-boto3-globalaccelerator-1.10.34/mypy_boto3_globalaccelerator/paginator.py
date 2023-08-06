e2f27036580b87094809bce8aa0920f1f32cc1b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:54 +0000

    Add package mypy-boto3-globalaccelerator/1.10.34/mypy-boto3-globalaccelerator-1.10.34.tar.gz

---
+++ packages/mypy-boto3-globalaccelerator/mypy-boto3-globalaccelerator-1.10.34.tar.gz/mypy-boto3-globalaccelerator-1.10.34/mypy_boto3_globalaccelerator/paginator.py
@@ -0,0 +1,61 @@
+"Main interface for globalaccelerator service Paginators"
+from __future__ import annotations
+
+from botocore.paginate import Paginator as Boto3Paginator
+from mypy_boto3_globalaccelerator.type_defs import (
+    ListAcceleratorsPaginatePaginationConfigTypeDef,
+    ListAcceleratorsPaginateResponseTypeDef,
+    ListEndpointGroupsPaginatePaginationConfigTypeDef,
+    ListEndpointGroupsPaginateResponseTypeDef,
+    ListListenersPaginatePaginationConfigTypeDef,
+    ListListenersPaginateResponseTypeDef,
+)
+
+
+__all__ = ("ListAcceleratorsPaginator", "ListEndpointGroupsPaginator", "ListListenersPaginator")
+
+
+class ListAcceleratorsPaginator(Boto3Paginator):
+    """
+    Paginator for `list_accelerators`
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self, PaginationConfig: ListAcceleratorsPaginatePaginationConfigTypeDef = None
+    ) -> ListAcceleratorsPaginateResponseTypeDef:
+        """
+        [ListAccelerators.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.34/reference/services/globalaccelerator.html#GlobalAccelerator.Paginator.ListAccelerators.paginate)
+        """
+
+
+class ListEndpointGroupsPaginator(Boto3Paginator):
+    """
+    Paginator for `list_endpoint_groups`
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self,
+        ListenerArn: str,
+        PaginationConfig: ListEndpointGroupsPaginatePaginationConfigTypeDef = None,
+    ) -> ListEndpointGroupsPaginateResponseTypeDef:
+        """
+        [ListEndpointGroups.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.34/reference/services/globalaccelerator.html#GlobalAccelerator.Paginator.ListEndpointGroups.paginate)
+        """
+
+
+class ListListenersPaginator(Boto3Paginator):
+    """
+    Paginator for `list_listeners`
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self,
+        AcceleratorArn: str,
+        PaginationConfig: ListListenersPaginatePaginationConfigTypeDef = None,
+    ) -> ListListenersPaginateResponseTypeDef:
+        """
+        [ListListeners.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.34/reference/services/globalaccelerator.html#GlobalAccelerator.Paginator.ListListeners.paginate)
+        """
