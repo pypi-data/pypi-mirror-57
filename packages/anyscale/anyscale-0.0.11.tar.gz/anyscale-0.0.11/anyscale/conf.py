2a591fe6061b4a6db5657ceed0f8bfc3eee1a520
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:12 +0000

    Add package anyscale/0.0.11/anyscale-0.0.11.tar.gz

---
+++ packages/anyscale/anyscale-0.0.11.tar.gz/anyscale-0.0.11/anyscale/conf.py
@@ -0,0 +1,7 @@
+AWS_PROFILE = None
+
+# TODO(pcm): This needs to be changed to a more production like URL.
+APPLICATION_URL = "http://34.220.43.10:5000"
+
+# Global variable that contains the server session token.
+CLI_TOKEN = None
