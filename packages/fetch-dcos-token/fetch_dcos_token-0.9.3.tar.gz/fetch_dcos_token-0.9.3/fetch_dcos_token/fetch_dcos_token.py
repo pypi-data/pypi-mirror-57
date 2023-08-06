de754841ab878db0869dfc5b71bfd4f6d0da9ad1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:13 +0000

    Add package fetch-dcos-token/0.9.3/fetch_dcos_token-0.9.3.tar.gz

---
+++ packages/fetch-dcos-token/fetch_dcos_token-0.9.3.tar.gz/fetch_dcos_token-0.9.3/fetch_dcos_token/fetch_dcos_token.py
@@ -0,0 +1,28 @@
+import json
+import requests
+import datetime
+import jwt
+import warnings
+warnings.filterwarnings("ignore")
+
+
+def tokenfetcher(cluster, username, userkey):
+    keyfile = open(userkey, "r")
+    private_key = keyfile.read()
+    thetoken = jwt.encode(
+        {"uid": username, "exp": datetime.datetime.utcnow() + datetime.timedelta(seconds=3600)},
+        private_key, algorithm='RS256'
+    )
+    logintoken = thetoken.decode("utf-8")
+    headers = {'Content-Type': 'application/json'}
+    theusername = '"' + username + '"'
+    thelogintoken = '"' + logintoken + '"'
+    thepayload = '{"uid": ' + theusername + ', ' + '"token" :' + thelogintoken + '}'
+
+    payload = thepayload
+    api_endpoint = '/acs/api/v1/auth/login'
+    api = cluster + api_endpoint
+
+    tokenreturn = requests.post(url=api, headers=headers, data=payload, verify=False)
+    token_dict = json.loads(tokenreturn.text)
+    return token_dict['token']
