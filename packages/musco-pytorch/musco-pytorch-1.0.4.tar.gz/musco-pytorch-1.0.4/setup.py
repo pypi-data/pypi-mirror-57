b700acfcc81563fc0d51db2ccbed78d782c10c6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:02 +0000

    Add package musco-pytorch/1.0.4/musco-pytorch-1.0.4.tar.gz

---
+++ packages/musco-pytorch/musco-pytorch-1.0.4.tar.gz/musco-pytorch-1.0.4/setup.py
@@ -0,0 +1,27 @@
+import subprocess
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+
+try:
+    from pip._internal.req import parse_requirements
+except ImportError:
+    from pip.req import parse_requirements
+
+
+def load_requirements(file_name):
+    requirements = parse_requirements(file_name, session="test")
+    return [str(item.req) for item in requirements]
+
+
+setup(
+    name="musco-pytorch",
+    version="1.0.4",
+    description="MUSCO: Multi-Stage COmpression of Neural Networks",
+    author="Julia Gusak, Maksym Kholiavchenko, Evgeny Ponomarev, Larisa Markeeva, Andrzej Cichocki, Ivan Oseledets",
+    author_email="julgusak@gmail.com",
+    url="https://github.com/musco-ai/musco-pytorch",
+    download_url="https://github.com/musco-ai/musco-pytorch/archive/1.0.4.tar.gz",
+    license="Apache-2.0",
+    packages=find_packages(),
+    install_requires=load_requirements("requirements.txt")
+)
