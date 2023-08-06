a38ae7e5a6519ecf14a0188417ce061f8a19408d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:34 +0000

    Add package mypy-boto3-cloudformation/1.10.34.1/mypy-boto3-cloudformation-1.10.34.1.tar.gz

---
+++ packages/mypy-boto3-cloudformation/mypy-boto3-cloudformation-1.10.34.1.tar.gz/mypy-boto3-cloudformation-1.10.34.1/mypy_boto3_cloudformation/__init__.py
@@ -0,0 +1,54 @@
+"Main interface for cloudformation service"
+
+from mypy_boto3_cloudformation.client import Client
+from mypy_boto3_cloudformation.paginator import (
+    DescribeAccountLimitsPaginator,
+    DescribeChangeSetPaginator,
+    DescribeStackEventsPaginator,
+    DescribeStacksPaginator,
+    ListChangeSetsPaginator,
+    ListExportsPaginator,
+    ListImportsPaginator,
+    ListStackInstancesPaginator,
+    ListStackResourcesPaginator,
+    ListStackSetOperationResultsPaginator,
+    ListStackSetOperationsPaginator,
+    ListStackSetsPaginator,
+    ListStacksPaginator,
+)
+from mypy_boto3_cloudformation.service_resource import ServiceResource
+from mypy_boto3_cloudformation.waiter import (
+    ChangeSetCreateCompleteWaiter,
+    StackCreateCompleteWaiter,
+    StackDeleteCompleteWaiter,
+    StackExistsWaiter,
+    StackImportCompleteWaiter,
+    StackUpdateCompleteWaiter,
+    TypeRegistrationCompleteWaiter,
+)
+
+
+__all__ = (
+    "Client",
+    "ServiceResource",
+    "ChangeSetCreateCompleteWaiter",
+    "StackCreateCompleteWaiter",
+    "StackDeleteCompleteWaiter",
+    "StackExistsWaiter",
+    "StackImportCompleteWaiter",
+    "StackUpdateCompleteWaiter",
+    "TypeRegistrationCompleteWaiter",
+    "DescribeAccountLimitsPaginator",
+    "DescribeChangeSetPaginator",
+    "DescribeStackEventsPaginator",
+    "DescribeStacksPaginator",
+    "ListChangeSetsPaginator",
+    "ListExportsPaginator",
+    "ListImportsPaginator",
+    "ListStackInstancesPaginator",
+    "ListStackResourcesPaginator",
+    "ListStackSetOperationResultsPaginator",
+    "ListStackSetOperationsPaginator",
+    "ListStackSetsPaginator",
+    "ListStacksPaginator",
+)
