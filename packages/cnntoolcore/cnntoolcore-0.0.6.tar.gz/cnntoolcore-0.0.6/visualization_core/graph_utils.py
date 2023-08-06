a089b9818172a65efc7cde6e1969cc4e187370c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:34 +0000

    Add package cnntoolcore/0.0.6/cnntoolcore-0.0.6.tar.gz

---
+++ packages/cnntoolcore/cnntoolcore-0.0.6.tar.gz/cnntoolcore-0.0.6/visualization_core/graph_utils.py
@@ -0,0 +1,18 @@
+class GraphUtils():
+    @staticmethod
+    def flatten_function_nodes(function_node, function_node_list):
+        if function_node not in function_node_list:
+            function_node_list.append(function_node)
+        for node in function_node.child_nodes:
+            GraphUtils.flatten_function_nodes(node, function_node_list)
+
+    # Sample call where torch_model_loading is pytorch torch_model_loading
+    # module_list = []
+    # self.flatten_modules(mode l, module_list)
+    @staticmethod
+    def flatten_modules(module, module_list):
+        if len(module._modules) > 0:
+            for mod in module._modules:
+                GraphUtils.flatten_modules(module._modules[mod], module_list)
+        else:
+            module_list.append(module)
