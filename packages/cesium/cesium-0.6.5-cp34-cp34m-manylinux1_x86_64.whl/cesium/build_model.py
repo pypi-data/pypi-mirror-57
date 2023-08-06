604626cab93b59f8f4be10f0b9468a572ad3a307
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:17 +0000

    Add package cesium/0.6.5/cesium-0.6.5-cp34-cp34m-manylinux1_x86_64.whl

---
+++ packages/cesium/cesium-0.6.5-cp34-cp34m-manylinux1_x86_64.whl/cesium/build_model.py
@@ -0,0 +1,98 @@
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+from sklearn.linear_model import LinearRegression, SGDClassifier,\
+    RidgeClassifierCV, ARDRegression, BayesianRidge
+from sklearn import grid_search
+
+
+__all__ = ['MODELS_TYPE_DICT', 'rectangularize_featureset',
+           'fit_model_optimize_hyperparams', 'build_model_from_featureset']
+
+
+MODELS_TYPE_DICT = {'RandomForestClassifier': RandomForestClassifier,
+                    'RandomForestRegressor': RandomForestRegressor,
+                    'LinearSGDClassifier': SGDClassifier,
+                    'LinearRegressor': LinearRegression,
+                    'RidgeClassifierCV': RidgeClassifierCV,
+                    'BayesianARDRegressor': ARDRegression,
+                    'BayesianRidgeRegressor': BayesianRidge}
+
+
+def rectangularize_featureset(featureset):
+    """Convert xarray.Dataset into (2d) Pandas.DataFrame for use with sklearn."""
+    featureset = featureset.drop([coord for coord in featureset.coords
+                                  if coord not in ['name', 'channel']])
+    feature_df = featureset.to_dataframe()
+    if 'channel' in featureset:
+        feature_df = feature_df.unstack(level='channel')
+        if len(featureset.channel) == 1:
+            feature_df.columns = [pair[0] for pair in feature_df.columns]
+        else:
+            feature_df.columns = ['_'.join([str(el) for el in pair])
+                                  for pair in feature_df.columns]
+    # sort columns by name for consistent ordering
+    feature_df = feature_df[sorted(feature_df.columns)]
+    return feature_df.loc[featureset.name]  # preserve original row ordering
+
+
+def fit_model_optimize_hyperparams(data, targets, model, params_to_optimize,
+                                   cv=None):
+    """Optimize estimator hyperparameters.
+
+    Perform hyperparamter optimization using
+    `sklearn.grid_search.GridSearchCV`.
+
+    Parameters
+    ----------
+    data : Pandas.DataFrame
+        Features for training model.
+    targets : Pandas.Series
+        Targets corresponding to feature vectors in `data`.
+    model : sklearn estimator object
+        The model/estimator whose hyperparameters are to be optimized.
+    params_to_optimize : dict or list of dict
+        Dictionary with parameter names as keys and lists of values to try
+        as values, or a list of such dictionaries.
+    cv : int, cross-validation generator or an iterable, optional
+        Number of folds (defaults to 3) or an iterable yielding train/test
+        splits. See documentation for `GridSearchCV` for details.
+
+    Returns
+    -------
+    `sklearn.grid_search.GridSearchCV` estimator object
+
+    """
+    optimized_model = grid_search.GridSearchCV(model, params_to_optimize, cv=cv)
+    optimized_model.fit(data, targets)
+    return optimized_model
+
+
+def build_model_from_featureset(featureset, model=None, model_type=None,
+                                model_options={}, params_to_optimize=None,
+                                cv=None):
+    """Build model from (non-rectangular) xarray.Dataset of features."""
+    if featureset.get('target') is None:
+        raise ValueError("Cannot build model for unlabeled feature set.")
+
+    if model is None:
+        if model_type:
+            model = MODELS_TYPE_DICT[model_type](**model_options)
+        else:
+            raise ValueError("If model is None, model_type must be specified")
+    feature_df = rectangularize_featureset(featureset)
+    try:
+        if params_to_optimize:
+            model = fit_model_optimize_hyperparams(feature_df, featureset['target'],
+                                                   model, params_to_optimize, cv)
+        else:
+            model.fit(feature_df, featureset['target'])
+    except ValueError as e:
+        nan_feats = np.isnan(feature_df).any()
+        inf_feats = np.isinf(feature_df).any()
+        message = "Invalid feature values detected:"
+        if nan_feats.any():
+            message += " NaN in {}.".format(", ".join(feature_df.columns[nan_feats]))
+        if inf_feats.any():
+            message += " Inf in {}.".format(", ".join(feature_df.columns[inf_feats]))
+        raise ValueError(message)
+    return model
---
+++ packages/cesium/cesium-0.6.5-cp34-cp34m-manylinux1_x86_64.whl/cesium/featurize.py
@@ -0,0 +1,351 @@
+import copy
+import os
+from collections import Iterable
+import numpy as np
+import pandas as pd
+import xarray as xr
+import dask.async
+import dask.multiprocessing
+from dask import delayed
+
+from . import data_management
+from . import time_series
+from . import util
+from .featureset import Featureset
+from .time_series import TimeSeries
+from . import obs_feature_tools as oft
+from . import science_feature_tools as sft
+
+__all__ = ['load_and_store_feature_data', 'featurize_time_series',
+           'featurize_single_ts', 'assemble_featureset']
+
+
+def featurize_single_ts(ts, features_to_use, custom_script_path=None,
+                        custom_functions=None):
+    """Compute feature values for a given single time-series. Data is
+    returned as dictionaries/lists of lists.
+
+    Parameters
+    ----------
+    ts : TimeSeries object
+        Single time series to be featurized.
+    features_to_use : list of str
+        List of feature names to be generated.
+    custom_functions : dict, optional
+        Dictionary of custom feature functions to be evaluated for the given
+        time series, or a dictionary representing a dask graph of function
+        evaluations. Dictionaries of functions should have keys `feature_name`
+        and values functions that take arguments (t, m, e); in the case of a
+        dask graph, these arrays should be referenced as 't', 'm', 'e',
+        respectively, and any values with keys present in `features_to_use`
+        will be computed.
+
+    Returns
+    -------
+    dict
+        Dictionary with feature names as keys, lists of feature values (one per
+        channel) as values.
+    """
+    all_feature_lists = {feature: [0.] * ts.n_channels
+                         for feature in features_to_use}
+    for (t_i, m_i, e_i), i in zip(ts.channels(), range(ts.n_channels)):
+        obs_features = oft.generate_obs_features(t_i, m_i, e_i,
+                                                 features_to_use)
+        science_features = sft.generate_science_features(t_i, m_i, e_i,
+                                                         features_to_use)
+        if custom_functions:
+            # If all values in custom_functions are functions, evaluate each
+            if all(hasattr(v, '__call__') for v in custom_functions.values()):
+                custom_features = {feature: f(t_i, m_i, e_i)
+                                   for feature, f in custom_functions.items()
+                                   if feature in features_to_use}
+            # Otherwise, custom_functions is a dask graph
+            else:
+                dask_graph = {key: value
+                              for key, value in custom_functions.items()
+                              if key in features_to_use}
+                dask_keys = list(dask_graph.keys())
+                dask_graph['t'] = t_i
+                dask_graph['m'] = m_i
+                dask_graph['e'] = e_i
+                dask_graph.update(dict(list(obs_features.items()) +
+                                       list(science_features.items()) +
+                                       list(ts.meta_features.items())))
+                custom_features = dict(zip(dask_keys,
+                                           dask.async.get_sync(dask_graph,
+                                                               dask_keys)))
+        else:
+            custom_features = {}
+
+        # We set values in this order so that custom features take priority
+        # over cesium features in the case of name conflicts
+        for feature, value in (list(obs_features.items()) +
+                               list(science_features.items()) +
+                               list(custom_features.items())):
+            all_feature_lists[feature][i] = value
+
+    return all_feature_lists
+
+
+def assemble_featureset(feature_dicts, time_series=None, targets=None,
+                        meta_feature_dicts=None, names=None):
+    """Transforms raw feature data (as returned by `featurize_single_ts`) into
+    an xarray.Dataset.
+
+    Parameters
+    ----------
+    feature_dicts : list of dict
+        List of dicts (one per time series file) with feature names as keys and
+        lists of feature values (one per channel) as values.
+    time_series : list of TimeSeries
+        If provided, the target, name, and metafeatures from the time series
+        objects will be used, overriding the `targets`, `meta_feature_dicts`,
+        and `names` values.
+    targets : list or pandas.Series, optional
+        If provided, the `target` coordinate of the featureset xarray.Dataset
+        will be set accordingly.
+    meta_feature_dicts : list of dict
+        If provided, the columns of `metadata` will be added as data variables
+        to the featureset xarray.Dataset.
+    names : list of str
+        If provided, the `name` coordinate of the featureset xarray.Dataset
+        will be set accordingly.
+
+    Returns
+    -------
+    xarray.Dataset
+        Featureset with `data_vars` containing feature values, and `coords`
+        containing names and targets (if applicable).
+    """
+    feature_names = feature_dicts[0].keys() if len(feature_dicts) > 0 else []
+    combined_feature_dict = {feature: (['name', 'channel'],
+                                       [d[feature] for d in feature_dicts])
+                             for feature in feature_names}
+
+    if time_series is not None:
+        targets, meta_feature_dicts, names = zip(*[(ts.target,
+                                                    ts.meta_features, ts.name)
+                                                   for ts in time_series])
+
+
+    if meta_feature_dicts is not None:
+        meta_feature_names = meta_feature_dicts[0].keys()
+        combined_feature_dict.update({feature: (['name'], [d[feature] for d in
+                                                           meta_feature_dicts])
+                                      for feature in meta_feature_names})
+    featureset = xr.Dataset(combined_feature_dict)
+    if names is not None:
+        featureset.coords['name'] = ('name', np.array(names))
+    if targets is not None and any(targets):
+        featureset.coords['target'] = ('name', np.array(targets))
+    return Featureset(featureset)
+
+
+def load_and_store_feature_data(features_path, output_path):
+    """Read features from CSV file and save as an xarray.Dataset."""
+    targets, meta_features = data_management.parse_headerfile(features_path)
+    meta_feature_dicts = meta_features.to_dict(orient='record')
+    featureset = assemble_featureset([], targets=targets,
+                                     meta_feature_dicts=meta_feature_dicts)
+    featureset.to_netcdf(output_path)
+    return Featureset(featureset)
+
+
+# TODO should this be changed to use TimeSeries objects? or maybe an optional
+# argument for TimeSeries? some redundancy here...
+def featurize_time_series(times, values, errors=None, features_to_use=[],
+                          targets=None, meta_features={}, labels=None,
+                          custom_script_path=None, custom_functions=None,
+                          scheduler=dask.multiprocessing.get):
+    """Versatile feature generation function for one or more time series.
+
+    For a single time series, inputs may have the form:
+
+    - `times`:  (n,) array or (p, n) array (for p channels of measurement)
+    - `values`: (n,) array or (p, n) array (for p channels of measurement)
+    - `errors`: (n,) array or (p, n) array (for p channels of measurement)
+
+    For multiple time series, inputs may have the form:
+
+    - `times`: list of (n,) arrays, list of (p, n) arrays (for p channels of
+      measurement), or list of lists of (n,) arrays (for
+      multichannel data with different time values per channel)
+    - `values`: list of (n,) arrays, list of (p, n) arrays (for p channels of
+      measurement), or list of lists of (n,) arrays (for
+      multichannel data with different time values per channel)
+    - `errors`: list of (n,) arrays, list of (p, n) arrays (for p channels of
+      measurement), or list of lists of (n,) arrays (for
+      multichannel data with different time values per channel)
+
+    In the case of multichannel measurements, each channel will be
+    featurized separately, and the data variables of the output
+    `xarray.Dataset` will be indexed by a `channel` coordinate.
+
+    Parameters
+    ----------
+    times : array, list of array, or list of lists of array
+        Array containing time values for a single time series, or a list of
+        arrays each containing time values for a single time series, or a list
+        of lists of arrays for multichannel data with different time values per
+        channel
+    values : array or list of array
+        Array containing measurement values for a single time series, or a list
+        of arrays each containing (possibly multivariate) measurement values
+        for a single time series, or a list of lists of arrays for multichannel
+        data with different time values per channel
+    errors : array or list/tuple of array, optional
+        Array containing measurement error values for a single time series, or
+        a list of arrays each containing (possibly multivariate) measurement
+        values for a single time series, or a list of lists of arrays for
+        multichannel data with different time values per channel
+    features_to_use : list of str, optional
+        List of feature names to be generated. Defaults to an empty list, which
+        will result in only meta_features features being stored.
+    targets : str/float or array-like, optional
+        Target or sequence of targets, one per time series (if applicable);
+        will be stored in the `target` coordinate of the resulting
+        `xarray.Dataset`.
+    meta_features : dict/Pandas.Series or list of dicts/Pandas.DataFrame
+        dict/Series (for a single time series) or DataFrame (for multiple time
+        series) of metafeature information; features are added to the output
+        featureset, and their values are consumable by custom feature scripts.
+    labels : str or list of str, optional
+        Label or list of labels for each time series, if applicable; will be
+        stored in the `name` coordinate of the resulting `xarray.Dataset`.
+    custom_script_path : str, optional
+        Path to Python script containing function definitions for the
+        generation of any custom features. Defaults to None.
+    custom_functions : dict, optional
+        Dictionary of custom feature functions to be evaluated for the given
+        time series, or a dictionary representing a dask graph of function
+        evaluations.  Dictionaries of functions should have keys `feature_name`
+        and values functions that take arguments (t, m, e); in the case of a
+        dask graph, these arrays should be referenced as 't', 'm', 'e',
+        respectively, and any values with keys present in `features_to_use`
+        will be computed.
+    scheduler : function, optional
+        `dask` scheduler function used to perform feature extraction
+        computation. Defaults to `dask.multiprocessing.get`.
+
+    Returns
+    -------
+    xarray.Dataset
+        Featureset with `data_vars` containing feature values and `coords`
+        containing labels (`name`) and targets (`target`), if applicable.
+    """
+    if times is None:
+        times = copy.deepcopy(values)
+        if isinstance(times, np.ndarray) and (times.ndim == 1
+                                              or 1 in times.shape):
+            times[:] = np.linspace(0., time_series.DEFAULT_MAX_TIME,
+                                   times.size)
+        else:
+            for t in times:
+                if isinstance(t, np.ndarray) and (t.ndim == 1 or 1 in t.shape):
+                    t[:] = np.linspace(0., time_series.DEFAULT_MAX_TIME,
+                                       t.size)
+                else:
+                    for t_i in t:
+                        t_i[:] = np.linspace(0., time_series.DEFAULT_MAX_TIME,
+                                             t_i.size)
+
+    if errors is None:
+        errors = copy.deepcopy(values)
+        if isinstance(errors, np.ndarray) and (errors.ndim == 1
+                                               or 1 in errors.shape):
+            errors[:] = time_series.DEFAULT_ERROR_VALUE
+        else:
+            for e in errors:
+                if isinstance(e, np.ndarray) and (e.ndim == 1 or 1 in e.shape):
+                    e[:] = time_series.DEFAULT_ERROR_VALUE
+                else:
+                    for e_i in e:
+                        e_i[:] = time_series.DEFAULT_ERROR_VALUE
+
+    # One single-channel time series:
+    if not isinstance(values[0], Iterable):
+        times, values, errors = [times], [values], [errors]
+    # One multi-channel time series:
+    elif isinstance(values, np.ndarray) and values.ndim == 2:
+        times, values, errors = [times], [values], [errors]
+
+    if labels is None:
+        labels = np.arange(len(times))
+
+    if targets is None:
+        targets = [None] * len(times)
+    targets = pd.Series(targets, index=labels)
+
+    if isinstance(meta_features, pd.Series):
+        meta_features = meta_features.to_dict()
+    meta_features = pd.DataFrame(meta_features, index=labels)
+
+    all_time_series = [delayed(TimeSeries(t, m, e, target=targets.loc[label],
+                                          meta_features=meta_features.loc[label],
+                                          name=label), pure=True)
+                       for t, m, e, label in zip(times, values, errors,
+                                                 labels)]
+
+    all_features = [delayed(featurize_single_ts, pure=True)(ts, features_to_use,
+                                                            custom_script_path,
+                                                            custom_functions)
+                    for ts in all_time_series]
+    result = delayed(assemble_featureset, pure=True)(all_features, all_time_series)
+    return result.compute(get=scheduler)
+
+
+def featurize_ts_files(ts_paths, features_to_use, output_path=None,
+                       custom_script_path=None, custom_functions=None,
+                       scheduler=dask.multiprocessing.get):
+    """Feature generation function for on-disk time series (NetCDF) files.
+
+    By default, computes features concurrently using the
+    `dask.multiprocessing.get` scheduler. Other possible options include
+    `dask.async.get_sync` for synchronous computation (e.g., when debugging),
+    or `dask.distributed.Executor.get` for distributed computation.
+
+    In the case of multichannel measurements, each channel will be
+    featurized separately, and the data variables of the output
+    `xarray.Dataset` will be indexed by a `channel` coordinate.
+
+    Parameters
+    ----------
+    ts_paths : list of str
+        List of paths to time series data, stored in NetCDF format. See
+        `time_series.from_netcdf` for details.
+    features_to_use : list of str, optional
+        List of feature names to be generated. Defaults to an empty list, which
+        will result in only meta_features features being stored.
+    custom_script_path : str, optional
+        Path to Python script containing function definitions for the
+        generation of any custom features. Defaults to None.
+    custom_functions : dict, optional
+        Dictionary of custom feature functions to be evaluated for the given
+        time series, or a dictionary representing a dask graph of function
+        evaluations.  Dictionaries of functions should have keys `feature_name`
+        and values functions that take arguments (t, m, e); in the case of a
+        dask graph, these arrays should be referenced as 't', 'm', 'e',
+        respectively, and any values with keys present in `features_to_use`
+        will be computed.
+    scheduler : function, optional
+        `dask` scheduler function used to perform feature extraction
+        computation. Defaults to `dask.multiprocessing.get`.
+
+    Returns
+    -------
+    xarray.Dataset
+        Featureset with `data_vars` containing feature values and `coords`
+        containing labels (`name`) and targets (`target`), if applicable.
+    """
+    all_time_series = [delayed(time_series.from_netcdf, pure=True)(ts_path)
+                       for ts_path in ts_paths]
+    all_features = [delayed(featurize_single_ts, pure=True)(ts, features_to_use,
+                                                            custom_script_path,
+                                                            custom_functions)
+                    for ts in all_time_series]
+    result = delayed(assemble_featureset, pure=True)(all_features, all_time_series)
+    fset = result.compute(get=scheduler)
+    if output_path:
+        fset.to_netcdf(output_path)
+
+    return fset
