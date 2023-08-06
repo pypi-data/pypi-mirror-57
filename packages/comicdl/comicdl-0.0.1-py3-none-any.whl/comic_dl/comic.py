7e115406ffc1c47f348de55ece89101adc313cf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:29 +0000

    Add package comicdl/0.0.1/comicdl-0.0.1-py3-none-any.whl

---
+++ packages/comicdl/comicdl-0.0.1-py3-none-any.whl/comic_dl/comic.py
@@ -0,0 +1,142 @@
+import re
+from multiprocessing.pool import ThreadPool
+
+from tqdm import tqdm
+
+from .common.db import ComicDB
+from .common.driver import Driver
+from .common.exceptions import ComicDoesNotExist, NetworkError
+from .common.utils import (
+    download_page, zip_comic, get_size, download_prompt
+)
+
+
+class Comic:
+    def __init__(self, title: str, link: str, alias: str,
+                 latest_issue: int = 0,
+                 last_downloaded: int = 0):
+        self.title = title
+        self.alias = alias
+        self.latest_issue = latest_issue
+        self.last_downloaded = last_downloaded
+        self.link = link
+
+    def __str__(self):
+        return self.title
+
+    def save(self):
+        with ComicDB() as db:
+            db.save(**vars(self))
+
+    @classmethod
+    def list_watched(cls) -> list:
+        """
+        Comic Class Method
+        -----------------------------------------------------------------------
+        :returns: list of watched Comic objects
+        """
+        with ComicDB() as db:
+            watched = db.get_all()
+        return list(map(lambda comic: Comic(*comic[1:]), watched))
+
+    @classmethod
+    def is_alias_unique(cls, alias) -> bool:
+        try:
+            cls.get_by_alias(alias)
+        except ComicDoesNotExist:
+            return True
+        return False
+
+    @classmethod
+    def get_by_alias(cls, alias):
+        with ComicDB() as db:
+            comic = db.get(alias)
+        if not comic:
+            raise ComicDoesNotExist()
+        return Comic(*comic[1:])
+
+    def get_image_links(self, driver: Driver, issue):
+        driver.get('{}/Issue-{}/'.format(self.link, issue),
+                   params={'quality': 'hq'})
+        driver.find_element_by_css_selector('script:nth-child(5)')
+        matches = re.findall(r'lstImages.push\("(.*)"\)', driver.page_source)
+        if matches:
+            for match in matches:
+                yield match
+        else:
+            raise NetworkError
+
+    def download_issue(self, driver: Driver, issue, many=False) -> str:
+        links = [link for link in self.get_image_links(driver, issue)]
+
+        if not many:
+            size = sum(size for size in
+                       ThreadPool(8).imap_unordered(get_size, links))
+            prompt = download_prompt(size)
+            if prompt.lower() != 'y':
+                return
+
+        entries = list(enumerate(links))
+        img_paths = [path for path in tqdm(
+            ThreadPool(8).imap_unordered(download_page, entries),
+            desc='{} - #{}'.format(self.title, issue),
+            total=len(entries),
+        )]
+        archive_name = '{} - #{}.cbz'.format(self.title, issue)
+        path = zip_comic(self.title, archive_name, img_paths)
+        self.last_downloaded = issue if issue > self.last_downloaded\
+            else self.last_downloaded
+        self.save()
+        return path
+
+    def download_issues(self, driver: Driver, start=1, end='last') -> list:
+        if end == 'last':
+            end = self.latest_issue
+
+        prompt = input('Can\'t precalculate the size of your download, '
+                       'are you sure you want to do this? [y/N]: ')
+        if prompt.lower() != 'y':
+            return
+
+        paths = []
+        for issue in range(start, end + 1):
+            path = self.download_issue(driver, issue, many=True)
+            paths.append(path)
+        return paths
+
+    def get_updates(self, driver: Driver) -> list:
+        available = self.list_available(driver)
+
+        def issue_num(title):
+            match = re.search(r'#(\d+)', title)
+            if match:
+                return int(match.group(1))
+            return False
+
+        last_read = list(filter(
+            lambda i: issue_num(i[0]) == self.last_downloaded,
+            available
+        ))[0]
+
+        idx = available.index(last_read)
+        updates = available[:idx]
+
+        latest_issue = issue_num(available[0][0])
+        self.latest_issue = latest_issue
+        self.save()
+
+        return updates
+
+    def list_available(self, driver: Driver) -> list:
+        driver.get(self.link)
+        available_issues = driver\
+            .find_elements_by_css_selector('table.listing td a')
+        available =\
+            [issue.get_attribute('textContent').strip()
+                for issue in available_issues]
+        dates = driver.\
+            find_elements_by_css_selector('table.listing tr td:nth-child(2)')
+        dates = [date.get_attribute('textContent').strip() for date in dates]
+
+        available = list(zip(available, dates))
+        return available
---
+++ packages/comicdl/comicdl-0.0.1-py3-none-any.whl/comic_dl/commands.py
@@ -0,0 +1,139 @@
+import argparse
+import sys
+
+from .common.driver import Driver
+from . import __version__
+from .common.exceptions import ArgumentNotSpecified, AliasNotSpecified
+from .handlers import HandlerMixin
+
+
+class CommandUtility(HandlerMixin):
+    def __init__(self, argv, args=None):
+        self.argv = argv
+        self.args = args
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, ext_type, exc_value, traceback):
+        try:
+            self.driver.quit()
+        except AttributeError:
+            pass
+
+    def execute(self):
+        parser = argparse.ArgumentParser()
+
+        parser.add_argument(
+            '-v',
+            '--version',
+            action='version',
+            version='%(prog)s {version}'.format(version=__version__)
+        )
+
+        parser.add_argument(
+            'command',
+            type=str,
+            help="""
+            Command Options:
+            * add: Finds comics based on query and adds your choice to the
+                      database under specified alias.
+
+            * watched: Lists all comics in database along with aliases.
+
+            * download: Downloads comic issues for comic specified by alias
+                      one of -i, -r or --all flags should be specified.
+
+            * issues: Lists available issues for comic specified by alias.
+
+            * updates: lists issues for watched comics that have not been
+                      downloaded.
+                 """,
+        )
+
+        parser.add_argument(
+            'alias',
+            type=str,
+            help='-Unique identifier for comics',
+            nargs='?'
+        )
+
+        parser.add_argument(
+            '--query',
+            '-q',
+            action='store',
+            type=str,
+            help='-Finds comics based on query and adds your choice to'
+                 ' database identified by specified alias',
+        )
+
+        download_options = parser.add_mutually_exclusive_group()
+        download_options.add_argument(
+            '--issue',
+            '-i',
+            type=int,
+            help='-Used with the download flag to specify issue to be '
+                 'downloaded',
+        )
+        download_options.add_argument(
+            '--range',
+            '-r',
+            type=str,
+            help='-Used to specify an inclusive range of issues to be '
+                 'downloaded. ex: -r=3-21'
+        )
+        download_options.add_argument(
+            '--all',
+            action='store_true',
+            help='-Used to specify that all available issues for a comic '
+                 'should be downloaded '
+        )
+
+        self.args = parser.parse_args(self.argv[1:]) \
+            if not self.args else self.args
+
+        if self.args.command == 'watched':
+            self.display_watched()
+
+        elif self.args.command == 'add':
+            query = self.args.query
+            if not query:
+                raise ArgumentNotSpecified('-q="<query>" flag is compulsory'
+                                           ' for this command')
+            self.driver = Driver()
+            self.add_comic_to_watched(query)
+
+        elif self.args.command == 'download':
+            alias = self.args.alias
+            if not alias:
+                raise AliasNotSpecified
+            self.driver = Driver()
+
+            if self.args.issue:
+                self.download_issue(alias, self.args.issue)
+
+            elif self.args.range or self.args.all:
+                self.download_issues(
+                    alias,
+                    self.args.range,
+                    all_=self.args.all
+                )
+
+        elif self.args.command == 'updates':
+            self.driver = Driver()
+            self.get_updates()
+
+        elif self.args.command == 'issues':
+            alias = self.args.alias
+            if not alias:
+                raise AliasNotSpecified
+            self.driver = Driver()
+            self.list_available(alias)
+
+        else:
+            parser.print_help()
+
+
+def execute_from_command_line():
+    with CommandUtility(sys.argv) as c:
+        c.execute()
