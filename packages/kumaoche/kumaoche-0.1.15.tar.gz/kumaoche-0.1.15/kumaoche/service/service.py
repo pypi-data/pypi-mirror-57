3515b31e61b42dc391afb52b2a817c67bef2d796
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:37 +0000

    Add package kumaoche/0.1.15/kumaoche-0.1.15.tar.gz

---
+++ packages/kumaoche/kumaoche-0.1.15.tar.gz/kumaoche-0.1.15/kumaoche/service/service.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from abc import ABCMeta, abstractmethod
+
+
+class Service(metaclass=ABCMeta):
+    @abstractmethod
+    def run(self, command: str):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def setup(self):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def update(self):
+        raise NotImplementedError()
