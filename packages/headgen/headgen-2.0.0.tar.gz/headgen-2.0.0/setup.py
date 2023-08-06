d97118d9b4469d12dde607c4cb78977b33a044b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:55 +0000

    Add package headgen/2.0.0/headgen-2.0.0.tar.gz

---
+++ packages/headgen/headgen-2.0.0.tar.gz/headgen-2.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='headgen',
+	version='2.0.0',
+	packages=find_packages(),
+	author='Miasnenko Dmitry',
+	author_email='d2ms2nk@gmail.com',
+	url='https://github.com/YoungMeatBoy/headgen.git',
+)
