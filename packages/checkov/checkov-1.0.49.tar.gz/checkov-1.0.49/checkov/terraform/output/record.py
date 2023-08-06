f6884c6e241a5a4c141e864d0253c2e1131c63b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:45 +0000

    Add package checkov/1.0.49/checkov-1.0.49.tar.gz

---
+++ packages/checkov/checkov-1.0.49.tar.gz/checkov-1.0.49/checkov/terraform/output/record.py
@@ -0,0 +1,20 @@
+class Record():
+    check_id = ""
+    check_name = ""
+    check_result = None
+    check_class = ""
+    code_block = ""
+    file_path = ""
+    file_line_range = []
+    resource = ""
+
+    def __init__(self, check_id, check_name, check_result, code_block, file_path, file_line_range, resource,
+                 check_class):
+        self.check_id = check_id
+        self.check_name = check_name
+        self.check_result = check_result
+        self.code_block = code_block
+        self.file_path = file_path
+        self.file_line_range = file_line_range
+        self.resource = resource
+        self.check_class = check_class
