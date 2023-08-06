bd56e3ab7c4dc15f3c35b41c16fb818a736069f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:21 +0000

    Add package brainrex/1.0.1/brainrex-1.0.1.tar.gz

---
+++ packages/brainrex/brainrex-1.0.1.tar.gz/brainrex-1.0.1/brainrex/api/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from brainrex.api.anomaly_api import AnomalyApi
+from brainrex.api.integrations_api import IntegrationsApi
+from brainrex.api.language_api import LanguageApi
