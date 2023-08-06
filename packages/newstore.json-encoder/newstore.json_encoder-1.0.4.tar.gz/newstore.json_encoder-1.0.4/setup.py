408fd9ea11cf33c908fc5b3a81421403148f790b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:36 +0000

    Add package newstore.json-encoder/1.0.4/newstore.json_encoder-1.0.4.tar.gz

---
+++ packages/newstore.json-encoder/newstore.json_encoder-1.0.4.tar.gz/newstore.json_encoder-1.0.4/setup.py
@@ -0,0 +1,32 @@
+import os
+
+from setuptools import setup
+
+VERSION = "1.0.4"
+NAMESPACE = "newstore"
+NAME = "{}.json_encoder".format(NAMESPACE)
+
+
+def local_text_file(file_name):
+    path = os.path.join(os.path.dirname(__file__), file_name)
+    with open(path, "rt") as fp:
+        file_data = fp.read()
+    return file_data
+
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description="JSONEncoder",
+    long_description=local_text_file("README.md"),
+    long_description_content_type="text/markdown",
+    author="NewStore Inc.",
+    author_email="dev@newstore.com",
+    url="https://github.com/NewStore-oss/json-encoder",
+    zip_safe=True,
+    packages=[NAME],
+    namespace_packages=[NAMESPACE],
+    python_requires=">=3.6,<3.9",
+    package_data={NAME: []},
+    install_requires=["setuptools"],
+)
