a93057dec7997746a23909e4eca6f8b78acb6e5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:32 +0000

    Add package UW-RestClients-AdSel/0.7/UW-RestClients-AdSel-0.7.tar.gz

---
+++ packages/UW-RestClients-AdSel/UW-RestClients-AdSel-0.7.tar.gz/UW-RestClients-AdSel-0.7/uw_adsel/tests/test_adsel.py
@@ -0,0 +1,58 @@
+from unittest import TestCase, mock
+from restclients_core.exceptions import DataFailureException
+from uw_adsel.utilities import fdao_adsel_override
+from uw_adsel import AdSel
+from datetime import datetime
+
+
+def mocked_get_now():
+    dt = datetime(2019, 11, 12, 0, 10, 21)
+    return dt
+
+
+class AdselTest(TestCase):
+    adsel = AdSel()
+
+    def test_request_headers(self):
+        self.assertEqual(self.adsel._headers(), {'Accept': 'application/json'})
+
+    def test_error(self):
+        with self.assertRaises(DataFailureException):
+            self.adsel._get_resource("/foobar/")
+
+    def test_get_majors(self):
+        majors = self.adsel.get_majors_by_qtr(0)
+        self.assertEqual(len(majors), 4)
+        self.assertEqual(majors[2].major_abbr, "CSE")
+        majors_unpaginated = self.adsel.get_majors_by_qtr(1)
+        self.assertEqual(len(majors_unpaginated), 2)
+
+    @mock.patch('uw_adsel.AdSel.get_now', side_effect=mocked_get_now)
+    def test_get_quarters(self, mock_obj):
+        quarters = self.adsel.get_quarters()
+        self.assertEqual(len(quarters), 2)
+        self.assertEqual(quarters[0].begin, datetime(2019, 11, 5, 0, 0, 0))
+        self.assertFalse(quarters[1].is_current)
+        self.assertTrue(quarters[0].is_current)
+
+    def test_get_cohorts(self):
+        cohorts = self.adsel.get_cohorts_by_qtr(0)
+        self.assertEqual(len(cohorts), 4)
+        self.assertEqual(cohorts[2].cohort_description, "Second Page Cohort")
+        cohorts_unpaginated = self.adsel.get_cohorts_by_qtr(1)
+        self.assertEqual(len(cohorts_unpaginated), 2)
+
+    @mock.patch('uw_adsel.AdSel.get_now', side_effect=mocked_get_now)
+    def test_get_now(self, mock_obj):
+        self.assertEqual(self.adsel.get_now(),
+                         datetime(2019, 11, 12, 0, 10, 21))
+
+    def test_get_activities(self):
+        activities = self.adsel.get_activities()
+        self.assertEqual(len(activities), 5)
+        self.assertEqual(activities[0].user, "javerage")
+
+    def test_get_application(self):
+        applications = self.adsel.get_applications_by_qtr_syskey(0, 123)
+        self.assertEqual(len(applications), 4)
+        self.assertEqual(applications[0].adsel_id, 1)
