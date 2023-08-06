ff14d231b1f8bf8eb61bcf27af6c3c6d3e2c394f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:31 +0000

    Add package mypy-boto3-sagemaker-runtime/1.10.37.1/mypy_boto3_sagemaker_runtime-1.10.37.1-py3-none-any.whl

---
+++ packages/mypy-boto3-sagemaker-runtime/mypy_boto3_sagemaker_runtime-1.10.37.1-py3-none-any.whl/mypy_boto3_sagemaker_runtime/type_defs.py
@@ -0,0 +1,25 @@
+"Main interface for sagemaker-runtime service type defs"
+import sys
+from typing import IO
+
+if sys.version_info >= (3, 8):
+    from typing import TypedDict
+else:
+    from typing_extensions import TypedDict
+from typing import Union
+
+
+_RequiredInvokeEndpointOutputTypeDef = TypedDict(
+    "_RequiredInvokeEndpointOutputTypeDef", {"Body": Union[bytes, IO]}
+)
+_OptionalInvokeEndpointOutputTypeDef = TypedDict(
+    "_OptionalInvokeEndpointOutputTypeDef",
+    {"ContentType": str, "InvokedProductionVariant": str, "CustomAttributes": str},
+    total=False,
+)
+
+
+class InvokeEndpointOutputTypeDef(
+    _RequiredInvokeEndpointOutputTypeDef, _OptionalInvokeEndpointOutputTypeDef
+):
+    pass
