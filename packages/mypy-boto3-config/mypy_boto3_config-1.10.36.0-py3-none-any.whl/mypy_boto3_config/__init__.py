3f3566fea7df01d88ff9d5b939dc1bdfcaf528b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:33 +0000

    Add package mypy-boto3-config/1.10.36.0/mypy_boto3_config-1.10.36.0-py3-none-any.whl

---
+++ packages/mypy-boto3-config/mypy_boto3_config-1.10.36.0-py3-none-any.whl/mypy_boto3_config/__init__.py
@@ -0,0 +1,44 @@
+"Main interface for config service"
+
+from mypy_boto3_config.client import Client
+from mypy_boto3_config.paginator import (
+    DescribeAggregateComplianceByConfigRulesPaginator,
+    DescribeAggregationAuthorizationsPaginator,
+    DescribeComplianceByConfigRulePaginator,
+    DescribeComplianceByResourcePaginator,
+    DescribeConfigRuleEvaluationStatusPaginator,
+    DescribeConfigRulesPaginator,
+    DescribeConfigurationAggregatorSourcesStatusPaginator,
+    DescribeConfigurationAggregatorsPaginator,
+    DescribePendingAggregationRequestsPaginator,
+    DescribeRemediationExecutionStatusPaginator,
+    DescribeRetentionConfigurationsPaginator,
+    GetAggregateComplianceDetailsByConfigRulePaginator,
+    GetComplianceDetailsByConfigRulePaginator,
+    GetComplianceDetailsByResourcePaginator,
+    GetResourceConfigHistoryPaginator,
+    ListAggregateDiscoveredResourcesPaginator,
+    ListDiscoveredResourcesPaginator,
+)
+
+
+__all__ = (
+    "Client",
+    "DescribeAggregateComplianceByConfigRulesPaginator",
+    "DescribeAggregationAuthorizationsPaginator",
+    "DescribeComplianceByConfigRulePaginator",
+    "DescribeComplianceByResourcePaginator",
+    "DescribeConfigRuleEvaluationStatusPaginator",
+    "DescribeConfigRulesPaginator",
+    "DescribeConfigurationAggregatorSourcesStatusPaginator",
+    "DescribeConfigurationAggregatorsPaginator",
+    "DescribePendingAggregationRequestsPaginator",
+    "DescribeRemediationExecutionStatusPaginator",
+    "DescribeRetentionConfigurationsPaginator",
+    "GetAggregateComplianceDetailsByConfigRulePaginator",
+    "GetComplianceDetailsByConfigRulePaginator",
+    "GetComplianceDetailsByResourcePaginator",
+    "GetResourceConfigHistoryPaginator",
+    "ListAggregateDiscoveredResourcesPaginator",
+    "ListDiscoveredResourcesPaginator",
+)
