a8209089078334c1bea1a5498a7fd3cf1fea3836
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:46 +0000

    Add package Python-Distributions-DS/0.1/Python-Distributions-DS-0.1.tar.gz

---
+++ packages/Python-Distributions-DS/Python-Distributions-DS-0.1.tar.gz/Python-Distributions-DS-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='Python-Distributions-DS',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      zip_safe=False)
