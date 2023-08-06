e0b9bf2ce460afaa40fc8c9ee785b5899d7612e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:36 +0000

    Add package Tilty/0.0.2/Tilty-0.0.2.tar.gz

---
+++ packages/Tilty/Tilty-0.0.2.tar.gz/Tilty-0.0.2/setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""Setup file for the package"""
+from setuptools import find_packages, setup
+
+setup(
+    name='Tilty',
+    description='A pluggable system to receive and transmit bluetooth events from the Tilt Hydrometer', # noqa
+    author='Marcus Young',
+    author_email='3vilpenguin@gmail.com',
+    py_modules=['tilty', 'blescan'],
+    version='0.0.2',
+    packages=find_packages(exclude=['tests*']),
+    install_requires=[
+        'Click',
+        'pybluez',
+    ],
+    entry_points={
+        'console_scripts': [
+            'tilty=tilty.cli:run',
+        ],
+    },
+)
