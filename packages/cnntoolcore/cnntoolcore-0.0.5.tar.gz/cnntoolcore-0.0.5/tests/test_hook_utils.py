60edcdf388eb928f7a9ac63c2c63cda4ed0710f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:44 +0000

    Add package cnntoolcore/0.0.5/cnntoolcore-0.0.5.tar.gz

---
+++ packages/cnntoolcore/cnntoolcore-0.0.5.tar.gz/cnntoolcore-0.0.5/tests/test_hook_utils.py
@@ -0,0 +1,19 @@
+import unittest
+
+import torchvision
+
+from visualization_utils.hook_utils import HookUtils
+
+
+
+class HookUtilsTest(unittest.TestCase):
+    def hook_fn(m, i, o):
+        pass
+
+    def test_should_register_hook(self):
+        model = torchvision.models.AlexNet()
+
+        hook_utils = HookUtils()
+        hook_utils.deep_hook_register(model, self.hook_fn)
+
+        self.assertEquals(len(model._modules['features']._modules['0']._forward_hooks), 1)
