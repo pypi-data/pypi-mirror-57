30e377a3240092ced037013828b48448f77a2c7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:02 +0000

    Add package svmlight-loader/0.1.0/svmlight-loader-0.1.0.tar.gz

---
+++ packages/svmlight-loader/svmlight-loader-0.1.0.tar.gz/svmlight-loader-0.1.0/svmlight_loader/__init__.py
@@ -0,0 +1,11 @@
+try:
+    from importlib import metadata
+except ImportError:
+    import importlib_metadata as metadata
+__version__ = metadata.version(__name__)
+
+from svmlight_loader._api import (
+    classification_from_lines,
+    multilabel_classification_from_lines,
+    regression_from_lines,
+)
