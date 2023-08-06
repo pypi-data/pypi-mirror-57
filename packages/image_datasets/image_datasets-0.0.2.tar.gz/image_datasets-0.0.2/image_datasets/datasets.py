ba85039e1ce32a2a20f855c7cad528c148a8491f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:22 +0000

    Add package image_datasets/0.0.2/image_datasets-0.0.2.tar.gz

---
+++ packages/image_datasets/image_datasets-0.0.2.tar.gz/image_datasets-0.0.2/image_datasets/datasets.py
@@ -0,0 +1,27 @@
+from image_datasets.core import Dataset
+import sys
+
+hosts = {
+  'pixplot': 'http://pixplot.yale.edu/datasets'
+}
+
+oslo = Dataset(
+  name='oslo',
+  image_path=hosts['pixplot'] + '/oslo/photos.tar',
+  metadata_path=hosts['pixplot'] + '/oslo/metadata.csv',
+)
+
+oslomini = Dataset(
+  name='oslomini',
+  image_path=hosts['pixplot'] + '/oslomini/photos.tar',
+  metadata_path=hosts['pixplot'] + '/oslomini/metadata.csv',
+)
+
+bain = Dataset(
+  name='bain',
+  image_path=hosts['pixplot'] + '/bain/photos.tar',
+  metadata_path=hosts['pixplot'] + '/bain/metadata.csv',
+)
+
+def list_all():
+  return ['oslo', 'oslomini', 'bain']
