6aa123399d978eb6a55d7475de6d085063d4d610
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:04 +0000

    Add package filetools/0.0.5/filetools-0.0.5.tar.gz

---
+++ packages/filetools/filetools-0.0.5.tar.gz/filetools-0.0.5/setup.py
@@ -0,0 +1,27 @@
+# coding: utf-8
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="filetools",
+    version="0.0.5",
+    author="Christian Winger",
+    author_email="c@wingechr.de",
+    description="file processing tools",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://pypi.org/project/filetools",
+    project_urls={
+        "Documentation": "https://filetools.readthedocs.io",
+        "Source": "https://github.com/wingechr/filetools",
+    },
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
