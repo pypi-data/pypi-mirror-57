9c3b5e5eda4d03ea1791b3e558bbf1974bff1ff1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:18 +0000

    Add package jiraya/0.1.4/jiraya-0.1.4.tar.gz

---
+++ packages/jiraya/jiraya-0.1.4.tar.gz/jiraya-0.1.4/setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+
+setup(
+    name='jiraya',
+    description='Simple Jira Command Line Interface.',
+    long_description='Simple Jira Command Line Interface.',
+    author='Rodrigo Chacon',
+    author_email='rochacon@gmail.com',
+    url='https://github.com/rochacon/jiraya',
+    license='MIT',
+    version='0.1.4',
+    install_requires=open('requirements.txt').readlines(),
+    scripts=['jiraya'],
+)
