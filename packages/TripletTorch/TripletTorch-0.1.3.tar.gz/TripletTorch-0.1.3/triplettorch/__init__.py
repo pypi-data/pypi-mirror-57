84beb561d895ef4287f34dca026e727c63f153de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:56 +0000

    Add package TripletTorch/0.1.3/TripletTorch-0.1.3.tar.gz

---
+++ packages/TripletTorch/TripletTorch-0.1.3.tar.gz/TripletTorch-0.1.3/triplettorch/__init__.py
@@ -0,0 +1,17 @@
+"""The Init File for triplettorch module
+
+The file imports all classes from the triplettorch module to expose them to
+the user for easier API usage.
+It provides a TripletDataset class as well as a TripletMiner class with two
+common types of Triplet Miner: AllTripletMiner and HardNegativeTripletMiner.
+"""
+from triplettorch.loss import HardNegativeTripletMiner
+from triplettorch.loss import AllTripletMiner
+from triplettorch.data import TripletDataset
+from triplettorch.loss import TripletMiner
+
+__name__ = "TripletTorch"
+__version__ = "0.1.3"
+__author__ = "yliess"
+__url__ = "https://github.com/TowardHumanizedInteraction/TripletTorch"
+__email__ = "hatiyliess86@gmail.com"
