1c36b5a6e4637e088e05430330bde2adc62988f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:17 +0000

    Add package pyscada-scripting/0.7.0/pyscada_scripting-0.7.0-py2-none-any.whl

---
+++ packages/pyscada-scripting/pyscada_scripting-0.7.0-py2-none-any.whl/pyscada/scripting/admin.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from pyscada.admin import admin_site
+
+from django.contrib import admin
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+#admin_site.register()
