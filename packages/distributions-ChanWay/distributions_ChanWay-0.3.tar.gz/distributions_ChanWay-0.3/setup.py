3830dd3e050cd6911d17a6548f68882398feb363
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:00 +0000

    Add package distributions-ChanWay/0.3/distributions_ChanWay-0.3.tar.gz

---
+++ packages/distributions-ChanWay/distributions_ChanWay-0.3.tar.gz/distributions_ChanWay-0.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(name='distributions_ChanWay',
+      version='0.3',
+      author='Udacity-ChanWay',
+      author_email='chanwayng@u.nus.edu',
+      description='Gaussian and binomial distributions',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      packages=['distributions_ChanWay'],
+      classifiers=[
+          'Programming Language :: Python :: 3',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent'
+      ],
+      zip_safe=False)
