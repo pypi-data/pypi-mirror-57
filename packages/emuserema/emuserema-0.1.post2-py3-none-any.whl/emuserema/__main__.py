72e51f712f683d3a62d617b2b589ca0d9c850cc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:08 +0000

    Add package emuserema/0.1.post2/emuserema-0.1.post2-py3-none-any.whl

---
+++ packages/emuserema/emuserema-0.1.post2-py3-none-any.whl/emuserema/__main__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from emuserema import Emuserema
+from sys import argv
+
+
+def main():
+    emuserema = Emuserema()
+    if len(argv) > 1:
+        if argv[1] == '-t':
+            print("Configuration parsing was successful.")
+            exit(0)
+    emuserema.render()
+
+
+if __name__ == '__main__':
+    main()
