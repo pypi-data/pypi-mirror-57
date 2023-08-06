e9330366fb731d23810673c17871298444a7a864
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:38 +0000

    Add package mypy-boto3-mq/1.10.35.0/mypy_boto3_mq-1.10.35.0-py3-none-any.whl

---
+++ packages/mypy-boto3-mq/mypy_boto3_mq-1.10.35.0-py3-none-any.whl/mypy_boto3_mq/__init__.py
@@ -0,0 +1,7 @@
+"Main interface for mq service"
+
+from mypy_boto3_mq.client import Client
+from mypy_boto3_mq.paginator import ListBrokersPaginator
+
+
+__all__ = ("Client", "ListBrokersPaginator")
