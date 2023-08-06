e3d742caca4251d72a11cef3724d8e090a305e06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:28 +0000

    Add package package-wy/1.0.0/package_wy-1.0.0.tar.gz

---
+++ packages/package-wy/package_wy-1.0.0.tar.gz/package_wy-1.0.0/setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+@author: WYR。
+"""
+
+from setuptools import setup
+
+setup(
+    name= 'package_wy',
+    version= '1.0.0',
+    packages = ['package_wy'],
+    author='TuNan',
+    author_email='wuuyanru@163.com',
+    license='Apache',
+)
