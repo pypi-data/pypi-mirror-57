b23d026de525b4946d6bf503b4569fb9348c7c82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:28 +0000

    Add package mypy-boto3-elbv2/1.10.35.0/mypy_boto3_elbv2-1.10.35.0-py3-none-any.whl

---
+++ packages/mypy-boto3-elbv2/mypy_boto3_elbv2-1.10.35.0-py3-none-any.whl/mypy_boto3_elbv2/__init__.py
@@ -0,0 +1,36 @@
+"Main interface for elbv2 service"
+
+from mypy_boto3_elbv2.client import Client
+from mypy_boto3_elbv2.paginator import (
+    DescribeAccountLimitsPaginator,
+    DescribeListenerCertificatesPaginator,
+    DescribeListenersPaginator,
+    DescribeLoadBalancersPaginator,
+    DescribeRulesPaginator,
+    DescribeSSLPoliciesPaginator,
+    DescribeTargetGroupsPaginator,
+)
+from mypy_boto3_elbv2.waiter import (
+    LoadBalancerAvailableWaiter,
+    LoadBalancerExistsWaiter,
+    LoadBalancersDeletedWaiter,
+    TargetDeregisteredWaiter,
+    TargetInServiceWaiter,
+)
+
+
+__all__ = (
+    "Client",
+    "LoadBalancerAvailableWaiter",
+    "LoadBalancerExistsWaiter",
+    "LoadBalancersDeletedWaiter",
+    "TargetDeregisteredWaiter",
+    "TargetInServiceWaiter",
+    "DescribeAccountLimitsPaginator",
+    "DescribeListenerCertificatesPaginator",
+    "DescribeListenersPaginator",
+    "DescribeLoadBalancersPaginator",
+    "DescribeRulesPaginator",
+    "DescribeSSLPoliciesPaginator",
+    "DescribeTargetGroupsPaginator",
+)
