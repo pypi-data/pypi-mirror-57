c97b5051ae82129d41850bd41d9b79000eedb1a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:39 +0000

    Add package websub/0.1.1/websub-0.1.1.tar.gz

---
+++ packages/websub/websub-0.1.1.tar.gz/websub-0.1.1/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+      name="websub",
+      version="0.1.1",
+      author="Jonathan de Jong",
+      author_email="jonathan@automatia.nl",
+      description="WebSub Client for python, based off of gohubbub",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/ShadowJonathan/pywebsub",
+      packages=setuptools.find_packages(),
+      classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+      ],
+      install_requires=[
+            'sanic',
+            'httpx',
+            'anyio',
+            'bs4',
+            'lxml'
+      ],
+      setup_requires=['wheel'],
+      extras_require={
+            'dev': [
+                  'ipython'
+            ]
+      }
+)
