8242ac82eda1a605a3abce47e9803879c9a80cc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:59 +0000

    Add package nexus3-cli/2.1.2/nexus3_cli-2.1.2-py2.py3-none-any.whl

---
+++ packages/nexus3-cli/nexus3_cli-2.1.2-py2.py3-none-any.whl/nexuscli/exception.py
@@ -0,0 +1,44 @@
+class NexusClientAPIError(Exception):
+    """Unexpected response from Nexus service."""
+    pass
+
+
+class NexusClientInvalidCredentials(Exception):
+    """
+    Login credentials not accepted by Nexus service. Usually the result of a
+    HTTP 401 response.
+    """
+    pass
+
+
+class NexusClientInvalidRepositoryPath(Exception):
+    """
+    Used when an operation against the Nexus service uses an invalid or
+    non-existent path.
+    """
+    pass
+
+
+class NexusClientInvalidRepository(Exception):
+    """The given repository does not exist in Nexus."""
+    pass
+
+
+class NexusClientInvalidCleanupPolicy(Exception):
+    """The given cleanup policy does not exist in Nexus."""
+    pass
+
+
+class NexusClientCreateRepositoryError(Exception):
+    """Used when a repository creation operation in Nexus fails."""
+    pass
+
+
+class NexusClientCreateCleanupPolicyError(Exception):
+    """Used when a cleanup policy creation operation in Nexus fails."""
+    pass
+
+
+class DownloadError(Exception):
+    """Error retrieving artefact from Nexus service."""
+    pass
