3de7af24179673231b597bd6ba842ea234c0c8ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:44 +0000

    Add package encrypted-dns/1.1.1/encrypted_dns-1.1.1-py3-none-any.whl

---
+++ packages/encrypted-dns/encrypted_dns-1.1.1-py3-none-any.whl/encrypted_dns/__init__.py
@@ -0,0 +1,8 @@
+import encrypted_dns.log as log
+import encrypted_dns.main as main
+import encrypted_dns.parse as parse
+import encrypted_dns.server as server
+import encrypted_dns.upstream as upstream
+import encrypted_dns.utils as utils
+from encrypted_dns.config import Configuration
+from encrypted_dns.struct import StructQuery, StructResponse
