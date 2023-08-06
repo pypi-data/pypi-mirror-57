da03424377a54be31b92b7649e58e50e1495bea4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:14 +0000

    Add package python-clu/0.5.1/python_clu-0.5.1-py3-none-any.whl

---
+++ packages/python-clu/python_clu-0.5.1-py3-none-any.whl/clu/repl/cli/print_version.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+def print_version_command():
+    """ Print a simple string with CLU’s current semantic version tag """
+    from clu.version import version_info as version
+    
+    semantic = f"{version.major}.{version.minor}.{version.patch}"
+    if version.pre:
+        semantic += f"-{version.pre}"
+        if version.build:
+            semantic += f"+{version.build}"
+        semantic += " [SNAPSHOT]"
+    else:
+        semantic += " [release]"
+    
+    copyright = f"CLU version {semantic} © 2010-2032 Alexander Böhn"
+    
+    print(copyright)
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(print_version_command())
