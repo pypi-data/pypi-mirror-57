9a2cb28ca431ad6830ba976b21e4dae206205852
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:04 +0000

    Add package espressodb/1.0.0/espressodb-1.0.0.tar.gz

---
+++ packages/espressodb/espressodb-1.0.0.tar.gz/espressodb-1.0.0/espressodb/__init__.py
@@ -0,0 +1,42 @@
+# pylint: disable=C0413
+"""Initializes minimal settings to launch EspressoDB
+"""
+from django import setup as _setup
+from django.conf import settings
+
+__version__ = "1.0.0"
+
+DEFAULT_OPTIONS = {
+    "DEBUG": True,
+    "INSTALLED_APPS": [
+        "espressodb.base",
+        "espressodb.documentation",
+        "espressodb.management",
+        "espressodb.notifications",
+        "django.contrib.auth",
+        "django.contrib.contenttypes",
+    ],
+    "LOGGING": {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "handlers": {"console": {"level": "INFO", "class": "logging.StreamHandler"}},
+        "loggers": {
+            "espressodb": {"handlers": ["console"], "level": "DEBUG", "propagate": True}
+        },
+    },
+}
+
+
+def init(**kwargs):
+    """Initializes minimal settings to launch EspressoDB without a project
+
+    Launches  ``django.conf.settings.configure`` and runs ``django.setup``.
+    This is needed to use EspressoDB command line tools.
+
+    Keyword Args:
+        kwargs: Kwargs are fed to ``settings.configure``.
+    """
+    options = {**DEFAULT_OPTIONS, **kwargs}
+    settings.configure(**options)
+
+    _setup()
