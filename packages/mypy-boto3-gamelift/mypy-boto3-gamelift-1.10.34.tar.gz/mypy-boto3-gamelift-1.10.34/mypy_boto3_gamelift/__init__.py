7078d2b01f0feb16dbc3c03002fc8ae0d2d9d4f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:54 +0000

    Add package mypy-boto3-gamelift/1.10.34/mypy-boto3-gamelift-1.10.34.tar.gz

---
+++ packages/mypy-boto3-gamelift/mypy-boto3-gamelift-1.10.34.tar.gz/mypy-boto3-gamelift-1.10.34/mypy_boto3_gamelift/__init__.py
@@ -0,0 +1,42 @@
+"Main interface for gamelift service"
+
+from mypy_boto3_gamelift.client import Client
+from mypy_boto3_gamelift.paginator import (
+    DescribeFleetAttributesPaginator,
+    DescribeFleetCapacityPaginator,
+    DescribeFleetEventsPaginator,
+    DescribeFleetUtilizationPaginator,
+    DescribeGameSessionDetailsPaginator,
+    DescribeGameSessionQueuesPaginator,
+    DescribeGameSessionsPaginator,
+    DescribeInstancesPaginator,
+    DescribeMatchmakingConfigurationsPaginator,
+    DescribeMatchmakingRuleSetsPaginator,
+    DescribePlayerSessionsPaginator,
+    DescribeScalingPoliciesPaginator,
+    ListAliasesPaginator,
+    ListBuildsPaginator,
+    ListFleetsPaginator,
+    SearchGameSessionsPaginator,
+)
+
+
+__all__ = (
+    "Client",
+    "DescribeFleetAttributesPaginator",
+    "DescribeFleetCapacityPaginator",
+    "DescribeFleetEventsPaginator",
+    "DescribeFleetUtilizationPaginator",
+    "DescribeGameSessionDetailsPaginator",
+    "DescribeGameSessionQueuesPaginator",
+    "DescribeGameSessionsPaginator",
+    "DescribeInstancesPaginator",
+    "DescribeMatchmakingConfigurationsPaginator",
+    "DescribeMatchmakingRuleSetsPaginator",
+    "DescribePlayerSessionsPaginator",
+    "DescribeScalingPoliciesPaginator",
+    "ListAliasesPaginator",
+    "ListBuildsPaginator",
+    "ListFleetsPaginator",
+    "SearchGameSessionsPaginator",
+)
