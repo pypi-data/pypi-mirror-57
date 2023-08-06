0286a95f7ee1deb7cac6503986ff08a0f14d72ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:20 +0000

    Add package mypy-boto3-greengrass/1.10.39.3/mypy-boto3-greengrass-1.10.39.3.tar.gz

---
+++ packages/mypy-boto3-greengrass/mypy-boto3-greengrass-1.10.39.3.tar.gz/mypy-boto3-greengrass-1.10.39.3/setup.py
@@ -0,0 +1,54 @@
+import sys
+
+from setuptools import setup
+from setuptools.command.install import install
+
+
+LONG_DESCRIPTION = """
+See [mypy_boto3](https://pypi.org/project/mypy-boto3/) for more info.
+"""
+
+
+class PostInstallCommand(install):
+    def run(self):
+        install.run(self)
+        print("mypy_boto3: Running post-install script")
+        try:
+            from mypy_boto3.main import maybe_build_index
+
+            maybe_build_index()
+            print("mypy_boto3: Package index updated")
+        except:
+            print("mypy_boto3: Package index update failed", sys.exc_info())
+
+
+setup(
+    name="mypy-boto3-greengrass",
+    version="1.10.39.3",
+    packages=["mypy_boto3_greengrass"],
+    url="https://github.com/vemel/mypy_boto3",
+    license="MIT License",
+    author="Vlad Emelianov",
+    author_email="vlad.emelianov.nz@gmail.com",
+    description="Mypy-friendly boto3 1.10.39 type annotations for greengrass service.",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Environment :: Console",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Typing :: Typed",
+    ],
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    package_data={"mypy_boto3_greengrass": ["py.typed"]},
+    install_requires=["typing_extensions; python_version < '3.8'",],
+    zip_safe=False,
+    cmdclass={"install": PostInstallCommand},
+)
