0fd2e324d837f75dc7223a66ee70ce7faaa39991
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:03 +0000

    Add package fifteenrock/0.1.55/fifteenrock-0.1.55.tar.gz

---
+++ packages/fifteenrock/fifteenrock-0.1.55.tar.gz/fifteenrock-0.1.55/fifteenrock/template/main.py
@@ -0,0 +1,11 @@
+# main.py
+# deploy this for everyone and other apps!
+import fifteenrock
+from . import fiteenrock_project_main
+
+
+def main(context, event):
+    return context.Response(body=fiteenrock_project_main._main(event),
+                            headers={},
+                            content_type='text/json',
+                            status_code=200)
