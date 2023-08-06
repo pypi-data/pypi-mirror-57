3eace6b49da6f48dbaa0a9197f36ca4c3fc2d11d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:04 +0000

    Add package pip-check/2.6/pip-check-2.6.tar.gz

---
+++ packages/pip-check/pip-check-2.6.tar.gz/pip-check-2.6/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+from sys import exit
+
+from setuptools import find_packages, setup
+
+long_description = u'\n\n'.join((
+    open('README.rst').read(),
+))
+
+setup(
+    name='pip-check',
+    version='2.6',
+    description='Display installed pip packages and their update status..',
+    long_description=long_description,
+    author='Martin Mahner',
+    author_email='martin@mahner.org',
+    url='https://github.com/bartTC/pip-check/',
+    classifiers=[
+    ],
+    package_dir={"": "src"},
+    packages=find_packages(
+        where="src",
+    ),
+    package_data={},
+    include_package_data=True,
+    entry_points={'console_scripts': [
+        'pip-check = pip_check:main',
+    ]},
+    python_requires=">=3.5",
+    install_requires=[
+        'terminaltables',
+        'colorclass',
+        'pip>=9',
+    ],
+)
