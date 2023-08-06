346166e7ca60d5c48f73f7bd392d1a4e2040535a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:34 +0000

    Add package aiohttp-swagger3/0.3.0/aiohttp_swagger3-0.3.0-py3-none-any.whl

---
+++ packages/aiohttp-swagger3/aiohttp_swagger3-0.3.0-py3-none-any.whl/aiohttp_swagger3/__init__.py
@@ -0,0 +1,7 @@
+__all__ = ("SwaggerDocs", "SwaggerFile", "SwaggerUiSettings", "__version__")
+__version__ = "0.3.0"
+__author__ = "Valetov Konstantin"
+
+from .swagger_docs import SwaggerDocs
+from .swagger_file import SwaggerFile
+from .swagger_ui_settings import SwaggerUiSettings
