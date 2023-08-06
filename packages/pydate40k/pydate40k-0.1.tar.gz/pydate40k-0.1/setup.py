9013447ddeff27b60cee2cba3c13e872753978c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:08 +0000

    Add package pydate40k/0.1/pydate40k-0.1.tar.gz

---
+++ packages/pydate40k/pydate40k-0.1.tar.gz/pydate40k-0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(name='pydate40k',
+      version='0.1',
+      url='https://github.com/UltimatePancake/pydate40k',
+      license='MIT',
+      author='Pier Gaetani',
+      author_email='pier.gaetani@gmail.com',
+      description='Return a date in the format of the Imperial Calendar',
+      packages=find_packages(exclude=['tests']),
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      python_requires='>=3.7',
+      classifiers=[
+          "Development Status :: 3 - Alpha",
+          "Programming Language :: Python :: 3.7",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+      ],
+      zip_safe=False)
