5d9e8b9b462513fbd327d920e411113d5d691f77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:40 +0000

    Add package alas-pkg-core/0.0.5/alas_pkg_core-0.0.5-py2.py3-none-any.whl

---
+++ packages/alas-pkg-core/alas_pkg_core-0.0.5-py2.py3-none-any.whl/alas_pkg_core/logger/logger_manager.py
@@ -0,0 +1,29 @@
+import sys
+from alas_pkg_core.logger.logger_repository import SysLoggerRepository
+import os
+
+
+class LoggerManager(object):
+    log_name = 'my-log'
+
+    def __init__(self, loggerRepository: LoggerRepository, debug=False):
+        super().__init__()
+        self.loggerRepository = loggerRepository
+        self.sysloggerRepository = SysLoggerRepository()
+        serv_name = os.getenv('SERVICE-NAME', None)
+        self.debug = debug
+        if serv_name:
+            self.log_name = serv_name
+
+    def log_info(self, msg):
+        if not self.debug:
+            self.loggerRepository.write(self.log_name, msg, severity="INFO")
+        else:
+            print(msg)
+
+    def log_error(self, msg):
+        if not self.debug:
+            self.loggerRepository.write(self.log_name, msg, severity='ERROR')
+        else:
+            self.sysloggerRepository.write(
+                self.log_name, msg, severity='ERROR')
