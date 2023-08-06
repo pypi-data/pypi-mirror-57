2edeea26a42d4a81f18770e5d71d3b82523058ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:26 +0000

    Add package Mopidy/3.0.0a7/Mopidy-3.0.0a7.tar.gz

---
+++ packages/Mopidy/Mopidy-3.0.0a7.tar.gz/Mopidy-3.0.0a7/tests/test_exceptions.py
@@ -0,0 +1,38 @@
+import unittest
+
+from mopidy import exceptions
+
+
+class ExceptionsTest(unittest.TestCase):
+    def test_exception_can_include_message_string(self):
+        exc = exceptions.MopidyException("foo")
+
+        assert exc.message == "foo"
+        assert str(exc) == "foo"
+
+    def test_backend_error_is_a_mopidy_exception(self):
+        assert issubclass(exceptions.BackendError, exceptions.MopidyException)
+
+    def test_extension_error_is_a_mopidy_exception(self):
+        assert issubclass(exceptions.ExtensionError, exceptions.MopidyException)
+
+    def test_find_error_is_a_mopidy_exception(self):
+        assert issubclass(exceptions.FindError, exceptions.MopidyException)
+
+    def test_find_error_can_store_an_errno(self):
+        exc = exceptions.FindError("msg", errno=1234)
+
+        assert exc.message == "msg"
+        assert exc.errno == 1234
+
+    def test_frontend_error_is_a_mopidy_exception(self):
+        assert issubclass(exceptions.FrontendError, exceptions.MopidyException)
+
+    def test_mixer_error_is_a_mopidy_exception(self):
+        assert issubclass(exceptions.MixerError, exceptions.MopidyException)
+
+    def test_scanner_error_is_a_mopidy_exception(self):
+        assert issubclass(exceptions.ScannerError, exceptions.MopidyException)
+
+    def test_audio_error_is_a_mopidy_exception(self):
+        assert issubclass(exceptions.AudioException, exceptions.MopidyException)
