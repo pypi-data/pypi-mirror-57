2c3db2b072cf322e38ef41848f3ede20aaa6b9d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:14 +0000

    Add package adb-shell/0.1.0/adb_shell-0.1.0.tar.gz

---
+++ packages/adb-shell/adb_shell-0.1.0.tar.gz/adb_shell-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='adb_shell',
+    version='0.1.0',
+    description='ADB shell functionality',
+    author='Jeff Irion',
+    author_email='jefflirion@users.noreply.github.com',
+    packages=['adb_shell', 'adb_shell.auth', 'adb_shell.handle'],
+    install_requires=['cryptography', 'pyasn1', 'rsa'],
+    tests_require=['pycryptodome'],
+    classifiers=['Operating System :: OS Independent',
+                 'Programming Language :: Python :: 3',
+                 'Programming Language :: Python :: 2'],
+    test_suite='tests'
+)
