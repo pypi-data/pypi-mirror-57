2e1af385da288bdaa657291dc2d712864e13aeb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:25 +0000

    Add package Execution-Time/1.1.0/Execution%20Time-1.1.0.tar.gz

---
+++ packages/Execution-Time/Execution%20Time-1.1.0.tar.gz/Execution Time-1.1.0/execution_time/tests/test_execution.py
@@ -0,0 +1,21 @@
+import unittest
+from execution_time import ExecutionTime
+import time
+from unittest.mock import Mock
+import logging
+
+class TestExecutionTime(unittest.TestCase):
+
+    def test_default_decorator(self):
+        """
+        Test ExecutionTime with default arguments
+        """
+        func = Mock()
+        func.__name__ = 'foo'
+        ex = ExecutionTime()
+        decorated = ex.timeit(func)
+        decorated()
+        self.assertTrue(ex.logtime_data,msg="logtime_data dict should not be empty")
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ packages/Execution-Time/Execution%20Time-1.1.0.tar.gz/Execution Time-1.1.0/setup.py
@@ -0,0 +1,31 @@
+import pathlib
+import setuptools
+import execution_time
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setuptools.setup(
+    name=execution_time.__name__,
+    version=execution_time.__version__,
+    description=u"package which provides you with a decorator to measure execution time of functions",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/siddhant-curious/Python-Method-Execution-Time",
+    author=execution_time.__author__,
+    author_email="siddhant1042@gmail.com",
+    keyword='performance execution time package',
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+)
