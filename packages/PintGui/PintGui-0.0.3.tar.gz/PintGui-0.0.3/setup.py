a2a10ed4cda897fe8475a4c6e0d4c4eed78723ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:30 +0000

    Add package PintGui/0.0.3/PintGui-0.0.3.tar.gz

---
+++ packages/PintGui/PintGui-0.0.3.tar.gz/PintGui-0.0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+version = '0.0.3'
+
+setup(
+    name="PintGui",
+    version=version,
+    py_modules=['PintGui'],
+    install_requires=['pint','pyside2'],
+    entry_points = '''
+      [gui_scripts]
+      PintGui = PintGui.scripts.main:start
+      '''
+    )
