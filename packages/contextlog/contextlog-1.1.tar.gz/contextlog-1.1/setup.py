927cd1d950a20dd1e9d87901a3101c6b2292d290
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:56 +0000

    Add package contextlog/1.1/contextlog-1.1.tar.gz

---
+++ packages/contextlog/contextlog-1.1.tar.gz/contextlog-1.1/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+
+# =====
+if __name__ == "__main__":
+    setuptools.setup(
+        name="contextlog",
+        version="1.1",
+        url="https://github.com/mdevaev/contextlog",
+        license="LGPLv3",
+        author="Maxim Devaev",
+        author_email="mdevaev@gmail.com",
+        description="Context-based logger and formatters collection",
+        platforms="any",
+
+        packages=[
+            "contextlog",
+        ],
+
+        install_requires=[
+            "colorlog",
+        ],
+
+        classifiers=[  # http://pypi.python.org/pypi?:action=list_classifiers
+            "Development Status :: 4 - Beta",
+            "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+            "Programming Language :: Python :: 3",
+            "Topic :: Software Development :: Libraries :: Python Modules",
+            "Topic :: System :: Logging",
+        ],
+    )
