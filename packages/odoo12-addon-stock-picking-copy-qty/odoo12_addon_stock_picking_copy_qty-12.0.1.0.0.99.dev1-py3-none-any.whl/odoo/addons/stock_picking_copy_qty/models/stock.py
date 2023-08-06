3a1a75aafac8c5c9f352008ca829b38339bd3f48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:01 +0000

    Add package odoo12-addon-stock-picking-copy-qty/12.0.1.0.0.99.dev1/odoo12_addon_stock_picking_copy_qty-12.0.1.0.0.99.dev1-py3-none-any.whl

---
+++ packages/odoo12-addon-stock-picking-copy-qty/odoo12_addon_stock_picking_copy_qty-12.0.1.0.0.99.dev1-py3-none-any.whl/odoo/addons/stock_picking_copy_qty/models/stock.py
@@ -0,0 +1,18 @@
+# Copyright 2019 Coop IT Easy SCRL fs
+#   Robin Keunen <robin@coopiteasy.be>
+#   Vincent Van Rossem <vincent@coopiteasy.be>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+
+from odoo import models, fields, api, _
+
+
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+
+    @api.multi
+    def copy_qty(self):
+        self.ensure_one()
+        for move_without_package in self.move_ids_without_package:
+            move_without_package.quantity_done = move_without_package.product_uom_qty
+        return True
