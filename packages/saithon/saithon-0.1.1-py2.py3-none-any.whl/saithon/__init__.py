fb515a366f24cdc3694b4423d5682560da9f6d88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:00 +0000

    Add package saithon/0.1.1/saithon-0.1.1-py2.py3-none-any.whl

---
+++ packages/saithon/saithon-0.1.1-py2.py3-none-any.whl/saithon/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+from ._version import get_versions
+
+__author__ = 'Sayan Dey'
+__email__ = 'mr.sayan.dey@gmail.com'
+__version__ = get_versions()['version']
+del get_versions
+
