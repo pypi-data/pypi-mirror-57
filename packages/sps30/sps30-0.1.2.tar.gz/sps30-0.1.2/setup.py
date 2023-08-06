c336fb2deac1fc137d47b295493bb7fabe8f7ea9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:09 +0000

    Add package sps30/0.1.2/sps30-0.1.2.tar.gz

---
+++ packages/sps30/sps30-0.1.2.tar.gz/sps30-0.1.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sps30",
+    version="0.1.2",
+    author="Feyzi Kesim",
+    author_email="feyzikesim@gmail.com",
+    description="Python3 I2C Driver for SPS30 PM Sensor",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/feyzikesim/sps30",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
