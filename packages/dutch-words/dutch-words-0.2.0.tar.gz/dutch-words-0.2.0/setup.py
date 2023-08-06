593a55bda6add526dc59ef5e9ed43cefaa9089c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:07 +0000

    Add package dutch-words/0.2.0/dutch-words-0.2.0.tar.gz

---
+++ packages/dutch-words/dutch-words-0.2.0.tar.gz/dutch-words-0.2.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['dutch_words']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'dutch-words',
+    'version': '0.2.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Ramon de Jezus',
+    'author_email': 'rdejezus@leukeleu.nl',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
