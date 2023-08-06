bd04ab52e6763c747c0a10b61fff387707ddfe02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:31 +0000

    Add package fastapi/0.44.1/fastapi-0.44.1.tar.gz

---
+++ packages/fastapi/fastapi-0.44.1.tar.gz/fastapi-0.44.1/docs/src/body_nested_models/tutorial008.py
@@ -0,0 +1,16 @@
+from typing import List
+
+from fastapi import FastAPI
+from pydantic import BaseModel, UrlStr
+
+app = FastAPI()
+
+
+class Image(BaseModel):
+    url: UrlStr
+    name: str
+
+
+@app.post("/images/multiple/")
+async def create_multiple_images(*, images: List[Image]):
+    return images
