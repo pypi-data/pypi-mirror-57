053a6aa3647f89f59116e653d0d1ad1ab3ecf385
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:58 +0000

    Add package Galaxy-ML/0.8.0/Galaxy-ML-0.8.0.tar.gz

---
+++ packages/Galaxy-ML/Galaxy-ML-0.8.0.tar.gz/Galaxy-ML-0.8.0/galaxy_ml/iraps_classifier.py
@@ -0,0 +1,17 @@
+"""
+This module has been moved. For backward compatibility, links are still
+provided.
+"""
+from .binarize_target import (IRAPSCore, IRAPSClassifier,
+                              _BinarizeTargetProbaScorer,
+                              BinarizeTargetClassifier,
+                              BinarizeTargetRegressor,
+                              BinarizeTargetTransformer,
+                              binarize_auc_scorer,
+                              binarize_average_precision_scorer)
+
+
+__all__ = ('IRAPSCore', 'IRAPSClassifier', 'binarize_auc_scorer',
+           'binarize_average_precision_scorer', 'BinarizeTargetClassifier',
+           'BinarizeTargetRegressor', 'BinarizeTargetTransformer',
+           '_BinarizeTargetProbaScorer')
