2b0a570729a934bba32e4236c48249eeb6a2431c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:31 +0000

    Add package amazonpy/1.0.2/amazonpy-1.0.2-py3-none-any.whl

---
+++ packages/amazonpy/amazonpy-1.0.2-py3-none-any.whl/amazonpy/config.py
@@ -0,0 +1,14 @@
+from fake_useragent import UserAgent
+
+class Config:
+    host = 'https://www.amazon.co.jp/'
+    p_url = host + 'dp/{}/'
+
+    price_classes = ['priceBlockBuyingPriceString', 'priceBlockSalePriceString']
+
+    temp_dir = 'temp/{}/'
+
+    header = {
+        "Referer": "https://www.amazon.co.jp/",
+        "User-Agent": UserAgent().safari
+    }
