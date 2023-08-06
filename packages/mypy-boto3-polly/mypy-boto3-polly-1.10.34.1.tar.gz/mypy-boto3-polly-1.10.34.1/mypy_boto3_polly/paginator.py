3b2ab43f7b6d0532c979b66d5cb4fdcd3b3bf736
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:02 +0000

    Add package mypy-boto3-polly/1.10.34.1/mypy-boto3-polly-1.10.34.1.tar.gz

---
+++ packages/mypy-boto3-polly/mypy-boto3-polly-1.10.34.1.tar.gz/mypy-boto3-polly-1.10.34.1/mypy_boto3_polly/paginator.py
@@ -0,0 +1,97 @@
+"Main interface for polly service Paginators"
+from __future__ import annotations
+
+import sys
+from botocore.paginate import Paginator as Boto3Paginator
+from mypy_boto3_polly.type_defs import (
+    DescribeVoicesOutputTypeDef,
+    ListLexiconsOutputTypeDef,
+    ListSpeechSynthesisTasksOutputTypeDef,
+    PaginatorConfigTypeDef,
+)
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+
+__all__ = ("DescribeVoicesPaginator", "ListLexiconsPaginator", "ListSpeechSynthesisTasksPaginator")
+
+
+class DescribeVoicesPaginator(Boto3Paginator):
+    """
+    [Paginator.DescribeVoices documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.34/reference/services/polly.html#Polly.Paginator.DescribeVoices)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self,
+        Engine: Literal["standard", "neural"] = None,
+        LanguageCode: Literal[
+            "arb",
+            "cmn-CN",
+            "cy-GB",
+            "da-DK",
+            "de-DE",
+            "en-AU",
+            "en-GB",
+            "en-GB-WLS",
+            "en-IN",
+            "en-US",
+            "es-ES",
+            "es-MX",
+            "es-US",
+            "fr-CA",
+            "fr-FR",
+            "is-IS",
+            "it-IT",
+            "ja-JP",
+            "hi-IN",
+            "ko-KR",
+            "nb-NO",
+            "nl-NL",
+            "pl-PL",
+            "pt-BR",
+            "pt-PT",
+            "ro-RO",
+            "ru-RU",
+            "sv-SE",
+            "tr-TR",
+        ] = None,
+        IncludeAdditionalLanguageCodes: bool = None,
+        PaginationConfig: PaginatorConfigTypeDef = None,
+    ) -> DescribeVoicesOutputTypeDef:
+        """
+        [DescribeVoices.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.34/reference/services/polly.html#Polly.Paginator.DescribeVoices.paginate)
+        """
+
+
+class ListLexiconsPaginator(Boto3Paginator):
+    """
+    [Paginator.ListLexicons documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.34/reference/services/polly.html#Polly.Paginator.ListLexicons)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self, PaginationConfig: PaginatorConfigTypeDef = None
+    ) -> ListLexiconsOutputTypeDef:
+        """
+        [ListLexicons.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.34/reference/services/polly.html#Polly.Paginator.ListLexicons.paginate)
+        """
+
+
+class ListSpeechSynthesisTasksPaginator(Boto3Paginator):
+    """
+    [Paginator.ListSpeechSynthesisTasks documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.34/reference/services/polly.html#Polly.Paginator.ListSpeechSynthesisTasks)
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self,
+        Status: Literal["scheduled", "inProgress", "completed", "failed"] = None,
+        PaginationConfig: PaginatorConfigTypeDef = None,
+    ) -> ListSpeechSynthesisTasksOutputTypeDef:
+        """
+        [ListSpeechSynthesisTasks.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.34/reference/services/polly.html#Polly.Paginator.ListSpeechSynthesisTasks.paginate)
+        """
