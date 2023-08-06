816762380fe8f2b55caf295ba2b4431c52e1b8c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:18 +0000

    Add package bolinette/0.0.11/Bolinette-0.0.11.tar.gz

---
+++ packages/bolinette/Bolinette-0.0.11.tar.gz/Bolinette-0.0.11/setup.py
@@ -0,0 +1,47 @@
+import setuptools
+from distutils.core import setup
+
+setup(
+    name='Bolinette',
+    packages=[
+        'bolinette',
+        'bolinette.cli',
+        'bolinette.cli.commands',
+        'bolinette.cli.nodes',
+        'bolinette.controllers',
+        'bolinette.marshalling',
+        'bolinette.models',
+        'bolinette.services',
+        'bolinette.testing'
+    ],
+    version='0.0.11',
+    license='MIT',
+    description='Bolinette, a web framework built on top of Flask',
+    author='Pierre Chat',
+    author_email='pierrechat89@hotmail.fr',
+    url='https://github.com/TheCaptainCat/bolinette',
+    keywords=['Flask', 'Bolinette', 'Web', 'Framework'],
+    install_requires=[
+        'Flask==1.1.1',
+        'Flask-Bcrypt==0.7.1',
+        'Flask-JWT-Extended==3.21.0',
+        'Flask-Script==2.0.6',
+        'Flask-SQLAlchemy==2.4.0',
+        'PyJWT==1.7.1'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+    ],
+    setup_requires=[
+        'wheel'
+    ],
+    entry_points= {
+        'console_scripts': [
+            'blnt=bolinette.cli:main'
+        ]
+    }
+)
