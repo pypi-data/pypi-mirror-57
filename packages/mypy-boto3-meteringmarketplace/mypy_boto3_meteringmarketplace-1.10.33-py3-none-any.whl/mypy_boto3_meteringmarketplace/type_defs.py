d8fa9b9e1dcc0a1b51b3f553969fe457596eb764
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:05 +0000

    Add package mypy-boto3-meteringmarketplace/1.10.33/mypy_boto3_meteringmarketplace-1.10.33-py3-none-any.whl

---
+++ packages/mypy-boto3-meteringmarketplace/mypy_boto3_meteringmarketplace-1.10.33-py3-none-any.whl/mypy_boto3_meteringmarketplace/type_defs.py
@@ -0,0 +1,80 @@
+"Main interface for meteringmarketplace service type defs"
+from __future__ import annotations
+
+from datetime import datetime
+import sys
+from typing import List
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+if sys.version_info >= (3, 8):
+    from typing import TypedDict
+else:
+    from typing_extensions import TypedDict
+
+
+ClientBatchMeterUsageResponseResultsUsageRecordTypeDef = TypedDict(
+    "ClientBatchMeterUsageResponseResultsUsageRecordTypeDef",
+    {"Timestamp": datetime, "CustomerIdentifier": str, "Dimension": str, "Quantity": int},
+    total=False,
+)
+
+ClientBatchMeterUsageResponseResultsTypeDef = TypedDict(
+    "ClientBatchMeterUsageResponseResultsTypeDef",
+    {
+        "UsageRecord": ClientBatchMeterUsageResponseResultsUsageRecordTypeDef,
+        "MeteringRecordId": str,
+        "Status": Literal["Success", "CustomerNotSubscribed", "DuplicateRecord"],
+    },
+    total=False,
+)
+
+ClientBatchMeterUsageResponseUnprocessedRecordsTypeDef = TypedDict(
+    "ClientBatchMeterUsageResponseUnprocessedRecordsTypeDef",
+    {"Timestamp": datetime, "CustomerIdentifier": str, "Dimension": str, "Quantity": int},
+    total=False,
+)
+
+ClientBatchMeterUsageResponseTypeDef = TypedDict(
+    "ClientBatchMeterUsageResponseTypeDef",
+    {
+        "Results": List[ClientBatchMeterUsageResponseResultsTypeDef],
+        "UnprocessedRecords": List[ClientBatchMeterUsageResponseUnprocessedRecordsTypeDef],
+    },
+    total=False,
+)
+
+_RequiredClientBatchMeterUsageUsageRecordsTypeDef = TypedDict(
+    "_RequiredClientBatchMeterUsageUsageRecordsTypeDef", {"Timestamp": datetime}
+)
+_OptionalClientBatchMeterUsageUsageRecordsTypeDef = TypedDict(
+    "_OptionalClientBatchMeterUsageUsageRecordsTypeDef",
+    {"CustomerIdentifier": str, "Dimension": str, "Quantity": int},
+    total=False,
+)
+
+
+class ClientBatchMeterUsageUsageRecordsTypeDef(
+    _RequiredClientBatchMeterUsageUsageRecordsTypeDef,
+    _OptionalClientBatchMeterUsageUsageRecordsTypeDef,
+):
+    pass
+
+
+ClientMeterUsageResponseTypeDef = TypedDict(
+    "ClientMeterUsageResponseTypeDef", {"MeteringRecordId": str}, total=False
+)
+
+ClientRegisterUsageResponseTypeDef = TypedDict(
+    "ClientRegisterUsageResponseTypeDef",
+    {"PublicKeyRotationTimestamp": datetime, "Signature": str},
+    total=False,
+)
+
+ClientResolveCustomerResponseTypeDef = TypedDict(
+    "ClientResolveCustomerResponseTypeDef",
+    {"CustomerIdentifier": str, "ProductCode": str},
+    total=False,
+)
