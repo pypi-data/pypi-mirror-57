337699f46420058f78c5db94232651bca18a238c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:32 +0000

    Add package altair-data-server/0.2.1/altair_data_server-0.2.1-py2.py3-none-any.whl

---
+++ packages/altair-data-server/altair_data_server-0.2.1-py2.py3-none-any.whl/altair_data_server/__init__.py
@@ -0,0 +1,5 @@
+"""Altair data server"""
+__version__ = '0.2.1'
+__all__ = ['AltairDataServer', 'data_server', 'data_server_proxied']
+
+from ._altair_server import AltairDataServer, data_server, data_server_proxied
