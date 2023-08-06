d42f8e86ad97658ef501b6667f86e10d56bb6dea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:41 +0000

    Add package tapioca-jarbas/1.0.1/tapioca_jarbas-1.0.1-py3-none-any.whl

---
+++ packages/tapioca-jarbas/tapioca_jarbas-1.0.1-py3-none-any.whl/tapioca_jarbas/resource_mapping.py
@@ -0,0 +1,30 @@
+RESOURCE_MAPPING = {
+    'reimbursement': {
+        'resource': 'reimbursement/{document_id}/',
+        'docs': 'https://github.com/datasciencebr/jarbas#get-apireimbursementdocument_id',
+    },
+    'receipt': {
+        'resource': 'reimbursement/{document_id}/receipt/',
+        'docs': 'https://github.com/datasciencebr/jarbas#get-apireimbursementdocument_idreceipt',
+    },
+    'reimbursement_list': {
+        'resource': 'reimbursement/',
+        'docs': 'https://github.com/datasciencebr/jarbas#get-apireimbursement',
+    },
+    'reimbursement_same_day': {
+        'resource': 'reimbursement/{document_id}/same_day/',
+        'docs': 'https://github.com/datasciencebr/jarbas#get-apireimbursementdocument_idsame_day',
+    },
+    'subquota': {
+        'resource': 'subquota/',
+        'docs': 'https://github.com/datasciencebr/jarbas#get-apisubquota',
+    },
+    'applicant': {
+        'resource': 'applicant/',
+        'docs': 'https://github.com/datasciencebr/jarbas#get-apiapplicant',
+    },
+    'company': {
+        'resource': 'company/{cnpj}/',
+        'docs': 'https://github.com/datasciencebr/jarbas#get-apicompanycnpj',
+    },
+}
---
+++ packages/tapioca-jarbas/tapioca_jarbas-1.0.1-py3-none-any.whl/tapioca_jarbas/tapioca_jarbas.py
@@ -0,0 +1,25 @@
+from tapioca import (
+    TapiocaAdapter, generate_wrapper_from_adapter, JSONAdapterMixin)
+
+from .resource_mapping import RESOURCE_MAPPING
+
+
+class JarbasClientAdapter(JSONAdapterMixin, TapiocaAdapter):
+    api_root = 'https://jarbas.serenata.ai/api/chamber_of_deputies/'
+    resource_mapping = RESOURCE_MAPPING
+
+    def get_iterator_list(self, response_data):
+        return response_data.get('results', response_data)
+
+    def get_iterator_next_request_kwargs(self, iterator_request_kwargs,
+                                         response_data, response):
+        next_url = response_data.get('next', '')
+        if not next_url:
+            return
+
+        iterator_request_kwargs['url'] = next_url
+        iterator_request_kwargs.pop('params', None)  # these are sent in the next_url
+        return iterator_request_kwargs
+
+
+Jarbas = generate_wrapper_from_adapter(JarbasClientAdapter)
