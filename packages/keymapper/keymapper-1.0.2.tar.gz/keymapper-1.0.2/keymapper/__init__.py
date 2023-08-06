ad7daf5e7dae47e08f7693ff95b7157e657cefad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:44 +0000

    Add package keymapper/1.0.2/keymapper-1.0.2.tar.gz

---
+++ packages/keymapper/keymapper-1.0.2.tar.gz/keymapper-1.0.2/keymapper/__init__.py
@@ -0,0 +1,112 @@
+import json, re
+
+
+class KeyMapper(dict):
+    """
+    Example:
+        km = KeyMapper({'messages': {'message1': 'Hello World!'}}})
+        print(km['messages.message1'])
+    Variables:
+        __delimiter__ is set to dot-notation by default, unless specified otherwise.
+    """
+    __delimiter__ = "."  # Default
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for arg in args:
+            if isinstance(arg, dict):
+                for k, v in arg.items():
+                    self.__dict__.update({k: v})
+        if kwargs:
+            self.__delimiter__ = kwargs['delimiter']
+
+    def __repr__(self):
+        return '{}(dict={})'.format(self.__class__, self.__dict__)
+
+    def __str__(self):
+        return 'Key Mapped: {}'.format(self.__dict__)
+
+    def __getattr__(self, attr):
+        try:
+            return self.get(attr)
+        except Exception as e:
+            raise e
+
+    def __setattr__(self, key, value):
+        try:
+            self.__setitem__(key, value)
+        except Exception as e:
+            raise e
+
+    def __delattr__(self, item):
+        try:
+            self.__delitem__(item)
+        except Exception as e:
+            raise e
+
+    def __getitem__(self, key):
+        try:
+            if self.__delimiter__ in key:
+                return self.__mapper__(self.__dict__, key.split(self.__delimiter__), self.__getitem__.__name__)
+            else:
+                return self.get(key)
+        except Exception as e:
+            raise e
+
+    def __setitem__(self, key, value):
+        try:
+            if self.__delimiter__ in key:
+                self.__mapper__(self.__dict__, key.split(self.__delimiter__), self.__setitem__.__name__, value)
+            else:
+                super().__setitem__(key, value)
+                self.__dict__.update({key: value})
+        except Exception as e:
+            raise e
+
+    def __delitem__(self, key):
+        try:
+            if self.__delimiter__ in key:
+                self.__mapper__(self.__dict__, key.split(self.__delimiter__), self.__delitem__.__name__)
+            else:
+                super().__delitem__(key)
+                del self.__dict__[key]
+        except Exception as e:
+            raise e
+
+    def pprint(self, *args):
+        try:
+            if len(args) > 0:
+                return json.dumps(args[0], indent=4, ensure_ascii=False)
+            return json.dumps(self, indent=4, ensure_ascii=False)
+        except Exception as e:
+            raise e
+
+    @classmethod
+    def __mapper__(cls, d, m, callback, *args, **kwargs):
+        for i, k in enumerate(m):
+            key = k if not re.search(r'^[0-9]+$', k) else int(k)
+            try:
+                if str(key) in d or type(key) == int and d[key]:
+                    if str(key) != m[-1] or i != len(m) - 1:
+                        return cls.__mapper__(d[key], m[1:], callback, *args, **kwargs)
+                    elif str(key) == m[-1] and i == len(m) - 1:
+                        if callback == '__setitem__':
+                            d[key] = args[0]
+                            return None
+                        elif callback == '__delitem__':
+                            del d[key]
+                            return None
+                        else:
+                            return d[key]
+            except Exception as e:
+                raise e
+            else:
+                if i == len(m) - 1:
+                    if callback == '__setitem__':
+                        d[m[-1]] = args[0]
+                        return None
+                else:
+                    raise KeyError('{}'.format(m[i]))
+        else:
+            if callback == '__getitem__':
+                return d
---
+++ packages/keymapper/keymapper-1.0.2.tar.gz/keymapper-1.0.2/setup.py
@@ -0,0 +1,38 @@
+import os
+from setuptools import setup, find_packages
+
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+with open(os.path.join(ROOT_DIR, 'README.md'), 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='keymapper',
+    version='1.0.2',
+    license='MIT',
+    description='Dictionary key mapping tool that enables the user to declare a single keyword regardless of depth.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://gitlab.com/rburns629/KeyMapper',
+    author='Robert Burns',
+    author_email='rburns629@gmail.com',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    keywords='dict dot delimiter key map dynamic dictionary object notation',
+    packages=find_packages(exclude=['tests']),
+    project_urls={
+        'Bug Reports': 'https://gitlab.com/rburns629/KeyMapper/issues',
+        'Source': 'https://gitlab.com/rburns629/KeyMapper',
+    },
+)
