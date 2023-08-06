f2d9fd3a42c216f5c421a1809ce7896ff45bcae7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:21 +0000

    Add package mypy-boto3-codebuild/1.10.36.1/mypy_boto3_codebuild-1.10.36.1-py3-none-any.whl

---
+++ packages/mypy-boto3-codebuild/mypy_boto3_codebuild-1.10.36.1-py3-none-any.whl/mypy_boto3_codebuild/__init__.py
@@ -0,0 +1,17 @@
+"Main interface for codebuild service"
+
+from mypy_boto3_codebuild.client import CodeBuildClient, CodeBuildClient as Client
+from mypy_boto3_codebuild.paginator import (
+    ListBuildsForProjectPaginator,
+    ListBuildsPaginator,
+    ListProjectsPaginator,
+)
+
+
+__all__ = (
+    "Client",
+    "CodeBuildClient",
+    "ListBuildsForProjectPaginator",
+    "ListBuildsPaginator",
+    "ListProjectsPaginator",
+)
