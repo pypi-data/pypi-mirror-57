a82a88d0d4350f455ff0b83b7e7964b201a293cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:06 +0000

    Add package cnntoolcore/0.1.5/cnntoolcore-0.1.5.tar.gz

---
+++ packages/cnntoolcore/cnntoolcore-0.1.5.tar.gz/cnntoolcore-0.1.5/plugins_management/plugin_selector.py
@@ -0,0 +1,12 @@
+from plugins_management.plugin_repository import get_plugin_repository
+
+
+class PluginSelector():
+    @staticmethod
+    def get_all_plugin_names():
+        return [plugin.name for plugin in get_plugin_repository().get_all_plugins()]
+
+    @staticmethod
+    def get_selected_plugins(name_list):
+        repository = get_plugin_repository()
+        return [repository.get_plugin(name) for name in name_list]
