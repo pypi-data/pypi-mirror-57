8bcd3fc4fcbcc67918d0ee654ebe9784748928c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:18 +0000

    Add package class-cli/0.3.1/class_cli-0.3.1.tar.gz

---
+++ packages/class-cli/class_cli-0.3.1.tar.gz/class_cli-0.3.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+ 
+setup(name='class_cli',
+      version='0.3.1',
+      url='https://github.com/yoavhayun/CLI',
+      license='GNU General Public License v3.0',
+      author='Yoav Hayun',
+      author_email='yoavhayun@gmail.com',
+      description='Converts a python class into a CLI program',
+      packages=find_packages(),
+      long_description=open("README.md").read(),
+      long_description_content_type="text/markdown",
+      zip_safe=False,
+      install_requires=['prompt_toolkit>=3.0'],
+      python_requires='>=3.0'
+)
