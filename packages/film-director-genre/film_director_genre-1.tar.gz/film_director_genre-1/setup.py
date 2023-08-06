d34e58334357aec54d9d404658ed2f343b129657
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:44 +0000

    Add package film-director-genre/1/film_director_genre-1.tar.gz

---
+++ packages/film-director-genre/film_director_genre-1.tar.gz/film_director_genre-1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='film_director_genre',
+    author='Sydorenko Valeriia',
+    packages=['film'],
+    install_requires=['pandas', 'matplotlib','numpy'],
+    version='1',
+    license='MIT',
+    description='This package analyzes data of films and directors',
+)
