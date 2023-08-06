fb2c0130141181d70ee603c34b87f476e33ff1dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:55 +0000

    Add package django-custom-user-models/0.0.1/django-custom-user-models-0.0.1.tar.gz

---
+++ packages/django-custom-user-models/django-custom-user-models-0.0.1.tar.gz/django-custom-user-models-0.0.1/CustomAuth/models/finance_mixin.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class FinanceMixin(models.Model):
+    wallet = models.PositiveIntegerField(
+        _('Credit of user'),
+        default=0
+    )
