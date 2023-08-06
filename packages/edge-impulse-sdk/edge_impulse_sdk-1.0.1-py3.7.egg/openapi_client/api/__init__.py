8c2975f9e7e5836c200c137dd21d857aa1e33d8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:45 +0000

    Add package edge-impulse-sdk/1.0.1/edge_impulse_sdk-1.0.1-py3.7.egg

---
+++ packages/edge-impulse-sdk/edge_impulse_sdk-1.0.1-py3.7.egg/openapi_client/api/__init__.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from openapi_client.api.auth_api import AuthApi
+from openapi_client.api.classify_api import ClassifyApi
+from openapi_client.api.dsp_api import DSPApi
+from openapi_client.api.deployment_api import DeploymentApi
+from openapi_client.api.devices_api import DevicesApi
+from openapi_client.api.impulse_api import ImpulseApi
+from openapi_client.api.jobs_api import JobsApi
+from openapi_client.api.learn_api import LearnApi
+from openapi_client.api.login_api import LoginApi
+from openapi_client.api.projects_api import ProjectsApi
+from openapi_client.api.raw_data_api import RawDataApi
+from openapi_client.api.user_api import UserApi
