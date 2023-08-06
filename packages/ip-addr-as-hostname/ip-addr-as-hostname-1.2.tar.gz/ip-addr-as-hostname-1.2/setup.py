b0b492c00dcc0296d2f84e11971a40e37c9fec42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:59 +0000

    Add package ip-addr-as-hostname/1.2/ip-addr-as-hostname-1.2.tar.gz

---
+++ packages/ip-addr-as-hostname/ip-addr-as-hostname-1.2.tar.gz/ip-addr-as-hostname-1.2/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='ip-addr-as-hostname',
+    version='1.2',
+    packages=[''],
+    url='',
+    license='LGPLv3',
+    author='alon-sage',
+    author_email='alon.sage@gmail.com',
+    description='',
+    install_requires=[
+        'dnspython'
+    ]
+)
