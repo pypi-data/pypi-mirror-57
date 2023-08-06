77e7a80e83a5b1fc173b9c85744e00bb42838be3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:33 +0000

    Add package robogame-engine/1.2.0/robogame_engine-1.2.0.tar.gz

---
+++ packages/robogame-engine/robogame_engine-1.2.0.tar.gz/robogame_engine-1.2.0/README.md
@@ -0,0 +1,5 @@
+[![Build Status](https://travis-ci.org/suguby/robogame_engine.svg?branch=master)](https://travis-ci.org/suguby/robogame_engine)
+
+### Robo Game Engine
+
+The package allows you to create RoboGames with python. As example see https://github.com/suguby/astrobox
---
+++ packages/robogame-engine/robogame_engine-1.2.0.tar.gz/robogame_engine-1.2.0/tests/test_moving.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+from robogame_engine.scene import Scene
+from robogame_engine.objects import GameObject
+from robogame_engine.geometry import Point
+
+
+class TestMoving(unittest.TestCase):
+
+    def setUp(self):
+        self.scene = Scene(field=(100, 100), theme_mod_path='tests.default_theme')
+
+    def test_move_at_point(self):
+        obj = GameObject(coord=Point(x=10, y=10))
+        obj.move_at(target=Point(x=10, y=300), speed=2)
+        for i in range(5):
+            self.scene.game_step()
+        self.assertEqual(obj.y, 20)
+
