f58e06c6c46ad09e02eddb5a3125f766bc6e7cbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:27 +0000

    Add package amb-distributions/0.1/amb_distributions-0.1.tar.gz

---
+++ packages/amb-distributions/amb_distributions-0.1.tar.gz/amb_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='amb_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['amb_distributions'],
+      zip_safe=False)
