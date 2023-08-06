25c360529972c67c20edc1313c43b5b892946039
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:36 +0000

    Add package mypy-boto3-cloudhsm/1.10.33/mypy-boto3-cloudhsm-1.10.33.tar.gz

---
+++ packages/mypy-boto3-cloudhsm/mypy-boto3-cloudhsm-1.10.33.tar.gz/mypy-boto3-cloudhsm-1.10.33/mypy_boto3_cloudhsm/paginator.py
@@ -0,0 +1,57 @@
+"Main interface for cloudhsm service Paginators"
+from __future__ import annotations
+
+from botocore.paginate import Paginator as Boto3Paginator
+from mypy_boto3_cloudhsm.type_defs import (
+    ListHapgsPaginatePaginationConfigTypeDef,
+    ListHapgsPaginateResponseTypeDef,
+    ListHsmsPaginatePaginationConfigTypeDef,
+    ListHsmsPaginateResponseTypeDef,
+    ListLunaClientsPaginatePaginationConfigTypeDef,
+    ListLunaClientsPaginateResponseTypeDef,
+)
+
+
+__all__ = ("ListHapgsPaginator", "ListHsmsPaginator", "ListLunaClientsPaginator")
+
+
+class ListHapgsPaginator(Boto3Paginator):
+    """
+    Paginator for `list_hapgs`
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self, PaginationConfig: ListHapgsPaginatePaginationConfigTypeDef = None
+    ) -> ListHapgsPaginateResponseTypeDef:
+        """
+        [ListHapgs.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.33/reference/services/cloudhsm.html#CloudHSM.Paginator.ListHapgs.paginate)
+        """
+
+
+class ListHsmsPaginator(Boto3Paginator):
+    """
+    Paginator for `list_hsms`
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self, PaginationConfig: ListHsmsPaginatePaginationConfigTypeDef = None
+    ) -> ListHsmsPaginateResponseTypeDef:
+        """
+        [ListHsms.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.33/reference/services/cloudhsm.html#CloudHSM.Paginator.ListHsms.paginate)
+        """
+
+
+class ListLunaClientsPaginator(Boto3Paginator):
+    """
+    Paginator for `list_luna_clients`
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self, PaginationConfig: ListLunaClientsPaginatePaginationConfigTypeDef = None
+    ) -> ListLunaClientsPaginateResponseTypeDef:
+        """
+        [ListLunaClients.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.33/reference/services/cloudhsm.html#CloudHSM.Paginator.ListLunaClients.paginate)
+        """
