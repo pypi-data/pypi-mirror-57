b28f355d1a150b1c0759fff6c771c895e1e94945
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:22 +0000

    Add package dagster-aws/0.6.6/dagster_aws-0.6.6-py3-none-any.whl

---
+++ packages/dagster-aws/dagster_aws-0.6.6-py3-none-any.whl/dagster_aws/s3/resources.py
@@ -0,0 +1,38 @@
+from dagster import Field, resource
+
+from .utils import create_s3_session
+
+
+class S3Resource(object):
+    def __init__(self, s3_session):
+        self.session = s3_session
+
+    def put_object(self, **kwargs):
+        '''This mirrors the put_object boto3 API:
+        https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3.html#S3.Bucket.put_object
+
+        The config schema for this API is applied to the put_object_to_s3_bytes solid vs. at the
+        resource level here.
+        '''
+        return self.session.put_object(**kwargs)
+
+    def upload_fileobj(self, fileobj, bucket, key):
+        '''This mirrors the upload_file boto3 API:
+        https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3.html#S3.Client.upload_fileobj
+        '''
+        return self.session.upload_fileobj(fileobj, bucket, key)
+
+
+@resource(
+    {
+        'use_unsigned_session': Field(
+            bool,
+            description='Specifies whether to use an unsigned S3 session',
+            is_optional=True,
+            default_value=True,
+        )
+    }
+)
+def s3_resource(context):
+    use_unsigned_session = context.resource_config['use_unsigned_session']
+    return S3Resource(create_s3_session(signed=not use_unsigned_session))
---
+++ packages/dagster-aws/dagster_aws-0.6.6-py3-none-any.whl/dagster_aws_tests/emr_tests/test_emr.py
@@ -0,0 +1,68 @@
+import pytest
+from dagster_aws.emr.solids import EmrRunJobFlowSolidDefinition
+from moto import mock_emr
+
+from dagster import execute_pipeline, pipeline
+
+
+@pytest.mark.skip
+@mock_emr
+def test_run_emr_job():
+    @pipeline
+    def test_pipe():
+        EmrRunJobFlowSolidDefinition('test')()
+
+    emr_config = {
+        'Name': 'test-pyspark',
+        'ReleaseLabel': 'emr-5.23.0',
+        'Instances': {
+            'MasterInstanceType': 'm4.large',
+            'SlaveInstanceType': 'm4.large',
+            'InstanceCount': 4,
+            'TerminationProtected': False,
+        },
+        'Applications': [{'Name': 'Spark'}],
+        'BootstrapActions': [
+            {
+                'Name': 'Spark Default Config',
+                'ScriptBootstrapAction': {
+                    'Path': 's3://support.elasticmapreduce/spark/maximize-spark-default-config'
+                },
+            }
+        ],
+        'Steps': [
+            {
+                'Name': 'Setup Debugging',
+                'ActionOnFailure': 'TERMINATE_CLUSTER',
+                'HadoopJarStep': {'Jar': 'command-runner.jar', 'Args': ['state-pusher-script']},
+            },
+            {
+                'Name': 'setup - copy files',
+                'ActionOnFailure': 'TERMINATE_CLUSTER',
+                'HadoopJarStep': {
+                    'Jar': 'command-runner.jar',
+                    'Args': [
+                        'aws',
+                        's3',
+                        'cp',
+                        's3://elementl-public/pyspark/hello_world.py',
+                        '/home/hadoop/',
+                    ],
+                },
+            },
+            {
+                'Name': 'Run Spark',
+                'ActionOnFailure': 'TERMINATE_CLUSTER',
+                'HadoopJarStep': {
+                    'Jar': 'command-runner.jar',
+                    'Args': ['spark-submit', '/home/hadoop/main.py'],
+                },
+            },
+        ],
+        'VisibleToAllUsers': True,
+        'JobFlowRole': 'EMR_EC2_DefaultRole',
+        'ServiceRole': 'EMR_DefaultRole',
+    }
+    config = {'solids': {'test': {'config': {'job_config': emr_config, 'aws_region': 'us-east-1'}}}}
+    result = execute_pipeline(test_pipe, config)
+    assert result.success
