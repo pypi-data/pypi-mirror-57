67894070e2c8d29b0136d6dc6b98bca3f018a5e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:26 +0000

    Add package dict-str/0.0.1/dict_str-0.0.1.tar.gz

---
+++ packages/dict-str/dict_str-0.0.1.tar.gz/dict_str-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dict_str',
+    version='0.0.1',
+    keywords='str to dict_str',
+    description='a str to dict_str tool',
+    license='MIT License',
+    url='https://github.com/xuehongbo/work',
+    author='HongBo Xue',
+    author_email='505386086@qq.com',
+    packages=find_packages(),
+    platforms='any',
+    install_requires=[],
+)
