f1b8d312e1931b18f8141674c016293e79ed24ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:35 +0000

    Add package birdspotter/0.1.1a1/birdspotter-0.1.1a1.tar.gz

---
+++ packages/birdspotter/birdspotter-0.1.1a1.tar.gz/birdspotter-0.1.1a1/setup.py
@@ -0,0 +1,36 @@
+import setuptools 
+from distutils.core import setup
+setup(
+        name = 'birdspotter',
+        packages = ['birdspotter'],
+        version = '0.1.1a1',
+        license = 'MIT',
+        description = 'A package to measure the influence and botness of twitter users, from twitter dumps',
+        author = 'Rohit Ram',
+        author_email = 'rohitram96@gmail.com',
+        url = 'https://github.com/behavioral-ds/BirdSpotter',
+        download_url = 'https://github.com/behavioral-ds/BirdSpotter/archive/0.1.1a1.tar.gz',
+        keywords = ['twitter', 'influence', 'botness', 'birdspotter', 'dumps'],
+        python_requires='>=3',
+        setup_requires=['wheel'],
+        install_requires=[
+            'numpy',
+            'pandas',
+            'python-dateutil',
+            'pytz',
+            'scikit-learn',
+            'scipy',
+            'six',
+            'sklearn',
+            'xgboost==0.81'
+        ],
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Developers',
+            'Topic :: Sociology',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+        ],
+    )
