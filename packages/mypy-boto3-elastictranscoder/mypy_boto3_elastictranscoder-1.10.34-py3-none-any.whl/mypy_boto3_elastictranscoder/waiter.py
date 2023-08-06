5e8ffb3d46b095e8f3b7e344815b7ff210ff18b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:50 +0000

    Add package mypy-boto3-elastictranscoder/1.10.34/mypy_boto3_elastictranscoder-1.10.34-py3-none-any.whl

---
+++ packages/mypy-boto3-elastictranscoder/mypy_boto3_elastictranscoder-1.10.34-py3-none-any.whl/mypy_boto3_elastictranscoder/waiter.py
@@ -0,0 +1,20 @@
+"Main interface for elastictranscoder service Waiters"
+from __future__ import annotations
+
+from botocore.waiter import Waiter as Boto3Waiter
+from mypy_boto3_elastictranscoder.type_defs import JobCompleteWaitWaiterConfigTypeDef
+
+
+__all__ = ("JobCompleteWaiter",)
+
+
+class JobCompleteWaiter(Boto3Waiter):
+    """
+    Waiter for `job_complete` name.
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def wait(self, Id: str, WaiterConfig: JobCompleteWaitWaiterConfigTypeDef = None) -> None:
+        """
+        [job_complete.wait documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.34/reference/services/elastictranscoder.html#ElasticTranscoder.Waiter.job_complete.wait)
+        """
