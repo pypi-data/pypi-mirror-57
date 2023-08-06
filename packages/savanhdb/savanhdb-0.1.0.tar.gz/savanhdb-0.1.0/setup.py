58206f19506fb2b436b8c9392868679d796cc303
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:04 +0000

    Add package savanhdb/0.1.0/savanhdb-0.1.0.tar.gz

---
+++ packages/savanhdb/savanhdb-0.1.0.tar.gz/savanhdb-0.1.0/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="savanhdb", 
+    version="0.1.0",
+    author="Sam Vanhoutte",
+    author_email="sam.vanhoutte@marchitec.be",
+    description="Databricks spark Azure packages",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/samvanhoutte/databricks",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires = [
+        'azure.eventgrid',
+        'json'
+    ],
+    python_requires='>=3.6',
+    setup_requires=['wheel']
+)
