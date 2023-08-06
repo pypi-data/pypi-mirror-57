90f6f619031e096f53068d5541fb330d2eb3d6ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:33 +0000

    Add package mypy-boto3-dax/1.10.36.0/mypy-boto3-dax-1.10.36.0.tar.gz

---
+++ packages/mypy-boto3-dax/mypy-boto3-dax-1.10.36.0.tar.gz/mypy-boto3-dax-1.10.36.0/mypy_boto3_dax/__init__.py
@@ -0,0 +1,24 @@
+"Main interface for dax service"
+
+from mypy_boto3_dax.client import Client
+from mypy_boto3_dax.paginator import (
+    DescribeClustersPaginator,
+    DescribeDefaultParametersPaginator,
+    DescribeEventsPaginator,
+    DescribeParameterGroupsPaginator,
+    DescribeParametersPaginator,
+    DescribeSubnetGroupsPaginator,
+    ListTagsPaginator,
+)
+
+
+__all__ = (
+    "Client",
+    "DescribeClustersPaginator",
+    "DescribeDefaultParametersPaginator",
+    "DescribeEventsPaginator",
+    "DescribeParameterGroupsPaginator",
+    "DescribeParametersPaginator",
+    "DescribeSubnetGroupsPaginator",
+    "ListTagsPaginator",
+)
