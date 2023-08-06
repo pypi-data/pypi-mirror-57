9f874553da34fd94b0833c37004b870e8ce0d6c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:55 +0000

    Add package metabasepy2/2.0.0/metabasepy2-2.0.0-py2.py3-none-any.whl

---
+++ packages/metabasepy2/metabasepy2-2.0.0-py2.py3-none-any.whl/metabasepy2/__init__.py
@@ -0,0 +1,11 @@
+from metabasepy2.client import (
+    Client,
+    AuthorizationFailedException,
+    RequestException
+)
+
+from metabasepy2.table_parser import (
+    MetabaseTableParser,
+    MetabaseTable,
+    MetabaseResultInvalidException
+)
