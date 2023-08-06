7a6f2ee998a212bacd882f419ec6619f458ac89a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:33 +0000

    Add package extra-tools/1.0.0/extra_tools-1.0.0.tar.gz

---
+++ packages/extra-tools/extra_tools-1.0.0.tar.gz/extra_tools-1.0.0/README.md
@@ -0,0 +1,2 @@
+#There is nothing here
+Get Lost
---
+++ packages/extra-tools/extra_tools-1.0.0.tar.gz/extra_tools-1.0.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+
+setup(
+    name="extra_tools",
+    version="1.0.0",
+    description="A Python package for extra_tools",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/nikhilkumarsingh/weather-reporter",
+    author="AnupamKris",
+    author_email="anupamkris13262@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["extratools"],
+    include_package_data=True,
+    install_requires=["requests"],
+    # entry_points={
+    #     "console_scripts": [
+    #         "weather-reporter=weather_reporter.cli:main",
+    #     ]
+    # },
+)
