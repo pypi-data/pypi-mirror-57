8fd28aa1bd59fa29e34412529e8b12c5b633c04b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:55 +0000

    Add package mypy-boto3-savingsplans/1.10.36.1/mypy-boto3-savingsplans-1.10.36.1.tar.gz

---
+++ packages/mypy-boto3-savingsplans/mypy-boto3-savingsplans-1.10.36.1.tar.gz/mypy-boto3-savingsplans-1.10.36.1/mypy_boto3_savingsplans/__init__.py
@@ -0,0 +1,6 @@
+"Main interface for savingsplans service"
+
+from mypy_boto3_savingsplans.client import SavingsPlansClient as Client, SavingsPlansClient
+
+
+__all__ = ("Client", "SavingsPlansClient")
