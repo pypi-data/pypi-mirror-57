da72d7b34bb1c56fdd511d60dc058bf9ec13e98d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:48 +0000

    Add package color2vec/1.0.3/color2vec-1.0.3.tar.gz

---
+++ packages/color2vec/color2vec-1.0.3.tar.gz/color2vec-1.0.3/setup.py
@@ -0,0 +1,39 @@
+import setuptools
+import os
+print(os.getcwd())
+
+# from pip.req import parse_requirements
+
+def parse_requirements(filename):
+    """ load requirements from a pip requirements file """
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#")]
+
+
+install_reqs = parse_requirements("requirements.txt")
+# print(install_reqs)
+reqs = install_reqs
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="color2vec",
+    version="1.0.3",
+    author="chandan mishra",
+    author_email="chandan.mishra@greendeck.co",
+    description="Greendeck color to vector package.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=['color2vec', 'color2vec.src', 'color2vec.src.data.', 'color2vec.src.model.'],
+    package_data={'color2vec': ['src/data/*', 'src/model/*']},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=reqs,
+    include_package_data=True,
+    zip_safe=False
+)
