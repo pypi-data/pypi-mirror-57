50c81ceb3c2b635206ba923f61d00b51b84d711b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:49 +0000

    Add package PyScorecard/0.2/PyScorecard-0.2.tar.gz

---
+++ packages/PyScorecard/PyScorecard-0.2.tar.gz/PyScorecard-0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+	name='PyScorecard',
+	url='https://github.com/deino475/PyScorecard',
+	author='Nile Dixon',
+	author_email='niledixon475@gmail.com',
+	packages=['PyScorecard'],
+	install_requires=['requests'],
+	version='0.2',
+	license='GNU GPL-V2',
+	description='An unofficial API wrapper for the College Scorecard API.',
+	long_description=open('README.rst').read(),
+)
