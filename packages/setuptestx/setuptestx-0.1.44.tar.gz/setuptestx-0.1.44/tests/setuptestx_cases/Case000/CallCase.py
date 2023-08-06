790604fff3768a11b1408c7696024f4f7325132f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:43 +0000

    Add package setuptestx/0.1.44/setuptestx-0.1.44.tar.gz

---
+++ packages/setuptestx/setuptestx-0.1.44.tar.gz/setuptestx-0.1.44/tests/setuptestx_cases/Case000/CallCase.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import print_function
+
+import unittest
+
+class CallUnits(unittest.TestCase):
+
+    def testCall010(self):
+        
+        print("4TEST:")
+        # success when called at all
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
