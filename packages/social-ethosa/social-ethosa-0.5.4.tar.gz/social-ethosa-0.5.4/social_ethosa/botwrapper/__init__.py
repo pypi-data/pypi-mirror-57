726c6efb415664893f6e3e8fbab1506c8b260efc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:43 +0000

    Add package social-ethosa/0.5.4/social-ethosa-0.5.4.tar.gz

---
+++ packages/social-ethosa/social-ethosa-0.5.4.tar.gz/social-ethosa-0.5.4/social_ethosa/botwrapper/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# author: Ethosa
+
+from .botwrapper import BotWrapper
+from .User import User
+from .BetterBotBase import BetterBotBase
+from .BotBase import BotBase
+from .BetterUser import BetterUser
