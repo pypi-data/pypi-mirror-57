264a80e19bf1aae36a1175c6e18b879b7a6b8688
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:47 +0000

    Add package p3-test-driver/2.0.1/p3_test_driver-2.0.1.tar.gz

---
+++ packages/p3-test-driver/p3_test_driver-2.0.1.tar.gz/p3_test_driver-2.0.1/setup.py
@@ -0,0 +1,31 @@
+
+from setuptools import setup
+
+setup(
+    name='p3_test_driver',
+    version='2.0.1',
+    description='P3 Test Driver is designed to run a variety of benchmarks using an easily expandable plug-in system.',
+    url='http://github.com/claudiofahey/p3_test_driver',
+    author='Claudio Fahey',
+    author_email='claudio.fahey@dell.com',
+    packages=['p3_test_driver'],
+    package_data={
+        'p3_test_driver': ['plugins/*.py'],
+    },
+    install_requires=[
+        'requests',
+        'six',
+        'PyYaml',
+        'yapsy',
+    ],
+    entry_points={
+        'console_scripts': [
+            'p3_test_driver = p3_test_driver.p3_test_driver:main',
+        ],
+    },
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
