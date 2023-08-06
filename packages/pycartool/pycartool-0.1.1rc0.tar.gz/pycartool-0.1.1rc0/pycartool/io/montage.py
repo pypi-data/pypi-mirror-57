46d8fde6cb606df6c58fbc6f8c8b96e487d1667c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:14 +0000

    Add package pycartool/0.1.1rc0/pycartool-0.1.1rc0.tar.gz

---
+++ packages/pycartool/pycartool-0.1.1rc0.tar.gz/pycartool-0.1.1rc0/pycartool/io/montage.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# Authors: Tanguy Vivier <tanguy.viv@gmail.com>
+#          Victor Férat <victor.ferat@live.fr>
+#
+# License: BSD (3-clause)
+
+from mne.channels import make_dig_montage
+import numpy as np
+
+def read_xyz(filename):
+    """Reads and convert xyz positions to a mne montage type
+
+    Parameters
+    ----------
+    filename : str
+        The filename of the xyz file.
+    Returns
+    -------
+    montage : mne.channels.montage.Montage
+        Montage for EEG electrode locations.
+    """
+
+    n = int(open(filename).readline().lstrip().split(' ')[0])
+    coord = np.loadtxt(filename, skiprows=1, usecols=(0, 1, 2), max_rows=n)
+    names = np.loadtxt(filename, skiprows=1, usecols=3, max_rows=n,
+                       dtype=np.dtype(str))
+    names = names.tolist()
+    ch_pos = dict()
+    for i, name in enumerate(names):
+        ch_pos[name] = coord[i]
+    montage = make_dig_montage(ch_pos=ch_pos, coord_frame='head')
+    return montage
---
+++ packages/pycartool/pycartool-0.1.1rc0.tar.gz/pycartool-0.1.1rc0/pycartool/tests/test_sef.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Authors: Victor Férat <victor.ferat@live.fr>
+#
+# License: BSD (3-clause)
+
+import os
+from ..io.sef import read_sef, write_sef
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+data_path = os.path.join(dir_path, "data")
+
+datasef = os.path.join(data_path, "sample-sef.sef")
+
+
+def test_read_sef():
+    read_sef(datasef)
+    return()
+
+
+def test_write_sef():
+    sef = read_sef(datasef)
+    write_sef("test.sef", sef)
+    return()
