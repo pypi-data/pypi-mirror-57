620baaecae43ea0e023ee939053f3e87c9b45fdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:21 +0000

    Add package stancmd/0.2.1/stancmd-0.2.1.tar.gz

---
+++ packages/stancmd/stancmd-0.2.1.tar.gz/stancmd-0.2.1/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name='stancmd',
+    version='0.2.1',
+    description='A useless command line patience tester',
+    url='https://gitlab.com/quentin-dev/stan',
+    author='Quentin Barbarat',
+    author_email='q.barbarat@gmail.com',
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    entry_points={
+        'console_scripts': ['stancmd=stan.stan:main'],
+    }
+)
