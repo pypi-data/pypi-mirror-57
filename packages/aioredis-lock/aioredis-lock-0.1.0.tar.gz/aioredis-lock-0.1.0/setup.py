acd013991bf19861c10a152254ee816f6a57ab02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:29 +0000

    Add package aioredis-lock/0.1.0/aioredis-lock-0.1.0.tar.gz

---
+++ packages/aioredis-lock/aioredis-lock-0.1.0.tar.gz/aioredis-lock-0.1.0/setup.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+from setuptools import find_packages, setup
+
+
+__version__ = "0.1.0"
+
+
+with open(Path(__file__).parent / "README.md") as f:
+    long_description = f.read()
+
+
+setup(
+    author="Matt Rasband",
+    author_email="matt.rasband@gmail.com",
+    description="Wrapper to provide distributed locks in aioredis",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=["aioredis"],
+    name="aioredis-lock",
+    packages=find_packages(exclude=["tests"]),
+    python_requires=">=3.7",
+    url="https://github.com/mrasband/aioredis-lock",
+    version=__version__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: Implementation :: CPython",
+    ],
+    keywords=["aioredis", "redis", "locking", "distributed", "asyncio"],
+)
