a3d31965373552995853fa44dc0651a9bb5c0de0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:31 +0000

    Add package mypy-boto3-support/1.10.37.1/mypy_boto3_support-1.10.37.1-py3-none-any.whl

---
+++ packages/mypy-boto3-support/mypy_boto3_support-1.10.37.1-py3-none-any.whl/mypy_boto3_support/__init__.py
@@ -0,0 +1,7 @@
+"Main interface for support service"
+
+from mypy_boto3_support.client import SupportClient as Client, SupportClient
+from mypy_boto3_support.paginator import DescribeCasesPaginator, DescribeCommunicationsPaginator
+
+
+__all__ = ("Client", "DescribeCasesPaginator", "DescribeCommunicationsPaginator", "SupportClient")
