e2815fe21f953d856d958ea9594fd09e50105307
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:02 +0000

    Add package chromeprinter/0.0.2/chromeprinter-0.0.2.tar.gz

---
+++ packages/chromeprinter/chromeprinter-0.0.2.tar.gz/chromeprinter-0.0.2/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name='chromeprinter',
+    version='0.0.2',
+    packages=setuptools.find_packages(),
+    url='',
+    author='OLixao',
+    install_requires=['selenium'],
+    author_email='contact@olixao.ml',
+    description='Print chrome page with chromedriver',
+    long_description='none',
+    long_description_content_type="text/markdown"
+)
