7741ed244bd3c8cb6a9791753d7a059533e41655
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:31 +0000

    Add package datamodelz/0.1.1/datamodelz-0.1.1.tar.gz

---
+++ packages/datamodelz/datamodelz-0.1.1.tar.gz/datamodelz-0.1.1/datamodelz/consts.py
@@ -0,0 +1,19 @@
+# metadata fields
+id_string = "id"
+calculated_string = "calculated"
+frequency_string = "frequency"
+min_value_string = "minValue"
+max_value_string = "maxValue"
+start_time_string = "startTime"
+is_integer_string = "isInteger"
+delay_string = "delay"
+
+# frequencies
+instant_string = "instant"
+daily_string = "daily"
+weekly_string = "weekly"
+monthly_string = "monthly"
+quarterly_string = "quarterly"
+annually_string = "annually"
+
+frequencies = [instant_string, daily_string, weekly_string, monthly_string, quarterly_string, annually_string]
