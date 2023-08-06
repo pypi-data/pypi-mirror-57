88308fe23c5b9b4fa7b828c37d0a89c9b1a29da8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:41 +0000

    Add package esm-analysis/0.1.0/esm_analysis-0.1.0.tar.gz

---
+++ packages/esm-analysis/esm_analysis-0.1.0.tar.gz/esm_analysis-0.1.0/esm_analysis/cli.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+"""Console script for esm_analysis."""
+import logging
+import sys
+
+import click
+
+from esm_analysis import EsmAnalysis
+
+@click.group()
+@click.option("--verbose", default=False, is_flag=True)
+def main(args=None, verbose=False):
+    """Console script for esm_analysis."""
+    if verbose:
+        logging.basicConfig(level=logging.INFO)
+    return 0
+
+@main.command()
+@click.argument('varname')
+@click.option("--preferred_analysis_dir", default=None)
+def fldmean(varname, preferred_analysis_dir=None):
+    """Fldmean generator
+
+    Parameters
+    ----------
+    varname : str
+        The variable name to make a fldmean for. This will automatically figure
+        out which model ``varname`` belongs to.
+
+    Examples
+    --------
+
+    ..code ::
+
+        $ esm_analysis fldmean temp2
+    """
+    click.echo("This will generate a fldmean for: %s" % varname)
+    click.echo("You passed in preferred_analysis_dir: %s" % preferred_analysis_dir)
+    analyzer = EsmAnalysis(preferred_analysis_dir=preferred_analysis_dir)
+    analyzer.initialize_analysis_components(preferred_analysis_dir=preferred_analysis_dir)
+    analyzer.fldmean(varname)
+
+
+if __name__ == "__main__":
+    sys.exit(main())  # pragma: no cover
