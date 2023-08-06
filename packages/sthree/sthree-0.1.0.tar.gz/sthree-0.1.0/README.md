ec61a7d43336b2d75b4bd82edda663b99de126c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:36 +0000

    Add package sthree/0.1.0/sthree-0.1.0.tar.gz

---
+++ packages/sthree/sthree-0.1.0.tar.gz/sthree-0.1.0/README.md
@@ -0,0 +1,32 @@
+# Typed access to Amazon S3 for humans
+
+## Installation
+
+```shell script
+pip install three
+```
+
+`three` requires `boto3` for its operation, but it is not listed as package dependency because, if you'd want to use `three` in AWS Lambda, you do not need to install `boto3` there - since it is already installed.
+
+If you're not using Lambda you may run
+
+```shell script
+pip install three[boto]
+```
+
+to make sure all dependencies are satisfied.
+
+## Usage Examples
+
+```python
+from sthree import Keys
+from itertools import islice    
+
+for key in islice(Keys('my-bucket'), 10):
+    print(key)
+
+```
+
+## Motivation
+
+Motivation to build this library is to simplify using Amazon S3 in Python applications and to make development experience a bit more pleasant and Pythonic, as well as to benefit from Python's type hinting capabilities.   
---
+++ packages/sthree/sthree-0.1.0.tar.gz/sthree-0.1.0/setup.py
@@ -0,0 +1,43 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sthree",
+    version="0.1.0",
+    author="Anatoly Scherbakov",
+    author_email="altaisoft@gmail.com",
+    description=(
+        "A suite of typed data structures backed by Amazon S3."
+    ),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/anatoly-scherbakov/sthree',
+    packages=setuptools.find_packages(exclude=[
+        'tests',
+        'run.py'
+    ]),
+    install_requires=[
+        'boto3_type_annotations'
+    ],
+    extras_require={
+        'boto': [
+            'boto3'
+        ],
+        'dev': [
+            'pytest',
+            'boto3',
+            'moto',
+            'fire',
+            'twine'
+        ]
+    },
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
