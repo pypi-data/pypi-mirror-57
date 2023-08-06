27058326cefdcd7a6f4b26a227f0bdbc7b787da5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:09 +0000

    Add package sysrsync/0.2.0/sysrsync-0.2.0.tar.gz

---
+++ packages/sysrsync/sysrsync-0.2.0.tar.gz/sysrsync-0.2.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['sysrsync', 'sysrsync.helpers']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['toml>=0.10.0,<0.11.0']
+
+setup_kwargs = {
+    'name': 'sysrsync',
+    'version': '0.2.0',
+    'description': 'Simple and safe python wrapper for calling system rsync',
+    'long_description': None,
+    'author': 'Gabriel Chamon',
+    'author_email': 'gchamon@live.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
