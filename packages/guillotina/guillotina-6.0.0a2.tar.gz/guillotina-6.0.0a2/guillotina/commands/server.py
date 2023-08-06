890e01d17bba5fd5111f33a8d4a7db304d6ab0be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:58 +0000

    Add package guillotina/6.0.0a2/guillotina-6.0.0a2.tar.gz

---
+++ packages/guillotina/guillotina-6.0.0a2.tar.gz/guillotina-6.0.0a2/guillotina/commands/server.py
@@ -0,0 +1,53 @@
+from guillotina.commands import Command
+
+
+class ServerCommand(Command):
+    description = "Guillotina server runner"
+    profiler = line_profiler = None
+
+    def get_parser(self):
+        parser = super(ServerCommand, self).get_parser()
+        parser.add_argument(
+            "-r",
+            "--reload",
+            action="store_true",
+            dest="reload",
+            help="Auto reload on code changes",
+            default=False,
+        )
+        parser.add_argument("--port", help="Override port to run this server on", default=None, type=int)
+        parser.add_argument("--host", help="Override host to run this server on", default=None)
+
+        parser.add_argument("--asgi-server", default="uvicorn", type=str)
+        return parser
+
+    def run(self, arguments, settings, app):
+        port = arguments.port or settings.get("address", settings.get("port"))
+        host = arguments.host or settings.get("host", "0.0.0.0")
+
+        if arguments.asgi_server == "uvicorn":
+            import uvicorn  # type: ignore
+
+            config = uvicorn.Config(
+                app,
+                host=host,
+                port=port,
+                reload=arguments.reload,
+                access_log=False,
+                **settings["server_settings"]["uvicorn"],
+            )
+            server = uvicorn.Server(config)
+            self.loop.run_until_complete(server.serve())
+
+        elif arguments.asgi_server == "hypercorn":
+            import asyncio
+
+            from hypercorn.asyncio import serve  # type: ignore
+            from hypercorn.config import Config  # type: ignore
+
+            config = Config()
+            config.bind = [f"{host}:{port}"]
+            config.use_reloader = arguments.reload
+            asyncio.run(serve(app, config))
+        else:
+            raise Exception(f"Server {arguments.asgi_server} not supported")
