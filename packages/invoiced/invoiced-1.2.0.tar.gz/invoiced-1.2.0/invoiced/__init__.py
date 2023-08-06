e464981eb034fe05ffd0bfa01b4c962028b68f18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:18 +0000

    Add package invoiced/1.2.0/invoiced-1.2.0.tar.gz

---
+++ packages/invoiced/invoiced-1.2.0.tar.gz/invoiced-1.2.0/invoiced/__init__.py
@@ -0,0 +1,28 @@
+from invoiced.client import Client  # noqa
+from invoiced.version import VERSION  # noqa
+from invoiced.objects import (  # noqa
+    Attachment,
+    BankAccount,
+    Card,
+    CatalogItem,
+    Contact,
+    Coupon,
+    CreditNote,
+    Customer,
+    Email,
+    Estimate,
+    Event,
+    File,
+    Invoice,
+    Letter,
+    LineItem,
+    Note,
+    PaymentPlan,
+    PaymentSource,
+    Plan,
+    Subscription,
+    Task,
+    TaxRate,
+    TextMessage,
+    Transaction)
+from invoiced.operations import List  # noqa
