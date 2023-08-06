64adc2ef3691a525467970259e221adfde874797
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:45 +0000

    Add package qtrer/1.0.1/qtrer-1.0.1.tar.gz

---
+++ packages/qtrer/qtrer-1.0.1.tar.gz/qtrer-1.0.1/qt_ts.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import xml.etree.ElementTree as ElementTree
+import logging
+
+
+class QtTs(object):
+    def __init__(self, ts_path, tr_table):
+        self.ts_path = ts_path
+        self.xml_tree = ElementTree.parse(ts_path)
+        self.tr_table = tr_table
+
+    def __get_tr_list(self, locale_name):
+        if locale_name in self.tr_table:
+            return self.tr_table[locale_name]
+        else:
+            return []
+
+    def __get_target_tr_list(self, locale_name):
+        logging.info("target locale name: %s" % locale_name)
+        tr_list = self.__get_tr_list(locale_name)
+        if tr_list:
+            return tr_list
+
+        language_name = locale_name[0:2]
+        for key in self.tr_table.keys():
+            if key.startswith(language_name):
+                logging.info("Found transltion %s" % key)
+                return self.tr_table[key]
+
+        logging.error("Fail to translate to %s", locale_name)
+        raise KeyError("%s: Target translation NOT found" % locale_name)
+
+    def __get_tr(self, source_string, source_tr_list, target_tr_list):
+        try:
+            index = source_tr_list.index(source_string)
+            return target_tr_list[index]
+        except (ValueError, IndexError):
+            logging.error("no source string '%s' found" % source_string)
+            raise
+
+    def tr(self, locale_name):
+        root = self.xml_tree.getroot()
+        source_tr_list = self.__get_tr_list("source")
+        if not source_tr_list:
+            logging.error("no source list found")
+            exit(-1)
+        target_tr_list = self.__get_target_tr_list(locale_name)
+        en_tr_list = self.__get_target_tr_list("en")
+        for context in root:
+            for message in context:
+                if message.tag != "message":
+                    continue
+                source = message.find("source")
+                target_string = self.__get_tr(source.text, source_tr_list,
+                                              target_tr_list)
+                if target_string is None:
+                    continue
+                if target_string.strip() == "":  #若翻译词条为空，使用英文翻译
+                    target_string = self.__get_tr(source.text, source_tr_list,
+                                                  en_tr_list)
+
+                translation = message.find("translation")
+                translation.attrib.clear()
+                translation.text = target_string
+                logging.info(
+                    "translate %s to %s" % (source.text, translation.text))
+
+    def save(self, path):
+        self.xml_tree.write(path, encoding="utf-8", xml_declaration=True)
+        with open(path, "r+", encoding="utf-8") as file:
+            contents = file.readlines()
+            contents.insert(1, "<!DOCTYPE TS>\n")
+            file.seek(0)
+            file.writelines(contents)
+
+
+if __name__ == "__main__":
+    ts = QtTs("./zh_CN.ts")
+    ts.set_target_language("zh", "CN")
+    ts.save()
---
+++ packages/qtrer/qtrer-1.0.1.tar.gz/qtrer-1.0.1/qtrer.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Translate Qt ts file
+
+Usage:
+    qtrer [--ts_dir=<ts_dir> --excel_dir=<excel_dir>]
+
+Options:
+    --ts_dir=<ts_dir>                       Qt翻译文件的目录
+    --excel_dir=<excel_dir>               Excel翻译文件的路径
+"""
+
+import os
+import logging
+import sys
+import docopt
+
+from excel_parser import ExcelParser
+from qt_ts import QtTs
+from opencc_translator import OpenccTranslator
+
+
+def main():
+    arg = docopt.docopt(__doc__)
+    qt_ts_file_dir = arg["--ts_dir"]
+    translation_file_dir = arg["--excel_dir"]
+
+    if qt_ts_file_dir is None:
+        qt_ts_file_dir = "./translation"
+    if translation_file_dir is None:
+        translation_file_dir = "./doc/translation"
+
+    translation = {}
+    for item in os.listdir(translation_file_dir):
+        file_path = os.path.join(translation_file_dir, item)
+        if not os.path.isfile(file_path) or not file_path.endswith(".xlsx"):
+            continue
+
+        parser = ExcelParser()
+        parser.parse(file_path)
+        translation.update(parser.translations)
+
+    zh_TW_translator = OpenccTranslator("s2twp")
+    translation["zh_TW"] = zh_TW_translator.generate(translation["zh_CN"])
+
+    zh_HK_translator = OpenccTranslator("s2hk")
+    translation["zh_HK"] = zh_HK_translator.generate(translation["zh_CN"])
+    logging.info(translation.keys())
+
+    for item in os.listdir(qt_ts_file_dir):
+        file_path = os.path.join(qt_ts_file_dir, item)
+        if not file_path.endswith(".ts"):
+            continue
+
+        qt_ts = QtTs(file_path, translation)
+        locale_name = os.path.splitext(os.path.basename(file_path))[0]
+        try:
+            qt_ts.tr(locale_name)
+        except KeyError as e:
+            logging.error(e.args)
+            raise
+        qt_ts.save(file_path)
+
+
+if __name__ == "__main__":
+    main()
