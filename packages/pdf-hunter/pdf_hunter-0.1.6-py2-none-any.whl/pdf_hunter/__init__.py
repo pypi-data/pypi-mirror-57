403c4de001c6487533ce3df9a33262484dd60dc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:16 +0000

    Add package pdf-hunter/0.1.6/pdf_hunter-0.1.6-py2-none-any.whl

---
+++ packages/pdf-hunter/pdf_hunter-0.1.6-py2-none-any.whl/pdf_hunter/__init__.py
@@ -0,0 +1,9 @@
+from .search import (  # noqa: F401
+    get_pdf_urls,
+    validate_pdf_url,
+    get_pdf_name,
+    download_file,
+    download_pdf_files,
+)
+
+from .url_transforms import UrlTransforms  # noqa: F401
