cd2afafc0d102a14819a60922df61c98fb583e5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:43 +0000

    Add package tmalibrary/9.12/tmalibrary-9.12-py3-none-any.whl

---
+++ packages/tmalibrary/tmalibrary-9.12-py3-none-any.whl/tmalibrary/actuator/HandleRequest.py
@@ -0,0 +1,35 @@
+from .KeyManager import KeyManager
+import base64
+import json
+from .ActuatorPayload import ActuatorPayload
+import os
+from flask import Response
+
+class HandleRequest:
+
+	def generateResponse(self, plainResponse):
+
+		keymanager = KeyManager()
+		privateKeyPath = "keys/priv-key-actuator"
+		privateKey = keymanager.getPrivateKey(privateKeyPath)
+
+		signedResponse = keymanager.sign(plainResponse,privateKey)
+		signedResponseEncoded = base64.b64encode(signedResponse)
+
+		publicKeyExecutorPath = "keys/pub-key-executor"
+		publicKeyExecutor = keymanager.getPublicKey(publicKeyExecutorPath)
+		encryptedMessage = keymanager.encrypt(plainResponse,publicKeyExecutor)
+		response = base64.b64encode(encryptedMessage)
+		return Response("{}\n{}".format(response, signedResponseEncoded), mimetype='application/octet-stream')
+
+	def processRequest(self, request):
+
+		# TODO: Handle Requests where the key is not valid
+
+		privateKeyPath = "keys/priv-key-actuator"
+		keymanager = KeyManager()
+		privateKey = keymanager.getPrivateKey(privateKeyPath)
+		decryptData = keymanager.decrypt(request, privateKey)
+		input = json.loads(decryptData)
+		payload = ActuatorPayload(input)
+		return payload
