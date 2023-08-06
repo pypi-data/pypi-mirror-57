07b40c252d6fff400fc338ca7a760b959131057d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:49 +0000

    Add package bandwidth-sdk/5.0.0/bandwidth-sdk-5.0.0.tar.gz

---
+++ packages/bandwidth-sdk/bandwidth-sdk-5.0.0.tar.gz/bandwidth-sdk-5.0.0/bandwidth/voice/bxml/verbs/__init__.py
@@ -0,0 +1,12 @@
+
+from .hangup import Hangup
+from .send_dtmf import SendDtmf
+from .gather import Gather
+from .pause import Pause
+from .phone_number import PhoneNumber
+from .redirect import Redirect
+from .speak_sentence import SpeakSentence
+from .transfer import Transfer
+from .play_audio import PlayAudio
+from .forward import Forward
+from .record import Record
---
+++ packages/bandwidth-sdk/bandwidth-sdk-5.0.0.tar.gz/bandwidth-sdk-5.0.0/bandwidth/voice/bxml/verbs/hangup.py
@@ -0,0 +1,16 @@
+
+"""
+hangup.py
+
+Representation of Bandwidth's hangup BXML verb
+
+@copyright Bandwidth INC
+"""
+
+from .base_verb import AbstractBxmlVerb
+
+
+class Hangup(AbstractBxmlVerb):
+
+    def to_bxml(self):
+        return "<Hangup/>"
