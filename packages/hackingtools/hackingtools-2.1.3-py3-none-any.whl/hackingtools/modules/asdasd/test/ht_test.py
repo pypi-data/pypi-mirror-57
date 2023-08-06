a3f3223ffd1b276d56ba3f222becb23553f0a08d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:25 +0000

    Add package hackingtools/2.1.3/hackingtools-2.1.3-py3-none-any.whl

---
+++ packages/hackingtools/hackingtools-2.1.3-py3-none-any.whl/hackingtools/modules/asdasd/test/ht_test.py
@@ -0,0 +1,29 @@
+from hackingtools.core import Logger, Utils, Config
+if Utils.amIdjango(__name__):
+	from .library.core import hackingtools as ht
+else:
+	import hackingtools as ht
+import os
+
+config = Config.getConfig(parentKey='modules', key='ht_test')
+output_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'output'))
+
+class StartModule():
+
+	def __init__(self):
+		pass
+
+	def help(self):
+		Logger.printMessage(message=ht.getFunctionsNamesFromModule('ht_test'), debug_module=True)
+
+	def hola(self, aa, name, fileName=None, edad=10):
+		return name
+
+	def liwiskas(self, asddas, sadasd, fileNaeeme=None, edasdad=10):
+		return edasdad
+
+	def mimimi(self, a, asdasd='', edasdad=10):
+		return edasdad
+
+	def mimismi(self, sadssa, asdsa='', esadsaddasdad=10):
+		return asdsa
