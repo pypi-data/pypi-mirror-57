f68b98a9424d1e3e9d775186b202032cee8951c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:39 +0000

    Add package csvmigrationtool/0.0.4/csvmigrationtool-0.0.4.tar.gz

---
+++ packages/csvmigrationtool/csvmigrationtool-0.0.4.tar.gz/csvmigrationtool-0.0.4/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='csvmigrationtool',
+      version='0.0.4',
+      author='Tong Wang',
+      author_email='wangtong4348@gmail.com',
+      license='MIT',
+      packages=['csvmigrationtool'],
+      install_requires=['atlassian-python-api','PyYAML'],
+      zip_safe=False)
