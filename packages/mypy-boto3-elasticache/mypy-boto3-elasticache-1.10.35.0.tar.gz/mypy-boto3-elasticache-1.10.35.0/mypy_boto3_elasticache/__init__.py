6e8a00b54bbb2ce5bf1def4792dc957dc217833c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:28 +0000

    Add package mypy-boto3-elasticache/1.10.35.0/mypy-boto3-elasticache-1.10.35.0.tar.gz

---
+++ packages/mypy-boto3-elasticache/mypy-boto3-elasticache-1.10.35.0.tar.gz/mypy-boto3-elasticache-1.10.35.0/mypy_boto3_elasticache/__init__.py
@@ -0,0 +1,48 @@
+"Main interface for elasticache service"
+
+from mypy_boto3_elasticache.client import Client
+from mypy_boto3_elasticache.paginator import (
+    DescribeCacheClustersPaginator,
+    DescribeCacheEngineVersionsPaginator,
+    DescribeCacheParameterGroupsPaginator,
+    DescribeCacheParametersPaginator,
+    DescribeCacheSecurityGroupsPaginator,
+    DescribeCacheSubnetGroupsPaginator,
+    DescribeEngineDefaultParametersPaginator,
+    DescribeEventsPaginator,
+    DescribeReplicationGroupsPaginator,
+    DescribeReservedCacheNodesOfferingsPaginator,
+    DescribeReservedCacheNodesPaginator,
+    DescribeServiceUpdatesPaginator,
+    DescribeSnapshotsPaginator,
+    DescribeUpdateActionsPaginator,
+)
+from mypy_boto3_elasticache.waiter import (
+    CacheClusterAvailableWaiter,
+    CacheClusterDeletedWaiter,
+    ReplicationGroupAvailableWaiter,
+    ReplicationGroupDeletedWaiter,
+)
+
+
+__all__ = (
+    "Client",
+    "CacheClusterAvailableWaiter",
+    "CacheClusterDeletedWaiter",
+    "ReplicationGroupAvailableWaiter",
+    "ReplicationGroupDeletedWaiter",
+    "DescribeCacheClustersPaginator",
+    "DescribeCacheEngineVersionsPaginator",
+    "DescribeCacheParameterGroupsPaginator",
+    "DescribeCacheParametersPaginator",
+    "DescribeCacheSecurityGroupsPaginator",
+    "DescribeCacheSubnetGroupsPaginator",
+    "DescribeEngineDefaultParametersPaginator",
+    "DescribeEventsPaginator",
+    "DescribeReplicationGroupsPaginator",
+    "DescribeReservedCacheNodesPaginator",
+    "DescribeReservedCacheNodesOfferingsPaginator",
+    "DescribeServiceUpdatesPaginator",
+    "DescribeSnapshotsPaginator",
+    "DescribeUpdateActionsPaginator",
+)
