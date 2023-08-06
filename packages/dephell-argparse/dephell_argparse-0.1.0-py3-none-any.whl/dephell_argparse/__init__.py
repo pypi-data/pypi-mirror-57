a638afa2f2c593a85888d42309c92dbcb5da72d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:04 +0000

    Add package dephell-argparse/0.1.0/dephell_argparse-0.1.0-py3-none-any.whl

---
+++ packages/dephell-argparse/dephell_argparse-0.1.0-py3-none-any.whl/dephell_argparse/__init__.py
@@ -0,0 +1,14 @@
+"""Argparse on steroids: groups, commands, colors.
+"""
+
+# app
+from ._handler import CommandHandler
+from ._parser import Parser
+
+
+__version__ = '0.1.0'
+
+__all__ = [
+    'CommandHandler',
+    'Parser',
+]
