df6c85fe15503fd8fb6d51296d9a4c4c65418546
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:10 +0000

    Add package card-cli/1.1.5/card-cli-1.1.5.tar.gz

---
+++ packages/card-cli/card-cli-1.1.5.tar.gz/card-cli-1.1.5/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="card-cli",
+    version="1.1.5",
+    author='Wietse Gielen',
+    author_email='giertz@inuits.eu',
+    description='card-cli is a command line utility for reading smartcards and magnetic stripe cards.',
+    long_description=long_description,
+    url='https://github.com/WietseGielen/card-reader-cli',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    include_package_data=True,
+    install_requires=[
+        'pyusb==1.0.0b2',
+    ],
+    entry_points={
+        'console_scripts': [
+            'card-cli = card_reader:main',
+        ]
+    },
+)
