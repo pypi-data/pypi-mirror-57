a4b7a99609469267116ac1235f44563a311ed045
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:49 +0000

    Add package kaga-logger-kagamino/0.1.1/kaga-logger-kagamino-0.1.1.tar.gz

---
+++ packages/kaga-logger-kagamino/kaga-logger-kagamino-0.1.1.tar.gz/kaga-logger-kagamino-0.1.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open('README.md', 'r') as readme:
+    long_description = readme.read()
+
+setuptools.setup(
+    name="kaga-logger-kagamino",
+    version="0.1.1",
+    author="Martin Kagamino Lehoux",
+    author_email="martin@lehoux.net",
+    description="A simple colored logger for python projects",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/martinlehoux/kaga-logger",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+    ],
+    python_requires=">=3.7"
+)
