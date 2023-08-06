90ea0aef29fd60708aff8dd52c139f30b6fecced
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:34 +0000

    Add package waldur-mastermind/4.2.2/waldur-mastermind-4.2.2.tar.gz

---
+++ packages/waldur-mastermind/waldur-mastermind-4.2.2.tar.gz/waldur-mastermind-4.2.2/src/waldur_rancher/extension.py
@@ -0,0 +1,40 @@
+from waldur_core.core import WaldurExtension
+
+
+class RancherExtension(WaldurExtension):
+
+    class Settings:
+        WALDUR_RANCHER = {
+            'RANCHER_NODE_CLOUD_INIT_TEMPLATE': '#cloud-config\n'
+                                                'packages: \n'
+                                                '  - curl\n'
+                                                'runcmd:\n'
+                                                '  - curl -fsSL '
+                                                'https://get.docker.com -o get-docker.sh; sh get-docker.sh\n'
+                                                '  - [ sh, -c, "{command}" ]\n',
+            'ROLE_REQUIREMENT': {
+                'controllplane': {'CPU': 2, 'RAM': 4096},
+                'etcd': {'CPU': 2, 'RAM': 4096},
+                'worker': {'CPU': 1, 'RAM': 1024},
+            }
+        }
+
+    @staticmethod
+    def django_app():
+        return 'waldur_rancher'
+
+    @staticmethod
+    def rest_urls():
+        from .urls import register_in
+        return register_in
+
+    @staticmethod
+    def celery_tasks():
+        from datetime import timedelta
+        return {
+            'waldur-rancher-update-node-states': {
+                'task': 'waldur_rancher.update_node_states',
+                'schedule': timedelta(hours=24),
+                'args': (),
+            },
+        }
