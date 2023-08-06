2e0d737ea46a99a8525f32466961c019da7d32af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:10 +0000

    Add package yawast/0.10.0/yawast-0.10.0-py3-none-any.whl

---
+++ packages/yawast/yawast-0.10.0-py3-none-any.whl/yawast/scanner/plugins/http/http_utils.py
@@ -0,0 +1,30 @@
+#  Copyright (c) 2013 - 2019 Adam Caudill and Contributors.
+#  This file is part of YAWAST which is released under the MIT license.
+#  See the LICENSE file or go to https://yawast.org/license/ for full license details.
+
+from requests import Response
+
+from yawast.shared import utils
+
+
+def is_text(res: Response) -> bool:
+    """
+    Returns True if the body is HTML, or at least seems like text
+    :param res:
+    :return:
+    """
+    has_text = False
+
+    if len(res.content) == 0:
+        # don't bother with these, if the body is empty
+        has_text = False
+    elif "Content-Type" in res.headers and "text/html" in res.headers["Content-Type"]:
+        # it's HTML, go
+        has_text = True
+    elif "Content-Type" not in res.headers:
+        # this is something, but the server doesn't tell us what
+        # so, we will check to see if if we can treat it like text
+        if utils.is_printable_str(res.content):
+            has_text = True
+
+    return has_text
---
+++ packages/yawast/yawast-0.10.0-py3-none-any.whl/yawast/scanner/plugins/http/response_scanner.py
@@ -0,0 +1,166 @@
+#  Copyright (c) 2013 - 2019 Adam Caudill and Contributors.
+#  This file is part of YAWAST which is released under the MIT license.
+#  See the LICENSE file or go to https://yawast.org/license/ for full license details.
+
+import inspect
+from typing import List, Union
+
+from bs4 import BeautifulSoup
+from requests.models import Response
+
+from yawast.reporting.enums import Vulnerabilities
+from yawast.scanner.plugins.evidence import Evidence
+from yawast.scanner.plugins.http import http_basic, retirejs, error_checker, http_utils
+from yawast.scanner.plugins.http.servers import rails, apache_tomcat
+from yawast.scanner.plugins.result import Result
+from yawast.shared import network, output
+
+
+def check_response(
+    url: str, res: Response, soup: Union[BeautifulSoup, None] = None
+) -> List[Result]:
+    # make sure we actually have something
+    if res is None:
+        return []
+
+    results: List[Result] = []
+
+    raw_full = network.http_build_raw_response(res)
+
+    if http_utils.is_text(res):
+        body = res.text
+
+        if soup is None:
+            soup = BeautifulSoup(body, "html.parser")
+
+        # check for things thar require parsed HTML
+        results += retirejs.get_results(soup, url, res)
+        results += apache_tomcat.get_version(url, res)
+        results += error_checker.check_response(url, res, body)
+
+        results += _check_cache_headers(url, res)
+
+    results += http_basic.get_header_issues(res, raw_full, url)
+    results += http_basic.get_cookie_issues(res, url)
+
+    # this function will trigger a recursive call, as it calls this to check the response.
+    # to deal with this, we'll check the caller, to make sure it's not what we're about to call.
+    if "check_cve_2019_5418" not in inspect.stack()[1].function:
+        results += rails.check_cve_2019_5418(url)
+
+    results += _check_charset(url, res)
+
+    return results
+
+
+def _check_charset(url: str, res: Response) -> List[Result]:
+    results: List[Result] = []
+
+    # if the body is empty, we really don't care about this
+    if len(res.content) == 0:
+        return results
+
+    try:
+        if "Content-Type" in res.headers:
+            content_type = str(res.headers["Content-Type"]).lower()
+
+            if "charset" not in content_type and "text/html" in content_type:
+                # not charset specified
+                results.append(
+                    Result.from_evidence(
+                        Evidence.from_response(res, {"content-type": content_type}),
+                        f"Charset Not Defined in '{res.headers['Content-Type']}' at {url}",
+                        Vulnerabilities.HTTP_HEADER_CONTENT_TYPE_NO_CHARSET,
+                    )
+                )
+        else:
+            # content-type missing
+            results.append(
+                Result.from_evidence(
+                    Evidence.from_response(res),
+                    f"Content-Type Missing: {url} ({res.request.method} - {res.status_code})",
+                    Vulnerabilities.HTTP_HEADER_CONTENT_TYPE_MISSING,
+                )
+            )
+    except Exception:
+        output.debug_exception()
+
+    return results
+
+
+def _check_cache_headers(url: str, res: Response) -> List[Result]:
+    results = []
+
+    try:
+        if "Cache-Control" in res.headers:
+            # we have the header, check the content
+            if "public" in str(res.headers["Cache-Control"]).lower():
+                results.append(
+                    Result.from_evidence(
+                        Evidence.from_response(res),
+                        f"Cache-Control: Public: {url}",
+                        Vulnerabilities.HTTP_HEADER_CACHE_CONTROL_PUBLIC,
+                    )
+                )
+
+            if "no-cache" not in str(res.headers["Cache-Control"]).lower():
+                results.append(
+                    Result.from_evidence(
+                        Evidence.from_response(res),
+                        f"Cache-Control: no-cache Not Found: {url}",
+                        Vulnerabilities.HTTP_HEADER_CACHE_CONTROL_NO_CACHE_MISSING,
+                    )
+                )
+
+            if "no-store" not in str(res.headers["Cache-Control"]).lower():
+                results.append(
+                    Result.from_evidence(
+                        Evidence.from_response(res),
+                        f"Cache-Control: no-store Not Found: {url}",
+                        Vulnerabilities.HTTP_HEADER_CACHE_CONTROL_NO_STORE_MISSING,
+                    )
+                )
+
+            if "private" not in str(res.headers["Cache-Control"]).lower():
+                results.append(
+                    Result.from_evidence(
+                        Evidence.from_response(res),
+                        f"Cache-Control: private Not Found: {url}",
+                        Vulnerabilities.HTTP_HEADER_CACHE_CONTROL_PRIVATE_MISSING,
+                    )
+                )
+        else:
+            # header missing
+            results.append(
+                Result.from_evidence(
+                    Evidence.from_response(res),
+                    f"Cache-Control Header Not Found: {url}",
+                    Vulnerabilities.HTTP_HEADER_CACHE_CONTROL_MISSING,
+                )
+            )
+
+        if "Expires" not in res.headers:
+            results.append(
+                Result.from_evidence(
+                    Evidence.from_response(res),
+                    f"Expires Header Not Found: {url}",
+                    Vulnerabilities.HTTP_HEADER_EXPIRES_MISSING,
+                )
+            )
+
+        else:
+            # TODO: parse the value and see if it's less than now
+            pass
+
+        if "Pragma" not in res.headers or "no-cache" not in str(res.headers["Pragma"]):
+            results.append(
+                Result.from_evidence(
+                    Evidence.from_response(res),
+                    f"Pragma: no-cache Not Found: {url}",
+                    Vulnerabilities.HTTP_HEADER_PRAGMA_NO_CACHE_MISSING,
+                )
+            )
+    except Exception:
+        output.debug_exception()
+
+    return results
