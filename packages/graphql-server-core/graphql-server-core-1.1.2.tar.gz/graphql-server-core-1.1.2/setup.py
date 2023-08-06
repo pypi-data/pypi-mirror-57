aa2f0f9e383a64ba596c0864266352f3cf440c52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:42 +0000

    Add package graphql-server-core/1.1.2/graphql-server-core-1.1.2.tar.gz

---
+++ packages/graphql-server-core/graphql-server-core-1.1.2.tar.gz/graphql-server-core-1.1.2/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+required_packages = ["graphql-core>=2.1,<3", "promise"]
+
+setup(
+    name="graphql-server-core",
+    version="1.1.2",
+    description="GraphQL Server tools for powering your server",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/graphql-python/graphql-server-core",
+    download_url="https://github.com/graphql-python/graphql-server-core/releases",
+    author="Syrus Akbary",
+    author_email="me@syrusakbary.com",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "License :: OSI Approved :: MIT License",
+    ],
+    keywords="api graphql protocol rest",
+    packages=find_packages(exclude=["tests"]),
+    install_requires=required_packages,
+    tests_require=["pytest>=3.0,<4"],
+    include_package_data=True,
+    zip_safe=False,
+    platforms="any",
+)
