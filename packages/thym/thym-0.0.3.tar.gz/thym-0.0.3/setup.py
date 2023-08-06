4cf0befd45c2c072d6f2078f02e2082148361ab8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:46 +0000

    Add package thym/0.0.3/thym-0.0.3.tar.gz

---
+++ packages/thym/thym-0.0.3.tar.gz/thym-0.0.3/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+import thym
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="thym",
+    version=thym.__version__,
+    author="Apostolos Georgiadis",
+    author_email="apostolos.georgiadis@nfiniity.com",
+    description="Bundle of timing utilities such as Timer, Stopwatch, etc. ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/apgeorg/thym.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
---
+++ packages/thym/thym-0.0.3.tar.gz/thym-0.0.3/thym/__init__.py
@@ -0,0 +1,4 @@
+from .timer import Timer
+from .stopwatch import Stopwatch
+
+__version__="0.0.3"
