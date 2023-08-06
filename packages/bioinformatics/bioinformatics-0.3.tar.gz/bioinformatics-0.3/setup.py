ecebcc02ef922d671a75568ec0a5908dc8e7a8e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:37 +0000

    Add package bioinformatics/0.3/bioinformatics-0.3.tar.gz

---
+++ packages/bioinformatics/bioinformatics-0.3.tar.gz/bioinformatics-0.3/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name='bioinformatics',
+      version='0.3',
+      description='Bioinformatic algorithms from exercises on Rosalind',
+      url='http://github.com/chembiodrew/bioinformatics',
+      download_url = 'https://github.com/chembiodrew/bioinformatics/archive/testing3.tar.gz', 
+      author='ChemBioDrew',
+      author_email='hendrickson19@berkeley.edu',
+      license='MIT',
+      packages=['bioinformatics'],
+      zip_safe=False)
