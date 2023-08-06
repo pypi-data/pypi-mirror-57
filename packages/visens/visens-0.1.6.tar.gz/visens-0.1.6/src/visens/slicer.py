27fb318cdd8cb25b3a471027182ebdcac45279ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:14 +0000

    Add package visens/0.1.6/visens-0.1.6.tar.gz

---
+++ packages/visens/visens-0.1.6.tar.gz/visens-0.1.6/src/visens/slicer.py
@@ -0,0 +1,93 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider
+from .load import load
+from .imviewer import ImageViewer
+
+
+class Slicer(object):
+
+    def __init__(self, x, y, z, tof, dt=0.0, filename="", colormap="viridis",
+                 vmin=None, vmax=None, log=False, side_panels=True, clab=""):
+
+        # Initial index
+        self.ind = 0
+
+        # Use ImageViewer class
+        self.imv = ImageViewer(x, y, z[:, :, self.ind], filename=filename,
+                      colormap=colormap, vmin=vmin, vmax=vmax, log=log,
+                      side_panels=side_panels, clab=clab,
+                      xlabel="x position [m]", ylabel="y position [m]")
+
+        self.x = x
+        self.y = y
+        self.z = z
+        self.tof = tof
+        self.dt = dt
+        self.nslices = z.shape[-1]
+
+        # Set limits
+        if self.imv.side_panels:
+            self.z_sumy = np.sum(z, axis=0)
+            self.z_sumx = np.sum(z, axis=1)
+            self.imv.ax3.set_ylim(top=np.nanmax(self.z_sumy))
+            self.imv.ax4.set_xlim(right=np.nanmax(self.z_sumx))
+
+        # Connect canvas to scroll
+        self.imv.fig.canvas.mpl_connect("scroll_event", self.onscroll)
+
+        # Add mpl slider widget
+        self.ax_slider = self.imv.fig.add_axes([0.23, 0.02, 0.56, 0.03])
+        self.slider = Slider(self.ax_slider, "Tof [us]", tof.min(), tof.max(),
+                             valinit=tof.min())
+        # Connect slider to function
+        self.slider.on_changed(self.update)
+
+    def onscroll(self, event):
+        """
+        Allow moving the slider with the mouse wheel
+        """
+        if event.button == "up":
+            self.ind = np.clip(self.ind + 1, 0, self.nslices - 1)
+        else:
+            self.ind = np.clip(self.ind - 1, 0, self.nslices - 1)
+        self.slider.set_val(self.ind * self.dt)
+
+    def update(self, val=None):
+        """
+        Update the image with new data according to slider value
+        """
+        self.ind = np.clip(int(round(val/self.dt)), 0, self.nslices - 1)
+        self.imv.im.set_data(self.z[:, :, self.ind])
+        if self.imv.side_panels:
+            self.imv.plot_x.set_data(self.x, self.z_sumy[:, self.ind])
+            self.imv.plot_y.set_data(self.z_sumx[:, self.ind], self.y)
+
+
+def slicer(filename=None, data=None, colormap="viridis", vmin=None, vmax=None,
+           log=False, nbins=256, transpose=False, side_panels=True, **kwargs):
+    """
+    Make a 2D image viewer with a slider to navigate in the Tof dimension.
+    You can also scroll with the mouse wheel to change the slider position.
+    """
+
+    if data is None:
+        data = load(filename, ids=True, tofs=True, **kwargs)
+
+    t = np.linspace(0.0, 7.2e4, nbins + 1)
+    z, xe, ye = np.histogram2d(data.ids, data.tofs/1.0e3,
+                               bins=[np.arange(-0.5, data.nx * data.ny + 0.5),
+                                     t])
+    z = z.reshape(data.ny, data.nx, nbins)
+    # Transpose should be True for old December 2018 files
+    if transpose:
+        z = np.transpose(z, axes=[1, 0, 2])
+
+    # Create slicer object
+    sl = Slicer(data.x[0, :], data.y[:, 0], z, 0.5*(t[1:] + t[:-1]),
+                dt=t[1]-t[0], filename=filename, colormap=colormap, vmin=vmin,
+                vmax=vmax, log=log, side_panels=side_panels, clab="Counts")
+    # Assume we are always opening in interactive mode
+    sl.imv.fig.show()
+
+    return sl
---
+++ packages/visens/visens-0.1.6.tar.gz/visens-0.1.6/src/visens/tof.py
@@ -0,0 +1,47 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from .load import load
+
+
+def tof(filename=None, data=None, xmin=None, xmax=None, logx=False, logy=False,
+        logxy=False, nbins=512, save=None, ax=None, **kwargs):
+    """
+    Make counts vs tof histogram
+    """
+
+    if data is None:
+        data = load(filename, tofs=True, **kwargs)
+
+    tofs = data.tofs / 1.0e3
+    if xmin is None:
+        xmin = np.amin(tofs)
+    if xmax is None:
+        xmax = np.amax(tofs)
+
+    show = True
+    if ax is None:
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+    else:
+        show = False
+
+    ax.grid(True, color="lightgray", linestyle="dotted")
+    ax.set_axisbelow(True)
+    if logx or logxy:
+        ax.set_xscale("log", nonposx='clip')
+    if logy or logxy:
+        ax.set_yscale("log", nonposy='clip')
+    y, x = np.histogram(tofs, bins=np.linspace(xmin, xmax, nbins + 1))
+    y = np.concatenate(([0], y))
+    ax.step(x, y)
+    ax.fill_between(x, y, step="pre", alpha=0.6)
+    ax.set_xlabel("Time-of-flight [microseconds]")
+    ax.set_ylabel("Counts")
+    if filename is not None:
+        ax.set_title(filename.split("/")[-1])
+
+    if show:
+        if save is not None:
+            fig.savefig(save, bbox_inches="tight")
+        else:
+            plt.show()
