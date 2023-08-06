084e0073d55f9522aa621aea023cc2fdbcb15b2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:25 +0000

    Add package mc-sokect/0.0.3/mc_sokect-0.0.3.tar.gz

---
+++ packages/mc-sokect/mc_sokect-0.0.3.tar.gz/mc_sokect-0.0.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+setup(
+    name='mc_sokect',
+    version='0.0.3',
+    author='Hugn_王海涛',
+    author_email='wang1183478375@outlook.com',
+    url ='https://www.coding4fun.com.cn/',
+    install_requires=['urllib3>=1.25.7',
+                      'demjson>=2.2.4'
+                      ],
+    data_files=[('',['mc_sokect.py'])
+                ],
+    include_package_data = True, 
+    zip_safe=False,
+    )
