e86769a43135fd60fff0b8f1e7c50b0fd4cceefa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:30 +0000

    Add package office365/0.3.1/office365-0.3.1.tar.gz

---
+++ packages/office365/office365-0.3.1.tar.gz/office365-0.3.1/office/blob.py
@@ -0,0 +1,107 @@
+from __future__ import annotations
+
+import os
+from typing import Any, Iterator
+
+from maybe import Maybe
+from subtypes import NameSpace, Str
+from pathmagic import File, Dir, PathLike
+
+from office import resources
+from .config import Config
+
+
+class BlobStorage:
+    """A class representing a blob storage account. Takes a connection alias which must exist in the library config settings. If none is provided, the default connection will be used."""
+
+    def __init__(self, connection: str = None) -> None:
+        import azure.storage.blob as blob
+
+        self.config = Config()
+        self.connection = Maybe(connection).else_(self.config.data.default_connections.blob)
+        settings = self.config.data.connections.blob[self.connection]
+
+        self.blob, self.service = blob, blob.BlockBlobService(account_name=settings.account, account_key=settings.key)
+        self.containers = BlobContainerNameSpace(self)
+        self.blob_type_mappings = File.from_resource(package=resources, name="blob_content_types", extension="json").content
+
+
+class BlobContainerNameSpace(NameSpace):
+    """A namespace class representing a collection of blob containers."""
+
+    def __init__(self, blob_manager: BlobStorage) -> None:
+        self._manager = blob_manager
+        super().__init__({container.name: BlobContainer(container=container, blob_manager=self._manager) for container in self._manager.service.list_containers()})
+
+
+class BlobContainer:
+    """A class representing a blob container, with methods for accessing (supports item access) and iterating over its blobs."""
+
+    def __init__(self, container: Any, blob_manager: BlobStorage) -> None:
+        self.raw, self.name, self.manager = container, container.name, blob_manager
+        self.service = self.manager.service
+        self._cached_len = 0
+
+    def __repr__(self) -> str:
+        return f"{type(self).__name__}(name={repr(self.name)}, num_blobs={repr(self._cached_len or '?')})"
+
+    def __str__(self) -> str:
+        return self.name
+
+    def __len__(self) -> int:
+        self._cached_len = sum([1 for blob in self.service.list_blobs(self.name)])
+        return self._cached_len
+
+    def __bool__(self) -> bool:
+        return len(self) > 0
+
+    def __iter__(self) -> Iterator[Blob]:
+        return iter(Blob(blob=name, container=self) for name in self.service.list_blobs(self.name))
+
+    def __getitem__(self, key: str) -> Blob:
+        return Blob(blob=self.service.get_blob_properties(container_name=self.name, blob_name=key), container=self)
+
+    def download_blob_to(self, name: str, folder: PathLike) -> PathLike:
+        """Download the named blob to the given folder. It will keep its blob 'basename' as its new name."""
+        return self[name].download_to(folder)
+
+    def download_blob_to_path(self, name: str, path: PathLike) -> PathLike:
+        """Download the named blob to the given path."""
+        return self[name].download_to_path(path)
+
+    def upload_blob_from(self, file: PathLike, name: str = None) -> Blob:
+        """Create a new blob within this container in storage from the given file path."""
+        from azure.storage.blob.models import ContentSettings
+
+        file = File.from_pathlike(file)
+        content_type = self.manager.blob_type_mappings[file.extension]
+        blob_name = file.name if name is None else name
+        self.service.create_blob_from_path(container_name=self.name, blob_name=blob_name, file_path=str(file), content_settings=ContentSettings(content_type=content_type))
+
+        return self[blob_name]
+
+
+class Blob:
+    """A class representing a blob in storage within a container, with methods for interacting with it."""
+
+    def __init__(self, blob: Any, container: BlobContainer) -> None:
+        self.raw, self.name, self.container = blob, blob.name, container
+        self.service = self.container.manager.service
+
+    def __repr__(self) -> str:
+        return f"{type(self).__name__}(name={repr(self.name)}, container={repr(self.container.name)})"
+
+    def download_to(self, folder: PathLike, name: str = None) -> File:
+        """Download this blob to the given folder. It will keep its blob 'basename' as its new name."""
+        file = Dir.from_pathlike(folder).new_file(Str(self.name).slice.after_last("/") or self.name if name is None else name)
+        self.service.get_blob_to_path(container_name=self.container.name, blob_name=self.name, file_path=str(file))
+        return file
+
+    def download_to_path(self, path: PathLike) -> File:
+        """Download this blob to the given path."""
+        self.service.get_blob_to_path(container_name=self.container.name, blob_name=self.name, file_path=os.fspath(path))
+        return File.from_pathlike(path)
+
+    def delete(self) -> None:
+        """Permanently delete this blob within its container in storage."""
+        self.service.delete_blob(container_name=self.container.name, blob_name=self.name)
