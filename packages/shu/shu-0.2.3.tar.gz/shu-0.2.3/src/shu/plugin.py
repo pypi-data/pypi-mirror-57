aebdcfd70eb45abdf777fcf86b6f1b478486671c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:49 +0000

    Add package shu/0.2.3/shu-0.2.3.tar.gz

---
+++ packages/shu/shu-0.2.3.tar.gz/shu-0.2.3/src/shu/plugin.py
@@ -0,0 +1,47 @@
+# This is the class you derive to create a plugin
+import logging
+
+from airflow.plugins_manager import AirflowPlugin
+from airflow.www.app import csrf
+
+from shu.api import create_api, create_swagger
+from shu.resources import dags
+
+import flask
+
+log = logging.getLogger(__name__)
+
+blueprint = flask.Blueprint("test", __name__)
+
+
+@blueprint.route("/test", methods=["POST"])
+def test():
+    return "Hello world!"
+
+
+# csrf.exempt(blueprint)
+
+
+def create_blueprints():
+    api = create_api("/internal")
+    api.add_namespace(dags)
+    swagger = create_swagger()
+    csrf.exempt(api.blueprint)
+    csrf.exempt(swagger)
+    return [api.blueprint, swagger, blueprint]
+
+
+# Defining the plugin class
+class Plugin(AirflowPlugin):
+    name = "internal"
+    operators = []
+    sensors = []
+    hooks = []
+    executors = []
+    macros = []
+    admin_views = []
+    flask_blueprints = create_blueprints()
+    menu_links = []
+    appbuilder_views = []
+    appbuilder_menu_items = []
+    global_operator_extra_links = []
