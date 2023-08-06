f76fbc1c8aa5efdc1a7df4c0fd4dfe578f29dd6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:12 +0000

    Add package pyprojecttemplate/0.2.0/pyprojecttemplate-0.2.0.tar.gz

---
+++ packages/pyprojecttemplate/pyprojecttemplate-0.2.0.tar.gz/pyprojecttemplate-0.2.0/pyprojecttemplate/basicmath.py
@@ -0,0 +1,24 @@
+"""
+Basic Sample Python to define common mathematical functions
+"""
+import math
+
+def calculate_square(num):
+    """
+    Returns the square of a given number
+    """
+    return num * num
+
+
+def add(a, b):
+    """
+    Returns the sum of two numbers
+    """
+    return a + b
+
+
+def calculate_sin(x):
+    """
+    Return the sine of x (measured in radians).
+    """
+    return math.sin(x)
