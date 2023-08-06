8b11a4f0e5653348904cfe664db30fd0cb087a06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:49 +0000

    Add package financepy/0.153/financepy-0.153-py3-none-any.whl

---
+++ packages/financepy/financepy-0.153-py3-none-any.whl/financepy/finutils/FinHelperFunctions.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 07 14:31:53 2016
+
+@author: Dominic O'Kane
+"""
+import numpy as np
+from numba import njit
+from .FinDate import FinDate
+from .FinGlobalVariables import gDaysInYear
+
+##########################################################################
+
+
+def inputFrequency(f):
+    if f in [-1, 0, 1, 2, 3, 4, 6, 12]:
+        return f
+    else:
+        raise ValueError("Unknown frequency" + str(f))
+
+###############################################################################
+
+
+def inputTime(dt, curve):
+    small = 1e-8
+
+    def check(t):
+        if t < 0.0:
+            raise ValueError("Date " + str(dt) +
+                             " is before curve date " + str(curve._curveDate))
+        elif t < small:
+            t = small
+        return t
+
+    if isinstance(dt, float):
+        t = dt
+        return check(t)
+    elif isinstance(dt, FinDate):
+        t = (dt - curve._curveDate) / gDaysInYear
+        return check(t)
+    elif isinstance(dt, np.ndarray):
+        t = dt
+        if np.any(t) < 0:
+            raise ValueError("Date is before curve value date.")
+        t = np.maximum(small, t)
+        return t
+    else:
+        raise ValueError("Unknown type.")
+
+###############################################################################
+
+@njit(fastmath=True, cache=True)
+def listdiff(a, b):
+    ''' Calculate a vector of differences between two equal sized vectors. '''
+
+    if len(a) != len(b):
+        raise ValueError("Cannot diff lists with different sizes")
+        return []
+
+    diff = []
+    for x, y in zip(a, b):
+        diff.append(x - y)
+
+    return diff
+
+##########################################################################
+
+
+@njit(fastmath=True, cache=True)
+def dotproduct(xVector, yVector):
+    dotprod = 0.0
+    n = len(xVector)
+    for i in range(0, n):
+        dotprod += xVector[i] * yVector[i]
+    return dotprod
+
+##########################################################################
+
+
+@njit(fastmath=True, cache=True)
+def frange(start, stop, step):
+    x = []
+    while start <= stop:
+        x.append(start)
+        start += step
+
+    return x
+
+##########################################################################
+
+
+@njit(fastmath=True, cache=True)
+def normaliseWeights(wtVector):
+    n = len(wtVector)
+    sumWts = 0.0
+    for i in range(0, n):
+        sumWts += wtVector[i]
+    for i in range(0, n):
+        wtVector[i] = wtVector[i]/sumWts
+    return wtVector
+
+##########################################################################
