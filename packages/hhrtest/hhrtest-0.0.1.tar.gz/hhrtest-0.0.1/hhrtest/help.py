6f9682b764f7389a921c5976d3970da355f2fa40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:51 +0000

    Add package hhrtest/0.0.1/hhrtest-0.0.1.tar.gz

---
+++ packages/hhrtest/hhrtest-0.0.1.tar.gz/hhrtest-0.0.1/hhrtest/help.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+def sum(*values):
+    s = 0
+    for v in values:
+        i = int(v)
+        s = s + i
+    print s
+
+def output():
+    print 'http://hhrtest.me'
