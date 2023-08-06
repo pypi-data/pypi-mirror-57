b2b5399d59e76adee447f0e88761fff2917b8fc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:51 +0000

    Add package aurora-common-utils/0.0.5/aurora_common_utils-0.0.5-py3-none-any.whl

---
+++ packages/aurora-common-utils/aurora_common_utils-0.0.5-py3-none-any.whl/file_utils/files.py
@@ -0,0 +1,33 @@
+import os
+from os.path import abspath, dirname
+def resource_content(file_name):
+    f = open('resource/' + file_name, 'r', encoding='UTF-8')
+    str_content = f.read()
+    f.close()
+    return str_content
+
+def config_content(file_name):
+    f = open('config/' + file_name, 'r', encoding='UTF-8')
+    str_content = f.read()
+    f.close()
+    return str_content
+
+
+def file_content(parent_dir_name, file_name):
+    f = open(parent_dir_name + '/' + file_name, 'r', encoding='UTF-8')
+    str_content = f.read()
+    f.close()
+    return str_content
+
+
+def read_content(file_name, path):
+    f = None
+    try:
+        BASE_DIR = dirname(abspath(path))
+        resource_full_path = os.path.join(BASE_DIR, file_name)
+        f = open(resource_full_path, 'r', encoding='UTF-8')
+        str_content = f.read()
+    finally:
+        if f is not None:
+            f.close()
+    return str_content
