334772f796441bee77fd9187bc38f13d3fe1d561
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:39 +0000

    Add package kitconcept.glossary/1.0.0a8/kitconcept.glossary-1.0.0a8-py2-none-any.whl

---
+++ packages/kitconcept.glossary/kitconcept.glossary-1.0.0a8-py2-none-any.whl/kitconcept/glossary/content.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from kitconcept.glossary.interfaces import IGlossary
+from kitconcept.glossary.interfaces import IGlossaryTerm
+from plone.dexterity.content import Container
+from plone.dexterity.content import Item
+from zope.interface import implementer
+
+import lxml
+
+
+@implementer(IGlossary)
+class Glossary(Container):
+
+    """A Glossary is a container for Terms."""
+
+
+@implementer(IGlossaryTerm)
+class GlossaryTerm(Item):
+    def Description(self):
+        if not self.definition:
+            return ""
+        tree = lxml.html.fromstring(u"<div>%s</div>" % self.definition.output)
+        text = tree.text_content()
+        return text
