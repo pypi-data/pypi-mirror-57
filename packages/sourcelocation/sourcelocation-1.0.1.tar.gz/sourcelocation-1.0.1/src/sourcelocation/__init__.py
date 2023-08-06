d17c83d3e82223fb968578557ec4a0f59bdb30bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:33 +0000

    Add package sourcelocation/1.0.1/sourcelocation-1.0.1.tar.gz

---
+++ packages/sourcelocation/sourcelocation-1.0.1.tar.gz/sourcelocation-1.0.1/src/sourcelocation/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+This module provides a simple set of data structures for representing source
+code locations (e.g., characters, character ranges, lines, etc.).
+"""
+__all__ = (
+    '__version__',
+    'FileLine',
+    'FileLineMap',
+    'FileLineSet',
+    'Location',
+    'LocationRange',
+    'FileLocation',
+    'FileLocationRange'
+)
+__version__ = '1.0.1'
+
+from .fileline import FileLine, FileLineMap, FileLineSet
+from .location import (Location, LocationRange,
+                       FileLocation, FileLocationRange)
