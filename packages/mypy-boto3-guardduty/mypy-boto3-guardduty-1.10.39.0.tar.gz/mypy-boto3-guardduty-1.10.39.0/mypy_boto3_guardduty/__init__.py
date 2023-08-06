676f4738438b1ff0e8983a6a632d0ac87a86a2c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:15 +0000

    Add package mypy-boto3-guardduty/1.10.39.0/mypy-boto3-guardduty-1.10.39.0.tar.gz

---
+++ packages/mypy-boto3-guardduty/mypy-boto3-guardduty-1.10.39.0.tar.gz/mypy-boto3-guardduty-1.10.39.0/mypy_boto3_guardduty/__init__.py
@@ -0,0 +1,25 @@
+"Main interface for guardduty service"
+
+from mypy_boto3_guardduty.client import GuardDutyClient, GuardDutyClient as Client
+from mypy_boto3_guardduty.paginator import (
+    ListDetectorsPaginator,
+    ListFiltersPaginator,
+    ListFindingsPaginator,
+    ListIPSetsPaginator,
+    ListInvitationsPaginator,
+    ListMembersPaginator,
+    ListThreatIntelSetsPaginator,
+)
+
+
+__all__ = (
+    "Client",
+    "GuardDutyClient",
+    "ListDetectorsPaginator",
+    "ListFiltersPaginator",
+    "ListFindingsPaginator",
+    "ListIPSetsPaginator",
+    "ListInvitationsPaginator",
+    "ListMembersPaginator",
+    "ListThreatIntelSetsPaginator",
+)
