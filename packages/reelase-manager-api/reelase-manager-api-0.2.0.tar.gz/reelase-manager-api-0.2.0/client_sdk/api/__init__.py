fb7915f008ea71b9efeac72a7522b9a46a100621
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:22 +0000

    Add package reelase-manager-api/0.2.0/reelase-manager-api-0.2.0.tar.gz

---
+++ packages/reelase-manager-api/reelase-manager-api-0.2.0.tar.gz/reelase-manager-api-0.2.0/client_sdk/api/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from client_sdk.api.project_api import ProjectApi
+from client_sdk.api.version_semantic_api import VersionSemanticApi
