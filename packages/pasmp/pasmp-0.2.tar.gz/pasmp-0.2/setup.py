4689453de309fd8396bc0141fb40bb8645c2f53c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:15 +0000

    Add package pasmp/0.2/pasmp-0.2.tar.gz

---
+++ packages/pasmp/pasmp-0.2.tar.gz/pasmp-0.2/setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+from __future__ import absolute_import
+from __future__ import unicode_literals
+import os
+
+from setuptools import setup, find_packages
+
+try:
+    with open('README.rst') as f:
+        readme = f.read()
+except IOError:
+    readme = ''
+
+
+def _requires_from_file(filename):
+    return open(filename).read().splitlines()
+
+
+# version
+here = os.path.dirname(os.path.abspath(__file__))
+version = next((line.split('=')[1].strip().replace("'", '')
+                for line in open(os.path.join(here,
+                                              'atomsware\pasmp',
+                                              '__init__.py'))
+                if line.startswith('__version__ = ')),
+               '0.0.dev0')
+
+setup(
+    name="pasmp",
+    version=0.2,
+    url='https://www.atomsware.co.jp',
+    author='atomsware',
+    author_email='shimada@atomsware.co.jp',
+    maintainer='atomsware',
+    maintainer_email='shimada@atomsware.co.jp',
+    description='Python API for Swift Message Processing',
+    long_description=readme,
+    packages=find_packages(),
+    install_requires=[],
+    license="MIT",
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'License :: OSI Approved :: MIT License',
+    ],
+    entry_points="""
+    """,
+)
