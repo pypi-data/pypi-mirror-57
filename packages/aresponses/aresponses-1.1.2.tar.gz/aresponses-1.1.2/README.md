7f3c855649ca22bae38e618789864a5e14ee4cb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:04 +0000

    Add package aresponses/1.1.2/aresponses-1.1.2.tar.gz

---
+++ packages/aresponses/aresponses-1.1.2.tar.gz/aresponses-1.1.2/README.md
@@ -0,0 +1,153 @@
+
+
+# aresponses
+
+[![image](https://img.shields.io/pypi/v/aresponses.svg)](https://pypi.org/project/aresponses/)
+[![image](https://img.shields.io/pypi/pyversions/aresponses.svg)](https://pypi.org/project/aresponses/)
+[![build status](https://travis-ci.org/CircleUp/aresponses.svg)](https://travis-ci.org/CircleUp/aresponses)
+[![codecov.io](https://codecov.io/github/CircleUp/aresponses/coverage.svg?branch=master)](https://codecov.io/github/CircleUp/aresponses)
+[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/ambv/black)
+
+
+an asyncio testing server for mocking external services
+
+## Features
+ - Fast mocks using actual network connections
+ - allows mocking some types of network issues
+ - use regular expression matching for domain, path, or method 
+ - works with https requests as well (by switching them to http requests)
+ - works with callables
+ 
+## Usage
+
+*aresponses.add(host_pattern, path_pattern, method_pattern, response)*
+
+Host, path, or method may be either strings (exact match) or regular expressions.
+
+When a request is received the first matching response will be returned (based on the order it was received in).
+
+Requires Python 3.6 or greater.
+
+## Example
+```python
+import pytest
+import aiohttp
+
+@pytest.mark.asyncio
+async def test_foo(aresponses):
+    # text as response (defaults to status 200 response)
+    aresponses.add('foo.com', '/', 'get', 'hi there!!')
+
+    # custom status code response
+    aresponses.add('foo.com', '/', 'get', aresponses.Response(text='error', status=500))
+
+    # passthrough response (makes an actual network call)
+    aresponses.add('httpstat.us', '/200', 'get', aresponses.passthrough)
+
+    # custom handler response
+    def my_handler(request):
+        return aresponses.Response(status=200, text=str(request.url))
+
+    aresponses.add('foo.com', '/', 'get', my_handler)
+    
+    # JSON response
+    aresponses.add('foo.com', '/', 'get', aresponses.Response(body=b'{"status":"ok"}'))
+
+    url = 'http://foo.com'
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            text = await response.text()
+            assert text == 'hi there!!'
+
+        async with session.get(url) as response:
+            text = await response.text()
+            assert text == 'error'
+            assert response.status == 500
+
+        async with session.get('https://httpstat.us/200') as response:
+            text = await response.text()
+        assert text == '200 OK'
+
+        async with session.get(url) as response:
+            text = await response.text()
+            assert text == 'http://foo.com/'
+        
+        async with session.get(url) as response:
+            data = await response.json()
+            assert data == {"state": "ok"}
+```
+
+```python
+import aiohttp
+import pytest
+import aresponses
+
+@pytest.mark.asyncio
+async def test_foo(event_loop):
+    async with aresponses.ResponsesMockServer(loop=event_loop) as arsps:
+        arsps.add('foo.com', '/', 'get', 'hi there!!')
+        arsps.add(arsps.ANY, '/', 'get', arsps.Response(text='hey!'))
+        
+        async with aiohttp.ClientSession(loop=event_loop) as session:
+            async with session.get('http://foo.com') as response:
+                text = await response.text()
+                assert text == 'hi'
+            
+            async with session.get('https://google.com') as response:
+                text = await response.text()
+                assert text == 'hey!'
+        
+```
+
+### aresponses with [pytest-aiohttp](https://github.com/aio-libs/pytest-aiohttp)
+
+If you need to use aresponses together with pytest-aiohttp, you should re-initialize main aresponses fixture with `loop` fixture
+```python
+from aresponses import ResponsesMockServer
+
+@pytest.fixture
+async def aresponses(loop):
+    async with ResponsesMockServer(loop=loop) as server:
+        yield server
+```
+
+## Contributing
+
+### Dev environment setup
+  - **install pyenv and pyenv-virtualenv**  - Makes it easy to install specific versions of python and switch between them. Make sure you install the virtualenv bash hook
+  - `git clone` the repo and `cd` into it.
+  - `make init` - installs proper version of python, creates the virtual environment, activates it and installs all the requirements
+  
+### Submitting a feature request  
+  - **`git checkout -b my-feature-branch`** 
+  - **make some cool changes**
+  - **`make autoformat`**
+  - **`make test`**
+  - **`make lint`**
+  - **create pull request**
+
+### Updating package on pypi
+  - `make deploy`
+
+## Changelog
+
+#### 1.1.2
+- make passthrough feature work with binary data
+
+#### 1.1.1
+- regex fix for Python 3.7.0
+
+#### 1.1.0
+- Added passthrough option to permit live network calls
+- Added example of using a callable as a response
+
+#### 1.0.0
+
+- Added an optional `match_querystring` argument that lets you match on querystring as well
+
+
+## Contributors
+* Bryce Drennan, CircleUp <aresponses@brycedrennan.com>
+* Marco Castelluccio, Mozilla <mcastelluccio@mozilla.com>
+* Jesse Vogt, CircleUp <jesse.vogt@gmail.com>
+* Pavol Vargovcik, Kiwi.com <pavol.vargovcik@gmail.com>
---
+++ packages/aresponses/aresponses-1.1.2.tar.gz/aresponses-1.1.2/aresponses/main.py
@@ -0,0 +1,174 @@
+import asyncio
+import logging
+from functools import partial
+
+import pytest
+from aiohttp import web, ClientSession
+from aiohttp.client_reqrep import ClientRequest
+from aiohttp.connector import TCPConnector
+from aiohttp.helpers import sentinel
+from aiohttp.test_utils import BaseTestServer
+from aiohttp.web_response import StreamResponse
+from aiohttp.web_runner import ServerRunner
+from aiohttp.web_server import Server
+
+from aresponses.utils import _text_matches_pattern, ANY
+
+logger = logging.getLogger(__name__)
+
+
+class RawResponse(StreamResponse):
+    """
+    Allow complete control over the response
+
+    Useful for mocking invalid responses
+    """
+
+    def __init__(self, body):
+        super().__init__()
+        self._body = body
+
+    async def _start(self, request, *_, **__):
+        self._req = request
+        self._keep_alive = False
+        writer = self._payload_writer = request._payload_writer
+        return writer
+
+    async def write_eof(self, *_, **__):
+        await super().write_eof(self._body)
+
+
+class ResponsesMockServer(BaseTestServer):
+    ANY = ANY
+    Response = web.Response
+    RawResponse = RawResponse
+
+    def __init__(self, *, scheme=sentinel, host="127.0.0.1", **kwargs):
+        self._responses = []
+        self._host_patterns = set()
+        self._exception = None
+        super().__init__(scheme=scheme, host=host, **kwargs)
+
+    async def _make_runner(self, debug=True, **kwargs):
+        srv = Server(self._handler, loop=self._loop, debug=True, **kwargs)
+        return ServerRunner(srv, debug=debug, **kwargs)
+
+    async def _close_hook(self):
+        return
+
+    async def _handler(self, request):
+        return await self._find_response(request)
+
+    def add(self, host, path=ANY, method=ANY, response="", match_querystring=False):
+        if isinstance(host, str):
+            host = host.lower()
+
+        if isinstance(method, str):
+            method = method.lower()
+
+        self._host_patterns.add(host)
+        self._responses.append((host, path, method, response, match_querystring))
+
+    def _host_matches(self, match_host):
+        match_host = match_host.lower()
+        for host_pattern in self._host_patterns:
+            if _text_matches_pattern(host_pattern, match_host):
+                return True
+
+        return False
+
+    async def _find_response(self, request):
+        host, path, path_qs, method = request.host, request.path, request.path_qs, request.method
+        logger.info(f"Looking for match for {host} {path} {method}")  # noqa
+        i = 0
+        host_matched = False
+        path_matched = False
+        for host_pattern, path_pattern, method_pattern, response, match_querystring in self._responses:
+            if _text_matches_pattern(host_pattern, host):
+                host_matched = True
+                if (not match_querystring and _text_matches_pattern(path_pattern, path)) or (
+                    match_querystring and _text_matches_pattern(path_pattern, path_qs)
+                ):
+                    path_matched = True
+                    if _text_matches_pattern(method_pattern, method.lower()):
+                        del self._responses[i]
+
+                        if callable(response):
+                            if asyncio.iscoroutinefunction(response):
+                                return await response(request)
+                            return response(request)
+
+                        if isinstance(response, str):
+                            return self.Response(body=response)
+
+                        return response
+            i += 1
+        self._exception = Exception(f"No Match found for {host} {path} {method}.  Host Match: {host_matched}  Path Match: {path_matched}")
+        self._loop.stop()
+        raise self._exception  # noqa
+
+    async def passthrough(self, request):
+        """Make non-mocked network request"""
+        connector = TCPConnector()
+        connector._resolve_host = partial(self._old_resolver_mock, connector)
+
+        new_is_ssl = ClientRequest.is_ssl
+        ClientRequest.is_ssl = self._old_is_ssl
+        try:
+            original_request = request.clone(scheme="https" if request.headers["AResponsesIsSSL"] else "http")
+
+            headers = {k: v for k, v in request.headers.items() if k != "AResponsesIsSSL"}
+
+            async with ClientSession(connector=connector) as session:
+                async with getattr(session, request.method.lower())(original_request.url, headers=headers, data=(await request.read())) as r:
+                    headers = {k: v for k, v in r.headers.items() if k.lower() == "content-type"}
+                    data = await r.read()
+                    response = self.Response(body=data, status=r.status, headers=headers)
+                    return response
+        finally:
+            ClientRequest.is_ssl = new_is_ssl
+
+    async def __aenter__(self):
+        await self.start_server(loop=self._loop)
+
+        self._old_resolver_mock = TCPConnector._resolve_host
+
+        async def _resolver_mock(_self, host, port, traces=None):
+            return [{"hostname": host, "host": "127.0.0.1", "port": self.port, "family": _self._family, "proto": 0, "flags": 0}]
+
+        TCPConnector._resolve_host = _resolver_mock
+
+        self._old_is_ssl = ClientRequest.is_ssl
+
+        def new_is_ssl(_self):
+            return False
+
+        ClientRequest.is_ssl = new_is_ssl
+
+        # store whether a request was an SSL request in the `AResponsesIsSSL` header
+        self._old_init = ClientRequest.__init__
+
+        def new_init(_self, *largs, **kwargs):
+            self._old_init(_self, *largs, **kwargs)
+
+            is_ssl = "1" if self._old_is_ssl(_self) else ""
+            _self.update_headers({**_self.headers, "AResponsesIsSSL": is_ssl})
+
+        ClientRequest.__init__ = new_init
+
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        TCPConnector._resolve_host = self._old_resolver_mock
+        ClientRequest.is_ssl = self._old_is_ssl
+
+        await self.close()
+        if self._exception:
+            pytest.fail(str(self._exception))
+            raise self._exception  # noqa
+
+
+@pytest.fixture
+async def aresponses(event_loop):
+    async with ResponsesMockServer(loop=event_loop) as server:
+        yield server
