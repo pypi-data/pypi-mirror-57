93c4c6f7b11101995493d9b9c7c1454cac78916e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:02 +0000

    Add package lazyxml/1.3.0/lazyxml-1.3.0.tar.gz

---
+++ packages/lazyxml/lazyxml-1.3.0.tar.gz/lazyxml-1.3.0/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+import sys
+
+from setuptools import setup
+
+import lazyxml
+
+if sys.version_info[:2] < (2, 7) or sys.version_info[0] >= 3:
+    sys.exit('Python 2.7.x is required.')
+
+with open('README.md') as fp:
+    readme = fp.read()
+
+setup(name=lazyxml.__title__,
+      version=lazyxml.__version__,
+      description=lazyxml.__description__,
+      long_description_content_type='text/markdown',
+      long_description=readme,
+      author=lazyxml.__author__,
+      author_email=lazyxml.__author_email__,
+      maintainer=lazyxml.__author__,
+      maintainer_email=lazyxml.__author_email__,
+      url='https://github.com/heronotears/lazyxml',
+      packages=[lazyxml.__title__],
+      license=lazyxml.__license__,
+      python_requires='>=2.7, <3.0',
+      classifiers=[]
+      )
