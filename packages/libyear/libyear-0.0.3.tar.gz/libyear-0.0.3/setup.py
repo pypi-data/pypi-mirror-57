dce2d03e70413131197c89bfa03d1f9a477b5d70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:34 +0000

    Add package libyear/0.0.3/libyear-0.0.3.tar.gz

---
+++ packages/libyear/libyear-0.0.3.tar.gz/libyear-0.0.3/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+from libyear.utils import load_requirements
+
+setup(
+    name='libyear',
+    version='0.0.3',
+    description='A simple measure of software dependency freshness.',
+    long_description=open('README.md').read(),
+    author='nasirhjafri',
+    url='https://github.com/nasirhjafri/libyear',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    packages=['libyear'],
+    py_modules=['libyear'],
+    scripts=['libyear/libyear'],
+    dependency_links=[
+    ],
+    install_requires=load_requirements('requirements.txt'),
+)
