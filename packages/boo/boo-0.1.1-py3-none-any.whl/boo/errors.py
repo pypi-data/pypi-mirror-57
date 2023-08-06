a3ca0dd9660f2f654b73e914311f2a96229f3453
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:12 +0000

    Add package boo/0.1.1/boo-0.1.1-py3-none-any.whl

---
+++ packages/boo/boo-0.1.1-py3-none-any.whl/boo/errors.py
@@ -0,0 +1,27 @@
+class UnclassifiableCodeError(ValueError):
+    pass
+
+class WrongYearError(ValueError):
+    def __init__(self, year):
+        super().__init__(f"Year not supported: {year}.\n"
+                          "Try year starting 2012 up to recent year.\n"
+                          "Update definitions in year.py if necessary")
+        
+
+class DirectoryNotFound(FileNotFoundError):
+    pass
+
+
+class NoRawFileError(FileNotFoundError):
+    def __init__(self, year):
+        super().__init__(
+            f"Raw CSV file not downloaded for {year}. "
+            f"Try `boo.download({year})`.")
+
+
+class NoProcessedFileError(FileNotFoundError):
+    def __init__(self, year):
+        super().__init__(
+            f"Final CSV file not built for {year}. "
+            f"Try `boo.build({year})`."
+        )
