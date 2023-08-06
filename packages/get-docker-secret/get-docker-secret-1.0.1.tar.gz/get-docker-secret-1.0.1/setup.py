92981aee90933da782dcf4c05463a8a96cfa60d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:11 +0000

    Add package get-docker-secret/1.0.1/get-docker-secret-1.0.1.tar.gz

---
+++ packages/get-docker-secret/get-docker-secret-1.0.1.tar.gz/get-docker-secret-1.0.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(
+    name='get-docker-secret',
+    version='1.0.1',
+    description='Utility function to fetch docker secrets/envvars.',
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url='https://github.com/fischerfredl/get-docker-secret',
+    author='Alfred Melch',
+    author_email='dev@melch.pro',
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
+    keywords=['docker', 'secret', 'envvar', 'config'],
+    py_modules=['get_docker_secret'],
+    install_requires=[],
+    test_suite='tests',
+    tests_require=[],
+)
