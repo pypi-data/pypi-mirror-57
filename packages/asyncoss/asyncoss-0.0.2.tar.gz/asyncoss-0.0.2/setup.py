c89dd6ef001fce5692faf1cee13fefd48ace818d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:32 +0000

    Add package asyncoss/0.0.2/asyncoss-0.0.2.tar.gz

---
+++ packages/asyncoss/asyncoss-0.0.2.tar.gz/asyncoss-0.0.2/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+    readme = f.read()
+
+setup(
+    name='asyncoss',
+    version='0.0.2',
+    description='A async aliyun OSS library.',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author='jerevia',
+    author_email='trilliondawn@gmail.com',
+    license='MIT',
+    install_requires=['aiohttp', 'oss2'],
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ]
+)
