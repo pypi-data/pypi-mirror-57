94f381b8ebc623d605c250b1793c6e004b45d837
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:15 +0000

    Add package mypy-boto3-servicediscovery/1.10.33/mypy_boto3_servicediscovery-1.10.33-py3-none-any.whl

---
+++ packages/mypy-boto3-servicediscovery/mypy_boto3_servicediscovery-1.10.33-py3-none-any.whl/mypy_boto3_servicediscovery/__init__.py
@@ -0,0 +1,18 @@
+"Main interface for servicediscovery service"
+
+from mypy_boto3_servicediscovery.client import Client
+from mypy_boto3_servicediscovery.paginator import (
+    ListInstancesPaginator,
+    ListNamespacesPaginator,
+    ListOperationsPaginator,
+    ListServicesPaginator,
+)
+
+
+__all__ = (
+    "Client",
+    "ListInstancesPaginator",
+    "ListNamespacesPaginator",
+    "ListOperationsPaginator",
+    "ListServicesPaginator",
+)
