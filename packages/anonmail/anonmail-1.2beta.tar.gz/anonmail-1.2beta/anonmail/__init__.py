b26ea582fedc58b2379f6918aee6d2c1ec3d0b2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:12 +0000

    Add package anonmail/1.2beta/anonmail-1.2beta.tar.gz

---
+++ packages/anonmail/anonmail-1.2beta.tar.gz/anonmail-1.2beta/anonmail/__init__.py
@@ -0,0 +1,10 @@
+
+__title__ = 'anonmail'
+__version__ = '1.2beta'
+__author__ = 'SpeedX'
+__license__ = 'GNU GENERAL PUBLIC LICENSE'
+__copyright__ = 'Copyright 2019 SpeedX'
+
+from .anonmail import *
+
+
