27a569196a08f124712e7a91a6141df65f030ed3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:15 +0000

    Add package mypy-boto3-acm/1.10.36.1/mypy-boto3-acm-1.10.36.1.tar.gz

---
+++ packages/mypy-boto3-acm/mypy-boto3-acm-1.10.36.1.tar.gz/mypy-boto3-acm-1.10.36.1/mypy_boto3_acm/__init__.py
@@ -0,0 +1,8 @@
+"Main interface for acm service"
+
+from mypy_boto3_acm.client import ACMClient as Client, ACMClient
+from mypy_boto3_acm.paginator import ListCertificatesPaginator
+from mypy_boto3_acm.waiter import CertificateValidatedWaiter
+
+
+__all__ = ("ACMClient", "CertificateValidatedWaiter", "Client", "ListCertificatesPaginator")
