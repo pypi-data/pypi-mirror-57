710c787622eff1f2eb838dae8afc41d173be5385
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:20 +0000

    Add package package-wy/1.1.0/package_wy-1.1.0-py3-none-any.whl

---
+++ packages/package-wy/package_wy-1.1.0-py3-none-any.whl/package_wy/lib_wy.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+@author: WYR。
+"""
+def add(x,y):
+    return x+y
+
+def division(x,y):
+    return x/y
+
+def mutiply(x,y):
+    return x*y
+
+def subtract(x,y):
+    return x-y
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
