33c0b2d82e82d27e03ee4f97d4fcac404705c978
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:45 +0000

    Add package pypendency/0.0.3/pypendency-0.0.3.tar.gz

---
+++ packages/pypendency/pypendency-0.0.3.tar.gz/pypendency-0.0.3/setup.py
@@ -0,0 +1,43 @@
+import sys
+from typing import List
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as readme:
+    README = readme.read()
+
+
+def _get_install_requires() -> List[str]:
+    requirements = ['pyyaml']
+    if sys.version_info < (3, 7):
+        requirements.append('dataclasses')
+
+    return requirements
+
+
+setup(
+    name='pypendency',
+    version='0.0.3',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    author='Marcos Hernandez Juarez',
+    author_email='marcos.hernandez@feverup.com',
+    description='A dependency injection tool for python',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    license='MIT License',
+    url='https://github.com/Feverup/pypendency',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries',
+        'Typing :: Typed',
+    ],
+    python_requires='>=3.6',
+    install_requires=_get_install_requires(),
+    include_package_data=True,
+)
