b04413d8b3f2f89e115d1b6139162d6a9bd1f64c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:45 +0000

    Add package fortnite-api/1.0.5/fortnite-api-1.0.5.tar.gz

---
+++ packages/fortnite-api/fortnite-api-1.0.5.tar.gz/fortnite-api-1.0.5/fortnite_api/news.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+
+
+class News:
+
+    def __init__(self, data):
+        self.br = GameModeNews(data.get('br'))
+        self.stw = GameModeNews(data.get('stw'))
+        self.creative = GameModeNews(data.get('creative'))
+        self.raw_data = data
+
+
+class GameModeNews:
+
+    def __init__(self, data):
+        try:
+            self.last_modified = datetime.strptime(data.get('lastModified'), '%Y-%m-%dT%H:%M:%S%z')
+        except ValueError:
+            self.last_modified = None
+        self.entries = [NewsEntry(entry) for entry in data.get('messages')]
+        self.raw_data = data
+
+
+class NewsEntry:
+
+    def __init__(self, data):
+        self.image_url = data.get('image')
+        self.hidden = data.get('hidden')
+        self.message_type = data.get('messageType')
+        self.type = data.get('type')
+        self.banner = data.get('adspace')
+        self.title = data.get('title')
+        self.description = data.get('body')
+        self.spotlight = data.get('spotlight')
+        self.raw_data = data
