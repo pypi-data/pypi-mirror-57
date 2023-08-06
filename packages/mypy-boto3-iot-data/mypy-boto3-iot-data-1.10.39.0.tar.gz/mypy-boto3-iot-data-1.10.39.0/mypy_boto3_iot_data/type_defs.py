ab87f0c81fa16f828ab813b2b51f20abf4d2ecaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:15 +0000

    Add package mypy-boto3-iot-data/1.10.39.0/mypy-boto3-iot-data-1.10.39.0.tar.gz

---
+++ packages/mypy-boto3-iot-data/mypy-boto3-iot-data-1.10.39.0.tar.gz/mypy-boto3-iot-data-1.10.39.0/mypy_boto3_iot_data/type_defs.py
@@ -0,0 +1,21 @@
+"Main interface for iot-data service type defs"
+import sys
+from typing import IO, Union
+
+if sys.version_info >= (3, 8):
+    from typing import TypedDict
+else:
+    from typing_extensions import TypedDict
+
+
+DeleteThingShadowResponseTypeDef = TypedDict(
+    "DeleteThingShadowResponseTypeDef", {"payload": Union[bytes, IO]}
+)
+
+GetThingShadowResponseTypeDef = TypedDict(
+    "GetThingShadowResponseTypeDef", {"payload": Union[bytes, IO]}, total=False
+)
+
+UpdateThingShadowResponseTypeDef = TypedDict(
+    "UpdateThingShadowResponseTypeDef", {"payload": Union[bytes, IO]}, total=False
+)
