d66a731b7d4e2b1b079d2fd75f1088321c8f8888
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:27 +0000

    Add package discopy/0.1.2b0/discopy-0.1.2b0-py3.7.egg

---
+++ packages/discopy/discopy-0.1.2b0-py3.7.egg/discopy/config.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""
+If fast, checking axioms is disabled (approximately twice faster).
+"""
+
+VERSION = '0.1.2b'
+
+JAX = True
+
+IGNORE = [
+    "No GPU/TPU found, falling back to CPU.",
+    "Casting complex values to real discards the imaginary part"]
