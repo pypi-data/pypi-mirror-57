ba4383611fc8cc3ba4d188f42526f3abcefba566
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:25 +0000

    Add package syrupy/0.0.4/syrupy-0.0.4-py3-none-any.whl

---
+++ packages/syrupy/syrupy-0.0.4-py3-none-any.whl/syrupy/serializer.py
@@ -0,0 +1,12 @@
+from typing import Callable
+
+
+class SnapshotSerializer:
+    def __init__(self):
+        pass
+
+    def encode(self, data):
+        return data
+
+    def decode(self, data):
+        return data
