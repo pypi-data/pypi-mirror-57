6d771aee9d87f49968ce85a1d79e1758188d1d4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:38 +0000

    Add package mypy-boto3-cognito-identity/1.10.33/mypy_boto3_cognito_identity-1.10.33-py3-none-any.whl

---
+++ packages/mypy-boto3-cognito-identity/mypy_boto3_cognito_identity-1.10.33-py3-none-any.whl/mypy_boto3_cognito_identity/paginator.py
@@ -0,0 +1,25 @@
+"Main interface for cognito-identity service Paginators"
+from __future__ import annotations
+
+from botocore.paginate import Paginator as Boto3Paginator
+from mypy_boto3_cognito_identity.type_defs import (
+    ListIdentityPoolsPaginatePaginationConfigTypeDef,
+    ListIdentityPoolsPaginateResponseTypeDef,
+)
+
+
+__all__ = ("ListIdentityPoolsPaginator",)
+
+
+class ListIdentityPoolsPaginator(Boto3Paginator):
+    """
+    Paginator for `list_identity_pools`
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self, PaginationConfig: ListIdentityPoolsPaginatePaginationConfigTypeDef = None
+    ) -> ListIdentityPoolsPaginateResponseTypeDef:
+        """
+        [ListIdentityPools.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.33/reference/services/cognito-identity.html#CognitoIdentity.Paginator.ListIdentityPools.paginate)
+        """
