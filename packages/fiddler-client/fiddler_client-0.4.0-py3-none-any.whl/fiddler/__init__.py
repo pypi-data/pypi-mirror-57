bf9e53147372e05026a7b68ceb8b3c57b6f78549
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:49 +0000

    Add package fiddler-client/0.4.0/fiddler_client-0.4.0-py3-none-any.whl

---
+++ packages/fiddler-client/fiddler_client-0.4.0-py3-none-any.whl/fiddler/__init__.py
@@ -0,0 +1,34 @@
+"""
+Fiddler Client Module
+=====================
+
+A Python client for Fiddler service.
+
+TODO: Add Licence.
+"""
+
+from ._version import __version__
+from .fiddler_api import FiddlerApi
+from .client import Fiddler
+from .client import PredictionEventBundle
+from .core_objects import (
+    Column,
+    DatasetInfo,
+    DataType,
+    ModelInfo,
+    ModelInputType,
+    ModelTask,
+)
+
+__all__ = [
+    '__version__',
+    'Column',
+    'DatasetInfo',
+    'DataType',
+    'Fiddler',
+    'FiddlerApi',
+    'ModelInfo',
+    'ModelInputType',
+    'ModelTask',
+    'PredictionEventBundle',
+]
