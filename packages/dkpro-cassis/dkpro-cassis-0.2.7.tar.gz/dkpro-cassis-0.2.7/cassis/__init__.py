840c9e66bb44c63338eeef6f32ea06dd04006bd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:57 +0000

    Add package dkpro-cassis/0.2.7/dkpro-cassis-0.2.7.tar.gz

---
+++ packages/dkpro-cassis/dkpro-cassis-0.2.7.tar.gz/dkpro-cassis-0.2.7/cassis/__init__.py
@@ -0,0 +1,11 @@
+"""UIMA CAS processing library in Python."""
+
+from .cas import Cas, Sofa, View
+from .typesystem import TypeSystem, load_typesystem, load_dkpro_core_typesystem
+from .xmi import load_cas_from_xmi
+
+__all__ = ["Cas", "Sofa", "View", "TypeSystem", "load_typesystem", "load_dkpro_core_typesystem", "load_cas_from_xmi"]
+
+del cas
+del typesystem
+del xmi
---
+++ packages/dkpro-cassis/dkpro-cassis-0.2.7.tar.gz/dkpro-cassis-0.2.7/tests/test_cas.py
@@ -0,0 +1,278 @@
+import random
+
+import attr
+
+from tests.fixtures import *
+
+from cassis.cas import Cas
+
+# Cas
+
+
+def test_default_typesystem_is_not_shared():
+    # https://github.com/dkpro/dkpro-cassis/issues/67
+    cas1 = Cas()
+    cas2 = Cas()
+
+    t1 = cas1.typesystem.create_type(name="test.Type")
+    t2 = cas2.typesystem.create_type(name="test.Type")
+
+
+def test_default_typesystem_is_not_shared_load_from_xmi(empty_cas_xmi):
+    # https://github.com/dkpro/dkpro-cassis/issues/67
+    cas1 = load_cas_from_xmi(empty_cas_xmi)
+    cas2 = load_cas_from_xmi(empty_cas_xmi)
+
+    t1 = cas1.typesystem.create_type(name="test.Type")
+    t2 = cas2.typesystem.create_type(name="test.Type")
+
+
+# View
+
+
+def test_initial_view_is_created():
+    cas = Cas()
+
+    view = cas.get_view("_InitialView")
+
+    sofa = view.get_sofa()
+    attr.validate(sofa)
+    assert sofa.sofaID == "_InitialView"
+
+
+def test_create_view_creates_view():
+    cas = Cas()
+
+    view = cas.create_view("testView")
+    sofa = view.get_sofa()
+
+    attr.validate(sofa)
+    assert sofa.sofaID == "testView"
+
+
+def test_create_view_throws_if_view_already_exists():
+    cas = Cas()
+    cas.create_view("testView")
+
+    with pytest.raises(ValueError, match=r"A view with name \[testView\] already exists!"):
+        cas.create_view("testView")
+
+
+def test_get_view_finds_existing_view():
+    cas = Cas()
+    cas.create_view("testView")
+    cas.sofa_string = "Initial"
+
+    view = cas.get_view("testView")
+    view.sofa_string = "testView42"
+
+    sofa = view.get_sofa()
+    attr.validate(sofa)
+    assert sofa.sofaID == "testView"
+    assert cas.sofa_string == "Initial"
+    assert view.sofa_string == "testView42"
+
+
+def test_get_view_throws_if_view_does_not_exist():
+    cas = Cas()
+
+    with pytest.raises(KeyError, match=r"There is no view with name \[testView\] in this CAS!"):
+        cas.get_view("testView")
+
+
+# Sofa
+
+
+def test_sofa_string_can_be_set_and_read():
+    cas = Cas()
+
+    cas.sofa_string = "I am a test sofa string!"
+
+    assert cas.sofa_string == "I am a test sofa string!"
+
+
+def test_sofa_mime_can_be_set_and_read():
+    cas = Cas()
+
+    cas.sofa_mime = "text/plain"
+
+    assert cas.sofa_mime == "text/plain"
+
+
+def test_sofa_uri_can_be_set_and_read():
+    cas = Cas()
+
+    cas.sofa_uri = "https://raw.githubusercontent.com/dkpro/dkpro-cassis/master/README.rst"
+
+    assert cas.sofa_uri == "https://raw.githubusercontent.com/dkpro/dkpro-cassis/master/README.rst"
+
+
+# Select
+
+
+def test_select(tokens, sentences):
+    cas = Cas()
+    cas.add_annotations(tokens + sentences)
+
+    actual_tokens = list(cas.select("cassis.Token"))
+    actual_sentences = list(cas.select("cassis.Sentence"))
+
+    assert actual_tokens == tokens
+    assert actual_sentences == sentences
+
+
+def test_select_covered(tokens, sentences):
+    cas = Cas()
+    cas.add_annotations(tokens + sentences)
+    first_sentence, second_sentence = sentences
+    tokens_in_first_sentence = tokens[:6]
+    tokens_in_second_sentence = tokens[6:]
+
+    actual_tokens_in_first_sentence = list(cas.select_covered("cassis.Token", first_sentence))
+    actual_tokens_in_second_sentence = list(cas.select_covered("cassis.Token", second_sentence))
+
+    assert actual_tokens_in_first_sentence == tokens_in_first_sentence
+    assert actual_tokens_in_second_sentence == tokens_in_second_sentence
+
+
+def test_select_covering(tokens, sentences):
+    cas = Cas()
+    cas.add_annotations(tokens + sentences)
+    actual_first_sentence, actual_second_sentence = sentences
+    tokens_in_first_sentence = tokens[:6]
+    tokens_in_second_sentence = tokens[6:]
+
+    for token in tokens_in_first_sentence:
+        result = list(cas.select_covering("cassis.Sentence", token))
+        first_sentence = result[0]
+
+        assert len(result) == 1
+        assert actual_first_sentence == first_sentence
+
+    for token in tokens_in_second_sentence:
+        result = list(cas.select_covering("cassis.Sentence", token))
+        second_sentence = result[0]
+
+        assert len(result) == 1
+        assert actual_second_sentence == second_sentence
+
+
+def test_select_only_returns_annotations_of_current_view(tokens, sentences):
+    cas = Cas()
+    cas.add_annotations(tokens)
+    view = cas.create_view("testView")
+    view.add_annotations(sentences)
+
+    actual_annotations_in_initial_view = list(cas.get_view("_InitialView").select_all())
+    actual_annotations_in_test_view = list(cas.get_view("testView").select_all())
+
+    assert tokens == actual_annotations_in_initial_view
+    assert sentences == actual_annotations_in_test_view
+
+
+def test_select_returns_feature_structures(cas_with_collections_xmi, typesystem_with_collections_xml):
+    typesystem = load_typesystem(typesystem_with_collections_xml)
+    cas = load_cas_from_xmi(cas_with_collections_xmi, typesystem=typesystem)
+
+    arrs = list(cas.select("uima.cas.StringArray"))
+
+    assert len(arrs) == 1
+
+
+# Covered text
+
+
+def test_get_covered_text_tokens(tokens):
+    cas = Cas()
+    cas.sofa_string = "Joe waited for the train . The train was late ."
+
+    actual_text = [token.get_covered_text() for token in tokens]
+
+    expected_text = ["Joe", "waited", "for", "the", "train", ".", "The", "train", "was", "late", "."]
+    assert actual_text == expected_text
+
+
+def test_FeatureStructure_get_covered_text_tokens(tokens):
+    cas = Cas()
+    cas.sofa_string = "Joe waited for the train . The train was late ."
+
+    actual_text = [token.get_covered_text() for token in tokens]
+
+    expected_text = ["Joe", "waited", "for", "the", "train", ".", "The", "train", "was", "late", "."]
+    assert actual_text == expected_text
+
+
+def test_get_covered_text_sentences(sentences):
+    cas = Cas()
+    cas.sofa_string = "Joe waited for the train . The train was late ."
+
+    actual_text = [sentence.get_covered_text() for sentence in sentences]
+
+    expected_text = ["Joe waited for the train .", "The train was late ."]
+    assert actual_text == expected_text
+
+
+def test_FeatureStructure_get_covered_text_sentences(sentences):
+    cas = Cas()
+    cas.sofa_string = "Joe waited for the train . The train was late ."
+
+    actual_text = [sentence.get_covered_text() for sentence in sentences]
+
+    expected_text = ["Joe waited for the train .", "The train was late ."]
+    assert actual_text == expected_text
+
+
+# Adding annotations
+
+
+def test_add_annotation(small_typesystem_xml):
+    typesystem = load_typesystem(small_typesystem_xml)
+    TokenType = typesystem.get_type("cassis.Token")
+    cas = Cas(typesystem)
+    cas.sofa_string = "Joe waited for the train ."
+
+    tokens = [
+        TokenType(begin=0, end=3, id="0", pos="NNP"),
+        TokenType(begin=4, end=10, id="1", pos="VBD"),
+        TokenType(begin=11, end=14, id="2", pos="IN"),
+        TokenType(begin=15, end=18, id="3", pos="DT"),
+        TokenType(begin=19, end=24, id="4", pos="NN"),
+        TokenType(begin=25, end=26, id="5", pos="."),
+    ]
+    for token in tokens:
+        cas.add_annotation(token)
+
+    actual_tokens = list(cas.select(TokenType.name))
+    assert actual_tokens == tokens
+
+
+def test_add_annotation_generates_ids(small_typesystem_xml, tokens):
+    typesystem = load_typesystem(small_typesystem_xml)
+    cas = Cas(typesystem)
+    TokenType = typesystem.get_type("cassis.Token")
+
+    tokens = [
+        TokenType(begin=0, end=3, id="0", pos="NNP"),
+        TokenType(begin=4, end=10, id="1", pos="VBD"),
+        TokenType(begin=11, end=14, id="2", pos="IN"),
+        TokenType(begin=15, end=18, id="3", pos="DT"),
+        TokenType(begin=19, end=24, id="4", pos="NN"),
+        TokenType(begin=25, end=26, id="5", pos="."),
+    ]
+    for token in tokens:
+        cas.add_annotation(token)
+
+    actual_tokens = list(cas.select(TokenType.name))
+    assert all([token.xmiID is not None for token in actual_tokens])
+
+
+def test_annotations_are_ordered_correctly(tokens):
+    annotations = list(tokens)
+    random.shuffle(list(annotations))
+    cas = Cas()
+    for token in annotations:
+        cas.add_annotation(token)
+
+    actual_tokens = list(cas.select("cassis.Token"))
+
+    assert actual_tokens == tokens
