05a264d452016d6f07c5e6d20fc124698f8c8b8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:20 +0000

    Add package pysatMagVect/0.4.0/pysatMagVect-0.4.0-cp38-cp38-macosx_10_9_x86_64.whl

---
+++ packages/pysatMagVect/pysatMagVect-0.4.0-cp38-cp38-macosx_10_9_x86_64.whl/pysatMagVect/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import os
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'version.txt')) as version_file:
+    __version__ = version_file.read().strip()
+del here
+
+on_rtd = os.environ.get('ONREADTHEDOCS') == 'True'
+
+if not on_rtd:
+    from . import igrf
+else:
+    igrf = None
+    
+from . import _core
+from ._core import *
+from . import satellite
+
+__all__ = []
