f1449c2698919aefe57dab781936e0d181bd044b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:48 +0000

    Add package foxtail-blog/0.7.0/foxtail-blog-0.7.0.tar.gz

---
+++ packages/foxtail-blog/foxtail-blog-0.7.0.tar.gz/foxtail-blog-0.7.0/foxtail_blog/__init__.py
@@ -0,0 +1,3 @@
+VERSION = (0, 7, 0)
+
+default_app_config = "foxtail_blog.apps.BlogConfig"
---
+++ packages/foxtail-blog/foxtail-blog-0.7.0.tar.gz/foxtail-blog-0.7.0/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="foxtail-blog",
+    version="0.7.0",
+    author="Luke Rogers",
+    author_email="lukeroge@gmail.com",
+    description="A blog.",
+    install_requires=['django>=2.2', 'django-markdownfield', 'django-taggit',
+                      'django-versatileimagefield', 'django-crispy-forms', 'django-recaptcha'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dmptrluke/foxtail-blog",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
