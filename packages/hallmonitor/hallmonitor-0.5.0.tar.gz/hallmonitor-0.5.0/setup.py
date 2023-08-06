fdb5e64ef897fe1e0ab46e3109111ab981a3f980
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:06 +0000

    Add package hallmonitor/0.5.0/hallmonitor-0.5.0.tar.gz

---
+++ packages/hallmonitor/hallmonitor-0.5.0.tar.gz/hallmonitor-0.5.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hallmonitor',
+    version='0.5.0',
+    description='An integration testing library that runs tests from yaml configuration.',
+    python_requires='~=3.6',
+    packages=find_packages(),
+    scripts=['hallmonitor/hallmonitor'],
+    install_requires=[
+        'pyyaml',
+        'requests',
+        'colorama',
+        'apscheduler',
+        'schedule'
+    ]
+)
