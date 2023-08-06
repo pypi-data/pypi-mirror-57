ac5b3ac95e04ee5b0e094e7c5f24a859cbddc468
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:45 +0000

    Add package sqlitedao/0.0.3/sqlitedao-0.0.3.tar.gz

---
+++ packages/sqlitedao/sqlitedao-0.0.3.tar.gz/sqlitedao-0.0.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='sqlitedao',
+      version='0.0.3',
+      description='simple single file dao for sqlite, WARNING: Not ready until 0.1.0',
+      author='Rocky Li',
+      url='https://github.com/Aperocky/sqlitedao',
+      author_email='aperocky@gmail.com',
+      license='MIT',
+      packages=['sqlitedao'])
