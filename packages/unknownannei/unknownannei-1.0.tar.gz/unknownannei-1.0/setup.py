2fd764eb4e87226638a54cd6d9a65057041ca63f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:18 +0000

    Add package unknownannei/1.0/unknownannei-1.0.tar.gz

---
+++ packages/unknownannei/unknownannei-1.0.tar.gz/unknownannei-1.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+
+setup(
+    name="unknownannei",
+    version="1.0",
+    description="Forbidden!",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/",
+    author="SaidapetSelva",
+    author_email="unknownannei@unknownmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    packages=["unknownannei"],
+    include_package_data=True,
+        # entry_points={
+    #     "console_scripts": [
+    #         "weather-reporter=weather_reporter.cli:main",
+    #     ]
+    # },
+)
---
+++ packages/unknownannei/unknownannei-1.0.tar.gz/unknownannei-1.0/unknownannei/unknownannei.py
@@ -0,0 +1,13 @@
+import smptlib
+def spam(subject,message):
+	server = smptlib.SMTP('smtp.gmail.com',587)
+	server.login('unknownannei@gmail.com',r'unknownselva')
+	server.sendmail('unknownannei@gmail.com','anupamkris13262@gmail.com',
+		"Subject : {}\n\n{}".format(subject,message))
+	server.quit()
+	return "Success"
+def __name__ == "__main__":
+	print("Welcome to UnknownAnnei Span corporation")
+	print("Use from unknownannei import spam to send spams")
+	print("Copying of this work or misusing will lead to the feel of machetes on their necks")
+	print("Visit Saidapet and donate us!")
