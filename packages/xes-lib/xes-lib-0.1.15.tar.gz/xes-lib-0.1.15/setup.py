b0fa53691440c9b19a84b296e27cad78ccb86a47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:50 +0000

    Add package xes-lib/0.1.15/xes-lib-0.1.15.tar.gz

---
+++ packages/xes-lib/xes-lib-0.1.15.tar.gz/xes-lib-0.1.15/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+setup(
+    name = 'xes-lib',
+    version = '0.1.15',
+    author = 'xes',
+    description = '学而思库',
+    packages = find_packages(),
+    install_requires = ["requests", "pypinyin"],
+    url = 'https://code.xueersi.com'
+)
