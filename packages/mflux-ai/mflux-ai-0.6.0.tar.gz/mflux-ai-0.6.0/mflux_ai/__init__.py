7b3c73728bd6e1b0b5d7eb98795a88856028ce8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:13 +0000

    Add package mflux-ai/0.6.0/mflux-ai-0.6.0.tar.gz

---
+++ packages/mflux-ai/mflux-ai-0.6.0.tar.gz/mflux-ai-0.6.0/mflux_ai/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import warnings
+
+import mflux_ai.mflux_ai as mflux_ai
+from .core import init, get_minio_client, ensure_bucket_exists, put_dataset, get_dataset
+
+"""Top-level package for mflux-ai."""
+
+__author__ = """AIA Science AS"""
+__email__ = "mflux.ai@aiascience.com"
+__version__ = "0.6.0"
+
+
+def set_env_vars(token):
+    warnings.warn(
+        "mflux_ai.set_env_vars() is deprecated. Please use mflux_ai.init() instead.",
+        DeprecationWarning,
+    )
+    return init(token)
---
+++ packages/mflux-ai/mflux-ai-0.6.0.tar.gz/mflux-ai-0.6.0/tests/test_core.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for the `mflux_ai` package."""
+import os
+import warnings
+
+import responses
+
+import mflux_ai
+from mflux_ai.core import SERVER_HOST
+
+
+@responses.activate
+def test_mflux_ai_init():
+    """Test the init function."""
+    content = {
+        "minio_secret_key": "minio_secret",
+        "minio_access_key": "minio_access",
+        "minio_server": "http://192.198.0.1:9000",
+        "mlflow_server": "http://192.198.0.1:5000",
+    }
+
+    responses.add(
+        responses.Response(
+            method="GET", url=SERVER_HOST + "/api/env_vars/", json=content, status=200
+        )
+    )
+
+    mflux_ai.init("thisshouldbevalidtoken")
+    assert os.environ.get("MLFLOW_TRACKING_URI") == content["mlflow_server"]
+    assert os.environ.get("MLFLOW_S3_ENDPOINT_URL") == content["minio_server"]
+    assert os.environ.get("AWS_SECRET_ACCESS_KEY") == content["minio_secret_key"]
+    assert os.environ.get("AWS_ACCESS_KEY_ID") == content["minio_access_key"]
+    assert os.environ.get("MLFLOW_TRACKING_TOKEN") == "thisshouldbevalidtoken"
+
+
+@responses.activate
+def test_mflux_ai_deprecated_set_env_vars():
+    """Test the deprecated set_env_vars function."""
+    content = {
+        "minio_secret_key": "minio_secret",
+        "minio_access_key": "minio_access",
+        "minio_server": "http://192.198.0.1:9000",
+        "mlflow_server": "http://192.198.0.1:5000",
+    }
+
+    responses.add(
+        responses.Response(
+            method="GET", url=SERVER_HOST + "/api/env_vars/", json=content, status=200
+        )
+    )
+
+    with warnings.catch_warnings(record=True) as w:
+        # Cause all warnings to always be triggered.
+        warnings.simplefilter("always")
+        mflux_ai.set_env_vars("thisshouldbevalidtoken")
+        assert len(w) == 1
+        assert issubclass(w[-1].category, DeprecationWarning)
+        assert "deprecated" in str(w[-1].message)
+
+    assert os.environ.get("MLFLOW_TRACKING_URI") == content["mlflow_server"]
+    assert os.environ.get("MLFLOW_S3_ENDPOINT_URL") == content["minio_server"]
+    assert os.environ.get("AWS_SECRET_ACCESS_KEY") == content["minio_secret_key"]
+    assert os.environ.get("AWS_ACCESS_KEY_ID") == content["minio_access_key"]
+    assert os.environ.get("MLFLOW_TRACKING_TOKEN") == "thisshouldbevalidtoken"
