78086a51ee4c511bca97cb66994f484d3556e365
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:56 +0000

    Add package foxtail-blog/0.5.2/foxtail-blog-0.5.2.tar.gz

---
+++ packages/foxtail-blog/foxtail-blog-0.5.2.tar.gz/foxtail-blog-0.5.2/foxtail_blog/admin.py
@@ -0,0 +1,53 @@
+from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+from django.db.models import TextField
+from django.forms import Textarea
+from django.template.defaultfilters import truncatechars
+from django.urls import reverse
+from django.utils.html import format_html
+
+from .models import Post, Comment
+
+
+class PostAdmin(ModelAdmin):
+    fieldsets = (
+        ('Content', {
+            'fields': ('title', 'tags', 'author', 'text')
+        }),
+        ('Image', {
+            'fields': ('image',),
+        }),
+        ('Advanced options', {
+            'fields': ('slug', 'allow_comments'),
+        }),
+    )
+
+    formfield_overrides = {
+        TextField: {'widget': Textarea(attrs={'rows': 40, 'cols': 120})},
+    }
+
+    prepopulated_fields = {"slug": ("title",)}
+    list_filter = ('created', 'tags', 'author')
+    list_display = ('title', 'tag_list', 'created', 'modified', 'author')
+
+    def tag_list(self, obj):
+        return u", ".join(o.name for o in obj.tags.all())
+
+
+class CommentAdmin(ModelAdmin):
+    list_display = ('text_preview', 'post_link', 'author', 'created_date')
+
+    def post_link(self, obj):
+        return format_html('<a href="{}">{}</a>',
+                           reverse("admin:foxtail_blog_post_change", args=(obj.post.pk,)),
+                           obj.post.title)
+
+    def text_preview(self, obj):
+        return truncatechars(obj.text, 50)
+
+    text_preview.short_description = "Comment"
+    post_link.short_description = "Post"
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
---
+++ packages/foxtail-blog/foxtail-blog-0.5.2.tar.gz/foxtail-blog-0.5.2/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="foxtail-blog",
+    version="0.5.2",
+    author="Luke Rogers",
+    author_email="lukeroge@gmail.com",
+    description="A blog.",
+    install_requires=['django>=2.2', 'bleach', 'bleach_whitelist', 'markdown', 'django-taggit',
+                      'django-versatileimagefield', 'django-crispy-forms', 'django-recaptcha'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dmptrluke/foxtail-blog",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
