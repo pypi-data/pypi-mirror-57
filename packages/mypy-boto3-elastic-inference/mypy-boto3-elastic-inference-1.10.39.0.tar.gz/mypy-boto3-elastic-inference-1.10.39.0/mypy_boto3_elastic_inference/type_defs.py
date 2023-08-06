f2f3fd4d0e0e485a60ef9a5c8864f83d2ca64fe7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:14 +0000

    Add package mypy-boto3-elastic-inference/1.10.39.0/mypy-boto3-elastic-inference-1.10.39.0.tar.gz

---
+++ packages/mypy-boto3-elastic-inference/mypy-boto3-elastic-inference-1.10.39.0.tar.gz/mypy-boto3-elastic-inference-1.10.39.0/mypy_boto3_elastic_inference/type_defs.py
@@ -0,0 +1,13 @@
+"Main interface for elastic-inference service type defs"
+import sys
+from typing import Dict
+
+if sys.version_info >= (3, 8):
+    from typing import TypedDict
+else:
+    from typing_extensions import TypedDict
+
+
+ListTagsForResourceResultTypeDef = TypedDict(
+    "ListTagsForResourceResultTypeDef", {"tags": Dict[str, str]}, total=False
+)
