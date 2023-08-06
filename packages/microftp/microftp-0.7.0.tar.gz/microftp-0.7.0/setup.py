b7f42b599578f60a5aa979b2ba30ad9eb749be33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:41 +0000

    Add package microftp/0.7.0/microftp-0.7.0.tar.gz

---
+++ packages/microftp/microftp-0.7.0.tar.gz/microftp-0.7.0/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="microftp",
+    version="0.7.0",
+    author="Vlatko Kosturjak",
+    author_email="vlatko.kosturjak@gmail.com",
+    description="Small FTP library client to handle broken servers",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kost/microftp-python",
+    packages=setuptools.find_packages(),
+    py_modules=['microftp'],
+    install_requires=[],
+    classifiers=[
+      "Programming Language :: Python :: 2",
+      "Programming Language :: Python :: 3",
+      "License :: OSI Approved :: MIT License",
+      "Operating System :: OS Independent",
+    ],
+    scripts=['scripts/microftpcmd']
+)
+
