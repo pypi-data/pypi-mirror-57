504256a03a9e17992997ec20d6f7050e9aa90b08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:39 +0000

    Add package TitleRank/0.0.5/TitleRank-0.0.5-py2.py3-none-any.whl

---
+++ packages/TitleRank/TitleRank-0.0.5-py2.py3-none-any.whl/TitleRank/open.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# -*-coding:utf-8 -*-
+#Reference:**********************************************
+# @Time     : 2019-12-10 19:59
+# @Author   : 病虎
+# @E-mail   : victor.xsyang@gmail.com
+# @File     : open.py
+# @User     : ora
+# @Software: PyCharm
+# @Description: 
+#Reference:**********************************************
+stopwords_path="TitleRank/data/stopwords.txt"
+STOPWORDS = set(map(str.strip, open(stopwords_path).readlines()))
