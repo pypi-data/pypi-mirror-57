f0759564ddd1d50f5bbaf50d647e44c30e581c25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:08 +0000

    Add package tksheet/3.7/tksheet-3.7.tar.gz

---
+++ packages/tksheet/tksheet-3.7.tar.gz/tksheet-3.7/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+setup(
+  name = 'tksheet',
+  py_modules = ['tksheet'],
+  version = '3.7',
+  license='MIT',
+  description = 'Tkinter table / sheet widget',
+  author = 'ragardner',
+  author_email = 'ragardner@protonmail.com',
+  url = 'https://github.com/ragardner/tksheet',
+  download_url = 'https://github.com/ragardner/tksheet/archive/3.7.tar.gz',
+  keywords = ['tkinter', 'table', 'widget'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
