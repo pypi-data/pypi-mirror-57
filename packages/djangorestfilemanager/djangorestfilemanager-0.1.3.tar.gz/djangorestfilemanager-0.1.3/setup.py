b507d13164a934ee003d0441a9828eca9906c392
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:55 +0000

    Add package djangorestfilemanager/0.1.3/djangorestfilemanager-0.1.3.tar.gz

---
+++ packages/djangorestfilemanager/djangorestfilemanager-0.1.3.tar.gz/djangorestfilemanager-0.1.3/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(name='djangorestfilemanager',
+      version='0.1.3',
+      description='Django REST File Manager',
+      url='https://gitlab.com/kas-factory/packages/django-rest-file-manager',
+      author='Avelino @ KF',
+      author_email='avelino@kasfactory.net',
+      license='MIT',
+      packages=find_packages(),
+      package_data={
+          'djangorestfilemanager': ['djangorestfilemanager/*',
+                                    'djangorestfilemanager/migrations/*',
+                                    'djangorestfilemanager/locale/en/LC_MESSAGES/django.po',
+                                    'djangorestfilemanager/locale/es/LC_MESSAGES/django.po',
+                                    'djangorestfilemanager/locale/en/LC_MESSAGES/django.mo',
+                                    'djangorestfilemanager/locale/es/LC_MESSAGES/django.mo',
+                                    'djangorestfilemanager/test/*'
+                                    'djangorestfilemanager/test/files/*'
+                                    ]
+      },
+      install_requires=[
+          'Django',
+          'djangorestframework',
+          'django-storages',
+          'django-filter',
+          'django-cleanup',
+          'djangopubsub',
+          'boto3',
+          'python-dateutil'
+      ],
+      zip_safe=False
+      )
