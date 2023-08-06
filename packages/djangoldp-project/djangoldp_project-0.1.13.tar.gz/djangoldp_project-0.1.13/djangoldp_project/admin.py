2af156c90a9d06b08d0ac019b17963cb68c74945
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:20 +0000

    Add package djangoldp-project/0.1.13/djangoldp_project-0.1.13.tar.gz

---
+++ packages/djangoldp-project/djangoldp_project-0.1.13.tar.gz/djangoldp_project-0.1.13/djangoldp_project/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from guardian.admin import GuardedModelAdmin
+from .models import Project, Member, Customer, BusinessProvider
+
+
+class TeamInline(admin.TabularInline):
+    model = Member
+    extra = 0
+
+
+class ProjectAdmin(GuardedModelAdmin):
+    inlines = [TeamInline]
+
+
+admin.site.register(Project, ProjectAdmin)
+
