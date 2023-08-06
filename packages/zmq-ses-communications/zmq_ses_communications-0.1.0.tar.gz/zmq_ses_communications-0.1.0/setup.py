30b74a7e1d7a0f3f684d091290b7f1132094f0da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:39 +0000

    Add package zmq-ses-communications/0.1.0/zmq_ses_communications-0.1.0.tar.gz

---
+++ packages/zmq-ses-communications/zmq_ses_communications-0.1.0.tar.gz/zmq_ses_communications-0.1.0/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['zmq_ses_communications',
+ 'zmq_ses_communications.client',
+ 'zmq_ses_communications.client.msgs',
+ 'zmq_ses_communications.server']
+
+package_data = \
+{'': ['*'], 'zmq_ses_communications': ['tests/*']}
+
+install_requires = \
+['protobuf>=3.11.1,<4.0.0', 'psutil>=5.6.7,<6.0.0', 'pyzmq>=18.1.1,<19.0.0']
+
+setup_kwargs = {
+    'name': 'zmq-ses-communications',
+    'version': '0.1.0',
+    'description': 'communication backend for creating a distributed system for integrating devices in a smart factory',
+    'long_description': None,
+    'author': 'Vinu Home',
+    'author_email': 'vinuvnair@outlook.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
---
+++ packages/zmq-ses-communications/zmq_ses_communications-0.1.0.tar.gz/zmq_ses_communications-0.1.0/zmq_ses_communications/client/pub_sub_node.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import asyncio
+import logging
+from threading import Thread
+
+from .async_pub import Publisher
+from .async_sub import Subscriber
+
+
+class PubSubNode(Publisher, Subscriber):
+    def __init__(self, host, port):
+        self.pubsub_logger = logging.getLogger("PubSubNode")
+        Publisher.__init__(self, host, port)
+        Subscriber.__init__(self, host, port + 1)
+        pub = Thread(target=self.run_pub)
+        subs = ["test", "HEARTBEAT"]
+        sub = Thread(target=self.run_sub, args=(subs,))
+        pub.start()
+        sub.start()
+        self.pubsub_logger.info("Started Publisher and subscriber threads")
+
+
+if __name__ == "__main__":
+    N1 = PubSubNode("127.0.0.1", 6000)
+    print("this will not print")
+    N1.create_publisher()
