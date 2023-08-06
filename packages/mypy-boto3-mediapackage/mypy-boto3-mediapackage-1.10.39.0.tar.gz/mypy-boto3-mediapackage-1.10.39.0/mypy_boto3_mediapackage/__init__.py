412e86966a8f55c3fa0af7228551e92fd56421a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:16 +0000

    Add package mypy-boto3-mediapackage/1.10.39.0/mypy-boto3-mediapackage-1.10.39.0.tar.gz

---
+++ packages/mypy-boto3-mediapackage/mypy-boto3-mediapackage-1.10.39.0.tar.gz/mypy-boto3-mediapackage-1.10.39.0/mypy_boto3_mediapackage/__init__.py
@@ -0,0 +1,17 @@
+"Main interface for mediapackage service"
+
+from mypy_boto3_mediapackage.client import MediaPackageClient, MediaPackageClient as Client
+from mypy_boto3_mediapackage.paginator import (
+    ListChannelsPaginator,
+    ListHarvestJobsPaginator,
+    ListOriginEndpointsPaginator,
+)
+
+
+__all__ = (
+    "Client",
+    "ListChannelsPaginator",
+    "ListHarvestJobsPaginator",
+    "ListOriginEndpointsPaginator",
+    "MediaPackageClient",
+)
