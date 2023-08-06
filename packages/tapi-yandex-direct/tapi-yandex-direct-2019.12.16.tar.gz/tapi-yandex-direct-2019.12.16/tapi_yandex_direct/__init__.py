90c7f61929e54513dda6bd44da372cc49e637fc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:55 +0000

    Add package tapi-yandex-direct/2019.12.16/tapi-yandex-direct-2019.12.16.tar.gz

---
+++ packages/tapi-yandex-direct/tapi-yandex-direct-2019.12.16.tar.gz/tapi-yandex-direct-2019.12.16/tapi_yandex_direct/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Pavel Maksimov'
+__email__ = 'vur21@ya.com'
+__version__ = '2019.12.16'
+
+
+from .tapi_yandex_direct import YandexDirect, GetTokenYandexDirect
+from .resource_mapping import *
