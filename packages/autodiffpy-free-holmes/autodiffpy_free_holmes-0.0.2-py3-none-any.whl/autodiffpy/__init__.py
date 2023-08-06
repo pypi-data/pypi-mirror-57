329e386dcc854a2b1003e706a83bd0f6cd74e9be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:15 +0000

    Add package autodiffpy-free-holmes/0.0.2/autodiffpy_free_holmes-0.0.2-py3-none-any.whl

---
+++ packages/autodiffpy-free-holmes/autodiffpy_free_holmes-0.0.2-py3-none-any.whl/autodiffpy/__init__.py
@@ -0,0 +1,2 @@
+from .forward import Constant, Var, Sin, Cos, Tan, Csc, Sec, Cot, Log, Vector
+from .reverse import Reverse, sin, cos, tan, sec, csc, cot, exp, sinh, cosh, tanh, sech, csch, coth, ln, log2, log10, log, sqrt, rVector
