1c1d2c9fa25c06f834440dc013302f59eb560061
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:48 +0000

    Add package color2vec/1.0.3/color2vec-1.0.3-py3-none-any.whl

---
+++ packages/color2vec/color2vec-1.0.3-py3-none-any.whl/color2vec/__init__.py
@@ -0,0 +1,11 @@
+print("you are using Greendeck's color2vec package")
+
+# your package information
+name = "color2vec"
+__version__ = "1.0.3"
+author = "chandan mishra"
+author_email = "chandan.mishra@greendeck.co"
+url = ""
+
+# import sub packages
+from .src.color2vec import Color2Vec
