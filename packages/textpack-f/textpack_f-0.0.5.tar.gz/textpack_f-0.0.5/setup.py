4ed42f926149e5cc367076f21732c583f027ace0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:32 +0000

    Add package textpack-f/0.0.5/textpack_f-0.0.5.tar.gz

---
+++ packages/textpack-f/textpack_f-0.0.5.tar.gz/textpack_f-0.0.5/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="textpack_f",
+    version="0.0.5",
+    author="Luke Whyte",
+    author_email="lukeawhyte@gmail.com",
+    description="Quickly identify and group similar text strings in a large dataset",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lukewhyte/textpack",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'pandas',
+        'sklearn',
+        'scipy',
+        'numpy',
+        'cython',
+        'sparse-dot-topn'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
