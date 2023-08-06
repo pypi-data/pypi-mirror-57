34cd463576ddf7056bdc33673d09633cd9838d35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:07 +0000

    Add package repobee-plug/0.12.0a1/repobee-plug-0.12.0a1.tar.gz

---
+++ packages/repobee-plug/repobee-plug-0.12.0a1.tar.gz/repobee-plug-0.12.0a1/repobee_plug/exception.py
@@ -0,0 +1,27 @@
+"""Exceptions for repobee_plug.
+
+.. module:: exception
+    :synopsis: Exceptions for repobee_plug.
+
+.. moduleauthor:: Simon Larsén
+"""
+
+
+class PlugError(Exception):
+    """Base class for all repobee_plug exceptions."""
+
+
+class HookNameError(PlugError):
+    """Raise when a public method in a class that inherits from
+    :py:class:`~repobee_plug.Plugin` does not have a hook name.
+    """
+
+
+class ExtensionCommandError(PlugError):
+    """Raise when an :py:class:~repobee_plug.containers.ExtensionCommand: is
+    incorrectly defined.
+    """
+
+
+class APIImplementationError(PlugError):
+    """Raise when an API is defined incorrectly."""
