46e573601e13c52943472e889455a11f58a48eff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:43 +0000

    Add package chepy/1.3.1/chepy-1.3.1.tar.gz

---
+++ packages/chepy/chepy-1.3.1.tar.gz/chepy-1.3.1/chepy/__init__.py
@@ -0,0 +1,42 @@
+from .modules.aritmeticlogic import AritmeticLogic
+from .modules.codetidy import CodeTidy
+from .modules.compression import Compression
+from .modules.dataformat import DataFormat
+from .modules.datetimemodule import DateTime
+from .modules.encryptionencoding import EncryptionEncoding
+from .modules.extractors import Extractors
+from .modules.forensics import Forensics
+from .modules.hashing import Hashing
+from .modules.language import Language
+from .modules.multimedia import Multimedia
+from .modules.networking import Networking
+from .modules.other import Other
+from .modules.pcap import Pcap
+from .modules.publickey import Publickey
+from .modules.utils import Utils
+
+from .conf import Config
+
+_plugins = Config().load_plugins()
+
+
+class Chepy(
+    AritmeticLogic,
+    CodeTidy,
+    Compression,
+    DataFormat,
+    DateTime,
+    EncryptionEncoding,
+    Extractors,
+    Forensics,
+    Hashing,
+    Language,
+    Multimedia,
+    Networking,
+    Other,
+    Pcap,
+    Publickey,
+    Utils,
+    *_plugins
+):
+    pass
