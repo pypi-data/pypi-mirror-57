c5db8b18fe3442f490e27fbab4ed94e37217ef45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:41 +0000

    Add package neotasker/0.0.4/neotasker-0.0.4.tar.gz

---
+++ packages/neotasker/neotasker-0.0.4.tar.gz/neotasker-0.0.4/neotasker/__init__.py
@@ -0,0 +1,28 @@
+__author__ = 'Altertech, https://www.altertech.com/'
+__copyright__ = 'Copyright (c) 2019 Altertech'
+__license__ = 'MIT'
+__version__ = '0.0.4'
+
+from neotasker.supervisor import TaskSupervisor
+
+task_supervisor = TaskSupervisor(supervisor_id='default')
+
+from neotasker.f import FunctionCollection
+
+from neotasker.localproxy import LocalProxy
+
+from neotasker.workers import background_worker
+from neotasker.workers import BackgroundWorker, BackgroundAsyncWorker
+from neotasker.workers import BackgroundEventWorker, BackgroundQueueWorker
+from neotasker.workers import BackgroundIntervalWorker
+
+import neotasker.supervisor
+import aiosched
+
+g = LocalProxy()
+
+
+def set_debug(mode=True):
+    neotasker.supervisor.debug = mode
+    neotasker.workers.debug = mode
+    aiosched.set_debug(mode)
