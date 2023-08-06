2e06c8e64f55a22a67b338ced046b59019ff0c6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:29 +0000

    Add package ipython-dawet-sql/0.1b2/ipython-dawet-sql-0.1b2.tar.gz

---
+++ packages/ipython-dawet-sql/ipython-dawet-sql-0.1b2.tar.gz/ipython-dawet-sql-0.1b2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='ipython-dawet-sql',
+    version='0.1b2',
+    packages=['dawetsql'],
+    url='https://gitlab.com/wakataw/ipython-dawet-sql',
+    license='MIT',
+    author='Agung Pratama',
+    author_email='agungpratama1001@gmail.com',
+    description='Ipython ODBC SQL Magic for Dawet',
+    install_requires=[
+        'pandas',
+        'pypyodbc',
+        'ipython',
+        'ipywidgets',
+        'cryptography',
+        'xlsxwriter'
+    ]
+)
