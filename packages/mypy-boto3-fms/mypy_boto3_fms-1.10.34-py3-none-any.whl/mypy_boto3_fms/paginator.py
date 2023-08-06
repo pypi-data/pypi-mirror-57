579118422f3bc8d2fbe58bb611b7f01f3b506c33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:51 +0000

    Add package mypy-boto3-fms/1.10.34/mypy_boto3_fms-1.10.34-py3-none-any.whl

---
+++ packages/mypy-boto3-fms/mypy_boto3_fms-1.10.34-py3-none-any.whl/mypy_boto3_fms/paginator.py
@@ -0,0 +1,59 @@
+"Main interface for fms service Paginators"
+from __future__ import annotations
+
+from botocore.paginate import Paginator as Boto3Paginator
+from mypy_boto3_fms.type_defs import (
+    ListComplianceStatusPaginatePaginationConfigTypeDef,
+    ListComplianceStatusPaginateResponseTypeDef,
+    ListMemberAccountsPaginatePaginationConfigTypeDef,
+    ListMemberAccountsPaginateResponseTypeDef,
+    ListPoliciesPaginatePaginationConfigTypeDef,
+    ListPoliciesPaginateResponseTypeDef,
+)
+
+
+__all__ = ("ListComplianceStatusPaginator", "ListMemberAccountsPaginator", "ListPoliciesPaginator")
+
+
+class ListComplianceStatusPaginator(Boto3Paginator):
+    """
+    Paginator for `list_compliance_status`
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self,
+        PolicyId: str,
+        PaginationConfig: ListComplianceStatusPaginatePaginationConfigTypeDef = None,
+    ) -> ListComplianceStatusPaginateResponseTypeDef:
+        """
+        [ListComplianceStatus.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.34/reference/services/fms.html#FMS.Paginator.ListComplianceStatus.paginate)
+        """
+
+
+class ListMemberAccountsPaginator(Boto3Paginator):
+    """
+    Paginator for `list_member_accounts`
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self, PaginationConfig: ListMemberAccountsPaginatePaginationConfigTypeDef = None
+    ) -> ListMemberAccountsPaginateResponseTypeDef:
+        """
+        [ListMemberAccounts.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.34/reference/services/fms.html#FMS.Paginator.ListMemberAccounts.paginate)
+        """
+
+
+class ListPoliciesPaginator(Boto3Paginator):
+    """
+    Paginator for `list_policies`
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self, PaginationConfig: ListPoliciesPaginatePaginationConfigTypeDef = None
+    ) -> ListPoliciesPaginateResponseTypeDef:
+        """
+        [ListPolicies.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.34/reference/services/fms.html#FMS.Paginator.ListPolicies.paginate)
+        """
