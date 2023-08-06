e199084848ed8ddf472aa17a3233e56a0123e151
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:52 +0000

    Add package openwisp-utils/0.3/openwisp_utils-0.3-py2.py3-none-any.whl

---
+++ packages/openwisp-utils/openwisp_utils-0.3-py2.py3-none-any.whl/openwisp_utils/admin_theme/admin.py
@@ -0,0 +1,22 @@
+from django.conf import settings
+from django.contrib import admin
+from django.utils.translation import ugettext_lazy
+
+
+def openwisp_admin(site_url=None):
+    # <title>
+    admin.site.site_title = getattr(settings,
+                                    'OPENWISP_ADMIN_SITE_TITLE',
+                                    'OpenWISP Admin')
+    # link to frontend
+    admin.site.site_url = site_url
+    # h1 text
+    admin.site.site_header = getattr(settings,
+                                     'OPENWISP_ADMIN_SITE_HEADER',
+                                     'OpenWISP')
+    # text at the top of the admin index page
+    admin.site.index_title = ugettext_lazy(
+        getattr(settings,
+                'OPENWISP_ADMIN_INDEX_TITLE',
+                'Network administration')
+    )
