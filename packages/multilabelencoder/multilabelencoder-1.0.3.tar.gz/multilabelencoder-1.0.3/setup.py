463ff698eb2d1b36a392f47b854f18eaa2521c34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:06 +0000

    Add package multilabelencoder/1.0.3/multilabelencoder-1.0.3.tar.gz

---
+++ packages/multilabelencoder/multilabelencoder-1.0.3.tar.gz/multilabelencoder-1.0.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+setup(  name = 'multilabelencoder',
+        version = "1.0.3",
+        license = 'MIT',
+        description = 'Labelencoder for many pandas dataframe columns package',
+        scripts=['multilabelencoder.py'] ,
+#        packages = ['multilabelencoder'],
+#        package_data = {'hmglib': ['readme.txt']},
+        install_requires = ['sklearn'],
+        zip_safe = False,
+        author = "Emna Jaoua",
+        author_email = 'emnajaoua1@gmail.com',
+        maintainer = "Emna Jaoua",
+        maintainer_email = "emnajaoua1@gmail.com",
+        dependency_links= [''],
+      url="https://github.com/emnajaoua/multilabelencoder"
+     )
