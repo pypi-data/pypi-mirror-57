0fc102f231a667f3d6594623138d0c47ffb8695d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:55 +0000

    Add package mypy-boto3-sms/1.10.37.0/mypy-boto3-sms-1.10.37.0.tar.gz

---
+++ packages/mypy-boto3-sms/mypy-boto3-sms-1.10.37.0.tar.gz/mypy-boto3-sms-1.10.37.0/mypy_boto3_sms/__init__.py
@@ -0,0 +1,21 @@
+"Main interface for sms service"
+
+from mypy_boto3_sms.client import SMSClient, SMSClient as Client
+from mypy_boto3_sms.paginator import (
+    GetConnectorsPaginator,
+    GetReplicationJobsPaginator,
+    GetReplicationRunsPaginator,
+    GetServersPaginator,
+    ListAppsPaginator,
+)
+
+
+__all__ = (
+    "Client",
+    "GetConnectorsPaginator",
+    "GetReplicationJobsPaginator",
+    "GetReplicationRunsPaginator",
+    "GetServersPaginator",
+    "ListAppsPaginator",
+    "SMSClient",
+)
