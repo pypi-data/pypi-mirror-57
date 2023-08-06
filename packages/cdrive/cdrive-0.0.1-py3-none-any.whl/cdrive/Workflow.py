cac508a7fa18de007697c09a75cfb0ea3fe53ea4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:14 +0000

    Add package cdrive/0.0.1/cdrive-0.0.1-py3-none-any.whl

---
+++ packages/cdrive/cdrive-0.0.1-py3-none-any.whl/cdrive/Workflow.py
@@ -0,0 +1,7 @@
+class Workflow:
+    def __init__(self, token, domain):
+        self.token = token
+        self.domain = domain
+
+    def user_details():
+       return ("User details will be returned here") 
