0ecb8ae9a7e172cf92510bd66937e576304fa0df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:34 +0000

    Add package mypy-boto3-lex-models/1.10.38.1/mypy-boto3-lex-models-1.10.38.1.tar.gz

---
+++ packages/mypy-boto3-lex-models/mypy-boto3-lex-models-1.10.38.1.tar.gz/mypy-boto3-lex-models-1.10.38.1/mypy_boto3_lex_models/__init__.py
@@ -0,0 +1,34 @@
+"Main interface for lex-models service"
+
+from mypy_boto3_lex_models.client import (
+    LexModelBuildingServiceClient,
+    LexModelBuildingServiceClient as Client,
+)
+from mypy_boto3_lex_models.paginator import (
+    GetBotAliasesPaginator,
+    GetBotChannelAssociationsPaginator,
+    GetBotVersionsPaginator,
+    GetBotsPaginator,
+    GetBuiltinIntentsPaginator,
+    GetBuiltinSlotTypesPaginator,
+    GetIntentVersionsPaginator,
+    GetIntentsPaginator,
+    GetSlotTypeVersionsPaginator,
+    GetSlotTypesPaginator,
+)
+
+
+__all__ = (
+    "Client",
+    "GetBotAliasesPaginator",
+    "GetBotChannelAssociationsPaginator",
+    "GetBotVersionsPaginator",
+    "GetBotsPaginator",
+    "GetBuiltinIntentsPaginator",
+    "GetBuiltinSlotTypesPaginator",
+    "GetIntentVersionsPaginator",
+    "GetIntentsPaginator",
+    "GetSlotTypeVersionsPaginator",
+    "GetSlotTypesPaginator",
+    "LexModelBuildingServiceClient",
+)
