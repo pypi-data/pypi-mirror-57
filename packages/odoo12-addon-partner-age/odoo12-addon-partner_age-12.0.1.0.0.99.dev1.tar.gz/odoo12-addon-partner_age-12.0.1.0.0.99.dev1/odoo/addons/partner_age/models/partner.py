23d2d469ea93bb5b2db48b8e9a9ad767586d01ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:00 +0000

    Add package odoo12-addon-partner-age/12.0.1.0.0.99.dev1/odoo12-addon-partner_age-12.0.1.0.0.99.dev1.tar.gz

---
+++ packages/odoo12-addon-partner-age/odoo12-addon-partner_age-12.0.1.0.0.99.dev1.tar.gz/odoo12-addon-partner_age-12.0.1.0.0.99.dev1/odoo/addons/partner_age/models/partner.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+from openerp.tools import DEFAULT_SERVER_DATE_FORMAT as OE_DFORMAT
+
+from openerp import models, fields, api
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    def _search_age(self, operator, value):
+        if operator not in ('=', '!=', '<', '<=', '>', '>=', 'in', 'not in'):
+            return []
+        query = """SELECT id
+                   FROM "%s"
+                   WHERE extract(year from age(CURRENT_DATE,
+                                               birthdate_date)) %s %%s""" % \
+                (self._table, operator)
+        self.env.cr.execute(query, (value,))
+        ids = [t[0] for t in self.env.cr.fetchall()]
+        return [('id', 'in', ids)]
+
+    @api.one
+    @api.depends('birthdate_date')
+    def _compute_age(self):
+        if self.birthdate_date:
+            dBday = datetime.strptime(str(self.birthdate_date),
+                                      OE_DFORMAT).date()
+            dToday = datetime.now().date()
+            self.age = dToday.year - dBday.year - ((
+                dToday.month, dToday.day) < (dBday.month, dBday.day))
+
+    age = fields.Integer(string='Age',
+                         compute='_compute_age',
+                         search='_search_age')
