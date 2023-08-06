659f7ddd21d4afe2258eee85d8b8c35bd9c1b679
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:07 +0000

    Add package click-option-group/0.2.1/click-option-group-0.2.1.tar.gz

---
+++ packages/click-option-group/click-option-group-0.2.1.tar.gz/click-option-group-0.2.1/setup.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+import pathlib
+from setuptools import setup
+
+
+PACKAGE_NAME = 'click_option_group'
+ROOT_DIR = pathlib.Path(__file__).parent
+
+
+def get_version():
+    version = {}
+    version_file = ROOT_DIR / PACKAGE_NAME / '_version.py'
+    exec(version_file.read_text(), version)
+    return version['__version__']
+
+
+def get_long_description():
+    readme = ROOT_DIR / 'README.md'
+    return readme.read_text(encoding='utf-8')
+
+
+setup(
+    name='click-option-group',
+    version=get_version(),
+    packages=[PACKAGE_NAME],
+    python_requires='>=3.6,<4',
+    install_requires=[
+        'Click>=7.0,<8',
+    ],
+    url='https://github.com/espdev/click-option-group',
+    project_urls={
+        "Code": 'https://github.com/espdev/click-option-group',
+        "Issue tracker": 'https://github.com/espdev/click-option-group/issues',
+    },
+    license='BSD-3-Clause',
+    author='Eugene Prilepin',
+    author_email='esp.home@gmail.com',
+    description='Option groups missing in Click',
+    long_description=get_long_description(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Environment :: Console',
+        'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
