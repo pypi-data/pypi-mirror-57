5dcf1566269de77ee143781288998f114ae6c469
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:05 +0000

    Add package mozci/0.1.5/mozci-0.1.5.tar.gz

---
+++ packages/mozci/mozci-0.1.5.tar.gz/mozci-0.1.5/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['mozci']
+
+package_data = \
+{'': ['*'], 'mozci': ['queries/*']}
+
+install_requires = \
+['adr>=0.16.5,<0.17.0']
+
+setup_kwargs = {
+    'name': 'mozci',
+    'version': '0.1.5',
+    'description': '',
+    'long_description': None,
+    'author': 'Andrew Halberstadt',
+    'author_email': 'ahal@mozilla.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
