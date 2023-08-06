31ef8c48c80c3615de32ad26c93f406895fb82ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:11 +0000

    Add package ScrapyElasticSearch/0.9.2/ScrapyElasticSearch-0.9.2.tar.gz

---
+++ packages/ScrapyElasticSearch/ScrapyElasticSearch-0.9.2.tar.gz/ScrapyElasticSearch-0.9.2/setup.py
@@ -0,0 +1,35 @@
+import ast
+import re
+from distutils.core import setup
+from setuptools import find_packages
+
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+with open('scrapyelasticsearch/__init__.py', 'rb') as f:
+    version = str(ast.literal_eval(_version_re.search(
+        f.read().decode('utf-8')).group(1)))
+
+setup(name='ScrapyElasticSearch',
+      version=version,
+      license='Apache License, Version 2.0',
+      description='Scrapy pipeline which allow you to store multiple scrapy items in Elastic Search.',
+      long_description=open('README.rst').read(),
+      author='Jay Zeng, Michael Malocha, Julien Duponchelle',
+      author_email='jayzeng@jay-zeng.com, michael@knockrentals.com, julien@duponchelle.info',
+      url='https://github.com/knockrentals/scrapy-elasticsearch.git',
+      download_url='https://github.com/knockrentals/scrapy-elasticsearch/tarball/master',
+      keywords='scrapy elastic search',
+      packages=find_packages(),
+      platforms=['Any'],
+      install_requires=['scrapy', 'elasticsearch'],
+      extras_require={
+          'requests': ['requests>=2.10.0'],
+          'ntlm': ['requests_ntlm>=0.2.0'],
+          'ssl': ['certifi>=2017.7.27.1']
+      },
+      classifiers=['Development Status :: 4 - Beta',
+                   'Environment :: No Input/Output (Daemon)',
+                   'License :: OSI Approved :: Apache Software License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python']
+      )
