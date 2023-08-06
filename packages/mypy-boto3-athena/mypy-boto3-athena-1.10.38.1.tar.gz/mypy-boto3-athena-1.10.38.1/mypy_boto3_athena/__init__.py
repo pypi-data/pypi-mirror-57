213b3598619366099b6deae7b6d18c6795f55744
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:20 +0000

    Add package mypy-boto3-athena/1.10.38.1/mypy-boto3-athena-1.10.38.1.tar.gz

---
+++ packages/mypy-boto3-athena/mypy-boto3-athena-1.10.38.1.tar.gz/mypy-boto3-athena-1.10.38.1/mypy_boto3_athena/__init__.py
@@ -0,0 +1,17 @@
+"Main interface for athena service"
+
+from mypy_boto3_athena.client import AthenaClient as Client, AthenaClient
+from mypy_boto3_athena.paginator import (
+    GetQueryResultsPaginator,
+    ListNamedQueriesPaginator,
+    ListQueryExecutionsPaginator,
+)
+
+
+__all__ = (
+    "AthenaClient",
+    "Client",
+    "GetQueryResultsPaginator",
+    "ListNamedQueriesPaginator",
+    "ListQueryExecutionsPaginator",
+)
