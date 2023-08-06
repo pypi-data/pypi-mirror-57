236f4f787cbe25e7e5499e1c8e7ebca95e273dfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:31 +0000

    Add package mypy-boto3-sso-oidc/1.10.37.1/mypy_boto3_sso_oidc-1.10.37.1-py3-none-any.whl

---
+++ packages/mypy-boto3-sso-oidc/mypy_boto3_sso_oidc-1.10.37.1-py3-none-any.whl/mypy_boto3_sso_oidc/type_defs.py
@@ -0,0 +1,40 @@
+"Main interface for sso-oidc service type defs"
+import sys
+
+if sys.version_info >= (3, 8):
+    from typing import TypedDict
+else:
+    from typing_extensions import TypedDict
+
+
+CreateTokenResponseTypeDef = TypedDict(
+    "CreateTokenResponseTypeDef",
+    {"accessToken": str, "tokenType": str, "expiresIn": int, "refreshToken": str, "idToken": str},
+    total=False,
+)
+
+RegisterClientResponseTypeDef = TypedDict(
+    "RegisterClientResponseTypeDef",
+    {
+        "clientId": str,
+        "clientSecret": str,
+        "clientIdIssuedAt": int,
+        "clientSecretExpiresAt": int,
+        "authorizationEndpoint": str,
+        "tokenEndpoint": str,
+    },
+    total=False,
+)
+
+StartDeviceAuthorizationResponseTypeDef = TypedDict(
+    "StartDeviceAuthorizationResponseTypeDef",
+    {
+        "deviceCode": str,
+        "userCode": str,
+        "verificationUri": str,
+        "verificationUriComplete": str,
+        "expiresIn": int,
+        "interval": int,
+    },
+    total=False,
+)
