fb0b9c18ce62d90541838bea07408704f87f8143
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:07 +0000

    Add package click-option-group/0.2.1/click_option_group-0.2.1-py3-none-any.whl

---
+++ packages/click-option-group/click_option_group-0.2.1-py3-none-any.whl/click_option_group/__init__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+click-option-group
+~~~~~~~~~~~~~~~~~~
+
+Option groups missing in Click
+
+:copyright: © 2019 by Eugene Prilepin
+:license: BSD, see LICENSE for more details.
+"""
+
+from ._version import __version__  # noqa
+
+from ._core import (
+    GroupedOption,
+    OptionGroup,
+    RequiredAnyOptionGroup,
+    RequiredAllOptionGroup,
+    MutuallyExclusiveOptionGroup,
+    RequiredMutuallyExclusiveOptionGroup,
+)
+
+from ._decorators import optgroup
+
+
+__all__ = [
+    'optgroup',
+    'GroupedOption',
+    'OptionGroup',
+    'RequiredAnyOptionGroup',
+    'RequiredAllOptionGroup',
+    'MutuallyExclusiveOptionGroup',
+    'RequiredMutuallyExclusiveOptionGroup',
+]
