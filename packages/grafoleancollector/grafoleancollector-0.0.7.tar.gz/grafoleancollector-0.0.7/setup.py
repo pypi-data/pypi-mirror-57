59e228d3bfedee801005f34fb4ba5a69e671d2fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:14 +0000

    Add package grafoleancollector/0.0.7/grafoleancollector-0.0.7.tar.gz

---
+++ packages/grafoleancollector/grafoleancollector-0.0.7.tar.gz/grafoleancollector-0.0.7/setup.py
@@ -0,0 +1,18 @@
+import os
+from setuptools import setup, find_packages
+
+requirements_txt = os.path.dirname(os.path.realpath(__file__)) + '/requirements.txt'
+with open(requirements_txt, "rt") as f:
+    install_requires = f.read().splitlines()[1:]
+
+setup(name='grafoleancollector',
+      version='v0.0.7',
+      url='https://gitlab.com/grafolean/grafolean-collector',
+      license='Commons Clause + Apache 2.0',
+      author='Anže Škerlavaj',
+      author_email='info@grafolean.com',
+      description='Common utilities for creating (controlled) collectors for Grafolean',
+      packages=find_packages(exclude=['tests']),
+      long_description=open('README.md').read(),
+      install_requires=install_requires,
+      zip_safe=False)
