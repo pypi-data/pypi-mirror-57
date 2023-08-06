8f8e7e02bb269957f5582cbf2939c791098afb4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:38 +0000

    Add package Temu-Greet/0.0.1/Temu-Greet-0.0.1.tar.gz

---
+++ packages/Temu-Greet/Temu-Greet-0.0.1.tar.gz/Temu-Greet-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md","r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name = "Temu-Greet",
+    version = "0.0.1",
+    author = "George Temu",
+    author_email = "georgetemu123@gmail.com",
+    description = "A small greeting package",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/pypa/sampleproject1",
+    packages = setuptools.find_packages(),
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires = ">=3.6",
+)
