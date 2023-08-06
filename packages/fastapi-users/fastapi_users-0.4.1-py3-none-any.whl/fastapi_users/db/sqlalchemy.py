f8b5598a99bf1b1dfb4d6dd46ea28d758410356b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:49 +0000

    Add package fastapi-users/0.4.1/fastapi_users-0.4.1-py3-none-any.whl

---
+++ packages/fastapi-users/fastapi_users-0.4.1-py3-none-any.whl/fastapi_users/db/sqlalchemy.py
@@ -0,0 +1,66 @@
+from typing import List, Optional
+
+from databases import Database
+from sqlalchemy import Boolean, Column, String, Table
+
+from fastapi_users.db.base import BaseUserDatabase
+from fastapi_users.models import BaseUserDB
+
+
+class SQLAlchemyBaseUserTable:
+    """Base SQLAlchemy users table definition."""
+
+    __tablename__ = "user"
+
+    id = Column(String, primary_key=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    is_active = Column(Boolean, default=True, nullable=False)
+    is_superuser = Column(Boolean, default=False, nullable=False)
+
+
+class SQLAlchemyUserDatabase(BaseUserDatabase):
+    """
+    Database adapter for SQLAlchemy.
+
+    :param database: `Database` instance from `encode/databases`.
+    :param users: SQLAlchemy users table instance.
+    """
+
+    database: Database
+    users: Table
+
+    def __init__(self, database: Database, users: Table):
+        self.database = database
+        self.users = users
+
+    async def list(self) -> List[BaseUserDB]:
+        query = self.users.select()
+        users = await self.database.fetch_all(query)
+        return [BaseUserDB(**user) for user in users]
+
+    async def get(self, id: str) -> Optional[BaseUserDB]:
+        query = self.users.select().where(self.users.c.id == id)
+        user = await self.database.fetch_one(query)
+        return BaseUserDB(**user) if user else None
+
+    async def get_by_email(self, email: str) -> Optional[BaseUserDB]:
+        query = self.users.select().where(self.users.c.email == email)
+        user = await self.database.fetch_one(query)
+        return BaseUserDB(**user) if user else None
+
+    async def create(self, user: BaseUserDB) -> BaseUserDB:
+        query = self.users.insert().values(**user.dict())
+        await self.database.execute(query)
+        return user
+
+    async def update(self, user: BaseUserDB) -> BaseUserDB:
+        query = (
+            self.users.update().where(self.users.c.id == user.id).values(**user.dict())
+        )
+        await self.database.execute(query)
+        return user
+
+    async def delete(self, user: BaseUserDB) -> None:
+        query = self.users.delete().where(self.users.c.id == user.id)
+        await self.database.execute(query)
---
+++ packages/fastapi-users/fastapi_users-0.4.1-py3-none-any.whl/fastapi_users/models.py
@@ -0,0 +1,61 @@
+import uuid
+from typing import Optional, Type
+
+import pydantic
+from pydantic import BaseModel, EmailStr
+
+
+class BaseUser(BaseModel):
+    """Base User model."""
+
+    id: Optional[str] = None
+    email: Optional[EmailStr] = None
+    is_active: Optional[bool] = True
+    is_superuser: Optional[bool] = False
+
+    @pydantic.validator("id", pre=True, always=True)
+    def default_id(cls, v):
+        return v or str(uuid.uuid4())
+
+    def create_update_dict(self):
+        return self.dict(
+            exclude_unset=True, exclude={"id", "is_superuser", "is_active"}
+        )
+
+    def create_update_dict_superuser(self):
+        return self.dict(exclude_unset=True, exclude={"id"})
+
+    class Config:
+        orm_mode = True
+
+
+class BaseUserCreate(BaseUser):
+    email: EmailStr
+    password: str
+
+
+class BaseUserUpdate(BaseUser):
+    password: Optional[str]
+
+
+class BaseUserDB(BaseUser):
+    hashed_password: str
+
+
+class Models:
+    """Generate models inheriting from the custom User model."""
+
+    def __init__(self, user_model: Type[BaseUser]):
+        class UserCreate(user_model, BaseUserCreate):  # type: ignore
+            pass
+
+        class UserUpdate(user_model, BaseUserUpdate):  # type: ignore
+            pass
+
+        class UserDB(user_model, BaseUserDB):  # type: ignore
+            pass
+
+        self.User = user_model
+        self.UserCreate = UserCreate
+        self.UserUpdate = UserUpdate
+        self.UserDB = UserDB
