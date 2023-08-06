41ffea1261e489cfcc29ba07c7dccbe0984b7bb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:15 +0000

    Add package micrometa/0.0.8/micrometa-0.0.8.tar.gz

---
+++ packages/micrometa/micrometa-0.0.8.tar.gz/micrometa-0.0.8/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+requirements = ["configparser", "natsort", "pyyaml", "luddite"]
+
+setup(
+    name="micrometa",
+    version="0.0.8",
+    description="Reading of microscopy metadata",
+    install_requires=requirements,
+    extras_require={"dev": ["black", "pytest-cov", "pytest", "coveralls"]},
+    python_requires=">=3.6",
+    packages=find_packages(),
+    include_package_data=True,
+    url="https://github.com/adamltyson/micrometa",
+    author="Adam Tyson",
+    author_email="adam.tyson@ucl.ac.uk",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+    ],
+    zip_safe=False,
+)
