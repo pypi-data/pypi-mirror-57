50e857136f94401c14421f07a50536a84f5678cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:09 +0000

    Add package stylist/0.1/stylist-0.1-py3-none-any.whl

---
+++ packages/stylist/stylist-0.1-py3-none-any.whl/stylist/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+##############################################################################
+# (c) Crown copyright 2019 Met Office. All rights reserved.
+# The file LICENCE, distributed with this code, contains details of the terms
+# under which the code may be used.
+##############################################################################
+'''
+Extensible code style checker currently supporting Fortran, PSyclone DSL, etc
+'''
+
+__version__ = '0.1'
