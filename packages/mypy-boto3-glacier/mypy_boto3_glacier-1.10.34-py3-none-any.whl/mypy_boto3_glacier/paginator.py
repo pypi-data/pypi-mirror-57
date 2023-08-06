7a20c5b15f75c14fdd34687b774e5b531bdfdb83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:54 +0000

    Add package mypy-boto3-glacier/1.10.34/mypy_boto3_glacier-1.10.34-py3-none-any.whl

---
+++ packages/mypy-boto3-glacier/mypy_boto3_glacier-1.10.34-py3-none-any.whl/mypy_boto3_glacier/paginator.py
@@ -0,0 +1,92 @@
+"Main interface for glacier service Paginators"
+from __future__ import annotations
+
+from botocore.paginate import Paginator as Boto3Paginator
+from mypy_boto3_glacier.type_defs import (
+    ListJobsPaginatePaginationConfigTypeDef,
+    ListJobsPaginateResponseTypeDef,
+    ListMultipartUploadsPaginatePaginationConfigTypeDef,
+    ListMultipartUploadsPaginateResponseTypeDef,
+    ListPartsPaginatePaginationConfigTypeDef,
+    ListPartsPaginateResponseTypeDef,
+    ListVaultsPaginatePaginationConfigTypeDef,
+    ListVaultsPaginateResponseTypeDef,
+)
+
+
+__all__ = (
+    "ListJobsPaginator",
+    "ListMultipartUploadsPaginator",
+    "ListPartsPaginator",
+    "ListVaultsPaginator",
+)
+
+
+class ListJobsPaginator(Boto3Paginator):
+    """
+    Paginator for `list_jobs`
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self,
+        vaultName: str,
+        accountId: str = None,
+        statuscode: str = None,
+        completed: str = None,
+        PaginationConfig: ListJobsPaginatePaginationConfigTypeDef = None,
+    ) -> ListJobsPaginateResponseTypeDef:
+        """
+        [ListJobs.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.34/reference/services/glacier.html#Glacier.Paginator.ListJobs.paginate)
+        """
+
+
+class ListMultipartUploadsPaginator(Boto3Paginator):
+    """
+    Paginator for `list_multipart_uploads`
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self,
+        vaultName: str,
+        accountId: str = None,
+        PaginationConfig: ListMultipartUploadsPaginatePaginationConfigTypeDef = None,
+    ) -> ListMultipartUploadsPaginateResponseTypeDef:
+        """
+        [ListMultipartUploads.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.34/reference/services/glacier.html#Glacier.Paginator.ListMultipartUploads.paginate)
+        """
+
+
+class ListPartsPaginator(Boto3Paginator):
+    """
+    Paginator for `list_parts`
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self,
+        vaultName: str,
+        uploadId: str,
+        accountId: str = None,
+        PaginationConfig: ListPartsPaginatePaginationConfigTypeDef = None,
+    ) -> ListPartsPaginateResponseTypeDef:
+        """
+        [ListParts.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.34/reference/services/glacier.html#Glacier.Paginator.ListParts.paginate)
+        """
+
+
+class ListVaultsPaginator(Boto3Paginator):
+    """
+    Paginator for `list_vaults`
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self,
+        accountId: str = None,
+        PaginationConfig: ListVaultsPaginatePaginationConfigTypeDef = None,
+    ) -> ListVaultsPaginateResponseTypeDef:
+        """
+        [ListVaults.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.34/reference/services/glacier.html#Glacier.Paginator.ListVaults.paginate)
+        """
