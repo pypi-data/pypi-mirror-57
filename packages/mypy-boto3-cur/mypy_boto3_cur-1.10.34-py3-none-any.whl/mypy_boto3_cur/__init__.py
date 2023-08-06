f37d8f7e99b08002bbaec1289d977e69d86c1db4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:45 +0000

    Add package mypy-boto3-cur/1.10.34/mypy_boto3_cur-1.10.34-py3-none-any.whl

---
+++ packages/mypy-boto3-cur/mypy_boto3_cur-1.10.34-py3-none-any.whl/mypy_boto3_cur/__init__.py
@@ -0,0 +1,7 @@
+"Main interface for cur service"
+
+from mypy_boto3_cur.client import Client
+from mypy_boto3_cur.paginator import DescribeReportDefinitionsPaginator
+
+
+__all__ = ("Client", "DescribeReportDefinitionsPaginator")
