6f45013b04293a8027372cbb295cbbe8f0219270
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:29 +0000

    Add package attabox/0.1.1/attabox-0.1.1.tar.gz

---
+++ packages/attabox/attabox-0.1.1.tar.gz/attabox-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='attabox',
+      version='0.1.1',
+      description='Attali Utility Library',
+      url='http://github.com/',
+      author='Poidudes',
+      author_email='yonatan.attali@gmail.com',
+      license='MIT',
+      packages=['attabox'],
+      zip_safe=False)
