05d7df2c376328ce5f8cafe3e83783895e43b210
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:11 +0000

    Add package serial-visualisation/1.0.8/serial-visualisation-1.0.8.tar.gz

---
+++ packages/serial-visualisation/serial-visualisation-1.0.8.tar.gz/serial-visualisation-1.0.8/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open('README.md') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='serial-visualisation',
+    version='1.0.8',
+    author='ES-Alexander',
+    author_email='sandman.esalexander@gmail.com',
+    description='A package for visualisation of serial data in a grid.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/ES-Alexander/serial-visualisation',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'numpy',
+        'opencv-python',
+        'pyserial',
+    ],
+)
