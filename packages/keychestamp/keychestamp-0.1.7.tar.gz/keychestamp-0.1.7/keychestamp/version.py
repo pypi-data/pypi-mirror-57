723566c3e68e84632f2ff9aa4297652c0d4db0fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:47 +0000

    Add package keychestamp/0.1.7/keychestamp-0.1.7.tar.gz

---
+++ packages/keychestamp/keychestamp-0.1.7.tar.gz/keychestamp-0.1.7/keychestamp/version.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+***
+Module: keychestamp
+***
+
+ Copyright (C) KeyChest Ltd, registered in the United Kingdom.
+ This file is owned exclusively by KeyChest Ltd.
+ Unauthorized copying of this file, via any medium is strictly prohibited
+ Proprietary and confidential
+ Written by Dan Cvrcek <support@keychest.net>, 2019
+"""
+__copyright__ = 'KeyChest Ltd'
+__email__ = 'support@keychest.net'
+__status__ = 'Development'
+
+proxy_version = '0.1.7'
