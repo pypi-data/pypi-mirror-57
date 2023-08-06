5233ca7fa9e691f950476b188acc6279ecd76910
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:31 +0000

    Add package django-mssql-backend/2.5.0/django-mssql-backend-2.5.0.tar.gz

---
+++ packages/django-mssql-backend/django-mssql-backend-2.5.0.tar.gz/django-mssql-backend-2.5.0/setup.py
@@ -0,0 +1,36 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+CLASSIFIERS = [
+    'License :: OSI Approved :: BSD License',
+    'Framework :: Django',
+    "Operating System :: POSIX :: Linux",
+    "Operating System :: Microsoft :: Windows",
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+]
+
+setup(
+    name='django-mssql-backend',
+    version='2.5.0',
+    description='Django backend for Microsoft SQL Server',
+    long_description=open('README.rst').read(),
+    author='ES Solutions AB',
+    author_email='info@essolutions.se',
+    url='https://github.com/ESSolutions/django-mssql-backend',
+    license='BSD',
+    packages=['sql_server', 'sql_server.pyodbc'],
+    install_requires=[
+        'pyodbc>=3.0',
+    ],
+    extras_require={
+        'tests': ['dj-database-url==0.5.0'],
+    },
+    classifiers=CLASSIFIERS,
+    keywords='django',
+)
