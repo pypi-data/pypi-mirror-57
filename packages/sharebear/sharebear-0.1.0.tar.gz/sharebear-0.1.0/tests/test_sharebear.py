c05c6ff9122348c9632be0238d2d3aec17bfa8bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:53 +0000

    Add package sharebear/0.1.0/sharebear-0.1.0.tar.gz

---
+++ packages/sharebear/sharebear-0.1.0.tar.gz/sharebear-0.1.0/tests/test_sharebear.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `sharebear` package."""
+
+
+import unittest
+
+from sharebear import sharebear
+
+
+class TestSharebear(unittest.TestCase):
+    """Tests for `sharebear` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
