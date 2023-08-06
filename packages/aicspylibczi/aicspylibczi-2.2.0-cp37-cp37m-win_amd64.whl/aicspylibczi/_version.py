10f6f49790ff5bf1ad2b594787a9b03fea0563d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:39 +0000

    Add package aicspylibczi/2.2.0/aicspylibczi-2.2.0-cp37-cp37m-win_amd64.whl

---
+++ packages/aicspylibczi/aicspylibczi-2.2.0-cp37-cp37m-win_amd64.whl/aicspylibczi/_version.py
@@ -0,0 +1,9 @@
+# This file is part of aicspylibczi.
+#
+# You should have received a copy of the GNU General Public License
+# along with aicspylibczi.  If not, see <https://www.gnu.org/licenses/>.
+
+"""Version information."""
+
+# The following line *must* be the last in the module, exactly as formatted:
+__version__ = "2.2.0"
