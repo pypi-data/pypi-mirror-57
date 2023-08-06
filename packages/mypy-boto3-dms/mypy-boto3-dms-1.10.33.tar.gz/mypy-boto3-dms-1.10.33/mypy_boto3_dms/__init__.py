5234a62f88608c9d466999e79b81670f4e45b6ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:42 +0000

    Add package mypy-boto3-dms/1.10.33/mypy-boto3-dms-1.10.33.tar.gz

---
+++ packages/mypy-boto3-dms/mypy-boto3-dms-1.10.33.tar.gz/mypy-boto3-dms-1.10.33/mypy_boto3_dms/__init__.py
@@ -0,0 +1,54 @@
+"Main interface for dms service"
+
+from mypy_boto3_dms.client import Client
+from mypy_boto3_dms.paginator import (
+    DescribeCertificatesPaginator,
+    DescribeConnectionsPaginator,
+    DescribeEndpointTypesPaginator,
+    DescribeEndpointsPaginator,
+    DescribeEventSubscriptionsPaginator,
+    DescribeEventsPaginator,
+    DescribeOrderableReplicationInstancesPaginator,
+    DescribeReplicationInstancesPaginator,
+    DescribeReplicationSubnetGroupsPaginator,
+    DescribeReplicationTaskAssessmentResultsPaginator,
+    DescribeReplicationTasksPaginator,
+    DescribeSchemasPaginator,
+    DescribeTableStatisticsPaginator,
+)
+from mypy_boto3_dms.waiter import (
+    EndpointDeletedWaiter,
+    ReplicationInstanceAvailableWaiter,
+    ReplicationInstanceDeletedWaiter,
+    ReplicationTaskDeletedWaiter,
+    ReplicationTaskReadyWaiter,
+    ReplicationTaskRunningWaiter,
+    ReplicationTaskStoppedWaiter,
+    TestConnectionSucceedsWaiter,
+)
+
+
+__all__ = (
+    "Client",
+    "EndpointDeletedWaiter",
+    "ReplicationInstanceAvailableWaiter",
+    "ReplicationInstanceDeletedWaiter",
+    "ReplicationTaskDeletedWaiter",
+    "ReplicationTaskReadyWaiter",
+    "ReplicationTaskRunningWaiter",
+    "ReplicationTaskStoppedWaiter",
+    "TestConnectionSucceedsWaiter",
+    "DescribeCertificatesPaginator",
+    "DescribeConnectionsPaginator",
+    "DescribeEndpointTypesPaginator",
+    "DescribeEndpointsPaginator",
+    "DescribeEventSubscriptionsPaginator",
+    "DescribeEventsPaginator",
+    "DescribeOrderableReplicationInstancesPaginator",
+    "DescribeReplicationInstancesPaginator",
+    "DescribeReplicationSubnetGroupsPaginator",
+    "DescribeReplicationTaskAssessmentResultsPaginator",
+    "DescribeReplicationTasksPaginator",
+    "DescribeSchemasPaginator",
+    "DescribeTableStatisticsPaginator",
+)
