1f1df16ca07d9d3f55571f24cf67e8aa238da9d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:49 +0000

    Add package venvgit2/0.28.2/venvgit2-0.28.2.tar.gz

---
+++ packages/venvgit2/venvgit2-0.28.2.tar.gz/venvgit2-0.28.2/pygit2/src/types.h
@@ -0,0 +1,222 @@
+/*
+ * Copyright 2010-2019 The pygit2 contributors
+ *
+ * This file is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2,
+ * as published by the Free Software Foundation.
+ *
+ * In addition to the permissions in the GNU General Public License,
+ * the authors give you unlimited permission to link the compiled
+ * version of this file into combinations with other programs,
+ * and to distribute those combinations without any restriction
+ * coming from the use of this file.  (The General Public License
+ * restrictions do apply in other respects; for example, they cover
+ * modification of the file, and distribution when not linked into
+ * a combined executable.)
+ *
+ * This file is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifndef INCLUDE_pygit2_objects_h
+#define INCLUDE_pygit2_objects_h
+
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+#include <git2.h>
+
+#if !(LIBGIT2_VER_MAJOR == 0 && LIBGIT2_VER_MINOR == 28)
+#error You need a compatible libgit2 version (v0.28.x)
+#endif
+
+/*
+ * Python objects
+ *
+ **/
+
+/* git_repository */
+typedef struct {
+    PyObject_HEAD
+    git_repository *repo;
+    PyObject *index;  /* It will be None for a bare repository */
+    PyObject *config; /* It will be None for a bare repository */
+    int owned;    /* _from_c() sometimes means we don't own the C pointer */
+} Repository;
+
+
+typedef struct {
+    PyObject_HEAD
+    git_oid oid;
+} Oid;
+
+
+#define SIMPLE_TYPE(_name, _ptr_type, _ptr_name) \
+        typedef struct {\
+            PyObject_HEAD\
+            Repository *repo;\
+            _ptr_type *_ptr_name;\
+        } _name;
+
+
+/* git object types
+ *
+ * The structs for some of the object subtypes are identical except for
+ * the type of their object pointers. */
+SIMPLE_TYPE(Object, git_object, obj)
+SIMPLE_TYPE(Commit, git_commit, commit)
+SIMPLE_TYPE(Tree, git_tree, tree)
+SIMPLE_TYPE(Blob, git_blob, blob)
+SIMPLE_TYPE(Tag, git_tag, tag)
+
+/* git_worktree */
+typedef struct {
+    PyObject_HEAD
+    Repository *repo;
+    git_worktree *worktree;
+} Worktree;
+
+/* git_note */
+typedef struct {
+    PyObject_HEAD
+    Repository *repo;
+    git_note *note;
+    PyObject* annotated_id;
+} Note;
+
+typedef struct {
+    PyObject_HEAD
+    Repository *repo;
+    git_note_iterator* iter;
+    char* ref;
+} NoteIter;
+
+/* git_patch */
+typedef struct {
+    PyObject_HEAD
+    git_patch *patch;
+    Blob* oldblob;
+    Blob* newblob;
+} Patch;
+
+/* git_diff */
+SIMPLE_TYPE(Diff, git_diff, diff)
+
+typedef struct {
+    PyObject_HEAD
+    Diff *diff;
+    size_t i;
+    size_t n;
+} DeltasIter;
+
+typedef struct {
+    PyObject_HEAD
+    Diff *diff;
+    size_t i;
+    size_t n;
+} DiffIter;
+
+typedef struct {
+    PyObject_HEAD
+    PyObject *id;
+    char *path;
+    PyObject *raw_path;
+    git_off_t size;
+    uint32_t flags;
+    uint16_t mode;
+} DiffFile;
+
+typedef struct {
+    PyObject_HEAD
+    git_delta_t status;
+    uint32_t flags;
+    uint16_t similarity;
+    uint16_t nfiles;
+    PyObject *old_file;
+    PyObject *new_file;
+} DiffDelta;
+
+typedef struct {
+    PyObject_HEAD
+    Patch *patch;
+    const git_diff_hunk *hunk;
+    size_t idx;
+    size_t n_lines;
+} DiffHunk;
+
+typedef struct {
+    PyObject_HEAD
+    DiffHunk *hunk;
+    const git_diff_line *line;
+} DiffLine;
+
+SIMPLE_TYPE(DiffStats, git_diff_stats, stats);
+
+/* git_tree_walk , git_treebuilder*/
+SIMPLE_TYPE(TreeBuilder, git_treebuilder, bld)
+
+typedef struct {
+    PyObject_HEAD
+    const git_tree_entry *entry;
+} TreeEntry;
+
+typedef struct {
+    PyObject_HEAD
+    Tree *owner;
+    int i;
+} TreeIter;
+
+
+/* git_index */
+SIMPLE_TYPE(Index, git_index, index)
+
+typedef struct {
+    PyObject_HEAD
+    git_index_entry entry;
+} IndexEntry;
+
+
+/* git_reference, git_reflog */
+SIMPLE_TYPE(Walker, git_revwalk, walk)
+
+SIMPLE_TYPE(Reference, git_reference, reference)
+
+typedef Reference Branch;
+
+typedef struct {
+    PyObject_HEAD
+    git_signature *signature;
+    PyObject *oid_old;
+    PyObject *oid_new;
+    char *message;
+} RefLogEntry;
+
+typedef struct {
+    PyObject_HEAD
+    git_reflog *reflog;
+    size_t i;
+    size_t size;
+} RefLogIter;
+
+
+/* git_signature */
+typedef struct {
+    PyObject_HEAD
+    Object *obj;
+    const git_signature *signature;
+    char *encoding;
+} Signature;
+
+/* git_mailmap */
+typedef struct {
+    PyObject_HEAD
+    git_mailmap *mailmap;
+} Mailmap;
+
+#endif
---
+++ packages/venvgit2/venvgit2-0.28.2.tar.gz/venvgit2-0.28.2/setup.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+"""
+    setup
+    ~~~~~
+"""
+
+import os
+import platform
+from shutil import rmtree
+from tempfile import mkdtemp
+from setuptools import setup
+from setuptools.command.install import install
+from subprocess import check_call, CalledProcessError
+
+
+class Dirs(object):
+    """Get a scratch space for compiling and stuff"""
+
+    def __init__(self):
+        self.prefix = os.environ.get('VIRTUAL_ENV')
+        assert self.prefix is not None, 'Aborting: please activate a virtualenv'
+        self.src = os.getcwd()
+        self.libgit2_src = os.path.join(self.src, 'libgit2')  # source
+        self.pygit2_src = os.path.join(self.src, 'pygit2')  # source
+
+    def __enter__(self):
+        self.work_dir = mkdtemp()
+        return self
+
+    def __exit__(self, *args):
+        os.chdir(self.src)
+        rmtree(self.work_dir)
+
+
+def cmake_is_installed():
+    print('Looking for cmake...')
+    try:
+        check_call(['which', 'cmake'])
+        return True
+    except CalledProcessError:
+        return False
+
+
+def pythondev_is_installed():
+    """ inspried by http://stackoverflow.com/questions/4848566/check-for-existence-of-python-dev-files-from-bash-script"""
+    print('Looking for python dev files...')
+    try:
+        from distutils import sysconfig
+    except ImportError:
+        return False
+    includepy = sysconfig.get_config_vars()['INCLUDEPY']
+    pythonh = os.path.join(includepy, 'Python.h')
+    dev_present = os.path.isfile(pythonh)
+    return dev_present
+
+
+def install_libgit2(dirs):
+    """See https://github.com/libgit2/libgit2/blob/development/README.md"""
+    os.chdir(dirs.work_dir)
+    print('libgit2 cmake...')
+    libdir = os.path.join(dirs.prefix, 'lib')
+    check_call(['cmake',
+                dirs.libgit2_src,
+                '-DCMAKE_INSTALL_PREFIX={0}'.format(dirs.prefix),
+                '-DCMAKE_INSTALL_RPATH={0}'.format(libdir),
+                '-DCMAKE_INSTALL_NAME_DIR={0}'.format(libdir),
+                '-DCMAKE_INSTALL_RPATH_USE_LINK_PATH:BOOL=TRUE',
+                '-DBUILD_CLAR=OFF'])
+    print('libgit2 making...')
+    check_call(['cmake', '--build', dirs.work_dir, '--target', 'install'])
+
+
+def install_pygit2(dirs):
+    """See http://www.pygit2.org/install.html"""
+    os.environ['LIBGIT2'] = dirs.prefix
+    platform_name = platform.system()
+    if 'Windows' not in platform_name and 'Darwin' not in platform_name:
+        # The following has two linux-specific options: -rpath= is a GNUism and
+        # --enable-new-dtags is only for ELF, so let's
+        os.environ['LDFLAGS'] = "-Wl,-rpath='{0}/lib',--enable-new-dtags {1}".format(dirs.prefix, os.environ.get('LDFLAGS', ''))
+    script = os.path.join(dirs.pygit2_src, 'setup.py')
+    os.chdir(dirs.pygit2_src)
+    print('pygit2 clean...')
+    check_call(['python', script, 'clean'])
+    print('pygit2 build...')
+    check_call(['python', script, 'build'])
+    print('pygit2 install...')
+    check_call(['python', script, 'install'])
+
+
+class InstallEverything(install):
+    """Install everything..."""
+    def run(self):
+        assert cmake_is_installed(), "Aborting: please install cmake"
+        assert pythondev_is_installed(), "Aborting: please install python development headers"
+        install.do_egg_install(self)  # install_requires... http://stackoverflow.com/a/22179371
+        with Dirs() as dirs:
+            install_libgit2(dirs)
+            install_pygit2(dirs)
+
+
+setup(
+    name="venvgit2",
+    description="Install libgit2 and pygit2 in a virtualenv",
+    long_description=open('README.rst').read(),
+    version="0.28.2",
+    install_requires=[
+        'cffi>=0.8.6',
+    ],
+    cmdclass={
+        'install': InstallEverything,
+    },
+    author='uniphil',
+    author_email='uniphil@gmail.com',
+    license='Public Domain',
+    url='https://github.com/uniphil/venvgit2',
+)
