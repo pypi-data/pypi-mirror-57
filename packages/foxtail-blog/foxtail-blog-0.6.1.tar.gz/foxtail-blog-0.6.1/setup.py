46964c5ad81ddde8b88fe829d3ab3585a7fcf50b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:59 +0000

    Add package foxtail-blog/0.6.1/foxtail-blog-0.6.1.tar.gz

---
+++ packages/foxtail-blog/foxtail-blog-0.6.1.tar.gz/foxtail-blog-0.6.1/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="foxtail-blog",
+    version="0.6.1",
+    author="Luke Rogers",
+    author_email="lukeroge@gmail.com",
+    description="A blog.",
+    install_requires=['django>=2.2', 'bleach', 'bleach_whitelist', 'markdown', 'django-taggit',
+                      'django-versatileimagefield', 'django-crispy-forms', 'django-recaptcha'],
+    extras_require={
+        ':python_version == "3.6"': [
+            'dataclasses',
+        ],
+    },
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dmptrluke/foxtail-blog",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
