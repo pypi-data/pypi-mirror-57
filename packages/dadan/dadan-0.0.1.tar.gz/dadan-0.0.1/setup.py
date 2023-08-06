272af03adb51f95cba16895fd20b12c00abcc1e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:20 +0000

    Add package dadan/0.0.1/dadan-0.0.1.tar.gz

---
+++ packages/dadan/dadan-0.0.1.tar.gz/dadan-0.0.1/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+from setuptools import setup
+
+setup(name='dadan',
+      version='0.0.1',
+      description='dadan Natural Language Processing',
+      author='dadan',
+      author_email='dadan@qq.com',
+      license='MIT',
+      packages=['dadan'],
+      package_dir={'dadan': 'dadan'},
+      package_data={'dadan': ['*.*', 'cluster/*', 'data/*', 'model/*',
+		'normal/*', 'segment/*', 'segment/dict/*','segment/model/*',
+		'sentiment/*', 'sentiment/model/*', 'topic/*']}
+      )
