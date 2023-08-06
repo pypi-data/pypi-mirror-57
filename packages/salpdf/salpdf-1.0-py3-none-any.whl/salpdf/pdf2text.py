090bc25db79be80a704a0520d4d96e94c5d911de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:18 +0000

    Add package salpdf/1.0/salpdf-1.0-py3-none-any.whl

---
+++ packages/salpdf/salpdf-1.0-py3-none-any.whl/salpdf/pdf2text.py
@@ -0,0 +1,16 @@
+
+
+class Converter:
+    """A simple converter for converting PDFs to text."""
+
+    def convert(self, path):
+        """
+        Convert the given PDF to text.
+
+        Parameters:
+        path (str): The Path to a PDF file.
+
+        Returns:
+        str: The content of the PDF file.
+        """
+        print("pdf2text")
