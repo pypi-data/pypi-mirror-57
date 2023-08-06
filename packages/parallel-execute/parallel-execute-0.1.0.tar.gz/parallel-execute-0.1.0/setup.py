2391ea62a4a80e8f02f1bff10ec2fa3447224b52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:52 +0000

    Add package parallel-execute/0.1.0/parallel-execute-0.1.0.tar.gz

---
+++ packages/parallel-execute/parallel-execute-0.1.0.tar.gz/parallel-execute-0.1.0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+setuptools.setup(
+    name="parallel-execute",
+    version="0.1.0",
+    author="Sahil Pardeshi",
+    author_email="sahilrp7@gmail.com",
+    description="Python wrappers for easy multiprocessing and threading",
+    long_description=open('README.rst').read(),
+    url="https://github.com/parallel-execute/parallel-execute",
+    packages=setuptools.find_packages(),
+    license='MIT License',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        "Operating System :: OS Independent",
+    ]
+)
