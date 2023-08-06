3800a8b225e602c96175aacfb0f07392ff0b7ffc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:59 +0000

    Add package mypy-boto3-kinesisanalyticsv2/1.10.34/mypy-boto3-kinesisanalyticsv2-1.10.34.tar.gz

---
+++ packages/mypy-boto3-kinesisanalyticsv2/mypy-boto3-kinesisanalyticsv2-1.10.34.tar.gz/mypy-boto3-kinesisanalyticsv2-1.10.34/mypy_boto3_kinesisanalyticsv2/paginator.py
@@ -0,0 +1,43 @@
+"Main interface for kinesisanalyticsv2 service Paginators"
+from __future__ import annotations
+
+from botocore.paginate import Paginator as Boto3Paginator
+from mypy_boto3_kinesisanalyticsv2.type_defs import (
+    ListApplicationSnapshotsPaginatePaginationConfigTypeDef,
+    ListApplicationSnapshotsPaginateResponseTypeDef,
+    ListApplicationsPaginatePaginationConfigTypeDef,
+    ListApplicationsPaginateResponseTypeDef,
+)
+
+
+__all__ = ("ListApplicationSnapshotsPaginator", "ListApplicationsPaginator")
+
+
+class ListApplicationSnapshotsPaginator(Boto3Paginator):
+    """
+    Paginator for `list_application_snapshots`
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self,
+        ApplicationName: str,
+        PaginationConfig: ListApplicationSnapshotsPaginatePaginationConfigTypeDef = None,
+    ) -> ListApplicationSnapshotsPaginateResponseTypeDef:
+        """
+        [ListApplicationSnapshots.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.34/reference/services/kinesisanalyticsv2.html#KinesisAnalyticsV2.Paginator.ListApplicationSnapshots.paginate)
+        """
+
+
+class ListApplicationsPaginator(Boto3Paginator):
+    """
+    Paginator for `list_applications`
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self, PaginationConfig: ListApplicationsPaginatePaginationConfigTypeDef = None
+    ) -> ListApplicationsPaginateResponseTypeDef:
+        """
+        [ListApplications.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.34/reference/services/kinesisanalyticsv2.html#KinesisAnalyticsV2.Paginator.ListApplications.paginate)
+        """
