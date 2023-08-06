9eecdea55f5dadcdeb28fea0df829238516bf532
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:37 +0000

    Add package ulars/1.0.4b0/ulars-1.0.4b0.tar.gz

---
+++ packages/ulars/ulars-1.0.4b0.tar.gz/ulars-1.0.4b0/CHANGELOG.md
@@ -0,0 +1,5 @@
+# Lars Change History
+
+## 0.0.1
+
+Initial release.
---
+++ packages/ulars/ulars-1.0.4b0.tar.gz/ulars-1.0.4b0/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+from lars.core.version import get_version
+
+VERSION = get_version()
+
+f = open('README.md', 'r')
+LONG_DESCRIPTION = f.read()
+f.close()
+
+setup(
+    name='ulars',
+    version=VERSION,
+    description='From txt to sqlite3 parser of logs',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    author='Camille Galladjov',
+    author_email='ujinjinjin@outlook.com',
+    url='https://github.com/Ujinjinjin/lars',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup', 'tests*']),
+    package_data={'lars': ['templates/*']},
+    include_package_data=True,
+    entry_points="""
+        [console_scripts]
+        lars = lars.main:main
+    """,
+    install_requires=[
+        'cement==3.0.4',
+        'jinja2',
+        'pyyaml',
+        'colorlog',
+        'tinydb',
+        'progress',
+        'PyYAML',
+    ]
+)
