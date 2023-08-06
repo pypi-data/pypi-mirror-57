4383ecf2db9ee874570179d13b67966fd3f0438c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:39 +0000

    Add package comment-creator/0.0.5/comment_creator-0.0.5.tar.gz

---
+++ packages/comment-creator/comment_creator-0.0.5.tar.gz/comment_creator-0.0.5/README.md
@@ -0,0 +1,38 @@
+# Comment generator
+
+When passed a word, this piece of code transforms the text into a B I G hashtag style word.
+
+Great for printing little pieces of code when running long pieces of code.
+
+Example:
+
+```python
+# pip install comment-creator
+
+from comment_creator import comment_creator
+
+comment_creator.generate_comment("dates started")
+
+
+```
+
+Function can output letters and white space at the moment. Will add numbers.
+ 
+ Output:
+ 
+ ```python                                                                                                                        
+                            
+####   #####  #####  #####   ###          ###   #####  #####  #####  #####  #####  #### 
+##  #  #   #    #    ##     #            #        #    #   #  ##  #    #    ##     ##  #
+##  #  #####    #    ####    ###          ###     #    #####  #####    #    ####   ##  #
+##  #  #   #    #    ##         #            #    #    #   #  ## #     #    ##     ##  #
+####   #   #    #    #####   ###          ###     #    #   #  ##  #    #    #####  #### 
+                                                                                              
+```
+ 
+## Improvement since 0.0.5
+1) Added numbers to the character dictionary
+
+2) You can pass an optional second string argument: "warn"(yellow), "success"(green) or "fail"(red) and the output will be in the respective color. Default color is the default print color statement of your IDE.
+
+
---
+++ packages/comment-creator/comment_creator-0.0.5.tar.gz/comment_creator-0.0.5/comment_creator/comment_creator.py
@@ -0,0 +1,294 @@
+# Copyright (c) 2018 The Python Packaging Authority
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+def generate_comment(text, type_of=None):
+    char_dict = {
+        "a": """
+#####
+#   #
+#####
+#   #
+#   #
+              """,
+        "b": """
+#### 
+##  #
+#### 
+##  #
+#### 
+              """,
+        "c": """
+ ####
+##   
+##   
+##   
+ ####
+              """,
+        "d": """
+#### 
+##  #
+##  #
+##  #
+#### 
+              """,
+        "e": """
+#####
+##   
+#### 
+##   
+#####
+              """,
+        "f": """
+#####
+##   
+#### 
+##   
+##   
+              """,
+        "g": """
+ ####
+#    
+#### 
+#   #
+ ### 
+              """,
+        "h": """
+##  ##
+##  ##
+######
+##  ##
+##  ##
+              """,
+        "i": """
+#####
+  #  
+  #  
+  #  
+#####
+              """,
+        "j": """
+#####
+  #  
+  #  
+  #  
+##   
+              """,
+        "k": """
+##  #
+## # 
+##   
+## # 
+##  #
+              """,
+        "l": """
+##   
+##   
+##   
+##   
+#####
+              """,
+        "m": """
+#   #
+## ##
+# # #
+#   #
+#   #
+              """,
+        "n": """
+#   #
+##  #
+# # #
+#  ##
+#   #
+              """,
+        "o": """
+ ### 
+#   #
+#   #
+#   #
+ ### 
+              """,
+        "p": """
+#####
+##  #
+#####
+##   
+##   
+              """,
+        "q": """
+ ### 
+#   #
+#   #
+#  ##
+ ####
+              """,
+        "r": """
+#####
+##  #
+#####
+## # 
+##  #
+              """,
+        "s": """
+ ### 
+#    
+ ### 
+    #
+ ### 
+              """,
+        "t": """
+#####
+  #  
+  #  
+  #  
+  #  
+              """,
+        "u": """
+## ##
+## ##
+## ##
+## ##
+ ### 
+              """,
+        "v": """
+## ##
+## ##
+## ##
+## ##
+ ### 
+              """,
+        "w": """
+##         ##
+##   ###   ##
+##  ## ##  ##
+##  ## ##  ##
+ ####   ####
+              """,
+        "x": """
+# # 
+# # 
+ #  
+# # 
+# # 
+              """,
+        "y": """
+#   #
+ # # 
+  #  
+  #  
+  #  
+              """,
+        "z": """
+#####
+   # 
+  #  
+ #   
+#####
+              """,
+        " ": """
+
+
+
+
+
+        """,
+        "0" : """
+ ### 
+#   #
+#   #
+#   #
+ ### 
+              """,
+        "1" : """
+  ## 
+ ### 
+# ## 
+  ## 
+  ## 
+              """,
+        "2" : """
+ ### 
+#   #
+   # 
+  #  
+#####
+              """,
+        "3" : """
+ ### 
+    #
+  ## 
+    #
+ ### 
+              """,
+        "4" : """
+  ## 
+ # # 
+#####
+   # 
+   # 
+              """,
+        "5" : """
+#####
+#    
+#### 
+    #
+ ### 
+              """,
+        "6" : """
+#####
+#    
+#####
+#   #
+#####
+              """,
+        "7" : """
+#####
+   # 
+  #  
+ #   
+#    
+              """,
+        "8" : """
+ ### 
+#   #
+ ### 
+#   #
+ ### 
+              """,
+        "9" : """
+ ### 
+#   #
+ ####
+    #
+ ### 
+              """
+    }
+    text_letters = [char_dict[i.lower()].splitlines() for i in text]
+    color_dict = {
+        "warn" : "\033[93m",
+        "success" : "\033[0;32m",
+        "fail" : "\033[1;31m",
+        None: ""
+    }
+    for lines in zip(*text_letters):
+        print(f'{color_dict[type_of]}{"  ".join(x for x in lines)}{color_dict[type_of]}')
+
+
+
