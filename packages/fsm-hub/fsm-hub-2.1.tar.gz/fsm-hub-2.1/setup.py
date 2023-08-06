28432c8499635a7f18b7f933ebbb48255d598be3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:24 +0000

    Add package fsm-hub/2.1/fsm-hub-2.1.tar.gz

---
+++ packages/fsm-hub/fsm-hub-2.1.tar.gz/fsm-hub-2.1/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+author = "lwzm"
+name = "fsm-hub"
+
+setup(
+    name=name,
+    version="2.1",
+    description="Finite State Machine storage hub",
+    author=author,
+    author_email="{}@qq.com".format(author),
+    url="https://github.com/{}/{}".format(author, name),
+    keywords="fsm http web store".split(),
+    packages=["fsm_hub"],
+    scripts=[name, "fsm-step"],
+    install_requires="pony pyyaml falcon requests".split(),
+    classifiers=[
+        "Environment :: Console",
+        "Programming Language :: Python :: 3",
+    ],
+)
