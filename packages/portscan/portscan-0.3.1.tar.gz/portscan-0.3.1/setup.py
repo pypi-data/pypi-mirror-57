a618becf092b9b81f5492ed2de435927a9341c1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:35 +0000

    Add package portscan/0.3.1/portscan-0.3.1.tar.gz

---
+++ packages/portscan/portscan-0.3.1.tar.gz/portscan-0.3.1/setup.py
@@ -0,0 +1,44 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+with open("readme.md", "r") as fin:
+    readme=fin.read()
+
+setup(
+    name="portscan",
+    version="0.3.1",
+    description="Simple port scanning utility at terminal",
+    author="Rocky Li",
+    author_email="aperocky@gmail.com",
+    license="MIT",
+    url="https://github.com/Aperocky/PortScan",
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    keywords=[
+        "port",
+        "scanner",
+        "multithreading",
+        "queue",
+        "terminal",
+        "utility",
+    ],
+    classifiers=[
+        "Environment :: Console",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: English",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+        "Operating System :: Unix",
+        "Programming Language :: Python :: 3",
+        "Topic :: Utilities",
+    ],
+    install_requires=[],
+    py_modules=['portscan'],
+    entry_points={
+        'console_scripts': [
+            "portscan=portscan:main",
+        ],
+    },
+)
