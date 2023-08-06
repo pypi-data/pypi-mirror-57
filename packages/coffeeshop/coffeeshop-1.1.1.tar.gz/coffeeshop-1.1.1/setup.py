d9fa6a4d238e5abd93ac9f253444706646e7ab9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:43 +0000

    Add package coffeeshop/1.1.1/coffeeshop-1.1.1.tar.gz

---
+++ packages/coffeeshop/coffeeshop-1.1.1.tar.gz/coffeeshop-1.1.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+
+setup(
+
+    name = "coffeeshop",
+    version = "1.1.1",
+    description = "A python package that sends your deep learning training loss to your slack channel after every specified epoch.",
+    long_description = readme(),
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/CleanPegasus/coffeeshop",
+    author = "Arunkumar L - A roboVITics project",
+    author_email = "arunk609@gmail.com",
+    license = "MIT",
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7"
+    ],
+    packages = ["coffeeshop"],
+    include_package_data = True,
+    install_requires = ["tensorflow", "slackclient"]
+
+)
