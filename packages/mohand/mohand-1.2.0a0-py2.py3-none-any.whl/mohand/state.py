17c0eb3f9e356a878fdb6ba8fd4543e860108997
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:47 +0000

    Add package mohand/1.2.0a0/mohand-1.2.0a0-py2.py3-none-any.whl

---
+++ packages/mohand/mohand-1.2.0a0-py2.py3-none-any.whl/mohand/state.py
@@ -0,0 +1,24 @@
+# encoding=utf8
+"""
+用以提供全局参数的声明
+"""
+from __future__ import unicode_literals
+
+from collections import OrderedDict
+
+from mohand.utils import MohandDict
+from mohand.version import get_cli_version
+
+env = MohandDict({
+    'version': OrderedDict({
+        'mohand': get_cli_version(),
+    }),
+    'plugin_namespace': 'mohand.plugin.hand',
+
+    # 终端再赋值
+    'handfile': 'handfile',
+})
+"""
+全局环境字典，包含所有的配置
+部分配置，如 ``version`` ，会在加载完扩展插件后进行再次填充
+"""
