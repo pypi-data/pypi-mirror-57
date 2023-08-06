063ea651cebe5e8d27b481d92ffb59ea91630fdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:23 +0000

    Add package django-observable-models/0.3.0/django-observable-models-0.3.0.tar.gz

---
+++ packages/django-observable-models/django-observable-models-0.3.0.tar.gz/django-observable-models-0.3.0/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['observable_models']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['django>=3.0,<4.0', 'rx==1.6.1']
+
+setup_kwargs = {
+    'name': 'django-observable-models',
+    'version': '0.3.0',
+    'description': '',
+    'long_description': "# Django Observable Models\n\nDjango Observable Models allows you to subscribe to model operations using rxpy Observables. This is particularly useful for implementing things like GraphQL subscriptions.\n\n## Installation\n\n```bash\n$ pip install django-observable-models\n```\n\n## Usage\n\n```python\n# yourapp/models.py\nfrom observable_models.models import ObservableModel\n\nclass YourModel(ObservableModel):\n    pass\n\n# Subscribe to model creation\nYourModel.model_events.pipe(filter(lamda event: event['operation'] == YourModel.CREATED))\n\n# Subscribe to model updates \nYourModel.model_events.pipe(filter(lamda event: event['operation'] == YourModel.UPDATED))\n\n# Subscribe to model deletion \nYourModel.model_events.pipe(filter(lamda event: event['operation'] == YourModel.DELETED))\n```",
+    'author': 'Jayden Windle',
+    'author_email': 'jaydenwindle@gmail.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
