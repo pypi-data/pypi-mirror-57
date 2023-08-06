d3ee2956b494372c8358a56189414bee8ee14431
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:08 +0000

    Add package sharebear/0.3.0/sharebear-0.3.0.tar.gz

---
+++ packages/sharebear/sharebear-0.3.0.tar.gz/sharebear-0.3.0/setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = ['requirements.txt']
+
+setup_requirements = [ ]
+
+test_requirements = [ ]
+
+setup(
+    author="Anthony Edward Galassi",
+    author_email='anthony.galassi@protonmail.com',
+    python_requires='>=3.5',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    description="This project provides a simple api for setting and getting variables and objects from a text file.",
+    entry_points={
+        'console_scripts': [
+            'sharebear=sharebear.cli:main',
+        ],
+    },
+    install_requires=requirements,
+    license="BSD license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='sharebear',
+    name='sharebear',
+    packages=find_packages(include=['sharebear', 'sharebear.*']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/bendhouseart/sharebear',
+    version='0.3.0',
+    zip_safe=False,
+)
