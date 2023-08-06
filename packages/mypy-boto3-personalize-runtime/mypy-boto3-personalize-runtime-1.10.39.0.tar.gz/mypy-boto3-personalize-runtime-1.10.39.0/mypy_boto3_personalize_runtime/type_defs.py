a1175225ac462328ace9326cdd646cfbdad51d55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:49:46 +0000

    Add package mypy-boto3-personalize-runtime/1.10.39.0/mypy-boto3-personalize-runtime-1.10.39.0.tar.gz

---
+++ packages/mypy-boto3-personalize-runtime/mypy-boto3-personalize-runtime-1.10.39.0.tar.gz/mypy-boto3-personalize-runtime-1.10.39.0/mypy_boto3_personalize_runtime/type_defs.py
@@ -0,0 +1,21 @@
+"Main interface for personalize-runtime service type defs"
+import sys
+from typing import List
+
+if sys.version_info >= (3, 8):
+    from typing import TypedDict
+else:
+    from typing_extensions import TypedDict
+
+
+PredictedItemTypeDef = TypedDict("PredictedItemTypeDef", {"itemId": str}, total=False)
+
+GetPersonalizedRankingResponseTypeDef = TypedDict(
+    "GetPersonalizedRankingResponseTypeDef",
+    {"personalizedRanking": List[PredictedItemTypeDef]},
+    total=False,
+)
+
+GetRecommendationsResponseTypeDef = TypedDict(
+    "GetRecommendationsResponseTypeDef", {"itemList": List[PredictedItemTypeDef]}, total=False
+)
