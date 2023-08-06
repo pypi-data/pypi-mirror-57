6319f9cb9f3de5ae6602496112222db7ee460cec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:19 +0000

    Add package dcoshelpers/0.5.3/dcoshelpers-0.5.3.tar.gz

---
+++ packages/dcoshelpers/dcoshelpers-0.5.3.tar.gz/dcoshelpers-0.5.3/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+
+setuptools.setup(
+    name='dcoshelpers',
+    version='0.5.3',
+    author='Martijn Dekkers',
+    author_email='pypi@dekkers.org.uk',
+    description='Simple module to get a JWT token from a DC/OS master',
+    url='https://github.com/m-dekkers/fetch_dcos_token',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=['dcoshelpers'],
+    install_requires=['pyjwt', 'requests', 'datetime'],
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
