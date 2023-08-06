bb3c9ddc1ff7b5c054e4e78cb64c74115a827303
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:04 +0000

    Add package geoplotlib-fixed/0.0.1/geoplotlib-fixed-0.0.1.tar.gz

---
+++ packages/geoplotlib-fixed/geoplotlib-fixed-0.0.1.tar.gz/geoplotlib-fixed-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='geoplotlib-fixed',
+      use_2to3=True,
+      packages=['geoplotlib-fixed'],
+      version='0.0.1',
+      description='python toolbox for geographic visualizations',
+      author='John Landa',
+      author_email='jonathanlanda@gmail.com',
+      url='https://github.com/johnlanda/geoplotlib',
+      download_url='https://github.com/johnlanda/geoplotlib-fixed/archive/v0.0.1.tar.gz',
+      install_requires=['numpy>=1.12','pyglet>=1.2.4']
+)
