07d4865dcd3596f90db516f20cf45800e2fd2ab2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:48 +0000

    Add package sparktorch/0.1.2/sparktorch-0.1.2.tar.gz

---
+++ packages/sparktorch/sparktorch-0.1.2.tar.gz/sparktorch-0.1.2/sparktorch/hogwild.py
@@ -0,0 +1,187 @@
+"""
+The MIT License
+
+Copyright 2019 Derek Miller
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions
+of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
+from sparktorch.util import handle_features, load_torch_model, DataObj, load_base_torch
+from sparktorch.server import Server
+from pyspark.rdd import RDD
+import requests
+import dill
+from typing import Dict, List
+from uuid import uuid4
+import torch
+import numpy as np
+
+
+def get_state_dict(master_url: str = 'localhost:3000', retry=True) -> Dict:
+    try:
+        r = requests.get('http://{0}/parameters'.format(master_url), timeout=10)
+    except Exception as e:
+        if retry:
+            r = requests.get('http://{0}/parameters'.format(master_url), timeout=10)
+        else:
+            raise e
+
+    state_dict = dill.loads(r.content)
+    return state_dict
+
+
+def put_deltas_to_server(delta: List, master_url: str = 'localhost:3000', retry=True):
+    try:
+        requests.post('http://{0}/update'.format(master_url), data=dill.dumps(delta), timeout=10)
+    except Exception as e:
+        if retry:
+            requests.post('http://{0}/update'.format(master_url), data=dill.dumps(delta), timeout=10)
+
+
+def put_early_stop(loss, master_url: str = 'localhost:3000', retry=True):
+    try:
+        return requests.post('http://{0}/losses'.format(master_url), json={'loss': loss}, timeout=10).json()
+    except Exception as e:
+        if retry:
+            return requests.post('http://{0}/losses'.format(master_url), json={'loss': loss}, timeout=10).json()
+
+
+def get_main(master_url: str = 'localhost:3000') -> str:
+    r = requests.get('http://{0}/'.format(master_url), timeout=3)
+    return r.text
+
+
+def handle_model(
+    data: List[DataObj],
+    torch_obj: str,
+    master_url: str = 'localhost:3000',
+    iters: int = 1000,
+    verbose: int = 1,
+    early_stop_patience: int = -1,
+    mini_batch: int = -1,
+    validation_pct: float = 0
+):
+
+    partition_id = str(uuid4())
+    if data is None:
+        return 'finished'
+
+    data_obj = handle_features(data, validation_pct)
+    if data_obj.x_train is None:
+        return 'finished'
+
+    x_train = data_obj.x_train
+    y_train = data_obj.y_train if data_obj.y_train is not None else x_train
+    x_val = data_obj.x_val
+    y_val = data_obj.y_val if data_obj.y_val is not None else x_val
+
+    torch_obj = load_torch_model(torch_obj)
+
+    model = torch_obj.model
+    model.train()
+
+    criterion = torch_obj.criterion
+
+    for i in range(iters):
+
+        if 0 < mini_batch < len(data_obj.x_train):
+            idxs = np.random.choice(len(data_obj.x_train), mini_batch, replace=False).tolist()
+            x_train = data_obj.x_train[idxs]
+            y_train = data_obj.y_train[idxs]
+
+        state_dict = get_state_dict(master_url)
+        model.load_state_dict(state_dict)
+
+        y_pred = model(x_train)
+
+        try:
+            loss = criterion(y_pred, y_train)
+        except RuntimeError as e:
+            y_train = torch.flatten(y_train.long())
+            loss = criterion(y_pred, y_train)
+
+        loss.backward()
+
+        val_loss = None
+        if x_val is not None:
+            pred_val = model(x_val)
+            try:
+                val_loss = criterion(pred_val, y_val)
+            except RuntimeError as e:
+                y_val = torch.flatten(y_val.long())
+                val_loss = criterion(pred_val, y_val)
+            val_loss = val_loss.item()
+
+        gradients = []
+        for param in model.parameters():
+            gradients.append(param.grad)
+
+        put_deltas_to_server(gradients, master_url)
+
+        loss_v = loss.item()
+        if verbose:
+            print(f"Partition: {partition_id}. Iteration: {i}. Loss: {loss_v}, Val Loss: {val_loss}")
+
+        if early_stop_patience > 0:
+            loss_to_use = val_loss if val_loss is not None else loss_v
+            should_stop = put_early_stop(loss_to_use, master_url)
+            if should_stop['stop']:
+                break
+
+    return "finished"
+
+
+def train(
+    rdd: RDD,
+    torch_obj: str,
+    server: Server,
+    iters: int = 10,
+    partition_shuffles: int = 1,
+    verbose: int = 1,
+    early_stop_patience: int = -1,
+    mini_batch: int = -1,
+    validation_pct: float = 0.0
+) -> Dict:
+    try:
+        torch_obj, _ = load_base_torch(torch_obj)
+
+        master_url = str(server.master_url)
+
+        for i in range(partition_shuffles):
+            rdd.mapPartitions(
+                lambda x: handle_model(
+                    x,
+                    torch_obj=torch_obj,
+                    master_url=master_url,
+                    iters=iters,
+                    verbose=verbose,
+                    early_stop_patience=early_stop_patience,
+                    mini_batch=mini_batch,
+                    validation_pct=validation_pct
+                )
+            ).foreach(lambda x: x)
+
+            if partition_shuffles - i > 1:
+                num_partitions = rdd.getNumPartitions()
+                rdd = rdd.repartition(num_partitions)
+
+        state_dict = get_state_dict(master_url)
+        server.stop_server()
+
+        return state_dict
+
+    except Exception as e:
+        server.stop_server()
+        raise e
+
---
+++ packages/sparktorch/sparktorch-0.1.2.tar.gz/sparktorch-0.1.2/sparktorch/torch_distributed.py
@@ -0,0 +1,349 @@
+"""
+The MIT License
+
+Copyright 2019 Derek Miller
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions
+of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
+from sparktorch.pipeline_util import PysparkReaderWriter
+from sparktorch.util import DataObj, load_torch_model
+from sparktorch.server import Server
+from sparktorch.hogwild import train, get_main
+from sparktorch.distributed import train_distributed
+import numpy as np
+import torch
+import codecs
+
+from pyspark.ml.param import Param, Params, TypeConverters
+from pyspark.ml.param.shared import HasInputCol, HasPredictionCol, HasLabelCol
+from pyspark.ml.base import Estimator
+from pyspark.ml import Model
+from pyspark.ml.util import Identifiable, MLReadable, MLWritable
+from pyspark import keyword_only
+import time
+from pyspark.sql import functions as F
+import dill
+from pyspark.sql.types import DoubleType
+from pyspark.ml.linalg import VectorUDT, Vectors
+from pyspark import SparkContext
+
+
+def handle_data(input_col, label_col):
+    def inner(dataset):
+        res = [
+            DataObj(
+                x_train=np.asarray(data[input_col].toArray()),
+                y_train=data[label_col] if label_col else None,
+                x_val=None,
+                y_val=None
+            ) for data in dataset
+        ]
+        return res
+
+    return inner
+
+
+class SparkTorchModel(Model, HasInputCol, HasPredictionCol, PysparkReaderWriter, MLReadable, MLWritable, Identifiable):
+
+    modStr = Param(Params._dummy(), "modStr", "", typeConverter=TypeConverters.toString)
+    useVectorOut = Param(Params._dummy(), "useVectorOut", "", typeConverter=TypeConverters.toBoolean)
+
+    @keyword_only
+    def __init__(
+        self,
+        inputCol=None,
+        predictionCol=None,
+        modStr=None,
+        useVectorOut=None
+    ):
+        super().__init__()
+        self._setDefault(
+            inputCol='encoded',
+            predictionCol='predicted',
+            modStr='',
+            useVectorOut=False
+        )
+        kwargs = self._input_kwargs
+        self.setParams(**kwargs)
+
+    @keyword_only
+    def setParams(
+        self,
+        inputCol=None,
+        predictionCol=None,
+        modStr=None,
+        useVectorOut=None
+    ):
+        kwargs = self._input_kwargs
+        return self._set(**kwargs)
+
+    def getPytorchModel(self):
+        mod_str = self.getOrDefault(self.modStr)
+        return dill.loads(codecs.decode(mod_str.encode(), "base64"))
+
+    def _transform(self, dataset):
+        inp = self.getOrDefault(self.inputCol)
+        out = self.getOrDefault(self.predictionCol)
+        mod_str = self.getOrDefault(self.modStr)
+        use_vector_out = self.getOrDefault(self.useVectorOut)
+
+        model = dill.loads(codecs.decode(mod_str.encode(), "base64"))
+        model_broadcast = dataset._sc.broadcast(model)
+
+        def predict_vec(data):
+            features = data.toArray().reshape((1, len(data)))
+            x_data = torch.from_numpy(features).float()
+            model = model_broadcast.value
+            model.eval()
+            return Vectors.dense(model(x_data).detach().numpy().flatten())
+
+        def predict_float(data):
+            features = data.toArray().reshape((1, len(data)))
+            x_data = torch.from_numpy(features).float()
+            model = model_broadcast.value
+            model.eval()
+            raw_prediction = model(x_data).detach().numpy().flatten()
+            if len(raw_prediction) > 1:
+                return float(np.argmax(raw_prediction))
+            return float(raw_prediction[0])
+
+        if use_vector_out:
+            udfGenerateCode = F.udf(predict_vec, VectorUDT())
+        else:
+            udfGenerateCode = F.udf(predict_float, DoubleType())
+
+        return dataset.withColumn(out, udfGenerateCode(inp))
+
+
+class SparkTorch(
+    Estimator,
+    HasInputCol,
+    HasPredictionCol,
+    HasLabelCol,
+    PysparkReaderWriter,
+    MLReadable,
+    MLWritable,
+    Identifiable
+):
+
+    torchObj = Param(Params._dummy(), "torchObj", "The serialized torch object", typeConverter=TypeConverters.toString)
+    mode = Param(Params._dummy(), "mode", "The training mode", typeConverter=TypeConverters.toString)
+    device = Param(Params._dummy(), "device", "", typeConverter=TypeConverters.toString)
+    iters = Param(Params._dummy(), "iters", "", typeConverter=TypeConverters.toInt)
+    partitions = Param(Params._dummy(), "partitions", "", typeConverter=TypeConverters.toInt)
+    verbose = Param(Params._dummy(), "verbose", "", typeConverter=TypeConverters.toInt)
+    acquireLock = Param(Params._dummy(), "acquireLock", "", typeConverter=TypeConverters.toBoolean)
+    partitionShuffles = Param(Params._dummy(), "partitionShuffles", "", typeConverter=TypeConverters.toInt)
+    port = Param(Params._dummy(), "port", "", typeConverter=TypeConverters.toInt)
+    useBarrier = Param(Params._dummy(), "useBarrier", "", typeConverter=TypeConverters.toBoolean)
+    useVectorOut = Param(Params._dummy(), "useVectorOut", "", typeConverter=TypeConverters.toBoolean)
+    earlyStopPatience = Param(Params._dummy(), "earlyStopPatience", "", typeConverter=TypeConverters.toInt)
+    miniBatch = Param(Params._dummy(), "miniBatch", "", typeConverter=TypeConverters.toInt)
+    validationPct = Param(Params._dummy(), "validationPct", "", typeConverter=TypeConverters.toFloat)
+
+    @keyword_only
+    def __init__(
+        self,
+        inputCol=None,
+        labelCol=None,
+        torchObj=None,
+        iters=None,
+        predictionCol=None,
+        partitions=None,
+        acquireLock=None,
+        verbose=None,
+        partitionShuffles=None,
+        port=None,
+        useBarrier=None,
+        useVectorOut=None,
+        earlyStopPatience=None,
+        miniBatch=None,
+        validationPct=None,
+        mode=None,
+        device=None
+    ):
+        super().__init__()
+        self._setDefault(
+            inputCol='features',
+            labelCol=None,
+            torchObj='',
+            iters=10,
+            predictionCol='predicted',
+            partitions=-1,
+            acquireLock=True,
+            verbose=0,
+            partitionShuffles=1,
+            port=3000,
+            useBarrier=False,
+            useVectorOut=False,
+            earlyStopPatience=-1,
+            miniBatch=-1,
+            validationPct=0.0,
+            mode='synchronous',
+            device='cpu'
+        )
+        kwargs = self._input_kwargs
+        self.setParams(**kwargs)
+
+    @keyword_only
+    def setParams(
+        self,
+        inputCol=None,
+        labelCol=None,
+        torchObj=None,
+        iters=None,
+        predictionCol=None,
+        partitions=None,
+        acquireLock=None,
+        verbose=None,
+        partitionShuffles=None,
+        port=None,
+        useBarrier=None,
+        useVectorOut=None,
+        earlyStopPatience=None,
+        miniBatch=None,
+        validationPct=None,
+        mode=None,
+        device=None
+    ):
+        kwargs = self._input_kwargs
+        return self._set(**kwargs)
+
+    def getTorchObj(self):
+        return self.getOrDefault(self.torchObj)
+
+    def getEarlyStopPatience(self):
+        return self.getOrDefault(self.earlyStopPatience)
+
+    def getIters(self):
+        return self.getOrDefault(self.iters)
+
+    def getPartitions(self):
+        return self.getOrDefault(self.partitions)
+
+    def getVerbose(self):
+        return self.getOrDefault(self.verbose)
+
+    def getAqcuireLock(self):
+        return self.getOrDefault(self.acquireLock)
+
+    def getPartitionShuffles(self):
+        return self.getOrDefault(self.partitionShuffles)
+
+    def getPort(self):
+        return self.getOrDefault(self.port)
+
+    def getBarrier(self):
+        return self.getOrDefault(self.useBarrier)
+
+    def getVectorOut(self):
+        return self.getOrDefault(self.useVectorOut)
+
+    def getMiniBatch(self):
+        return self.getOrDefault(self.miniBatch)
+
+    def getValidationPct(self):
+        return self.getOrDefault(self.validationPct)
+
+    def getMode(self):
+        return self.getOrDefault(self.mode)
+
+    def getDevice(self):
+        return self.getOrDefault(self.device)
+
+    def _fit(self, dataset):
+        inp_col = self.getInputCol()
+        label = self.getLabelCol()
+        prediction = self.getPredictionCol()
+
+        torch_obj = self.getTorchObj()
+        iters = self.getIters()
+        partitions = self.getPartitions()
+        acquire_lock = self.getAqcuireLock()
+        verbose = self.getVerbose()
+        partition_shuffles = self.getPartitionShuffles()
+        port = self.getPort()
+        barrier = self.getBarrier()
+        use_vector_out = self.getVectorOut()
+        early_stop_patience = self.getEarlyStopPatience()
+        mini_batch = self.getMiniBatch()
+        validation_pct = self.getValidationPct()
+        mode = self.getMode()
+        device = self.getDevice()
+
+        rdd = dataset.rdd.mapPartitions(handle_data(inp_col, label))
+
+        if partitions > 0:
+            rdd = rdd.repartition(partitions)
+
+        partitions = partitions if partitions > 0 else rdd.getNumPartitions()
+
+        master_url = SparkContext._active_spark_context.getConf().get("spark.driver.host").__str__()
+
+        if mode == 'synchronous':
+
+            state_dict = train_distributed(
+                rdd=rdd,
+                torch_obj=torch_obj,
+                iters=iters,
+                partition_shuffles=1,
+                verbose=verbose,
+                mini_batch=mini_batch,
+                validation_pct=validation_pct,
+                world_size=partitions+1,
+                device=device,
+                early_stop_patience=early_stop_patience
+            )
+
+        elif mode == 'hogwild':
+
+            if barrier:
+                rdd = rdd.barrier()
+
+            server = Server(
+                torch_obj=torch_obj,
+                master_url=master_url + ":" + str(port),
+                port=port,
+                acquire_lock=acquire_lock,
+                early_stop_patience=early_stop_patience,
+                window_len=partitions
+            )
+
+            server.start_server()
+            time.sleep(5)
+            print(f'Server is running {get_main(master_url + ":" + str(port))}')
+
+            state_dict = train(
+                rdd, torch_obj, server, iters,
+                partition_shuffles, verbose=verbose,
+                early_stop_patience=early_stop_patience,
+                mini_batch=mini_batch,
+                validation_pct=validation_pct
+            )
+
+        else:
+            raise RuntimeError(f"Mode: {mode} not recognized.")
+
+        loaded = load_torch_model(torch_obj, from_json=True)
+        model = loaded.model
+        model.load_state_dict(state_dict)
+        dumped_model = codecs.encode(dill.dumps(model), "base64").decode()
+
+        return SparkTorchModel(
+            inputCol=inp_col,
+            predictionCol=prediction,
+            modStr=dumped_model,
+            useVectorOut=use_vector_out
+        )
