e1fa2ca0148717a235eff84c7987ce1a80c0faa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:10 +0000

    Add package xactor/0.1.2/xactor-0.1.2.tar.gz

---
+++ packages/xactor/xactor-0.1.2.tar.gz/xactor-0.1.2/test_xactor/test_hello.py
@@ -0,0 +1,30 @@
+"""Hello World in XActor."""
+
+import logging
+import xactor.mpi_actor as xa
+
+class Greeter:
+    def greet(self, name):
+        print("Greetings to %s from %s" % (xa.current_rank(), name))
+
+class Main:
+    def main(self):
+        # Create the greeters on very rank
+        greeter_id = "greeter"
+        for rank in xa.ranks():
+            xa.create_actor(rank, greeter_id, Greeter)
+
+        # Send them the greet message
+        every_greeter = xa.ActorProxy(xa.EVERY_RANK, greeter_id)
+        every_greeter.greet("world", send_immediate=True)
+
+        # we are done now
+        xa.stop()
+
+def test_greeter():
+    # Create the main actor
+    xa.start("main", Main)
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    test_greeter()
