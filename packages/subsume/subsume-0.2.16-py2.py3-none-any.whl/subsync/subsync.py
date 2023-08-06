3867888e4490b219a714802d036b59a0f9033dbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:51 +0000

    Add package subsume/0.2.16/subsume-0.2.16-py2.py3-none-any.whl

---
+++ packages/subsume/subsume-0.2.16-py2.py3-none-any.whl/subsync/subsync.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+import argparse
+import logging
+import sys
+
+from sklearn.pipeline import Pipeline
+
+from .aligners import FFTAligner, MaxScoreAligner
+from .speech_transformers import SubtitleSpeechTransformer, VideoSpeechTransformer
+from .subtitle_parsers import SrtParser, SrtOffseter
+from .version import __version__
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+FRAME_RATE = 48000
+SAMPLE_RATE = 100
+
+
+def make_srt_speech_pipeline(encoding, max_subtitle_seconds, start_seconds):
+    return Pipeline([
+        ('parse', SrtParser(encoding=encoding,
+                            max_subtitle_seconds=max_subtitle_seconds,
+                            start_seconds=start_seconds)),
+        ('speech_extract', SubtitleSpeechTransformer(sample_rate=SAMPLE_RATE,
+                                                     start_seconds=start_seconds))
+    ])
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Synchronize subtitles with video.')
+    parser.add_argument('-v', '--version', action='version',
+                        version='%(prog)s {version}'.format(version=__version__))
+    parser.add_argument('reference',
+                        help='Correct reference (video or srt) to which to sync input subtitles.')
+    parser.add_argument('-i', '--srtin', help='Input subtitles file (default=stdin).')
+    parser.add_argument('-o', '--srtout', help='Output subtitles file (default=stdout).')
+    parser.add_argument('--encoding', default='infer',
+                        help='What encoding to use for reading input subtitles.')
+    parser.add_argument('--max-subtitle-seconds', type=float, default=10,
+                        help='Maximum duration for a subtitle to appear on-screen.')
+    parser.add_argument('--start-seconds', type=int, default=0,
+                        help='Start time for processing.')
+    parser.add_argument('--output-encoding', default='same',
+                        help='What encoding to use for writing output subtitles '
+                             '(default=same as for input).')
+    parser.add_argument('--reference-encoding',
+                        help='What encoding to use for reading / writing reference subtitles '
+                             '(if applicable).')
+    parser.add_argument('--vlc-mode', action='store_true', help=argparse.SUPPRESS)
+    args = parser.parse_args()
+    if args.vlc_mode:
+        logger.setLevel(logging.CRITICAL)
+    if args.reference.endswith('srt'):
+        reference_pipe = make_srt_speech_pipeline(args.reference_encoding or 'infer',
+                                                  args.max_subtitle_seconds,
+                                                  args.start_seconds)
+    else:
+        if args.reference_encoding is not None:
+            logger.warning('Reference srt encoding specified, but reference was a video file')
+        reference_pipe = Pipeline([
+            ('speech_extract', VideoSpeechTransformer(sample_rate=SAMPLE_RATE,
+                                                      frame_rate=FRAME_RATE,
+                                                      start_seconds=args.start_seconds,
+                                                      vlc_mode=args.vlc_mode))
+        ])
+    srtin_pipe = make_srt_speech_pipeline(args.encoding,
+                                          args.max_subtitle_seconds,
+                                          args.start_seconds)
+    logger.info('computing alignments...')
+    offset_seconds = MaxScoreAligner(FFTAligner).fit_transform(
+        srtin_pipe.fit_transform(args.srtin),
+        reference_pipe.fit_transform(args.reference)
+    ) / float(SAMPLE_RATE)
+    logger.info('offset seconds: %.3f', offset_seconds)
+    SrtOffseter(offset_seconds).fit_transform(
+        srtin_pipe.named_steps['parse'].subs_).set_encoding(
+        args.output_encoding).write_file(args.srtout)
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ packages/subsume/subsume-0.2.16-py2.py3-none-any.whl/subtools/subtitle_parsers.py
@@ -0,0 +1,146 @@
+# -*- coding: utf-8 -*-
+import logging
+import re
+import sys
+from datetime import timedelta
+
+from sklearn.base import TransformerMixin
+import srt
+
+from .file_utils import open_file
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+TS_REGEX = u"[,.:，．。：]".join([r"(\d+", r"\d{2}", r"\d{2}", r")(\d{3})\d+"])
+
+
+def _srt_parse(s, max_subtitle_seconds=None, start_seconds=0, tolerant=True):
+    start_time = timedelta(seconds=start_seconds)
+    s = re.sub(TS_REGEX, r"\1\2", s)
+    subs = srt.parse(s)
+    subs_list = []
+    max_duration = timedelta(days=1)
+    if max_subtitle_seconds is not None:
+        max_duration = timedelta(seconds=max_subtitle_seconds)
+    while True:
+        try:
+            next_sub = next(subs)
+            if next_sub.start < start_time:
+                continue
+            next_sub.end = min(next_sub.end, next_sub.start + max_duration)
+            subs_list.append(next_sub)
+        # We don't catch SRTParseError here b/c that typically raised when we
+        # are trying to parse with the wrong encoding, in which case we might
+        # be able to try another one on the *entire* set of subtitles elsewhere.
+        except ValueError as e:
+            if tolerant:
+                logger.warning(e)
+                continue
+            else:
+                raise
+        except StopIteration:
+            break
+    return subs_list
+
+
+class _SrtMixin(object):
+    def __init__(self, subs=None):
+        self.subs_ = subs
+
+    def set_encoding(self, encoding):
+        self.subs_.set_encoding(encoding)
+        return self
+
+
+class SrtSubtitles(list):
+    def __init__(self, *args, **kwargs):
+        encoding = kwargs.pop('encoding', None)
+        if encoding is None:
+            raise ValueError('encoding must be specified')
+        super(self.__class__, self).__init__(*args, **kwargs)
+        self._encoding = encoding
+
+    def set_encoding(self, encoding):
+        if encoding != 'same':
+            self._encoding = encoding
+        return self
+
+    @property
+    def encoding(self):
+        return self._encoding
+
+    def write_file(self, fname):
+        if sys.version_info[0] > 2:
+            with open(fname or sys.stdout.fileno(), 'w', encoding=self.encoding) as f:
+                return f.write(srt.compose(self))
+        else:
+            with (fname and open(fname, 'w')) or sys.stdout as f:
+                return f.write(srt.compose(self).encode(self.encoding))
+
+
+class SrtParser(_SrtMixin, TransformerMixin):
+    def __init__(self, encoding='infer', max_subtitle_seconds=None, start_seconds=0):
+        super(self.__class__, self).__init__()
+        self.encoding_to_use = encoding
+        self.sub_skippers = []
+        self.max_subtitle_seconds = max_subtitle_seconds
+        self.start_seconds = start_seconds
+
+    def fit(self, fname, *_):
+        encodings_to_try = (self.encoding_to_use,)
+        if self.encoding_to_use == 'infer':
+            encodings_to_try = ('utf-8', 'utf-8-sig', 'chinese', 'latin-1', 'utf-16')
+        with open_file(fname, 'rb') as f:
+            subs = f.read()
+        exc = None
+        for encoding in encodings_to_try:
+            try:
+                self.subs_ = SrtSubtitles(
+                    _srt_parse(subs.decode(encoding).strip(),
+                               max_subtitle_seconds=self.max_subtitle_seconds,
+                               start_seconds=self.start_seconds),
+                    encoding=encoding
+                )
+                return self
+            except Exception as e:
+                exc = e
+                continue
+        raise exc
+
+    def transform(self, *_):
+        return self.subs_
+
+
+class SrtOffseter(_SrtMixin, TransformerMixin):
+    def __init__(self, td_seconds):
+        super(_SrtMixin, self).__init__()
+        if not isinstance(td_seconds, timedelta):
+            self.td_seconds = timedelta(seconds=td_seconds)
+        else:
+            self.td_seconds = td_seconds
+
+    def fit(self, subs, *_):
+        offset_subs = []
+        for sub in subs:
+            offset_subs.append(srt.Subtitle(index=sub.index,
+                                            start=sub.start + self.td_seconds,
+                                            end=sub.end + self.td_seconds,
+                                            content=sub.content))
+        self.subs_ = SrtSubtitles(offset_subs, encoding=subs.encoding)
+        return self
+
+    def transform(self, *_):
+        return self.subs_
+
+
+def read_srt_from_file(fname, encoding='infer'):
+    return SrtParser(encoding).fit_transform(fname)
+
+
+def write_srt_to_file(fname, subs, encoding):
+    return SrtSubtitles(subs, encoding=encoding).write_file(fname)
+
+
+def srt_offset(subs, td_seconds):
+    return SrtOffseter(td_seconds).fit_transform(subs)
