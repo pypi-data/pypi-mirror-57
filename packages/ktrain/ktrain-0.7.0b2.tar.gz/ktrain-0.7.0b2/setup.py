f04403fc320c1fb366df356136ce38404a4974ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:55 +0000

    Add package ktrain/0.7.0b2/ktrain-0.7.0b2.tar.gz

---
+++ packages/ktrain/ktrain-0.7.0b2.tar.gz/ktrain-0.7.0b2/setup.py
@@ -0,0 +1,77 @@
+from distutils.core import setup
+import setuptools
+
+with open('README.md') as readme_file: readme = readme_file.read()
+exec(open('ktrain/version.py').read())
+
+
+# install forked versions of libraries when necessary
+import subprocess
+import sys
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+
+import tempfile
+import os.path
+with open(os.path.join(tempfile.gettempdir(), 'ktrain_pip_install'), 'w') as f:
+    f.write(" ".join(sys.argv))
+
+if 'install' in sys.argv[1:] or 'bdist_wheel' in sys.argv[1:]:
+    # stellargraph fork
+    # reason:  avoid installing tensorflow and overwriting tensorflow-gpu
+    install('git+https://github.com/amaiya/stellargraph@no_tf_dep_082')
+    ## eli5 fork 
+    # reason: eli5 support for tf.keras
+    install('git+https://github.com/amaiya/eli5@tfkeras_0_10_1')
+
+
+setup(
+  name = 'ktrain',
+  packages = setuptools.find_packages(),
+  version = __version__,
+  license='MIT',
+  description = 'ktrain is a lightweight wrapper for Keras to help train neural networks',
+  long_description = readme,
+  long_description_content_type = 'text/markdown',
+  author = 'Arun S. Maiya',
+  author_email = 'arun@maiya.net',
+  url = 'https://github.com/amaiya/ktrain',
+  keywords = ['keras', 'deep learning', 'machine learning'],
+  install_requires=[
+          'scikit-learn >= 0.20.0',
+          'matplotlib >= 3.0.0',
+          'pandas >= 0.24.2',
+          'fastprogress >= 0.1.21',
+          'keras_bert',
+          'requests',
+          'seqeval',
+          'joblib',
+          'langdetect',
+          'jieba',
+          'cchardet',
+          'networkx==2.3',
+          'bokeh',
+          #'eli5 >= 0.10.0',
+          #'pillow'
+      ],
+  classifiers=[  # Optional
+    # How mature is this project? Common values are
+    #   3 - Alpha
+    #   4 - Beta
+    #   5 - Production/Stable
+    'Development Status :: 3 - Alpha',
+
+    # Indicate who your project is intended for
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+
+    # Pick your license as you wish
+    'License :: OSI Approved :: MIT License',
+
+    # Specify the Python versions you support here. In particular, ensure
+    # that you indicate whether you support Python 2, Python 3 or both.
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
