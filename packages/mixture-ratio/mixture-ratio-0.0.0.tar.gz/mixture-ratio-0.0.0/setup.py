ec97bb870e3f641bce81aede7be57bc01d02e6f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:59 +0000

    Add package mixture-ratio/0.0.0/mixture-ratio-0.0.0.tar.gz

---
+++ packages/mixture-ratio/mixture-ratio-0.0.0.tar.gz/mixture-ratio-0.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='mixture-ratio',
+      version='0.0.00',
+      url='https://github.com/marko-ruman/mixture-ratio',
+      license='MIT',
+      author='Marko Ruman',
+      author_email='marko.ruman@gmail.com',
+      description='Implementation of Mixture Ratio probabilistic model',
+      packages=find_packages(),
+      # long_description=long_description,
+      zip_safe=False)
