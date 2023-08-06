104eb2e841117952a66ecf6575bcf8929ef6a932
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:27 +0000

    Add package color2vec/1.0.0/color2vec-1.0.0.tar.gz

---
+++ packages/color2vec/color2vec-1.0.0.tar.gz/color2vec-1.0.0/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+
+setuptools.setup(
+    name="color2vec",
+    version="1.0.0",
+    author="chandan mishra",
+    author_email="chandan.mishra@greendeck.co",
+    description="Greendeck color to vector package.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=['color2vec', 'color2vec.src', 'color2vec.src.data', 'color2vec.src.model'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'tensorflow==1.8.0', 'tqdm', 'keras==2.1.5', 'numpy', 'pandas', 'scipy'
+    ],
+    include_package_data=True,
+    zip_safe=False
+)
