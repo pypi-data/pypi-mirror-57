aa867d4adc5fabe729a05818924f6ee3071a2d68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:42 +0000

    Add package saves3k/0.1.3/saves3k-0.1.3.tar.gz

---
+++ packages/saves3k/saves3k-0.1.3.tar.gz/saves3k-0.1.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='saves3k',
+    version='0.1.3',
+    url='',
+    license='',
+    author='Renan Costa',
+    author_email='renanpcosta@gmail.com',
+    keywords='',
+    description=u'',
+    packages=['saves3k'],
+    install_requires=[],
+)
