eadfc6c215658bf4fa5c4722841ccd8449b82e5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:44 +0000

    Add package test-client-176883/1.0.2/test_client_176883-1.0.2.tar.gz

---
+++ packages/test-client-176883/test_client_176883-1.0.2.tar.gz/test_client_176883-1.0.2/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+setuptools.setup(
+    # How you named your package folder (MyLib)
+    name='test_client_176883',
+    packages=['test_client_176883'],   # Chose the same as "name"
+    version='1.0.2',      # Start with a small number and increase it with every change you make
+    license='MIT',
+    # Give a short description about your library
+    description='TYPE YOUR DESCRIPTION HERE',
+    author='Thuan do',                   # Type in your name
+    author_email='thuan14121999@gmail.com',      # Type in your E-Mail
+    # Provide either the link to your github or to your website
+    url='https://github.com/thuan1412',
+    # Keywords that define your package best
+    keywords=['SOME', 'MEANINGFULL', 'KEYWORDS'],
+    install_requires=[
+        "requests",
+    ],
+    py_modules=['api_request'],
+    classifiers=[
+        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Development Status :: 3 - Alpha',
+        # Define that your audience are developers
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',   # Again, pick a license
+        # Specify which pyhton versions that you want to support
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
---
+++ packages/test-client-176883/test_client_176883-1.0.2.tar.gz/test_client_176883-1.0.2/test_client_176883/add_item.py
@@ -0,0 +1,16 @@
+class AddItem():
+    def __init__(self, item_id, cat_id):
+        """
+        Required parameters
+        """
+
+        self.item_id = item_id
+        self.cat_id = cat_id
+        self.method = "POST"
+        self.path = "/reze/v1/item"
+
+    def get_body_parameters(self):
+        p = dict()
+        p['itemId'] = self.item_id
+        p['catId'] = self.cat_id
+        return p
