0e2b5b382603c80613982f15c82c09f15ecfe0dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:11 +0000

    Add package rbg2-distributions/0.2/rbg2_distributions-0.2.tar.gz

---
+++ packages/rbg2-distributions/rbg2_distributions-0.2.tar.gz/rbg2_distributions-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='rbg2_distributions',
+      version='0.2',
+      description='Gaussian distributions',
+      author='HyunSuk O',
+      author_email='oh_hyunseok@hotmail.com',
+      packages=['rbg2_distributions'],
+      zip_safe=False)
