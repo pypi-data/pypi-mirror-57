b226236ff612b3186477cc2683aae0b5d48eb3b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:03 +0000

    Add package ossTools/0.1.13/ossTools-0.1.13.tar.gz

---
+++ packages/ossTools/ossTools-0.1.13.tar.gz/ossTools-0.1.13/setup.py
@@ -0,0 +1,33 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="ossTools",
+    version="0.1.13",
+    description="Useful tools that simplify OSS file operation.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/allankevinrichie/ossTools",
+    author="Suyue (Ziheng Zhang)",
+    author_email="suyue.zzh@alibaba-inc.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["ossTools"],
+    include_package_data=True,
+    install_requires=["oss2", "six", "opencv-contrib-python", "tensorboardX"],
+)
