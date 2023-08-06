252916751c9427220df5b2d7204d7fef80591855
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:32 +0000

    Add package lhub-extractors/0.2.9/lhub_extractors-0.2.9.tar.gz

---
+++ packages/lhub-extractors/lhub_extractors-0.2.9.tar.gz/lhub_extractors-0.2.9/lhub_extractors/shim_exec.py
@@ -0,0 +1,35 @@
+import click
+import json
+import sys
+
+from lhub_extractors import util
+from lhub_extractors import feature_extractor
+
+def run_extractor(entrypoint_fn):
+
+    output_table = ["hello world"]
+    node_metadata = {}
+
+    # for data in sys.stdin.readlines():
+    #     as_dict = json.loads(data)
+    #
+    #     if as_dict.has_key['node_metadata']:
+    #         node_metadata = as_dict['node_metadata']
+    #
+    #     if as_dict.has_key['row']:
+    #         row = as_dict['row']
+    #         output_table.append(row)
+
+    util.print_result(output_table)
+
+@click.command()
+@click.option("--entrypoint", "-e", required=True)
+def main(entrypoint):
+    errors = util.import_workdir()
+    if errors:
+        print("error")
+        #util.hard_exit_from_instantiation(str(errors[0]))
+    print(feature_extractor.all())
+    entrypoint_fn = feature_extractor.all().get(entrypoint).function
+    assert entrypoint_fn is not None
+    run_extractor(entrypoint_fn)
