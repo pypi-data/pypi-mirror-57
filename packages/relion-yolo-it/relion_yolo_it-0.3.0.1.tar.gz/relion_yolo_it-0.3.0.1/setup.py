536afa3a23a437e7c37fd3b8561893d0b71914f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:44 +0000

    Add package relion-yolo-it/0.3.0.1/relion_yolo_it-0.3.0.1.tar.gz

---
+++ packages/relion-yolo-it/relion_yolo_it-0.3.0.1.tar.gz/relion_yolo_it-0.3.0.1/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+from setuptools import setup, find_packages
+
+requirements = ["gemmi==0.2.8", "numpy==1.14.5"]
+setup_requirements = []
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name="relion_yolo_it",
+    author="Donovan Webb",
+    author_email="donovan.webb@diamond.ac.uk",
+    url = "https://github.com/DonovanWebb/CryoloRelion/tree/pypiLive",
+    download_url = "https://github.com/DonovanWebb/CryoloRelion/archive/0.3.0.tar.gz",
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Natural Language :: English",
+    ],
+    description="Updated relion_it with cryolo wrappers",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=requirements,
+    license="GPLv2",
+    include_package_data=True,
+    packages=find_packages(),
+    scripts=[
+        "relion_yolo_it/cryolo_relion_it.py",
+        "relion_yolo_it/CryoloPipeline.py",
+        "relion_yolo_it/CryoloExternalJob.py",
+        "relion_yolo_it/CryoloFineTuneJob.py"
+    ],
+    python_requires='==3.6.8',
+    setup_requires=setup_requirements,
+    version="0.3.0.1",
+    zip_safe=False,
+)
+
