b7a4c1c8cce99ba4a39aec3a58ca4fdfcfa017a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:18 +0000

    Add package nesterpelu/1.0.2/nesterpelu-1.0.2.tar.gz

---
+++ packages/nesterpelu/nesterpelu-1.0.2.tar.gz/nesterpelu-1.0.2/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'nesterpelu',
+    version = '1.0.2',
+    py_modules = ['nesterpelu'],
+    author = 'Jose',
+    author_email = 'jose.rivascoloma@gmail.com',
+    url = 'http://www.google.com',
+    description = 'Un simple impresor de listas anidadas',
+)
