53285d966c2dda3991a30f5a0c680233b7e09c82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:08 +0000

    Add package mypy-boto3-shield/1.10.34.1/mypy_boto3_shield-1.10.34.1-py3-none-any.whl

---
+++ packages/mypy-boto3-shield/mypy_boto3_shield-1.10.34.1-py3-none-any.whl/mypy_boto3_shield/__init__.py
@@ -0,0 +1,7 @@
+"Main interface for shield service"
+
+from mypy_boto3_shield.client import Client
+from mypy_boto3_shield.paginator import ListAttacksPaginator, ListProtectionsPaginator
+
+
+__all__ = ("Client", "ListAttacksPaginator", "ListProtectionsPaginator")
