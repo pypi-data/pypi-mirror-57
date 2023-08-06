fe297e7ce828f4d845bc903b6b4789745a32d289
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:45 +0000

    Add package mypy-boto3-dataexchange/1.10.34/mypy_boto3_dataexchange-1.10.34-py3-none-any.whl

---
+++ packages/mypy-boto3-dataexchange/mypy_boto3_dataexchange-1.10.34-py3-none-any.whl/mypy_boto3_dataexchange/paginator.py
@@ -0,0 +1,88 @@
+"Main interface for dataexchange service Paginators"
+from __future__ import annotations
+
+from botocore.paginate import Paginator as Boto3Paginator
+from mypy_boto3_dataexchange.type_defs import (
+    ListDataSetRevisionsPaginatePaginationConfigTypeDef,
+    ListDataSetRevisionsPaginateResponseTypeDef,
+    ListDataSetsPaginatePaginationConfigTypeDef,
+    ListDataSetsPaginateResponseTypeDef,
+    ListJobsPaginatePaginationConfigTypeDef,
+    ListJobsPaginateResponseTypeDef,
+    ListRevisionAssetsPaginatePaginationConfigTypeDef,
+    ListRevisionAssetsPaginateResponseTypeDef,
+)
+
+
+__all__ = (
+    "ListDataSetRevisionsPaginator",
+    "ListDataSetsPaginator",
+    "ListJobsPaginator",
+    "ListRevisionAssetsPaginator",
+)
+
+
+class ListDataSetRevisionsPaginator(Boto3Paginator):
+    """
+    Paginator for `list_data_set_revisions`
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self,
+        DataSetId: str,
+        PaginationConfig: ListDataSetRevisionsPaginatePaginationConfigTypeDef = None,
+    ) -> ListDataSetRevisionsPaginateResponseTypeDef:
+        """
+        [ListDataSetRevisions.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.34/reference/services/dataexchange.html#DataExchange.Paginator.ListDataSetRevisions.paginate)
+        """
+
+
+class ListDataSetsPaginator(Boto3Paginator):
+    """
+    Paginator for `list_data_sets`
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self,
+        Origin: str = None,
+        PaginationConfig: ListDataSetsPaginatePaginationConfigTypeDef = None,
+    ) -> ListDataSetsPaginateResponseTypeDef:
+        """
+        [ListDataSets.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.34/reference/services/dataexchange.html#DataExchange.Paginator.ListDataSets.paginate)
+        """
+
+
+class ListJobsPaginator(Boto3Paginator):
+    """
+    Paginator for `list_jobs`
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self,
+        DataSetId: str = None,
+        RevisionId: str = None,
+        PaginationConfig: ListJobsPaginatePaginationConfigTypeDef = None,
+    ) -> ListJobsPaginateResponseTypeDef:
+        """
+        [ListJobs.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.34/reference/services/dataexchange.html#DataExchange.Paginator.ListJobs.paginate)
+        """
+
+
+class ListRevisionAssetsPaginator(Boto3Paginator):
+    """
+    Paginator for `list_revision_assets`
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def paginate(
+        self,
+        DataSetId: str,
+        RevisionId: str,
+        PaginationConfig: ListRevisionAssetsPaginatePaginationConfigTypeDef = None,
+    ) -> ListRevisionAssetsPaginateResponseTypeDef:
+        """
+        [ListRevisionAssets.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.34/reference/services/dataexchange.html#DataExchange.Paginator.ListRevisionAssets.paginate)
+        """
