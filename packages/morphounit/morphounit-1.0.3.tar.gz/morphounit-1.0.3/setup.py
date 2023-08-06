0f0e25c69263dfae66db1f5c5fd79699278af6c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:11 +0000

    Add package morphounit/1.0.3/morphounit-1.0.3.tar.gz

---
+++ packages/morphounit/morphounit-1.0.3.tar.gz/morphounit-1.0.3/setup.py
@@ -0,0 +1,24 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='morphounit',
+    version='1.0.3',
+    author='Shailesh Appukuttan, Pedro Garcia',
+    author_email='shailesh.appukuttan@unic.cnrs-gif.fr, pedro.garcia@unic.cnrs-gif.fr',
+    packages=['morphounit',
+              'morphounit.capabilities',
+              'morphounit.tests',
+              'morphounit.scores',
+              'morphounit.plots'],
+    url='https://github.com/appukuttan-shailesh/morphounit',
+    keywords = ['morphology', 'structure', 'circuit', 'testing', 'validation framework'],
+    license='MIT',
+    description='A SciUnit library for data-driven testing of neuronal morphologies.',
+    long_description="",
+    install_requires=['neo','elephant','sciunit>=0.1.5.2', 'tabulate'],
+    dependency_links = ['git+http://github.com/neuralensemble/python-neo.git#egg=neo-0.4.0dev',
+                        'https://github.com/scidash/sciunit/tarball/dev']
+)
