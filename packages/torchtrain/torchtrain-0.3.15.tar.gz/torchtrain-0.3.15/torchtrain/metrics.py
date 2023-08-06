c7332b6fba53682e87f40a62ad5b7b9ea316a473
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:46 +0000

    Add package torchtrain/0.3.15/torchtrain-0.3.15.tar.gz

---
+++ packages/torchtrain/torchtrain-0.3.15.tar.gz/torchtrain-0.3.15/torchtrain/metrics.py
@@ -0,0 +1,32 @@
+class AverageAggregator:
+    def __init__(self, criterion):
+        self.criterion = criterion
+        self.reset()
+
+    def reset(self):
+        self.value = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, outputs, labels):
+        self.batch_score = self.criterion(outputs, labels)
+        batch_value = (
+            self.batch_score.item()
+            if hasattr(self.batch_score, "item")
+            else self.batch_score
+        )
+        batch_size = labels.size(0)
+        self.sum += batch_value * batch_size
+        self.count += batch_size
+        self.value = self.sum / self.count
+
+    def get_value(self, reset=False):
+        value = self.value
+        if reset:
+            self.reset()
+        return value
+
+    def get_batch_score(self):
+        batch_score = self.batch_score
+        del self.batch_score
+        return batch_score
