a2bb92b35141e92423632732137ab5217bdac8ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:34 +0000

    Add package waldur-mastermind/4.2.2/waldur_mastermind-4.2.2-py3-none-any.whl

---
+++ packages/waldur-mastermind/waldur_mastermind-4.2.2-py3-none-any.whl/waldur_mastermind/marketplace_rancher/processors.py
@@ -0,0 +1,15 @@
+from waldur_rancher import views as rancher_views
+from waldur_mastermind.marketplace import processors
+
+
+class RancherCreateProcessor(processors.BaseCreateResourceProcessor):
+    viewset = rancher_views.ClusterViewSet
+    fields = (
+        'name',
+        'description',
+        'nodes',
+    )
+
+
+class RancherDeleteProcessor(processors.DeleteResourceProcessor):
+    viewset = rancher_views.ClusterViewSet
