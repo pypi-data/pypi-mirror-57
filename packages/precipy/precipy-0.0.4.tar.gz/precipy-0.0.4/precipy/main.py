1e412a61e21000bdebfd7a9f47473ac6ac15f3e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:02 +0000

    Add package precipy/0.0.4/precipy-0.0.4.tar.gz

---
+++ packages/precipy/precipy-0.0.4.tar.gz/precipy-0.0.4/precipy/main.py
@@ -0,0 +1,12 @@
+from precipy.batch import Batch
+
+def render(request):
+    batch = Batch(request)
+    batch.generate_analytics()
+    output = batch.process_filters()
+    return output
+
+from precipy.mock import Request
+import sys
+if __name__ == '__main__':
+    render(Request(sys.argv[1]))
