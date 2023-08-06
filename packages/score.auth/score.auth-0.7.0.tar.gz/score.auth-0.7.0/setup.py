7eac4c85ebec8fac15aa06b407608340881fbf6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:11 +0000

    Add package score.auth/0.7.0/score.auth-0.7.0.tar.gz

---
+++ packages/score.auth/score.auth-0.7.0.tar.gz/score.auth-0.7.0/setup.py
@@ -0,0 +1,41 @@
+import os
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.rst')) as f:
+    README = f.read()
+
+setup(
+    name='score.auth',
+    version='0.7.0',
+    description='Authorization and Authentication for The SCORE Framework',
+    long_description=README,
+    author='strg.at',
+    author_email='support@strg.at',
+    url='http://strg.at',
+    keywords='score framework authentication authorization pyramid',
+    packages=['score', 'score.auth'],
+    namespace_packages=['score'],
+    zip_safe=False,
+    license='LGPL',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Framework :: Pyramid',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Lesser General '
+            'Public License v3 or later (LGPLv3+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Security',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+    ],
+    include_package_data=True,
+    install_requires=[
+        'score.init >= 0.3',
+        'score.ctx >= 0.6, <0.7',
+    ],
+)
