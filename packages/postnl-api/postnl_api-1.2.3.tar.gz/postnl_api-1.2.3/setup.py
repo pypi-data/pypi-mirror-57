f96423148f87d8347b42b0dae6ac975316fbbba4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:25 +0000

    Add package postnl-api/1.2.3/postnl_api-1.2.3.tar.gz

---
+++ packages/postnl-api/postnl_api-1.2.3.tar.gz/postnl_api-1.2.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="postnl_api",
+    version="1.2.3",
+    description="Python wrapper for the PostNL API, a way to track packages and letters.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/imicknl/python-postnl-api",
+    author="Mick Vleeshouwer",
+    author_email="mick@imick.nl",
+    license="MIT",
+    install_requires=["requests>=2.0"],
+    packages=find_packages(),
+    zip_safe=True,
+)
