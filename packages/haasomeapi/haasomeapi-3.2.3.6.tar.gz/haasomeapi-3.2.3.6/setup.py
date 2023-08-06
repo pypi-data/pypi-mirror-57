70ebf59d7e863d6a0ec754bf389b0342065153e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:04 +0000

    Add package haasomeapi/3.2.3.6/haasomeapi-3.2.3.6.tar.gz

---
+++ packages/haasomeapi/haasomeapi-3.2.3.6.tar.gz/haasomeapi-3.2.3.6/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(name='haasomeapi',
+      version='3.2.3.6',
+      description='Python module to interact with the Haasonline Local API',
+      url='http://github.com/haasome-tools/haasomeapi',
+      author='Haasome Tools',
+      author_email='haasomeapi@haasometools.com',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+            'requests',
+            'python-dateutil'
+      ],
+      zip_safe=False)
