ec753e83af35dbd3152d3d8dadc7d383d2fc63b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:09 +0000

    Add package pyprojecttemplate/0.1.0/pyprojecttemplate-0.1.0-py3-none-any.whl

---
+++ packages/pyprojecttemplate/pyprojecttemplate-0.1.0-py3-none-any.whl/pyprojecttemplate/basicmath.py
@@ -0,0 +1,17 @@
+"""
+Basic Sample Python to define common mathematical functions
+"""
+
+
+def calculate_square(num):
+    """
+    Returns the square of a given number
+    """
+    return num * num
+
+
+def add(a, b):
+    """
+    Returns the sum of two numbers
+    """
+    return a + b
---
+++ packages/pyprojecttemplate/pyprojecttemplate-0.1.0-py3-none-any.whl/pytemplate/demo.py
@@ -0,0 +1,5 @@
+from pyprojecttemplate import basicmath
+
+number = 12
+print("The square of the number {num} is {num_squared}".format(num=number,
+                                                               num_squared=basicmath.calculate_square(number)))
