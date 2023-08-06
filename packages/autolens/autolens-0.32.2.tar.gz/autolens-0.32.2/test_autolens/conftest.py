b0f1fbfeb9fc330d91472a9cb69185c2aba64445
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:01 +0000

    Add package autolens/0.32.2/autolens-0.32.2.tar.gz

---
+++ packages/autolens/autolens-0.32.2.tar.gz/autolens-0.32.2/test_autolens/conftest.py
@@ -0,0 +1,29 @@
+import os
+
+import pytest
+from matplotlib import pyplot
+
+import autofit as af
+
+
+@pytest.fixture(name="general_config", autouse=True)
+def make_general_config():
+    general_config_path = "{}/test_files/config/plotting/".format(
+        os.path.dirname(os.path.realpath(__file__))
+    )
+    af.conf.instance.general = af.conf.NamedConfig(general_config_path + "general.ini")
+
+
+class PlotPatch(object):
+    def __init__(self):
+        self.paths = []
+
+    def __call__(self, path, *args, **kwargs):
+        self.paths.append(path)
+
+
+@pytest.fixture(name="plot_patch")
+def make_plot_patch(monkeypatch):
+    plot_patch = PlotPatch()
+    monkeypatch.setattr(pyplot, "savefig", plot_patch)
+    return plot_patch
