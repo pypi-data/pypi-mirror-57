c3bd7f0631ad7ef013c60a58aafe68f79bbaeeab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:29 +0000

    Add package OpenNMT-tf/2.4.0/OpenNMT-tf-2.4.0.tar.gz

---
+++ packages/OpenNMT-tf/OpenNMT-tf-2.4.0.tar.gz/OpenNMT-tf-2.4.0/opennmt/bin/detokenize_text.py
@@ -0,0 +1,24 @@
+"""Standalone script to detokenize a corpus."""
+
+from __future__ import print_function
+
+import argparse
+
+from opennmt import tokenizers
+
+
+def main():
+  parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+  parser.add_argument(
+      "--delimiter", default=" ",
+      help="Token delimiter used in text serialization.")
+  parser.add_argument(
+      "--tokenizer_config", default=None,
+      help="Tokenization configuration.")
+  args = parser.parse_args()
+
+  tokenizer = tokenizers.make_tokenizer(args.tokenizer_config)
+  tokenizer.detokenize_stream(delimiter=args.delimiter)
+
+if __name__ == "__main__":
+  main()
