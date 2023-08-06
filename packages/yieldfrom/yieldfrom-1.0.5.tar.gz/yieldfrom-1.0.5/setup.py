1609389d358674b75a303ed41278a31d74a17fd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:24 +0000

    Add package yieldfrom/1.0.5/yieldfrom-1.0.5.tar.gz

---
+++ packages/yieldfrom/yieldfrom-1.0.5.tar.gz/yieldfrom-1.0.5/setup.py
@@ -0,0 +1,31 @@
+import os
+
+from setuptools import setup
+
+version = '1.0.5'
+
+with open(os.path.join(os.path.dirname(__file__), "README.md")) as readme:
+    long_description = readme.read()
+
+setup(
+    name='yieldfrom',
+    version=version,
+    description="A backport of the `yield from` semantic from Python 3.x to "
+                "Python 2.7",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='MIT',
+    author='Amir Rachum',
+    author_email='amir@rachum.com',
+    url='https://github.com/Nurdok/yieldfrom/',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Environment :: Console',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+    ],
+    keywords='yield,from,yield from,generators,backport,iterators',
+    py_modules=('yieldfrom',),
+)
