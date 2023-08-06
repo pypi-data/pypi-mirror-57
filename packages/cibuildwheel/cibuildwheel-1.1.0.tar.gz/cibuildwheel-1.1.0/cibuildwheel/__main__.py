0d2638a92f42401b3fb182af3d7c611d58e9a201
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:31 +0000

    Add package cibuildwheel/1.1.0/cibuildwheel-1.1.0.tar.gz

---
+++ packages/cibuildwheel/cibuildwheel-1.1.0.tar.gz/cibuildwheel-1.1.0/cibuildwheel/__main__.py
@@ -0,0 +1,262 @@
+from __future__ import print_function
+import argparse, os, subprocess, sys, textwrap
+
+import cibuildwheel
+import cibuildwheel.linux, cibuildwheel.windows, cibuildwheel.macos
+from cibuildwheel.environment import parse_environment, EnvironmentParseError
+from cibuildwheel.util import BuildSelector, Unbuffered
+
+def get_option_from_environment(option_name, platform=None, default=None):
+    '''
+    Returns an option from the environment, optionally scoped by the platform.
+
+    Example:
+      get_option_from_environment('CIBW_COLOR', platform='macos')
+
+      This will return the value of CIBW_COLOR_MACOS if it exists, otherwise the value of
+      CIBW_COLOR.
+    '''
+    if platform:
+        option = os.environ.get('%s_%s' % (option_name, platform.upper()))
+        if option is not None:
+            return option
+
+    return os.environ.get(option_name, default)
+
+
+def strtobool(val):
+    if val.lower() in ('y', 'yes', 't', 'true', 'on', '1'):
+        return True
+    return False
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Build wheels for all the platforms.',
+        epilog=('Most options are supplied via environment variables. '
+                'See https://github.com/joerick/cibuildwheel#options for info.'))
+
+    parser.add_argument('--platform',
+                        choices=['auto', 'linux', 'macos', 'windows'],
+                        default=os.environ.get('CIBW_PLATFORM', 'auto'),
+                        help=('Platform to build for. For "linux" you need docker running, on Mac '
+                              'or Linux. For "macos", you need a Mac machine, and note that this '
+                              'script is going to automatically install MacPython on your system, '
+                              'so don\'t run on your development machine. For "windows", you need to '
+                              'run in Windows, and it will build and test for all versions of '
+                              'Python. Default: auto.'))
+    parser.add_argument('--output-dir',
+                        default=os.environ.get('CIBW_OUTPUT_DIR', 'wheelhouse'),
+                        help='Destination folder for the wheels.')
+    parser.add_argument('project_dir',
+                        default='.',
+                        nargs='?',
+                        help=('Path to the project that you want wheels for. Default: the current '
+                              'directory.'))
+
+    parser.add_argument('--print-build-identifiers',
+                        action='store_true',
+                        help='Print the build identifiers matched by the current invocation and exit.')
+
+    args = parser.parse_args()
+
+    detect_obsolete_options()
+    
+    if args.platform != 'auto':
+        platform = args.platform
+    else:
+        ci = strtobool(os.environ.get('CI', 'false')) or 'BITRISE_BUILD_NUMBER' in os.environ or 'AZURE_HTTP_USER_AGENT' in os.environ
+        if not ci:
+            print('cibuildwheel: Unable to detect platform. cibuildwheel should run on your CI server, '
+                  'Travis CI, AppVeyor, Azure Pipelines and CircleCI are supported. You can run on your '
+                  'development machine or other CI providers using the --platform argument. Check --help '
+                  'output for more information.',
+                  file=sys.stderr)
+            exit(2)
+        if sys.platform.startswith('linux'):
+            platform = 'linux'
+        elif sys.platform == 'darwin':
+            platform = 'macos'
+        elif sys.platform == 'win32':
+            platform = 'windows'
+        else:
+            print('cibuildwheel: Unable to detect platform from "sys.platform" in a CI environment. You can run '
+                  'cibuildwheel using the --platform argument. Check --help output for more information.',
+                  file=sys.stderr)
+            exit(2)
+
+
+    output_dir = args.output_dir
+    test_command = get_option_from_environment('CIBW_TEST_COMMAND', platform=platform)
+    test_requires = get_option_from_environment('CIBW_TEST_REQUIRES', platform=platform, default='').split()
+    test_extras = get_option_from_environment('CIBW_TEST_EXTRAS', platform=platform, default='')
+    project_dir = args.project_dir
+    before_build = get_option_from_environment('CIBW_BEFORE_BUILD', platform=platform)
+    build_verbosity = get_option_from_environment('CIBW_BUILD_VERBOSITY', platform=platform, default='')
+    build_config, skip_config = os.environ.get('CIBW_BUILD', '*'), os.environ.get('CIBW_SKIP', '')
+    if platform == 'linux':
+        repair_command_default = 'auditwheel repair -w {dest_dir} {wheel}'
+    elif platform == 'macos':
+        repair_command_default = 'delocate-listdeps {wheel} && delocate-wheel -w {dest_dir} {wheel}'
+    else:
+        repair_command_default = ''
+    repair_command = get_option_from_environment('CIBW_REPAIR_WHEEL_COMMAND', platform=platform, default=repair_command_default)
+    environment_config = get_option_from_environment('CIBW_ENVIRONMENT', platform=platform, default='')
+
+    if test_extras:
+        test_extras = '[{0}]'.format(test_extras)
+
+    try:
+        build_verbosity = min(3, max(-3, int(build_verbosity)))
+    except ValueError:
+        build_verbosity = 0
+
+    try:
+        environment = parse_environment(environment_config)
+    except (EnvironmentParseError, ValueError) as e:
+        print('cibuildwheel: Malformed environment option "%s"' % environment_config, file=sys.stderr)
+        import traceback
+        traceback.print_exc(None, sys.stderr)
+        exit(2)
+
+    build_selector = BuildSelector(build_config, skip_config)
+
+    # Add CIBUILDWHEEL environment variable
+    # This needs to be passed on to the docker container in linux.py
+    os.environ['CIBUILDWHEEL'] = '1'
+
+    if not os.path.exists(os.path.join(project_dir, 'setup.py')):
+        print('cibuildwheel: Could not find setup.py at root of project', file=sys.stderr)
+        exit(2)
+
+    if args.print_build_identifiers:
+        print_build_identifiers(platform, build_selector)
+        exit(0)
+
+    build_options = dict(
+        project_dir=project_dir,
+        output_dir=output_dir,
+        test_command=test_command,
+        test_requires=test_requires,
+        test_extras=test_extras,
+        before_build=before_build,
+        build_verbosity=build_verbosity,
+        build_selector=build_selector,
+        repair_command=repair_command,
+        environment=environment,
+    )
+
+    if platform == 'linux':
+        manylinux_x86_64_image = os.environ.get('CIBW_MANYLINUX_X86_64_IMAGE', 'manylinux2010')
+        manylinux_i686_image = os.environ.get('CIBW_MANYLINUX_I686_IMAGE', 'manylinux2010')
+
+        default_manylinux_images_x86_64 = {'manylinux1': 'quay.io/pypa/manylinux1_x86_64',
+                                           'manylinux2010': 'quay.io/pypa/manylinux2010_x86_64',
+                                           'manylinux2014': 'quay.io/pypa/manylinux2014_x86_64'}
+        default_manylinux_images_i686 = {'manylinux1': 'quay.io/pypa/manylinux1_i686',
+                                         'manylinux2010': 'quay.io/pypa/manylinux2010_i686',
+                                         'manylinux2014': 'quay.io/pypa/manylinux2014_i686'}
+
+        build_options.update(
+            manylinux_images={'x86_64': default_manylinux_images_x86_64.get(manylinux_x86_64_image) or manylinux_x86_64_image,
+                              'i686': default_manylinux_images_i686.get(manylinux_i686_image) or manylinux_i686_image},
+        )
+    elif platform == 'macos':
+        pass
+    elif platform == 'windows':
+        pass
+
+    # Python is buffering by default when running on the CI platforms, giving problems interleaving subprocess call output with unflushed calls to 'print'
+    sys.stdout = Unbuffered(sys.stdout)
+
+    print_preamble(platform, build_options)
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    if platform == 'linux':
+        cibuildwheel.linux.build(**build_options)
+    elif platform == 'windows':
+        cibuildwheel.windows.build(**build_options)
+    elif platform == 'macos':
+        cibuildwheel.macos.build(**build_options)
+    else:
+        print('cibuildwheel: Unsupported platform: {}'.format(platform), file=sys.stderr)
+        exit(2)
+
+
+def detect_obsolete_options():
+    # Check the old 'MANYLINUX1_*_IMAGE' options
+    for (deprecated, alternative) in [('CIBW_MANYLINUX1_X86_64_IMAGE', 'CIBW_MANYLINUX_X86_64_IMAGE'),
+                                      ('CIBW_MANYLINUX1_I686_IMAGE', 'CIBW_MANYLINUX_I686_IMAGE')]:
+        if deprecated in os.environ:
+            print("'{}' has been deprecated, and will be removed in a future release. Use the option '{}' instead.".format(deprecated, alternative))
+            if alternative not in os.environ:
+                print("Using value of option '{}' as replacement for '{}'".format(deprecated, alternative))
+                os.environ[alternative] = os.environ[deprecated]
+            else:
+                print("Option '{}' is not empty. Please unset '{}'".format(alternative, deprecated))
+                exit(2)
+
+    # Check for 'manylinux1' in the 'CIBW_BUILD' and 'CIBW_SKIP' options
+    for deprecated in ['CIBW_BUILD', 'CIBW_SKIP']:
+        if deprecated in os.environ and 'manylinux1' in os.environ[deprecated]:
+            print("Build identifiers with 'manylinux1' been deprecated. Replacing all occurences of 'manylinux1' by 'manylinux' in the option '{}'".format(deprecated))
+            os.environ[deprecated] = os.environ[deprecated].replace('manylinux1', 'manylinux')
+
+
+def print_preamble(platform, build_options):
+    print(textwrap.dedent('''
+             _ _       _ _   _       _           _
+         ___|_| |_ _ _|_| |_| |_ _ _| |_ ___ ___| |
+        |  _| | . | | | | | . | | | |   | -_| -_| |
+        |___|_|___|___|_|_|___|_____|_|_|___|___|_|
+        '''))
+
+    print('cibuildwheel version %s\n' % cibuildwheel.__version__)
+
+
+    print('Build options:')
+    print('  platform: %r' % platform)
+    for option, value in sorted(build_options.items()):
+        print('  %s: %r' % (option, value))
+
+    warnings = detect_warnings(platform, build_options)
+    if warnings:
+        print('\nWarnings:')
+        for warning in warnings:
+            print('  ' + warning)
+
+    print('\nHere we go!\n')
+
+
+def print_build_identifiers(platform, build_selector):
+    if platform == 'linux':
+        python_configurations = cibuildwheel.linux.get_python_configurations(build_selector)
+    elif platform == 'windows':
+        python_configurations = cibuildwheel.windows.get_python_configurations(build_selector)
+    elif platform == 'macos':
+        python_configurations = cibuildwheel.macos.get_python_configurations(build_selector)
+    else:
+        python_configurations = []
+
+    for config in python_configurations:
+        print(config.identifier)
+
+
+def detect_warnings(platform, build_options):
+    warnings = []
+
+    # warn about deprecated {python} and {pip}
+    for option_name in ['test_command', 'before_build']:
+        option_value = build_options.get(option_name)
+
+        if option_value:
+            if '{python}' in option_value or '{pip}' in option_value:
+                warnings.append(option_name + ": '{python}' and '{pip}' are no longer needed, and will be removed in a future release. Simply use 'python' or 'pip' instead.")
+
+    return warnings
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/cibuildwheel/cibuildwheel-1.1.0.tar.gz/cibuildwheel-1.1.0/cibuildwheel/windows.py
@@ -0,0 +1,195 @@
+from __future__ import print_function
+import os, tempfile, subprocess, shutil, sys
+from collections import namedtuple
+from glob import glob
+
+try:
+    from shlex import quote as shlex_quote
+except ImportError:
+    from pipes import quote as shlex_quote
+
+try:
+    from urllib.request import urlopen
+except ImportError:
+    from urllib2 import urlopen
+
+from .util import prepare_command, get_build_verbosity_extra_flags
+
+
+IS_RUNNING_ON_AZURE = os.path.exists('C:\\hostedtoolcache')
+IS_RUNNING_ON_TRAVIS = os.environ.get('TRAVIS_OS_NAME') == 'windows'
+
+
+def get_python_path(config):
+    nuget_args = get_nuget_args(config)
+    return os.path.join(nuget_args[-1], nuget_args[0] + "." + config.version, "tools")
+
+
+def get_nuget_args(configuration):
+    python_name = "python" if configuration.version[0] == '3' else "python2"
+    if configuration.arch == "32":
+        python_name = python_name + "x86"
+    return [python_name, "-Version", configuration.version, "-OutputDirectory", "C:/cibw/python"]
+
+def get_python_configurations(build_selector):
+    PythonConfiguration = namedtuple('PythonConfiguration', ['version', 'arch', 'identifier'])
+    python_configurations = [
+        PythonConfiguration(version='2.7.17', arch="32", identifier='cp27-win32'),
+        PythonConfiguration(version='2.7.17', arch="64", identifier='cp27-win_amd64'),
+        PythonConfiguration(version='3.5.4', arch="32", identifier='cp35-win32'),
+        PythonConfiguration(version='3.5.4', arch="64", identifier='cp35-win_amd64'),
+        PythonConfiguration(version='3.6.8', arch="32", identifier='cp36-win32'),
+        PythonConfiguration(version='3.6.8', arch="64", identifier='cp36-win_amd64'),
+        PythonConfiguration(version='3.7.5', arch="32", identifier='cp37-win32'),
+        PythonConfiguration(version='3.7.5', arch="64", identifier='cp37-win_amd64'),
+        PythonConfiguration(version='3.8.0', arch="32", identifier='cp38-win32'),
+        PythonConfiguration(version='3.8.0', arch="64", identifier='cp38-win_amd64'),
+    ]
+
+    if IS_RUNNING_ON_TRAVIS:
+        # cannot install VCForPython27.msi which is needed for compiling C software
+        # try with (and similar): msiexec /i VCForPython27.msi ALLUSERS=1 ACCEPT=YES /passive
+        python_configurations = [c for c in python_configurations if not c.version.startswith('2.7.')]
+
+     # skip builds as required
+    python_configurations = [c for c in python_configurations if build_selector(c.identifier)]
+
+    return python_configurations
+
+
+def build(project_dir, output_dir, test_command, test_requires, test_extras, before_build, build_verbosity, build_selector, repair_command, environment):
+    def simple_shell(args, env=None, cwd=None):
+        print('+ ' + ' '.join(args))
+        args = ['cmd', '/E:ON', '/V:ON', '/C'] + args
+        return subprocess.check_call(' '.join(args), env=env, cwd=cwd)
+    def download(url, dest):
+        print('+ Download ' + url + ' to ' + dest)
+        dest_dir = os.path.dirname(dest)
+        if not os.path.exists(dest_dir):
+            os.makedirs(dest_dir)
+        response = urlopen(url)
+        try:
+            with open(dest, 'wb') as file:
+                file.write(response.read())
+        finally:
+            response.close()
+    if IS_RUNNING_ON_AZURE or IS_RUNNING_ON_TRAVIS:
+        shell = simple_shell
+    else:
+        run_with_env = os.path.abspath(os.path.join(os.path.dirname(__file__), 'resources', 'appveyor_run_with_env.cmd'))
+
+        # run_with_env is a cmd file that sets the right environment variables
+        # to build on AppVeyor.
+        def shell(args, env=None, cwd=None):
+            # print the command executing for the logs
+            print('+ ' + ' '.join(args))
+            args = ['cmd', '/E:ON', '/V:ON', '/C', run_with_env] + args
+            return subprocess.check_call(' '.join(args), env=env, cwd=cwd)
+
+    abs_project_dir = os.path.abspath(project_dir)
+    temp_dir = tempfile.mkdtemp(prefix='cibuildwheel')
+    built_wheel_dir = os.path.join(temp_dir, 'built_wheel')
+    repaired_wheel_dir = os.path.join(temp_dir, 'repaired_wheel')
+
+    # install nuget as best way to provide python
+    nuget = 'C:\\cibw\\nuget.exe'
+    download('https://dist.nuget.org/win-x86-commandline/latest/nuget.exe', nuget)
+    # get pip fo this installation which not have.
+    get_pip_script = 'C:\\cibw\\get-pip.py'
+    download('https://bootstrap.pypa.io/get-pip.py', get_pip_script)
+
+    python_configurations = get_python_configurations(build_selector)
+    for config in python_configurations:
+        # install Python
+        config_python_path = get_python_path(config)
+        simple_shell([nuget, "install"] + get_nuget_args(config))
+        assert os.path.exists(os.path.join(config_python_path, 'python.exe'))
+
+        # set up PATH and environment variables for run_with_env
+        env = os.environ.copy()
+        env['PYTHON_VERSION'] = config.version
+        env['PYTHON_ARCH'] = config.arch
+        env['PATH'] = os.pathsep.join([
+            config_python_path,
+            os.path.join(config_python_path, 'Scripts'),
+            env['PATH']
+        ])
+        # update env with results from CIBW_ENVIRONMENT
+        env = environment.as_dictionary(prev_environment=env)
+
+        # for the logs - check we're running the right version of python
+        simple_shell(['where', 'python'], env=env)
+        simple_shell(['python', '--version'], env=env)
+        simple_shell(['python', '-c', '"import struct; print(struct.calcsize(\'P\') * 8)\"'], env=env)
+
+        # make sure pip is installed
+        if not os.path.exists(os.path.join(config_python_path, 'Scripts', 'pip.exe')):
+            simple_shell(['python', get_pip_script], env=env, cwd="C:\\cibw")
+        assert os.path.exists(os.path.join(config_python_path, 'Scripts', 'pip.exe'))
+
+        # prepare the Python environment
+        simple_shell(['python', '-m', 'pip', 'install', '--upgrade', 'pip'], env=env)
+        simple_shell(['pip', '--version'], env=env)
+        simple_shell(['pip', 'install', '--upgrade', 'setuptools', 'wheel'], env=env)
+
+        # run the before_build command
+        if before_build:
+            before_build_prepared = prepare_command(before_build, project=abs_project_dir)
+            shell([before_build_prepared], env=env)
+
+        # build the wheel
+        if os.path.exists(built_wheel_dir):
+            shutil.rmtree(built_wheel_dir)
+        os.makedirs(built_wheel_dir)
+        shell(['pip', 'wheel', abs_project_dir, '-w', built_wheel_dir, '--no-deps'] + get_build_verbosity_extra_flags(build_verbosity), env=env)
+        built_wheel = glob(os.path.join(built_wheel_dir, '*.whl'))[0]
+
+        # repair the wheel
+        if os.path.exists(repaired_wheel_dir):
+            shutil.rmtree(repaired_wheel_dir)
+        os.makedirs(repaired_wheel_dir)
+        if built_wheel.endswith('none-any.whl') or not repair_command:
+            # pure Python wheel or empty repair command
+            shutil.move(built_wheel, repaired_wheel_dir)
+        else:
+            repair_command_prepared = prepare_command(repair_command, wheel=built_wheel, dest_dir=repaired_wheel_dir)
+            shell([repair_command_prepared], env=env)
+        repaired_wheel = glob(os.path.join(repaired_wheel_dir, '*.whl'))[0]
+
+        if test_command:
+            # set up a virtual environment to install and test from, to make sure
+            # there are no dependencies that were pulled in at build time.
+            shell(['pip', 'install', 'virtualenv'], env=env)
+            venv_dir = tempfile.mkdtemp()
+            shell(['python', '-m', 'virtualenv', venv_dir], env=env)
+
+            virtualenv_env = env.copy()
+            virtualenv_env['PATH'] = os.pathsep.join([
+                os.path.join(venv_dir, 'Scripts'),
+                virtualenv_env['PATH'],
+            ])
+
+            # check that we are using the Python from the virtual environment
+            shell(['which', 'python'], env=virtualenv_env)
+
+            # install the wheel
+            shell(['pip', 'install', repaired_wheel + test_extras], env=virtualenv_env)
+
+            # test the wheel
+            if test_requires:
+                shell(['pip', 'install'] + test_requires, env=virtualenv_env)
+
+            # run the tests from c:\, with an absolute path in the command
+            # (this ensures that Python runs the tests against the installed wheel
+            # and not the repo code)
+            test_command_prepared = prepare_command(test_command, project=abs_project_dir)
+            shell([test_command_prepared], cwd='c:\\', env=virtualenv_env)
+
+            # clean up
+            shutil.rmtree(venv_dir)
+
+        # we're all done here; move it to output (remove if already exists)
+        dst = os.path.join(output_dir, os.path.basename(repaired_wheel))
+        if os.path.isfile(dst):
+            os.remove(dst)
+        shutil.move(repaired_wheel, dst)
