92bba325ce7548c5c5964287ce7556845c9ff47a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:53 +0000

    Add package refnx/0.1.10/refnx-0.1.10-cp35-cp35m-manylinux2010_x86_64.whl

---
+++ packages/refnx/refnx-0.1.10-cp35-cp35m-manylinux2010_x86_64.whl/refnx/version.py
@@ -0,0 +1,9 @@
+
+# THIS FILE IS GENERATED FROM REFNX SETUP.PY
+short_version = '0.1.10'
+version = '0.1.10'
+full_version = '0.1.10'
+git_revision = 'e1c25d2745a73293e74d4d34f6846b0686ee900e'
+release = True
+if not release:
+    version = full_version
