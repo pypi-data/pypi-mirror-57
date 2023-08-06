73cd72ff214260c98027c35dfe32cc1397933d06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:23 +0000

    Add package georss-generic-client/0.3/georss_generic_client-0.3.tar.gz

---
+++ packages/georss-generic-client/georss_generic_client-0.3.tar.gz/georss_generic_client-0.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+REQUIRES = [
+    'georss_client>=0.10',
+]
+
+setup(
+    name="georss_generic_client",
+    version="0.3",
+    author="Malte Franken",
+    author_email="coding@subspace.de",
+    description="A GeoRSS generic client library.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/exxamalte/python-georss-generic-client",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=REQUIRES
+)
