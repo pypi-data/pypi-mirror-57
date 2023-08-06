b50ae77e41e1bda29cbf8396cfd02580673a0800
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:13 +0000

    Add package cappy239/0.0.13/cappy239-0.0.13-py3-none-any.whl

---
+++ packages/cappy239/cappy239-0.0.13-py3-none-any.whl/cappy239/__version__.py
@@ -0,0 +1,18 @@
+#   ______       _       _______  _______  ____  ____  _____   ______    ______   
+# .' ___  |     / \     |_   __ \|_   __ \|_  _||_  _|/ ___ `./ ____ `..' ____ '. 
+#/ .'   \_|    / _ \      | |__) | | |__) | \ \  / / |_/___) |`'  __) || (____) | 
+#| |          / ___ \     |  ___/  |  ___/   \ \/ /   .'____.'_  |__ '.'_.____. | 
+#\ `.___.'\ _/ /   \ \_  _| |_    _| |_      _|  |_  / /_____| \____) || \____| | 
+# `.____ .'|____| |____||_____|  |_____|    |______| |_______|\______.' \______,' 
+#                                                           By Adriano P. Almeida                                                                                 
+
+__title__ = 'cappy239'
+__description__ = 'Algorithms Computational Mathematics I: Spectral Analysis of Stochastic Processes taught by Dr Reinaldo Roberto Rosa at National Institute for Spacial Research (INPE).'
+__url__ = 'https://github.com/AdrianoPereira/cappy239'
+__version__ = '0.0.13'
+__build__ = 0x022100
+__author__ = 'Adriano P. Almeida'
+__author_email__ = 'adriano.almeida@inpe.br'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2019 INPE'
+__cake__ = u'\u2728 \U0001f370 \u2728'
