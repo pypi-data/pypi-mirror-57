b635adb0844cf29aebb26bf8d23b6f0f9e9b23be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:37 +0000

    Add package mypy-boto3-codedeploy/1.10.33/mypy-boto3-codedeploy-1.10.33.tar.gz

---
+++ packages/mypy-boto3-codedeploy/mypy-boto3-codedeploy-1.10.33.tar.gz/mypy-boto3-codedeploy-1.10.33/mypy_boto3_codedeploy/waiter.py
@@ -0,0 +1,22 @@
+"Main interface for codedeploy service Waiters"
+from __future__ import annotations
+
+from botocore.waiter import Waiter as Boto3Waiter
+from mypy_boto3_codedeploy.type_defs import DeploymentSuccessfulWaitWaiterConfigTypeDef
+
+
+__all__ = ("DeploymentSuccessfulWaiter",)
+
+
+class DeploymentSuccessfulWaiter(Boto3Waiter):
+    """
+    Waiter for `deployment_successful` name.
+    """
+
+    # pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+    def wait(
+        self, deploymentId: str, WaiterConfig: DeploymentSuccessfulWaitWaiterConfigTypeDef = None
+    ) -> None:
+        """
+        [deployment_successful.wait documentation](https://boto3.amazonaws.com/v1/documentation/api/1.10.33/reference/services/codedeploy.html#CodeDeploy.Waiter.deployment_successful.wait)
+        """
