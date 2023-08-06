12d557728ebab6171e7318f3f6b5415ef614794c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:20 +0000

    Add package vk-botting/0.4.1/vk_botting-0.4.1-py3-none-any.whl

---
+++ packages/vk-botting/vk_botting-0.4.1-py3-none-any.whl/vk_botting/utils.py
@@ -0,0 +1,30 @@
+from inspect import isawaitable
+import json
+
+
+async def async_all(gen, *, check=isawaitable):
+    for elem in gen:
+        if check(elem):
+            elem = await elem
+        if not elem:
+            return False
+    return True
+
+
+async def maybe_coroutine(f, *args, **kwargs):
+    value = f(*args, **kwargs)
+    if isawaitable(value):
+        return await value
+    else:
+        return value
+
+
+def find(predicate, seq):
+    for element in seq:
+        if predicate(element):
+            return element
+    return None
+
+
+def to_json(obj):
+    return json.dumps(obj, separators=(',', ':'), ensure_ascii=True)
