fa51e5969c1b41dc526ed22b0699f83659f9477e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:39 +0000

    Add package nervaluate/0.1.6/nervaluate-0.1.6-py3-none-any.whl

---
+++ packages/nervaluate/nervaluate-0.1.6-py3-none-any.whl/nervaluate/__version__.py
@@ -0,0 +1,7 @@
+__name__="nervaluate"
+__version__="0.1.6"
+__description__="NER evaluation done right"
+__url__="https://pypi.org/project/nervaluate"
+__author__="David Batista, Matthew Upson"
+__author_email__="david.batista@gmail.com,matthew.a.upson@gmail.com"
+__license__="GNU GPL v3"
