483caffd1adb7e612b319dadbeca7b486df9196d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:30 +0000

    Add package autox/0.0.7/autox-0.0.7-py3-none-any.whl

---
+++ packages/autox/autox-0.0.7-py3-none-any.whl/autox/run.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+# 执行包：run regression cases begin with "tezign*.json"
+
+import os
+import autox.core.myemail as email
+import autox.util.common as common
+
+if os.path.exists(os.getcwd()+'/log'):
+
+    # 每次执行前清空错误日志
+    if os.path.isfile(os.getcwd()+'/log/error.log'):
+        f = open(os.getcwd()+'/log/error.log', 'r+')
+        f.truncate()
+    else:
+        pass
+else:
+    os.mkdir(os.getcwd()+'/log')
+
+report_path = os.getcwd()+'/report'
+
+if os.path.exists(report_path):
+    if os.path.isfile(report_path + '/测试报告.html'):
+        os.remove(report_path + '/测试报告.html')
+    else:
+        pass
+else:
+    os.mkdir(os.getcwd()+'/report')
+
+if os.path.exists(os.getcwd()+'/model'):
+    pass
+else:
+    os.mkdir(os.getcwd()+'/model')
+
+regTest = common.MyRegression()
+
+
+def run(scenario_list):
+    report = regTest.build_report(scenario_list)
+    return report
+    # email.email(report)
+
+
+
+
+
+
+
+
+
+
+
+
