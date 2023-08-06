f64b47c2465ac35451a3e9478ad0a5d8eef144aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:26 +0000

    Add package alegra/0.1.1/alegra-0.1.1-py3-none-any.whl

---
+++ packages/alegra/alegra-0.1.1-py3-none-any.whl/alegra/__init__.py
@@ -0,0 +1,11 @@
+from alegra.resources import Contact
+from alegra.resources import Invoice
+from alegra.resources import Item
+from alegra.resources import Retention
+from alegra.resources import Tax
+
+
+user = None
+token = None
+api_base = "https://api.alegra.com/api"
+api_version = "v1"
