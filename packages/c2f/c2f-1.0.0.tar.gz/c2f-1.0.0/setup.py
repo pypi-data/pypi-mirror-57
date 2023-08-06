7243281ed78c676deb1d0a537d3d99a9e7dc66ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:53 +0000

    Add package c2f/1.0.0/c2f-1.0.0.tar.gz

---
+++ packages/c2f/c2f-1.0.0.tar.gz/c2f-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+from Cython.Build import cythonize
+
+setup(
+    name='c2f',
+    version='1.0.0',
+    py_modules=['c2f'],
+    ext_modules=cythonize('c2f.py'),
+    long_description=open('README.rst').read(),
+    long_description_content_type='text/plain',
+)
