067afbd70932547502468decb679fb4cab16d509
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:01 +0000

    Add package skutil/0.0.16/skutil-0.0.16-py2.py3-none-any.whl

---
+++ packages/skutil/skutil-0.0.16-py2.py3-none-any.whl/skutil/estimators/__init__.py
@@ -0,0 +1,21 @@
+from .classifier_map import (  # noqa: F401
+    classifier_cls_by_name,
+    classifier_by_params,
+)
+from .col_ignore_clf import (  # noqa: F401
+    IxColIgnoringClassifier,
+    ObjColIgnoringClassifier,
+    PatternColIgnoringClassifier,
+)
+
+for name in [
+    'col_ignore_clf', 'classifier_map', 'name',
+]:
+    try:
+        globals().pop(name)
+    except KeyError:
+        pass
+try:
+    del name  # pylint: disable=W0631
+except NameError:
+    pass
