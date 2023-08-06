e360bc4e7daa58cebbc4b2fe5dac83ddb92bfa35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:38 +0000

    Add package eadf/0.3b0/eadf-0.3b0.tar.gz

---
+++ packages/eadf/eadf-0.3b0.tar.gz/eadf-0.3b0/test/test_core.py
@@ -0,0 +1,32 @@
+# Copyright 2019 S. Pawar, S. Semper
+#     https://www.tu-ilmenau.de/it-ems/
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from eadf.core import evaluatePattern
+from eadf.core import evaluateGradient
+from eadf.core import evaluateHessian
+from eadf.core import fourierToSampled
+from eadf.core import regularSamplingToGrid
+from eadf.core import sampledToFourier
+from eadf.core import symmetrizeData
+from eadf.arrays import generateUCA
+from eadf.eadf import EADF
+import numpy as np
+import unittest
+from unittest.mock import patch
+
+
+if __name__ == "__main__":
+    unittest.main()
