60880ee21d9bc4bb5d1b2104c27076fddb37e9bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:23 +0000

    Add package label-maker/0.7.0/label-maker-0.7.0.tar.gz

---
+++ packages/label-maker/label-maker-0.7.0.tar.gz/label-maker-0.7.0/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+from os import path as op
+import io
+from imp import load_source
+from setuptools import setup, find_packages
+
+__version__ = load_source('label_maker.version', 'label_maker/version.py').__version__
+
+here = op.abspath(op.dirname(__file__))
+
+# get the dependencies and installs
+with io.open(op.join(here, 'requirements.txt'), encoding='utf-8') as f:
+    all_reqs = f.read().split('\n')
+
+install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
+dependency_links = [x.strip().replace('git+', '') for x in all_reqs if 'git+' not in x]
+
+# readme
+with open('README.md') as f:
+    readme = f.read()
+
+setup(
+    name='label-maker',
+    author='Drew Bollinger',
+    author_email='drew@developmentseed.org',
+    version=__version__,
+    description='Data preparation for satellite machine learning',
+    url='https://github.com/developmentseed/label-maker/',
+    license='MIT',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6'
+    ],
+    keywords='',
+    entry_points={
+        'console_scripts': ['label-maker=label_maker.main:cli'],
+    },
+    packages=find_packages(exclude=['docs', 'tests*']),
+    include_package_data=True,
+    install_requires=install_requires,
+    dependency_links=dependency_links,
+    long_description=readme,
+    long_description_content_type="text/markdown"
+)
