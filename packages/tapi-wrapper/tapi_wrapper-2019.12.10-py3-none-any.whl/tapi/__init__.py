810969c505f4d3214c32092766de3d02d3cf74a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:18 +0000

    Add package tapi-wrapper/2019.12.10/tapi_wrapper-2019.12.10-py3-none-any.whl

---
+++ packages/tapi-wrapper/tapi_wrapper-2019.12.10-py3-none-any.whl/tapi/__init__.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+
+from __future__ import unicode_literals
+
+__author__ = 'Pavel Maksimov'
+__email__ = 'vur21@ya.ru'
+__version__ = '2019.12.10'
+
+
+from .adapters import (
+    generate_wrapper_from_adapter,
+    TapiAdapter,
+    FormAdapterMixin, JSONAdapterMixin)
