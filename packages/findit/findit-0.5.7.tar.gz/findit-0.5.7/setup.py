5383a1046cd433bde4c849cb40f49f8f116015ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:39 +0000

    Add package findit/0.5.7/findit-0.5.7.tar.gz

---
+++ packages/findit/findit-0.5.7.tar.gz/findit-0.5.7/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+
+install_requirement_list = [
+    "opencv-python",
+    "imutils",
+    "numpy",
+    "loguru",
+    "opencv-contrib-python==3.4.2.17",
+    "scikit-learn",
+    "scikit-image",
+    "scipy",
+]
+
+extras_require_dict = {"web": ["flask", "gevent"]}
+
+setup(
+    name="findit",
+    version="0.5.7",
+    description="find target icon on your picture, and get its position",
+    author="williamfzc",
+    author_email="fengzc@vip.qq.com",
+    url="https://github.com/williamfzc/findit",
+    packages=find_packages(),
+    python_requires=">=3.6,<3.8",
+    install_requires=install_requirement_list,
+    extras_require=extras_require_dict,
+)
