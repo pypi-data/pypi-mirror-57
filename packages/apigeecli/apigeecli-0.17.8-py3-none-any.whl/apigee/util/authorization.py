b6eadf17be0280a8e655a03fca42f94df4481957
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:41 +0000

    Add package apigeecli/0.17.8/apigeecli-0.17.8-py3-none-any.whl

---
+++ packages/apigeecli/apigeecli-0.17.8-py3-none-any.whl/apigee/util/authorization.py
@@ -0,0 +1,23 @@
+import configparser
+import base64
+
+from apigee import APIGEE_CLI_CREDENTIALS_FILE
+from apigee.util import mfa_with_pyotp
+
+def set_header(hdrs, args):
+    if hdrs is None:
+        hdrs = dict()
+    if args.mfa_secret:
+        hdrs['Authorization'] = 'Bearer ' + mfa_with_pyotp.get_access_token(args)
+    else:
+        hdrs['Authorization'] = 'Basic ' + base64.b64encode((args.username + ':' + args.password).encode()).decode()
+    return hdrs
+
+def get_credential(section, key):
+    try:
+        config = configparser.ConfigParser()
+        config.read(APIGEE_CLI_CREDENTIALS_FILE)
+        if section in config:
+            return config[section][key]
+    except:
+        return None
