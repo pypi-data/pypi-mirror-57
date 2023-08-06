471f7276a3856a55068b67d38cfd8b4d06ce5c50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:32 +0000

    Add package pipeline-exec/0.3/pipeline-exec-0.3.tar.gz

---
+++ packages/pipeline-exec/pipeline-exec-0.3.tar.gz/pipeline-exec-0.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='pipeline-exec',
+    version='0.3',
+    packages=['pipeline_exec'],
+    url='https://github.com/Sikilabs/pipeline_exec',
+    license='MIT',
+    author='Sikilabs Technologies Inc',
+    author_email='info@sikilabs.com',
+    description='A Pipeline to process an Object Collection',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
