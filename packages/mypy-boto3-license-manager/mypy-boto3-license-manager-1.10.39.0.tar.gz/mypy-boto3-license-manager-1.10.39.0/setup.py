d0438ffd4aab483783a032d92e20e2cd9898f85b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:16 +0000

    Add package mypy-boto3-license-manager/1.10.39.0/mypy-boto3-license-manager-1.10.39.0.tar.gz

---
+++ packages/mypy-boto3-license-manager/mypy-boto3-license-manager-1.10.39.0.tar.gz/mypy-boto3-license-manager-1.10.39.0/setup.py
@@ -0,0 +1,52 @@
+from setuptools import setup
+from setuptools.command.install import install
+
+
+LONG_DESCRIPTION = """
+See [mypy_boto3](https://pypi.org/project/mypy-boto3/) for more info.
+"""
+
+
+class PostInstallCommand(install):
+    def run(self):
+        install.run(self)
+        try:
+            from mypy_boto3.main import maybe_build_index
+
+            maybe_build_index()
+        except:
+            pass
+
+
+setup(
+    name="mypy-boto3-license-manager",
+    version="1.10.39.0",
+    packages=["mypy_boto3_license_manager"],
+    url="https://github.com/vemel/mypy_boto3",
+    license="MIT License",
+    author="Vlad Emelianov",
+    author_email="vlad.emelianov.nz@gmail.com",
+    description="Mypy-friendly boto3 1.10.39 type annotations for license-manager service.",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Environment :: Console",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Typing :: Typed",
+    ],
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    package_data={"mypy_boto3_license_manager": ["py.typed"]},
+    install_requires=["typing_extensions; python_version < '3.8'", "mypy-boto3==1.10.39.0",],
+    zip_safe=False,
+    cmdclass={"install": PostInstallCommand,},
+)
