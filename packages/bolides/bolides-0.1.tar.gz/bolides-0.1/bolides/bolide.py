82a286cd96ad7dc36434cb62e33cf48d670208d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:27 +0000

    Add package bolides/0.1/bolides-0.1.tar.gz

---
+++ packages/bolides/bolides-0.1.tar.gz/bolides-0.1/bolides/bolide.py
@@ -0,0 +1,32 @@
+import requests
+
+from . import API_ENDPOINT_EVENT
+
+
+class Bolide():
+
+    def __init__(self, eventid):
+        self.eventid = eventid
+        self.json = self._load_json(eventid)['data'][0]
+
+    def _load_json(self, eventid):
+        url = f"{API_ENDPOINT_EVENT}/{eventid}"
+        r = requests.get(url)
+        return r.json()
+    
+    @property
+    def latitude(self):
+        return self.json['latitude']
+
+    @property
+    def longitude(self):
+        return self.json['longitude']
+
+    @property
+    def datetime(self):
+        return self.json['datetime']
+
+    @property
+    def geodata(self):
+        return [self.json['attachments'][idx]['geoData']
+                for idx in range(len(self.json['attachments']))]
