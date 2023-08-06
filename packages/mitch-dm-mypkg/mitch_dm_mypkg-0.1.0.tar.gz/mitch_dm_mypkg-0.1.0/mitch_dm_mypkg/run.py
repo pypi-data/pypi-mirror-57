d8f0f197901f10a610652e9cb83b7a9056ba9cc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:47 +0000

    Add package mitch-dm-mypkg/0.1.0/mitch_dm_mypkg-0.1.0.tar.gz

---
+++ packages/mitch-dm-mypkg/mitch_dm_mypkg-0.1.0.tar.gz/mitch_dm_mypkg-0.1.0/mitch_dm_mypkg/run.py
@@ -0,0 +1,4 @@
+import boto3
+import click
+
+print("hello world")
---
+++ packages/mitch-dm-mypkg/mitch_dm_mypkg-0.1.0.tar.gz/mitch_dm_mypkg-0.1.0/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['mitch_dm_mypkg']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['boto3>=1.10,<2.0', 'click>=7.0,<8.0']
+
+setup_kwargs = {
+    'name': 'mitch-dm-mypkg',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Mitch Della Marta',
+    'author_email': 'mitchdellamarta@gmail.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
