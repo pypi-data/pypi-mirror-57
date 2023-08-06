50bd7a56bc132e37155c703cd9b93242e2636b8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:24 +0000

    Add package plone.app.contentlisting/1.3.4/plone.app.contentlisting-1.3.4.tar.gz

---
+++ packages/plone.app.contentlisting/plone.app.contentlisting-1.3.4.tar.gz/plone.app.contentlisting-1.3.4/setup.py
@@ -0,0 +1,52 @@
+from setuptools import setup, find_packages
+import os
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+version = '1.3.4'
+
+long_description = '{0}\n\n{1}'.format(
+    read('README.rst'),
+    read('CHANGES.rst')
+)
+
+setup(
+    name='plone.app.contentlisting',
+    version=version,
+    description="Listing of content for the Plone CMS",
+    long_description=long_description,
+    classifiers=[
+        "Framework :: Plone",
+        "Framework :: Plone :: 4.3",
+        "Framework :: Plone :: 5.1",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+    ],
+    keywords='content list Plone',
+    author='Plone Foundation',
+    author_email='plone-developers@lists.sourceforge.net',
+    url='https://pypi.python.org/pypi/plone.app.contentlisting',
+    license='GPL version 2',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['plone', 'plone.app'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'Products.CMFPlone',
+    ],
+    extras_require={
+        'test': [
+            'plone.app.contenttypes',
+            'plone.app.testing',
+            'plone.app.robotframework',
+        ],
+    },
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
