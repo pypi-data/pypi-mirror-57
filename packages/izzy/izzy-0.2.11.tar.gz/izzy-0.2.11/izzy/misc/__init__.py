96942ff673a4f249957fbf7741383c2da9c7111b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:54 +0000

    Add package izzy/0.2.11/izzy-0.2.11.tar.gz

---
+++ packages/izzy/izzy-0.2.11.tar.gz/izzy-0.2.11/izzy/misc/__init__.py
@@ -0,0 +1,16 @@
+
+from .arraylike import ArrayLike
+from .Refunction import *
+from .functions import *
+
+__all__ = [
+    'array_to_series',
+    'ArrayLike',
+    'assert_type',
+    'equal',
+    'flag_numeric',
+    'Refunction',
+    'get_name',
+    'pivot',
+    'safe_as_float'
+]
