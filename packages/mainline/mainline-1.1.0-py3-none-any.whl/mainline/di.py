890acbb63ce4485b7ff74be9b94da191d365a05f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:27 +0000

    Add package mainline/1.1.0/mainline-1.1.0-py3-none-any.whl

---
+++ packages/mainline/mainline-1.1.0-py3-none-any.whl/mainline/di.py
@@ -0,0 +1,270 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import functools
+
+from mainline.catalog import ICatalog, Catalog
+from mainline.exceptions import UnresolvableError
+from mainline.injection import ClassPropertyInjector, AutoSpecInjector, SpecInjector
+from mainline.scope import ScopeRegistry, NoneScope, GlobalScope
+from mainline.provider import Provider, provider_factory
+
+_sentinel = object()
+
+
+class Di(ICatalog):
+    '''
+    Dependency injection container.
+    '''
+    scopes = ScopeRegistry()
+
+    # Hook these in for usability
+    provider = staticmethod(provider_factory)
+    Catalog = Catalog
+    Provider = Provider
+
+    # alias
+    p = provider
+
+    def __init__(self, providers_factory=Catalog, dependencies_factory=dict):
+        self._providers = providers_factory()
+        self._dependencies = dependencies_factory()
+        super(Di, self).__init__()
+
+    @property
+    def providers(self):
+        '''
+        Public attribute for provider mapping
+        '''
+        return self._providers
+
+    @property
+    def dependencies(self):
+        '''
+        Public attribute for dependency mapping
+        '''
+        return self._dependencies
+
+    def update(self, catalog=None, dependencies=None, allow_overwrite=False):
+        '''
+        Convenience method to update this Di instance with the specified contents.
+
+        :param catalog: ICatalog supporting class or mapping
+        :type catalog: ICatalog or collections.Mapping
+        :param dependencies: Mapping of dependencies
+        :type dependencies: collections.Mapping
+        :param allow_overwrite: If True, allow overwriting existing keys. Only applies to providers.
+        :type allow_overwrite: bool
+        '''
+        if catalog:
+            self._providers.update(catalog, allow_overwrite=allow_overwrite)
+        if dependencies:
+            self._dependencies.update(dependencies)
+
+    def get_deps(self, obj):
+        '''
+        Returns dependencies for key.
+
+        :param key: Dependent holder key
+        :type key: object
+        :return: Dependencies
+        :rtype: set
+        '''
+        return self._dependencies.get(obj, set())
+
+    def get_missing_deps(self, obj):
+        '''
+        Returns missing dependencies for provider key.
+        Missing meaning no instance can be provided at this time.
+
+        :param key: Provider key
+        :type key: object
+        :return: Missing dependencies
+        :rtype: list
+        '''
+        deps = self.get_deps(obj)
+        ret = []
+        for key in deps:
+            provider = self._providers.get(key)
+            if provider and provider.providable:
+                continue
+            ret.append(key)
+        return ret
+
+    def iresolve(self, *keys):
+        '''
+        Iterates over resolved instances for given provider keys.
+
+        :param keys: Provider keys
+        :type keys: tuple
+        :return: Iterator of resolved instances
+        :rtype: generator
+        '''
+        for key in keys:
+            missing = self.get_missing_deps(key)
+            if missing:
+                raise UnresolvableError("Missing dependencies for %s: %s" % (key, missing))
+
+            provider = self._providers.get(key)
+            if not provider:
+                raise UnresolvableError("Provider does not exist for %s" % key)
+
+            yield provider()
+
+    def resolve(self, *keys):
+        '''
+        Returns resolved instances for given provider keys.
+
+        If only one positional argument is given, only one is returned.
+
+        :param keys: Provider keys
+        :type keys: tuple
+        :return: Resolved instance(s); if only one key given, otherwise list of them.
+        :rtype: object or list
+        '''
+        instances = list(self.iresolve(*keys))
+        if len(keys) == 1:
+            return instances[0]
+        return instances
+
+    def resolve_deps(self, obj):
+        '''
+        Returns list of resolved dependencies for given obj.
+
+        :param obj: Object to lookup dependencies for
+        :type obj: object
+        :return: Resolved dependencies
+        :rtype: list
+        '''
+        deps = self.get_deps(obj)
+        return list(self.iresolve(*deps))
+
+    def register_factory(self, key, factory=_sentinel, scope=NoneScope, allow_overwrite=False):
+        '''
+        Creates and registers a provider using the given key, factory, and scope.
+        Can also be used as a decorator.
+
+        :param key: Provider key
+        :type key:  object
+        :param factory: Factory callable
+        :type factory: callable
+        :param scope: Scope key, factory, or instance
+        :type scope: object or callable
+        :return: Factory (or None if we're creating a provider without a factory)
+        :rtype: callable or None
+        '''
+        if factory is _sentinel:
+            return functools.partial(self.register_factory, key, scope=scope, allow_overwrite=allow_overwrite)
+        if not allow_overwrite and key in self._providers:
+            raise KeyError("Key %s already exists" % key)
+        provider = self.provider(factory, scope)
+        self._providers[key] = provider
+        return factory
+
+    f = register_factory
+
+    def set_instance(self, key, instance, default_scope=GlobalScope):
+        '''
+        Sets instance under specified provider key. If a provider for specified key does not exist, one is created
+        without a factory using the given scope.
+
+        :param key: Provider key
+        :type key: object
+        :param instance: Instance
+        :type instance: object
+        :param default_scope: Scope key, factory, or instance
+        :type default_scope: object or callable
+        '''
+        if key not in self._providers:
+            # We don't know how to create this kind of instance at this time, so add it without a factory.
+            factory = None
+            self.register_factory(key, factory, default_scope)
+        self._providers[key].set_instance(instance)
+
+    def depends_on(self, *keys):
+        '''
+        Decorator that marks the wrapped as depending on specified provider keys.
+
+        :param keys: Provider keys to mark as dependencies for wrapped
+        :type keys: tuple
+        :return: decorator
+        :rtype: decorator
+        '''
+
+        def decorator(wrapped):
+            if keys:
+                if wrapped not in self._dependencies:
+                    self._dependencies[wrapped] = set()
+                self._dependencies[wrapped].update(keys)
+            return wrapped
+
+        return decorator
+
+    def inject_classproperty(self, key, name=None, replace_on_access=False):
+        '''
+        Decorator that injects the specified key as a classproperty.
+
+        If replace_on_access is True, then it replaces itself with the instance on first lookup.
+
+        :param key: Provider key
+        :type key: object
+        :param name: Name of classproperty, defaults to key
+        :type name: str
+        :param replace_on_access: If True, replace the classproperty with the actual value on first lookup
+        :type replace_on_access: bool
+        '''
+        return ClassPropertyInjector(self, key, name=name, replace_on_access=replace_on_access)
+
+    def inject_exact(self, *args, **kwargs):
+        '''
+        Decorator that injects the specified arguments when the wrapped is called. Argspec is modified on the wrapped
+        accordingly.
+
+        Positional arguments are injected in the order they are given.
+
+        Keyword arguments are injected with the given key name, eg omg='whoa' would inject as omg=<whoa instance>.
+
+        Any specified provider args are added as dependencies for the wrapped.
+
+        :param args: Provider args to inject as positional arguments
+        :type args: tuple
+        :param kwargs: Mapping of keyword argument name to provider args to inject as keyword arguments
+        :type kwargs: dict
+        :return: decorator
+        :rtype: decorator
+        '''
+        return SpecInjector(self, *args, **kwargs)
+
+    def auto_inject(self, *args, **kwargs):
+        '''
+        Decorator that magically inspects the argspec of the wrapped upon call, injecting provider instances as names
+        match.
+
+        Performance wise, this is currently slower than `inject_exact` as more is done each runtime to determine what
+        needs to be injected.
+
+        Positional arguments are added as dependencies to wrapped.
+
+        Keyword arguments are handled similarly to inject(), being a mapping of keyword argument name to provider key.
+
+        Any specified provider args are added as dependencies for the wrapped.
+
+        If dependencies are associated with the wrapped, only those are checked for match in the argspec.
+
+        :param args: Provider args to inject as positional arguments
+        :type args: tuple
+        :param kwargs: Mapping of keyword argument name to provider args to inject as keyword arguments
+        :type kwargs: dict
+        :return: decorator
+        :rtype: decorator
+        '''
+        return AutoSpecInjector(self, *args, **kwargs)
+
+    inject = inject_exact
+
+    # This makes us callable, default to inject_exact for now.
+    __call__ = inject
+
+    # aliases
+    i = inject
+    ai = auto_inject
+
---
+++ packages/mainline/mainline-1.1.0-py3-none-any.whl/mainline/scope.py
@@ -0,0 +1,204 @@
+import os
+import threading
+import collections
+
+from mainline.utils import ProxyMutableMapping
+
+_sentinel = object()
+
+
+class IScope(ProxyMutableMapping):
+    register = False
+    name = None
+
+    instances = None
+    instances_factory = dict
+
+    def __init__(self, *args, **kwargs):
+        if self.instances is None:
+            self.instances = self.instances_factory()
+        super(IScope, self).__init__(self.instances)
+        self.update(dict(*args, **kwargs))
+
+    def __str__(self):
+        return self.name
+
+    def __key_transform__(self, key):
+        return key
+
+    class _key(str):
+        pass
+
+    def _key_factory(self, key):
+        if not isinstance(key, self._key):
+            key = self.__key_transform__(key)
+            key = self._key(key)
+        return key
+
+    def __contains__(self, key):
+        key = self._key_factory(key)
+        return super(IScope, self).__contains__(key)
+
+    def __getitem__(self, key):
+        key = self._key_factory(key)
+        return super(IScope, self).__getitem__(key)
+
+    def __setitem__(self, key, value):
+        key = self._key_factory(key)
+        super(IScope, self).__setitem__(key, value)
+
+    def __delitem__(self, key):
+        key = self._key_factory(key)
+        super(IScope, self).__delitem__(key)
+
+
+class NoneScope(IScope):
+    register = True
+    name = 'none'
+
+    def __setitem__(self, key, value):
+        return
+
+
+class GlobalScope(IScope):
+    register = True
+    name = 'global'
+
+
+class SingletonScope(GlobalScope):
+    """ Alias for GlobalScope
+    """
+
+
+class ProcessScope(IScope):
+    register = True
+    name = 'process'
+
+    def __key_transform__(self, key):
+        return '%s_%s' % (os.getpid(), key)
+
+
+class ThreadScope(IScope):
+    register = True
+    name = 'thread'
+
+    def __init__(self, *args, **kwargs):
+        self._thread_local = threading.local()
+        super(ThreadScope, self).__init__(*args, **kwargs)
+
+    def instances_factory(self):
+        if not hasattr(self._thread_local, 'instances'):
+            self._thread_local.instances = dict()
+        return self._thread_local.instances
+
+
+class ProxyScope(IScope):
+
+    def __init__(self, scope, *args, **kwargs):
+        self.instances = scope
+        super(ProxyScope, self).__init__(*args, **kwargs)
+
+
+class NamespacedProxyScope(ProxyScope):
+
+    def __init__(self, namespace, scope, *args, **kwargs):
+        self.namespace = namespace
+        super(NamespacedProxyScope, self).__init__(scope, *args, **kwargs)
+
+    @property
+    def name(self):
+        return self.namespace
+
+    def __key_transform__(self, key):
+        return '%s__%s' % (self.namespace, key)
+
+
+class ContextScope(IScope):
+    register = True
+    name = 'context'
+
+    def __init__(self, *args, **kwargs):
+        self.stack = []
+
+        super(ContextScope, self).__init__(*args, **kwargs)
+
+        self.store = self.instances
+
+    ## TODO ChainMap
+
+    def _set_instances(self):
+        stack = tuple(self.stack)
+
+        if stack not in self.store:
+            self.store[stack] = self.instances_factory()
+
+        self.instances = self.store[stack]
+        self._set_mapping(self.instances)
+
+    def __enter__(self, context=_sentinel):
+        if context is _sentinel:
+            context = object()
+
+        self.stack.append(context)
+        self._set_instances()
+
+    def __exit__(self, type, value, traceback):
+        self.stack.pop()
+        self._set_instances()
+
+
+SCOPE_FACTORIES = {}
+
+
+class ScopeRegistry(ProxyMutableMapping):
+    _factories = SCOPE_FACTORIES
+
+    def __init__(self):
+        super(ScopeRegistry, self).__init__(self._factories)
+        self._build()
+
+    def _build(self):
+        classes = IScope.__subclasses__()
+        classes = filter(lambda x: x.register, classes)
+        list(map(self.register_factory, classes))
+
+    def register_factory(self, factory, name=None):
+        if name is None:
+            # name = str(factory)
+            name = getattr(factory, 'name', None)
+        if name:
+            self._factories[name] = factory
+        self._factories[factory] = factory
+
+    def resolve(self, scope_or_scope_factory, instantiate_factory=True):
+        if self.is_scope_instance(scope_or_scope_factory):
+            instance = scope_or_scope_factory
+            return instance
+
+        elif self.is_scope_factory(scope_or_scope_factory):
+            factory = scope_or_scope_factory
+            if not instantiate_factory:
+                return factory
+            instance = factory()
+            return instance
+
+        elif scope_or_scope_factory in self._factories:
+            factory = self._factories[scope_or_scope_factory]
+            return self.resolve(factory)
+
+        else:
+            raise KeyError("Scope %s is not known" % scope_or_scope_factory)
+
+    _scope_type = collections.MutableMapping
+
+    @classmethod
+    def is_scope_factory(cls, obj):
+        return callable(obj) and issubclass(obj, cls._scope_type)
+
+    @classmethod
+    def is_scope_instance(cls, obj):
+        return isinstance(obj, cls._scope_type)
+
+    @classmethod
+    def is_scope(cls, obj):
+        return cls.is_scope_factory(obj) or cls.is_scope_instance(obj)
