8b9c00ae2850fbd4bb76a2a4d9c6f49980e26e01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:35 +0000

    Add package fpipe/0.0.8/fpipe-0.0.8-py3-none-any.whl

---
+++ packages/fpipe/fpipe-0.0.8-py3-none-any.whl/fpipe/generators/ftp.py
@@ -0,0 +1,35 @@
+from typing import Iterable
+
+from fpipe.file.file import Path
+from fpipe.utils import FTPClient
+from fpipe.file import FileStream, File, FileStreamGenerator, FileMeta
+
+
+class FTPFile(File):
+    def __init__(self, path: str, host: str, username: str, password: str, port: int, block_size: int = 2 ** 23):
+        super().__init__(meta=[Path(path)])
+        self.path = path
+        self.host = host
+        self.username = username
+        self.password = password
+        self.port = port
+        self.block_size = block_size
+
+
+class FTPFileGenerator(FileStreamGenerator):
+    def __init__(self, files: Iterable[File]):
+        super().__init__(files)
+
+    def __iter__(self) -> Iterable[FileStream]:
+        for source in self.files:
+
+            ftp_client = FTPClient(host=source.host,
+                                   username=source.username,
+                                   password=source.password,
+                                   blocksize=source.block_size,
+                                   port=source.port)
+            try:
+                ftp_client.write_to_file_threaded(source.meta(Path).value)
+                yield FileStream(ftp_client.bytes_io, parent=source)
+            except Exception as e:
+                raise e
